80ff44f3d6bab17ebfbfc878a68594c246246d3a
Brandon Martin-Anderson <badhill@gmail.com>
2009-09-16 00:54:23 +0000

    Added the interface Locatable and SpatialVertex, which implements Locatable.

---
+++ jags/src/main/java/org/opentripplanner/jags/core/SpatialVertex.java
@@ -0,0 +1,16 @@
+package org.opentripplanner.jags.core;
+
+public class SpatialVertex extends Vertex implements Locatable {
+	private static final long serialVersionUID = 7784891800734704432L;
+	double x,y;
+	
+	public SpatialVertex(String label, double x, double y) {
+		super(label);
+		this.x = x;
+		this.y = y;
+	}
+
+	public double distance(double x, double y) {
+		return Math.pow((Math.pow(this.x-x,2)+Math.pow(this.y-y,2)),0.5);
+	}
+}
