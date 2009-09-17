6c79672e463533bc46b19a286cecf50d6700e0fe
Brandon Martin-Anderson <badhill@gmail.com>
2009-09-17 07:56:38 +0000

    A toe into using Hibernate to persist really really big transit feeds during use

---
+++ jags/src/main/java/org/opentripplanner/jags/gtfs/db/GTFSDB.java
@@ -0,0 +1,40 @@
+package org.opentripplanner.jags.gtfs.db;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.opentripplanner.jags.gtfs.Feed;
+import org.opentripplanner.jags.gtfs.Stop;
+
+public class GTFSDB {
+	SessionFactory sessionFactory;
+	
+	public GTFSDB() {
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+	}
+	
+	public void store(Feed feed) throws SecurityException, IllegalArgumentException, IOException, NoSuchFieldException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+		feed.loadStops();
+		
+		Session session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+		
+		for( Stop stop : feed.getAllStops()) {
+			System.out.println( stop );
+			session.save(stop);
+		}
+		
+		session.getTransaction().commit();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Feed feed = new Feed( "caltrain_gtfs.zip" );
+		
+		GTFSDB gtfsdb = new GTFSDB();
+		gtfsdb.store(feed);
+	}
+}
