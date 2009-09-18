aa57aed6741f5ed14c07396e568ff37b77ef2fe2
Brandon Martin-Anderson <badhill@gmail.com>
2009-09-18 00:32:46 +0000

    Simplified GTFSTime

--- jags/src/main/java/org/opentripplanner/jags/gtfs/types/GTFSTime.java
+++ jags/src/main/java/org/opentripplanner/jags/gtfs/types/GTFSTime.java
@@ -1,9 +1,6 @@
 package org.opentripplanner.jags.gtfs.types;
 
 public class GTFSTime {
-	int hour;
-	int minute;
-	int second;
 	int time;
 	
 	public GTFSTime() {
@@ -12,42 +9,18 @@ public class GTFSTime {
 	
 	public GTFSTime(String in) {
 		String[] components = in.split(":");
-		hour = Integer.parseInt( components[0] );
-		minute = Integer.parseInt( components[1] );
-		second = Integer.parseInt( components[2] );
+		int hour = Integer.parseInt( components[0] );
+		int minute = Integer.parseInt( components[1] );
+		int second = Integer.parseInt( components[2] );
 		time = hour*3600+minute*60+second;
 	}
 	
 	public int getSecondsSinceMidnight() {
-		return hour*3600+minute*60+second;
+		return time;
 	}
 	
 	public String toString() {
-		return hour+":"+minute+":"+second;
-	}
-	
-	public int getHour() {
-		return hour;
-	}
-
-	public void setHour(int hour) {
-		this.hour = hour;
-	}
-
-	public int getMinute() {
-		return minute;
-	}
-
-	public void setMinute(int minute) {
-		this.minute = minute;
-	}
-
-	public int getSecond() {
-		return second;
-	}
-
-	public void setSecond(int second) {
-		this.second = second;
+		return time/3600+":"+(time%3600)/60+":"+time%60;
 	}
 	
 	public int getTime() {
