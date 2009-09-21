c6130fe649be5164445faa4d6b5bca501192e152
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2009-09-21 19:07:42 +0000

    Fix TestKao by creating a new feed before loading it into the graph (this test was erroring with a null pointer exception because the stopTable iterator had run its course from the first part of the test and thus no stops were actually being loaded, causing the second part of the test to fail)

--- jags/src/test/java/org/opentripplanner/jags/test/TestKao.java
+++ jags/src/test/java/org/opentripplanner/jags/test/TestKao.java
@@ -37,8 +37,9 @@ public class TestKao extends TestCase {
 		
 		assertTrue( ((Hop)path.get(path.size()-1).payload).end.arrival_time.getSecondsSinceMidnight()==48540 );
 		
+		Feed feed2 = new Feed(new PackagedFeed( "caltrain_gtfs.zip" ));
 		Graph gg = new Graph();
-		GTFSHopLoader h2 = new GTFSHopLoader(gg,feed);
+		GTFSHopLoader h2 = new GTFSHopLoader(gg,feed2);
 		h2.load();
 		ShortestPathTree spt = org.opentripplanner.jags.algorithm.Dijkstra.getShortestPathTree(gg, 
 											   "Millbrae Caltrain", 
