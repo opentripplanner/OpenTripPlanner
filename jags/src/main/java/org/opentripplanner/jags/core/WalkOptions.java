6b6eb473f6581ca40fe108d50bffa67e79d639f0
David Turner <novalis@novalis.org>
2009-10-06 17:17:05 +0000

    add bike option

--- jags/src/main/java/org/opentripplanner/jags/core/WalkOptions.java
+++ jags/src/main/java/org/opentripplanner/jags/core/WalkOptions.java
@@ -1,8 +1,10 @@
 package org.opentripplanner.jags.core;
 public class WalkOptions {
     public double speed;
+    public boolean bicycle;
     
     public WalkOptions() {
         this.speed = 0.85;
+        this.bicycle = false;
     }
 }
