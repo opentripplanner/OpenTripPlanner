ff1f7ddfb00d2dab6bf0d65644171c2a913c85ee
David Turner <novalis@novalis.org>
2009-11-16 20:00:42 +0000

    document how to get data for shapefile loader

--- opentripplanner-routing/src/test/java/org/opentripplanner/routing/edgetype/loader/TestShapefileLoader.java
+++ opentripplanner-routing/src/test/java/org/opentripplanner/routing/edgetype/loader/TestShapefileLoader.java
@@ -30,8 +30,28 @@ public class TestShapefileLoader extends TestCase {
     
     public void testBasic() throws Exception {
         Graph gg = new Graph();
+    
         File file = new File("src/test/resources/nyc_streets/streets.shp");
 
+        if (!file.exists()) {
+            System.out.println("No New York City basemap; skipping; see comment here for details");
+            /* This test requires the New York City base map, available at:
+             * http://www.nyc.gov/html/dcp/html/bytes/dwnlion.shtml
+             * Download the MapInfo file.  This must be converted to a ShapeFile.
+             * unzip nyc_lion09ami.zip
+             * ogr2ogr -f 'ESRI Shapefile' nyc_streets/streets.shp lion/MNLION1.tab
+             * ogr2ogr -update -append -f 'ESRI Shapefile'  nyc_streets lion/SILION1.tab -nln streets
+             * ogr2ogr -update -append -f 'ESRI Shapefile'  nyc_streets lion/QNLION1.tab -nln streets
+             * ogr2ogr -update -append -f 'ESRI Shapefile'  nyc_streets lion/BKLION1.tab -nln streets
+             * ogr2ogr -update -append -f 'ESRI Shapefile'  nyc_streets lion/BXLION1.tab -nln streets
+             * 
+             * It also requires the NYC Subway data in GTFS:
+             * cd src/test/resources
+             * wget http://data.topplabs.org/data/mta_nyct_subway/subway.zip
+             */
+            return;
+        }
+        
         ShapefileDataStore dataStore = new ShapefileDataStore(file.toURI().toURL());
 
         String typeNames[] = dataStore.getTypeNames();
@@ -50,19 +70,17 @@ public class TestShapefileLoader extends TestCase {
 
         GTFSHopLoader hl = new GTFSHopLoader(gg, context);
         hl.load();
-        
+
         NetworkLinker nl = new NetworkLinker(gg);
         nl.createLinkage();
-                
+
         System.out.println("vertices: " + gg.getVertices().size());
         Vertex start = gg.getVertex("PARK PL at VANDERBILT AV");
         Vertex end = gg.getVertex("GRAND ST at LAFAYETTE ST");
-        //Vertex start = gg.getVertex("6 Ave at W 23rd St");
-        //Vertex end = gg.getVertex("5th Ave at E 81st St");
        
         assertNotNull(start);
         assertNotNull(end);
-                    
+
         ShortestPathTree spt = Dijkstra.getShortestPathTree(gg, 
                    start.getLabel(), 
                    end.getLabel(), 
@@ -71,13 +89,6 @@ public class TestShapefileLoader extends TestCase {
         GraphPath path = spt.getPath(end);
         
         assertNotNull(path);
-        SPTEdge street = null;
-        for (int i = 0; i < path.edges.size(); ++i) {
-        	street = path.edges.get(i);
-            System.out.println(street.payload.getName() + ": " + 
-            		street.payload.getDistance() + " from " + 
-            		street.fromv + " to " + street.tov);
-        }
-        System.out.println("arrive at: " + street.getToSPTVertex().state.getTime());                 
+
     }
 }
