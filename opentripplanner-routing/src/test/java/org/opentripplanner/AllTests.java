0e1d89a71876ba3c10e29352a2bc1bc052463b4c
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2009-11-24 22:41:52 +0000

    Remove outdated test and add new test to AllTests suite.

--- opentripplanner-routing/src/test/java/org/opentripplanner/AllTests.java
+++ opentripplanner-routing/src/test/java/org/opentripplanner/AllTests.java
@@ -5,7 +5,7 @@ import org.opentripplanner.routing.TestHalfEdges;
 import org.opentripplanner.routing.algorithm.TestDijkstra;
 import org.opentripplanner.routing.core.TestGraph;
 import org.opentripplanner.routing.edgetype.TestStreet;
-import org.opentripplanner.routing.edgetype.factory.TestHopFactory;
+import org.opentripplanner.routing.edgetype.loader.TestPatternHopLoader;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -17,7 +17,7 @@ public class AllTests {
         // $JUnit-BEGIN$
         suite.addTestSuite(TestDijkstra.class);
         suite.addTestSuite(TestGraph.class);
-        suite.addTestSuite(TestHopFactory.class);
+        suite.addTestSuite(TestPatternHopLoader.class);
         suite.addTestSuite(TestHalfEdges.class);
         suite.addTestSuite(TestNarrativeGenerator.class);
         suite.addTestSuite(TestStreet.class);
