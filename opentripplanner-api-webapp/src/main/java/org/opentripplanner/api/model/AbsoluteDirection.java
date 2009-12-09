57f67d27650a4b37f8756c984990a15ff8a4a97f
David Turner <novalis@novalis.org>
2009-12-09 22:09:32 +0000

    oops, forgot this one

---
+++ opentripplanner-api-webapp/src/main/java/org/opentripplanner/api/model/AbsoluteDirection.java
@@ -0,0 +1,8 @@
+package org.opentripplanner.api.model;
+
+/** 
+ * An absolute cardinal or intermediate direction 
+ */
+public enum AbsoluteDirection {
+    NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST
+}
