00b28bc7363eddee27f41bfa5b8d29936c635311
David Turner <novalis@novalis.org>
2009-12-14 18:26:06 +0000

    fix license duplication

--- opentripplanner-api-webapp/src/main/java/org/opentripplanner/api/model/Place.java
+++ opentripplanner-api-webapp/src/main/java/org/opentripplanner/api/model/Place.java
@@ -1,29 +1,28 @@
 /* This program is free software: you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation, either version 3 of
-   the License, or (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-*/
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
 
-package org.opentripplanner.api.model;
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-import java.util.logging.Logger;
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-import javax.xml.bind.annotation.XmlElement;
+package org.opentripplanner.api.model; 
 
-import org.opentripplanner.util.Constants;
-/**
- *
- */
+import java.util.logging.Logger; 
+
+import javax.xml.bind.annotation.XmlElement; 
+
+import org.opentripplanner.util.Constants; 
+
+/** 
+* 
+*/ 
 public class Place {
-
     protected static final Logger LOGGER = Logger.getLogger(Place.class.getCanonicalName());
 
     public String name = null;
