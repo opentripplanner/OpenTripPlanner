daa0c4ca9d1e32d772c27821f977cda91838e389
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2010-01-07 16:38:11 +0000

    Spanish localization for webapp contributed by Francisco José Peñarrubia

---
+++ opentripplanner-webapp/src/main/webapp/js/otp/locale/Spanish.js
@@ -0,0 +1,164 @@
+/* This program is free software: you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+otp.namespace("otp.locale");
+
+/**
+  * @class
+  */
+otp.locale.Spanish = {
+
+    contextMenu : 
+    {
+        fromHere         : "Salir desde aquí",
+        toHere           : "Llegar hasta aquí",
+
+        centerHere       : "Centrar mapa aquí",
+        zoomInHere       : "Acercar",
+        zoomOutHere      : "Alejar",
+        previous         : "Último encuadre",
+        next             : "Siguiente encuadre"
+    },
+
+    service : 
+    {
+        weekdays:  "días de la semana",
+        saturday:  "Sábado",
+        sunday:    "Domingo",
+        schedule:  "Horario"
+    },
+
+    indicators : 
+    {
+        ok         : "OK",
+        date       : "Fecha",
+        loading    : "Cargando",
+        searching  : "Buscando...",
+        qEmptyText : "Dirección, intersección,  punto de inteés o Identificador de Parada..."
+        	
+    },
+
+    buttons: 
+    {
+        reverse       : "Cambiar",
+        reverseTip    : "<b>Camibar orígen-destino</b><br/>Plan a return trip by reversing this trip's start and end points, and adjusting the time forward.",
+        reverseMiniTip: "Cambiar orígen-destino",
+
+        clear         : "Inicializar",
+        clearTip      : "<b>Inicializar</b><br/>Inicializar el mapa y los botones activos.",
+
+        fullScreen    : "Pantalla completa",
+        fullScreenTip : "<b>Pantalla completa</b><br/>Mostrar - ocultar paneles",
+        
+        print         : "Imprimir",
+        printTip      : "<b>Imprimir</b><br/>Imprimir este plan de viaje junto con las paradas.",
+        
+        feedback      : "Feedback",
+        feedbackTip   : "<b>Feedback</b><br/>Send your thoughts or experiences with the map",
+
+        submit       : "Enviar",
+        clear        : "Borrar",
+        ok           : "OK",
+        cancel       : "Cancelar",
+        yes          : "Sí",
+        no           : "No"
+    },
+
+    // note: keep these lower case (and uppercase via template / code if needed)
+    directions : 
+    {
+        southEast: "sureste",
+        southWest: "sudoeste",
+        northEast: "nordeste",
+        northWest: "noroeste",
+        north:     "norte",
+        west:      "oeste",
+        south:     "sur",
+        east:      "este",
+        bound:     "límite"
+    },
+
+    tripPlanner :
+    {
+        labels : 
+        {
+            panelTitle    : "Planificador de viajes Autobuses Interurbanos",
+            tabTitle      : "Planificar un Viaje",
+            inputTitle    : "Detalles del Viaje",
+            optTitle      : "Preferencias (opcional)",
+            submitMsg     : "Planificando su Viaje...",
+            optionalTitle : "",
+            date          : "Fecha",
+            time          : "Hora",
+            when          : "Tiempo",
+            from          : "Desde",
+            fromHere      : "Desde aquí",
+            to            : "Hasta",
+            toHere        : "Hasta aquí",
+            minimize      : "Mostrar el",
+            maxWalkDistance: "Máxima distancia hasta la parada",
+            arriveDepart  : "Llegada/Salida a",
+            mode          : "Modo de viaje",
+            go            : "Empezar",
+            planTrip      : "Planifique su Viaje",
+            newTrip       : "Nuevo Viaje"
+        },
+
+        error:
+        {
+            title        : 'Trip Planner Error',
+            deadMsg      : "Map Trip Planner is currently not responding. Please wait a few minutes to try again, or try the text trip planner (see link below).",
+            geoFromMsg   : "Por favor, seleccione la posición de salida de su Viaje: ",
+            geoToMsg     : "Por favor, seleccione la posición de llegada de su Viaje: "
+        },
+
+        options: 
+        [
+          ['TRANSFERS', 'Mínimo número de Transbordos'],
+          ['QUICK', 'Viaje más corto'] 
+        ],
+    
+        arriveDepart: 
+        [
+          ['false', 'Salida'], 
+          ['true', 'Llegada']
+        ],
+    
+        maxWalkDistance : 
+        [
+            ['500',   '500 metros'],
+            ['1000',   '1 Km'],
+            ['5000',   '5 Km'],
+            ['10000',  '10 Km'],
+            ['20000',  '20 Km']
+        ],
+    
+        mode : 
+        [
+            ['BUSISH,TRAINISH,WALK', 'Bus y Tren'],
+            ['BUSISH,WALK', 'Solo Bus'],
+            ['TRAINISH,WALK', 'Solo Tren'],
+            ['WALK', 'Solo a pié'],
+            ['BICYCLE', 'Bicicleta']
+        ],
+
+        wheelchair :
+        [
+            ['false', 'No se requiere un viaje con accesibilidad'],
+            ['true', 'Se requiere un viaje adaptado a Silla de Ruedas']
+        ]
+    },
+
+    CLASS_NAME : "otp.locale.Spanish"
+};
