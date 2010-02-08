fc43bb1b19983874a722fcd9c2d762bb67e4f509
Zsombor Welker <flaktack@welker.hu>
2010-02-08 20:07:36 +0000

    Modify OSM vertex unification code to handle one-way streets better.
    One-way streets which forked into two (one-way) streets (sum of outgoing edges
    == 2) in some circumstance had two of the three vertices unified,
    with the third left dangling (inaccessible).

--- opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/osm/OpenStreetMapGraphBuilderImpl.java
+++ opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/osm/OpenStreetMapGraphBuilderImpl.java
@@ -190,15 +190,45 @@ public class OpenStreetMapGraphBuilderImpl implements GraphBuilder {
                     if (outEdges != null) {
                         /* If this is not an intersection or street name change, unify the vertices */
                         boolean unified = false;
-                        if (outEdges.size() == 2) {
-                            for (Edge out : outEdges) {
-                                Vertex fromVertex = out.getFromVertex();
-                                if (tov != fromVertex && out.getName() == in.getName()) {
-                                    Intersection v = (Intersection) tov;
-                                    v.mergeFrom(graph, (Intersection) fromVertex);
-                                    graph.removeVertex(fromVertex);
-                                    unified = true;
-                                    break;
+
+                        /*
+                         * Some physical (oneway) streets may be represented by one edge only
+                         * (since no parallel/coincident edge is needed) in which case the vertices should
+                         * still be unified. (This also results in lone vertices at the end of ways being unified,
+                         * but that doesn't hurt anyone.)
+                         */
+                        if (outEdges.size() == 1) {
+                            Edge out = outEdges.get(0);
+                            Vertex fromVertex = out.getFromVertex();
+                            if (tov != fromVertex && out.getName() == in.getName()) {
+                                Intersection v = (Intersection) tov;
+                                v.mergeFrom(graph, (Intersection) fromVertex);
+                                graph.removeVertex(fromVertex);
+                                unified = true;
+                            }
+                        }
+                        else if (outEdges.size() == 2) {
+                            /*
+                             * Checking only the number of edges leaving a certain point isn't enough
+                             * to determine whether there is an intersection:
+                             * If a (non-pedestrian) oneway street forks into two oneway streets, than there
+                             * are two edges leaving the intersection, but unifying the vertices is not the
+                             * right thing to do. (And the name might not even change, if the edges are
+                             * iterated over in a suitable manner)
+                             *
+                             * So, for unification to ensue, it also has to be ensured that one of the
+                             * two outgoing edges is parallel/coincident to the examined edge.
+                             */
+                            if(outEdges.get(0).getToVertex() == in.getFromVertex() || outEdges.get(1).getToVertex() == in.getFromVertex()) {
+                                for (Edge out : outEdges) {
+                                    Vertex fromVertex = out.getFromVertex();
+                                    if (tov != fromVertex && out.getName() == in.getName()) {
+                                        Intersection v = (Intersection) tov;
+                                        v.mergeFrom(graph, (Intersection) fromVertex);
+                                        graph.removeVertex(fromVertex);
+                                        unified = true;
+                                        break;
+                                    }
                                 }
                             }
                         }
