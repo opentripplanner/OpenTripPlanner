c1a0e38d5e0a5235a0f2ef93857982ee14dcf5f5
David Turner <novalis@novalis.org>
2010-04-13 19:05:23 +0000

    javadocify comment

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/core/StreetIntersectionVertex.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/core/StreetIntersectionVertex.java
@@ -13,7 +13,7 @@
 
 package org.opentripplanner.routing.core;
 
-/* a marker interface for all vertices that can be at the end of streets */
+/** A marker interface for all vertices that can be at the end of streets */
 public interface StreetIntersectionVertex {
 
 }
