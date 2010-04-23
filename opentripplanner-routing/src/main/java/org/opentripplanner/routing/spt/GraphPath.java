8dcd9bacfb316d00fd60abc2ed3e494d413410c4
David Turner <novalis@novalis.org>
2010-04-23 19:45:20 +0000

    handle cases where bike walking starts mid-block

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/spt/GraphPath.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/spt/GraphPath.java
@@ -102,8 +102,12 @@ public class GraphPath {
         }
         TraverseOptions options = vertices.lastElement().options;
         ListIterator<SPTEdge> iterator = edges.listIterator(vertices.size() - 1);
+        SPTEdge firstEdge = edges.get(0);
         while (iterator.hasPrevious()) {
             SPTEdge edge = iterator.previous();
+            if (edge == firstEdge) {
+                state.lastEdgeWasStreet = false;
+            }
             TraverseResult result = edge.payload.traverseBack(state, options);
             assert (result != null);
             state = result.state;
