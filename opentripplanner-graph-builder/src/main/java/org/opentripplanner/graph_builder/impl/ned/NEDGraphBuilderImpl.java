b0b0a1ecd6c97c4ad06635e9128e1d40e367a9d2
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2010-04-26 20:05:49 +0000

    use standard distance function; clarify var names and other cleanup (refs #163)

--- opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/ned/NEDGraphBuilderImpl.java
+++ opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/ned/NEDGraphBuilderImpl.java
@@ -13,7 +13,6 @@
 
 package org.opentripplanner.graph_builder.impl.ned;
 
-import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -30,6 +29,7 @@ import org.opentripplanner.routing.core.Edge;
 import org.opentripplanner.routing.core.Graph;
 import org.opentripplanner.routing.core.Vertex;
 import org.opentripplanner.routing.edgetype.Street;
+import org.opentripplanner.routing.impl.DistanceLibrary;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
@@ -52,17 +52,17 @@ public class NEDGraphBuilderImpl implements GraphBuilder {
     private Coverage coverage;
 
     /**
-     * The sampling frequency in meters. Defaults to 10m, the approximate resolution of 1/3 arc-second
-     * NED data.
+     * The distance between samples in meters. Defaults to 10m, the approximate resolution of 1/3
+     * arc-second NED data.
      */
-    private double sampleFreqM = 10;
+    private double distanceBetweenSamplesM = 10;
 
     public void setGridCoverageFactory(NEDGridCoverageFactory factory) {
         gridCoverageFactory = factory;
     }
 
-    public void setSampleFrequency(double freq) {
-        sampleFreqM = freq;
+    public void setDistanceBetweenSamplesM(double distance) {
+        distanceBetweenSamplesM = distance;
     }
 
     @Override
@@ -99,36 +99,34 @@ public class NEDGraphBuilderImpl implements GraphBuilder {
         
         List<Coordinate> coordList = new LinkedList<Coordinate>();
 
-        // calculate the total edge length in m and degrees
-        double edgeLenD=0, edgeLenM = 0;
+        // calculate the total edge length in meters
+        double edgeLenM = 0;
         for (int i = 0; i < coords.length-1; i++) {
-            edgeLenD += Point2D.distance(coords[i].x, coords[i].y, coords[i+1].x, coords[i+1].y);
-            edgeLenM += haversine(coords[i].x, coords[i].y, coords[i+1].x, coords[i+1].y);
+            edgeLenM += DistanceLibrary.distance(coords[i].y, coords[i].x, coords[i+1].y, coords[i+1].x);
         }
         
         // initial sample (x = 0)
-        coordList.add(new Coordinate(0, getElevation(coords[0])));            
+        coordList.add(new Coordinate(0, getElevation(coords[0])));
 
         // loop for edge-internal samples
-        for(double x = sampleFreqM; x < edgeLenM; x += sampleFreqM) {
-            // avoid final-segment samples less than half the frequency length:
-            if(edgeLenM-x < sampleFreqM/2) continue;
+        for (double x = distanceBetweenSamplesM; x < edgeLenM; x += distanceBetweenSamplesM) {
+            // avoid final-segment samples less than half the distance between samples:
+            if (edgeLenM-x < distanceBetweenSamplesM/2) {
+                break;
+            }
             
-            Coordinate internal = getPointAlongEdge(coords, edgeLenD, x/edgeLenM);
-            coordList.add(new Coordinate(x, getElevation(internal)));            
+            Coordinate internal = getPointAlongEdge(coords, edgeLenM, x/edgeLenM);
+            coordList.add(new Coordinate(x, getElevation(internal)));    
         }
 
         // final sample (x = edge length)
-        //System.out.println("final sample x="+edgeLenM+" coord="+coords[coords.length-1]);
         coordList.add(new Coordinate(edgeLenM, getElevation(coords[coords.length-1])));            
 
         // construct the PCS
         Coordinate coordArr[] = new Coordinate[coordList.size()];
         PackedCoordinateSequence elevPCS = new PackedCoordinateSequence.Double(coordList
                 .toArray(coordArr));
-
-        //for (Coordinate c : elevPCS.toCoordinateArray()) System.out.println(" "+c.toString());
-
+        
         st.setElevationProfile(elevPCS);
             
     }
@@ -149,13 +147,13 @@ public class NEDGraphBuilderImpl implements GraphBuilder {
         // endpoints of current segment within edge:
         double x1 = coords[0].x, y1 = coords[0].y, x2, y2;
         
-        for(int i = 1; i < coords.length-1; i++) { //loop through inner points
+        for (int i = 1; i < coords.length-1; i++) { //loop through inner points
           Coordinate innerPt = coords[i];
           x2 = innerPt.x;
           y2 = innerPt.y;
           
-          // percentage of total edge length represented by current sigment:
-          double pct = Point2D.distance(x1, y1, x2, y2) / length;
+          // percentage of total edge length represented by current segment:
+          double pct = DistanceLibrary.distance(y1, x1, y2, x2) / length;
 
           if (pctThrough + pct > t) { // if current segment contains 't,' we're done
             double pctAlongSeg = (t - pctThrough) / pct;
@@ -171,37 +169,15 @@ public class NEDGraphBuilderImpl implements GraphBuilder {
         x2 = coords[coords.length-1].x;
         y2 = coords[coords.length-1].y;
 
-        double pct = Point2D.distance(x1, y1, x2, y2) / length;
+        double pct = DistanceLibrary.distance(y1, x1, y2, x2) / length;
         double pctAlongSeg = (t - pctThrough)/pct;
         
         return new Coordinate(x1 + (pctAlongSeg * (x2 - x1)), y1 + (pctAlongSeg * (y2 - y1)));
       }
 
-
-    /**
-     * Uses the haversine formula to calculate the distance in meters between two lat/lon
-     * coordinates.
-     * 
-     * @param lon1 The start longitude in decimal degrees
-     * @param lat1 The start latitude in decimal degrees
-     * @param lon2 The end longitude in decimal degrees
-     * @param lat2 The end latitude in decimal degrees
-     * @return the length in meters along the earth's surface
-     */
-    private double haversine(double lon1, double lat1, double lon2, double lat2) {
-        double r = 6378137;
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLon = Math.toRadians(lon2-lon1); 
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * 
-                Math.sin(dLon/2) * Math.sin(dLon/2); 
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        return r * c;
-    }
-
    
     /**
-     * Method for retreiving the elevation at a given Coordinate.
+     * Method for retrieving the elevation at a given Coordinate.
      * 
      * @param c the coordinate (NAD83)
      * @return elevation in meters
@@ -211,7 +187,7 @@ public class NEDGraphBuilderImpl implements GraphBuilder {
     }
     
     /**
-     * Method for retreiving the elevation at a given (x, y) pair.
+     * Method for retrieving the elevation at a given (x, y) pair.
      * 
      * @param x the query longitude (NAD83)
      * @param y the query latitude (NAD83)
