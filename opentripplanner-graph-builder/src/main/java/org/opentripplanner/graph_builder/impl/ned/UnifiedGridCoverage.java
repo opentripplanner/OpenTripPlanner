4b6d9e5afca25515e6da4337d60e3eb2fde88164
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2010-04-30 18:24:22 +0000

    Hacky fix for slopped streets issue (fixes #163)

--- opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/ned/UnifiedGridCoverage.java
+++ opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/ned/UnifiedGridCoverage.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 
 import org.geotools.coverage.AbstractCoverage;
 import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.geometry.DirectPosition2D;
 import org.opengis.coverage.CannotEvaluateException;
 import org.opengis.coverage.Coverage;
 import org.opengis.coverage.PointOutsideCoverageException;
@@ -28,6 +29,9 @@ public class UnifiedGridCoverage extends AbstractCoverage {
 
     private static final long serialVersionUID = -7798801307087575896L;
 
+    // Used in the fix for #163. See comments below.
+    private static final double OFFSET_HACK = 0.0001;
+    
     private ArrayList<Coverage> regions;
 
     protected UnifiedGridCoverage(CharSequence name, Coverage coverage) {
@@ -43,7 +47,22 @@ public class UnifiedGridCoverage extends AbstractCoverage {
         return null;
     }
 
-    public double[] evaluate(DirectPosition point, double[] values)
+    /*
+     * This is a big hack to get around the issues faced in #163:
+     * http://opentripplanner.org/ticket/163
+     * 
+     * See http://osgeo-org.1803224.n2.nabble.com/Reading-tiff-files-td4970295.html#a4970295 and the
+     * previous ticket for details on the issues here.
+     * 
+     * The workaround until the bug is fixed in GeoTools or another solution is found is: 
+     *  * Try to evaluate the point
+     *  * If the point is found, return the value
+     *  * Otherwise, offset the point slightly and try again
+     *  * Return the result
+     *  
+     *  Dumb, but it appears to work.
+     */
+    public double[] evaluate_orig(DirectPosition point, double[] values)
             throws PointOutsideCoverageException, CannotEvaluateException {
         for (Coverage region : regions) {
             double[] result;
@@ -55,10 +74,22 @@ public class UnifiedGridCoverage extends AbstractCoverage {
             return result;
         }
         /* not found */
-
-        return values;
+        return null;
     }
+    
+    public double[] evaluate(DirectPosition point, double[] values)
+            throws PointOutsideCoverageException, CannotEvaluateException {
 
+        double[] result = evaluate_orig(point, values);
+        if (result == null) {
+            // If at first you don't succeed...
+            result = evaluate_orig(new DirectPosition2D(
+                    point.getCoordinate()[0] + OFFSET_HACK, 
+                    point.getCoordinate()[1] + OFFSET_HACK), values);
+        }
+        return result;
+    }
+    
     @Override
     public int getNumSampleDimensions() {
         return regions.get(0).getNumSampleDimensions();
