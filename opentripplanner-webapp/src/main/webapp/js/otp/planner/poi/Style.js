dceabef6037bdb7bfe5f9b4b19125bc61f591ba0
Frank Purcell <PurcellF@tri-met.org>
2010-10-21 21:23:16 +0000

    image fixes start/end offset fix

--- opentripplanner-webapp/src/main/webapp/js/otp/planner/poi/Style.js
+++ opentripplanner-webapp/src/main/webapp/js/otp/planner/poi/Style.js
@@ -19,30 +19,30 @@ otp.namespace("otp.planner.poi");
  */
 otp.planner.poi.Style = {
     green : {
-              strokeColor: "#00FF00",
-              strokeWidth: 3,
-              strokeDashstyle: "dashdot",
-              pointRadius: 6,
-              pointerEvents: "visiblePainted"
+        strokeColor: "#00FF00",
+        strokeWidth: 3,
+        strokeDashstyle: "dashdot",
+        pointRadius: 6,
+        pointerEvents: "visiblePainted"
     },
 
     fromTrip : {
-              graphicWidth:     20,
-              graphicHeight:    34,
-              graphicXOffset:  -10,
-              graphicYOffset:  -34,
-              externalGraphic: "images/map/trip/start.png",
-              cursor:          "pointer", 
-              fillOpacity:     "1.0"
+        graphicWidth:     21,
+        graphicHeight:    39,
+        graphicXOffset:  -11,
+        graphicYOffset:  -39,
+        externalGraphic: "images/map/trip/start.png",
+        cursor:          "pointer", 
+        fillOpacity:     "1.0"
     },
     toTrip : {
-              graphicWidth:     20,
-              graphicHeight:    34,
-              graphicXOffset:  -10,
-              graphicYOffset:  -34,
-              externalGraphic: "images/map/trip/end.png",
-              cursor:          "pointer", 
-              fillOpacity:     "1.0"
+        graphicWidth:     21,
+        graphicHeight:    39,
+        graphicXOffset:  -11,
+        graphicYOffset:  -39,
+        externalGraphic: "images/map/trip/end.png",
+        cursor:          "pointer", 
+        fillOpacity:     "1.0"
     },
 
     CLASS_NAME: "otp.planner.poi.Style"
