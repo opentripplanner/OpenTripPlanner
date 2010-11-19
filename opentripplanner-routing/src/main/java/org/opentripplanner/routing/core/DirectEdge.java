8974a3b97aae3ca524641e998724e274f2d6fd09
David Turner <novalis@novalis.org>
2010-11-19 23:37:16 +0000

    patch from bdferris to separate edge narratives from edges (stupid, stupid svn)

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/core/DirectEdge.java
@@ -0,0 +1,10 @@
+package org.opentripplanner.routing.core;
+
+public interface DirectEdge extends Edge, EdgeNarrative {
+
+}
+package org.opentripplanner.routing.core;
+
+public interface DirectEdge extends Edge, EdgeNarrative {
+
+}
