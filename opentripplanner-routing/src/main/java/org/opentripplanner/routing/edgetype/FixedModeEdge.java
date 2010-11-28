48536e7142f5a0ab3a473b5989fe20dbb9e69fd5
David Turner <novalis@novalis.org>
2010-11-28 20:35:01 +0000

    add missing class

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/FixedModeEdge.java
@@ -0,0 +1,62 @@
+package org.opentripplanner.routing.edgetype;
+
+import org.onebusaway.gtfs.model.Trip;
+import org.opentripplanner.routing.core.State;
+import org.opentripplanner.routing.core.EdgeNarrative;
+import org.opentripplanner.routing.core.TraverseMode;
+import org.opentripplanner.routing.core.Vertex;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+public class FixedModeEdge implements EdgeNarrative {
+    EdgeNarrative base;
+    TraverseMode mode;
+
+    public FixedModeEdge(EdgeNarrative base, TraverseMode mode) {
+	this.base = base;
+	this.mode = mode;
+    }
+
+    @Override
+    public String getDirection() {
+	return base.getDirection();
+    }
+
+    @Override
+    public double getDistance() {
+        return base.getDistance();
+    }
+
+    @Override
+    public Geometry getGeometry() {
+        return base.getGeometry();
+    }
+
+    @Override
+    public TraverseMode getMode() {
+        return mode;
+    }
+
+    @Override
+    public String getName() {
+        return base.getName();
+    }
+
+    @Override
+    public Trip getTrip() {
+        return base.getTrip();
+    }
+
+    public Vertex getFromVertex() {
+        return base.getFromVertex();
+    }
+
+    public Vertex getToVertex() {
+        return base.getToVertex();
+    }
+
+    @Override
+    public String getName(State state) {
+        return base.getName(state);
+    }
+}
