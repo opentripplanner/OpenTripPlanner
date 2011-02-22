927a8e39fad3d02e36c68fd0b05c2831304d253e
Brian Ferris <bdferris@onebusaway.org>
2011-02-22 01:40:44 +0000

    Remove unused code.

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/algorithm/AStar.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/algorithm/AStar.java
@@ -25,8 +25,6 @@ import org.opentripplanner.routing.core.DirectEdge;
 import org.opentripplanner.routing.core.Edge;
 import org.opentripplanner.routing.core.EdgeNarrative;
 import org.opentripplanner.routing.core.Graph;
-import org.opentripplanner.routing.core.GraphVertex;
-import org.opentripplanner.routing.core.HasEdges;
 import org.opentripplanner.routing.core.State;
 import org.opentripplanner.routing.core.TraverseOptions;
 import org.opentripplanner.routing.core.TraverseResult;
@@ -320,25 +318,6 @@ public class AStar {
 
             Collection<Edge> outgoing = GraphLibrary.getOutgoingEdges(graph, fromv, extraEdges);
 
-            if (fromv instanceof HasEdges) {
-                outgoing = extendEdges(outgoing, ((HasEdges) fromv).getOutgoing());
-            } else {
-                GraphVertex gv = graph.getGraphVertex(fromv);
-                if (gv != null)
-                    outgoing = extendEdges(outgoing, gv.getOutgoing());
-            }
-
-            if (extraEdges.containsKey(fromv))
-                outgoing = extendEdges(outgoing, extraEdges.get(fromv));
-
-            if (fromv instanceof StreetLocation) {
-                StreetLocation sl = (StreetLocation) fromv;
-                outgoing = extendEdges(outgoing, sl.getExtra());
-            }
-
-            if (outgoing == null)
-                outgoing = Collections.emptyList();
-
             for (Edge edge : outgoing) {
                 State state = spt_u.state;
 
@@ -385,24 +364,4 @@ public class AStar {
 
         return spt;
     }
-
-    private static <E extends Edge> Collection<Edge> extendEdges(Collection<Edge> existing,
-            Collection<E> additionalEdges) {
-
-        if (existing == null || existing.size() == 0) {
-            if (additionalEdges == null || additionalEdges.isEmpty())
-                return null;
-            return new ArrayList<Edge>(additionalEdges);
-        }
-
-        if (additionalEdges == null || additionalEdges.size() == 0)
-            return existing;
-
-        List<Edge> edges = new ArrayList<Edge>(existing.size() + additionalEdges.size());
-        edges.addAll(existing);
-        edges.addAll(additionalEdges);
-        return edges;
-    }
-
-
 }
