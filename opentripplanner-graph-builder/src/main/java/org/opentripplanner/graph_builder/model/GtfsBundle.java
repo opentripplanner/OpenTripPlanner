46bc4ed98b0e203b7529381e4276c6a1b2728c62
David Turner <novalis@novalis.org>
2011-03-22 17:51:31 +0000

    explain default agency id in gtfs builder (text by Brian Ferris)

--- opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/model/GtfsBundle.java
+++ opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/model/GtfsBundle.java
@@ -21,7 +21,7 @@ import java.util.Map;
 public class GtfsBundle {
 
     private File path;
-    
+
     private URL url;
 
     private String defaultAgencyId;
@@ -44,6 +44,17 @@ public class GtfsBundle {
         this.url = url;
     }
 
+    /**
+     * So that you can load multiple gtfs feeds into the same database / system without entity id
+     * collisions, everything has an agency id, including entities like stops, shapes, and service
+     * ids that don't explicitly have an agency id (as opposed to routes + trips + stop times).
+     * However, the spec doesn't currently have a method to specify which agency a stop
+     * should be assigned to in the case of multiple agencies being specified in the same feed.  
+     * Routes (and thus everything belonging to them) do have an agency id, but stops don't.
+     * The defaultAgencyId allows you to define which agency will be used as the default
+     * when figuring out which agency a stop should be assigned to (also applies to shapes + service
+     * ids as well). If not specified, the first agency in the agency list will be used.
+     */
     public String getDefaultAgencyId() {
         return defaultAgencyId;
     }
