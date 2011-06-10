3b0cb68cddd0b4a8bb4877dd44a380171a10738b
David Turner <novalis@novalis.org>
2011-06-10 20:10:59 +0000

    quickie test for transit index builder

---
+++ opentripplanner-graph-builder/src/test/java/org/opentripplanner/graph_builder/impl/transit_index/TransitIndexBuilderTest.java
@@ -0,0 +1,72 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package org.opentripplanner.graph_builder.impl.transit_index;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.opentripplanner.graph_builder.impl.GtfsGraphBuilderImpl;
+import org.opentripplanner.graph_builder.model.GtfsBundle;
+import org.opentripplanner.graph_builder.model.GtfsBundles;
+import org.opentripplanner.graph_builder.services.GraphBuilderWithGtfsDao;
+import org.opentripplanner.routing.core.Edge;
+import org.opentripplanner.routing.core.Graph;
+import org.opentripplanner.routing.edgetype.PreAlightEdge;
+import org.opentripplanner.routing.services.TransitIndexService;
+import org.opentripplanner.routing.transit_index.RouteVariant;
+
+public class TransitIndexBuilderTest extends TestCase {
+
+	public void testTransitIndexBuilder() throws IOException {
+		Graph graph = new Graph();
+
+		File testGtfs = new File("../opentripplanner-routing/src/test/resources/testagency.zip");
+		GtfsBundle bundle = new GtfsBundle();
+		bundle.setPath(testGtfs);
+		
+		GtfsBundles bundles = new GtfsBundles();
+		bundles.setBundles(Arrays.asList(bundle));
+
+		GtfsGraphBuilderImpl gtfsBuilder = new GtfsGraphBuilderImpl();
+		gtfsBuilder.setGtfsBundles(bundles);
+		
+		TransitIndexBuilder builder = new TransitIndexBuilder();
+		List<GraphBuilderWithGtfsDao> builders = new ArrayList<GraphBuilderWithGtfsDao>();
+		builders.add(builder);
+		gtfsBuilder.setGtfsGraphBuilders(builders);
+		
+		gtfsBuilder.buildGraph(graph);
+		
+		TransitIndexService index = graph.getService(TransitIndexService.class); 
+	
+		assertNotNull(index);
+		
+		Edge prealightEdge = index.getPrealightEdge(new AgencyAndId("agency", "A"));
+		assertTrue(prealightEdge instanceof PreAlightEdge);
+		
+		//route 18 is the only bidirectional route in the test data
+		List<RouteVariant> variantsForRoute = index.getVariantsForRoute(new AgencyAndId("agency", "18"));
+		assertEquals(2, variantsForRoute.size());
+		
+		variantsForRoute = index.getVariantsForRoute(new AgencyAndId("agency", "2"));
+		assertEquals(1, variantsForRoute.size());
+		
+	}
+}
