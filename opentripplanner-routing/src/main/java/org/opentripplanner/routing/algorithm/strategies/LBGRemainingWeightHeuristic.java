de2f9c57415abbdc30ffc5a923e559a663e10843
Andrew Byrd <andrew@fastmail.net>
2011-07-02 11:28:02 +0000

    Whitespace formatting

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/algorithm/strategies/LBGRemainingWeightHeuristic.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/algorithm/strategies/LBGRemainingWeightHeuristic.java
@@ -1,5 +1,7 @@
 package org.opentripplanner.routing.algorithm.strategies;
 
+import java.util.HashMap;
+
 import org.opentripplanner.routing.core.GenericVertex;
 import org.opentripplanner.routing.core.Graph;
 import org.opentripplanner.routing.core.LowerBoundGraph;
@@ -10,32 +12,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Intended primarily for testing of and experimentation with heuristics based on
- * the triangle inequality and metric embeddings.
+ * Intended primarily for testing of and experimentation with heuristics based on the triangle
+ * inequality and metric embeddings.
  * 
- * A heuristic that performs a single-source / all destinations shortest path search
- * in a weighted, directed graph whose shortest path metric is a lower bound on
- * path weight in our main, time-dependent graph. 
+ * A heuristic that performs a single-source / all destinations shortest path search in a weighted,
+ * directed graph whose shortest path metric is a lower bound on path weight in our main,
+ * time-dependent graph.
  * 
  * @author andrewbyrd
  */
 public class LBGRemainingWeightHeuristic implements RemainingWeightHeuristic {
-    private static Logger LOG = LoggerFactory.getLogger(LBGRemainingWeightHeuristic .class);
+    private static Logger LOG = LoggerFactory.getLogger(LBGRemainingWeightHeuristic.class);
 
-	LowerBoundGraph lbg;
-	Vertex target;
-	double[] weights;
-	
-	public LBGRemainingWeightHeuristic (Graph g) {
-		LOG.debug("BEGIN Making lower bound graph");
-		this.lbg = new LowerBoundGraph(g, LowerBoundGraph.OUTGOING);
-		LOG.debug("END   Making lower bound graph");
-	}
-	
+    private static HashMap<Graph, LowerBoundGraph> lbgCache = new HashMap<Graph, LowerBoundGraph>();
+
+    LowerBoundGraph lbg;
+
+    Vertex target;
+
+    double[] weights;
+
+    public LBGRemainingWeightHeuristic(Graph g) {
+        this.lbg = lbgCache.get(g);
+        if (this.lbg == null) {
+            LOG.debug("BEGIN Making lower bound graph");
+            this.lbg = new LowerBoundGraph(g, LowerBoundGraph.OUTGOING);
+            LOG.debug("END   Making lower bound graph");
+            lbgCache.put(g, this.lbg);
+        }
+    }
+
     @Override
     public double computeInitialWeight(State s, Vertex target) {
-    	recalculate(target);
-    	return 0;
+        recalculate(target);
+        return 0;
     }
 
     @Override
@@ -45,23 +55,24 @@ public class LBGRemainingWeightHeuristic implements RemainingWeightHeuristic {
 
     @Override
     public double computeReverseWeight(State s, Vertex target) {
-    	//return 0;
-    	int index = ((GenericVertex)s.getVertex()).getIndex();
+        // return 0;
+        int index = ((GenericVertex) s.getVertex()).getIndex();
         if (index < weights.length) {
-        	double h = weights[index];
-        	//System.out.printf("h=%f at %s\n", h, s.getVertex());
-        	return h == Double.POSITIVE_INFINITY ? 0 : h;
-        } else return 0;
+            double h = weights[index];
+            // System.out.printf("h=%f at %s\n", h, s.getVertex());
+            return h == Double.POSITIVE_INFINITY ? 0 : h;
+        } else
+            return 0;
     }
 
     private void recalculate(Vertex target) {
-    	if (target != this.target) {
-        	this.target = target;
-    		this.weights = lbg.sssp((StreetLocation)target);
-    	}
+        if (target != this.target) {
+            this.target = target;
+            this.weights = lbg.sssp((StreetLocation) target);
+        }
     }
 
-	@Override
-	public void reset() {		
-	}
+    @Override
+    public void reset() {
+    }
 }
