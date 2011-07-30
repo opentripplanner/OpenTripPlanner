7e8927a78e74f00cee7aafca32f8d7deb5bb124e
Andrew Byrd <andrew@fastmail.net>
2011-07-30 22:54:04 +0200

    Add edge class for timed transfers

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/TimedTransferEdge.java
@@ -0,0 +1,87 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package org.opentripplanner.routing.edgetype;
+
+import org.opentripplanner.routing.core.AbstractEdge;
+import org.opentripplanner.routing.core.EdgeNarrative;
+import org.opentripplanner.routing.core.State;
+import org.opentripplanner.routing.core.StateEditor;
+import org.opentripplanner.routing.core.TraverseMode;
+import org.opentripplanner.routing.core.Vertex;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * An edge represents what GTFS calls a timed transfer. This could also be referred to as a
+ * synchronized transfer: vehicles at one stop will wait for passengers alighting from another stop,
+ * so there are no walking, minimum transfer time, or schedule slack considerations.
+ * 
+ * In fact, our schedule slack and minimum transfer time implementation requires these special edges
+ * to allow 'instantaneous' synchronized transfers.
+ * 
+ * A TimedTransferEdge should connect a station_arrive vertex to a station_depart vertex, bypassing
+ * the preboard and prealight edges that handle the transfer table and schedule slack. The cost of
+ * boarding a vehicle should is added in (pattern)board edges, so it is still taken into account.
+ * 
+ * @author andrewbyrd
+ * 
+ */
+public class TimedTransferEdge extends AbstractEdge {
+
+    private static final long serialVersionUID = 20110730L; // MMMMDDYY
+
+    public TimedTransferEdge(Vertex from, Vertex to) {
+        super(from, to);
+    }
+
+    @Override
+    public State traverse(State s0) {
+        EdgeNarrative en = new FixedModeEdge(this, s0.getOptions().getModes().getNonTransitMode());
+        StateEditor s1 = s0.edit(this, en);
+        s1.incrementWeight(1);
+        return s1.makeState();
+    }
+
+    @Override
+    public double getDistance() {
+        return 0;
+    }
+
+    @Override
+    public Geometry getGeometry() {
+        return null;
+    }
+
+    @Override
+    public TraverseMode getMode() {
+        return TraverseMode.WALK;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof TimedTransferEdge) {
+            TimedTransferEdge other = (TimedTransferEdge) o;
+            return other.getFromVertex().equals(fromv) && other.getToVertex().equals(tov);
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "Timed transfer from " + fromv + " to " + tov;
+    }
+}
