8ce024f4642e82aec9f3530d786bae40990687af
Andrew Byrd <andrew@fastmail.net>
2011-10-18 14:06:46 +0200

    add method to provide shared immutable EnumSets of TraverseModes

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/core/TraverseMode.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/core/TraverseMode.java
@@ -15,9 +15,31 @@ package org.opentripplanner.routing.core;
 
 import javax.xml.bind.annotation.XmlType;
 
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Set;
+
 @XmlType(name="TraverseMode")  
 public enum TraverseMode {
     WALK, BICYCLE, CAR, TRAM, SUBWAY, RAIL, BUS, FERRY, CABLE_CAR, GONDOLA, FUNICULAR, TRANSIT, TRAINISH, BUSISH, BOARDING, ALIGHTING, TRANSFER, STL;
+
+    private static HashMap <Set<TraverseMode>, Set<TraverseMode>> setMap = 
+            new HashMap <Set<TraverseMode>, Set<TraverseMode>>();
+
+    public static Set<TraverseMode> internSet (Set<TraverseMode> modeSet) {
+        if (modeSet == null)
+            return null;
+        Set<TraverseMode> ret = setMap.get(modeSet);
+        if (ret == null) {
+            EnumSet<TraverseMode> backingSet = EnumSet.noneOf(TraverseMode.class);
+            backingSet.addAll(modeSet);
+            Set<TraverseMode> unmodifiableSet = Collections.unmodifiableSet(backingSet);
+            setMap.put(unmodifiableSet, unmodifiableSet);
+            ret = unmodifiableSet;
+        }
+        return ret;
+    }
 
     public boolean isTransit() {
         return this == TRAM || this == SUBWAY || this == RAIL || this == BUS || this == FERRY
