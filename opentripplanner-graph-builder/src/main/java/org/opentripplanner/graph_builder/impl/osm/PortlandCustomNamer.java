d5e4374c07176c3255673de1a4678832d35ab6da
David Turner <novalis@openplans.org>
2011-10-21 17:33:46 -0400

    formatting

--- opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/osm/PortlandCustomNamer.java
+++ opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/osm/PortlandCustomNamer.java
@@ -4,86 +4,83 @@ import org.opentripplanner.graph_builder.model.osm.OSMWay;
 import org.opentripplanner.graph_builder.services.osm.CustomNamer;
 
 /**
- * These rules were developed in consultation with Grant Humphries, PJ Houser,
- * and Mele Sax-Barnett. They describe which sidewalks and paths in the Portland
- * area should be specially designated in the narrative.
+ * These rules were developed in consultation with Grant Humphries, PJ Houser, and Mele Sax-Barnett.
+ * They describe which sidewalks and paths in the Portland area should be specially designated in
+ * the narrative.
  * 
  * @author novalis
  * 
  */
 public class PortlandCustomNamer implements CustomNamer {
 
-	public static String[] STREET_SUFFIXES = { "Avenue", "Street", "Drive",
-			"Court", "Highway", "Lane", "Way", "Place", "Road", "Boulevard",
-			"Alley" };
+    public static String[] STREET_SUFFIXES = { "Avenue", "Street", "Drive", "Court", "Highway",
+            "Lane", "Way", "Place", "Road", "Boulevard", "Alley" };
 
-	public static String[] PATH_WORDS = { "Trail", "Trails", "Greenway",
-			"Esplanade", "Spur", "Loop" };
+    public static String[] PATH_WORDS = { "Trail", "Trails", "Greenway", "Esplanade", "Spur",
+            "Loop" };
 
-	@Override
-	public String name(OSMWay way, String defaultName) {
-		if (!way.hasTag("name")) {
-			// this is already a generated name, so there's no need to add any
-			// additional data
-			return defaultName;
-		}
-		if (way.isTag("footway", "sidewalk") || way.isTag("path", "sidewalk")) {
-			if (isStreet(defaultName)) {
-				return sidewalk(defaultName);
-			}
-		}
-		String highway = way.getTag("highway");
-		if ("footway".equals(highway) || "path".equals(highway)
-				|| "cycleway".equals(highway)) {
-			if (!isObviouslyPath(defaultName)) {
-				return path(defaultName);
-			}
-		}
-		if ("pedestrian".equals(highway)) {
-		    return pedestrianStreet(defaultName);
-		}
-		return defaultName;
-	}
-
-	private boolean isStreet(String defaultName) {
-		for (String suffix : STREET_SUFFIXES) {
-			if (defaultName.endsWith(suffix)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean isObviouslyPath(String defaultName) {
-		for (String word : PATH_WORDS) {
-			if (defaultName.contains(word)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private String path(String name) {
-		if (!name.toLowerCase().contains("path")) {
-			name = name + " (path)".intern();
-		}
-		return name;
-	}
-
-        private String pedestrianStreet(String name) {
-                if (!name.toLowerCase().contains("pedestrian street")) {
-                        name = name + " (pedestrian street)".intern();
-                }
-                return name;
-
+    @Override
+    public String name(OSMWay way, String defaultName) {
+        if (!way.hasTag("name")) {
+            // this is already a generated name, so there's no need to add any
+            // additional data
+            return defaultName;
         }
+        if (way.isTag("footway", "sidewalk") || way.isTag("path", "sidewalk")) {
+            if (isStreet(defaultName)) {
+                return sidewalk(defaultName);
+            }
+        }
+        String highway = way.getTag("highway");
+        if ("footway".equals(highway) || "path".equals(highway) || "cycleway".equals(highway)) {
+            if (!isObviouslyPath(defaultName)) {
+                return path(defaultName);
+            }
+        }
+        if ("pedestrian".equals(highway)) {
+            return pedestrianStreet(defaultName);
+        }
+        return defaultName;
+    }
 
-	private String sidewalk(String name) {
-		if (!name.toLowerCase().contains("sidewalk")) {
-			name = name + " (sidewalk)".intern();
-		}
-		return name;
+    private boolean isStreet(String defaultName) {
+        for (String suffix : STREET_SUFFIXES) {
+            if (defaultName.endsWith(suffix)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	}
+    private boolean isObviouslyPath(String defaultName) {
+        for (String word : PATH_WORDS) {
+            if (defaultName.contains(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    private String path(String name) {
+        if (!name.toLowerCase().contains("path")) {
+            name = name + " (path)".intern();
+        }
+        return name;
+    }
+
+    private String pedestrianStreet(String name) {
+        if (!name.toLowerCase().contains("pedestrian street")) {
+            name = name + " (pedestrian street)".intern();
+        }
+        return name;
+
+    }
+
+    private String sidewalk(String name) {
+        if (!name.toLowerCase().contains("sidewalk")) {
+            name = name + " (sidewalk)".intern();
+        }
+        return name;
+
+    }
 }
