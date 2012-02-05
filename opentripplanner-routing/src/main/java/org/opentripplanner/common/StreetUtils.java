7fa688f8e4d2b2a86bec1d624e23ce8eeaafab63
Matt Conway <mattwigway@gmail.com>
2012-02-05 09:26:33 -0800

    More indentation fixes. #582.

--- opentripplanner-routing/src/main/java/org/opentripplanner/common/StreetUtils.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/common/StreetUtils.java
@@ -192,51 +192,51 @@ public class StreetUtils {
         TraverseOptions options = new TraverseOptions(new TraverseModeSet(TraverseMode.WALK));
         
         for (Vertex gv : graph.getVertices()) {
-        if (!(gv instanceof EndpointVertex)) {
-        continue;
-        }
-        State s0 = new State(gv, options);
-        for (Edge e: gv.getOutgoing()) {
-        Vertex in = gv;
-        if (!(e instanceof StreetEdge)) {
-            continue;
-        }
-        State s1 = e.traverse(s0);
-        if (s1 == null) {
-            continue;
-        }
-        Vertex out = s1.getVertex();
+            if (!(gv instanceof EndpointVertex)) {
+                continue;
+            }
+            State s0 = new State(gv, options);
+            for (Edge e: gv.getOutgoing()) {
+                Vertex in = gv;
+                if (!(e instanceof StreetEdge)) {
+                    continue;
+                }
+                State s1 = e.traverse(s0);
+                if (s1 == null) {
+                    continue;
+                }
+                Vertex out = s1.getVertex();
                         
-        ArrayList<Vertex> vertexList = neighborsForVertex.get(in);
-        if (vertexList == null) {
-            vertexList = new ArrayList<Vertex>();
-            neighborsForVertex.put(in, vertexList);
+                ArrayList<Vertex> vertexList = neighborsForVertex.get(in);
+                if (vertexList == null) {
+                    vertexList = new ArrayList<Vertex>();
+                    neighborsForVertex.put(in, vertexList);
                 }
                 vertexList.add(out);
                 
-        vertexList = neighborsForVertex.get(out);
-        if (vertexList == null) {
-            vertexList = new ArrayList<Vertex>();
-            neighborsForVertex.put(out, vertexList);
+                vertexList = neighborsForVertex.get(out);
+                if (vertexList == null) {
+                    vertexList = new ArrayList<Vertex>();
+                    neighborsForVertex.put(out, vertexList);
                 }
                 vertexList.add(in);
-        }
+            }
         }
         
         ArrayList<HashSet<Vertex>> islands = new ArrayList<HashSet<Vertex>>();
         /* associate each node with a subgraph */
         for (Vertex gv : graph.getVertices()) {
-                if (!(gv instanceof EndpointVertex)) {
-                        continue;
-                }
-                Vertex vertex = gv;
-                if (subgraphs.containsKey(vertex)) {
-                        continue;
-                }
-                if (!neighborsForVertex.containsKey(vertex)) {
-                        continue;
-                }
-                HashSet<Vertex> subgraph = computeConnectedSubgraph(neighborsForVertex, vertex);
+            if (!(gv instanceof EndpointVertex)) {
+                continue;
+            }
+            Vertex vertex = gv;
+            if (subgraphs.containsKey(vertex)) {
+                continue;
+            }
+            if (!neighborsForVertex.containsKey(vertex)) {
+                continue;
+            }
+            HashSet<Vertex> subgraph = computeConnectedSubgraph(neighborsForVertex, vertex);
             for (Vertex subnode : subgraph) {
                 subgraphs.put(subnode, subgraph);
             }
@@ -244,35 +244,35 @@ public class StreetUtils {
         }
         
         /* remove all tiny subgraphs */
-/* removed 10/27/11, since it looks like PDX is fixed.
-        for (HashSet<Vertex> island : islands) {
-                if (island.size() < 20) {
-                        _log.warn("Depedestrianizing or deleting floating island at " + island.iterator().next());
-                        for (Vertex vertex : island) {
-                                depedestrianizeOrRemove(graph, vertex);
-                        }
-                } 
-        }
-*/
+        /* removed 10/27/11, since it looks like PDX is fixed.
+           for (HashSet<Vertex> island : islands) {
+           if (island.size() < 20) {
+           _log.warn("Depedestrianizing or deleting floating island at " + island.iterator().next());
+           for (Vertex vertex : island) {
+           depedestrianizeOrRemove(graph, vertex);
+           }
+           } 
+           }
+        */
     }
     
     private static void depedestrianizeOrRemove(Graph graph, Vertex v) {
         Collection<Edge> outgoing = new ArrayList<Edge>(v.getOutgoing());
-    for (Edge e : outgoing) {
-        if (e instanceof PlainStreetEdge) {
-        PlainStreetEdge pse = (PlainStreetEdge) e;
-        StreetTraversalPermission permission = pse.getPermission();
-        permission = permission.remove(StreetTraversalPermission.PEDESTRIAN);
-        if (permission == StreetTraversalPermission.NONE) {
-            graph.removeEdge(pse);
-        } else {
-            pse.setPermission(permission);
+        for (Edge e : outgoing) {
+            if (e instanceof PlainStreetEdge) {
+                PlainStreetEdge pse = (PlainStreetEdge) e;
+                StreetTraversalPermission permission = pse.getPermission();
+                permission = permission.remove(StreetTraversalPermission.PEDESTRIAN);
+                if (permission == StreetTraversalPermission.NONE) {
+                    graph.removeEdge(pse);
+                } else {
+                    pse.setPermission(permission);
+                }
+            }
         }
+        if (v.getOutgoing().size() == 0) {
+            graph.removeVertexAndEdges(v);
         }
-    }
-    if (v.getOutgoing().size() == 0) {
-        graph.removeVertexAndEdges(v);
-    }
     }
 
     private static HashSet<Vertex> computeConnectedSubgraph(
