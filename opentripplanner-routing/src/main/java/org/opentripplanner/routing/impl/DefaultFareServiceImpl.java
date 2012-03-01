6c539e40a68ddffe24a4d1c7fc29804e5e5c5c99
Andrew Byrd <andrew@fastmail.net>
2012-03-01 15:30:50 +0100

    refactor default fare service to allow code reuse by custom subclasses
    extract method for making rides from paths
    move ride cost minimizing into separate method (override in subclasses)
    check agencyId of fare attributes against ride blocks
    get ride zone info from hopEdge (cleaner, but also workaround for an
    apparent bug in path reversal)

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
@@ -14,7 +14,6 @@
 package org.opentripplanner.routing.impl;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Currency;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -25,21 +24,19 @@ import java.util.Set;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.gtfs.model.FareAttribute;
 import org.onebusaway.gtfs.model.Stop;
-import org.opentripplanner.routing.core.EdgeNarrative;
 import org.opentripplanner.routing.core.Fare;
 import org.opentripplanner.routing.core.FareRuleSet;
 import org.opentripplanner.routing.core.State;
-import org.opentripplanner.routing.core.TraverseMode;
 import org.opentripplanner.routing.core.WrappedCurrency;
 import org.opentripplanner.routing.core.Fare.FareType;
+import org.opentripplanner.routing.edgetype.HopEdge;
+import org.opentripplanner.routing.graph.Edge;
 import org.opentripplanner.routing.services.FareService;
 import org.opentripplanner.routing.spt.GraphPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * A set of edges on a single route
- * */
+/** A set of edges on a single route, with associated information for calculating fares */
 class Ride {
     
     AgencyAndId route;
@@ -54,9 +51,8 @@ class Ride {
 
     long endTime;
 
-    // generic classifier and start/end stops, unused in DefaultFareServiceImpl
-    // but maybe useful
-    // elsewhere
+    // in DefaultFareServiceImpl classifier is just the TraverseMode
+    // it can be used differently in custom fare services
     public Object classifier;
 
     public Stop firstStop;
@@ -71,17 +67,17 @@ class Ride {
         StringBuilder builder = new StringBuilder();
         builder.append("Ride");
         if (startZone != null) {
-            builder.append("(from ");
+            builder.append("(from zone ");
             builder.append(startZone);
         }
         if (endZone != null) {
-            builder.append(" to ");
+            builder.append(" to zone ");
             builder.append(endZone);
         }
-        builder.append(" on ");
+        builder.append(" on route ");
         builder.append(route);
         if (zones.size() > 0) {
-            builder.append(" through ");
+            builder.append(" through zones ");
             boolean first = true;
             for (String zone : zones) {
                 if (first) {
@@ -104,155 +100,172 @@ class Ride {
 }
 
 /**
- * This fare service impl handles the cases that GTFS handles. For more interesting fare structures, like New York's MTA, you get to implement your
- * own FareService. See this thread on gtfs-changes explaining the proper interpretation of fares.txt:
+ * This fare service impl handles the cases that GTFS handles within a single feed. 
+ * It cannot necessarily handle multi-feed graphs, because a rule-less fare attribute
+ * might be applied to rides on routes in another feed, for example.
+ * For more interesting fare structures like New York's MTA, or cities with multiple
+ * feeds and inter-feed transfer rules, you get to implement your own FareService. 
+ * See this thread on gtfs-changes explaining the proper interpretation of fares.txt:
  * http://groups.google.com/group/gtfs-changes/browse_thread /thread/8a4a48ae1e742517/4f81b826cb732f3b
  */
 public class DefaultFareServiceImpl implements FareService, Serializable {
-    private static final long serialVersionUID = 1L;
+
+    private static final long serialVersionUID = 20120229L;
 
     private static final Logger _log = LoggerFactory.getLogger(DefaultFareServiceImpl.class);
 
-    private HashMap<AgencyAndId, FareRuleSet> fareRules;
+    protected HashMap<AgencyAndId, FareRuleSet> fareRules;
 
-    private HashMap<AgencyAndId, FareAttribute> fareAttributes;
+    protected HashMap<AgencyAndId, FareAttribute> fareAttributes;
 
-    public DefaultFareServiceImpl(HashMap<AgencyAndId, FareRuleSet> fareRules,
-            HashMap<AgencyAndId, FareAttribute> fareAttributes) {
+    public DefaultFareServiceImpl(HashMap<AgencyAndId, FareRuleSet>   fareRules,
+                                  HashMap<AgencyAndId, FareAttribute> fareAttributes) {
         this.fareRules = fareRules;
         this.fareAttributes = fareAttributes;
     }
 
+    public static List<Ride> createRides(GraphPath path) {
+        List<Ride> rides = new LinkedList<Ride>();
+        Ride ride = null;
+        for (State state : path.states) {
+            Edge edge = state.getBackEdge();
+            if ( ! (edge instanceof HopEdge))
+                continue;
+            HopEdge hEdge = (HopEdge) edge;
+            if (ride == null || ! state.getRoute().equals(ride.route)) {
+                ride = new Ride();
+                rides.add(ride);
+                ride.startZone = hEdge.getStartStop().getZoneId();
+                ride.zones.add(ride.startZone);
+                ride.route = state.getRoute();
+                ride.startTime = state.getBackState().getTime();
+                ride.firstStop = hEdge.getStartStop();
+            }
+            ride.lastStop = hEdge.getEndStop();
+            ride.endZone  = ride.lastStop.getZoneId();
+            ride.zones.add(ride.endZone);
+            ride.endTime  = state.getTime();
+            // in default fare service, classify rides by mode 
+            ride.classifier = edge.getMode();
+        }
+        return rides;
+    }
+
+    // TODO: Overridable classify method for rides / make rides from list<state>
+    
     @Override
     public Fare getCost(GraphPath path) {
 
-        LinkedList<State> states = path.states;
-
-        // create rides
-        List<Ride> rides = new ArrayList<Ride>();
-        Ride newRide = null;
-        for (State curr : states) {
-            EdgeNarrative edgeNarrative = curr.getBackEdgeNarrative();
-            /* skip initial state, which has no back edges */
-            if (edgeNarrative == null)
-                continue;
-
-            String zone = curr.getZone();
-            AgencyAndId route = curr.getRoute();
-            TraverseMode mode = edgeNarrative.getMode();
-            if (zone == null && route == null) {
-                newRide = null;
-            } else {
-                if (mode.isTransit() || mode == TraverseMode.BOARDING) {
-                    if (newRide == null || !route.equals(newRide.route)) {
-                        newRide = new Ride();
-                        rides.add(newRide);
-                        newRide.startZone = zone;
-                        newRide.route = route;
-                        newRide.startTime = curr.getTime();
-                    }
-                    newRide.zones.add(zone);
-                    newRide.endZone = zone;
-                    newRide.endTime = curr.getTime();
-                }
-            }
-        }
-
-        // There are no rides, so there's no fare.
+        List<Ride> rides = createRides(path);
+        // If there are no rides, there's no fare.
         if (rides.size() == 0) {
             return null;
         }
+        // pick up a random currency from fareAttributes, 
+        // we assume that all tickets use the same currency
+        WrappedCurrency wrappedCurrency = null;
+        if (fareAttributes.size() > 0) {
+            wrappedCurrency = 
+                  new WrappedCurrency(fareAttributes.values().iterator().next().getCurrencyType());
+        }
+        int lowestCost = getLowestCost(rides);
+        if (lowestCost < Integer.MAX_VALUE) {
+            Fare fare = new Fare();
+            fare.addFare(FareType.regular, wrappedCurrency, lowestCost);
+            return fare;
+        } else {
+            return null;
+        }
+    }
 
-        // dynamic algorithm calculate fare cost
-        /*
-         * In row [i,j] remember best cost for trip from rides[i] to rides[j]
-         */
+    public int getLowestCost(List<Ride> rides) {
+        // Dynamic algorithm to calculate fare cost.
+        // Cell [i,j] holds the best (lowest) cost for a trip from rides[i] to rides[j]
         int[][] resultTable = new int[rides.size()][rides.size()];
 
         for (int i = 0; i < rides.size(); i++) {
             // each diagonal
             for (int j = 0; j < rides.size() - i; j++) {
-                int cost = calculateCost(rides, j, j + i);
+                int cost = calculateCost(rides.subList(j, j + i));
                 if (cost < 0) {
-                    continue;
+                    _log.error("negative cost for a ride sequence");
+                    cost = Integer.MAX_VALUE;
                 }
                 resultTable[j][j + i] = cost;
                 for (int k = 0; k < i; k++) {
-                    if (resultTable[j][j + i] > resultTable[j][j + k]
-                            + resultTable[j + k + 1][j + i])
-                        resultTable[j][j + i] = resultTable[j][k + j]
-                                + resultTable[j + k + 1][j + i];
+                    int via = resultTable[j][j + k] + resultTable[j + k + 1][j + i];
+                    if (resultTable[j][j + i] > via)
+                        resultTable[j][j + i] = via;
                 }
             }
         }
-
-        WrappedCurrency wrappedCurrency = null;
-        Fare fare = new Fare();
-        // pick up the random currency from fareAttributes, we assume that all tickets have the
-        // same currency
-        if (fareAttributes.size() > 0) {
-            wrappedCurrency = new WrappedCurrency(fareAttributes.values().iterator().next()
-                    .getCurrencyType());
-        }
-        fare.addFare(FareType.regular, wrappedCurrency, resultTable[0][rides.size() - 1]);
-
-        return fare;
+        return resultTable[0][rides.size() - 1];
     }
-
-    private int calculateCost(List<Ride> rides, int first, int last) {
+    
+    protected int calculateCost(List<Ride> rides) {
         Currency currency = null;
 
         Set<String> zones = new HashSet<String>();
         Set<AgencyAndId> routes = new HashSet<AgencyAndId>();
-        String startZone = null;
         int transfersUsed = -1;
-        float currentFare = -1;
-        long startTime = rides.get(first).startTime;
-        Ride ride = rides.get(first);
-        for (int i = first; i <= last; ++i) {
-            ride = rides.get(i);
-            if (startZone == null) {
-                startZone = ride.startZone;
+        
+        Ride firstRide = rides.get(0);
+        long   startTime = firstRide.startTime;
+        String startZone = firstRide.startZone;
+        String endZone = firstRide.endZone;
+        String agencyId  = firstRide.route.getAgencyId();                
+        long lastRideStartTime = firstRide.startTime;
+        long lastRideEndTime = firstRide.endTime;
+        for (Ride ride : rides) {
+            if ( ! ride.route.getAgencyId().equals(agencyId)) {
+                _log.debug("skipped multi-agency ride sequence {}", rides);
+                return Integer.MAX_VALUE;
             }
+            lastRideStartTime = ride.startTime;
+            lastRideEndTime = ride.endTime;
+            endZone = ride.endZone;
             routes.add(ride.route);
             zones.addAll(ride.zones);
             transfersUsed += 1;
         }
+        
+        FareAttribute bestAttribute = null;
         float bestFare = Float.MAX_VALUE;
-
-        long tripTime = ride.startTime - startTime;
-        long journeyTime = ride.endTime - startTime;
+        long tripTime = lastRideStartTime - startTime;
+        long journeyTime = lastRideEndTime - startTime;
         // find the best fare that matches this set of rides
         for (AgencyAndId fareId : fareAttributes.keySet()) {
+            if ( ! fareId.getAgencyId().equals(agencyId))
+                continue;
             FareRuleSet ruleSet = fareRules.get(fareId);
-            if (ruleSet == null || ruleSet.matches(startZone, ride.endZone, zones, routes)) {
+            if (ruleSet == null || ruleSet.matches(startZone, endZone, zones, routes)) {
                 FareAttribute attribute = fareAttributes.get(fareId);
                 if (attribute.isTransfersSet() && attribute.getTransfers() < transfersUsed) {
                     continue;
                 }
                 // assume transfers are evaluated at boarding time,
                 // as trimet does
-                if (attribute.isTransferDurationSet() && tripTime > attribute.getTransferDuration()) {
+                if (attribute.isTransferDurationSet() && 
+                    tripTime > attribute.getTransferDuration()) {
                     continue;
                 }
-                if (attribute.isJourneyDurationSet()
-                        && journeyTime > attribute.getJourneyDuration()) {
+                if (attribute.isJourneyDurationSet() && 
+                    journeyTime > attribute.getJourneyDuration()) {
                     continue;
                 }
                 float newFare = attribute.getPrice();
                 if (newFare < bestFare) {
+                    bestAttribute = attribute;
                     bestFare = newFare;
                     currency = Currency.getInstance(attribute.getCurrencyType());
                 }
             }
         }
+        _log.debug("{} best for {}", bestAttribute, rides);
 
         if (bestFare == Float.MAX_VALUE) {
-            if (currentFare == -1) {
-                // Problem: there's no fare for this ride.
-                _log.warn("No fare for a ride: " + ride);
-                return -1;
-            }
+            _log.warn("No fare for a ride sequence: {}", rides);
+            return Integer.MAX_VALUE;
         }
 
         return (int) Math.round(bestFare * Math.pow(10, currency.getDefaultFractionDigits()));
