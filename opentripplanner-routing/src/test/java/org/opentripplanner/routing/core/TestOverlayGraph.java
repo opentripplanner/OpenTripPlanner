6b72a8a65fd807de1267916846884cf05e6e472a
novalis <novalis@openplans.org>
2012-03-09 16:59:47 -0500

    remove cruft

--- opentripplanner-routing/src/test/java/org/opentripplanner/routing/core/TestOverlayGraph.java
+++ opentripplanner-routing/src/test/java/org/opentripplanner/routing/core/TestOverlayGraph.java
@@ -18,7 +18,6 @@ import junit.framework.TestCase;
 import org.opentripplanner.routing.graph.Edge;
 import org.opentripplanner.routing.edgetype.FreeEdge;
 import org.opentripplanner.routing.edgetype.SimpleEdge;
-import org.opentripplanner.routing.graph.Edge;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.routing.vertextype.IntersectionVertex;
