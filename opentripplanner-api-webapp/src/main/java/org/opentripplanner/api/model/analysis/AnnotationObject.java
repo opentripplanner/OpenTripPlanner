2162436cc87f547bd43bdb24ae28bbd2a0568d9a
novalis <novalis@openplans.org>
2012-03-15 17:36:31 -0400

    make fields on annotation objects optional in json

--- opentripplanner-api-webapp/src/main/java/org/opentripplanner/api/model/analysis/AnnotationObject.java
+++ opentripplanner-api-webapp/src/main/java/org/opentripplanner/api/model/analysis/AnnotationObject.java
@@ -2,20 +2,28 @@ package org.opentripplanner.api.model.analysis;
 
 import javax.xml.bind.annotation.XmlAttribute;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
 public class AnnotationObject {
 
+    @JsonSerialize(include=Inclusion.NON_NULL)
     @XmlAttribute
     public String vertex;
 
+    @JsonSerialize(include=Inclusion.NON_NULL)
     @XmlAttribute
     public Integer edge;
 
+    @JsonSerialize(include=Inclusion.NON_NULL)
     @XmlAttribute
     public String message;
 
+    @JsonSerialize(include=Inclusion.NON_NULL)
     @XmlAttribute
     public String agency;
 
+    @JsonSerialize(include=Inclusion.NON_NULL)
     @XmlAttribute
     public String id;
 
