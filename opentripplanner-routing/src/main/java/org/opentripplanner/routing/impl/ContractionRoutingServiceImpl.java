b0454a793a81138b3fe0aa5945b635eb1ba1599a
andrewbyrd <andrew@fastmail.net>
2012-04-09 04:51:00 +0200

    remove component annotation on contraction routing service
    (avoid compilation error crashes during injection)

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/ContractionRoutingServiceImpl.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/ContractionRoutingServiceImpl.java
@@ -41,7 +41,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class ContractionRoutingServiceImpl implements RoutingService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ContractionRoutingServiceImpl.class);
