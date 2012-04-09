5fcd2795fb6cfe6dd801268bfbce1421d1e12665
andrewbyrd <andrew@fastmail.net>
2012-04-09 03:20:13 +0200

    trivial path service implementing new interface

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/TrivialPathServiceImpl.java
@@ -0,0 +1,47 @@
+package org.opentripplanner.routing.impl;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.opentripplanner.common.model.NamedPlace;
+import org.opentripplanner.routing.algorithm.AStar;
+import org.opentripplanner.routing.core.TraverseOptions;
+import org.opentripplanner.routing.services.GraphService;
+import org.opentripplanner.routing.services.PathService;
+import org.opentripplanner.routing.services.SPTService;
+import org.opentripplanner.routing.spt.GraphPath;
+import org.opentripplanner.routing.spt.ShortestPathTree;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//@Singleton
+public class TrivialPathServiceImpl implements PathService {
+
+    @Autowired GraphService graphService;
+    
+    SPTService sptService = new AStar();
+    
+    @Override
+    public List<GraphPath> getPaths(TraverseOptions options) {
+        ShortestPathTree spt = sptService.getShortestPathTree(options);
+        if (spt == null)
+            return Collections.emptyList();
+        return spt.getPaths(options.toVertex, true);
+    }
+
+    @Override
+    public void setGraphService(GraphService graphService) {
+        this.graphService = graphService;
+    }
+
+    @Override
+    public GraphService getGraphService() {
+        return graphService;
+    }
+
+    @Override
+    public boolean isAccessible(NamedPlace place, TraverseOptions options) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
