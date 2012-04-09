b7257e525dae1168390ab8bd66e4756f1dd59335
andrewbyrd <andrew@fastmail.net>
2012-04-09 04:15:43 +0200

    autowire spt service in trivial path service

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/TrivialPathServiceImpl.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/TrivialPathServiceImpl.java
@@ -3,7 +3,6 @@ package org.opentripplanner.routing.impl;
 import java.util.Collections;
 import java.util.List;
 
-import org.opentripplanner.routing.algorithm.AStar;
 import org.opentripplanner.routing.core.TraverseOptions;
 import org.opentripplanner.routing.services.GraphService;
 import org.opentripplanner.routing.services.PathService;
@@ -12,12 +11,11 @@ import org.opentripplanner.routing.spt.GraphPath;
 import org.opentripplanner.routing.spt.ShortestPathTree;
 import org.springframework.beans.factory.annotation.Autowired;
 
-//@Singleton
 public class TrivialPathServiceImpl implements PathService {
 
     @Autowired GraphService graphService;
     
-    SPTService sptService = new AStar();
+    @Autowired SPTService sptService;
     
     @Override
     public List<GraphPath> getPaths(TraverseOptions options) {
