27be32f04abbf830e23114dc8993b58e94477500
Frank Purcell <fxpurcell@gmail.com>
2012-04-19 11:58:06 -0700

    Changed "Safest" to "Bike friendly" for English strings (some of which are in other lang files in need of translation)

--- opentripplanner-webapp/src/main/webapp/js/otp/locale/Hungarian.js
+++ opentripplanner-webapp/src/main/webapp/js/otp/locale/Hungarian.js
@@ -46,13 +46,13 @@ otp.locale.Hungarian = {
     // TODO Localize Me
     bikeTriangle : 
     {
-        safeName : "Safest",
-        safeSym  : "S",
+        safeName : "Bike friendly",
+        safeSym  : "B",
 
-        hillName : "Flattest",
+        hillName : "Flat",
         hillSym  : "F",
 
-        timeName : "Quickest",
+        timeName : "Quick",
         timeSym  : "Q"
     },
 
