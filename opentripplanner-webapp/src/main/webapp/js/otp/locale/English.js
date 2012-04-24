opentripplanner/OpenTripPlanner

commit f33f8576d54afa9097b00fdb1fc1fa2253d44eca

time parser / correction (e.g., strings like 1344 and 111pm now work fairly well).  optimize parameter

opentripplanner-webapp/src/main/webapp/js/otp/planner/Forms.js
@@ -247,11 +247,13 @@ otp.planner.StaticForms = {
         var to   = this.m_toForm.getRawValue();
         var fromPlace = this.m_fromForm.getNamedCoord();
         var toPlace   = this.m_toForm.getNamedCoord();
+        var time      = otp.util.DateUtils.parseTime(this.m_time.getRawValue());
         form.setValues({
             from      : from,
             to        : to,
-            fromPlace : fromPlace, 
-            toPlace   : toPlace
+            fromPlace : fromPlace,
+            toPlace   : toPlace,
+            time      : time
         });
     },
 
@@ -503,14 +505,14 @@ otp.planner.StaticForms = {
             if(params.after)
             {
                 time = params.after.replace(/\./g, "");
-                forms.m_time.setRawValue(time);
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
                 forms.m_arriveByForm.setValue('false');
                 time = true;
             }
             else if(params.by)
             {
                 time = params.by.replace(/\./g, "");
-                forms.m_time.setRawValue(time);
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
                 forms.m_arriveByForm.setValue('true');
                 time = true;
             }
@@ -518,7 +520,7 @@ otp.planner.StaticForms = {
             if(params.time)
             {
                 time = params.time.replace(/\./g, "");
-                forms.m_time.setRawValue(time);
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
                 time = true;
             }
 
@@ -533,13 +535,15 @@ otp.planner.StaticForms = {
 
             // optimize / minimize option : how to optimize the trip, safest, quickets, least tranfers, etc...
             if(params.min)
-                params.opt = params.min;
+                params.optimize = params.min;
             if(params.opt)
+                params.optimize = params.opt
+            if(params.optimize)
             {
-                forms.m_optimizeForm.setValue(params.opt);
+                forms.m_optimizeForm.setValue(params.optimize);
                 if(this.m_optionsManager)
                 {
-                    this.m_optionsManager.doOpt(params.opt);
+                    this.m_optionsManager.doOpt(params.optimize);
                 }
             }
 
@@ -557,7 +561,7 @@ otp.planner.StaticForms = {
             {
                 time = params.Hour + ":" +  params.Minute + " " + params.AmPm.toLowerCase();
                 time = time.replace(/\./g, "");
-                forms.m_time.setRawValue(time);
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
                 time = true;
             }
 
@@ -621,7 +625,7 @@ otp.planner.StaticForms = {
         retVal.fromPlace = this.m_fromForm.getNamedCoord();
         retVal.toPlace   = this.m_toForm.getNamedCoord();
         retVal.date      = this.m_date.getRawValue();
-        retVal.time      = this.m_time.getRawValue();
+        retVal.time      = otp.util.DateUtils.parseTime(this.m_time.getRawValue());
         retVal.arriveBy  = this.m_arriveByForm.getRawValue();
         retVal.opt       = this.m_optimizeForm.getValue();
         retVal.routerId  = this.m_routerIdForm.getValue();
@@ -1035,13 +1039,13 @@ otp.planner.StaticForms = {
     {
         this.m_date = new Ext.form.DateField({
             id:         'trip-date-form',
-            fieldLabel: this.locale.tripPlanner.labels.date,
             name:       'date',
+            fieldLabel: this.locale.tripPlanner.labels.date,
             format:     this.locale.time.date_format,
             allowBlank: false,
             msgTarget:  'qtip',
-            anchor:     "87%",
-            value:      new Date().format(this.locale.time.date_format)
+            value:      new Date().format(this.locale.time.date_format),
+            anchor:     "87%"
         });
 
         this.m_arriveByStore = otp.util.ExtUtils.makeStaticPullDownStore(this.locale.tripPlanner.arriveDepart);
@@ -1067,13 +1071,13 @@ otp.planner.StaticForms = {
 
         this.m_time = new Ext.ux.form.Spinner({
                 id         : 'trip-time-form',
+                name       : 'time',
                 fieldLabel : this.locale.tripPlanner.labels.when,
                 accelerate : true,
-                width      : 85,
                 msgTarget  : 'qtip',
                 value      : new Date().format(this.locale.time.time_format),
                 strategy   : new Ext.ux.form.Spinner.TimeStrategy({format:this.locale.time.time_format}),
-                name       : 'time'
+                width      : 85
         });
 
         var timePanel = {

opentripplanner-webapp/src/main/webapp/js/otp/util/DateUtils.js
@@ -121,7 +121,8 @@ otp.util.DateUtils = {
     },
     
     /** Make a Date object from an ISO 8601 date string (ignoring time zone) */
-    isoDateStringToDate : function(str) {
+    isoDateStringToDate : function(str)
+    {
         if (!str)
             return null;
         if (str.lastIndexOf("Z") != -1) {
@@ -133,18 +134,69 @@ otp.util.DateUtils = {
         return date;
     },
 
-    prettyDateTime : function(date) {
+    /** */
+    prettyDateTime : function(date)
+    {
         if (typeof date == "string") {
             date = this.isoDateStringToDate(date);
         }
         return date.format(this.DATE_TIME_FORMAT_STRING);
     },
-    
-    prettyTime : function(date) {
+
+    /** */
+    prettyTime : function(date)
+    {
         if (typeof date == "string") {
             date = this.isoDateStringToDate(date);
         }
         return date.format(this.TIME_FORMAT_STRING);
+    },
+
+    /** arbitrary time string parser / correction */
+    parseTime : function(t)
+    {
+        var time = t.trim();
+        time = time.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+    
+        var h = time[1];
+        var m = parseInt(time[2]) || null;
+        var am = time[3];
+    
+        if(h && h.length > 2)
+        {
+            if(m == null)
+                m = h.substring(h.length-2);
+            h = h.substring(0, h.length-2);
+        }
+        h = parseInt(h) || 12;
+        if(h > 12)
+        {
+            h = h % 12;
+            if(h == 0)
+                h = 12;
+            if(am == null || am == '')
+                am = 'p';
+        }
+        if(m == null || m > 59 || m < 0)
+            m = "00"
+
+        if(am && am == 'p')
+            am = "pm"
+        else  
+            am = "am"
+    
+        return  h + ":" + m + "" + am;
+    },
+
+    /** */
+    parseTimeTest : function(t)
+    {
+        var times = ['1:00 pm','1:00 p.m.','100 p','1:00p.m.','1:00p','1 pm','1 p.m.','1 p','1pm','1p.m.','1p','1:pm','13:00','13','944am', '1354','12335','1232p'];
+    
+        for ( var i = 0; i < times.length; i++ )
+        {
+          console.log(parseTime(times[i]));
+        }
     },
 
     CLASS_NAME : "otp.util.DateUtils"

commit 0827c0073360620473780940884eacf8ee42bf86

Fixes any date format issues introduced in f33f8576d54afa9097b00fdb1fc1fa2253d44eca, and fixes English format

opentripplanner-webapp/src/main/webapp/js/otp/locale/English.js
@@ -224,7 +224,7 @@ otp.locale.English = {
         seconds_abbrev : "secs",
         format         : "F jS, Y @ g:ia",
         date_format    : "m-d-Y",
-        time_format    : "g:ia",
+        time_format    : "g:i a",
         months         : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     },
 

opentripplanner-webapp/src/main/webapp/js/otp/planner/Forms.js
@@ -247,7 +247,7 @@ otp.planner.StaticForms = {
         var to   = this.m_toForm.getRawValue();
         var fromPlace = this.m_fromForm.getNamedCoord();
         var toPlace   = this.m_toForm.getNamedCoord();
-        var time      = otp.util.DateUtils.parseTime(this.m_time.getRawValue());
+        var time      = otp.util.DateUtils.parseTime(this.m_time.getRawValue(), this.locale.time.time_format);
         form.setValues({
             from      : from,
             to        : to,
@@ -505,14 +505,14 @@ otp.planner.StaticForms = {
             if(params.after)
             {
                 time = params.after.replace(/\./g, "");
-                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time, this.locale.time.time_format));
                 forms.m_arriveByForm.setValue('false');
                 time = true;
             }
             else if(params.by)
             {
                 time = params.by.replace(/\./g, "");
-                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time, this.locale.time.time_format));
                 forms.m_arriveByForm.setValue('true');
                 time = true;
             }
@@ -520,7 +520,7 @@ otp.planner.StaticForms = {
             if(params.time)
             {
                 time = params.time.replace(/\./g, "");
-                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time, this.locale.time.time_format));
                 time = true;
             }
 
@@ -561,7 +561,7 @@ otp.planner.StaticForms = {
             {
                 time = params.Hour + ":" +  params.Minute + " " + params.AmPm.toLowerCase();
                 time = time.replace(/\./g, "");
-                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time));
+                forms.m_time.setRawValue(otp.util.DateUtils.parseTime(time, this.locale.time.time_format));
                 time = true;
             }
 
@@ -625,7 +625,7 @@ otp.planner.StaticForms = {
         retVal.fromPlace = this.m_fromForm.getNamedCoord();
         retVal.toPlace   = this.m_toForm.getNamedCoord();
         retVal.date      = this.m_date.getRawValue();
-        retVal.time      = otp.util.DateUtils.parseTime(this.m_time.getRawValue());
+        retVal.time      = otp.util.DateUtils.parseTime(this.m_time.getRawValue(), this.locale.time.time_format);
         retVal.arriveBy  = this.m_arriveByForm.getRawValue();
         retVal.opt       = this.m_optimizeForm.getValue();
         retVal.routerId  = this.m_routerIdForm.getValue();

opentripplanner-webapp/src/main/webapp/js/otp/util/DateUtils.js
@@ -152,12 +152,12 @@ otp.util.DateUtils = {
         return date.format(this.TIME_FORMAT_STRING);
     },
 
-    /** arbitrary time string parser / correction */
-    parseTime : function(t)
+    /** arbitrary am/pm time string correction ... e.g., 1233pm gets formatted into 12:33pm, etc... */
+    correctAmPmTimeString : function(time, format)
     {
-        var time = t.trim();
+        time = time.trim();
         time = time.match(/(\d+)(?::(\d\d))?\s*(p?)/);
-    
+
         var h = time[1];
         var m = parseInt(time[2]) || null;
         var am = time[3];
@@ -184,18 +184,34 @@ otp.util.DateUtils = {
             am = "pm"
         else  
             am = "am"
-    
-        return  h + ":" + m + "" + am;
+
+        var space = "";
+        if(format.toLowerCase().charAt(format.length-2) == " ")
+            space = " ";
+
+        return  h + ":" + m + space + am;
+    },
+
+    /** time string parser / correction */
+    parseTime : function(time, format)
+    {
+        var retVal = time;
+        if(format && format.toLowerCase().charAt(format.length-1) == "a")
+        {
+            retVal = this.correctAmPmTimeString(time, format);
+        }
+        return retVal;
     },
 
     /** */
     parseTimeTest : function(t)
     {
         var times = ['1:00 pm','1:00 p.m.','100 p','1:00p.m.','1:00p','1 pm','1 p.m.','1 p','1pm','1p.m.','1p','1:pm','13:00','13','944am', '1354','12335','1232p'];
-    
+
         for ( var i = 0; i < times.length; i++ )
         {
-          console.log(parseTime(times[i]));
+          console.log(this.parseTime(times[i], 'g:i a'));
+          console.log(this.parseTime(times[i], 'H:i'));
         }
     },
 