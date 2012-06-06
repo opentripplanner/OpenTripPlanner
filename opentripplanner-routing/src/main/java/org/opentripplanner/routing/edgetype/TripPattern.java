c04a49d5f1f87d8774aaed783b0e054ad7f5ba9a
novalis <novalis@openplans.org>
2012-06-06 12:17:24 -0400

    make unnecesary data transient

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/TripPattern.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/TripPattern.java
@@ -2,9 +2,12 @@ package org.opentripplanner.routing.edgetype;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlTransient;
+
 import org.onebusaway.gtfs.model.Stop;
 
 /* simple interface for trip patterns */
 public interface TripPattern {
+    @XmlTransient
     List<Stop> getStops();
 }
