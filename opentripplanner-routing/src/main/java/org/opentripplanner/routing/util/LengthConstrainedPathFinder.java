5b102f700dc4d961ce81feeeb0db095cdffbd315
Andrew Byrd <andrew@fastmail.net>
2012-06-07 18:22:54 +0200

    update comment and imports

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/util/LengthConstrainedPathFinder.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/util/LengthConstrainedPathFinder.java
@@ -1,10 +1,7 @@
 package org.opentripplanner.routing.util;
 
 import java.security.InvalidParameterException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -12,29 +9,36 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.Stack;
-
 import org.opentripplanner.common.pqueue.BinHeap;
 import org.opentripplanner.routing.graph.Edge;
 
 /**
  * Path finder for use in debugging and testing the vehicle location inference
- * engine. 
+ * engine. Specs from Brandon Willard: Given an origin and destination edge,
+ * find all paths of a given length that could connect the two. The path length
+ * is to be interpreted as being exact, but some degree of tolerance is provided
+ * by the fact that edges have length. The complexity is exponential in path
+ * length. However, these methods will only be used over timesteps of around 30
+ * seconds, so search space should still be limited in practice. The paths are
+ * not necessarily needed - only the edges that make up those paths are
+ * essential. This fact can be exploited to reduce complexity by not storing
+ * back pointers and just keeping a set of edges touched. Rather than finding
+ * all paths with some exact length, we can also just place an upper bound on
+ * shortest path length, and include all edges that are in any such shortest
+ * path. We may actually want to index turn *vertices* and use them to begin at
+ * all turn edges in a bundle. 
  * 
- * Specs from Brandon Willard: Given an origin and destination edge,
- * find all paths of a given length that could connect the two. The path length is to
- * be interpreted as being exact, but some degree of tolerance is provided by the
- * fact that edges have length. If cycles are allowed in the solutions, the
- * complexity is exponential in path length. However, these methods will only be used over 
- * timesteps of around 30 seconds, so search space should still be limited in practice.
+ * Apparently skipping paths containing cycles is in fact not a win. Tracking which 
+ * edges have been visited with a multiset doubles the run time, and runtime remains
+ * almost unchanged when using a plain old hashset. There are just not that many 
+ * solution paths with repeated loops to eliminate! 
  * 
- * The paths are not necessarily needed - only the edges that make up those
- * paths are essential. This fact can be exploited to reduce complexity by not storing back
- * pointers and just keeping a set of edges touched. Rather than finding all
- * paths with some exact length, we can also just place an upper bound on shortest path
- * length, and include all edges that are in any such shortest path. 
- * 
- * We may actually want to index turn *vertices* and use them to begin at all turn edges in a bundle.
+ * This is likely because the length of loops must be "tuned" just right to hit the 
+ * target edge given the distance constraint. In a naive recursive enumeration, these
+ * useless loops would still be followed in circles until the target length is
+ * exceeded. Our pre-computed lower bound on distance to the destination helps
+ * prune paths containing repeated occurrences of "out of tune" loops before
+ * they branch out of control.
  * 
  * @author abyrd
  */
