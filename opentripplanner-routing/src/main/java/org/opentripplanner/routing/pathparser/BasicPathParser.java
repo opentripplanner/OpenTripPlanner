1beba76844ba12ebcf157edb2d303c955aa5eabc
novalis <novalis@openplans.org>
2012-06-08 15:57:17 -0400

    formatting

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/pathparser/BasicPathParser.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/pathparser/BasicPathParser.java
@@ -17,67 +17,69 @@ import org.opentripplanner.routing.vertextype.StreetVertex;
 import static org.opentripplanner.routing.automata.Nonterminal.*;
 
 /**
- * Some goals to build out for this path parser:
- * Reject paths that use crosswalks to 
- * Reject paths that take shortcuts through stops, by disallowing series of STL and requiring a transit trip after entering a station.  
- * Reject paths that reach the destination with a rented bicycle.
- * Disallow boarding transit without first parking a car.
- * Reject breaking no-through-traffic rules on driving legs.
+ * Some goals to build out for this path parser: Reject paths that use crosswalks to Reject paths
+ * that take shortcuts through stops, by disallowing series of STL and requiring a transit trip
+ * after entering a station. Reject paths that reach the destination with a rented bicycle. Disallow
+ * boarding transit without first parking a car. Reject breaking no-through-traffic rules on driving
+ * legs.
  */
 public class BasicPathParser extends PathParser {
 
-	private static final int STATION = 1;
-	private static final int TRANSIT = 2;
-	//3,4,5 come from StreetEdge.java
-	
-	private static final DFA DFA;
-	static {
-	    
-	        Nonterminal bikeNonStreet = star(choice(StreetEdge.CLASS_CROSSING, StreetEdge.CLASS_OTHERPATH));
-	        
-	        //(C|O)*(S+O(C|O)*)*(S*(C|O)*) -- the inverse of S+C+S+
-	        Nonterminal optionalNontransitLeg = 
-	                seq(bikeNonStreet, 
-	                    star(plus(StreetEdge.CLASS_STREET), StreetEdge.CLASS_OTHERPATH, bikeNonStreet), 
-	                    seq(star(StreetEdge.CLASS_STREET),bikeNonStreet));
-	                
-		Nonterminal transitLeg = seq(plus(STATION), plus(TRANSIT), plus(STATION));
-		Nonterminal itinerary = seq(optionalNontransitLeg, star(transitLeg, optionalNontransitLeg));
-		DFA = itinerary.toDFA().minimize();
-		System.out.println(DFA.toGraphViz());
-		System.out.println(DFA.dumpTable());
-	}
-	
-	@Override
-	protected DFA getDFA() {
-		return DFA;
-	}
-	
-	@Override
-	public int terminalFor(State state) {
-		Vertex v = state.getVertex();
-		if (v instanceof StreetVertex || v instanceof StreetLocation) {
-		    TraverseModeSet modes = state.getOptions().getModes();
-		    if (modes.contains(TraverseMode.BICYCLE) && (!modes.contains(TraverseMode.WALK) || !state.isBikeRenting())) {
-		        Edge edge = state.getBackEdge();
-			if (edge instanceof StreetEdge) {
-			    int cls = ((StreetEdge) edge).getStreetClass();
-			    return cls;
-			} else {
-			    return StreetEdge.CLASS_OTHERPATH;
-			}
-		    } else {
-		        return StreetEdge.CLASS_OTHERPATH;
-		    }
-		}
-		if (v instanceof OnboardVertex)
-			return TRANSIT;
-		if (v instanceof OffboardVertex)
-			return STATION;
-		if (v instanceof BikeRentalStationVertex)
+    private static final int STATION = 1;
+
+    private static final int TRANSIT = 2;
+
+    // 3,4,5 come from StreetEdge.java
+
+    private static final DFA DFA;
+    static {
+
+        Nonterminal bikeNonStreet = star(choice(StreetEdge.CLASS_CROSSING,
+                StreetEdge.CLASS_OTHERPATH));
+
+        // (C|O)*(S+O(C|O)*)*(S*(C|O)*) -- the inverse of S+C+S+
+        Nonterminal optionalNontransitLeg = seq(bikeNonStreet,
+                star(plus(StreetEdge.CLASS_STREET), StreetEdge.CLASS_OTHERPATH, bikeNonStreet),
+                seq(star(StreetEdge.CLASS_STREET), bikeNonStreet));
+
+        Nonterminal transitLeg = seq(plus(STATION), plus(TRANSIT), plus(STATION));
+        Nonterminal itinerary = seq(optionalNontransitLeg, star(transitLeg, optionalNontransitLeg));
+        DFA = itinerary.toDFA().minimize();
+        System.out.println(DFA.toGraphViz());
+        System.out.println(DFA.dumpTable());
+    }
+
+    @Override
+    protected DFA getDFA() {
+        return DFA;
+    }
+
+    @Override
+    public int terminalFor(State state) {
+        Vertex v = state.getVertex();
+        if (v instanceof StreetVertex || v instanceof StreetLocation) {
+            TraverseModeSet modes = state.getOptions().getModes();
+            if (modes.contains(TraverseMode.BICYCLE)
+                    && (!modes.contains(TraverseMode.WALK) || !state.isBikeRenting())) {
+                Edge edge = state.getBackEdge();
+                if (edge instanceof StreetEdge) {
+                    int cls = ((StreetEdge) edge).getStreetClass();
+                    return cls;
+                } else {
                     return StreetEdge.CLASS_OTHERPATH;
-		else 
-			throw new RuntimeException("failed to tokenize path");
-	}
-	
+                }
+            } else {
+                return StreetEdge.CLASS_OTHERPATH;
+            }
+        }
+        if (v instanceof OnboardVertex)
+            return TRANSIT;
+        if (v instanceof OffboardVertex)
+            return STATION;
+        if (v instanceof BikeRentalStationVertex)
+            return StreetEdge.CLASS_OTHERPATH;
+        else
+            throw new RuntimeException("failed to tokenize path");
+    }
+
 }
