e0754bd80bee672d41069466c4caf231c0049009
abyrd <abyrd@bijlmermeer>
2012-07-18 22:04:03 -0400

    formatting

--- opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/batch/CSVPopulation.java
+++ opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/batch/CSVPopulation.java
@@ -14,34 +14,43 @@ public class CSVPopulation extends BasicPopulation {
 
     private static final Logger LOG = LoggerFactory.getLogger(BasicPopulation.class);
 
-	@Setter public int latCol = 0;
-	@Setter public int lonCol = 1;
-	@Setter public int labelCol = 2;
-	@Setter public int inputCol = 3;
-	@Setter public boolean headers = true;
-	
-	@PostConstruct
-	public void loadIndividuals() {
-		try {
-			CSVReader reader = new CSVReader(new FileReader(sourceFilename));
-		    String [] nextLine;
-		    if (headers) {
-		    	reader.readNext();
-		    }
-		    while ((nextLine = reader.readNext()) != null) {
-		    	double lat = Double.parseDouble(nextLine[latCol]);
-		    	double lon = Double.parseDouble(nextLine[lonCol]);
-		    	String label = nextLine[labelCol];
-		    	Double input = Double.parseDouble(nextLine[inputCol]);
-		    	Individual individual = individualFactory.build(label, lon, lat, input);
-		    	this.add(individual);
-		    	//LOG.debug(individual.toString());
-		    }
-		    reader.close();
-		} catch (Exception e) {
-			LOG.error("exception while loading individuals from CSV file:");
-			e.printStackTrace();
-		}
-	}
-	
+    @Setter
+    public int latCol = 0;
+
+    @Setter
+    public int lonCol = 1;
+
+    @Setter
+    public int labelCol = 2;
+
+    @Setter
+    public int inputCol = 3;
+
+    @Setter
+    public boolean headers = true;
+
+    @PostConstruct
+    public void loadIndividuals() {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(sourceFilename));
+            String[] nextLine;
+            if (headers) {
+                reader.readNext();
+            }
+            while ((nextLine = reader.readNext()) != null) {
+                double lat = Double.parseDouble(nextLine[latCol]);
+                double lon = Double.parseDouble(nextLine[lonCol]);
+                String label = nextLine[labelCol];
+                Double input = Double.parseDouble(nextLine[inputCol]);
+                Individual individual = individualFactory.build(label, lon, lat, input);
+                this.add(individual);
+                // LOG.debug(individual.toString());
+            }
+            reader.close();
+        } catch (Exception e) {
+            LOG.error("exception while loading individuals from CSV file:");
+            e.printStackTrace();
+        }
+    }
+
 }
