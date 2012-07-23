2c612ad4bcf69d44109cd74907bbf2bffade0e3a
Andrew Byrd <abyrd@openplans.org>
2012-07-23 22:52:31 +0200

    explicitly call run method of BatchProcessor after context loaded

--- opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/batch/BatchProcessor.java
+++ opentripplanner-analyst/src/main/java/org/opentripplanner/analyst/batch/BatchProcessor.java
@@ -23,6 +23,24 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
+/* to cover several kinds of batch requests, there are two modes: agg and non-agg.
+ * The batch processor chooses a mode based on whether the aggregator property has been
+ * set or not.
+ * 
+ * In either mode, both the source and target population properties must be set.
+ * The batch analysis is always carried out as a loop over the source set.
+ * 
+ * In aggregate mode, the supplied aggregate function is evaluated over the target set for 
+ * every element of the source set. The resulting aggregate value is associated with the
+ * origin individual that produced it, and the entire set of aggregates are saved together 
+ * in a format appropriate for that population type. 
+ * Thus, aggregate mode produces a single output object/stream/buffer, containing one unit 
+ * of output (tuple/line/pixel) per individual in the source set.
+ * 
+ * In non-aggregate mode, one output object/stream/buffer is produced per source location.
+ * Thus, for S sources and D destinations, S output objects will be produced, each
+ * containing D data items.
+ */
 @Data
 public class BatchProcessor {
 
@@ -50,36 +68,10 @@ public class BatchProcessor {
         xmlReader.loadBeanDefinitions(new ClassPathResource(CONFIG));
         ctx.refresh();
         ctx.registerShutdownHook();
-        //ctx.getBean(BatchProcessor.class).run();
+        ctx.getBean(BatchProcessor.class).run();
     }
 
-    // actually probably better to run explicitly, so batch processor will do nothing unless it
-    // is run intentionally
-    @PostConstruct
-    private void runAfterSetters() {
-        /* to cover several kinds of batch requests, there are two modes: agg and non-agg.
-         * The batch processor chooses a mode based on whether the aggregator property has been
-         * set or not.
-         * AggregateOrigins=false
-         * 
-         * In either mode, both the source and target population properties must be set.
-         * The batch analysis is always carried out as a loop over the source set.
-         * 
-         * In aggregate mode, the supplied aggregate function is evaluated over the target set for 
-         * every element of the source set. The resulting aggregate value is associated with the
-         * origin individual that produced it, and the entire set of aggregates are saved together 
-         * in a format appropriate for that population type. 
-         * Thus, aggregate mode produces a single output object/stream/buffer, containing one unit 
-         * of output (tuple/line/pixel) per individual in the source set.
-         * 
-         * In non-aggregate mode, one output object/stream/buffer is produced per source location.
-         * Thus, for S sources and D destinations, S output objects will be produced, each
-         * containing D data items.
-         * 
-         * default TraverseOptions can be supplied.
-         * 
-         * Aggregate over origins or destinations option
-         */
+    private void run() {
         int nOrigins = origins.getIndividuals().size();
         if (aggregator == null) {
             if (nOrigins > 1 && !outputPath.contains("{}")) {
