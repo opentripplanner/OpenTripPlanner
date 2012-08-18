daf970016d1fe1ae53443a282bfbc4eb2de70921
Andrew Byrd <andrew@fastmail.net>
2012-08-18 14:20:27 +0200

    produce and store snapshots with frequency limiting
    with a little synchronization we could produce them on demand

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/TimetableSnapshot.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/TimetableSnapshot.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.opentripplanner.routing.core.RoutingContext;
 import org.opentripplanner.routing.edgetype.TableTripPattern.Timetable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // this is only currently in edgetype because that's where Trippattern is. move these classes elsewhere.
 
@@ -21,29 +24,34 @@ import org.opentripplanner.routing.edgetype.TableTripPattern.Timetable;
  */
 public class TimetableSnapshot {
     
+    private static final Logger LOG = LoggerFactory.getLogger(TimetableSnapshot.class);
+
     // Use HashMap not Map so we can clone.
+    // if this turns out to be slow/spacious we can use an array with integer pattern indexes
     private HashMap<TableTripPattern, Timetable> timetables = new HashMap<TableTripPattern, Timetable>(); 
     
     private Set<Timetable> dirty = new HashSet<Timetable>();
     
     /** Returns an updated timetable for the specified pattern if one is available in this snapshot, 
-     * or the originally scheduled timetable if there are no updates forin this snapshot. */
+     * or the originally scheduled timetable if there are no updates in this snapshot. */
     public Timetable resolve(TableTripPattern pattern) {
         Timetable timetable = timetables.get(pattern);
-        if (timetable == null)
+        if (timetable == null) {
             return pattern.scheduledTimetable;
-        else
+        } else {
+            //LOG.debug("returning modified timetable");
             return timetable;
+        }
     }
     
-    protected Timetable modify(TableTripPattern pattern) {
-        // check if this snapshot is already sealed in stone
-        if (dirty == null) 
+    public Timetable modify(TableTripPattern pattern) {
+        if (dirty == null) // this snapshot was already committed for reading
             throw new ConcurrentModificationException();
         Timetable existing = resolve(pattern);
         if (dirty.contains(existing)) {
-            return existing;
+            return existing; // allows modifying multiple trips on a single pattern
         } else {
+            // OR fresh = pattern.scheduledTimetable.copy();
             Timetable fresh = existing.copy();
             timetables.put(pattern, fresh);
             dirty.add(fresh);
@@ -51,12 +59,17 @@ public class TimetableSnapshot {
         }        
     }
     
-    protected void doneModifying() {
+    public void commit() {
         // summarize, index, etc. the new timetables
         for (Timetable tt : dirty)
             tt.finish();
         // mark this snapshot as henceforth immutable
         dirty = null;
+    }
+    
+    public String toString() {
+        String d = dirty == null ? "committed" : String.format("%d dirty", dirty.size());
+        return String.format("Timetable snapshot: %d timetables (%s)", timetables.size(), d);
     }
     
     @SuppressWarnings("unchecked")
