92f7d3ed134fea429c797e618f1f13856f9ac705
Matt Conway <mattwigway@gmail.com>
2012-08-20 16:08:18 -0700

    This should have been in the last commit. #803.

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/ElevatorEdge.java
@@ -0,0 +1,9 @@
+package org.opentripplanner.routing.edgetype;
+
+/**
+ * Marker interface indicating that an edge is part of an elevator.
+ * 
+ * @author mattwigway
+ */
+public interface ElevatorEdge {
+}
