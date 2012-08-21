c8f2bfdb42fa0e20c7a160425d8657019a8a804a
Andrew Byrd <andrew@fastmail.net>
2012-08-21 11:55:10 +0200

    add delegating TripTimes class for cancelled trips

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/trippattern/CancelledTripTimes.java
@@ -0,0 +1,21 @@
+package org.opentripplanner.routing.trippattern;
+
+/**
+ * A CancelledTripTimes represents the cancellation of an entire trip by reporting that the vehicle
+ * has already passed all stops.
+ */
+public class CancelledTripTimes extends DelegatingTripTimes implements TripTimes {
+
+    public CancelledTripTimes(ScheduledTripTimes sched) {
+        super(sched);
+    }
+
+    @Override public int getDepartureTime(int hop) {
+        return TripTimes.PASSED;
+    }
+    
+    @Override public int getArrivalTime(int hop) {
+        return TripTimes.PASSED;
+    }
+        
+}
