3ea8a721cbb31e02c9b841bf6aba3b74555c30b2
Andrew Byrd <andrew@fastmail.net>
2012-08-21 14:48:06 +0200

    add exponential decay parameter to DelayedTripTimes

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/trippattern/DecayingDelayTripTimes.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/trippattern/DecayingDelayTripTimes.java
@@ -2,33 +2,41 @@ package org.opentripplanner.routing.trippattern;
 
 /**
  * An DelayedTripTimes applies an offset to arrival and departure times based on a report that a
- * vehicle is a given number of seconds early or late, and optionally reports that the vehicle has 
+ * vehicle is a given number of seconds early or late, and reports that the vehicle has 
  * passed all stops up to a certain point based on a report of vehicle location.
  */
-public class DelayedTripTimes extends DelegatingTripTimes implements TripTimes {
+public class DecayingDelayTripTimes extends DelegatingTripTimes implements TripTimes {
 
     private final int currentStop;
-    
     private final int delay;
+    private final double k;
+    // compute decay lookup table?
     
-    public DelayedTripTimes(ScheduledTripTimes sched, int delay, int currentStop) {
+    public DecayingDelayTripTimes(ScheduledTripTimes sched, int currentStop, int delay, double decay) {
         super(sched);
         this.delay = delay;
         this.currentStop = currentStop;
+        this.k = decay;
     }
 
     @Override public int getDepartureTime(int hop) {
         int stop = hop;
         if (stop < currentStop)
             return TripTimes.PASSED;
-        return super.getDepartureTime(hop) + delay;
+        return super.getDepartureTime(hop) + decayedDelay(stop);
     }
     
     @Override public int getArrivalTime(int hop) {
         int stop = hop + 1;
         if (stop < currentStop)
             return TripTimes.PASSED;
-        return super.getArrivalTime(hop) + delay;
+        return super.getArrivalTime(hop) + decayedDelay(stop);
     }
         
+    private int decayedDelay(int stop) {
+        int n = stop - currentStop;
+        double decay = Math.pow(k, n);
+        return (int) (decay * delay);
+    }
+    
 }
