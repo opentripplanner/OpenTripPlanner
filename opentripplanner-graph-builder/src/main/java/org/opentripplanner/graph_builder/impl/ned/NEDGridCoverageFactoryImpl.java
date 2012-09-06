3c55fec0cf1a7cd1bcc26c23e6aae4b04a62c377
novalis <novalis@openplans.org>
2012-09-06 17:24:41 -0400

    fix #826 (I hope)

--- opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/ned/NEDGridCoverageFactoryImpl.java
+++ opentripplanner-graph-builder/src/main/java/org/opentripplanner/graph_builder/impl/ned/NEDGridCoverageFactoryImpl.java
@@ -18,13 +18,17 @@ import java.util.List;
 
 import javax.media.jai.InterpolationBilinear;
 
+import org.geotools.coverage.AbstractCoverage;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.Interpolator2D;
+import org.geotools.coverage.processing.CoverageProcessor;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.coverage.Coverage;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opentripplanner.graph_builder.services.ned.NEDGridCoverageFactory;
 import org.opentripplanner.graph_builder.services.ned.NEDTileSource;
 import org.opentripplanner.routing.graph.Graph;
-import org.opentripplanner.routing.services.GraphService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -64,6 +68,15 @@ public class NEDGridCoverageFactoryImpl implements NEDGridCoverageFactory {
                 factory.setPath(path);
                 GridCoverage2D regionCoverage = Interpolator2D.create(factory.getGridCoverage(),
                         new InterpolationBilinear());
+
+                CoverageProcessor processor = CoverageProcessor.getInstance();
+                ParameterValueGroup param = processor.getOperation("Resample").getParameters();
+                param.parameter("Source").setValue(regionCoverage);
+                CoordinateReferenceSystem wgs84 = DefaultGeographicCRS.WGS84;
+
+                param.parameter("CoordinateReferenceSystem").setValue(wgs84);
+                regionCoverage = (GridCoverage2D) processor.doOperation(param);
+
                 if (coverage == null) {
                     coverage = new UnifiedGridCoverage("unified", regionCoverage);
                 } else {
