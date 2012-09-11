900effcfc3a39a3ee21273ab7b4dfe54c1e6fafd
Andrew Byrd <andrew@fastmail.net>
2012-09-12 01:16:52 +0200

    remove duplicate indexing calls

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/factory/GTFSPatternHopFactory.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/edgetype/factory/GTFSPatternHopFactory.java
@@ -389,11 +389,11 @@ public class GTFSPatternHopFactory {
         loadTransfers(graph);
         if (_deleteUselessDwells) 
             deleteUselessDwells(graph);
-        /* this is the wrong place to do this: it should be done on all feeds at once, or at deserialization*/
-        _log.info("begin indexing large patterns");
-        for (TableTripPattern tp : context.tripPatternIds.keySet()) {
-            tp.finish();
-        }
+//        /* this is the wrong place to do this: it should be done on all feeds at once, or at deserialization*/
+//        _log.info("begin indexing large patterns");
+//        for (TableTripPattern tp : context.tripPatternIds.keySet()) {
+//            tp.finish();
+//        }
         _log.info("end indexing large patterns");
         clearCachedData();
         graph.putService(FareService.class, fareServiceFactory.makeFareService());
