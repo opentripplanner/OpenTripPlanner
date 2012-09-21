82a4bbfc576be7c102e89af760cf627026ad7c23
novalis <novalis@openplans.org>
2012-09-21 15:46:56 -0400

    ignore re-found paths instead of removing them

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/raptor/TargetBound.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/raptor/TargetBound.java
@@ -117,6 +117,12 @@ public class TargetBound implements SearchTerminationStrategy, SkipTraverseResul
     public void addBounder(State bounder) {
         for (Iterator<State> it = bounders.iterator(); it.hasNext(); ) {
             State old = it.next();
+
+            //exact dup
+            if (old.getNumBoardings() == bounder.getNumBoardings()
+                && old.getTime() == bounder.getTime()
+                && old.getWalkDistance() == bounder.getWalkDistance())
+            return;
             if (bounder.dominates(old)) {
                 it.remove();
                 removedBoundingStates.add(old);
@@ -201,10 +207,6 @@ public class TargetBound implements SearchTerminationStrategy, SkipTraverseResul
         int i = 0;
         boolean prevBounded = !bounders.isEmpty();
         for (State bounder : bounders) {
-            if (bounder.getWalkDistance() > options.getMaxWalkDistance() && current.getWalkDistance() + minWalk < options.getMaxWalkDistance()) {
-                continue;
-            }
-            
             if (current.getWeight() + minTime + walkTime * (options.getWalkReluctance() - 1) > bounder.getWeight() * WORST_WEIGHT_DIFFERENCE_FACTOR) {
                 return true;
             }
