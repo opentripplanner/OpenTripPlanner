81963f5b746f2e086469c5258afd821414632306
novalis <novalis@openplans.org>
2012-10-03 12:57:13 -0400

    trim extra itineraries

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/raptor/Raptor.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/raptor/Raptor.java
@@ -219,6 +219,8 @@ public class Raptor implements PathService {
         }
         Collections.sort(targetStates);
 
+        targetStates = targetStates.subList(0, options.getNumItineraries());
+
         List<GraphPath> paths = new ArrayList<GraphPath>();
         for (RaptorState targetState : targetStates) {
             // reconstruct path
