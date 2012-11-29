b73c6d3404f527c6059afce14cc3d52a0f6d5284
novalis <novalis@openplans.org>
2012-11-29 16:08:49 -0500

    Revert "remove debugging cruft"
    This reverts commit dffeaf6a1d20e18293541147265e9e422e9f9564.

--- opentripplanner-datastore/app/Bootstrap.java
+++ opentripplanner-datastore/app/Bootstrap.java
@@ -9,8 +9,14 @@ public class Bootstrap extends Job {
  
     public void doJob() {
         // Check if the database is empty
+        //User user = User.all().first();
+        User.em().createQuery("DELETE FROM User u").executeUpdate();
+
         if(User.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
+            System.out.println("HERE");
+        } else {
+            System.out.println("There are users");
         }
     }
  
