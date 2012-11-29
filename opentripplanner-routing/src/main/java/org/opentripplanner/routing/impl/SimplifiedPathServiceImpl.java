8dae82472e6729b2fc7eec3327b5bdf18df13670
Andrew Byrd <andrew@fastmail.net>
2012-11-29 21:48:59 +0100

    implement pathparser

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/SimplifiedPathServiceImpl.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/SimplifiedPathServiceImpl.java
@@ -13,27 +13,37 @@
 
 package org.opentripplanner.routing.impl;
 
-import java.util.Collections;
+import static org.opentripplanner.routing.automata.Nonterminal.choice;
+import static org.opentripplanner.routing.automata.Nonterminal.plus;
+import static org.opentripplanner.routing.automata.Nonterminal.seq;
+import static org.opentripplanner.routing.automata.Nonterminal.star;
+
 import java.util.List;
 
 import lombok.Setter;
 
-import org.opentripplanner.routing.algorithm.strategies.DefaultRemainingWeightHeuristic;
 import org.opentripplanner.routing.algorithm.strategies.ThreadedBidirectionalHeuristic;
 import org.opentripplanner.routing.automata.DFA;
+import org.opentripplanner.routing.automata.Nonterminal;
 import org.opentripplanner.routing.core.RoutingRequest;
 import org.opentripplanner.routing.core.State;
+import org.opentripplanner.routing.edgetype.SimpleTransfer;
+import org.opentripplanner.routing.edgetype.StreetEdge;
+import org.opentripplanner.routing.edgetype.StreetTransitLink;
+import org.opentripplanner.routing.graph.Edge;
+import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.routing.pathparser.PathParser;
 import org.opentripplanner.routing.services.GraphService;
 import org.opentripplanner.routing.services.PathService;
 import org.opentripplanner.routing.services.SPTService;
 import org.opentripplanner.routing.spt.GraphPath;
 import org.opentripplanner.routing.spt.ShortestPathTree;
+import org.opentripplanner.routing.vertextype.OffboardVertex;
+import org.opentripplanner.routing.vertextype.OnboardVertex;
+import org.opentripplanner.routing.vertextype.StreetVertex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.google.common.collect.Lists;
 
 public class SimplifiedPathServiceImpl implements PathService {
 
@@ -51,8 +61,6 @@ public class SimplifiedPathServiceImpl implements PathService {
     @Override
     public List<GraphPath> getPaths(RoutingRequest options) {
 
-        List<GraphPath> paths = Lists.newArrayList();
-
         if (options == null) {
             LOG.error("PathService was passed a null routing request.");
             return null;
@@ -60,14 +68,15 @@ public class SimplifiedPathServiceImpl implements PathService {
 
         if (options.rctx == null) {
             options.setRoutingContext(graphService.getGraph(options.getRouterId()));
-            options.rctx.pathParsers = new PathParser[] {};
+            options.rctx.pathParsers = new PathParser[] { new Parser() };
         }
 
         options.setMaxTransfers(Integer.MAX_VALUE);
         options.setMaxWalkDistance(Double.MAX_VALUE);
         //options.setMaxWalkDistance(60000);
         options.setWalkBoardCost(60*20);
-        options.setBikeBoardCost(60*25);
+        options.setBikeBoardCost(60*20);
+        options.setWalkReluctance(1.25);
         options.setTransferSlack(60*4);
         options.setBoardSlack(60*2);
         options.setAlightSlack(60*2);
@@ -87,35 +96,95 @@ public class SimplifiedPathServiceImpl implements PathService {
             LOG.warn("SPT was null.");
             return null;
         }
-        paths = spt.getPaths();
-        if (paths == null) {
-            LOG.warn("Paths was null.");
+        spt.getPaths().get(0).dump();
+        return spt.getPaths();
+        
+        /*
+        List<? extends State> states = spt.getStates(options.rctx.target);
+        if (states == null) {
+            LOG.warn("no states.");
             return null;
         }
-        if (paths.size() == 0) {
-            LOG.warn("Paths was 0-length.");
+        if (states.size() == 0) {
+            LOG.warn("0-length state list.");
             return null;
         }
+        State s = states.get(0);
+        options = options.clone();
+        options.setArriveBy( ! options.isArriveBy());
+        options.dateTime = s.getTime();
+        options.rctx = new RoutingContext(options, options.rctx.graph, 
+                options.rctx.fromVertex, options.rctx.toVertex, false);
+        
+        // existing heuristic has been aborted
+        heuristic = new ThreadedBidirectionalHeuristic(options.rctx.graph);
+        options.rctx.remainingWeightHeuristic = heuristic;
+        
+        searchBeginTime = System.currentTimeMillis();
+        LOG.debug("BEGIN REVERSE SEARCH");
+        spt = sptService.getShortestPathTree(options, timeout);
+        LOG.debug("END SEARCH ({} msec)", System.currentTimeMillis() - searchBeginTime);
+        heuristic.abort();
+        
         // We order the list of returned paths by the time of arrival or departure (not path duration)
         //Collections.sort(paths, new PathComparator(options.isArriveBy()));
-        // paths.get(0).dump();
+        //return Arrays.asList(path);
+        List<GraphPath> paths = spt.getPaths(options.rctx.target, false); 
+        paths.get(0).dump();
         return paths;
+        */
     }
 
     public static class Parser extends PathParser {
 
-        @Override
-        public int terminalFor(State state) {
-            // TODO Auto-generated method stub
-            return 0;
+        private static final int STREET = 1;
+        private static final int LINK = 2;
+        private static final int STATION = 3;
+        private static final int RIDE = 4;
+        private static final int XFER = 5;
+
+        private static final DFA DFA;
+
+        static {
+            Nonterminal nontransitLeg = plus(STREET);
+            Nonterminal transitLeg = seq(plus(STATION), plus(RIDE), plus(STATION));
+            Nonterminal transitItinerary = seq(
+                    nontransitLeg, 
+                    LINK, transitLeg, star(XFER, transitLeg), LINK, 
+                    nontransitLeg);
+            Nonterminal itinerary = choice(nontransitLeg, transitItinerary);
+            DFA = itinerary.toDFA().minimize();
+            System.out.println(DFA.toGraphViz());
+            System.out.println(DFA.dumpTable());
         }
 
         @Override
         protected DFA getDFA() {
-            // TODO Auto-generated method stub
-            return null;
+            return DFA;
         }
-        
+
+        @Override
+        public int terminalFor(State state) {
+            Vertex v = state.getVertex();
+            Edge e = state.getBackEdge();
+            if (e == null || e instanceof StreetEdge)
+                return STREET;
+            if (e instanceof StreetTransitLink)
+                return LINK;
+            if (e instanceof SimpleTransfer)
+                return XFER;
+            if (v instanceof OnboardVertex)
+                return RIDE;
+            if (v instanceof OffboardVertex)
+                return STATION;
+            if (v instanceof StreetVertex)
+                return STREET;
+            else {
+                LOG.debug("failed to tokenize path. vertex {} edge {}", v, e);
+                throw new RuntimeException("failed to tokenize path");
+            }
+        }
+
     }
     
 }
