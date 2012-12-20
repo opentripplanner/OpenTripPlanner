042568fea7dde2de19e3a93d5c823eb024c60036
David Emory <demory@openplans.org>
2012-12-19 21:46:36 -0500

    call-based history: backend work

--- opentripplanner-datastore/app/controllers/Application.java
+++ opentripplanner-datastore/app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.google.gson.Gson;
 import play.*;
 import play.cache.*;
 import play.mvc.*;
@@ -12,13 +13,37 @@ public class Application extends Controller {
 
     @Before
     public static void setCORS()  {
-        Http.Header hd = new Http.Header();
-        hd.name = "Access-Control-Allow-Origin";
-        hd.values = new ArrayList<String>();
-        hd.values.add("*");
-        Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);      
-    }
+        Http.Header origin = new Http.Header();
+        origin.name = "Access-Control-Allow-Origin";
+        origin.values = new ArrayList<String>();
+        origin.values.add("*");
+        Http.Response.current().headers.put("Access-Control-Allow-Origin",origin);      
+        
+        Http.Header headers = new Http.Header();
+        headers.name = "Access-Control-Allow-Headers";
+        headers.values = new ArrayList<String>();
+        headers.values.add("Origin, X-Requested-With, Content-Type, Accept");
+                
+        //headers.values.add("Origin");
+        //headers.values.add("X-Requested-With");
+        //headers.values.add("Accept");
+       
+        Http.Response.current().headers.put("Access-Control-Allow-Headers",headers);      
 
+    }
+    
+    /*@Util
+    public static void renderJSON(Object obj) {
+        if (request.params._contains("callback")) {
+            Gson gson = new Gson();
+            String json = gson.toJson(obj);
+            //System.out.println("returning as jsonp (u): "+json);
+            renderText(request.params.get("callback") + "(" + json + ")");            
+        } else {
+            renderJSON(obj);
+        }      
+    }*/
+    
     @Before(priority=0)
     public static void checkPassword() {
         request.user = null;
--- opentripplanner-datastore/app/controllers/Calltaker.java
+++ opentripplanner-datastore/app/controllers/Calltaker.java
@@ -1,48 +1,74 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.calltaker.Call;
 
 import java.util.*;
 
-import models.*;
+import models.calltaker.TripQuery;
+import play.data.binding.As;
 
 public class Calltaker extends Application {
-    
-  @Before
-    public static void setCORS()  {
-        Http.Header hd = new Http.Header();
-        hd.name = "Access-Control-Allow-Origin";
-        hd.values = new ArrayList<String>();
-        hd.values.add("*");
-        Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);      
-    }
-    
+
     public static void index() {
-        List<OTPQuery> queries = OTPQuery.all().fetch(10);
-        render(queries);
+        //List<OTPQuery> queries = OTPQuery.all().fetch(10);
+        //render(queries);
+        //List<Call> calls = Call.all().fetch(10);
+        List<TripQuery> items = TripQuery.all().fetch(10);
+        render(items);
+
     }
     
-    public static void getQueries(String userName, Integer limit) {
-        List<OTPQuery> queries;
+    public static void options() {        
+    }
+    
+    /*public static void newCall(String userName, 
+            @As("yyyy-MM-dd'T'HH:mm:ss") Date startTime,
+            @As("yyyy-MM-dd'T'HH:mm:ss") Date endTime) {
+        System.out.println("newCall startTime="+startTime);
+        Call c = new Call(userName, startTime, endTime);
+        c.save();
+        renderJSON(c.id);
+    }*/
+    
+    public static void newCall(Call call) {
+        call.userName = params.get("userName");
+        call.save();
+        renderJSON(call.id);
+    }
+    
+
+    public static void getCall(String userName, Integer limit) {
+        System.out.println("getCall");
+        List<Call> calls;
         if(limit == null)
-            queries = OTPQuery.find("userName", userName).fetch();
+            calls = Call.find("userName = '"+userName+"' order by startTime").fetch();
         else {
-            queries = OTPQuery.find("userName = '"+userName+"' order by timeStamp desc").fetch(limit);
+            calls = Call.find("userName = '"+userName+"' order by startTime").fetch(limit);
             System.out.println("fetched w/ limit = "+limit);
+        }
+
+        renderJSON(calls);
+    }
+
+    public static void getQuery(Call call, Integer limit) {
+        List<TripQuery> queries;
+        if(limit == null)
+            queries = TripQuery.find("call.id = '"+call.id+"' order by timeStamp").fetch();
+        else {
+            queries = TripQuery.find("call.id = '"+call.id+"' order by timeStamp").fetch(limit);
         }
         renderJSON(queries);
     }
     
-    public static void newQuery(String userName, String queryParams, String fromPlace, String toPlace) {
-        OTPQuery query = new OTPQuery(userName, queryParams, fromPlace, toPlace);
+    public static void newQuery(TripQuery query) {
+        System.out.println("nQ request params: " + request.params.allSimple());
+        query.userName = params.get("userName");
         query.save();
-        Long id = query.id;
-        render(userName, id);
+        renderJSON(query.id);
     }
 
     public static void deleteQuery(Long id) {
-        OTPQuery query = OTPQuery.findById(id);  
+        TripQuery query = TripQuery.findById(id);  
         query.delete();
         render(id);
     }  
