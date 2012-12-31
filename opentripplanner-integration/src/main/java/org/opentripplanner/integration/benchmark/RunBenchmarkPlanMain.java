e005a575c92a87a8f06769e4aba0c9b01fe2b3ea
Avi Flamholz <flamholz@ridewithvia.com>
2012-12-31 18:48:26 +0200

    Add a script that generates a benchmark plan from a graph rather than GTFS. Formatting fixes too.

--- opentripplanner-integration/src/main/java/org/opentripplanner/integration/benchmark/RunBenchmarkPlanMain.java
+++ opentripplanner-integration/src/main/java/org/opentripplanner/integration/benchmark/RunBenchmarkPlanMain.java
@@ -34,6 +34,8 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.opentripplanner.model.GraphBundle;
 import org.opentripplanner.routing.core.RoutingRequest;
 import org.opentripplanner.routing.error.VertexNotFoundException;
+import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.routing.graph.Graph.LoadLevel;
 import org.opentripplanner.routing.impl.SerializedGraphFactoryBean;
 import org.opentripplanner.routing.services.PathService;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -44,149 +46,155 @@ import org.springframework.core.io.ClassPathResource;
 
 public class RunBenchmarkPlanMain {
 
-  private static final String ARG_REPETITIONS = "repetitions";
+    private static final String ARG_REPETITIONS = "repetitions";
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    Options options = new Options();
-    options.addOption(ARG_REPETITIONS, true, "repetitions");
+        Options options = new Options();
+        options.addOption(ARG_REPETITIONS, true, "repetitions");
 
-    Parser parser = new GnuParser();
-    CommandLine cli = parser.parse(options, args);
-    args = cli.getArgs();
+        Parser parser = new GnuParser();
+        CommandLine cli = parser.parse(options, args);
+        args = cli.getArgs();
 
-    if (args.length != 2) {
-      System.err.println("usage: graph_path benchmark_path");
-      System.exit(-1);
-    }
-
-    RunBenchmarkPlanMain task = new RunBenchmarkPlanMain();
-    task.setGraphPath(new File(args[0]));
-    task.setBenchmarkPlan(new File(args[1]));
-
-    if (cli.hasOption(ARG_REPETITIONS))
-      task.setRepetitions(Integer.parseInt(cli.getOptionValue(ARG_REPETITIONS)));
-
-    task.run();
-  }
-
-  private File _graphPath;
-
-  private File _benchmarkPlan;
-
-  private int _repetitions = 1;
-
-  public void setGraphPath(File path) {
-    _graphPath = path;
-  }
-
-  public void setBenchmarkPlan(File benchmarkPlan) {
-    _benchmarkPlan = benchmarkPlan;
-  }
-
-  public void setRepetitions(int repetitions) {
-    _repetitions = repetitions;
-  }
-
-  public void run() throws Exception {
-    GenericApplicationContext context = getApplicationContext();
-    PathService service = (PathService) context.getBean("pathServiceImpl");
-
-    List<Plan> plans = readPlans();
-
-    long tTotal = 0;
-    for (int i = 0; i < _repetitions; i++) {
-      for (Plan plan : plans) {
-
-        String from = plan.latFrom + " " + plan.lonFrom;
-        String to = plan.latTo + " " + plan.lonTo;
-
-        try {
-          RoutingRequest opt = new RoutingRequest();
-          opt.setDateTime(plan.time);
-          opt.setFrom(from);
-          opt.setTo(to);
-          long t0 = System.currentTimeMillis();
-          service.getPaths(opt);
-          long t1 = System.currentTimeMillis();
-
-          System.out.println("t=" + (t1 - t0));
-          tTotal += t1 - t0;
-        } catch (VertexNotFoundException ex) {
-          System.out.println("no vertex: from=" + from + " to=" + to);
+        if (args.length != 2) {
+            System.err.println("usage: graph_path benchmark_path");
+            System.exit(-1);
         }
-      }
+
+        RunBenchmarkPlanMain task = new RunBenchmarkPlanMain();
+        task.setGraphPath(new File(args[0]));
+        task.setBenchmarkPlan(new File(args[1]));
+
+        if (cli.hasOption(ARG_REPETITIONS))
+            task.setRepetitions(Integer.parseInt(cli.getOptionValue(ARG_REPETITIONS)));
+
+        task.run();
     }
-    System.out.println("totalTime=" + tTotal);
-  }
 
-  private List<Plan> readPlans() throws FileNotFoundException, IOException,
-      ParseException {
-    BufferedReader reader = new BufferedReader(new FileReader(_benchmarkPlan));
-    String line = null;
-    List<Plan> plans = new ArrayList<Plan>();
+    private File _graphPath;
 
-    while ((line = reader.readLine()) != null) {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode obj = mapper.readTree(line);
-        plans.add(getJsonObjectAsPlan(obj));
+    private File _benchmarkPlan;
+
+    private int _repetitions = 1;
+
+    public void setGraphPath(File path) {
+        _graphPath = path;
     }
-    reader.close();
-    return plans;
-  }
 
-  private GenericApplicationContext getApplicationContext() {
+    public void setBenchmarkPlan(File benchmarkPlan) {
+        _benchmarkPlan = benchmarkPlan;
+    }
 
-    GenericApplicationContext ctx = new GenericApplicationContext();
-    XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(ctx);
-    xmlReader.loadBeanDefinitions(new ClassPathResource(
-        "org/opentripplanner/application-context.xml"));
+    public void setRepetitions(int repetitions) {
+        _repetitions = repetitions;
+    }
 
-    Map<String, BeanDefinition> additionalBeans = getAdditionalBeans();
-    for (Map.Entry<String, BeanDefinition> entry : additionalBeans.entrySet())
-      ctx.registerBeanDefinition(entry.getKey(), entry.getValue());
+    public void run() throws Exception {
+        GenericApplicationContext context = getApplicationContext();
+        PathService service = (PathService) context.getBean("pathServiceImpl");
 
-    ctx.refresh();
-    ctx.registerShutdownHook();
-    return ctx;
-  }
+        List<Plan> plans = readPlans();
 
-  private Map<String, BeanDefinition> getAdditionalBeans() {
+        long tTotal = 0;
+        for (int i = 0; i < _repetitions; i++) {
+            for (Plan plan : plans) {
 
-    Map<String, BeanDefinition> additionalBeans = new HashMap<String, BeanDefinition>();
+                String from = plan.latFrom + " " + plan.lonFrom;
+                String to = plan.latTo + " " + plan.lonTo;
 
-    BeanDefinitionBuilder bundlePath = BeanDefinitionBuilder.genericBeanDefinition(GraphBundle.class);
-    bundlePath.addPropertyValue("path", _graphPath);
-    additionalBeans.put("graphBundle", bundlePath.getBeanDefinition());
+                try {
+                    RoutingRequest opt = new RoutingRequest();
+                    opt.setDateTime(plan.time);
+                    opt.setFrom(from);
+                    opt.setTo(to);
+                    
+                    long t0 = System.currentTimeMillis();
+                    service.getPaths(opt);
+                    long t1 = System.currentTimeMillis();
 
-    BeanDefinitionBuilder graph = BeanDefinitionBuilder.genericBeanDefinition(SerializedGraphFactoryBean.class);
-    graph.addPropertyReference("graphBundle", "graphBundle");
-    additionalBeans.put("graph", graph.getBeanDefinition());
+                    System.out.println("t=" + (t1 - t0));
+                    tTotal += t1 - t0;
+                } catch (VertexNotFoundException ex) {
+                    System.out.println("no vertex: from=" + from + " to=" + to);
+                }
+            }
+        }
+        System.out.println("totalTime=" + tTotal);
+    }
 
-    return additionalBeans;
-  }
+    private List<Plan> readPlans() throws FileNotFoundException, IOException, ParseException {
+        BufferedReader reader = new BufferedReader(new FileReader(_benchmarkPlan));
+        String line = null;
+        List<Plan> plans = new ArrayList<Plan>();
 
-  private Plan getJsonObjectAsPlan(JsonNode obj) throws ParseException {
+        while ((line = reader.readLine()) != null) {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode obj = mapper.readTree(line);
+            plans.add(getJsonObjectAsPlan(obj));
+        }
+        reader.close();
+        return plans;
+    }
 
-    Plan plan = new Plan();
+    private GenericApplicationContext getApplicationContext() {
 
-    JsonNode from = obj.get("from");
-    plan.latFrom = from.get("lat").getValueAsDouble();
-    plan.lonFrom = from.get("lon").getValueAsDouble();
+        GenericApplicationContext ctx = new GenericApplicationContext();
+        XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(ctx);
+        xmlReader.loadBeanDefinitions(new ClassPathResource(
+                "org/opentripplanner/integration/application-context.xml"));
 
-    JsonNode to = obj.get("to");
-    plan.latTo = to.get("lat").getValueAsDouble();
-    plan.lonTo = to.get("lon").getValueAsDouble();
+        Map<String, BeanDefinition> additionalBeans = getAdditionalBeans();
+        for (Map.Entry<String, BeanDefinition> entry : additionalBeans.entrySet())
+            ctx.registerBeanDefinition(entry.getKey(), entry.getValue());
 
-    plan.time = DateLibrary.getIso8601StringAsDate(obj.get("time").getValueAsText());
-    return plan;
-  }
+        ctx.refresh();
+        ctx.registerShutdownHook();
+        return ctx;
+    }
 
-  private static class Plan {
-    public double latFrom;
-    public double lonFrom;
-    public double latTo;
-    public double lonTo;
-    public Date time;
-  }
+    private Map<String, BeanDefinition> getAdditionalBeans() {
+
+        Map<String, BeanDefinition> additionalBeans = new HashMap<String, BeanDefinition>();
+
+        BeanDefinitionBuilder bundlePath = BeanDefinitionBuilder
+                .genericBeanDefinition(GraphBundle.class);
+        bundlePath.addPropertyValue("path", _graphPath);
+        additionalBeans.put("graphBundle", bundlePath.getBeanDefinition());
+
+        BeanDefinitionBuilder graph = BeanDefinitionBuilder
+                .genericBeanDefinition(SerializedGraphFactoryBean.class);
+        graph.addPropertyReference("graphBundle", "graphBundle");
+        additionalBeans.put("graph", graph.getBeanDefinition());
+
+        return additionalBeans;
+    }
+
+    private Plan getJsonObjectAsPlan(JsonNode obj) throws ParseException {
+
+        Plan plan = new Plan();
+
+        JsonNode from = obj.get("from");
+        plan.latFrom = from.get("lat").getValueAsDouble();
+        plan.lonFrom = from.get("lon").getValueAsDouble();
+
+        JsonNode to = obj.get("to");
+        plan.latTo = to.get("lat").getValueAsDouble();
+        plan.lonTo = to.get("lon").getValueAsDouble();
+
+        plan.time = DateLibrary.getIso8601StringAsDate(obj.get("time").getValueAsText());
+        return plan;
+    }
+
+    private static class Plan {
+        public double latFrom;
+
+        public double lonFrom;
+
+        public double latTo;
+
+        public double lonTo;
+
+        public Date time;
+    }
 }
