a2627c5b96d1d43d994b750c8fecde60f3b38afb
Ben <ben.galon@moovitapp.com>
2013-01-01 09:09:55 +0200

    fix #947 license notice added to subgarph class

--- opentripplanner-routing/src/main/java/org/opentripplanner/common/geometry/Subgraph.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/common/geometry/Subgraph.java
@@ -1,3 +1,16 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 package org.opentripplanner.common.geometry;
 
 import com.vividsolutions.jts.algorithm.ConvexHull;
@@ -13,13 +26,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Ben
- * Date: 27/12/12
- * Time: 16:32
- * To change this template use File | Settings | File Templates.
- */
+
 public class Subgraph {
 
     private Set<Vertex> streetVertexSet;
