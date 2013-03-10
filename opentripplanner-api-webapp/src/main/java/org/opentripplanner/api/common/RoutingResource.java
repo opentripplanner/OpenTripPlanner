f2f1721136131f441f69177314749b2ef07c4602
Ben <ben.galon@moovitapp.com>
2013-03-10 21:50:03 +0200

    bug fix in calculating walk distance, remove duplicate walking distance.

--- opentripplanner-api-webapp/src/main/java/org/opentripplanner/api/common/RoutingResource.java
+++ opentripplanner-api-webapp/src/main/java/org/opentripplanner/api/common/RoutingResource.java
@@ -311,8 +311,6 @@ public abstract class RoutingResource {
         if (opt == OptimizeType.TRANSFERS) {
             opt = OptimizeType.QUICK;
             request.setTransferPenalty(get(transferPenalty, n, 0) + 1800);
-            //has been changed for testing
-//            request.setTransferPenalty(get(transferPenalty, n, request.getTransferPenalty()));
         } else {
             request.setTransferPenalty(get(transferPenalty, n, request.getTransferPenalty()));
         }
