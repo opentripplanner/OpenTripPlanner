c53581ff6483c4891766411522af183956e898a9
Avi Flamholz <flamholz@ridewithvia.com>
2013-04-02 18:13:04 -0400

    Add an IntersectionTraversalCostModel interface for calculating the cost of traversing intersections.

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/core/IntersectionTraversalCostModel.java
@@ -0,0 +1,37 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package org.opentripplanner.routing.core;
+
+import org.opentripplanner.routing.edgetype.PlainStreetEdge;
+import org.opentripplanner.routing.vertextype.IntersectionVertex;
+
+/**
+ * An interface to a model that computes the costs of turns.
+ * 
+ * Turn costs are in units of seconds - they represent the expected amount of time it would take to make a turn.
+ * 
+ * @author avi
+ */
+public interface IntersectionTraversalCostModel {
+    
+    /**
+     * Compute the cost of turning onto "to" from "from".
+     * 
+     * @return expected number of seconds the traversal is expected to take.
+     */
+    public double computeTraversalCost(IntersectionVertex v, PlainStreetEdge from,
+            PlainStreetEdge to, TraverseMode mode, RoutingRequest options, float fromSpeed,
+            float toSpeed);
+
+}
