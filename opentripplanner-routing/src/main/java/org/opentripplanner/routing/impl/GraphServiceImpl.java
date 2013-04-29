6f3b9aa0c27260d5be005159f469622c2db3e1da
laurentg <laurent.gregoire@gmail.com>
2013-04-29 16:12:06 +0200

    Add 'auto-discover' multi-graph mode.

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/GraphServiceImpl.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/GraphServiceImpl.java
@@ -51,6 +51,8 @@ public class GraphServiceImpl implements GraphService, ResourceLoaderAware {
 
     private static final Logger LOG = LoggerFactory.getLogger(GraphServiceImpl.class);
 
+    private static final String GRAPH_FILENAME = "Graph.obj";
+
     private String resourceBase = "file:/var/otp/graphs";
 
     private Map<String, Graph> graphs = new HashMap<String, Graph>();
@@ -76,6 +78,10 @@ public class GraphServiceImpl implements GraphService, ResourceLoaderAware {
     /** If true, on startup register the graph in the location defaultRouterId. */
     @Setter
     private boolean attemptRegisterDefault = true;
+
+    /** If true, automatically try to load all graphs present in resourceBase. */
+    @Setter
+    private boolean autoDiscover = false;
 
     /** 
      * Router IDs may contain alphanumeric characters, underscores, and dashes only. 
@@ -126,6 +132,10 @@ public class GraphServiceImpl implements GraphService, ResourceLoaderAware {
         if (attemptRegisterDefault && ! graphs.containsKey(defaultRouterId)) {
             LOG.info("Attempting to load graph for default routerId '{}'.", defaultRouterId);
             registerGraph(defaultRouterId, true);
+        }
+        if (autoDiscover) {
+            LOG.info("Auto discovering graphs under {}", resourceBase);
+            autoDiscoverGraphs();
         }
         if (this.getRouterIds().isEmpty()) {
             LOG.warn("No graphs have been loaded/registered. " +
@@ -263,4 +273,39 @@ public class GraphServiceImpl implements GraphService, ResourceLoaderAware {
         }
         return n;
     }
+    
+    private void autoDiscoverGraphs() {
+        synchronized (resourceBase) {
+            Resource base = resourceLoader.getResource(resourceBase);
+            try {
+                File baseFile = base.getFile();
+                for (String sub : baseFile.list()) {
+                    File subFile = new File(baseFile, sub);
+                    if (subFile.isDirectory()) {
+                        File graphFile = new File(subFile, GRAPH_FILENAME);
+                        if (graphFile.exists() && graphFile.canRead()) {
+                            LOG.info("Auto-discovered {}, trying to load.", graphFile);
+                            registerGraph(sub, true);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                // Can happen if base is not a standard directory (resource)
+                LOG.error(
+                        "Graph auto-discovering has been set, but {} is not a file resource, so I'm bailing-out.",
+                        resourceBase);
+                // Just warn the user, no need to throw exception
+                return;
+            }
+            // If a defaultRouterId has not been set, take first loaded graph
+            if (defaultRouterId.equals("") && !getRouterIds().isEmpty()) {
+                defaultRouterId = getRouterIds().iterator().next();
+                if (getRouterIds().size() > 1)
+                    LOG.warn("Default routerId not set, taking arbitrary one {}", defaultRouterId);
+                else
+                    LOG.info("Setting default routerId to {}", defaultRouterId);
+            }
+        }
+    }
+
 }
