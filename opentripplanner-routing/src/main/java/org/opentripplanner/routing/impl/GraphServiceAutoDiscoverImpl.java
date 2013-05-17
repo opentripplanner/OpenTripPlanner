0414fceed4715ba21df8b30ba2efc608c4bbd3ba
laurentg <laurent.gregoire@gmail.com>
2013-05-17 14:11:43 +0200

    Multi-graph auto-discovery: add periodic refresh.
    Also split static spring configuration and auto-discover mode in two classes.
    Use a common class which encapsulate common file-related functionalities.

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/GraphServiceAutoDiscoverImpl.java
@@ -0,0 +1,266 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (props, at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package org.opentripplanner.routing.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.annotation.PostConstruct;
+
+import lombok.Setter;
+
+import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.routing.graph.Graph.LoadLevel;
+import org.opentripplanner.routing.services.GraphService;
+import org.opentripplanner.routing.services.StreetVertexIndexFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+/**
+ * An implementation of the file-based GraphServiceFileImpl which auto-configure itself by scanning the root resource directory.
+ */
+@Scope("singleton")
+public class GraphServiceAutoDiscoverImpl implements GraphService, ResourceLoaderAware {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GraphServiceAutoDiscoverImpl.class);
+
+    private GraphServiceFileImpl decorated = new GraphServiceFileImpl();
+
+    /** Last timestamp upper bound when we auto-scanned resources. */
+    private long lastAutoScan = 0L;
+
+    /** The autoscan period in seconds */
+    @Setter
+    private int autoScanPeriodSec = 60;
+
+    /**
+     * The delay before loading a new graph, in seconds. We load a graph if it has been modified at least this amount of time in the past. This in
+     * order to give some time for non-atomic graph copy.
+     */
+    @Setter
+    private int loadDelaySec = 10;
+
+    /**
+     * @param indexFactory
+     */
+    public void setIndexFactory(StreetVertexIndexFactory indexFactory) {
+        decorated.setIndexFactory(indexFactory);
+    }
+
+    /**
+     * @param defaultRouterId
+     */
+    public void setDefaultRouterId(String defaultRouterId) {
+        decorated.setDefaultRouterId(defaultRouterId);
+    }
+
+    /**
+     * Sets a base path for graph loading from the filesystem. Serialized graph files will be retrieved from sub-directories immediately below this
+     * directory. The routerId of a graph is the same as the name of its sub-directory. This does the same thing as setResource, except the parameter
+     * is interpreted as a file path.
+     */
+    public void setPath(String path) {
+        decorated.setResource("file:" + path);
+    }
+
+    /**
+     * Sets a base path in the classpath or relative to the webapp root. This can be useful in cloud computing environments where webapps must be
+     * entirely self-contained. When OTP is running as a webapp, the ResourceLoader provided by Spring will be a ServletContextResourceLoader, so
+     * paths will be interpreted relative to the webapp root and WARs should be handled transparently. If you want to point to a location outside the
+     * webapp or you just want to be clear about exactly where the graphs are to be found, this path should be prefixed with 'classpath:','file:', or
+     * 'url:'.
+     */
+    public void setResource(String resourceBaseName) {
+        decorated.setResource(resourceBaseName);
+    }
+
+    @Override
+    public Graph getGraph() {
+        return decorated.getGraph();
+    }
+
+    @Override
+    public Graph getGraph(String routerId) {
+        return decorated.getGraph(routerId);
+    }
+
+    @Override
+    public void setLoadLevel(LoadLevel level) {
+        decorated.setLoadLevel(level);
+    }
+
+    // TODO Should we extract this interface in GraphService?
+    // See the (strange) cast to GraphServiceImpl in Routers.reloadGraphs()
+    public boolean reloadGraphs(boolean preEvict) {
+        return decorated.reloadGraphs(preEvict);
+    }
+
+    @Override
+    public Collection<String> getRouterIds() {
+        return decorated.getRouterIds();
+    }
+
+    @Override
+    public boolean registerGraph(String routerId, boolean preEvict) {
+        // Invalid in auto-discovery mode
+        return false;
+    }
+
+    @Override
+    public boolean registerGraph(String routerId, Graph graph) {
+        // Invalid in auto-discovery mode
+        return false;
+    }
+
+    @Override
+    public boolean evictGraph(String routerId) {
+        // Invalid in auto-discovery mode
+        return false;
+    }
+
+    @Override
+    public int evictAll() {
+        // Invalid in auto-discovery mode
+        return 0;
+    }
+
+    /** The resourceLoader setter is called by Spring via ResourceLoaderAware interface. */
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        decorated.setResourceLoader(resourceLoader);
+    }
+
+    /**
+     * Based on the autoRegister list, automatically register all routerIds for which we can find a graph file in a subdirectory of the resourceBase
+     * path. Also register and load the graph for the defaultRouterId and warn if no routerIds are registered.
+     */
+    @PostConstruct
+    // PostConstruct means run on startup after all injection has occurred
+    private void startup() {
+        /* Run the first one syncronously as other initialization methods may need a default router. */
+        autoDiscoverGraphs();
+        /* Starting with JDK7 we should use a directory change listener callback on baseResource instead. */
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                autoDiscoverGraphs();
+            }
+        }, autoScanPeriodSec * 1000L, autoScanPeriodSec * 1000L);
+    }
+
+    private synchronized void autoDiscoverGraphs() {
+        LOG.debug("Auto discovering graphs under {}", decorated.getResourceBase());
+        Collection<String> graphOnDisk = new HashSet<String>();
+        Collection<String> graphToLoad = new HashSet<String>();
+        // Only reload graph modified more than 1 mn ago.
+        long validEndTime = System.currentTimeMillis() - loadDelaySec * 1000;
+        Resource base = decorated.getResource(decorated.getResourceBase());
+        try {
+            File baseFile = base.getFile();
+            // First check for a root graph
+            File rootGraphFile = new File(baseFile, GraphServiceFileImpl.GRAPH_FILENAME);
+            if (rootGraphFile.exists() && rootGraphFile.canRead()) {
+                graphOnDisk.add("");
+                // lastModified can change, so test must be atomic here.
+                long lastModified = rootGraphFile.lastModified();
+                if (lastModified > lastAutoScan && lastModified <= validEndTime) {
+                    LOG.debug("Graph to (re)load: {}, lastModified={}", rootGraphFile, lastModified);
+                    graphToLoad.add("");
+                }
+            }
+            // Then graph in sub-directories
+            for (String sub : baseFile.list()) {
+                File subFile = new File(baseFile, sub);
+                if (subFile.isDirectory()) {
+                    File graphFile = new File(subFile, GraphServiceFileImpl.GRAPH_FILENAME);
+                    if (graphFile.exists() && graphFile.canRead()) {
+                        graphOnDisk.add(sub);
+                        long lastModified = graphFile.lastModified();
+                        if (lastModified > lastAutoScan && lastModified <= validEndTime) {
+                            LOG.debug("Graph to (re)load: {}, lastModified={}", graphFile,
+                                    lastModified);
+                            graphToLoad.add(sub);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            // Can happen if base is not a standard directory (resource)
+            LOG.error(
+                    "Graph auto-discovering has been set, but {} is not a file resource, so I'm bailing-out.",
+                    decorated.getResourceBase());
+            // Just warn the user, no need to throw exception
+        } finally {
+            lastAutoScan = validEndTime;
+        }
+
+        StringBuffer onDiskSb = new StringBuffer();
+        for (String routerId : graphOnDisk)
+            onDiskSb.append("[").append(routerId).append("]");
+        StringBuffer toLoadSb = new StringBuffer();
+        for (String routerId : graphToLoad)
+            toLoadSb.append("[").append(routerId).append("]");
+        LOG.debug("Found routers: {} - Must reload: {}", onDiskSb.toString(), toLoadSb.toString());
+        for (String routerId : graphToLoad) {
+            /*
+             * Do not set preEvict, because: 1) during loading of a new graph we want to keep one available; and 2) if the loading of a new graph
+             * fails we also want to keep the old one.
+             */
+            decorated.registerGraph(routerId, false);
+        }
+        for (String routerId : getRouterIds()) {
+            // Evict graph removed from disk.
+            if (!graphOnDisk.contains(routerId)) {
+                LOG.warn("Auto-evicting routerId '{}', not present on disk anymore.", routerId);
+                decorated.evictGraph(routerId);
+            }
+        }
+
+        /*
+         * If the defaultRouterId is not present, print a warning and set it to some default.
+         */
+        if (!getRouterIds().contains(decorated.getDefaultRouterId())) {
+            LOG.warn("Default routerId '{}' not available!", decorated.getDefaultRouterId());
+            if (!getRouterIds().isEmpty()) {
+                // Let's see which one we want to take by default
+                String defRouterId = null;
+                if (getRouterIds().contains("")) {
+                    // If we have a root graph, this should be a good default
+                    defRouterId = "";
+                    LOG.info("Setting default routerId to root graph ''");
+                } else {
+                    // Otherwise take first one present
+                    defRouterId = getRouterIds().iterator().next();
+                    if (getRouterIds().size() > 1)
+                        LOG.warn("Setting default routerId to arbitrary one '{}'", defRouterId);
+                    else
+                        LOG.info("Setting default routerId to '{}'", defRouterId);
+                }
+                decorated.setDefaultRouterId(defRouterId);
+            }
+        }
+        if (this.getRouterIds().isEmpty()) {
+            LOG.warn("No graphs have been loaded/registered. "
+                    + "You must place one or more graphs before routing.");
+        }
+    }
+}
---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/GraphServiceFileImpl.java
@@ -0,0 +1,230 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (props, at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package org.opentripplanner.routing.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.routing.graph.Graph.LoadLevel;
+import org.opentripplanner.routing.services.GraphService;
+import org.opentripplanner.routing.services.StreetVertexIndexFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+/**
+ * A GraphService implementation implementing loading graph from files or resources, but which does not load anything by itself. It rely on decorators
+ * instances to help it initialize/reload itself.
+ * 
+ * Note: Instead of a decorator pattern we should have used a strategy pattern (letting the GraphServiceImpl delegate the file mapping to various
+ * strategy/factory instances), but this would have broke down the spring API widely used. And also this could have been a bit more complex as
+ * auto-discover mode would have need callbacks.
+ * 
+ * @see GraphServiceImpl
+ * @see GraphServiceAutoDiscoverImpl
+ */
+public class GraphServiceFileImpl implements GraphService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GraphServiceFileImpl.class);
+
+    public static final String GRAPH_FILENAME = "Graph.obj";
+
+    @Getter
+    private String resourceBase = "file:/var/otp/graphs";
+
+    private Map<String, Graph> graphs = new HashMap<String, Graph>();
+
+    private Map<String, LoadLevel> levels = new HashMap<String, LoadLevel>();
+
+    private LoadLevel loadLevel = LoadLevel.FULL;
+    
+    @Setter
+    private StreetVertexIndexFactory indexFactory = new DefaultStreetVertexIndexFactory();
+
+    @Setter
+    @Getter
+    private String defaultRouterId = "";
+
+    /** The resourceLoader setter is called by decorators. */
+    @Setter
+    private ResourceLoader resourceLoader = null;
+
+    /** 
+     * Router IDs may contain alphanumeric characters, underscores, and dashes only. 
+     * This prevents any confusion caused by the presence of special characters that might have a 
+     * meaning for the filesystem.
+     */
+    private static Pattern routerIdPattern = Pattern.compile("[\\p{Alnum}_-]*");
+    
+    /**
+     * Sets a base path in the classpath or relative to the webapp root. This can be useful in 
+     * cloud computing environments where webapps must be entirely self-contained. When OTP is
+     * running as a webapp, the ResourceLoader provided by Spring will be a 
+     * ServletContextResourceLoader, so paths will be interpreted relative to the webapp root and 
+     * WARs should be handled transparently. If you want to point to a location outside the webapp 
+     * or you just want to be clear about exactly where the graphs are to be found, this path 
+     * should be prefixed with 'classpath:','file:', or 'url:'.
+     */
+    public void setResource(String resourceBaseName) {
+        this.resourceBase = resourceBaseName;
+    }
+    
+    public Graph getGraph() {
+        return getGraph(null);
+    }
+
+    public Graph getGraph(String routerId) {
+        if (routerId == null || routerId.isEmpty()) {
+            routerId = defaultRouterId;
+            LOG.debug("routerId not specified, set to default of '{}'", routerId);
+        }
+        synchronized (graphs) {
+            if (!graphs.containsKey(routerId)) {
+                LOG.error("no graph registered with the routerId '{}'", routerId);
+                return null;
+            } else {
+                return graphs.get(routerId);
+            }
+        }
+    }
+
+    @Override
+    public void setLoadLevel(LoadLevel level) {
+        if (level != loadLevel) {
+            loadLevel = level;
+            reloadGraphs(true);
+        }
+    }
+
+    private boolean routerIdLegal(String routerId) {
+        Matcher m = routerIdPattern.matcher(routerId);
+        return m.matches();
+    }
+
+    protected Graph loadGraph(String routerId) {
+        if ( ! routerIdLegal(routerId)) {
+            LOG.error("routerId '{}' contains characters other than alphanumeric, underscore, and dash.", routerId);
+            return null;
+        }
+        LOG.debug("loading serialized graph for routerId {}", routerId);
+        StringBuilder sb = new StringBuilder(resourceBase);
+        // S3 is intolerant of extra slashes in the URL, so only add them as needed
+        if (! (resourceBase.endsWith("/") || resourceBase.endsWith(File.pathSeparator))) {
+            sb.append("/");
+        }
+        if (routerId.length() > 0) { 
+            sb.append(routerId);
+            sb.append("/");
+        }
+        sb.append("Graph.obj");
+        String resourceLocation = sb.toString();
+        LOG.debug("graph file for routerId '{}' is at {}", routerId, resourceLocation);
+        Resource graphResource;
+        InputStream is;
+        try {
+            graphResource = resourceLoader.getResource(resourceLocation);
+            //graphResource = resourceBase.createRelative(graphId);
+            is = graphResource.getInputStream();
+        } catch (IOException ex) {
+            LOG.warn("Graph file not found or not openable for routerId '{}' under {}", routerId, resourceBase);
+            ex.printStackTrace();
+            return null;
+        }
+        LOG.debug("graph input stream successfully opened.");
+        LOG.info("Loading graph...");
+        try {
+            return Graph.load(new ObjectInputStream(is), loadLevel, indexFactory);
+        } catch (Exception ex) {
+            LOG.error("Exception while loading graph from {}.", graphResource);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean reloadGraphs(boolean preEvict) {
+        boolean allSucceeded = true;
+        synchronized (graphs) {
+            for (String routerId : this.getRouterIds()) {
+                boolean success = registerGraph(routerId, preEvict);
+                allSucceeded &= success;
+            }
+        }
+        return allSucceeded;
+    }
+    
+    @Override
+    public Collection<String> getRouterIds() {
+        return new ArrayList<String>(graphs.keySet());
+    }
+
+    @Override
+    public boolean registerGraph(String routerId, boolean preEvict) {
+        if (preEvict)
+            evictGraph(routerId);
+        LOG.info("registering routerId '{}'", routerId);
+        Graph graph = this.loadGraph(routerId);
+        if (graph != null) {
+            synchronized (graphs) {
+                graphs.put(routerId, graph);
+            }
+            levels.put(routerId, loadLevel);
+            return true;
+        }
+        LOG.info("routerId '{}' was not registered (graph was null).", routerId);
+        return false;
+    }
+
+    @Override
+    public boolean registerGraph(String routerId, Graph graph) {
+        Graph existing = graphs.put(routerId, graph);
+        return existing == null;
+    }
+    
+    @Override
+    public boolean evictGraph(String routerId) {
+        LOG.debug("evicting graph {}", routerId);
+        synchronized (graphs) {
+            Graph existing = graphs.remove(routerId);
+            return existing != null;
+        }
+    }
+
+    @Override
+    public int evictAll() {
+        int n;
+        synchronized(graphs) {
+            n = graphs.size(); 
+            graphs.clear();
+        }
+        return n;
+    }
+    
+    public Resource getResource(String location) {
+        return resourceLoader.getResource(location);
+    }
+
+}
