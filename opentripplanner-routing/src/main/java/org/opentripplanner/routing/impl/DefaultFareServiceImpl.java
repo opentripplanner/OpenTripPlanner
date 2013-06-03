4da62b1053cced3034787f6578736cb2d7cfbaf8
laurentg <laurent.gregoire@gmail.com>
2013-06-03 18:20:52 +0200

    Do not warn for missing fares if none avail.
    Conflicts:
    
    	opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
@@ -268,9 +268,12 @@ public class DefaultFareServiceImpl implements FareService, Serializable {
         }
         LOG.debug("{} best for {}", bestAttribute, rides);
 
-        if (bestFare == Float.POSITIVE_INFINITY)
-            LOG.warn("No fare for a ride sequence: {}", rides);
-
+        if (bestFare == Float.POSITIVE_INFINITY) {
+            if (fareAttributes.isEmpty())
+                LOG.info("No fare for a ride sequence: {}", rides);
+            else
+                LOG.warn("No fare for a ride sequence: {}", rides);
+        }
         return bestFare;
 
     }

laurentg/OpenTripPlanner-mecatran

Do not warn for missing fares if none avail.

opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
@@ -268,8 +268,12 @@ public class DefaultFareServiceImpl implements FareService, Serializable {
         }
         _log.debug("{} best for {}", bestAttribute, rides);
 
-        if (bestFare == Float.POSITIVE_INFINITY)
-            _log.warn("No fare for a ride sequence: {}", rides);
+        if (bestFare == Float.POSITIVE_INFINITY) {
+            if (fareAttributes.isEmpty())
+                _log.info("No fare for a ride sequence: {}", rides);
+            else
+                _log.warn("No fare for a ride sequence: {}", rides);
+        }
 
         return bestFare;
 

opentripplanner/OpenTripPlanner

Conflicts:
	opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java

opentripplanner-routing/src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
@@ -268,9 +268,12 @@ public class DefaultFareServiceImpl implements FareService, Serializable {
         }
         LOG.debug("{} best for {}", bestAttribute, rides);
 
-        if (bestFare == Float.POSITIVE_INFINITY)
-            LOG.warn("No fare for a ride sequence: {}", rides);
-
+        if (bestFare == Float.POSITIVE_INFINITY) {
+            if (fareAttributes.isEmpty())
+                LOG.info("No fare for a ride sequence: {}", rides);
+            else
+                LOG.warn("No fare for a ride sequence: {}", rides);
+        }
         return bestFare;
 
     }