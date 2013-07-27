e47fd1e821f8dd1aa10d664d6f6d8ef6b1f16a4f
Andrew Byrd <andrew@fastmail.net>
2013-07-27 04:01:22 +0200

    include client WAR in standalone JAR, and serve its contents
    also remove manifest, since it's being produced by shade

--- otp-standalone/src/main/java/org/opentripplanner/standalone/GrizzlyServer.java
+++ otp-standalone/src/main/java/org/opentripplanner/standalone/GrizzlyServer.java
@@ -1,7 +1,12 @@
 package org.opentripplanner.standalone;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.BindException;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -12,6 +17,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import com.google.common.io.Files;
+import com.google.common.io.InputSupplier;
 import com.sun.jersey.api.container.ContainerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
@@ -20,7 +27,14 @@ import com.sun.jersey.core.spi.component.ioc.IoCComponentProviderFactory;
 public class GrizzlyServer {
 
     private static final Logger LOG = LoggerFactory.getLogger(GrizzlyServer.class);
-
+    
+    static {
+        // Remove existing handlers attached to the j.u.l root logger
+        SLF4JBridgeHandler.removeHandlersForRootLogger();  // (since SLF4J 1.6.5)
+        // Bridge j.u.l (used by Jersey) to the SLF4J root logger
+        SLF4JBridgeHandler.install();
+    }
+    
     /** A factory that hands Jersey OTP modules to inject. */
     private IoCComponentProviderFactory iocFactory;
     
@@ -33,14 +47,43 @@ public class GrizzlyServer {
         this.params = params;
     }
 
+    public static final String CLIENT_WAR_FILENAME = "client.war";
+    
+    private static class ClientWarSupplier implements InputSupplier<InputStream> {
+        @Override public InputStream getInput() throws IOException {
+            InputStream istream =  ClassLoader.getSystemResourceAsStream(CLIENT_WAR_FILENAME);
+            if (istream == null) throw new IOException("Cannot find client WAR.");
+            return istream;
+        }
+    }
+
+    /** 
+     * Create an HttpHandler to serve up static content from a client WAR inside the OTP JAR.
+     * Zip files have the file index at the /end/, so they cannot be properly decoded as a stream.
+     * Zip streams do not offer random access, and ZipFiles are considered preferable in all cases.
+     * Grizzly does include a static file server but it does not work on resources within a JAR, 
+     * only files. Solution: copy the ZIP outside the JAR and expand it in a temp directory.
+     * JVM does cache classpath resource streams, but let's just do it manually.
+     * Interestingly this even works in Eclipse. 
+     * The WAR artifact copying and expansion is still working.    
+     */
+    public HttpHandler makeClientStaticHandler () {
+        File tempDir = Files.createTempDir();
+        File clientWar = new File(tempDir, CLIENT_WAR_FILENAME);
+        try {
+            Files.copy(new ClientWarSupplier(), clientWar);
+            ZipFile zip = new ZipFile(clientWar);
+            zip.extractAll(tempDir.toString());
+        } catch (ZipException e) {
+            LOG.error("Error expanding client WAR, client will not be available.");
+        } catch (IOException e) {
+            LOG.error("Error copying client WAR, client will not be available.");
+        }
+        return new StaticHttpHandler(tempDir.toString());
+    }
+    
     public void run() {
         
-        /* CONFIGURE LOGGING (make static?) */
-        // Remove existing handlers attached to the j.u.l root logger
-        SLF4JBridgeHandler.removeHandlersForRootLogger();  // (since SLF4J 1.6.5)
-        // Bridge j.u.l (used by Jersey) to the SLF4J root logger
-        SLF4JBridgeHandler.install();
-
         /* Rather than use Jersey's GrizzlyServerFactory we will construct one manually, so we can
            set the number of threads, etc. */
         LOG.info("Starting OTP Grizzly server on port {} using graphs at {}", params.port, params.graphDirectory);
@@ -69,8 +112,7 @@ public class GrizzlyServer {
         /* 2. A static content server for the client JS apps etc.
               This is a filesystem path, not classpath. Files are relative to the project dir, so
               from ./ we can reach e.g. target/classes/data-sources.xml */
-        params.staticDirectory = "../opentripplanner-leaflet-client/src/main/webapp/";
-        HttpHandler staticHandler = new StaticHttpHandler(params.staticDirectory);
+        HttpHandler staticHandler = makeClientStaticHandler();
         httpServer.getServerConfiguration().addHttpHandler(staticHandler, "/");
         
         /* RELINQUISH CONTROL TO THE SERVER THREAD */
@@ -88,5 +130,6 @@ public class GrizzlyServer {
         }
         
     }
-    
+        
 }
+
