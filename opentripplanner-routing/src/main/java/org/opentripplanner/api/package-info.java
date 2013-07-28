6b61679610b638ccbe305647decb2f7e31d10bd3
Andrew Byrd <andrew@fastmail.net>
2013-07-29 01:01:48 +0200

    merge otp-rest-api into otp-routing module

---
+++ opentripplanner-routing/src/main/java/org/opentripplanner/api/package-info.java
@@ -0,0 +1,7 @@
+/**
+    This package contains the JAX-RS-annotated REST resource classes for the OpenTripPlanner public
+    API, i.e. the Jersey REST endpoints. We provide the REST API as both a WAR-packaged servlet 
+    (module opentripplanner-api-webapp) and a standalone Grizzly-based command-line invoked server 
+    (module otp-standalone). 
+ */
+package org.opentripplanner.api;
