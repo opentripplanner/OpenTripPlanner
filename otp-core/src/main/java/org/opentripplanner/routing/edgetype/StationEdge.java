50289b1a714e5bae65fb4d2e667efe8b68971328
Andrew Byrd <andrew@fastmail.net>
2013-08-08 17:00:18 +0200

    add marker interface for station edges

---
+++ otp-core/src/main/java/org/opentripplanner/routing/edgetype/StationEdge.java
@@ -0,0 +1,7 @@
+package org.opentripplanner.routing.edgetype;
+
+/** 
+ * Marker interface for edges within transit stations. This helps categorize segments of paths for
+ * parsing.
+ */
+public interface StationEdge { }
