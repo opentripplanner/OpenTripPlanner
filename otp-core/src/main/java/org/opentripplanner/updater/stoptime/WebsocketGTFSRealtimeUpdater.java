98d2f18ccdc73b4c2b9f63c4c395f86dbe699af7
Andrew Byrd <andrew@fastmail.net>
2013-08-15 23:26:13 +0200

    websocket updater: less logging, stub preferences configuration

--- otp-core/src/main/java/org/opentripplanner/updater/stoptime/WebsocketGTFSRealtimeUpdater.java
+++ otp-core/src/main/java/org/opentripplanner/updater/stoptime/WebsocketGTFSRealtimeUpdater.java
@@ -2,9 +2,13 @@ package org.opentripplanner.updater.stoptime;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.prefs.Preferences;
 
 import lombok.AllArgsConstructor;
 import lombok.Setter;
+
+import org.opentripplanner.configuration.PreferencesConfigurable;
+import org.opentripplanner.routing.graph.Graph;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.transit.realtime.GtfsRealtime;
@@ -25,7 +29,7 @@ import com.ning.http.client.websocket.WebSocketUpgradeHandler;
  * 
  * @author abyrd
  */
-public class WebsocketGTFSRealtimeUpdater {
+public class WebsocketGTFSRealtimeUpdater implements PreferencesConfigurable {
     
     /** The URL at which the websocket stream of GTFS-RT messages will be found. */
     @Setter private String url;
@@ -40,7 +44,7 @@ public class WebsocketGTFSRealtimeUpdater {
         // AsyncHttpClient. This would be done as follows:
         // AsyncHttpClientConfig config = new AsyncHttpClientConfig.Builder().build();
         // AsyncHttpClient client = new AsyncHttpClient(new GrizzlyAsyncHttpProvider(config), config);        
-        // Using Netty by default instead:
+        // Using Netty by default:
         AsyncHttpClient client = new AsyncHttpClient(); 
         WebSocketListener listener = new Listener();
         WebSocketUpgradeHandler handler = 
@@ -56,9 +60,21 @@ public class WebsocketGTFSRealtimeUpdater {
     }
     
     public class Listener extends DefaultWebSocketListener {
+        private int n_received = 0;
+        private long last_log_time = System.currentTimeMillis();
         @Override
         public void onMessage(byte[] message) {
-            System.out.println("Received message: " + message);
+            n_received += 1;
+            // System.out.println("Received message number " + n_received);
+            if (n_received % 100 == 0) {
+                long this_log_time = System.currentTimeMillis();
+                long elapsed_time = this_log_time - last_log_time;
+                System.out.printf("Received %d messages in %d msec, %f msgs/sec \n",
+                        n_received, elapsed_time, n_received / (elapsed_time / 1000d));
+                n_received = 0;
+                last_log_time = System.currentTimeMillis();
+            }
+                
             Runnable runnable = new DecoderTask(message);
             executor.execute(runnable);
         }
@@ -68,9 +84,9 @@ public class WebsocketGTFSRealtimeUpdater {
     public static class DecoderTask implements Runnable { //, GraphUpdaterRunnable 
         private byte[] bytes;
         @Override public void run() {
-            System.out.println("Decoding message: " + bytes);
+            // System.out.println("Decoding message: " + bytes);
             UpdateStreamer us = new UpdateStreamer(bytes);
-            System.out.println("Updates: " + us.getUpdates());
+            // System.out.println("Updates: " + us.getUpdates());
         }
     }
 
@@ -83,7 +99,7 @@ public class WebsocketGTFSRealtimeUpdater {
             try {
                 System.out.println("Parsing protocol buffers from message: " + bytes);
                 feed = GtfsRealtime.FeedMessage.parseFrom(bytes);
-                System.out.println("Feed: " + feed);
+                // System.out.println("Feed: " + feed);
                 return feed;
             } catch (InvalidProtocolBufferException e) {
                 e.printStackTrace();
@@ -95,10 +111,15 @@ public class WebsocketGTFSRealtimeUpdater {
     
     public static void main (String[] argv) {
         WebsocketGTFSRealtimeUpdater wu = new WebsocketGTFSRealtimeUpdater();
-//        wu.setUrl("ws://echo.websocket.org");
         wu.setUrl("ws://ovapi.nl:8088/tripUpdates");
         wu.setExecutor(Executors.newSingleThreadExecutor());
         wu.start();
+    }
+
+    /** This method configures the WebsocketGTFSRealtimeUpdater based on Java Preferences */
+    @Override
+    public void configure(Graph graph, Preferences preferences) throws Exception {
+        // TODO Auto-generated method stub        
     }
     
 }
