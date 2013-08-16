4a48ebb7f68d5a6004141d616f89371a0190edcc
Andrew Byrd <andrew@fastmail.net>
2013-08-16 11:12:52 +0200

    hard-wire LongDistancePathService to use GenericAStar with 3 paths

--- otp-core/src/main/java/org/opentripplanner/routing/impl/LongDistancePathService.java
+++ otp-core/src/main/java/org/opentripplanner/routing/impl/LongDistancePathService.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import lombok.Setter;
 
+import org.opentripplanner.routing.algorithm.GenericAStar;
 import org.opentripplanner.routing.algorithm.strategies.DefaultRemainingWeightHeuristic;
 import org.opentripplanner.routing.algorithm.strategies.InterleavedBidirectionalHeuristic;
 import org.opentripplanner.routing.algorithm.strategies.RemainingWeightHeuristic;
@@ -65,11 +66,17 @@ public class LongDistancePathService implements PathService {
     @Autowired @Setter
     private GraphService graphService;
     
-    @Autowired @Setter
     private SPTService sptService;
 
     @Setter 
     private double timeout = 0; // seconds
+    
+    public LongDistancePathService () {
+        // Use an instance of GenericAStar that finds 3 paths instead of 1
+        GenericAStar astar = new GenericAStar();
+        astar.setNPaths(3);
+        sptService = astar;
+    }
     
     @Override
     public List<GraphPath> getPaths(RoutingRequest options) {
