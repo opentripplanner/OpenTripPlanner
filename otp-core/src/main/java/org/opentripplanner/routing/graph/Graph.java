a209e0c26d471ab058531fb4a92f77ff970097e9
Zsombor Welker <flaktack@welker.hu>
2013-08-22 17:01:41 +0200

    Add @Getter for Graph#transitService{Starts,Ends}

--- otp-core/src/main/java/org/opentripplanner/routing/graph/Graph.java
+++ otp-core/src/main/java/org/opentripplanner/routing/graph/Graph.java
@@ -76,6 +76,7 @@ import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multiset;
 import com.vividsolutions.jts.geom.Envelope;
+import lombok.Getter;
 
 /**
  * A graph is really just one or more indexes into a set of vertexes. It used to keep edgelists for each vertex, but those are in the vertex now.
@@ -91,8 +92,10 @@ public class Graph implements Serializable {
     private final Map<Edge, Set<AlertPatch>> alertPatches = new HashMap<Edge, Set<AlertPatch>>(0);
 
     // transit feed validity information in seconds since epoch
+    @Getter
     private long transitServiceStarts = Long.MAX_VALUE;
 
+    @Getter
     private long transitServiceEnds = 0;
 
     private Map<Class<?>, Object> _services = new HashMap<Class<?>, Object>();
