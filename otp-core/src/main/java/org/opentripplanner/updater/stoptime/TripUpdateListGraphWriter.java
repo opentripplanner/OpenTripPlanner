1b2e4f4597c815bd16a3444eb33b6208a731fd62
jkoelewijn <git@jkoelewijn.nl>
2013-08-23 17:19:33 +0200

    Added first version of TripUpdateListGraphWriter

---
+++ otp-core/src/main/java/org/opentripplanner/updater/stoptime/TripUpdateListGraphWriter.java
@@ -0,0 +1,41 @@
+/* This program is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package org.opentripplanner.updater.stoptime;
+
+import java.util.List;
+
+import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.routing.trippattern.TripUpdateList;
+import org.opentripplanner.updater.GraphWriterRunnable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TripUpdateListGraphWriter implements GraphWriterRunnable {
+    
+    private static Logger LOG = LoggerFactory.getLogger(TripUpdateListGraphWriter.class);
+    
+    /**
+     * The list with updates to apply to the graph
+     */
+    private List<TripUpdateList> updates;
+    
+    public TripUpdateListGraphWriter(List<TripUpdateList> updates) {
+        this.updates = updates;
+    }
+    
+    @Override
+    public void run(Graph graph) {
+        // Apply updates to graph
+    }
+}
