a05812057b45da85d17fd2147d959d5fc7f552b6
laurentg <laurent.gregoire@gmail.com>
2013-11-05 14:29:49 +0100

    Remove warning by adding serialVersionUid.

--- otp-core/src/main/java/org/opentripplanner/api/common/ParameterException.java
+++ otp-core/src/main/java/org/opentripplanner/api/common/ParameterException.java
@@ -18,7 +18,8 @@ package org.opentripplanner.api.common;
  * Signals a problem parsing or interpreting a query parameter.
  */
 public class ParameterException extends Exception {
-
+    private static final long serialVersionUID = 1L;
+    
     public Message message;
     
     public ParameterException(Message message) {
