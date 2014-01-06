08d5a9c7c338cf39bf6d4801eaf2c57263f2d5ad
Andrew Byrd <andrew@fastmail.net>
2014-01-06 12:30:39 +0100

    add optional distance field to Stop JSON model

--- otp-core/src/main/java/org/opentripplanner/index/model/StopShort.java
+++ otp-core/src/main/java/org/opentripplanner/index/model/StopShort.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.onebusaway.gtfs.model.Stop;
 
 import com.beust.jcommander.internal.Lists;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 public class StopShort {
 
@@ -15,12 +17,21 @@ public class StopShort {
     public double lat;
     public double lon;
     
+    /** Distance to the stop when it is returned from a location-based query. */
+    @JsonInclude(Include.NON_NULL) public Integer dist;
+    
     public StopShort (Stop stop) {
         agency = stop.getId().getAgencyId();
         id = stop.getId().getId();
         name = stop.getName();
         lat = stop.getLat();
         lon = stop.getLon();
+    }
+
+    /** @param distance in integral meters, to avoid serializing a bunch of decimal places. */
+    public StopShort(Stop stop, int distance) {
+        this(stop);
+        this.dist = distance;
     }
 
     public static List<StopShort> list (Collection<Stop> in) {
