a1804260783e84e80e88109b0859685f3443bb18
Andrew Byrd <andrew@fastmail.net>
2014-01-06 22:08:53 +0100

    modify detailed pattern model to include trips

--- otp-core/src/main/java/org/opentripplanner/index/model/PatternDetail.java
+++ otp-core/src/main/java/org/opentripplanner/index/model/PatternDetail.java
@@ -2,7 +2,6 @@ package org.opentripplanner.index.model;
 
 import java.util.Collection;
 
-import org.onebusaway.gtfs.model.Stop;
 import org.opentripplanner.routing.edgetype.TableTripPattern;
 
 import com.beust.jcommander.internal.Lists;
@@ -11,15 +10,15 @@ public class PatternDetail extends PatternShort {
 
     public String routeId;
     public Collection<StopShort> stops = Lists.newArrayList();
+    public Collection<TripShort> trips = Lists.newArrayList();
     
     // Include all known headsigns
     
     public PatternDetail(TableTripPattern pattern) {
         super (pattern);
         routeId = pattern.route.getId().getId();
-        for (Stop stop : pattern.getStops()) {
-            stops.add(new StopShort(stop));
-        }
+        stops = StopShort.list(pattern.getStops());
+        trips = TripShort.list(pattern.getTrips());
     }
     
 }
