ef4edcfc87a901302098ed6b3bc54526cc4c5998
Andrew Byrd <andrew@fastmail.net>
2014-02-12 14:42:04 +0100

    add package info files for Javadoc

---
+++ otp-core/src/main/java/org/opentripplanner/api/parameter/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains classes which interpret incoming HTTP query parameters. Query parameters
+ * arrive as Strings, and Jersey will automatically call constructors with a single String argument.
+ */
+package org.opentripplanner.api.parameter;
+
