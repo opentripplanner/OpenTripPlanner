7b6ad0ad854200893be0eab3239b6da024e09b35
Andrew Byrd <andrew@fastmail.net>
2014-02-12 14:42:04 +0100

    add package info files for Javadoc

---
+++ otp-core/src/main/java/org/opentripplanner/api/resource/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the JAX-RS-annotated REST resource classes for the OpenTripPlanner public
+ * API, i.e. the Jersey REST endpoints. 
+ */
+package org.opentripplanner.api.resource;
+
