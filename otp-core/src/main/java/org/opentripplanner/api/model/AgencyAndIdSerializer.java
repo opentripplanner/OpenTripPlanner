111990eb9b282b369fd2b65cef11e5a55530c4a4
Andrew Byrd <andrew@fastmail.net>
2014-02-22 04:38:48 +0100

    include module-maker method in AgencyAndId serializer

--- otp-core/src/main/java/org/opentripplanner/api/model/AgencyAndIdSerializer.java
+++ otp-core/src/main/java/org/opentripplanner/api/model/AgencyAndIdSerializer.java
@@ -6,18 +6,26 @@ import org.onebusaway.gtfs.model.AgencyAndId;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class AgencyAndIdSerializer extends JsonSerializer<AgencyAndId> {
 
     public static final String SEPARATOR = ":";
+
+    public static SimpleModule makeModule () {
+        Version moduleVersion = new Version(1, 0, 0, null, null, null);
+        SimpleModule module = new SimpleModule("OTP", moduleVersion);
+        module.addSerializer(AgencyAndId.class, new AgencyAndIdSerializer());
+        return module;
+    }
+
     @Override
     public void serialize(AgencyAndId a, JsonGenerator gen, SerializerProvider prov)
             throws IOException, JsonProcessingException {
-
         gen.writeString(a.getAgencyId() + SEPARATOR + a.getId());
-        
     }
 
     // Gets around type erasure, allowing 
