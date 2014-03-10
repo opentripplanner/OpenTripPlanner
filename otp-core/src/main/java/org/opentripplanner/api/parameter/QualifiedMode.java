b34600fdc198accbf3172223db4324b74125f871
Andrew Byrd <andrew@fastmail.net>
2014-03-10 22:00:48 +0100

    add sketch of "qualified modes" expressing park and ride / bike rental

---
+++ otp-core/src/main/java/org/opentripplanner/api/parameter/QualifiedMode.java
@@ -0,0 +1,35 @@
+package org.opentripplanner.api.parameter;
+
+import java.security.InvalidParameterException;
+import java.util.Set;
+
+import org.opentripplanner.routing.core.TraverseMode;
+
+import com.beust.jcommander.internal.Sets;
+
+public class QualifiedMode {
+
+    public final TraverseMode mode;
+    public final Set<Qualifier> qualifiers = Sets.newHashSet();
+    
+    public QualifiedMode(String qMode) {
+        String[] elements = qMode.split("_");
+        mode = TraverseMode.valueOf(elements[0]);
+        if (mode == null) {
+            throw new InvalidParameterException();
+        }
+        for (int i = 0; i < elements.length; i++) {
+            Qualifier q = Qualifier.valueOf(elements[i]);
+            if (q == null) {
+                throw new InvalidParameterException();
+            } else {
+                qualifiers.add(q);
+            }
+        }
+    }
+
+}
+
+enum Qualifier {
+    RENT, HAVE, PARK, KEEP
+}
