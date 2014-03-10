1cc6b25bf2c45a9b007507b56fe557a87950d27a
Andrew Byrd <andrew@fastmail.net>
2014-03-10 16:02:54 +0100

    Add node tracker class and test

---
+++ otp-core/src/main/java/org/opentripplanner/osm/NodeTrackerTest.java
@@ -0,0 +1,43 @@
+package org.opentripplanner.osm;
+
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import com.beust.jcommander.internal.Sets;
+
+public class NodeTrackerTest extends TestCase {
+
+    /** Check the NodeTracker against a stock Set<Long>. */
+    public static void testAgainstSet() {
+        // track big numbers > 2^32
+        for (long lo : new long[] {1L << 6, 1L << 16, 1L << 34, 1L << 50} ) {
+            Set<Long> numbers = Sets.newHashSet();
+            NodeTracker tracker = new NodeTracker();
+            long hi = 0L;
+            int count = 0;
+            for (int i = 0; i < 3000; i++) {            
+                long n = i * i + lo;
+                numbers.add(n);
+                tracker.set(n);
+                hi = n;
+                count++;
+            }
+            assertTrue(numbers.size() == count);
+            System.out.printf("Testing node tracker on %d numbers in range [%d,%d]\n", count, lo, hi);
+            /* Note that a Set<Long> containing 0L returns false for contains((int)0). */
+            for (long i = lo; i < hi; i++) {
+                if (tracker.get(i)) {
+                    assertTrue(numbers.contains(i));
+                } else {
+                    assertFalse(numbers.contains(i));
+                }
+                if (numbers.contains(i)) {
+                    assertTrue(tracker.get(i));
+                } else {
+                    assertFalse(tracker.get(i));
+                }
+            }
+        }
+    }
+}
