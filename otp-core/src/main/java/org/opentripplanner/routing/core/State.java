aa2371d0c375e7b48bd45c62e4469caacf1d6e75
Andrew Byrd <andrew@fastmail.net>
2014-03-21 16:40:43 +0100

    comment

--- otp-core/src/main/java/org/opentripplanner/routing/core/State.java
+++ otp-core/src/main/java/org/opentripplanner/routing/core/State.java
@@ -314,9 +314,10 @@ public class State implements Cloneable {
             return false;
 
         if (this.routeSequenceSubset(other)) {
-            // TODO subset is not really the right idea, try adding || this.getNumBoardings() <= other.getNumBoardings()
+            // TODO subset is not really the right idea
             return this.weight <= other.weight &&
                     other.getElapsedTimeSeconds() >= this.getElapsedTimeSeconds();
+            // && this.getNumBoardings() <= other.getNumBoardings();
         }
 
         // If returning more than one result from GenericAStar, the search can be very slow
