29d1f1317e7082b1c12d10ad5947d6610b54637c
Andrew Byrd <andrew@fastmail.net>
2014-03-21 18:51:20 +0100

    remove /ws/ prefix and add routerId to conform to new API

--- otp-leaflet-client/src/main/webapp/js/otp/core/Webapp.js
+++ otp-leaflet-client/src/main/webapp/js/otp/core/Webapp.js
@@ -42,7 +42,7 @@ otp.core.Webapp = otp.Class({
         var this_ = this;
         
         otp.config.resourcePath = otp.config.resourcePath || "";
-        otp.config.restService = otp.config.restService || "otp-rest-servlet";
+        otp.config.restService = otp.config.restService || "otp-rest-servlet/default";
 
 
         // set the urlRoot variable, if needed
