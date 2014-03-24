71d15a8965dd51f9cd53fcb40819391555a8163c
Andrew Byrd <andrew@fastmail.net>
2014-03-24 14:15:19 +0100

    remove unmaintained thrift API

--- otp-thrift-api/src/main/resources/scripts/build_thrift.sh
+++
@@ -1,5 +0,0 @@
-thrift -r -gen java:beans opentripplanner-api-thrift/src/main/thrift/api.thrift
-thrift -r -gen py opentripplanner-api-thrift/src/main/thrift/api.thrift
-rm -rf opentripplanner-api-thrift/src/main/java/org/opentripplanner/api/thrift/definition/
-mkdir -p opentripplanner-api-thrift/src/main/java/org/opentripplanner/api/thrift/definition/ 
-cp gen-javabean/org/opentripplanner/api/thrift/definition/*.java opentripplanner-api-thrift/src/main/java/org/opentripplanner/api/thrift/definition/
