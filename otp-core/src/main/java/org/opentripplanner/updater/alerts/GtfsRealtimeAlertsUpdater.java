76ca0b6318d34089f0d0ccb38690fce524655008
Jorden Verwer <jorden.verwer@goabout.com>
2014-03-24 09:57:41 +0100

    Fix another typo

--- otp-core/src/main/java/org/opentripplanner/updater/alerts/GtfsRealtimeAlertsUpdater.java
+++ otp-core/src/main/java/org/opentripplanner/updater/alerts/GtfsRealtimeAlertsUpdater.java
@@ -115,7 +115,7 @@ public class GtfsRealtimeAlertsUpdater extends PollingGraphUpdater {
 
             lastTimestamp = feedTimestamp;
         } catch (Exception e) {
-            LOG.error("Eror reading gtfs-realtime feed from " + url, e);
+            LOG.error("Error reading gtfs-realtime feed from " + url, e);
         }
     }
 
