0ae9d46d3e18e6e227d3f49c6155944a582d0db2
Andrew Byrd <andrew@fastmail.net>
2014-04-01 15:57:13 +0200

    remove otp-analyst module. it should be external or merged into the main leaflet client.
    otp now has only a single maven-built submodule.

--- otp-analyst-client/src/main/webapp/js/lib/proj4js-1.0.3/lib/defs/EPSG2269.js
+++
@@ -1,1 +0,0 @@
-Proj4js.defs["EPSG:2269"] = "+proj=lcc +lat_1=46 +lat_2=44.33333333333334 +lat_0=43.66666666666666 +lon_0=-120.5 +x_0=2500000.0001424 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048 +no_defs";
--- otp-analyst-client/src/main/webapp/js/lib/proj4js-1.0.3/lib/defs/EPSG32126.js
+++
@@ -1,1 +0,0 @@
-Proj4js.defs["EPSG:32126"] = "+proj=lcc +lat_1=46 +lat_2=44.33333333333334 +lat_0=43.66666666666666 +lon_0=-120.5 +x_0=2500000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs";
