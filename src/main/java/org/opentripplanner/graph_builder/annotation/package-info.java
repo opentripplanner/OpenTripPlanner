a5a3ceb49ae94c2dbc85095f1b37339f2d27c0e6
Andrew Byrd <andrew@fastmail.net>
2014-04-23 16:12:09 +0200

    Add package-info explaining what graph builder annotations are.

---
+++ src/main/java/org/opentripplanner/graph_builder/annotation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Graph builder annotation classes represent errors or exceptional conditions encountered
+ * during the graph bulding process. They contain descriptive messages and potentially
+ * references to the objects in the graph that they annotate which facilitate visualization and
+ * cataloging/mapping of problems.
+ */
+package org.opentripplanner.graph_builder.annotation;
