15ef961b92a89a8ddf0e88242031f718854a6ecd
Jorden Verwer <jorden.verwer@goabout.com>
2014-05-07 10:12:13 +0200

    Fix AlertPatchTest

--- src/test/java/org/opentripplanner/routing/alertpatch/AlertPatchTest.java
+++ src/test/java/org/opentripplanner/routing/alertpatch/AlertPatchTest.java
@@ -14,23 +14,13 @@
 package org.opentripplanner.routing.alertpatch;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 import junit.framework.TestCase;
 
-import org.onebusaway.gtfs.model.Agency;
 import org.onebusaway.gtfs.model.AgencyAndId;
-import org.onebusaway.gtfs.model.Route;
-import org.onebusaway.gtfs.model.ServiceCalendar;
-import org.onebusaway.gtfs.model.ServiceCalendarDate;
-import org.onebusaway.gtfs.model.Stop;
 import org.onebusaway.gtfs.model.calendar.CalendarServiceData;
 import org.opentripplanner.ConstantsForTests;
 import org.opentripplanner.gtfs.GtfsContext;
@@ -38,24 +28,14 @@ import org.opentripplanner.gtfs.GtfsLibrary;
 import org.opentripplanner.routing.algorithm.GenericAStar;
 import org.opentripplanner.routing.core.RoutingRequest;
 import org.opentripplanner.routing.core.State;
-import org.opentripplanner.routing.core.TraverseMode;
-import org.opentripplanner.routing.edgetype.PatternHop;
-import org.opentripplanner.routing.edgetype.PreAlightEdge;
-import org.opentripplanner.routing.edgetype.PreBoardEdge;
-import org.opentripplanner.routing.edgetype.TripPattern;
-import org.opentripplanner.routing.edgetype.TransitBoardAlight;
 import org.opentripplanner.routing.edgetype.factory.GTFSPatternHopFactory;
 import org.opentripplanner.routing.graph.Edge;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graph.Vertex;
-import org.opentripplanner.routing.services.TransitIndexService;
+import org.opentripplanner.routing.impl.DefaultStreetVertexIndexFactory;
 import org.opentripplanner.routing.spt.GraphPath;
 import org.opentripplanner.routing.spt.ShortestPathTree;
-import org.opentripplanner.routing.transit_index.RouteSegment;
-import org.opentripplanner.routing.transit_index.RouteVariant;
 import org.opentripplanner.util.TestUtils;
-
-import com.vividsolutions.jts.geom.Coordinate;
 
 public class AlertPatchTest extends TestCase {
     private Graph graph;
@@ -65,163 +45,15 @@ public class AlertPatchTest extends TestCase {
     private GenericAStar aStar = new GenericAStar();
 
     public void setUp() throws Exception {
-
         GtfsContext context = GtfsLibrary.readGtfs(new File(ConstantsForTests.FAKE_GTFS));
         options = new RoutingRequest();
         graph = new Graph();
         GTFSPatternHopFactory factory = new GTFSPatternHopFactory(context);
         factory.run(graph);
         graph.putService(CalendarServiceData.class, GtfsLibrary.createCalendarServiceData(context.getDao()));
-
-        TransitIndexService index = new TransitIndexService() {
-            /*
-             * mock TransitIndexService always returns preboard/prealight edges for stop A and a
-             * subset of variants for route 1
-             */
-            @SuppressWarnings("deprecation")
-            @Override
-            public PreAlightEdge getPreAlightEdge(AgencyAndId stop) {
-                return (PreAlightEdge) graph.getVertex("agency_A_arrive").getOutgoing().iterator().next();
-            }
-
-            @SuppressWarnings("deprecation")
-            @Override
-            public PreBoardEdge getPreBoardEdge(AgencyAndId stop) {
-                return (PreBoardEdge) graph.getVertex("agency_A_depart").getIncoming().iterator().next();
-            }
-
-            @Override
-            public RouteVariant getVariantForTrip(AgencyAndId trip) {
-                return null;
-            }
-
-            @Override
-            public List<RouteVariant> getVariantsForAgency(String agency) {
-                return null;
-            }
-
-            @SuppressWarnings("deprecation")
-            @Override
-            public List<RouteVariant> getVariantsForRoute(AgencyAndId routeId) {
-                Route route = new Route();
-                route.setId(routeId);
-                route.setShortName(routeId.getId());
-
-                TransitBoardAlight somePatternBoard = (TransitBoardAlight) graph.getVertex("agency_A_depart")
-                        .getOutgoing().iterator().next();
-                assertTrue(somePatternBoard.isBoarding());
-                PatternHop somePatternHop = (PatternHop) somePatternBoard.getToVertex()
-                        .getOutgoing().iterator().next();
-
-                Stop stopA = somePatternHop.getBeginStop();
-                ArrayList<Stop> stops = new ArrayList<Stop>();
-                stops.add(stopA);
-
-                RouteVariant variant = new RouteVariant(route, stops);
-                RouteSegment segment = new RouteSegment(stopA.getId());
-
-                segment.board = somePatternBoard;
-                segment.hopOut = somePatternHop;
-                variant.addSegment(segment);
-
-                ArrayList<RouteVariant> variants = new ArrayList<RouteVariant>();
-                variants.add(variant);
-                return variants;
-            }
-
-            @Override
-            public List<String> getDirectionsForRoute(AgencyAndId route) {
-                return Collections.emptyList();
-            }
-
-            @Override
-            public List<TraverseMode> getAllModes() {
-                return Collections.emptyList();
-            }
-
-            @Override
-            public List<AgencyAndId> getAllRouteIds() {
-                return Collections.emptyList();
-            }
-
-            @Override
-            public void addCalendars(Collection<ServiceCalendar> allCalendars) {
-            }
-
-            @Override
-            public void addCalendarDates(Collection<ServiceCalendarDate> allDates) {
-            }
-
-            @Override
-            public List<String> getAllAgencies() {
-                return Arrays.asList("agency");
-            }
-
-            @Override
-            public List<ServiceCalendarDate> getCalendarDatesByAgency(String agency) {
-                return null;
-            }
-
-            @Override
-            public List<ServiceCalendar> getCalendarsByAgency(String agency) {
-                return null;
-            }
-
-            @Override
-            public Agency getAgency(String id) {
-                return null;
-            }
-
-            @Override
-            public List<AgencyAndId> getRoutesForStop(AgencyAndId stop) {
-                return null;
-            }
-
-            @Override
-            public List<TripPattern> getTripPatternsForStop(AgencyAndId stop) {
-                return null;
-            }
-
-            @Override
-            public Coordinate getCenter() {
-                return null;
-            }
-
-            @Override
-            public int getOvernightBreak() {
-                return 0;
-            }
-
-            @Override
-            public Collection<Stop> getStopsForRoute(AgencyAndId route) {
-                return Collections.emptyList();
-            }
-
-            @Override
-            public TripPattern getTripPatternForTrip(AgencyAndId tripId) {
-                return null;
-            }
-
-
-            @Override
-            public Map<AgencyAndId, Stop> getAllStops() {
-                return null;
-            }
-
-            @Override
-            public Map<AgencyAndId, Route> getAllRoutes() {
-                return null;
-            }
-
-            @Override
-            public List<Stop> getStopsForStation(AgencyAndId stop) {
-                return null;
-            }
-        };
-        graph.putService(TransitIndexService.class, index);
+        graph.index(new DefaultStreetVertexIndexFactory());
     }
 
-    @SuppressWarnings("deprecation")
     public void testStopAlertPatch() {
         AlertPatch snp1 = new AlertPatch();
         snp1.setTimePeriods(Collections.singletonList(new TimePeriod(
@@ -263,7 +95,6 @@ public class AlertPatchTest extends TestCase {
         assertEquals(expectedAlerts, unoptimizedAlerts);
     }
 
-    @SuppressWarnings("deprecation")
     public void testTimeRanges() {
         AlertPatch snp1 = new AlertPatch();
         LinkedList<TimePeriod> timePeriods = new LinkedList<TimePeriod>();
@@ -322,7 +153,6 @@ public class AlertPatchTest extends TestCase {
         assertEquals(expectedNotes, oneAlertPatchAlerts);
     }
 
-    @SuppressWarnings("deprecation")
     public void testRouteNotePatch() {
         AlertPatch rnp1 = new AlertPatch();
 
