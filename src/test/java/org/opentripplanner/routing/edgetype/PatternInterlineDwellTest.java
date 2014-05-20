13ac30a3e61cd908ae561a98fd2a088bf0c5da98
Andrew Byrd <andrew@fastmail.net>
2014-05-20 19:51:18 +0200

    expand interline dwell test.
    It fails, apparently because there is a cost mismatch between interline edges
    and board edges.

--- src/test/java/org/opentripplanner/routing/edgetype/PatternInterlineDwellTest.java
+++ src/test/java/org/opentripplanner/routing/edgetype/PatternInterlineDwellTest.java
@@ -21,6 +21,7 @@ import org.onebusaway.gtfs.model.StopTime;
 import org.onebusaway.gtfs.model.Trip;
 import org.onebusaway.gtfs.model.calendar.ServiceDate;
 import org.opentripplanner.GtfsTest;
+import org.opentripplanner.api.model.Leg;
 import org.opentripplanner.routing.core.RoutingContext;
 import org.opentripplanner.routing.core.RoutingRequest;
 import org.opentripplanner.routing.core.ServiceDay;
@@ -34,6 +35,9 @@ import org.opentripplanner.routing.vertextype.OnboardVertex;
 import org.opentripplanner.updater.stoptime.TimetableSnapshotSource;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.TimeZone;
 
@@ -45,12 +49,25 @@ import static org.mockito.Mockito.when;
 public class PatternInterlineDwellTest extends GtfsTest {
 
     @Override
+    public boolean isLongDistance() { return true; } // retrying wrecks the tests
+
+    @Override
     public String getFeedName() {
         return "gtfs/interlining";
     }
 
+    // TODO Allow using Calendar or ISOdate for testing, interpret it in the given graph's timezone.
+
     public void testInterlining() {
-        assertTrue(1 == 1);
+        Calendar calendar = new GregorianCalendar(2014, Calendar.JANUARY, 01, 00, 05, 00);
+        calendar.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        long time = calendar.getTime().getTime() / 1000;
+        // We should arrive at the destination using two legs, both of which are on
+        // the same route and with zero transfers.
+        Leg[] legs = plan(time, "il0", "il3", null, false, false, null, null, null, 2);
+        assertEquals(legs[0].routeId, "il1");
+        assertEquals(legs[1].routeId, "il1");
+        assertTrue(itinerary.transfers == 0);
     }
 
 }
