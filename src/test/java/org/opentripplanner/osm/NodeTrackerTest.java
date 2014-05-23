f135c9f124be76ed852cee30c373cbd4cc33e459
Andrew Byrd <andrew@fastmail.net>
2014-05-23 10:46:27 +0200

    NodeTrackerTest: reduce output and increase speed

--- src/test/java/org/opentripplanner/osm/NodeTrackerTest.java
+++ src/test/java/org/opentripplanner/osm/NodeTrackerTest.java
@@ -10,16 +10,22 @@ import org.junit.Test;
 
 public class NodeTrackerTest {
 
-    /** Check the NodeTracker against a stock Set<Long>. */
+    /**
+     * Check the NodeTracker against a stock Set<Long>.
+     * This is done in ranges that will include numbers significantly greater than 2^32
+     */
     @Test
     public void testAgainstSet() {
-        // track big numbers > 2^32
+        final int N = 1000;
+        // Set N numbers in each of four different ranges.
         for (long lo : new long[] {1L << 6, 1L << 16, 1L << 34, 1L << 50} ) {
+            // Simultaneously set up a basic Set<Long> that will serve as a reference
+            // and a NodeTracker that we want to test.
             Set<Long> numbers = Sets.newHashSet();
             NodeTracker tracker = new NodeTracker();
             long hi = 0L;
             int count = 0;
-            for (int i = 0; i < 3000; i++) {            
+            for (int i = 0; i < 1000; i++) {
                 long n = i * i + lo;
                 numbers.add(n);
                 tracker.add(n);
@@ -27,8 +33,8 @@ public class NodeTrackerTest {
                 count++;
             }
             assertTrue(numbers.size() == count);
-            System.out.printf("Testing node tracker on %d numbers in range [%d,%d]\n", count, lo, hi);
-            /* Note that a Set<Long> containing 0L returns false for contains((int)0). */
+            // Check that the two set implementations match for every value in the range (including unset ones).
+            // Note that a Set<Long> containing 0L returns false for contains((int)0).
             for (long i = lo; i < hi; i++) {
                 if (tracker.contains(i)) {
                     assertTrue(numbers.contains(i));
