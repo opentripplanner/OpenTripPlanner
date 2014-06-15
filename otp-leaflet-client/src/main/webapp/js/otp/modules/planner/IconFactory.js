0f0beb57c82c152656f11ae6b11c3c1563636561
Marko Burjek <s0mebody.slo@gmail.com>
2014-06-15 23:30:54 +0200

    Forgotten to change time format in icon

--- otp-leaflet-client/src/main/webapp/js/otp/modules/planner/IconFactory.js
+++ otp-leaflet-client/src/main/webapp/js/otp/modules/planner/IconFactory.js
@@ -317,7 +317,7 @@ otp.modules.planner.IconFactory = otp.Class({
         html += '<img src="'+otp.config.resourcePath+'images/mode/'+mode+'.png">';
         if(isOrigin) html += '<img src="'+otp.config.resourcePath+'images/mode/arrow.png" style="margin-left:2px;">';
         html += '</div>';
-        html +=  otp.util.Time.formatItinTime(time, "h:mm");
+        html +=  otp.util.Time.formatItinTime(time, otp.config.locale.time.time_format);
         
         if(quadrant === 'nw') anchor = [32,44];
         if(quadrant === 'ne') anchor = [0,44];
