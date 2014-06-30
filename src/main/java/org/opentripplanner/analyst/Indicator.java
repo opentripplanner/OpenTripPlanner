767c08e883ba21950c7798f16936958b3e70f28d
Brandon Martin-Anderson <badhill@gmail.com>
2014-06-30 00:10:01 -0700

    move things out of IndicatorLite to Indicator

--- src/main/java/org/opentripplanner/analyst/Indicator.java
+++ src/main/java/org/opentripplanner/analyst/Indicator.java
@@ -15,11 +15,46 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Indicator extends IndicatorLite {
 
 	protected PointSet origins; // the origins, which correspond one-to-one with the Quantiles array
+	protected PointSet targets; // the targets that were checked for reachability in this indicator
     protected TimeSurface surface; // actually there is one per origin, not a single one!
+    
+    Map<String,Integer> idTimeIndex = new ConcurrentHashMap<String,Integer>();
      
     public Indicator (SampleSet samples, TimeSurface surface, boolean retainTimes) {
         super(samples, surface, retainTimes); // for now we only do one-to-many
         this.surface = surface;
+        this.targets = samples.pset;
+    }
+    
+    public Integer getTime(String id) {
+    	
+    	if(times != null){
+    		synchronized(idTimeIndex) {
+        		if(idTimeIndex.keySet().size() < this.times[0].length) {
+            		idTimeIndex.clear();
+            		int i = 0;
+            		for(String sampleId : this.targets.ids) {
+            			idTimeIndex.put(sampleId, this.times[0][i]);
+            			i++;
+            		}
+            	}
+        	}
+        	return idTimeIndex.get(id);
+    	}
+    	else
+    		return null;
+    	
+    }
+    
+    protected void writeStructured(int i , JsonGenerator jgen) throws IOException{
+    	super.writeStructured(i, jgen);
+		/*
+		 * Write out travel times to each target ID if this is a
+		 * detailed indicator.
+		 */
+		if (this instanceof Indicator && times != null) {
+			((Indicator) this).targets.writeTimes(jgen, times[i]);
+		}
     }
 		
 }
--- src/main/java/org/opentripplanner/analyst/IndicatorLite.java
+++ src/main/java/org/opentripplanner/analyst/IndicatorLite.java
@@ -33,19 +33,15 @@ import com.fasterxml.jackson.core.JsonGenerator;
 public class IndicatorLite extends PointSet {
 
 	private static final long serialVersionUID = -6723127825189535112L;
-
-	protected PointSet targets; // the targets that were checked for reachability in this indicator
     
 	/*
 	 * The time to reach each target, for each origin.
 	 */
 	public int[][] times;
 	
-    Map<String,Integer> idTimeIndex = new ConcurrentHashMap<String,Integer>();
-
     public IndicatorLite (SampleSet samples, TimeSurface surface, boolean retainTimes) {
         super(1); // for now we only do one-to-many
-        this.targets = samples.pset;
+        PointSet targets = samples.pset;
         // Perform a deep copy of everything but the actual magnitudes for the attributes
         for (Category cat : targets.categories.values()) {
             this.categories.put(cat.id, new Category(cat));
@@ -65,24 +61,8 @@ public class IndicatorLite extends PointSet {
         }
     }
     
-    public Integer getTime(String id) {
-    	
-    	if(times != null){
-    		synchronized(idTimeIndex) {
-        		if(idTimeIndex.keySet().size() < this.times[0].length) {
-            		idTimeIndex.clear();
-            		int i = 0;
-            		for(String sampleId : this.targets.ids) {
-            			idTimeIndex.put(sampleId, this.times[0][i]);
-            			i++;
-            		}
-            	}
-        	}
-        	return idTimeIndex.get(id);
-    	}
-    	else
-    		return null;
-    	
+    public IndicatorLite (SampleSet samples, TimeSurface surface) {
+    	this( samples, surface, false );
     }
     
     /**
@@ -95,15 +75,6 @@ public class IndicatorLite extends PointSet {
 
     }
     
-    protected void writeStructured(int i , JsonGenerator jgen) throws IOException{
-    	super.writeStructured(i, jgen);
-		/*
-		 * Write out travel times to each target ID if this is a
-		 * detailed indicator.
-		 */
-		if (this instanceof Indicator && times != null) {
-			((Indicator) this).targets.writeTimes(jgen, times[i]);
-		}
-    }
+
 
 }
