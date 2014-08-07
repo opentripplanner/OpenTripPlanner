fe325d67bb1e32ace2fd6a4708acb02db3629d5d
Andrew Byrd <andrew@fastmail.net>
2014-08-07 14:45:05 +0200

    use agencyAndId in RouteShort instead of separate fields
    this moves toward the agency:id format

--- src/main/java/org/opentripplanner/index/model/RouteShort.java
+++ src/main/java/org/opentripplanner/index/model/RouteShort.java
@@ -3,6 +3,7 @@ package org.opentripplanner.index.model;
 import java.util.Collection;
 import java.util.List;
 
+import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.gtfs.model.Route;
 import org.opentripplanner.gtfs.GtfsLibrary;
 
@@ -10,16 +11,14 @@ import com.beust.jcommander.internal.Lists;
 
 public class RouteShort {
 
-    public String agency;
-    public String id;
+    public AgencyAndId routeId;
     public String shortName;
     public String longName;
     public String mode;
     public String color;
     
     public RouteShort (Route route) {
-        agency = route.getId().getAgencyId();
-        id = route.getId().getId();
+        routeId = route.getId();
         shortName = route.getShortName();
         longName = route.getLongName();
         mode = GtfsLibrary.getTraverseMode(route).toString();
