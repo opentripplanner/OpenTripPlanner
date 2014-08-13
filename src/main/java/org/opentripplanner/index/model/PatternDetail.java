fd764e940e10923caeb73e5226ac8837bad03fa1
Andrew Byrd <andrew@fastmail.net>
2014-08-13 17:49:32 +0200

    use feed:id instead of id alone in pattern API responses

--- src/main/java/org/opentripplanner/index/model/PatternDetail.java
+++ src/main/java/org/opentripplanner/index/model/PatternDetail.java
@@ -2,6 +2,7 @@ package org.opentripplanner.index.model;
 
 import java.util.Collection;
 
+import org.onebusaway.gtfs.model.AgencyAndId;
 import org.opentripplanner.routing.edgetype.TripPattern;
 
 import com.beust.jcommander.internal.Lists;
@@ -9,7 +10,7 @@ import com.beust.jcommander.internal.Lists;
 public class PatternDetail extends PatternShort {
 
     /* Maybe these should just be lists of IDs only, since there are stops and trips subendpoints. */
-    public String routeId;
+    public AgencyAndId routeId;
     public Collection<StopShort> stops = Lists.newArrayList();
     public Collection<TripShort> trips = Lists.newArrayList();
     
@@ -17,7 +18,7 @@ public class PatternDetail extends PatternShort {
     
     public PatternDetail(TripPattern pattern) {
         super (pattern);
-        routeId = pattern.route.getId().getId();
+        routeId = pattern.route.getId();
         stops = StopShort.list(pattern.getStops());
         trips = TripShort.list(pattern.getTrips());
     }
