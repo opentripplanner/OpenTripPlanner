2cf3d25134f37637f26823765a55e165d2c6c772
Marko Burjek <s0mebody.slo@gmail.com>
2014-08-18 11:30:06 +0200

    Added javadoc to LocalizedString

--- otp-core/src/main/java/org/opentripplanner/util/LocalizedString.java
+++ otp-core/src/main/java/org/opentripplanner/util/LocalizedString.java
@@ -23,6 +23,10 @@ import org.opentripplanner.openstreetmap.model.OSMWithTags;
 public class LocalizedString implements I18NString, Serializable {
     private static final Pattern pattern = Pattern.compile("\\{(.*?)\\}");
     private static final Matcher matcher = pattern.matcher("");
+    
+    /**
+     * Map which key has which tagName. Used only when building graph.
+     */
     private transient static Map<String, String> key_params;
     
     static {
@@ -32,12 +36,28 @@ public class LocalizedString implements I18NString, Serializable {
     private String key;
     private String[] params;
 
+    /**
+     * Creates String which can be localized
+     * @param key key of translation for this way set in {@link DefaultWayPropertySetSource} and translations read from from properties Files
+     * @param params Values with which tagNames are replaced in translations.
+     */
     public LocalizedString(String key, String[] params) {
         this.key = key;
         this.params = params;
     }
-    
-    //Gets params from translated key
+
+    /**
+     * Creates String which can be localized
+     * <p>
+     * Uses {@link #getTagNames() } to get which tag values are needed for this key.
+     * For each of this tag names tag value is get from OSM way.
+     * 
+     * For example. If key platform has key ref current value of tag ref in way is saved to be used in localizations.
+     * It currently assumes that tag exists in way. (otherwise this namer wouldn't be used)
+     * </p>
+     * @param key key of translation for this way set in {@link DefaultWayPropertySetSource} and translations read from from properties Files
+     * @param way OSM way from which tag values are read
+     */
     public LocalizedString(String key, OSMWithTags way) {
         this.key = key;
         List<String> lparams = new ArrayList<String>(1);
@@ -54,8 +74,16 @@ public class LocalizedString implements I18NString, Serializable {
     }
     
     /**
-     * Finds wanted tag names in name For example "Platform {ref}" ref is way tagname
-     * @return 
+     * Finds wanted tag names in name
+     * <p>
+     * It uses English localization for key to get tag names.
+     * Tag names have to be enclosed in brackets.
+     * 
+     * For example "Platform {ref}" ref is way tagname.
+     * 
+     * NOTE: Only one tag name is currently supported.
+     * </p>
+     * @return tagName
      */
     private String getTagNames() {
         if( key_params.containsKey(key)) {
