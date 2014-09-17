24d8aaa2f908e1018bd2ea83bdf64932da0f2cad
Andrew Byrd <andrew@fastmail.net>
2014-09-17 09:34:35 -0700

    time window intersection method for frequency-based profile routing

--- src/main/java/org/opentripplanner/profile/TimeWindow.java
+++ src/main/java/org/opentripplanner/profile/TimeWindow.java
@@ -16,4 +16,14 @@ class TimeWindow {
 	boolean includes (int t) {
         return t > from && t < to;
     }
+
+    /* @return the number of seconds in the given interval overlapping this window. */
+    int overlap(int from2, int to2, int serviceCode) {
+        if ( ! servicesRunning.get(serviceCode)) return 0;
+        if (from2 > to || to2 < from) return 0;
+        int max_from = Math.max(from, from2);
+        int min_to = Math.min(to, to2);
+        return min_to - max_from;
+    }
+
 }
