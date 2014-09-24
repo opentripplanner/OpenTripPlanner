8068723c774190aee3048863462968d6c9da4d63
Laurent Grégoire <laurent.gregoire@gmail.com>
2014-09-24 15:48:27 +0200

    Add i18n capabilities to OSM TemplateLibrary.

--- src/main/java/org/opentripplanner/graph_builder/impl/osm/TemplateLibrary.java
+++ src/main/java/org/opentripplanner/graph_builder/impl/osm/TemplateLibrary.java
@@ -13,6 +13,8 @@
 
 package org.opentripplanner.graph_builder.impl.osm;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,7 +31,7 @@ public class TemplateLibrary {
         StringBuffer gen_name = new StringBuffer();
 
         Matcher matcher = patternMatcher.matcher(pattern);
-        
+
         int lastEnd = 0;
         while (matcher.find()) {
             // add the stuff before the match
@@ -45,5 +47,50 @@ public class TemplateLibrary {
         gen_name.append(pattern, lastEnd, pattern.length());
 
         return gen_name.toString();
+    }
+
+    public static Map<String, String> generateI18N(String pattern, OSMWithTags way,
+            String defaultLang) {
+
+        if (pattern == null)
+            return null;
+
+        Map<String, StringBuffer> i18n = new HashMap<String, StringBuffer>();
+        i18n.put(defaultLang, new StringBuffer());
+        Matcher matcher = patternMatcher.matcher(pattern);
+
+        int lastEnd = 0;
+        while (matcher.find()) {
+            // add the stuff before the match
+            for (StringBuffer sb : i18n.values())
+                sb.append(pattern, lastEnd, matcher.start());
+            lastEnd = matcher.end();
+            // and then the value for the match
+            String defKey = matcher.group(1);
+            // scan all translated tags
+            Map<String, String> i18nTags = way.getTagsByPrefix(defKey);
+            if (i18nTags != null) {
+                for (Map.Entry<String, String> kv : i18nTags.entrySet()) {
+                    if (!kv.getKey().equals(defKey)) {
+                        String lang = kv.getKey().substring(defKey.length() + 1);
+                        if (!i18n.containsKey(lang))
+                            i18n.put(lang, new StringBuffer(i18n.get(defaultLang)));
+                    }
+                }
+            }
+            // get the simple value (eg: description=...)
+            String defTag = way.getTag(defKey);
+            // get the translated value, if exists
+            for (String lang : i18n.keySet()) {
+                String i18nTag = way.getTag(defKey + ":" + lang);
+                i18n.get(lang).append(i18nTag != null ? i18nTag : (defTag != null ? defTag : ""));
+            }
+        }
+        for (StringBuffer sb : i18n.values())
+            sb.append(pattern, lastEnd, pattern.length());
+        Map<String, String> out = new HashMap<String, String>(i18n.size());
+        for (Map.Entry<String, StringBuffer> kv : i18n.entrySet())
+            out.put(kv.getKey(), kv.getValue().toString());
+        return out;
     }
 }
