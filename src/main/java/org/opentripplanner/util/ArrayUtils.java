c33fc86f0283f107c5d4222937032268c0b2ed3b
Laurent Grégoire <laurent.gregoire@gmail.com>
2014-09-30 12:46:58 +0200

    Add ArrayUtils.deepEquals() for int arrays.

--- src/main/java/org/opentripplanner/util/ArrayUtils.java
+++ src/main/java/org/opentripplanner/util/ArrayUtils.java
@@ -27,4 +27,13 @@ public class ArrayUtils {
         }
         return false;
     }
+
+    public static boolean deepEquals(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length)
+            return false;
+        for (int i = 0; i < arr1.length; i++)
+            if (arr1[i] != arr2[i])
+                return false;
+        return true;
+    }
 }
