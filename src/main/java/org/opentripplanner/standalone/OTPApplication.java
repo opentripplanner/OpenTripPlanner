c670ae9fbc94699f7aa5a3fef7bd3d1fd5288d87
Andrew Byrd <abyrd@analyst.ovh.net>
2014-10-17 12:32:09 +0200

    Bump Jersey, Grizzly and Jackson versions.
    We observed a problem with threads getting stuck with 100% CPU usage in epoll.
    Using newer Jersey version seems to resolve this.
    Newer Jersey requires JSON serialization features to be added explicitly.

--- src/main/java/org/opentripplanner/standalone/OTPApplication.java
+++ src/main/java/org/opentripplanner/standalone/OTPApplication.java
@@ -1,5 +1,7 @@
 package org.opentripplanner.standalone;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.fasterxml.jackson.jaxrs.xml.JacksonXMLProvider;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
@@ -119,6 +121,10 @@ public class OTPApplication extends Application {
         return Sets.newHashSet (
             // Show exception messages in responses
             new OTPExceptionMapper(),
+            // Enable Jackson JSON response serialization
+            new JacksonJsonProvider(),
+            // Enable Jackson XML response serialization
+            new JacksonXMLProvider(),
             // Serialize POJOs (unannotated) JSON using Jackson
             new JSONObjectMapperProvider(),
             // Allow injecting the OTP server object into Jersey resource classes
