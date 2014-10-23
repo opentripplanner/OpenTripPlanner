b8db159491880e361ef75ee3ec0860259b1b2415
Laurent Grégoire <laurent.gregoire@gmail.com>
2014-10-23 18:15:51 +0200

    Add toString to OSMRelationMember. Format code.

--- src/main/java/org/opentripplanner/openstreetmap/model/OSMRelationMember.java
+++ src/main/java/org/opentripplanner/openstreetmap/model/OSMRelationMember.java
@@ -15,33 +15,38 @@ package org.opentripplanner.openstreetmap.model;
 
 public class OSMRelationMember {
 
-  private String type;
+    private String type;
 
-  private long ref;
+    private long ref;
 
-  private String role;
+    private String role;
 
-  public String getType() {
-    return type;
-  }
+    public String getType() {
+        return type;
+    }
 
-  public void setType(String type) {
-    this.type = type;
-  }
+    public void setType(String type) {
+        this.type = type;
+    }
 
-  public long getRef() {
-    return ref;
-  }
+    public long getRef() {
+        return ref;
+    }
 
-  public void setRef(long ref) {
-    this.ref = ref;
-  }
+    public void setRef(long ref) {
+        this.ref = ref;
+    }
 
-  public String getRole() {
-    return role;
-  }
+    public String getRole() {
+        return role;
+    }
 
-  public void setRole(String role) {
-    this.role = role;
-  }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "osm rel " + type + ":" + role + ":" + ref;
+    }
 }
