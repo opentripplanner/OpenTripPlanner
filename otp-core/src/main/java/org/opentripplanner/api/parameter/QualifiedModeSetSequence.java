1320ab581780274dc66ddb513dcf10bc27675937
Andrew Byrd <andrew@fastmail.net>
2014-10-29 10:43:24 +0100

    Do not automatically enable WALK mode. Fixes #1429

--- otp-core/src/main/java/org/opentripplanner/api/parameter/QualifiedModeSetSequence.java
+++ otp-core/src/main/java/org/opentripplanner/api/parameter/QualifiedModeSetSequence.java
@@ -53,7 +53,8 @@ public class QualifiedModeSetSequence {
         for (QualifiedMode qMode : qModes) {
             req.modes.setMode(qMode.mode, true);
         }
-        req.modes.setMode(TraverseMode.WALK, true); // always turn on WALK. TODO: why do we even need a walk mode?
+        // We used to always set WALK to true, but this forced walking when someone wanted to use a bike.
+        // TODO clearly define mode semantics: does presence of mode mean it is allowable, preferred... ?
         for (QualifiedMode qMode : qModes) {
             if (qMode.mode == TraverseMode.BICYCLE) {
                 if (qMode.qualifiers.contains(Qualifier.RENT)) {
