laurentg/OpenTripPlanner-plannerstack

Initialize non transit mode in reverse optimization.
Minor code formatting.

otp-core/src/main/java/org/opentripplanner/routing/core/State.java
@@ -531,6 +531,7 @@ public class State implements Cloneable {
         newState.stateData.usingRentedBike = stateData.usingRentedBike;
         newState.stateData.carParked = stateData.carParked;
         newState.stateData.bikeParked = stateData.bikeParked;
+        newState.stateData.nonTransitMode = stateData.nonTransitMode;
         return newState;
     }
 
@@ -743,9 +744,9 @@ public class State implements Cloneable {
 
                     ret = ((TransitBoardAlight) edge).traverse(ret, orig.getBackState().getTimeSeconds());
                     newInitialWaitTime = ret.stateData.initialWaitTime;
-                }
-                else                   
+                } else {
                     ret = edge.traverse(ret);
+                }
 
                 if (ret == null) {
                     LOG.warn("Cannot reverse path at edge: " + edge +

opentripplanner/OpenTripPlanner

Conflicts:
	src/main/java/org/opentripplanner/routing/core/State.java

src/main/java/org/opentripplanner/routing/core/State.java
@@ -491,6 +491,7 @@ public class State implements Cloneable {
         // TODO Check if those two lines are needed:
         newState.stateData.usingRentedBike = stateData.usingRentedBike;
         newState.stateData.carParked = stateData.carParked;
+        newState.stateData.bikeParked = stateData.bikeParked;
         return newState;
     }
 
@@ -703,9 +704,9 @@ public class State implements Cloneable {
 
                     ret = ((TransitBoardAlight) edge).traverse(ret, orig.getBackState().getTimeSeconds());
                     newInitialWaitTime = ret.stateData.initialWaitTime;
-                }
-                else                   
+                } else {
                     ret = edge.traverse(ret);
+                }
 
                 if (ret != null && ret.getBackMode() != null && orig.getBackMode() != null &&
                         ret.getBackMode() != orig.getBackMode()) {