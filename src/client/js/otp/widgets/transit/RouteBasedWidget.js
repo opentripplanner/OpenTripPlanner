89e3b1c76d4d4c0c8c38791b9822cfcadb762f72
aguiraf <aguiraf@gmail.com>
2014-11-25 18:15:38 +0100

    add initial fix for TripViewer and StopViewer widgets
    add reference to indexApi
    TODO: add support for variant (now patterns) and stopTimes

--- src/client/js/otp/widgets/transit/RouteBasedWidget.js
+++ src/client/js/otp/widgets/transit/RouteBasedWidget.js
@@ -2,39 +2,39 @@
    modify it under the terms of the GNU Lesser General Public License
    as published by the Free Software Foundation, either version 3 of
    the License, or (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 otp.namespace("otp.widgets.transit");
 
-otp.widgets.transit.RouteBasedWidget = 
+otp.widgets.transit.RouteBasedWidget =
     otp.Class(otp.widgets.Widget, {
 
     module : null,
 
     agency_id : null,
-    
+
     activeLeg : null,
     timeIndex : null,
-    
+
     routeLookup : [], // for retrieving route obj from numerical index in <select> element
-    
+
     lastSize : null,
-    //variantIndexLookup : null, 
-    
+    //variantIndexLookup : null,
+
     initialize : function(id, module, options) {
-    
+
         otp.widgets.Widget.prototype.initialize.call(this, id, module, options);
-        
+
         this.module = module;
-        
+
         var this_ = this;
 
         var routeSelectDiv = $('<div class="otp-tripViewer-select notDraggable" />').appendTo(this.mainDiv);
@@ -46,7 +46,7 @@ otp.widgets.transit.RouteBasedWidget =
             this_.newRouteSelected();
         });
 
-        _.each(module.webapp.transitIndex.routes, function(route, key) {
+        _.each(module.webapp.indexApi.routes, function(route, key) {
             var optionHtml = '<option>';
             if(route.routeData.routeShortName) optionHtml += '('+route.routeData.routeShortName+') ';
             if(route.routeData.routeLongName) optionHtml += route.routeData.routeLongName;
@@ -65,9 +65,9 @@ otp.widgets.transit.RouteBasedWidget =
         .change(function() {
             this_.newVariantSelected();
         });
-        
+
     },
-    
+
     newRouteSelected : function() {
         this.agency_id = null;
         this.activeLeg = null;
@@ -75,60 +75,60 @@ otp.widgets.transit.RouteBasedWidget =
         this.agency_id = route.routeData.id.agencyId + "_" + route.routeData.id.id;
         this.variantSelect.empty();
         this.clear() //stopList.empty();
-        this.checkAndLoadVariants();     
+        this.checkAndLoadVariants();
     },
-    
+
     newVariantSelected : function() {
         this.activeLeg = null;
         var variantName = this.variantSelect.val();
         //console.log("new variant selected: "+variantName);
         this.clear() //stopList.empty();
-        this.setActiveVariant(this.module.webapp.transitIndex.routes[this.agency_id].variants[variantName]);
+        this.setActiveVariant(this.module.webapp.indexApi.routes[this.agency_id].variants[variantName]);
     },
-    
+
     update : function(leg) {
         //this.clearTimes();
         this.activeLeg = leg;
         this.activeTime = leg.startTime;
         //this.times = times;
 
-        this.agency_id = leg.agencyId + "_" + leg.routeId;
-        
-        var tiRouteInfo = this.module.webapp.transitIndex.routes[this.agency_id];
+        this.agency_id = leg.agencyId + ":" + leg.routeId;
+
+        var tiRouteInfo = this.module.webapp.indexApi.routes[this.agency_id];
         $('#'+this.id+'-routeSelect option:eq('+tiRouteInfo.index+')').prop('selected', true);
-        
-        this.checkAndLoadVariants();      
+
+        this.checkAndLoadVariants();
     },
-        
+
     checkAndLoadVariants : function() {
-        var tiRouteInfo = this.module.webapp.transitIndex.routes[this.agency_id];
+        var tiRouteInfo = this.module.webapp.indexApi.routes[this.agency_id];
         if(tiRouteInfo.variants != null) {
             //console.log("variants exist");
             this.updateVariants();
         }
         else {
-            this.module.webapp.transitIndex.loadVariants(this.agency_id, this, this.updateVariants);
+            this.module.webapp.indexApi.loadVariants(this.agency_id, this, this.updateVariants);
         }
     },
 
     updateVariants : function() {
         var this_ = this;
-        var route = this.module.webapp.transitIndex.routes[this.agency_id];
+        var route = this.module.webapp.indexApi.routes[this.agency_id];
 
         if(!route.variants) {
-            console.log("ERROR: transitIndex.routes.["+this.agency_id+"].variants null in StopViewerWidget.updateVariants()");
+            console.log("ERROR: indexApi.routes.["+this.agency_id+"].variants null in StopViewerWidget.updateVariants()");
             return;
         }
-        
+
         if(this.activeLeg) {
-            this.module.webapp.transitIndex.readVariantForTrip(this.activeLeg.agencyId, this.activeLeg.tripId, this, this.setActiveVariant);
+            this.module.webapp.indexApi.readVariantForTrip(this.activeLeg.agencyId, this.activeLeg.tripId, this, this.setActiveVariant);
         }
 
         this.variantSelect.empty();
         _.each(route.variants, function(variant) {
             $('<option>'+variant.name+'</option>').appendTo(this_.variantSelect);
         });
-        
+
         if(!this.activeLeg) {
             this.newVariantSelected();
         }
@@ -136,19 +136,19 @@ otp.widgets.transit.RouteBasedWidget =
 
     setActiveVariant : function(variantData) {
         var this_ = this;
-        var route = this.module.webapp.transitIndex.routes[this.agency_id];
+        var route = this.module.webapp.indexApi.routes[this.agency_id];
         this.activeVariant = route.variants[variantData.name];
         $('#'+this.id+'-variantSelect option:eq('+(this.activeVariant.index)+')').prop('selected', true);
-        
+
         this.variantSelected(variantData);
     },
-    
+
     // functions to be implemented by subclasses:
-    
+
     clear : function() {
     },
-    
+
     variantSelected : function() {
     },
-        
+
 });
--- src/client/js/otp/widgets/transit/StopFinderWidget.js
+++ src/client/js/otp/widgets/transit/StopFinderWidget.js
@@ -2,30 +2,30 @@
    modify it under the terms of the GNU Lesser General Public License
    as published by the Free Software Foundation, either version 3 of
    the License, or (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 otp.namespace("otp.widgets.transit");
 
-otp.widgets.transit.StopFinderWidget = 
+otp.widgets.transit.StopFinderWidget =
     otp.Class(otp.widgets.Widget, {
 
     module : null,
 
     agency_id : null,
-    
+
     timeIndex : null,
-    
-        
+
+
     initialize : function(id, module, stopViewer) {
-    
+
         otp.widgets.Widget.prototype.initialize.call(this, id, module, {
             //TRANSLATORS: Widget title
             title : _tr('Stop Finder'),
@@ -35,10 +35,10 @@ otp.widgets.transit.StopFinderWidget =
             openInitially : false,
             persistOnClose : true,
         });
-        
+
         this.module = module;
         this.stopViewer = stopViewer;
-        
+
         var this_ = this;
 
         this.activeTime = moment();
@@ -53,12 +53,12 @@ otp.widgets.transit.StopFinderWidget =
             //TRANSLATORS: Search for Stops by ID/by Name
             search: _tr('Search')
         }
-          
+
         ich['otp-stopFinder'](translated_template).appendTo(this.mainDiv);
 
         this.agencySelect = this.mainDiv.find('.otp-stopFinder-agencySelect');
-        this.module.webapp.transitIndex.loadAgencies(this, function() {
-            for(var agencyId in this.module.webapp.transitIndex.agencies) {
+        this.module.webapp.indexApi.loadAgencies(this, function() {
+            for(var agencyId in this.module.webapp.indexApi.agencies) {
                 $("<option />").html(agencyId).appendTo(this_.agencySelect);
             }
         });
@@ -84,7 +84,7 @@ otp.widgets.transit.StopFinderWidget =
         });
 
         this.center();
-    },    
+    },
 
     updateStops : function(stops) {
         this.stopList.empty();
