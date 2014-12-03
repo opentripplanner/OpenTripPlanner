eb03543e75305ac1c25f88475e02ff40d3c3682b
Andrew Byrd <andrew@fastmail.net>
2014-12-03 19:17:42 +0100

    four transfer maximum. compare weights rather than times (single criterion).

--- src/main/java/org/opentripplanner/routing/impl/LongDistancePathService.java
+++ src/main/java/org/opentripplanner/routing/impl/LongDistancePathService.java
@@ -111,7 +111,7 @@ public class LongDistancePathService implements PathService {
          * search times on the LongDistancePathService, so we set it to the maximum we ever expect
          * to see. Because people may use either the traditional path services or the 
          * LongDistancePathService, we do not change the global default but override it here. */
-        options.setMaxTransfers(10);
+        options.setMaxTransfers(4);
 
         /* In long distance mode, maxWalk has a different meaning. It's the radius around the origin or destination
          * within which you can walk on the streets. If no value is provided, max walk defaults to the largest
