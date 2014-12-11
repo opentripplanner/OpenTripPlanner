4b0ff2c6e92119ce861a7b6ff662245ce18000ff
Laurent Grégoire <laurent.gregoire@gmail.com>
2014-12-11 10:01:37 +0100

    Move comment from interface to implementation.

--- src/main/java/org/opentripplanner/routing/impl/GraphServiceImpl.java
+++ src/main/java/org/opentripplanner/routing/impl/GraphServiceImpl.java
@@ -35,8 +35,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The primary implementation of the GraphService interface. It can handle multiple graphs, each
- * with its own routerId.
+ * A GraphService maps RouterIds to Graphs.
+ * 
+ * This graph service allows us to decouple the deserialization, loading, and management of the
+ * underlying graph objects from the classes that need access to the objects. This indirection
+ * allows us to provide multiple graphs distringuished by routerIds or to dynamically swap in new
+ * graphs if underlying data changes.
  * 
  * Delegate the graph creation/loading details to the GraphSource implementations.
  * 
@@ -86,9 +90,7 @@ public class GraphServiceImpl implements GraphService {
         }
     }
 
-    /**
-     * @param defaultRouterId
-     */
+    /** @param defaultRouterId The ID of the default router to return when no one is specified */
     @Override
     public void setDefaultRouterId(String defaultRouterId) {
         this.defaultRouterId = defaultRouterId;
@@ -106,11 +108,13 @@ public class GraphServiceImpl implements GraphService {
         cleanupWebapp();
     }
 
+    /** @return the current default graph object */
     @Override
     public Graph getGraph() throws GraphNotFoundException {
         return getGraph(null);
     }
 
+    /** @return the graph object for the given router ID */
     @Override
     public Graph getGraph(String routerId) throws GraphNotFoundException {
         if (routerId == null || routerId.isEmpty() || routerId.equalsIgnoreCase("default")) {
@@ -135,6 +139,14 @@ public class GraphServiceImpl implements GraphService {
         return graph;
     }
 
+    /**
+     * Reload all registered graphs from wherever they came from.
+     * 
+     * @param preEvict When true, release the existing graph (if any) before loading. This will
+     *        halve the amount of memory needed for the operation, but routing will be unavailable
+     *        for that graph during the load process
+     * @return whether the operation completed successfully
+     */
     @Override
     public boolean reloadGraphs(boolean preEvict) {
         boolean allSucceeded = true;
@@ -152,11 +164,22 @@ public class GraphServiceImpl implements GraphService {
         return allSucceeded;
     }
 
+    /** @return a collection of all valid router IDs for this server */
     @Override
     public Collection<String> getRouterIds() {
         return new ArrayList<String>(graphSources.keySet());
     }
 
+    /**
+     * Blocking method to associate the specified router ID with the corresponding graph file on
+     * disk, load that serialized graph, and enable its use in routing. The relationship between
+     * router IDs and paths in the filesystem is determined by the graphService implementation.
+     * 
+     * @param routerId
+     * @param preEvict
+     * 
+     * @return whether the operation completed successfully
+     */
     @Override
     public boolean registerGraph(String routerId, GraphSource graphSource) {
         LOG.info("Registering new graph '{}'", routerId);
@@ -181,6 +204,12 @@ public class GraphServiceImpl implements GraphService {
         }
     }
 
+    /**
+     * Dissociate a router ID from the corresponding graph object, and disable that router ID for
+     * use in routing.
+     * 
+     * @return whether a graph was associated with this router ID and was evicted.
+     */
     @Override
     public boolean evictGraph(String routerId) {
         LOG.info("Evicting graph '{}'", routerId);
@@ -196,6 +225,12 @@ public class GraphServiceImpl implements GraphService {
         }
     }
 
+    /**
+     * Dissocate all graphs from their router IDs and release references to the graphs to allow
+     * garbage collection. Routing will not be possible until new graphs are registered.
+     * 
+     * This is equivalent to calling evictGraph on every registered router ID.
+     */
     @Override
     public int evictAll() {
         LOG.info("Evincting all graphs.");
@@ -211,6 +246,10 @@ public class GraphServiceImpl implements GraphService {
         }
     }
 
+    /**
+     * @return The default GraphSource factory. Needed in case someone want to register or save a new
+     *         router with a router ID only (namely, via the web-service API).
+     */
     @Override
     public GraphSource.Factory getGraphSourceFactory() {
         return graphSourceFactory;
--- src/main/java/org/opentripplanner/routing/services/GraphService.java
+++ src/main/java/org/opentripplanner/routing/services/GraphService.java
@@ -18,69 +18,23 @@ import java.util.Collection;
 import org.opentripplanner.routing.error.GraphNotFoundException;
 import org.opentripplanner.routing.graph.Graph;
 
-/**
- * A GraphService maps RouterIds to Graphs.
- * 
- * The service interface allows us to decouple the deserialization, loading, and management of the
- * underlying graph objects from the classes that need access to the objects. This indirection
- * allows us to provide multiple graphs distringuished by routerIds or to dynamically swap in new
- * graphs if underlying data changes.
- */
 public interface GraphService {
 
-    /** @param defaultRouterId The ID of the default router to return when no one is specified */
     public void setDefaultRouterId(String defaultRouterId);
 
-    /** @return the current default graph object */
     public Graph getGraph() throws GraphNotFoundException;
 
-    /** @return the graph object for the given router ID */
     public Graph getGraph(String routerId) throws GraphNotFoundException;
 
-    /** @return a collection of all valid router IDs for this server */
     public Collection<String> getRouterIds();
 
-    /**
-     * Blocking method to associate the specified router ID with the corresponding graph file on
-     * disk, load that serialized graph, and enable its use in routing. The relationship between
-     * router IDs and paths in the filesystem is determined by the graphService implementation.
-     * 
-     * @param routerId
-     * @param preEvict
-     * 
-     * @return whether the operation completed successfully
-     */
     public boolean registerGraph(String routerId, GraphSource graphSource);
 
-    /**
-     * Reload all registered graphs from wherever they came from.
-     * 
-     * @param preEvict When true, release the existing graph (if any) before loading. This will
-     *        halve the amount of memory needed for the operation, but routing will be unavailable
-     *        for that graph during the load process
-     * @return whether the operation completed successfully
-     */
     public boolean reloadGraphs(boolean preEvict);
 
-    /**
-     * Dissociate a router ID from the corresponding graph object, and disable that router ID for
-     * use in routing.
-     * 
-     * @return whether a graph was associated with this router ID and was evicted.
-     */
     public boolean evictGraph(String routerId);
 
-    /**
-     * Dissocate all graphs from their router IDs and release references to the graphs to allow
-     * garbage collection. Routing will not be possible until new graphs are registered.
-     * 
-     * This is equivalent to calling evictGraph on every registered router ID.
-     */
     public int evictAll();
 
-    /**
-     * @return The default GraphSource factory. Needed in case someone want to register or save a new
-     *         router with a router ID only (namely, via the web-service API).
-     */
     public GraphSource.Factory getGraphSourceFactory();
 }
