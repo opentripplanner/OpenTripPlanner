e281f7c1a18f91c8de196d6584981e598490c741
Andrew Byrd <andrew@fastmail.net>
2015-01-07 15:01:35 +0100

    tabs to spaces (IntelliJ 14 decided to auto-detect that I wanted to use tabs)

--- src/main/java/org/opentripplanner/analyst/Histogram.java
+++ src/main/java/org/opentripplanner/analyst/Histogram.java
@@ -2,13 +2,9 @@ package org.opentripplanner.analyst;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.google.common.collect.Lists;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * A histogram summing/counting all time values into 1-minute bins (0-60 seconds = min 1, 61-120 = min 2, etc.)
@@ -18,61 +14,64 @@ public class Histogram implements Serializable {
     /**
      * The number of accessible features in each one-minute bin.
      * Index 0 is 0-1 minutes, index 50 is 50-51 minutes, etc.
-	 * The features are not weighted by their magnitudes, so values represent (for example) the number of places of
-	 * employment that can be reached rather than the total number of jobs in all those places of employment.
+     * The features are not weighted by their magnitudes, so values represent (for example) the number of places of
+     * employment that can be reached rather than the total number of jobs in all those places of employment.
      */
     public final int[] counts;
 
     /**
      * The weighted sum of all accessible features in each one-minute bin.
-	 * Index 0 is 0-1 minutes, index 50 is 50-51 minutes, etc.
-	 * Features are weighted by their magnitudes, so values represent (for example) the total number of jobs in
-	 * all accessible places of employment, rather than the number of places of employment.
+     * Index 0 is 0-1 minutes, index 50 is 50-51 minutes, etc.
+     * Features are weighted by their magnitudes, so values represent (for example) the total number of jobs in
+     * all accessible places of employment, rather than the number of places of employment.
      */
     public final int[] sums;
 
     /**
      * Represent the distribution of the given times using n+1 numbers.
-     * @param times the time at which each destination is reached. The array will be sorted in place.
+     *
+     * @param times   the time at which each destination is reached. The array will be sorted in place.
      * @param weights the weight or magnitude of each destination reached. parallel to times.
      */
-    public Histogram (int[] times, int[] weights, int nBins) {
+    public Histogram(int[] times, int[] weights, int nBins) {
 
-		counts = new int[nBins];
-		sums = new int[nBins];
+        counts = new int[nBins];
+        sums = new int[nBins];
 
-    	for(int i = 0; i < times.length; i++) {
+        for (int i = 0; i < times.length; i++) {
 
-    		if(times[i] < 0 || times[i] == Integer.MAX_VALUE) {
-				continue;
-			}
+            if (times[i] < 0 || times[i] == Integer.MAX_VALUE) {
+                continue;
+            }
 
-    		int minuteBin = (int) Math.floor(times[i] / 60.0);
-			if (minuteBin >= nBins) {
-				continue;
-			}
+            int minuteBin = (int) Math.floor(times[i] / 60.0);
+            if (minuteBin >= nBins) {
+                continue;
+            }
 
-    		counts[minuteBin] += 1;
-    		sums[minuteBin] += weights[i];
+            counts[minuteBin] += 1;
+            sums[minuteBin] += weights[i];
 
-    	}
+        }
 
     }
 
     public void writeJson(JsonGenerator jgen) throws JsonGenerationException, IOException {
 
-    	jgen.writeArrayFieldStart("sums"); {
-    		for(int sum : sums) {
-    			jgen.writeNumber(sum);
-    		}
-    	}
-    	jgen.writeEndArray();
+        jgen.writeArrayFieldStart("sums");
+        {
+            for (int sum : sums) {
+                jgen.writeNumber(sum);
+            }
+        }
+        jgen.writeEndArray();
 
-    	jgen.writeArrayFieldStart("counts"); {
-    		for(int count : counts) {
-    			jgen.writeNumber(count);
-    		}
-    	}
-    	jgen.writeEndArray();
+        jgen.writeArrayFieldStart("counts");
+        {
+            for (int count : counts) {
+                jgen.writeNumber(count);
+            }
+        }
+        jgen.writeEndArray();
     }
 }
