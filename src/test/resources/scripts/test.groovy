c95ee84f36354a4778c935128a64f809a521d412
Laurent Grégoire <laurent.gregoire@gmail.com>
2015-01-09 17:56:20 +0100

    Update demo scripts.

--- src/test/resources/scripts/test.groovy
+++ src/test/resources/scripts/test.groovy
@@ -3,30 +3,36 @@ def router = otp.getRouter()
 
 // Create a default request for a given time
 def req = otp.createRequest()
-req.setDateTime(2013, 6, 15, 10, 00, 00)
-req.setMaxTimeSec(3600)
+req.setDateTime(2015, 1, 9, 10, 00, 00)
+req.setMaxTimeSec(1800)
 
 // Create a regular and rectangular n x n grid of points
-def grid = otp.createSyntheticRasterPopulation(47.04, 47.07, -0.90, -0.85, 10, 10)
-// Load a CSV population file
-def lycees = otp.loadCSVPopulation("lycees.csv")
+def grid = otp.createGridPopulation(43.201, 43.359, 5.284, 5.483, 20, 20)
+
+// Load population files, CSV or GeoTIFF
+def colleges = otp.loadCSVPopulation('colleges.csv', 'Y', 'X')
+def population = otp.loadCSVPopulation('insee.csv', 'y', 'x')
+
+// Create a CSV output
+grid30Csv = otp.createCSVOutput()
+grid30Csv.setHeader([ 'lat', 'lon', 'min_college_time', 'n_colleges_30', 'n_pop_30' ])
 
 // For each point of the synthetic grid
 for (origin in grid) {
 
+	printf("Processing: %s\n", origin)
 	// Set the origin of the request to this point and run a search
 	req.setOrigin(origin)
 	def spt = router.plan(req)
+	if (spt == null) continue
 
-	// Evaluate times for all lycees population
-	def times = spt.evalTime(lycees)
-	// Find the time to nearest lycee
-	def minTime = times.min()
+	// Evaluate times for all colleges
+	def res = spt.evalTime(colleges)
+	// Find the time to nearest college
+	// TODO def minTime = res.min()
 
-	// Find the number of lycee nearer than 30mn
-	def n = times.findAll { it < 1800 }.size()
+	// Find the number of colleges nearer than 30mn
+	def n = res.findAll { it.getTime() < 1800 }.size()
 
 	// TODO: Save the results in an extended result set
-	printf("%s: %d %d\n", origin, minTime, n)
-
 }
--- src/test/resources/scripts/test.py
+++ src/test/resources/scripts/test.py
@@ -5,28 +5,44 @@ router = otp.getRouter()
 
 # Create a default request for a given time
 req = otp.createRequest()
-req.setDateTime(2013, 6, 15, 10, 00, 00)
-req.setMaxTimeSec(3600)
+req.setDateTime(2015, 1, 9, 10, 00, 00)
+req.setMaxTimeSec(1800)
 
 # Create a regular and rectangular n x n grid of points
-grid = otp.createSyntheticRasterPopulation(47.04, 47.07, -0.90, -0.85, 10, 10)
-# Load a CSV population file
-lycees = otp.loadCSVPopulation('lycees.csv')
+grid = otp.createGridPopulation(43.201, 43.359, 5.284, 5.483, 20, 20)
+
+# Load population files, CSV or GeoTIFF
+colleges = otp.loadCSVPopulation('colleges.csv', 'Y', 'X')
+population = otp.loadCSVPopulation('insee.csv', 'y', 'x')
+
+# Create a CSV output
+grid30Csv = otp.createCSVOutput()
+grid30Csv.setHeader([ 'lat', 'lon', 'min_college_time', 'n_colleges_30', 'n_pop_30' ])
 
 # For each point of the synthetic grid
 for origin in grid:
 
+	print "Processing: ", origin.getLatitude(), origin.getLongitude()
 	# Set the origin of the request to this point and run a search
 	req.setOrigin(origin)
 	spt = router.plan(req)
+	if spt is None: continue
 
-	# Evaluate times for all lycees population
-	times = spt.evalTime(lycees)
-	# Find the time to nearest lycee
-	minTime = min(times)
+	# Evaluate the SPT for all colleges
+	res = spt.eval(colleges)
+	# Find the time to nearest college
+	if len(res) == 0:	minTime = -1
+	else:			minTime = min([ r.getTime() for r in res ])
+	# Find the number of colleges < 30mn
+	nCollege30 = sum([ 1 for r in res if r.getTime() < 1800 ])
 
-	# Find the number of lycee nearer than 30mn
-	n = len([1 for time in times if time < 1800])
+	# Evaluate the SPT for the population
+	res = spt.eval(population)
+	# Sum population < 30mn
+	nPop30 = sum([ r.getIndividual().getFloatData('ind') for r in res if r.getTime() < 1800 ])
 
-	# TODO: Save the results in an extended result set
-	print origin, minTime, n
+	# Add a new row of result in the CSV output
+	grid30Csv.addRow([ origin.getLatitude(), origin.getLongitude(), minTime, nCollege30, nPop30 ])
+
+# Save the result
+grid30Csv.save('grid30.csv')
