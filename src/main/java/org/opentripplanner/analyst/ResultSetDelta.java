de9bda14cb79e759a6a0ac25d1330ff2152539b4
Matthew Wigginton Conway <matt@indicatrix.org>
2015-01-09 16:44:24 -0500

    remove all references to maps from resultsetdelta.

--- src/main/java/org/opentripplanner/analyst/ResultSetDelta.java
+++ src/main/java/org/opentripplanner/analyst/ResultSetDelta.java
@@ -1,8 +1,6 @@
 package org.opentripplanner.analyst;
 
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ResultSetDelta extends ResultSetWithTimes {
 
@@ -10,9 +8,6 @@ public class ResultSetDelta extends ResultSetWithTimes {
    
 	public int[] times2;
 	public int[] delta;
-
-	public Map<String,Integer> times2IdMap = new ConcurrentHashMap<String,Integer>();
-	public Map<String,Integer> deltaIdMap = new ConcurrentHashMap<String,Integer>();
 	
 	public ResultSetDelta(SampleSet samples1, SampleSet samples2, TimeSurface surface1, TimeSurface surface2) {
 		id = samples1.pset.id + "_" + surface1.id + "_" + surface2.id + "_delta";
@@ -29,18 +24,6 @@ public class ResultSetDelta extends ResultSetWithTimes {
 		}
         
         buildDeltaHistograms(samples1.pset);
-        
-		int i = 0;
-		for(String id : samples1.pset.ids) {
-			timeIdMap.put(id, times[i]);
-			times2IdMap.put(id, times2[i]);
-			deltaIdMap.put(id, delta[i]);
-			i++;
-		}
-	}
-	
-	public Integer getTime(String featureId) {
-		return timeIdMap.get(featureId);
 	}
 	
 	protected void buildDeltaHistograms(PointSet targets) {
