4b7079b967bb4d080256b3eddfe5aea9a13d0b5f
Andrew Byrd <andrew@fastmail.net>
2015-03-13 18:11:44 +0100

    remove deep copy method on ProfileRequest and make it cloneable
    matt conway confirmed that all reference fields are treated as
    immutable in analyst cluster, stock object.clone() is fine here.

--- src/main/java/org/opentripplanner/profile/ProfileRequest.java
+++ src/main/java/org/opentripplanner/profile/ProfileRequest.java
@@ -1,18 +1,14 @@
 package org.opentripplanner.profile;
 
-import java.io.Serializable;
-
 import org.joda.time.LocalDate;
-import org.opentripplanner.api.param.LatLon;
-import org.opentripplanner.routing.core.TraverseModeSet;
+import org.opentripplanner.api.parameter.QualifiedModeSetSequence;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.QueryParam;
+import java.io.Serializable;
 
 /**
  * All the modifiable parameters for profile routing.
  */
-public class ProfileRequest implements Serializable {
+public class ProfileRequest implements Serializable, Cloneable {
 
     public double fromLat;
     public double fromLon;
@@ -38,7 +34,7 @@ public class ProfileRequest implements Serializable {
     public LocalDate date;
     public Option.SortOrder orderBy;
     public int limit; // the maximum number of options presented PER ACCESS MODE
-    public TraverseModeSet accessModes, egressModes, directModes, transitModes;
+    public QualifiedModeSetSequence accessModes, egressModes, directModes, transitModes;
     public boolean analyst = false; // if true, propagate travel times out to street network
 
     /*
@@ -49,40 +45,5 @@ public class ProfileRequest implements Serializable {
       alternatives out a bit to account for the fact that they don't always run on schedule.
     */
     public int suboptimalMinutes;
-    
-    public ProfileRequest clone () {
-        ProfileRequest ret = new ProfileRequest();
-        ret.fromLat = fromLat;
-        ret.fromLon = fromLon;
-        ret.toLat = toLat;
-        ret.toLon = toLon;
-        ret.fromTime = fromTime;
-        ret.toTime = toTime;
-        
-        ret.walkSpeed = walkSpeed;
-        ret.bikeSpeed = bikeSpeed;
-        ret.carSpeed = carSpeed;
-        
-        ret.streetTime = streetTime;
-        ret.maxWalkTime = maxWalkTime;
-        ret.maxBikeTime = maxBikeTime;
-        ret.maxCarTime = maxCarTime;
-        ret.minBikeTime = minBikeTime;
-        ret.minCarTime = minCarTime;
-        
-        // LocalDate is immutable, no need to copy
-        ret.date = date;
-        // TODO: deep clone needed? mutable?
-        ret.orderBy = orderBy;
-        ret.limit = limit;
-        ret.accessModes = accessModes != null ? accessModes.clone() : null;
-        ret.egressModes = egressModes != null ? egressModes.clone() : null;
-        ret.directModes = directModes != null ? directModes.clone() : null;
-        ret.transitModes = transitModes != null ? transitModes.clone() : null;
-        
-        ret.analyst = analyst;
-        ret.suboptimalMinutes = suboptimalMinutes;
-        
-        return ret;
-    }
+
 }
