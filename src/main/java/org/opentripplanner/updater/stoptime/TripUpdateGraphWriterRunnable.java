38bf22a53dc74fc07d67384167401d3755ff8cbd
jkoelewijn <git@jkoelewijn.nl>
2015-03-23 17:32:32 +0100

    Fix another import

--- src/main/java/org/opentripplanner/updater/stoptime/TripUpdateGraphWriterRunnable.java
+++ src/main/java/org/opentripplanner/updater/stoptime/TripUpdateGraphWriterRunnable.java
@@ -15,14 +15,13 @@ package org.opentripplanner.updater.stoptime;
 
 import java.util.List;
 
-import jersey.repackaged.com.google.common.base.Preconditions;
-
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.updater.GraphWriterRunnable;
 import org.opentripplanner.updater.stoptime.TimetableSnapshotSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
 import com.google.transit.realtime.GtfsRealtime.TripUpdate;
 
 public class TripUpdateGraphWriterRunnable implements GraphWriterRunnable {
