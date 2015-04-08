48711be872d01d2ecfc9b030d38f898b3012870a
Matthew Wigginton Conway <matt@indicatrix.org>
2015-04-08 14:32:07 -0400

    add comments to profilerequest.

--- src/main/java/org/opentripplanner/profile/ProfileRequest.java
+++ src/main/java/org/opentripplanner/profile/ProfileRequest.java
@@ -11,41 +11,87 @@ import java.io.Serializable;
  */
 public class ProfileRequest implements Serializable, Cloneable {
 
+    /** The latitude of the origin. */
     public double fromLat;
+    
+    /** The longitude of the origin. */
     public double fromLon;
+    
+    /** The latitude of the destination. Must be set even in Analyst mode. */
     public double toLat;
+    
+    /** The longitude of the destination. Must be set even in Analyst mode. */
     public double toLon;
     
+    /** The beginning of the departure window, in seconds since midnight. */
     public int    fromTime;
+    
+    /** The end of the departure window, in seconds since midnight. */
     public int    toTime;
 
-    /* Speeds are in meters per second. */
+    /** The speed of walking, in meters per second */
     public float  walkSpeed;
+    
+    /** The speed of cycling, in meters per second */
     public float  bikeSpeed;
+    
+    /** The speed of driving, in meters per second */
     public float  carSpeed;
 
-    /* The following times are all in minutes. */
+    /** Maximum time to reach the destination without using transit */
     public int    streetTime;
+    
+    /** Maximum walk time before and after using transit */
     public int    maxWalkTime;
+    
+    /** Maximum bike time when using transit */
     public int    maxBikeTime;
+    
+    /** Maximum car time before when using transit */ 
     public int    maxCarTime;
+    
+    /** Minimum time to ride a bike (to prevent extremely short bike legs) */
     public int    minBikeTime;
+    
+    /** Minimum time to drive (to prevent extremely short driving legs) */
     public int    minCarTime;
 
+    /** The date of the search */
     public LocalDate date;
+    
+    /** The order in which to return multiple options */
     public Option.SortOrder orderBy;
-    public int limit; // the maximum number of options presented PER ACCESS MODE
-    public QualifiedModeSet accessModes, egressModes, directModes;
+    
+    /** the maximum number of options presented PER ACCESS MODE */
+    public int limit;
+    
+    /** The modes used to access transit */
+    public QualifiedModeSet accessModes;
+    
+    /** The modes used to reach the destination after leaving transit */
+    public QualifiedModeSet egressModes;
+    
+    /** The modes used to reach the destination without transit */
+    public QualifiedModeSet directModes;
+    
+    /** The transit modes used */
     public TraverseModeSet transitModes;
-    public boolean analyst = false; // if true, propagate travel times out to street network
+    
+    /** If true, disable all goal direction and propagate results to the street network */
+    public boolean analyst = false;
 
     /* The relative importance of different factors when biking */
+    /** The relative importance of maximizing safety when cycling */
     public int bikeSafe;
+    
+    /** The relative importance of minimizing hills when cycling */
     public int bikeSlope;
+    
+    /** The relative importance of minimizing time when cycling */
     public int bikeTime;
     // FIXME change "safe" to "danger" to consistently refer to the things being minimized
 
-    /*
+    /**
       This parameter compensates for the fact that GTFS does not contain information about schedule deviation (lateness).
       The min-max travel time range for some trains is zero, since the trips are reported to always have the same
       timings in the schedule. Such an option does not overlap (temporally) its alternatives, and is too easily
