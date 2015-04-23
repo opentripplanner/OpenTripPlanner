6855997225fb920db1a4025ae83450941676929b
Andrew Byrd <andrew@fastmail.net>
2015-04-23 16:16:22 +0200

    update repeated raptor conherency tester

--- src/main/java/org/opentripplanner/api/resource/RepeatedRaptorTestResource.java
+++ src/main/java/org/opentripplanner/api/resource/RepeatedRaptorTestResource.java
@@ -4,6 +4,7 @@ import org.joda.time.LocalDate;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.gtfs.model.Stop;
 import org.opentripplanner.analyst.SurfaceCache;
+import org.opentripplanner.analyst.TimeSurface;
 import org.opentripplanner.api.param.LatLon;
 import org.opentripplanner.api.parameter.QualifiedModeSet;
 import org.opentripplanner.profile.ProfileRequest;
@@ -11,13 +12,12 @@ import org.opentripplanner.profile.RepeatedRaptorProfileRouter;
 import org.opentripplanner.routing.core.TraverseModeSet;
 import org.opentripplanner.routing.error.VertexNotFoundException;
 import org.opentripplanner.routing.graph.Graph;
-import org.opentripplanner.routing.vertextype.TransitStop;
+import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.standalone.OTPServer;
 import org.opentripplanner.standalone.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -41,8 +41,8 @@ public class RepeatedRaptorTestResource {
 
     private int n_increase = 0;
     private int n_decrease= 0;
-    private int n_same= 0;
     private int n_total = 0;
+    private long sum_decrease = 0;
 
     public RepeatedRaptorTestResource (@Context OTPServer otpServer, @PathParam("routerId") String routerId) {
         Router router = otpServer.getRouter(routerId);
@@ -55,12 +55,11 @@ public class RepeatedRaptorTestResource {
     public Response profileRoute (
             @QueryParam("from") LatLon from,
             @QueryParam("fromStop") String fromStopString,
-            @QueryParam("banRoute") @DefaultValue("TriMet:100") String banRouteString) {
+            @QueryParam("banAgency") String banAgency,
+            @QueryParam("banRoute") String banRouteString) {
 
-        String[] banFields = banRouteString.split(":");
-        AgencyAndId banRoute = new AgencyAndId(banFields[0], banFields[1]);
         if (from != null) {
-            oneOrigin(from.lat, from.lon, banRoute);
+            oneOrigin(from.lat, from.lon, banAgency);
         } else {
             Collection<Stop> originStops = new ArrayList<>();
             if (fromStopString != null) {
@@ -72,19 +71,20 @@ public class RepeatedRaptorTestResource {
             LOG.info("from stops {}", originStops);
             for (Stop stop : originStops) {
                 LOG.info("{}/{}: {}", 0, originStops.size(), stop);
-                oneOrigin(stop.getLat(), stop.getLon(), banRoute);
+                // Shift slightly so we don't always search right on top of a transit stop.
+                oneOrigin(stop.getLat() + 0.001, stop.getLon() + 0.001, banAgency);
             }
         }
 
         return Response.ok().entity("OK\n").build();
     }
 
-    private void oneOrigin (double lat, double lon, AgencyAndId banRoute) {
+    private void oneOrigin (double lat, double lon, String banAgency) {
 
         ProfileRequest req = new ProfileRequest();
         req.fromLat      = lat;
         req.fromLon      = lon;
-        req.fromTime     = 60 * 60 * 7;
+        req.fromTime     = 60 * 60 * 8;
         req.toTime       = 60 * 60 * 9;
         req.walkSpeed    = 2;
         req.bikeSpeed    = 4;
@@ -102,7 +102,7 @@ public class RepeatedRaptorTestResource {
 
         final RepeatedRaptorProfileRouter router_a = new RepeatedRaptorProfileRouter(graph, req);
         final RepeatedRaptorProfileRouter router_b = new RepeatedRaptorProfileRouter(graph, req);
-        router_b.banRoute = banRoute;
+        router_b.banAgency = banAgency;
 
         try {
             router_a.route();
@@ -115,74 +115,41 @@ public class RepeatedRaptorTestResource {
         System.out.printf("stop, min_a, min_b, min_diff, max_a, max_b, max_diff\n");
         boolean decreased = false;
 
-        for (TransitStop destVertex : graph.index.stopVertexForStop.values()) {
-            int min_a = router_a.mins.get(destVertex);
-            int max_a = router_a.maxs.get(destVertex);
-            int min_b = router_b.mins.get(destVertex);
-            int max_b = router_b.maxs.get(destVertex);
-            long min_diff = (long) min_b - min_a;
-            long max_diff = (long) max_b - max_a;
-            if (min_b == Integer.MAX_VALUE) {
-                min_diff = Integer.MAX_VALUE;
-            }
-            if (max_b == Integer.MIN_VALUE) {
-                max_diff = Integer.MAX_VALUE;
-            }
-            n_total += 1;
-            if (min_diff < 0 || max_diff < 0) {
-                n_decrease += 1;
-                // Time decreased due to banning a route. This is bad, print it out.
-                System.out.printf("\"%s\",%d,%d,%d,%d,%d,%d\n",
-                        destVertex.getStop().getId(), min_a, min_b, min_diff, max_a, max_b, max_diff);
-                decreased = true;
-            } else if (min_diff > 0 || max_diff > 0) {
-                n_increase += 1;
-            } else {
-                n_same += 1;
-            }
-
-        }
-        if (decreased) {
-            LOG.error("Decreases happened at this origin!");
-        }
-        LOG.info("Stop Vertices: {} increased, {} decreased out of {} destinations total", n_increase, n_decrease, n_total);
-
-    /*
-        // Now compare the propagated results
+        // Compare the propagated results
         decreased = false;
         TimeSurface.RangeSet timeSurfaces_a = router_a.timeSurfaceRangeSet;
         TimeSurface.RangeSet timeSurfaces_b = router_b.timeSurfaceRangeSet;
         for (Vertex destVertex : timeSurfaces_a.min.times.keySet()) {
             int min_a = timeSurfaces_a.min.getTime(destVertex);
             int max_a = timeSurfaces_a.max.getTime(destVertex);
+            int avg_a = timeSurfaces_a.avg.getTime(destVertex);
             int min_b = timeSurfaces_b.min.getTime(destVertex);
             int max_b = timeSurfaces_b.max.getTime(destVertex);
+            int avg_b = timeSurfaces_b.avg.getTime(destVertex);
             long min_diff = (long) min_b - min_a;
             long max_diff = (long) max_b - max_a;
+            long avg_diff = (long) avg_b - avg_a;
             if (min_b == TimeSurface.UNREACHABLE) {
                 min_diff = Integer.MAX_VALUE;
-            }
-            if (max_b == TimeSurface.UNREACHABLE) {
                 max_diff = Integer.MAX_VALUE;
+                avg_diff = Integer.MAX_VALUE;
             }
             n_total += 1;
-            if (min_diff < 0 || max_diff < 0) {
+            if (min_diff < 0 || max_diff < 0 || avg_diff < 0) {
                 n_decrease += 1;
+                sum_decrease += max_diff;
                 // Time decreased due to banning a route. This is bad, print it out.
                 System.out.printf("\"%s\",%d,%d,%d,%d,%d,%d\n",
                         destVertex.getName(), min_a, min_b, min_diff, max_a, max_b, max_diff);
                 decreased = true;
-            } else if (min_diff > 0 || max_diff > 0) {
+            } else if (avg_diff > 0) {
                 n_increase += 1;
-            } else {
-                n_same += 1;
             }
 
         }
-        LOG.info("Street Vertices: {} increased, {} decreased out of {} destinations total", n_increase, n_decrease, n_total);
         if (decreased) {
-            LOG.error("Decreases happened at this origin!");
+            LOG.error("Decreases happened at propagated street vertices for this origin!");
         }
-    */
+        LOG.info("Street Vertices: {} increased, {} decreased out of {} destinations total", n_increase, n_decrease, n_total);
     }
 }
