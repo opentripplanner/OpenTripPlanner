221a6f063806b6d9a204825e919c646d605ee71c
Hannes Junnila <hannes.junnila@gmail.com>
2015-04-28 15:54:42 +0300

    Expose query parameters for the StopTime Index API

--- src/main/java/org/opentripplanner/routing/graph/GraphIndex.java
+++ src/main/java/org/opentripplanner/routing/graph/GraphIndex.java
@@ -306,7 +306,7 @@ public class GraphIndex {
      * @return
      */
     public Collection<StopTimesInPattern> stopTimesForStop(Stop stop) {
-        return getStopTimesForStop(stop, 24 * 60 * 60, 2);
+        return stopTimesForStop(stop, System.currentTimeMillis()/1000, 24 * 60 * 60, 2);
     }
 
     /**
@@ -318,14 +318,17 @@ public class GraphIndex {
      *
      * TODO: Add frequency based trips
      *
-     * @param stop
-     * @param timeRange
-     * @param numberOfDepartures
+     * @param stop Stop object to perform the search for
+     * @param startTime Start time for the search. Seconds from UNIX epoch
+     * @param timeRange Searches forward for timeRange seconds from startTime
+     * @param numberOfDepartures Number of departures to fetch per pattern
      * @return
      */
-    public List<StopTimesInPattern> getStopTimesForStop(Stop stop, int timeRange, int numberOfDepartures) {
+    public List<StopTimesInPattern> stopTimesForStop(Stop stop, long startTime, int timeRange, int numberOfDepartures) {
 
-        long now = System.currentTimeMillis()/1000;
+        if (startTime == 0) {
+            startTime = System.currentTimeMillis() / 1000;
+        }
         List<StopTimesInPattern> ret = new ArrayList<>();
         TimetableSnapshot snapshot = null;
         if (graph.timetableSnapshotSource != null) {
@@ -355,9 +358,9 @@ public class GraphIndex {
                     tt = pattern.scheduledTimetable;
                 }
 
-                if (!tt.temporallyViable(sd, now, timeRange, true)) continue;
+                if (!tt.temporallyViable(sd, startTime, timeRange, true)) continue;
 
-                int secondsSinceMidnight = sd.secondsSinceMidnight(now);
+                int secondsSinceMidnight = sd.secondsSinceMidnight(startTime);
                 int sidx = 0;
                 for (Stop currStop : pattern.stopPattern.stops) {
                     if (currStop == stop) {
@@ -403,8 +406,8 @@ public class GraphIndex {
      * Get a list of all trips that pass through a stop during a single ServiceDate. Useful when creating complete stop
      * timetables for a single day.
      *
-     * @param stop
-     * @param serviceDate
+     * @param stop Stop object to perform the search for
+     * @param serviceDate Return all departures for the specified date
      * @return
      */
     public List<StopTimesInPattern> getStopTimesForStop(Stop stop, ServiceDate serviceDate) {
