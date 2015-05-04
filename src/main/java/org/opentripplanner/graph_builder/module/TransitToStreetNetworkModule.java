ab0652a6b82a41542b849d2145556e772ebbfe04
Matthew Wigginton Conway <matt@indicatrix.org>
2015-05-04 09:47:10 -0400

    link transit stops directly to osm nodes.

--- src/main/java/org/opentripplanner/graph_builder/module/TransitToStreetNetworkModule.java
+++ src/main/java/org/opentripplanner/graph_builder/module/TransitToStreetNetworkModule.java
@@ -14,14 +14,23 @@
 package org.opentripplanner.graph_builder.module;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
 import org.opentripplanner.graph_builder.services.GraphBuilderModule;
+import org.opentripplanner.routing.edgetype.FreeEdge;
 import org.opentripplanner.routing.edgetype.loader.NetworkLinker;
 import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.routing.graph.Vertex;
+import org.opentripplanner.routing.impl.DefaultStreetVertexIndexFactory;
+import org.opentripplanner.routing.vertextype.OsmVertex;
+import org.opentripplanner.routing.vertextype.TransitStop;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Iterables;
+import com.vividsolutions.jts.geom.Envelope;
 
 /**
  * {@link org.opentripplanner.graph_builder.services.GraphBuilderModule} plugin that links up the stops of a transit network to a street network.
@@ -42,8 +51,42 @@ public class TransitToStreetNetworkModule implements GraphBuilderModule {
     @Override
     public void buildGraph(Graph graph, HashMap<Class<?>, Object> extra) {
         LOG.info("Linking transit stops to streets...");
-        NetworkLinker linker = new NetworkLinker(graph, extra);
-        linker.createLinkage();
+        
+        double radius = 0.01;
+
+    	graph.index(new DefaultStreetVertexIndexFactory());
+        
+        for (TransitStop tstop : Iterables.filter(graph.getVertices(), TransitStop.class)) {
+        	Envelope envelope = new Envelope(tstop.getCoordinate());
+        	envelope.expandBy(radius);
+            Collection<Vertex> nearby = graph.streetIndex.getVerticesForEnvelope(envelope);
+            
+            double radiusErsatz = radius * radius;
+            
+            double bestErsatz = Double.POSITIVE_INFINITY;
+            Vertex best = null;
+            
+            for (OsmVertex v : Iterables.filter(nearby, OsmVertex.class)) {
+            	double dx = v.getLat() - tstop.getLat();
+            	double dy = v.getLon() - tstop.getLon();
+            	
+            	double ersatz = dx * dx + dy * dy;
+            	
+            	// make the index deterministic
+            	if (ersatz > radiusErsatz)
+            		continue;
+            	
+            	if (ersatz < bestErsatz) {
+            		best = v;
+            		bestErsatz = ersatz;
+            	}
+            }
+            
+            if (best != null) {
+            	new FreeEdge(best, tstop);
+            	new FreeEdge(tstop, best);
+            }
+        }
     }
 
     @Override
