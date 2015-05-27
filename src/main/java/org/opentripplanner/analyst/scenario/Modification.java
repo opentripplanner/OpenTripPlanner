5dd285697d15ed763b9d2bbc9f8be4d03a77503c
Matthew Wigginton Conway <matt@indicatrix.org>
2015-05-27 15:52:36 -0400

    make classes serializable.

--- src/main/java/org/opentripplanner/analyst/scenario/Modification.java
+++ src/main/java/org/opentripplanner/analyst/scenario/Modification.java
@@ -2,13 +2,14 @@ package org.opentripplanner.analyst.scenario;
 
 import org.opentripplanner.profile.RaptorWorkerData;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * A Modification is a single change that can be applied non-destructively to RaptorWorkerData.
  */
-public abstract class Modification {
+public abstract class Modification implements Serializable {
 
     /** Distinguish between modification types when a list of Modifications are serialized out as JSON. */
     public abstract String getType();
--- src/main/java/org/opentripplanner/analyst/scenario/Scenario.java
+++ src/main/java/org/opentripplanner/analyst/scenario/Scenario.java
@@ -2,18 +2,19 @@ package org.opentripplanner.analyst.scenario;
 
 import com.beust.jcommander.internal.Lists;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * A scenario is an ordered sequence of modifications that will be applied non-destructively on top of a baseline graph.
  */
-public class Scenario {
+public class Scenario implements Serializable {
 
     public final int id;
 
     public String description = "no description provided";
 
-    public final List<Modification> modifications = Lists.newArrayList();
+    public List<Modification> modifications = Lists.newArrayList();
 
     public Scenario (int id) {
         this.id = id;
