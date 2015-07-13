7e0c516dfe943a8f54d22e105ee4106b4eecac5d
Hannes Junnila <hannes.junnila@gmail.com>
2015-07-13 19:05:43 +0300

    Add stops and clusters

--- src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
+++ src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
@@ -3,24 +3,151 @@ package org.opentripplanner.index;
 import graphql.Scalars;
 import graphql.schema.*;
 import org.onebusaway.gtfs.model.Route;
+import org.onebusaway.gtfs.model.Stop;
 import org.opentripplanner.gtfs.GtfsLibrary;
+import org.opentripplanner.profile.StopCluster;
+import org.opentripplanner.routing.edgetype.SimpleTransfer;
 import org.opentripplanner.routing.edgetype.TripPattern;
 import org.opentripplanner.routing.graph.GraphIndex;
+import org.opentripplanner.routing.vertextype.TransitVertex;
 
 import java.util.stream.Collectors;
 
 public class IndexGraphQLSchema {
 
     public GraphQLObjectType agencyType;
+    public GraphQLObjectType clusterType;
     public GraphQLObjectType patternType;
     public GraphQLObjectType routeType;
+    public GraphQLObjectType stoptimeType;
     public GraphQLObjectType stopType;
     public GraphQLObjectType tripType;
-
     public GraphQLObjectType queryType;
     public GraphQLSchema indexSchema;
 
     public IndexGraphQLSchema(GraphIndex index) {
+
+        clusterType = GraphQLObjectType.newObject()
+            .name("Cluster")
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("id")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("name")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("lat")
+                .type(new GraphQLNonNull(Scalars.GraphQLFloat))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("lon")
+                .type(new GraphQLNonNull(Scalars.GraphQLFloat))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("stops")
+                .type(new GraphQLList(new GraphQLNonNull(stopType)))
+                .dataFetcher(environment -> ((StopCluster) environment.getSource()).children)
+                .build())
+            .build();
+
+
+        stopType = GraphQLObjectType.newObject()
+            .name("Stop")
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("id")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("name")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("lat")
+                .type(new GraphQLNonNull(Scalars.GraphQLFloat))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("lon")
+                .type(new GraphQLNonNull(Scalars.GraphQLFloat))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("code")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("desc")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("zoneId")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("url")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("locationType")
+                .type(Scalars.GraphQLInt) //TODO:enum
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("parentStation")
+                .type(Scalars.GraphQLString) //TODO: get actual station type
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("wheelchairBoarding") //TODO:enum
+                .type(Scalars.GraphQLInt)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("direction")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("timezone")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("vehicleType")
+                .type(Scalars.GraphQLInt) //TODO:enum
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("platformCode")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("cluster")
+                .type(clusterType)
+                .dataFetcher(environment ->
+                    index.stopClusterForStop.get((Stop) environment.getSource()))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("routes")
+                .type(new GraphQLList(new GraphQLNonNull(routeType)))
+                .dataFetcher(environment ->
+                    index.patternsForStop.get((Stop) environment.getSource())
+                        .stream()
+                        .map(pattern -> pattern.route)
+                        .distinct()
+                        .collect(Collectors.toList()))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("patterns")
+                .type(new GraphQLList(patternType))
+                .dataFetcher(environment ->
+                    index.patternsForStop.get((Stop) environment.getSource()))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("transfers")               //TODO: add max distance as parameter?
+                .type(new GraphQLList(stopType)) //TODO: also distance?
+                .dataFetcher(environment ->
+                    index.stopVertexForStop.get((Stop) environment.getSource()).getOutgoing()
+                        .stream()
+                        .filter(edge -> edge instanceof SimpleTransfer)
+                        .map(edge -> ((TransitVertex) edge.getToVertex()).getStop())
+                        .collect(Collectors.toList()))
+                .build())
+            .build();
 
         patternType = GraphQLObjectType.newObject()
             .name("Pattern")
@@ -48,6 +175,7 @@ public class IndexGraphQLSchema {
                 .name("stops")
                 .type(new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(stopType))))
                 .build())
+            // TODO: add stoptimes
             .field(GraphQLFieldDefinition.newFieldDefinition()
                 .name("semanticHash")
                 .type(Scalars.GraphQLString)
