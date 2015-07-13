1081312a0c947cb0998e9d5f1ef1d9551a7f488e
Hannes Junnila <hannes.junnila@gmail.com>
2015-07-13 17:41:09 +0300

    Add more types

--- src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
+++ src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
@@ -1,13 +1,10 @@
 package org.opentripplanner.index;
 
 import graphql.Scalars;
-import graphql.schema.GraphQLFieldArgument;
-import graphql.schema.GraphQLFieldDefinition;
-import graphql.schema.GraphQLList;
-import graphql.schema.GraphQLNonNull;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLSchema;
+import graphql.schema.*;
+import org.onebusaway.gtfs.model.Route;
 import org.opentripplanner.gtfs.GtfsLibrary;
+import org.opentripplanner.routing.edgetype.TripPattern;
 import org.opentripplanner.routing.graph.GraphIndex;
 
 import java.util.stream.Collectors;
@@ -15,97 +12,175 @@ import java.util.stream.Collectors;
 public class IndexGraphQLSchema {
 
     public GraphQLObjectType agencyType;
+    public GraphQLObjectType patternType;
     public GraphQLObjectType routeType;
+    public GraphQLObjectType stopType;
+    public GraphQLObjectType tripType;
+
     public GraphQLObjectType queryType;
     public GraphQLSchema indexSchema;
 
     public IndexGraphQLSchema(GraphIndex index) {
 
+        patternType = GraphQLObjectType.newObject()
+            .name("Pattern")
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("route")
+                .type(new GraphQLNonNull(routeType))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("directionId")
+                .type(Scalars.GraphQLInt)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("name")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("code")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("trips")
+                .type(new GraphQLList(new GraphQLNonNull(tripType)))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("stops")
+                .type(new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(stopType))))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("semanticHash")
+                .type(Scalars.GraphQLString)
+                .dataFetcher(environment ->
+                    ((TripPattern) environment.getSource()).semanticHashString(null))
+                .build())
+            .build();
+
+
         routeType = GraphQLObjectType.newObject()
-                .name("Route")
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("id")
-                        .type(new GraphQLNonNull(Scalars.GraphQLString))
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("shortName")
-                        .type(Scalars.GraphQLString)
-                        .build())
-                .build();
+            .name("Route")
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("id")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("agency")
+                .type(agencyType)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("shortName")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("longName")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("type")
+                .type(Scalars.GraphQLInt) // TODO:Replace with enum
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("desc")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("url")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("color")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("textColor")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("bikesAllowed")
+                .type(Scalars.GraphQLInt) // TODO:Replace with enum
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("patterns")
+                .type(new GraphQLList(patternType))
+                .dataFetcher(environment ->
+                    index.patternsForRoute.get((Route) environment.getSource()))
+                .build())
+            .build();
 
         agencyType = GraphQLObjectType.newObject()
-                .name("Agency")
-                .description("Agency in the graph")
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("id")
-                        .description("Agency id")
-                        .type(new GraphQLNonNull(Scalars.GraphQLString))
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("name")
-                        .type(new GraphQLNonNull(Scalars.GraphQLString))
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("url")
-                        .type(new GraphQLNonNull(Scalars.GraphQLString))
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("timezone")
-                        .type(new GraphQLNonNull(Scalars.GraphQLString))
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("lang")
-                        .type(Scalars.GraphQLString)
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("phone")
-                        .type(Scalars.GraphQLString)
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("fareUrl")
-                        .type(Scalars.GraphQLString)
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("routes")
-                        .type(new GraphQLList(routeType))
-                        .dataFetcher(dataFetchingEnvironment -> index.routeForId.values().stream()
-                                .filter(route -> route.getAgency() == dataFetchingEnvironment.getSource())
-                                .collect(Collectors.toList()))
-                        .build())
-                .build();
+            .name("Agency")
+            .description("Agency in the graph")
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("id")
+                .description("Agency id")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("name")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("url")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("timezone")
+                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("lang")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("phone")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("fareUrl")
+                .type(Scalars.GraphQLString)
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("routes")
+                .type(new GraphQLList(routeType))
+                .dataFetcher(environment -> index.routeForId.values().stream()
+                    .filter(route -> route.getAgency() == environment.getSource())
+                    .collect(Collectors.toList()))
+                .build())
+            .build();
 
         queryType = GraphQLObjectType.newObject()
-                .name("QueryType")
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("agency")
-                        .type(agencyType)
-                        .argument(GraphQLFieldArgument.newFieldArgument()
-                                .name("id")
-                                .type(new GraphQLNonNull(Scalars.GraphQLString))
-                                .build())
-                        .dataFetcher(dataFetchingEnvironment ->
-                                index.agencyForId.get((String) dataFetchingEnvironment.getArgument("id")))
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("agencies")
-                        .type(new GraphQLList(agencyType))
-                        .dataFetcher(dataFetchingEnvironment ->
-                                index.agencyForId.values())
-                        .build())
-                .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("route")
-                        .type(routeType)
-                        .argument(GraphQLFieldArgument.newFieldArgument()
-                                .name("id")
-                                .type(new GraphQLNonNull(Scalars.GraphQLString))
-                                .build())
-                        .dataFetcher(dataFetchingEnvironment ->
-                                index.routeForId.get(GtfsLibrary.convertIdFromString(dataFetchingEnvironment.getArgument("id"))))
-                        .build())
-                .build();
+            .name("QueryType")
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("agency")
+                .type(agencyType)
+                .argument(GraphQLFieldArgument.newFieldArgument()
+                    .name("id")
+                    .type(new GraphQLNonNull(Scalars.GraphQLString))
+                    .build())
+                .dataFetcher(environment ->
+                    index.agencyForId.get((String) environment.getArgument("id")))
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("agencies")
+                .type(new GraphQLList(agencyType))
+                .dataFetcher(environment ->
+                    index.agencyForId.values())
+                .build())
+            .field(GraphQLFieldDefinition.newFieldDefinition()
+                .name("route")
+                .type(routeType)
+                .argument(GraphQLFieldArgument.newFieldArgument()
+                    .name("id")
+                    .type(new GraphQLNonNull(Scalars.GraphQLString))
+                    .build())
+                .dataFetcher(environment ->
+                    index.routeForId.get(
+                        GtfsLibrary.convertIdFromString(environment.getArgument("id"))))
+                .build())
+            .build();
 
         indexSchema = GraphQLSchema.newSchema()
-                .query(queryType)
-                .build();
+            .query(queryType)
+            .build();
     }
 }
