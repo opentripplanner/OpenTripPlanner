00a651762b7ab168d15f4a4af5b4bb2a3f07a090
Marko Burjek <s0mebody.slo@gmail.com>
2015-08-17 12:24:20 +0200

    Remove link to transit layer on Bike* annotations

--- src/main/java/org/opentripplanner/graph_builder/annotation/BikeParkUnlinked.java
+++ src/main/java/org/opentripplanner/graph_builder/annotation/BikeParkUnlinked.java
@@ -20,7 +20,7 @@ public class BikeParkUnlinked extends GraphBuilderAnnotation {
     private static final long serialVersionUID = 1L;
 
     private static final String FMT = "Bike park %s not near any streets; it will not be usable.";
-    private static final String HTMLFMT = "Bike park <a href=\"http://www.openstreetmap.org/?mlat=%s&mlon=%s&layers=T\">\"%s\"</a> not near any streets; it will not be usable.";
+    private static final String HTMLFMT = "Bike park <a href=\"http://www.openstreetmap.org/?mlat=%s&mlon=%s\">\"%s\"</a> not near any streets; it will not be usable.";
 
     final BikeParkVertex bikeParkVertex;
 
--- src/main/java/org/opentripplanner/graph_builder/annotation/BikeRentalStationUnlinked.java
+++ src/main/java/org/opentripplanner/graph_builder/annotation/BikeRentalStationUnlinked.java
@@ -20,7 +20,7 @@ public class BikeRentalStationUnlinked extends GraphBuilderAnnotation {
     private static final long serialVersionUID = 1L;
 
     public static final String FMT = "Bike rental station %s not near any streets; it will not be usable.";
-    private static final String HTMLFMT = "Bike rental station <a href=\"http://www.openstreetmap.org/?mlat=%s&mlon=%s&layers=T\">\"%s\"</a> not near any streets; it will not be usable.";
+    private static final String HTMLFMT = "Bike rental station <a href=\"http://www.openstreetmap.org/?mlat=%s&mlon=%s\">\"%s\"</a> not near any streets; it will not be usable.";
     
     final BikeRentalStationVertex station;
     
