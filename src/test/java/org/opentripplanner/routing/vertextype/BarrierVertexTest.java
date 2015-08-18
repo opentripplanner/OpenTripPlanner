1e6a3ad47bf3d50a3f9a09f7e9efa6b5ec09c01c
Marko Burjek <s0mebody.slo@gmail.com>
2015-08-18 12:09:20 +0200

    Add tests for permissions in street edges with bollard
    Since there are two kinds of permissions:
    - canTraverse(TraverseModeSet) which just checks street permissions
     without accounting  for bollard
    - canTraverse(RoutingRequest, TraverseMode) which is used in doTraverse
      and uses permissions of bollard
    
    First is used in searching for searching for nearest edges and car/walk
    edges in Park and ride.
    Second is used in actuall traversing. So it can happen that edge has
    canTraverse true but actually can't be traversed with specific mode.
    
    Edge for example has bollard at the end and isn't traversable with a
    car, but destination is couple of meters before the bollard and when
    edge is split for that, edge is traversable also for CAR.
    
    Fixes #1914

--- src/test/java/org/opentripplanner/routing/vertextype/BarrierVertexTest.java
+++ src/test/java/org/opentripplanner/routing/vertextype/BarrierVertexTest.java
@@ -13,9 +13,15 @@
 
 package org.opentripplanner.routing.vertextype;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.LineString;
 import org.junit.Test;
+import org.opentripplanner.common.geometry.GeometryUtils;
 import org.opentripplanner.graph_builder.module.osm.OSMFilter;
 import org.opentripplanner.openstreetmap.model.OSMNode;
+import org.opentripplanner.routing.core.TraverseMode;
+import org.opentripplanner.routing.core.TraverseModeSet;
+import org.opentripplanner.routing.edgetype.StreetEdge;
 import org.opentripplanner.routing.edgetype.StreetTraversalPermission;
 import org.opentripplanner.routing.graph.Graph;
 
@@ -65,8 +71,103 @@ public class BarrierVertexTest {
         OSMNode complexBarrier = new OSMNode();
         complexBarrier.addTag("barrier", "bollard");
         complexBarrier.addTag("access", "no");
+
         bv.setBarrierPermissions(OSMFilter
             .getPermissionsForEntity(complexBarrier, BarrierVertex.defaultBarrierPermissions));
         assertEquals(StreetTraversalPermission.NONE, bv.getBarrierPermissions());
+
+        OSMNode noBikeBollard = new OSMNode();
+        noBikeBollard.addTag("barrier", "bollard");
+        noBikeBollard.addTag("bicycle", "no");
+
+        bv.setBarrierPermissions(OSMFilter
+            .getPermissionsForEntity(noBikeBollard, BarrierVertex.defaultBarrierPermissions));
+        assertEquals(StreetTraversalPermission.PEDESTRIAN, bv.getBarrierPermissions());
+    }
+
+    /**
+     * Create an edge. If twoWay, create two edges (back and forth).
+     *
+     * @param vA
+     * @param vB
+     * @param length
+     * @param back true if this is a reverse edge
+     */
+    private StreetEdge edge(StreetVertex vA, StreetVertex vB, double length, boolean back) {
+        String labelA = vA.getLabel();
+        String labelB = vB.getLabel();
+        String name = String.format("%s_%s", labelA, labelB);
+        Coordinate[] coords = new Coordinate[2];
+        coords[0] = vA.getCoordinate();
+        coords[1] = vB.getCoordinate();
+        LineString geom = GeometryUtils.getGeometryFactory().createLineString(coords);
+
+        StreetTraversalPermission perm = StreetTraversalPermission.ALL;
+        return new StreetEdge(vA, vB, geom, name, length, perm, back);
+    }
+
+
+
+    @Test
+    public void testStreetsWithBollard() {
+        Graph _graph = new Graph();
+        //default permissions are PEDESTRIAND and BICYCLE
+        BarrierVertex bv = new BarrierVertex(_graph, "start_bollard", 2.0, 2.0);
+
+        StreetVertex endVertex = new IntersectionVertex(_graph, "end_vertex", 1.0, 2.0);
+
+        StreetEdge bv_to_endVertex_forward = edge(bv, endVertex, 100, false);
+
+        assertTrue(bv_to_endVertex_forward.canTraverse(new TraverseModeSet("CAR")));
+        assertTrue(bv_to_endVertex_forward.canTraverse(new TraverseModeSet("BICYCLE")));
+        assertTrue(bv_to_endVertex_forward.canTraverse(new TraverseModeSet("WALK")));
+
+        assertFalse(bv_to_endVertex_forward.canTraverseIncludingBarrier(TraverseMode.CAR));
+        assertTrue(bv_to_endVertex_forward.canTraverseIncludingBarrier(TraverseMode.BICYCLE));
+        assertTrue(bv_to_endVertex_forward.canTraverseIncludingBarrier(TraverseMode.WALK));
+
+        StreetEdge endVertex_to_bv_backward = edge(endVertex, bv, 100, true);
+
+        assertTrue(endVertex_to_bv_backward.canTraverse(new TraverseModeSet("CAR")));
+        assertTrue(endVertex_to_bv_backward.canTraverse(new TraverseModeSet("BICYCLE")));
+        assertTrue(endVertex_to_bv_backward.canTraverse(new TraverseModeSet("WALK")));
+
+        assertFalse(endVertex_to_bv_backward.canTraverseIncludingBarrier(TraverseMode.CAR));
+        assertTrue(endVertex_to_bv_backward.canTraverseIncludingBarrier(TraverseMode.BICYCLE));
+        assertTrue(endVertex_to_bv_backward.canTraverseIncludingBarrier(TraverseMode.WALK));
+
+        StreetEdge bv_to_endVertex_backward = edge(bv, endVertex, 100, true);
+
+        assertTrue(bv_to_endVertex_backward.canTraverse(new TraverseModeSet("CAR")));
+        assertTrue(bv_to_endVertex_backward.canTraverse(new TraverseModeSet("BICYCLE")));
+        assertTrue(bv_to_endVertex_backward.canTraverse(new TraverseModeSet("WALK")));
+
+        assertFalse(bv_to_endVertex_backward.canTraverseIncludingBarrier(TraverseMode.CAR));
+        assertTrue(bv_to_endVertex_backward.canTraverseIncludingBarrier(TraverseMode.BICYCLE));
+        assertTrue(bv_to_endVertex_backward.canTraverseIncludingBarrier(TraverseMode.WALK));
+
+        StreetEdge endVertex_to_bv_forward = edge(endVertex, bv, 100, false);
+
+        assertTrue(endVertex_to_bv_forward.canTraverse(new TraverseModeSet("CAR")));
+        assertTrue(endVertex_to_bv_forward.canTraverse(new TraverseModeSet("BICYCLE")));
+        assertTrue(endVertex_to_bv_forward.canTraverse(new TraverseModeSet("WALK")));
+
+        assertFalse(endVertex_to_bv_forward.canTraverseIncludingBarrier(TraverseMode.CAR));
+        assertTrue(endVertex_to_bv_forward.canTraverseIncludingBarrier(TraverseMode.BICYCLE));
+        assertTrue(endVertex_to_bv_forward.canTraverseIncludingBarrier(TraverseMode.WALK));
+
+
+        //tests bollard which doesn't allo cycling
+        BarrierVertex noBicycleBollard = new BarrierVertex(_graph, "no_bike_bollard", 1.5, 1);
+        noBicycleBollard.setBarrierPermissions(StreetTraversalPermission.PEDESTRIAN);
+        StreetEdge no_bike_to_endVertex = edge(noBicycleBollard, endVertex, 100, false);
+
+        assertTrue(no_bike_to_endVertex.canTraverse(new TraverseModeSet("CAR")));
+        assertTrue(no_bike_to_endVertex.canTraverse(new TraverseModeSet("BICYCLE")));
+        assertTrue(no_bike_to_endVertex.canTraverse(new TraverseModeSet("WALK")));
+
+        assertFalse(no_bike_to_endVertex.canTraverseIncludingBarrier(TraverseMode.CAR));
+        assertFalse(no_bike_to_endVertex.canTraverseIncludingBarrier(TraverseMode.BICYCLE));
+        assertTrue(no_bike_to_endVertex.canTraverseIncludingBarrier(TraverseMode.WALK));
     }
 }
