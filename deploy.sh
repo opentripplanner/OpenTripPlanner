1726498628b9350b4e523450ea6d70077d04201c
Andrew Byrd <andrew@fastmail.net>
2015-08-22 14:32:44 +0200

    Remove deployment script which is not used

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Deploy Maven artifacts to the Conveyal repo on S3 
-# iff this is not a pull request and the branch is master.
-after_success: |
-  if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    \# no need to run tests again
-    mvn deploy -DskipTests
-  fi
