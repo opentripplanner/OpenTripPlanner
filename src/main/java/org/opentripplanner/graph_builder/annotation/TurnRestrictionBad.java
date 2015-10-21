b161248f82d495dab55d2dd4fa6386ad9204e75c
Marko Burjek <s0mebody.slo@gmail.com>
2015-10-21 15:06:59 +0200

    Add graph annotations when turn restrictions are skipped
    Previously if turn restrictions weren't created because from/to edges
    were missing or edges weren't the correct angle they were silently
    ignored. This commit adds those skipped turn restrictions to log and
    adds them to graph annotations
    
    Fixes #2159

--- src/main/java/org/opentripplanner/graph_builder/annotation/TurnRestrictionBad.java
+++ src/main/java/org/opentripplanner/graph_builder/annotation/TurnRestrictionBad.java
@@ -17,23 +17,26 @@ public class TurnRestrictionBad extends GraphBuilderAnnotation {
 
     private static final long serialVersionUID = 1L;
 
-    public static final String FMT = "Bad turn restriction at relation %s";
-    public static final String HTMLFMT = "Bad turn restriction at relation <a href='http://www.openstreetmap.org/relation/%s'>%s</a>";
+    public static final String FMT = "Bad turn restriction at relation %s. Reason: %s";
+    public static final String HTMLFMT = "Bad turn restriction at relation <a href='http://www.openstreetmap.org/relation/%s'>%s</a>. Reason: %s";
     
     final long id;
-    
-    public TurnRestrictionBad(long id){
-    	this.id = id;
+
+    final String reason;
+
+    public TurnRestrictionBad(long relationOSMID, String reason) {
+        this.id = relationOSMID;
+        this.reason = reason;
     }
-    
+
     @Override
     public String getMessage() {
-        return String.format(FMT, id);
+        return String.format(FMT, id, reason);
     }
 
     @Override
     public String getHTMLMessage() {
-        return String.format(HTMLFMT, id, id);
+        return String.format(HTMLFMT, id, id, reason);
     }
 
 }
