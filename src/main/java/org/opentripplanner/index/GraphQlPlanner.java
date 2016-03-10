839502fbe4f6fcdabbe2c0170e70770d1659998d
Markku Rontu <markku.rontu@iki.fi>
2016-03-10 09:51:02 +0200

    feat: pass some more parameters

--- src/main/java/org/opentripplanner/index/GraphQlPlanner.java
+++ src/main/java/org/opentripplanner/index/GraphQlPlanner.java
@@ -2,6 +2,7 @@ package org.opentripplanner.index;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.opentripplanner.api.model.TripPlan;
 import org.opentripplanner.api.parameter.QualifiedModeSet;
@@ -30,16 +31,52 @@ public class GraphQlPlanner {
         return plan;
     }
 
+    private <T> void withArgument(DataFetchingEnvironment environment, String name, Consumer<T> consumer) {
+        if (environment.containsArgument(name)) {
+            consumer.accept(environment.getArgument(name));
+        }
+    }
+
     private RoutingRequest createRequest(DataFetchingEnvironment environment) {
         RoutingRequest request = new RoutingRequest();
-        request.from.lat = ((Number)environment.getArgument("fromLat")).doubleValue();
-        request.from.lng = ((Number)environment.getArgument("fromLon")).doubleValue();
-        request.to.lat = ((Number)environment.getArgument("toLat")).doubleValue();
-        request.to.lng = ((Number)environment.getArgument("toLon")).doubleValue();
-        request.setPreferredAgencies("HSL");
+
+        withArgument(environment, "fromPlace", request::setFromString);
+        withArgument(environment, "toPlace", request::setToString);
+        withArgument(environment, "intermediatePlaces", request::setIntermediatePlacesFromStrings);
+
+        // something for date, time, tz into setDateTime
+
+        withArgument(environment, "arriveBy", request::setArriveBy);
+        withArgument(environment, "wheelchair", request::setWheelchairAccessible);
+        withArgument(environment, "maxWalkDistance", request::setMaxWalkDistance);
+        withArgument(environment, "maxPreTransitTime", request::setMaxPreTransitTime);
+        withArgument(environment, "walkReluctance", request::setWalkReluctance);
+        withArgument(environment, "waitAtBeginningFactor", request::setWaitAtBeginningFactor);
+        withArgument(environment, "walkSpeed", (Double v) -> request.walkSpeed = v);
+        withArgument(environment, "bikeSpeed", (Double v) -> request.bikeSpeed = v);
+        withArgument(environment, "bikeSwitchTime", (Integer v) -> request.bikeSwitchTime = v);
+        withArgument(environment, "bikeSwitchCost", (Integer v) -> request.bikeSwitchCost = v);
+
+        //withArgument(environment, "optimize", request::setOptimize);
+        withArgument(environment, "triangleSafetyFactor", request::setTriangleSafetyFactor);
+        withArgument(environment, "triangleSlopeFactor", request::setTriangleSlopeFactor);
+        withArgument(environment, "triangleTimeFactor", request::setTriangleTimeFactor);
+
         QualifiedModeSet modes = new QualifiedModeSet("WALK,BUS");
         modes.applyToRoutingRequest(request);
         request.setModes(request.modes);
+
+        withArgument(environment, "minTransferTime", (Integer v) -> request.transferSlack= v);
+
+
+
+
+        withArgument(environment, "fromLat", (Double v) -> request.from.lat = v);
+        withArgument(environment, "fromLon", (Double v) -> request.from.lng = v);
+        withArgument(environment, "toLat", (Double v) -> request.to.lat = v);
+        withArgument(environment, "toLon", (Double v) -> request.to.lng = v);
+
+        request.setPreferredAgencies("HSL");
         request.setWheelchairAccessible(false);
         request.setMaxWalkDistance(2500.0);
         request.setWalkReluctance(2.0);
