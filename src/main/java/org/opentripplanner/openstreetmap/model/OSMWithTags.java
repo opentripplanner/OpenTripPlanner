91692a1ff5f714f95337fa27abaa39c2f07deec5
Pieter Buzing <pieter@west.nl>
2016-05-25 14:55:16 +0200

    Revert "Improve efficiency of linker by storing lowercase"
    This reverts commit cf7de0646b73865c0044114b4c6c5a3da8405367.

--- src/main/java/org/opentripplanner/openstreetmap/model/OSMWithTags.java
+++ src/main/java/org/opentripplanner/openstreetmap/model/OSMWithTags.java
@@ -57,7 +57,7 @@ public class OSMWithTags {
         if (_tags == null)
             _tags = new HashMap<String, String>();
 
-        _tags.put(tag.getK().toLowerCase(), tag.getV().toLowerCase());
+        _tags.put(tag.getK().toLowerCase(), tag.getV());
     }
 
     /**
@@ -70,7 +70,7 @@ public class OSMWithTags {
         if (_tags == null)
             _tags = new HashMap<String, String>();
 
-        _tags.put(key.toLowerCase(), value.toLowerCase());
+        _tags.put(key.toLowerCase(), value);
     }
 
     /**
@@ -125,7 +125,8 @@ public class OSMWithTags {
 
     /** @return a tag's value, converted to lower case. */
     public String getTag(String tag) {
-        if (_tags != null) {
+        tag = tag.toLowerCase();
+        if (_tags != null && _tags.containsKey(tag)) {
             return _tags.get(tag);
         }
         return null;
