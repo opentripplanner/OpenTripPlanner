d06e242a73ff002964fe44fbc12ef2a59c5c8969
Sami Siren <siren@foofactory.fi>
2016-07-13 07:29:24 +0300

    return proper serviceday when requesting stoptimesForDate from trip, also make servicedate optional (uses current date default)

--- src/main/java/org/opentripplanner/index/model/TripTimeShort.java
+++ src/main/java/org/opentripplanner/index/model/TripTimeShort.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.gtfs.model.Stop;
 import org.onebusaway.gtfs.model.Trip;
+import org.onebusaway.gtfs.model.calendar.ServiceDate;
 import org.opentripplanner.routing.core.ServiceDay;
 import org.opentripplanner.routing.edgetype.Timetable;
 import org.opentripplanner.routing.trippattern.RealTimeState;
@@ -53,18 +54,30 @@ public class TripTimeShort {
 
     public TripTimeShort(TripTimes tt, int i, Stop stop, ServiceDay sd) {
         this(tt, i, stop);
-        serviceDay = sd.time(0);
+        if (sd != null) {
+            serviceDay = sd.time(0);
+        }
+    }
+
+    
+    /**
+     * must pass in both table and trip, because tripTimes do not have stops.
+     */
+    public static List<TripTimeShort> fromTripTimes(Timetable table, Trip trip) {
+        return fromTripTimes(table, trip, null);
     }
 
     /**
      * must pass in both table and trip, because tripTimes do not have stops.
+     * @param serviceDay service day to set, if null none is set
      */
-    public static List<TripTimeShort> fromTripTimes (Timetable table, Trip trip) {
-        TripTimes times = table.getTripTimes(table.getTripIndex(trip.getId()));        
+    public static List<TripTimeShort> fromTripTimes(Timetable table, Trip trip,
+        ServiceDay serviceDay) {
+        TripTimes times = table.getTripTimes(table.getTripIndex(trip.getId()));
         List<TripTimeShort> out = Lists.newArrayList();
         // one per stop, not one per hop, thus the <= operator
         for (int i = 0; i < times.getNumStops(); ++i) {
-            out.add(new TripTimeShort(times, i, table.pattern.getStop(i)));
+            out.add(new TripTimeShort(times, i, table.pattern.getStop(i), serviceDay));
         }
         return out;
     }
