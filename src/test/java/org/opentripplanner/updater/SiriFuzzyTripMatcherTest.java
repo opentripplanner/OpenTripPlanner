8c52a83113412e33f23e126d631ba51aae694950
Lasse Tyrihjell <lassetyr@gmail.com>
2016-08-22 15:16:59 +0200

    Added test - Avoiding NPE

---
+++ src/test/java/org/opentripplanner/updater/SiriFuzzyTripMatcherTest.java
@@ -0,0 +1,67 @@
+package org.opentripplanner.updater;
+
+import com.google.transit.realtime.GtfsRealtime;
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.gtfs.model.Trip;
+import org.opentripplanner.GtfsTest;
+import uk.org.siri.siri20.CourseOfJourneyRefStructure;
+import uk.org.siri.siri20.VehicleActivityStructure;
+
+import java.util.Map;
+import java.util.Set;
+
+public class SiriFuzzyTripMatcherTest extends GtfsTest {
+
+    public void testMatch() throws Exception {
+        String feedId = graph.getFeedIds().iterator().next();
+
+        SiriFuzzyTripMatcher matcher = new SiriFuzzyTripMatcher(graph.index);
+
+        Set<Map.Entry<AgencyAndId, Trip>> entries = graph.index.tripForId.entrySet();
+
+
+        VehicleActivityStructure.MonitoredVehicleJourney monitoredVehicleJourney = new VehicleActivityStructure.MonitoredVehicleJourney();
+        CourseOfJourneyRefStructure courseOfJourney = new CourseOfJourneyRefStructure();
+        courseOfJourney.setValue("10W1020");
+        monitoredVehicleJourney.setCourseOfJourneyRef(courseOfJourney);
+
+
+        VehicleActivityStructure activity = new VehicleActivityStructure();
+        activity.setMonitoredVehicleJourney(monitoredVehicleJourney);
+        Set<Trip> match = matcher.match(activity);
+
+        assertTrue(match.size() == 1);
+
+        Trip trip = match.iterator().next();
+        assertNotNull(trip);
+        assertTrue(trip.getId().getId().equals(courseOfJourney.getValue()));
+
+    }
+
+    public void testNoMatch() throws Exception {
+        String feedId = graph.getFeedIds().iterator().next();
+
+        SiriFuzzyTripMatcher matcher = new SiriFuzzyTripMatcher(graph.index);
+
+        Set<Map.Entry<AgencyAndId, Trip>> entries = graph.index.tripForId.entrySet();
+
+
+        VehicleActivityStructure.MonitoredVehicleJourney monitoredVehicleJourney = new VehicleActivityStructure.MonitoredVehicleJourney();
+        CourseOfJourneyRefStructure courseOfJourney = new CourseOfJourneyRefStructure();
+        courseOfJourney.setValue("FOOBAR");
+        monitoredVehicleJourney.setCourseOfJourneyRef(courseOfJourney);
+
+
+        VehicleActivityStructure activity = new VehicleActivityStructure();
+        activity.setMonitoredVehicleJourney(monitoredVehicleJourney);
+        Set<Trip> match = matcher.match(activity);
+
+        assertNull(match);
+
+    }
+
+    @Override
+    public String getFeedName() {
+        return "google_transit.zip";
+    }
+}
