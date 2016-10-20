4d4baa463b5a605a41a644ee2b681d8d16d88f83
Thomas Dy <thatsmydoing@gmail.com>
2016-10-20 13:30:41 +0800

    Fix frequency bounds checking

--- src/main/java/org/opentripplanner/routing/trippattern/FrequencyEntry.java
+++ src/main/java/org/opentripplanner/routing/trippattern/FrequencyEntry.java
@@ -59,11 +59,11 @@ public class FrequencyEntry implements Serializable {
     }
 
     public int nextDepartureTime (int stop, int time) {
-        if (time > endTime) return -1;
         // Start time and end time are for the first stop in the trip. Find the time offset for this stop.
         int stopOffset = tripTimes.getDepartureTime(stop) - tripTimes.getDepartureTime(0);
         int beg = startTime + stopOffset; // First time a vehicle passes by this stop.
         int end = endTime + stopOffset; // Latest a vehicle can pass by this stop.
+        if (time > end) return -1;
         if (exactTimes) {
             for (int dep = beg; dep < end; dep += headway) {
                 if (dep >= time) return dep;
@@ -80,10 +80,10 @@ public class FrequencyEntry implements Serializable {
     }
 
     public int prevArrivalTime (int stop, int t) {
-        if (t < startTime) return -1;
         int stopOffset = tripTimes.getArrivalTime(stop) - tripTimes.getDepartureTime(0);
         int beg = startTime + stopOffset; // First time a vehicle passes by this stop.
         int end = endTime + stopOffset; // Latest a vehicle can pass by this stop.
+        if(t < beg) return -1;
         if (exactTimes) {
             for (int dep = end; dep > beg; dep -= headway) {
                 if (dep <= t) return dep;
