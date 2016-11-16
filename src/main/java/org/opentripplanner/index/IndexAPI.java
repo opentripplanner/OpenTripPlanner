bca19b43fc0158a040420b5c8b7337b8b36b9fd2
Hannes Junnila <hannes.junnila@gmail.com>
2016-11-16 10:30:53 +0200

    Allow for batching of GraphQL queries

--- src/main/java/org/opentripplanner/index/IndexAPI.java
+++ src/main/java/org/opentripplanner/index/IndexAPI.java
@@ -74,6 +74,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 // TODO move to org.opentripplanner.api.resource, this is a Jersey resource class
 
@@ -617,6 +621,46 @@ public class IndexAPI {
 //                                @QueryParam("variables") HashMap<String, Object> variables) {
 //        return index.getGraphQLResponse(query, variables == null ? new HashMap<>() : variables);
 //    }
+
+    @POST
+    @Path("/graphql/batch")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getGraphQLBatch (List<HashMap<String, Object>> queries, @HeaderParam("OTPTimeout") @DefaultValue("10000") int timeout, @HeaderParam("OTPMaxResolves") @DefaultValue("1000000") long maxResolves) {
+        List<Map<String, Object>> responses = new ArrayList<>();
+        List<Callable<Map>> futures = new ArrayList();
+
+        for (HashMap<String, Object> query : queries) {
+            Map<String, Object> variables;
+            if (query.get("variables") instanceof Map) {
+                variables = (Map) query.get("variables");
+            } else {
+                try {
+                    variables = deserializer.readValue((String) query.get("variables"), Map.class);
+                } catch (IOException e) {
+                    LOG.error("Variables must be a valid json object");
+                    return Response.status(Status.BAD_REQUEST).entity(MSG_400).build();
+                }
+            }
+            String operationName = (String) query.getOrDefault("operationName", null);
+
+            futures.add(() -> index.getGraphQLExecutionResult((String) query.get("query"), router,
+                variables, operationName, timeout, maxResolves));
+        }
+
+        try {
+            List<Future<Map>> results = index.threadPool.invokeAll(futures);
+
+            for (int i = 0; i < queries.size(); i++) {
+                HashMap<String, Object> response = new HashMap<>();
+                response.put("id", queries.get(i).get("id"));
+                response.put("payload", results.get(i).get());
+                responses.add(response);
+            }
+        } catch (CancellationException | ExecutionException |InterruptedException e) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+        }
+        return Response.status(Status.OK).entity(responses).build();
+    }
 
     /** Represents a transfer from a stop */
     private static class Transfer {
