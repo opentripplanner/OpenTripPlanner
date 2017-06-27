c5aa78e7fc60538136dd32a8a6f5b8a60a63541f
Arne Seime <arne.seime@greenbird.com>
2017-06-27 19:32:21 +0200

    Moved WayPropertySetSourceFactory single factory method into WayPropertySetSource

--- src/main/java/org/opentripplanner/graph_builder/module/osm/WayPropertySetSource.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/WayPropertySetSource.java
@@ -21,5 +21,22 @@ package org.opentripplanner.graph_builder.module.osm;
  */
 public interface WayPropertySetSource {
 
-    public void populateProperties(WayPropertySet wayPropertySet);
+	public void populateProperties(WayPropertySet wayPropertySet);
+
+	/**
+	 * Return the given WayPropertySetSource or throws IllegalArgumentException
+	 * if an unkown type is specified
+	 */
+	public static WayPropertySetSource fromConfig(String type) {
+		// type is set to "default" by GraphBuilderParameters if not provided in
+		// build-config.json
+		if ("default".equals(type)) {
+			return new DefaultWayPropertySetSource();
+		} else if ("norway".equals(type)) {
+			return new NorwayWayPropertySetSource();
+		} else {
+			throw new IllegalArgumentException(String.format("Unknown osmWayPropertySet: '%s'", type));
+		}
+	}
+
 }
--- src/main/java/org/opentripplanner/standalone/GraphBuilderParameters.java
+++ src/main/java/org/opentripplanner/standalone/GraphBuilderParameters.java
@@ -1,7 +1,6 @@
 package org.opentripplanner.standalone;
 
 import org.opentripplanner.graph_builder.module.osm.WayPropertySetSource;
-import org.opentripplanner.graph_builder.module.osm.WayPropertySetSourceFactory;
 import org.opentripplanner.graph_builder.services.osm.CustomNamer;
 import org.opentripplanner.routing.impl.DefaultFareServiceFactory;
 import org.opentripplanner.routing.services.FareServiceFactory;
@@ -158,7 +157,7 @@ public class GraphBuilderParameters {
         elevationBucket = S3BucketConfig.fromConfig(config.path("elevationBucket"));
         fareServiceFactory = DefaultFareServiceFactory.fromConfig(config.path("fares"));
         customNamer = CustomNamer.CustomNamerFactory.fromConfig(config.path("osmNaming"));
-        wayPropertySet = WayPropertySetSourceFactory.fromConfig(config.path("osmWayPropertySet").asText("default"));
+        wayPropertySet = WayPropertySetSource.fromConfig(config.path("osmWayPropertySet").asText("default"));
         staticBikeRental = config.path("staticBikeRental").asBoolean(false);
         staticParkAndRide = config.path("staticParkAndRide").asBoolean(true);
         staticBikeParkAndRide = config.path("staticBikeParkAndRide").asBoolean(false);
