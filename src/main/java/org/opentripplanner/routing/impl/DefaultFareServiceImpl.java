8e66a1bf1d3ff8903fd379fe699478ad4067485f
Simon Jacobs <sjacobs@camsys.com>
2017-09-19 13:34:58 -0400

    VGF-75 for fare calcuation, use feed_id from route, since call-n-ride can go to stops that are not in its feed. route.getId().getAgencyId() actually *is* the feed_id - route.getAgency() contains the agency from GTFS route.agency_id

--- src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
+++ src/main/java/org/opentripplanner/routing/impl/DefaultFareServiceImpl.java
@@ -352,12 +352,12 @@ public class DefaultFareServiceImpl implements FareService, Serializable {
         long   startTime = firstRide.startTime;
         String startZone = firstRide.startZone;
         String endZone = firstRide.endZone;
-        // stops don't really have an agency id, they have the per-feed default id
-        String feedId = firstRide.firstStop.getId().getAgencyId();  
+        // route agency ID is really per-feed default id (GTFS `agency_id` becomes route.agency.)
+        String feedId = firstRide.route.getAgencyId();
         long lastRideStartTime = firstRide.startTime;
         long lastRideEndTime = firstRide.endTime;
         for (Ride ride : rides) {
-            if ( ! ride.firstStop.getId().getAgencyId().equals(feedId)) {
+            if ( ! ride.route.getAgencyId().equals(feedId)) {
                 LOG.debug("skipped multi-feed ride sequence {}", rides);
                 return new FareAndId(Float.POSITIVE_INFINITY, null);
             }
