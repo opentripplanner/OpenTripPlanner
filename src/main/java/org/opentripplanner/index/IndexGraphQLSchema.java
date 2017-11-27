93bb0cf76abff1c8c881f5b1eacd9d0319679f57
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2017-11-27 14:47:25 +0100

    Changed graphql schema. Agency url can now be null.

--- src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
+++ src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
@@ -731,7 +731,7 @@ public class IndexGraphQLSchema {
                 .build())
             .field(GraphQLFieldDefinition.newFieldDefinition()
                 .name("url")
-                .type(new GraphQLNonNull(Scalars.GraphQLString))
+                .type(Scalars.GraphQLString)
                 .build())
             .field(GraphQLFieldDefinition.newFieldDefinition()
                 .name("timezone")
