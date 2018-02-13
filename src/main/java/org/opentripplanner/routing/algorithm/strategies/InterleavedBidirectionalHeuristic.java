bc220d175faf751a430cfe270cac7b970e62e808
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2018-02-13 18:48:27 +0100

    Also search street network when searching from transit stop

--- src/main/java/org/opentripplanner/routing/algorithm/strategies/InterleavedBidirectionalHeuristic.java
+++ src/main/java/org/opentripplanner/routing/algorithm/strategies/InterleavedBidirectionalHeuristic.java
@@ -18,6 +18,7 @@ import gnu.trove.map.hash.TObjectDoubleHashMap;
 import org.opentripplanner.common.pqueue.BinHeap;
 import org.opentripplanner.routing.core.RoutingRequest;
 import org.opentripplanner.routing.core.State;
+import org.opentripplanner.routing.edgetype.StationStopEdge;
 import org.opentripplanner.routing.edgetype.StreetTransitLink;
 import org.opentripplanner.routing.graph.Edge;
 import org.opentripplanner.routing.graph.Graph;
@@ -270,6 +271,7 @@ public class InterleavedBidirectionalHeuristic implements RemainingWeightHeurist
         if (fromTarget) {
             rr.setArriveBy(!rr.arriveBy);
         }
+        boolean stopReached = false;
         // Create a map that returns Infinity when it does not contain a vertex.
         TObjectDoubleMap<Vertex> vertices = new TObjectDoubleHashMap<>(100, 0.5f, Double.POSITIVE_INFINITY);
         ShortestPathTree spt = new DominanceFunction.MinimumWeight().getNewShortestPathTree(rr);
@@ -284,6 +286,9 @@ public class InterleavedBidirectionalHeuristic implements RemainingWeightHeurist
             }
             State s = pq.extract_min();
             Vertex v = s.getVertex();
+
+            boolean initialStop = v instanceof TransitStop && s.backEdge != null && s.backEdge instanceof StationStopEdge;
+
             // At this point the vertex is closed (pulled off heap).
             // This is the lowest cost we will ever see for this vertex. We can record the cost to reach it.
             if (v instanceof TransitStop) {
@@ -296,15 +301,27 @@ public class InterleavedBidirectionalHeuristic implements RemainingWeightHeurist
                         maxWeightSeen = weight;
                     }
                 }
-                continue;
+                if (!stopReached) {
+                    stopReached = true;
+                    rr.softWalkLimiting = false;
+                    rr.softPreTransitLimiting = false;
+                    if (s.walkDistance > rr.maxWalkDistance) {
+                        // Add 300 meters in order to search for nearby stops
+                        rr.maxWalkDistance = s.walkDistance + 300;
+                    }
+                }
+                if (!initialStop) continue;
             }
             // We don't test whether we're on an instanceof StreetVertex here because some other vertex types
             // (park and ride or bike rental related) that should also be explored and marked as usable.
             // Record the cost to reach this vertex.
-            if (!vertices.containsKey(v)) {
+            if (!vertices.containsKey(v) && !(v instanceof TransitStop)) {
                 vertices.put(v, (int) s.getWeight()); // FIXME time or weight? is RR using right mode?
             }
             for (Edge e : rr.arriveBy ? v.getIncoming() : v.getOutgoing()) {
+                if (v instanceof TransitStop && !(e instanceof StreetTransitLink)) {
+                    continue;
+                }
                 // arriveBy has been set to match actual directional behavior in this subsearch.
                 // Walk cutoff will happen in the street edge traversal method.
                 State s1 = e.traverse(s);
