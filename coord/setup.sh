a286eb8e0714e6828854edb4166c7f9c58d9e7e7
Mahmood Rahmani <mahmood@sidewalklabs.com>
2018-04-27 15:59:06 -0400

    fix path otp-base/graphs

--- coord/setup.sh
+++ coord/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get downtown DC road-network data.
-curl -L -o coord/dc/district-of-columbia-latest.osm.pbf http://download.geofabrik.de/north-america/us/district-of-columbia-latest.osm.pbf
+curl -L -o coord/otp-base/graphs/dc/district-of-columbia-latest.osm.pbf http://download.geofabrik.de/north-america/us/district-of-columbia-latest.osm.pbf
 
 # Get DC transit network data.
-curl -L -o coord/dc/dc.gtfs.zip https://transitfeeds.com/p/wmata/75/latest/download
+curl -L -o coord/otp-base/graphs/dc/dc.gtfs.zip https://transitfeeds.com/p/wmata/75/latest/download
 
 # Build the binary.
 mvn package -Dmaven.test.skip=true
