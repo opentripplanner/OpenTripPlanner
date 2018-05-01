ca3922c3c62356f711bae68449c4bc8ac291055e
Daniel Yehuda <danieljy@sidewalklabs.com>
2018-05-01 16:57:42 -0400

    Use a larger DC road network.

--- coord/download-data.sh
+++ coord/download-data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Get downtown DC road-network data.
-curl -L -o coord/otp-base/graphs/dc/district-of-columbia-latest.osm.pbf http://download.geofabrik.de/north-america/us/district-of-columbia-latest.osm.pbf
+# Get DC metro road network, which was generated once using this tool: http://extract.bbbike.org
+# Bounding box: (-77.458,38.678) (-76.607,39.114)
+curl -L -o coord/otp-base/graphs/dc/dc-metro.osm.pbf https://storage.googleapis.com/coord-osm/dc-metro.osm.pbf
 
 # Get DC transit network data.
 curl -L -o coord/otp-base/graphs/dc/dc.gtfs.zip https://transitfeeds.com/p/wmata/75/latest/download
