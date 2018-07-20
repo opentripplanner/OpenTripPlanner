092626e82d6d5ee89f5f5814f9b8a9f4d331211e
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-07-20 10:25:19 +0300

    Explain usage of argument 'startTransitTripId'

--- src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
+++ src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
@@ -684,7 +684,7 @@ public class IndexGraphQLSchema {
                 .build())
             .argument(GraphQLArgument.newArgument()
                 .name("startTransitTripId")
-                .description("A trip where this trip must start from (depart-onboard routing)")
+                .description("ID of the trip on which the itinerary starts. This argument can be used to plan itineraries when the user is already onboard a vehicle. When using this argument, arguments `time` and `from` should be set based on a vehicle position message received from the vehicle running the specified trip.  **Note:** this argument does not take into account the timetable of the trip (and therefore arguments `time` and `from` must be used correctly to get meaningful itineraries).")
                 .type(Scalars.GraphQLString)
                 .build())
             .argument(GraphQLArgument.newArgument()
