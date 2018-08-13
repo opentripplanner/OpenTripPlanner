f2b375364985b8dd83f791950d955e3ec5c9cb34
Thomas Gran <t2gran@gmail.com>
2018-08-13 17:29:49 +0200

    Add a single License file and remove all OTP and OBA file license headers.

--- src/main/java/org/opentripplanner/model/calendar/CalendarServiceData.java
+++ src/main/java/org/opentripplanner/model/calendar/CalendarServiceData.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2011 Brian Ferris <bdferris@onebusaway.org>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* This file is copied from OneBusAway project. */
 package org.opentripplanner.model.calendar;
 
 import org.opentripplanner.model.FeedId;
