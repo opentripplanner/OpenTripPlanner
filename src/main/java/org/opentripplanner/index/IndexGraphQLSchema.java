872acb28dd181288066a11ce7290d47420d01e62
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-08-23 15:19:35 +0300

    Add description for Route type

--- src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
+++ src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
@@ -1987,6 +1987,7 @@ public class IndexGraphQLSchema {
 
         routeType = GraphQLObjectType.newObject()
             .name("Route")
+            .description("Route represents a public transportation service, usually from point A to point B and *back*, shown to customers under a single name, e.g. bus 550. Routes contain patterns (see field `patterns`), which describe different variants of the route, e.g. outbound pattern from point A to point B and inbound pattern from point B to point A.")
 	    .withInterface(nodeInterface)
             .field(GraphQLFieldDefinition.newFieldDefinition()
                 .name("id")
