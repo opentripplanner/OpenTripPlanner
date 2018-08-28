2b4aca9febd0a9e40f1c15c2c94d4021d81c1eb7
Teemu <teemu.peltonen@cgi.com>
2018-08-28 13:47:19 +0300

    Fix test

--- src/test/java/org/opentripplanner/mmri/AllModesAndAgenciesTest.java
+++ src/test/java/org/opentripplanner/mmri/AllModesAndAgenciesTest.java
@@ -22,10 +22,10 @@ public class AllModesAndAgenciesTest extends MmriTest {
     }
 
     public void test1a1() {
-        Leg[] legs = plan(+1388530800L, "1a1", "1a6", null, false, false, null, "", "", 5);
+        Leg[] legs = plan(+1388530800L, "1a1", "1a6", null, false, false, null, "", "", 3);
 
         validateLeg(legs[0], 1388530860000L, 1388530920000L, "1a2", "1a1", null);
-        validateLeg(legs[2], 1388530980000L, 1388531040000L, "1a4", "1a3", null);
-        validateLeg(legs[4], 1388531100000L, 1388531160000L, "1a6", "1a5", null);
+        validateLeg(legs[1], 1388530980000L, 1388531040000L, "1a4", "1a3", null);
+        validateLeg(legs[2], 1388531100000L, 1388531160000L, "1a6", "1a5", null);
     }
 }
