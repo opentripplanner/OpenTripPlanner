a621b332a5c59333420c6f07f953efa43d2e9d96
TeemuP <teemu.peltonen@cgi.com>
2019-02-07 18:50:44 +0200

    Add cause, effect and severityLevel

--- src/main/java/org/opentripplanner/routing/alertpatch/Alert.java
+++ src/main/java/org/opentripplanner/routing/alertpatch/Alert.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.routing.alertpatch;
 
+import com.google.transit.realtime.GtfsRealtime;
 import org.opentripplanner.util.I18NString;
 import org.opentripplanner.util.NonLocalizedString;
 
@@ -30,6 +31,15 @@ public class Alert implements Serializable {
     //null means unknown
     @XmlElement
     public Date effectiveEndDate;
+
+    @XmlElement
+    public GtfsRealtime.Alert.Effect effect;
+
+    @XmlElement
+    public GtfsRealtime.Alert.Cause cause;
+
+    @XmlElement
+    public GtfsRealtime.Alert.SeverityLevel severityLevel;
 
     public static HashSet<Alert> newSimpleAlertSet(String text) {
         Alert note = createSimpleAlerts(text);
