d952f751a2930539cd676d763618e7b2f00d70ef
Dimitar Kotevski <dko@unwire.com>
2019-02-12 12:08:51 +0100

    PR comments; Removed "Exiting..." from a log line. Added javadocs for the return value of the run() method

--- src/main/java/org/opentripplanner/standalone/OTPMain.java
+++ src/main/java/org/opentripplanner/standalone/OTPMain.java
@@ -96,6 +96,10 @@ public class OTPMain {
     /**
      * Making OTPMain a concrete class and placing this logic an instance method instead of embedding it in the static
      * main method makes it possible to build graphs from web services or scripts, not just from the command line.
+     *
+     * @return
+     *         true - if the OTPServer starts successfully. If "Run an OTP API server" has been requested, this method will return when the web server shuts down;
+     *         false - if an error occurs while loading the graph;
      */
     public boolean run() {
 
@@ -118,7 +122,7 @@ public class OTPMain {
                     graphService.registerGraph("", new MemoryGraphSource("", graph));
                 }
             } else {
-                LOG.error("An error occurred while building the graph. Exiting.");
+                LOG.error("An error occurred while building the graph.");
                 return false;
             }
         }
