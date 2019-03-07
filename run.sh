f964cc33cb510c9ecf1ca94bd11e2df7ce2ab8ce
robbi5 <maxi@richt.name>
2019-03-07 17:29:00 +0100

    Use mesos env variable for azure dns workaround

--- run.sh
+++ run.sh
@@ -83,8 +83,8 @@ function process {
 
 #workaround for azure DNS issue
 
-if [ "$EUID" -eq 0 ]
-  then echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
+if [ -n "$MESOS_CONTAINER_NAME"  ]; then 
+  echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
 fi
 
 VERSION=$(version)
