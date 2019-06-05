ad3d4aebe09a3a3c4de1af7fad42ec46524f29d9
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-06-05 15:53:09 +0200

    Added javadoc and unit test to FeedScopeIdFactory

--- src/main/java/org/opentripplanner/netex/mapping/FeedScopedIdFactory.java
+++ src/main/java/org/opentripplanner/netex/mapping/FeedScopedIdFactory.java
@@ -2,8 +2,12 @@ package org.opentripplanner.netex.mapping;
 
 import org.opentripplanner.model.FeedScopedId;
 
-// TODO OTP2 - JavaDoc needed
-// TODO OTP2 - Add Unit tests
+/**
+ * GTFS uses the term agency_id, which is used to scope the ids of all other elements in OTP. Since NeTEx does not
+ * contain this id and also assumes ids are already unique, it is necessary to specify an id to use in the OTP model.
+ * <p>
+ * This factory is used to set the feed id once and then apply it to elements as they are created.
+ */
 public class FeedScopedIdFactory {
     private static String feedId = "NETEX_AGENCY_ID_NOT_SET";
 
