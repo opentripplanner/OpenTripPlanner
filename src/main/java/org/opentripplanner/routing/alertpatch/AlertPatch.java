787fc66a233e4a952c576551da9a1bd1894618e0
Thomas Gran <t2gran@gmail.com>
2019-07-26 16:43:25 +0200

    Formated a few long test messages lines in AlertPatch.

--- src/main/java/org/opentripplanner/routing/alertpatch/AlertPatch.java
+++ src/main/java/org/opentripplanner/routing/alertpatch/AlertPatch.java
@@ -1,25 +1,28 @@
 package org.opentripplanner.routing.alertpatch;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.*;
-
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
+import org.opentripplanner.api.adapters.AgencyAndIdAdapter;
 import org.opentripplanner.model.Agency;
 import org.opentripplanner.model.FeedScopedId;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.Trip;
-import org.opentripplanner.api.adapters.AgencyAndIdAdapter;
 import org.opentripplanner.routing.core.State;
 import org.opentripplanner.routing.edgetype.TripPattern;
 import org.opentripplanner.routing.graph.Graph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This adds a note to all boardings of a given route or stop (optionally, in a given direction)
@@ -123,13 +126,19 @@ public class AlertPatch implements Serializable {
                     }
                     for (int i = 0; i < tripPattern.stopPattern.stops.length; i++) {
                         if (stop == null || stop.equals(tripPattern.stopPattern.stops[i])) {
-                            throw new UnsupportedOperationException("Cannot add Alert patch to Board/Alight edges - transit edges do not exist anymore under Raptor.");
+                            throw new UnsupportedOperationException(
+                                    "Cannot add Alert patch to Board/Alight edges - "
+                                    + "transit edges do not exist anymore under Raptor."
+                            );
                         }
                     }
                 }
             }
         } else if (stop != null) {
-            throw new UnsupportedOperationException("Cannot add alert to TransitStop - PreBoard and PreAlight edges no longer exist.");
+            throw new UnsupportedOperationException(
+                    "Cannot add alert to TransitStop - "
+                    + "PreBoard and PreAlight edges no longer exist."
+            );
         }
     }
 
@@ -169,13 +178,19 @@ public class AlertPatch implements Serializable {
                     }
                     for (int i = 0; i < tripPattern.stopPattern.stops.length; i++) {
                         if (stop == null || stop.equals(tripPattern.stopPattern.stops[i])) {
-                            throw new UnsupportedOperationException("Cannot remove Alert patch from Board/Alight edges - transit edges do not exist anymore under Raptor.");
+                            throw new UnsupportedOperationException(
+                                    "Cannot remove Alert patch from Board/Alight edges - "
+                                    + "transit edges do not exist anymore under Raptor."
+                            );
                         }
                     }
                 }
             }
         } else if (stop != null) {
-            throw new UnsupportedOperationException("Cannot remove alert from TransitStop - PreBoard and PreAlight edges no longer exist.");
+            throw new UnsupportedOperationException(
+                    "Cannot remove alert from TransitStop - "
+                    + "PreBoard and PreAlight edges no longer exist."
+            );
         }
     }
 
