3bfdc60c70a667d978f764f084f850ae6a0add85
Leonard Ehrenfried <mail@leonard.io>
2019-10-25 13:08:35 +0200

    Make sure that routes are computed in parallel

--- src/main/java/org/opentripplanner/routing/impl/ComparingGraphPathFinder.java
+++ src/main/java/org/opentripplanner/routing/impl/ComparingGraphPathFinder.java
@@ -10,11 +10,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * This variant of the GraphPathFinder "fans out" a routing request into potentially multiple ones and compares the
  * sets of results. It can then discard nonsensical or inferiour results.
- *
+ * <p>
  * This is useful for the mode PARK_RIDE because this mode forces you to drive to a P&R station and take public transport
  * even if it would be _a lot_ faster to just drive to the destination all the way.
  */
@@ -31,28 +34,31 @@ public class ComparingGraphPathFinder extends GraphPathFinder {
 
         List<GraphPath> results;
         if (options.parkAndRide) {
-            // TODO: need to find out how to run this in parallel
-
             LOG.info("Detected a P&R routing request. Will execute two requests to also get car-only routes.");
 
-            // first the normal P&R
-            List<GraphPath> parkAndRideResults = new GraphPathFinder(router).graphPathFinderEntryPoint(options);
+            // the normal P&R
+            CompletableFuture<List<GraphPath>> parkAndRideResults = CompletableFuture.supplyAsync(() -> new GraphPathFinder(router).graphPathFinderEntryPoint(options));
+            // car-only
+            CompletableFuture<List<GraphPath>> carOnlyResult = CompletableFuture.supplyAsync(() -> runCarOnlyRequest(options));
+            // the CompletableFutures are there to make sure that the computations run in parallel
+            results = Stream.of(parkAndRideResults, carOnlyResult)
+                    .map(CompletableFuture::join)
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
 
-            // now car-only
-            RoutingRequest carOnlyOptions = options.clone();
-            carOnlyOptions.parkAndRide = false;
-            carOnlyOptions.setModes(new TraverseModeSet(TraverseMode.CAR));
 
-            assert(carOnlyOptions.modes.getCar());
-            List<GraphPath> carOnlyResult = new GraphPathFinder(router).graphPathFinderEntryPoint(carOnlyOptions);
-
-            results = Lists.newLinkedList();
-            results.addAll(parkAndRideResults);
-            results.addAll(carOnlyResult);
         } else {
             results = super.graphPathFinderEntryPoint(options);
         }
 
         return results;
+    }
+
+    private List<GraphPath> runCarOnlyRequest(RoutingRequest originalOptions) {
+        RoutingRequest carOnlyOptions = originalOptions.clone();
+        carOnlyOptions.parkAndRide = false;
+        carOnlyOptions.setModes(new TraverseModeSet(TraverseMode.CAR));
+
+        return new GraphPathFinder(router).graphPathFinderEntryPoint(carOnlyOptions);
     }
 }
