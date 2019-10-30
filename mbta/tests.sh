51bbe34deb75e84826fa5641c867afc2b880d8bc
lboyarsky <45011335+lboyarsky@users.noreply.github.com>
2019-10-30 11:22:31 -0400

    Increasing memory allocation for tests (#29)

--- mbta/tests.sh
+++ mbta/tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-mvn -Dmaven.repo.local="${SEMAPHORE_CACHE_DIR}/.m2/" clean test -Dgpg.skip -Dmaven.javadoc.skip=true
+mvn -Dmaven.repo.local="${SEMAPHORE_CACHE_DIR:-~}/.m2/" clean test -Dgpg.skip -Dmaven.javadoc.skip=true -DargLine="-Xmx4G"
