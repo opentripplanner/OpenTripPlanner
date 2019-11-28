541532167c44faa1c3c78446d9c1bc8e2acf1f17
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-11-28 14:17:46 +0100

    Added imports

--- src/ext/java/org/opentripplanner/ext/transmodelapi/mapping/TransmodelMappingUtil.java
+++ src/ext/java/org/opentripplanner/ext/transmodelapi/mapping/TransmodelMappingUtil.java
@@ -16,6 +16,7 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.TimeZone;
 import java.util.stream.Collectors;
 
