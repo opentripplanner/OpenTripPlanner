7e93635f100aef7b2956ce3d8a80bf6bb7e84010
Thomas Gran <t2gran@gmail.com>
2020-02-12 22:04:23 +0100

    Cleanup JavaDoc on ItineraryFilter

--- src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryFilter.java
+++ src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryFilter.java
@@ -5,15 +5,9 @@ import org.opentripplanner.model.plan.Itinerary;
 import java.util.List;
 
 /**
- * Filter or decorate itineraries. A filter can modify the list passed into it and/or the elements
- * in the list, but should not delete elements from the list.
- * <p>
- * This function should not change the List instance passed in, but may change the elements in that
- * List, and may return a List with a subset of the elements (or even different, new elements).
- * This can be achieved using streams. Example:
- * <pre>
- * return itineraries.stream().filter(...).collect(Collectors.toList());
- * </pre>
+ * Filter or decorate itineraries. A filter can reorder, modify the list or the elements in the
+ * list. It is not recommended to change the list passed into the filter, instead make a copy,
+ * change it and return the copy. It is allowed to pass on an immutable list.
  * <p>
  * A filter should do one thing! For example do not change the itineraries and delete elements in
  * the same filter. Instead create two filters and insert them after each other in the filter chain.
@@ -21,20 +15,29 @@ import java.util.List;
  * {@link org.opentripplanner.routing.algorithm.filterchain.filters.MaxLimitFilter} work with
  * both a normal filter chain and the
  * {@link org.opentripplanner.routing.algorithm.filterchain.filters.DebugFilterChain}, and the
- * logic can be reused in several places. So, because of this, most filters do not need to account
- * for being in debug-mode.
+ * logic can be reused in several places. So, because of this, most filters can ignore the
+ * debug-mode.
  */
 public interface ItineraryFilter {
 
     /**
      * A name used for debugging the filter chain.
-     *
+     * <p>
      * Use '-' so separate words like: {@code sort-on-duration-filter}
      */
     String name();
 
     /**
      * Process the given itineraries returning the result.
+     * <p>
+     * This function should not change the List instance passed in, but may change the elements. It
+     * can return a List with a subset of the elements (or even different, new elements). Note! that
+     * the list passed into the filter might be immutable.
+     * <p>
+     * This can be achieved using streams. Example:
+     * <pre>
+     * return itineraries.stream().filter(...).collect(Collectors.toList());
+     * </pre>
      */
     List<Itinerary> filter(List<Itinerary> itineraries);
 }
