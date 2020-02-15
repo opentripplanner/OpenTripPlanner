8427deed44180b00a0aaddcc00b5f64a2b8e45a8
Evan Siroky <evan.siroky@yahoo.com>
2020-02-14 16:45:51 -0800

    Only try to parse elevation cache if cacheElevations is true

--- src/main/java/org/opentripplanner/graph_builder/module/ned/ElevationModule.java
+++ src/main/java/org/opentripplanner/graph_builder/module/ned/ElevationModule.java
@@ -147,19 +147,15 @@ public class ElevationModule implements GraphBuilderModule {
         }
 
         // try to load in the cached elevation data
-        try {
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(cachedElevationsFile));
-            cachedElevations = (HashMap<String, PackedCoordinateSequence>) in.readObject();
-            log.info("Cached elevation data loaded into memory!");
-        } catch (IOException | ClassNotFoundException e) {
-            issueStore.add(
-                new Graphwide(
-                    String.format(
-                        "Cached elevations file could not be read in due to error: %s!",
-                        e.getMessage()
-                    )
-                )
-            );
+        if (cacheElevations) {
+            try {
+                ObjectInputStream in = new ObjectInputStream(new FileInputStream(cachedElevationsFile));
+                cachedElevations = (HashMap<String, PackedCoordinateSequence>) in.readObject();
+                log.info("Cached elevation data loaded into memory!");
+            } catch (IOException | ClassNotFoundException e) {
+                issueStore.add(new Graphwide(
+                    String.format("Cached elevations file could not be read in due to error: %s!", e.getMessage())));
+            }
         }
         log.info("Setting street elevation profiles from digital elevation model...");
 
