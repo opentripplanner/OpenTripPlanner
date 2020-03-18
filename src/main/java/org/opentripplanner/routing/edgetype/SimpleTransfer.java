e8f0890c0325b006ae7e22e902bee9c7dd9f7295
Hannes Junnila <hannes@kyyti.com>
2020-03-18 11:45:37 +0200

    Move SimpleTransfers from being edges to graph field

--- src/main/java/org/opentripplanner/routing/edgetype/SimpleTransfer.java
+++ src/main/java/org/opentripplanner/routing/edgetype/SimpleTransfer.java
@@ -1,95 +1,43 @@
 package org.opentripplanner.routing.edgetype;
 
-import org.locationtech.jts.geom.LineString;
-import org.opentripplanner.common.geometry.GeometryUtils;
-import org.opentripplanner.routing.core.RoutingRequest;
-import org.opentripplanner.routing.core.State;
-import org.opentripplanner.routing.core.StateEditor;
-import org.opentripplanner.routing.core.TraverseMode;
+import org.opentripplanner.model.Stop;
 import org.opentripplanner.routing.graph.Edge;
-import org.opentripplanner.routing.vertextype.TransitStopVertex;
 
+import java.io.Serializable;
 import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
 
 /**
  * Represents a transfer between stops that does not take the street network into account.
  *
  * TODO these should really have a set of valid modes in case bike vs. walk transfers are different
  */
-public class SimpleTransfer extends Edge {
-    private static final long serialVersionUID = 20140408L;
+public class SimpleTransfer implements Serializable {
+    private static final long serialVersionUID = 20200316L;
+    public final Stop from;
+    public final Stop to;
 
     private double effectiveWalkDistance;
     
     private List<Edge> edges;
 
-    public SimpleTransfer(TransitStopVertex from, TransitStopVertex to, double effectiveWalkDistance, List<Edge> edges) {
-        super(from, to);
+    public SimpleTransfer(Stop from, Stop to, double effectiveWalkDistance, List<Edge> edges) {
+        this.from = from;
+        this.to = to;
         this.effectiveWalkDistance = effectiveWalkDistance;
         this.edges = edges;
     }
 
-    @Override
-    public State traverse(State s0) {
-        // Forbid taking shortcuts composed of two transfers in a row
-        if (s0.backEdge instanceof SimpleTransfer) {
-            return null;
-        }
-        if (s0.backEdge instanceof StreetTransitLink) {
-            return null;
-        }
-        if(effectiveWalkDistance > s0.getOptions().maxTransferWalkDistance) {
-            return null;
-        }
-        // Only transfer right after riding a vehicle.
-        RoutingRequest rr = s0.getOptions();
-        double walkspeed = rr.walkSpeed;
-        StateEditor se = s0.edit(this);
-        se.setBackMode(TraverseMode.WALK);
-        int time = (int) Math.ceil(effectiveWalkDistance / walkspeed) + 2 * StreetTransitLink.STL_TRAVERSE_COST;
-        se.incrementTimeInSeconds(time);
-        se.incrementWeight(time * rr.walkReluctance);
-        se.incrementWalkDistance(effectiveWalkDistance);
-        return se.makeState();
-    }
-
-    @Override
     public String getName() {
-        return fromv.getName() + " => " + tov.getName();
-    }
-    
-    @Override
-    public String getName(Locale locale) {
-        //TODO: localize
-        return this.getName();
+        return from + " => " + to;
     }
 
-    @Override
-    public double weightLowerBound(RoutingRequest rr) {
-        int time = (int) (effectiveWalkDistance / rr.walkSpeed);
-        return (time * rr.walkReluctance);
-    }
-
-    @Override
     public double getDistanceMeters() {
         return edges.stream().mapToDouble(Edge::getDistanceMeters).sum();
     }
 
-    @Override
     public double getEffectiveWalkDistance(){
     	return this.effectiveWalkDistance;
     }
-
-    @Override
-    public LineString getGeometry(){
-        return GeometryUtils.concatenateLineStrings(
-                edges.stream()
-                        .filter(t -> t.getGeometry() != null)
-                        .map(Edge::getGeometry)
-                        .collect(Collectors.toList()));
-   }
 
     public List<Edge> getEdges() { return this.edges; }
 
