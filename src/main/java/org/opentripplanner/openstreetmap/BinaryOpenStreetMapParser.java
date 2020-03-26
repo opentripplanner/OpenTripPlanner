9b3dbf0dbce98396defc1cbfa6c6b08d508046bb
Jay Ni <lbt0506@gmail.com>
2020-03-26 23:19:59 +1300

    use another lib to parse osm.pbf but looks like osm nodes can't be read

--- src/main/java/org/opentripplanner/openstreetmap/BinaryOpenStreetMapParser.java
+++ src/main/java/org/opentripplanner/openstreetmap/BinaryOpenStreetMapParser.java
@@ -1,228 +1,165 @@
 package org.opentripplanner.openstreetmap;
 
+import de.topobyte.osm4j.core.access.OsmHandler;
+import de.topobyte.osm4j.core.access.OsmInputException;
+import de.topobyte.osm4j.core.model.iface.EntityType;
+import de.topobyte.osm4j.core.model.iface.OsmBounds;
+import de.topobyte.osm4j.core.model.iface.OsmNode;
+import de.topobyte.osm4j.core.model.iface.OsmRelation;
+import de.topobyte.osm4j.core.model.iface.OsmRelationMember;
+import de.topobyte.osm4j.core.model.iface.OsmTag;
+import de.topobyte.osm4j.core.model.iface.OsmWay;
+import de.topobyte.osm4j.pbf.seq.PbfReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
 import org.opentripplanner.graph_builder.module.osm.OSMDatabase;
-import org.opentripplanner.openstreetmap.model.*;
+import org.opentripplanner.openstreetmap.model.OSMNode;
+import org.opentripplanner.openstreetmap.model.OSMNodeRef;
+import org.opentripplanner.openstreetmap.model.OSMRelation;
+import org.opentripplanner.openstreetmap.model.OSMRelationMember;
+import org.opentripplanner.openstreetmap.model.OSMTag;
+import org.opentripplanner.openstreetmap.model.OSMWay;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class BinaryOpenStreetMapParser implements OsmHandler {
 
-import crosby.binary.BinaryParser;
-import crosby.binary.Osmformat;
+  OSMDatabase osmdb;
+  private boolean parseWays = true;
+  private boolean parseRelations = true;
+  private boolean parseNodes = true;
+  private InputStream inputStream;
 
-/**
- * Parser for the OpenStreetMap PBF Format.
- *
- * @since 0.4
- */
-public class BinaryOpenStreetMapParser extends BinaryParser {
+  public BinaryOpenStreetMapParser(OSMDatabase osmdb) {
+    this.osmdb = osmdb;
+  }
 
-    private OSMDatabase osmdb;
-    private boolean parseWays = true;
-    private boolean parseRelations = true;
-    private boolean parseNodes = true;
-    private Map<String, String> stringTable = new HashMap<String, String>();
+  public void process(FileInputStream inputStream) throws OsmInputException {
+    this.inputStream = inputStream;
+    PbfReader reader = new PbfReader(inputStream, true);
+    reader.setHandler(this);
+    reader.read();
+  }
 
-    public BinaryOpenStreetMapParser(OSMDatabase osmdb) {
-        this.osmdb = osmdb;
+  /**
+   * Should relations be parsed
+   */
+  public void setParseWays(boolean parseWays) {
+    this.parseWays = parseWays;
+  }
+
+  /**
+   * Should relations be parsed
+   */
+  public void setParseRelations(boolean parseRelations) {
+    this.parseRelations = parseRelations;
+  }
+
+  /**
+   * Should nodes be parsed
+   */
+  public void setParseNodes(boolean parseNodes) {
+    this.parseNodes = parseNodes;
+  }
+
+  @Override
+  public void handle(OsmBounds osmBounds) throws IOException {
+
+  }
+
+  int nodeCount = 0;
+
+  @Override
+  public void handle(OsmNode osmNode) throws IOException {
+    if (!parseNodes) {
+      return;
+    }
+    OSMNode tmp = new OSMNode();
+    tmp.setId(osmNode.getId());
+    tmp.lat = osmNode.getLatitude();
+    tmp.lon = osmNode.getLongitude();
+    for (int i = 0; i < osmNode.getNumberOfTags(); i++) {
+      OSMTag tag = new OSMTag();
+      OsmTag osmTag = osmNode.getTag(i);
+      tag.setK(osmTag.getKey());
+      tag.setV(osmTag.getValue());
+      tmp.addTag(tag);
     }
 
-    // The strings are already being pulled from a string table in the PBF file,
-    // but there appears to be a separate string table per 8k-entry PBF file block.
-    // String.intern grinds to a halt on large PBF files (as it did on GTFS import), so 
-    // we implement our own. 
-    public String internalize(String s) {
-        String fromTable = stringTable.get(s);
-        if (fromTable == null) {
-            stringTable.put(s, s);
-            return s;
-        } 
-        return fromTable;
+    osmdb.addNode(tmp);
+    nodeCount++;
+  }
+
+  @Override
+  public void handle(OsmWay osmWay) throws IOException {
+    if (!parseWays) {
+      return;
     }
 
-    public void complete() {
-        // Jump in circles
+    OSMWay tmp = new OSMWay();
+    tmp.setId(osmWay.getId());
+
+    for (int index = 0; index < osmWay.getNumberOfTags(); index++) {
+      OSMTag tag = new OSMTag();
+      OsmTag osmTag = osmWay.getTag(index);
+
+      tag.setK(osmTag.getKey());
+      tag.setV(osmTag.getValue());
+      tmp.addTag(tag);
     }
 
-    @Override
-    protected void parseNodes(List<Osmformat.Node> nodes) {
-        if(!parseNodes) {
-            return;
-        }
+    for (int index = 0; index < osmWay.getNumberOfNodes(); index++) {
+      OSMNodeRef nodeRef = new OSMNodeRef();
+      nodeRef.setRef(osmWay.getNodeId(index));
+    }
+    osmdb.addWay(tmp);
+  }
 
-        for (Osmformat.Node i : nodes) {
-            OSMNode tmp = new OSMNode();
-            tmp.setId(i.getId());
-            tmp.lat = parseLat(i.getLat());
-            tmp.lon = parseLon(i.getLon());
+  @Override
+  public void handle(OsmRelation osmRelation) throws IOException {
+    if (!parseRelations) {
+      return;
+    }
+    OSMRelation tmp = new OSMRelation();
+    tmp.setId(osmRelation.getId());
 
-            for (int j = 0; j < i.getKeysCount(); j++) {
-                String key = internalize(getStringById(i.getKeys(j)));
-                // if handler.retain_tag(key) // TODO: filter tags
-                String value = internalize(getStringById(i.getVals(j)));
-                OSMTag tag = new OSMTag();
-                tag.setK(key);
-                tag.setV(value);
-                tmp.addTag(tag);
-            }
-
-            osmdb.addNode(tmp);
-        }
+    for (int i = 0; i < osmRelation.getNumberOfTags(); i++) {
+      OSMTag tag = new OSMTag();
+      OsmTag osmTag = osmRelation.getTag(i);
+      tag.setK(osmTag.getKey());
+      tag.setV(osmTag.getValue());
+      tmp.addTag(tag);
     }
 
-    @Override
-    protected void parseDense(Osmformat.DenseNodes nodes) {
-        long lastId = 0, lastLat = 0, lastLon = 0;
-        int j = 0; // Index into the keysvals array.
-
-        if(!parseNodes) {
-            return;
-        }
-
-        for (int i = 0; i < nodes.getIdCount(); i++) {
-            OSMNode tmp = new OSMNode();
-
-            long lat = nodes.getLat(i) + lastLat;
-            lastLat = lat;
-            long lon = nodes.getLon(i) + lastLon;
-            lastLon = lon;
-            long id = nodes.getId(i) + lastId;
-            lastId = id;
-            double latf = parseLat(lat), lonf = parseLon(lon);
-
-            tmp.setId(id);
-            tmp.lat = latf;
-            tmp.lon = lonf;
-
-            // If empty, assume that nothing here has keys or vals.
-            if (nodes.getKeysValsCount() > 0) {
-                while (nodes.getKeysVals(j) != 0) {
-                    int keyid = nodes.getKeysVals(j++);
-                    int valid = nodes.getKeysVals(j++);
-
-                    OSMTag tag = new OSMTag();
-                    String key = internalize(getStringById(keyid));
-                    String value = internalize(getStringById(valid));
-                    tag.setK(key);
-                    tag.setV(value);
-                    tmp.addTag(tag);
-                }
-                j++; // Skip over the '0' delimiter.
-            }
-
-            osmdb.addNode(tmp);
-        }
+    for (int i = 0; i < osmRelation.getNumberOfMembers(); i++) {
+      OsmRelationMember m = osmRelation.getMember(i);
+      OSMRelationMember member = new OSMRelationMember();
+      setRelationMemberTypeString(member, m.getType());
+      member.setRole(m.getRole());
+      member.setRef(m.getId());
     }
+  }
 
-    @Override
-    protected void parseWays(List<Osmformat.Way> ways) {
-        if(!parseWays) {
-            return;
-        }
-
-        for (Osmformat.Way i : ways) {
-            OSMWay tmp = new OSMWay();
-            tmp.setId(i.getId());
-
-            for (int j = 0; j < i.getKeysCount(); j++) {
-                OSMTag tag = new OSMTag();
-                String key = internalize(getStringById(i.getKeys(j)));
-                String value = internalize(getStringById(i.getVals(j)));
-                tag.setK(key);
-                tag.setV(value);
-                tmp.addTag(tag);
-            }
-
-            long lastId = 0;
-            for (long j : i.getRefsList()) {
-                OSMNodeRef nodeRef = new OSMNodeRef();
-                nodeRef.setRef(j + lastId);
-                tmp.addNodeRef(nodeRef);
-
-                lastId = j + lastId;
-            }
-
-            osmdb.addWay(tmp);
-        }
+  public void complete() {
+    // Jump in circles
+    if (inputStream != null) {
+      IOUtils.closeQuietly(inputStream);
     }
+  }
 
-    @Override
-    protected void parseRelations(List<Osmformat.Relation> rels) {
-        if(!parseRelations) {
-            return;
-        }
-
-        for (Osmformat.Relation i : rels) {
-            OSMRelation tmp = new OSMRelation();
-            tmp.setId(i.getId());
-
-            for (int j = 0; j < i.getKeysCount(); j++) {
-                OSMTag tag = new OSMTag();
-                String key = internalize(getStringById(i.getKeys(j)));
-                String value = internalize(getStringById(i.getVals(j)));
-                tag.setK(key);
-                tag.setV(value);
-                tmp.addTag(tag);
-            }
-
-            long lastMid = 0;
-            for (int j = 0; j < i.getMemidsCount(); j++) {
-                OSMRelationMember relMember = new OSMRelationMember();
-                long mid = lastMid + i.getMemids(j);
-
-                relMember.setRef(mid);
-                lastMid = mid;
-
-                relMember.setRole(internalize(getStringById(i.getRolesSid(j))));
-
-                if (i.getTypes(j) == Osmformat.Relation.MemberType.NODE) {
-                    relMember.setType("node");
-                } else if (i.getTypes(j) == Osmformat.Relation.MemberType.WAY) {
-                    relMember.setType("way");
-                } else if (i.getTypes(j) == Osmformat.Relation.MemberType.RELATION) {
-                    relMember.setType("relation");
-                } else {
-                    assert false; // TODO; Illegal file?
-                }
-
-                tmp.addMember(relMember);
-            }
-
-            osmdb.addRelation(tmp);
-        }
+  private void setRelationMemberTypeString(OSMRelationMember m, EntityType type) {
+    switch (type) {
+      case Way:
+        m.setType("way");
+        break;
+      case Relation:
+        m.setType("relation");
+        break;
+      case Node:
+        m.setType("node");
+        break;
+      default:
+        assert false;
     }
-
-    @Override
-    public void parse(Osmformat.HeaderBlock block) {
-        for (String s : block.getRequiredFeaturesList()) {
-            if (s.equals("OsmSchema-V0.6")) {
-                continue; // We can parse this.
-            }
-            if (s.equals("DenseNodes")) {
-                continue; // We can parse this.
-            }
-            throw new IllegalStateException("File requires unknown feature: " + s);
-        }
-    }
-
-    /**
-     * Should relations be parsed
-     */
-    public void setParseWays(boolean parseWays) {
-        this.parseWays = parseWays;
-    }
-
-    /**
-     * Should relations be parsed
-     */
-    public void setParseRelations(boolean parseRelations) {
-        this.parseRelations = parseRelations;
-    }
-
-    /**
-     * Should nodes be parsed
-     */
-    public void setParseNodes(boolean parseNodes) {
-        this.parseNodes = parseNodes;
-    }
+  }
 }
