e0b3127e69f55481a3d098f1d8eb7dd47b1d4a54
Evan Siroky <evan.siroky@yahoo.com>
2020-04-04 18:00:24 -0700

    Various improvements to elevation module
    - Add comment about validity of cached elevations based on input data
    - Go back to failing hard when downloading NED tiles except for when a tile is missing in the s3 bucket
    - Refactor so that Geotiff files are only loaded into GridCoverage2D instances once, but use multiple Interpolator2D instances

--- src/main/java/org/opentripplanner/graph_builder/module/ned/GeotiffGridCoverageFactoryImpl.java
+++ src/main/java/org/opentripplanner/graph_builder/module/ned/GeotiffGridCoverageFactoryImpl.java
@@ -2,7 +2,6 @@ package org.opentripplanner.graph_builder.module.ned;
 
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.util.factory.Hints;
-import org.geotools.coverage.grid.Interpolator2D;
 import org.geotools.gce.geotiff.GeoTiffFormat;
 import org.geotools.gce.geotiff.GeoTiffReader;
 import org.opentripplanner.datastore.FileType;
@@ -13,7 +12,6 @@ import org.opentripplanner.datastore.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.media.jai.InterpolationBilinear;
 import java.io.File;
 import java.io.IOException;
 
@@ -35,23 +33,28 @@ public class GeotiffGridCoverageFactoryImpl implements ElevationGridCoverageFact
         this(new FileDataSource(path, FileType.DEM));
     }
 
+    /**
+     * Lazy-creates a GridCoverage2D instance by loading the specific elevation file into memory. During a refactor in
+     * the year 2020, the code at one point was written such that each coverage instance was created and wrapped in the
+     * Interpolator2D interpolator for each thread to use. However, benchmarking showed that this caused longer run
+     * times which is likely due to too much memory competing for a slot in the processor cache.
+     */
     @Override
     public GridCoverage2D getGridCoverage() {
-        GridCoverage2D coverage;
-        try {
-            // There is a serious standardization failure around the axis order of WGS84. See issue #1930.
-            // GeoTools assumes strict EPSG axis order of (latitude, longitude) unless told otherwise.
-            // Both NED and SRTM data use the longitude-first axis order, so OTP makes grid coverages
-            // for unprojected DEMs assuming coordinates are in (longitude, latitude) order.
-            Hints forceLongLat = new Hints(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE);
-            GeoTiffFormat format = new GeoTiffFormat();
-            GeoTiffReader reader = format.getReader(getSource(), forceLongLat);
-            coverage = reader.read(null);
-            LOG.debug("Elevation model CRS is: {}", coverage.getCoordinateReferenceSystem2D());
-            // TODO might bicubic interpolation give better results?
-            coverage = Interpolator2D.create(coverage, new InterpolationBilinear());
-        } catch (IOException e) {
-            throw new RuntimeException("Error getting coverage automatically. ", e);
+        if (coverage == null) {
+            try {
+                // There is a serious standardization failure around the axis order of WGS84. See issue #1930.
+                // GeoTools assumes strict EPSG axis order of (latitude, longitude) unless told otherwise.
+                // Both NED and SRTM data use the longitude-first axis order, so OTP makes grid coverages
+                // for unprojected DEMs assuming coordinates are in (longitude, latitude) order.
+                Hints forceLongLat = new Hints(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE);
+                GeoTiffFormat format = new GeoTiffFormat();
+                GeoTiffReader reader = format.getReader(getSource(), forceLongLat);
+                coverage = reader.read(null);
+                LOG.debug("Elevation model CRS is: {}", coverage.getCoordinateReferenceSystem2D());
+            } catch (IOException e) {
+                throw new RuntimeException("Error getting coverage automatically. ", e);
+            }
         }
         return coverage;
     }
@@ -67,9 +70,9 @@ public class GeotiffGridCoverageFactoryImpl implements ElevationGridCoverageFact
         }
     }
 
+    /**
+     * Nothing to do here. File should already exist on computer.
+     */
     @Override
-    public void fetchData(Graph graph) {
-        //nothing to do here
-    }
-
+    public void fetchData(Graph graph) { }
 }
