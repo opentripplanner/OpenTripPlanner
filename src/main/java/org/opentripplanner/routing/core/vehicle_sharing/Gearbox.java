04800d8fd02a3801dff0a28257289b27b75b5910
Adam Wiktor <a.wiktor@vooom.pl>
2020-04-09 14:41:47 +0200

    Rework vehicles description

--- src/main/java/org/opentripplanner/routing/core/vehicle_sharing/Gearbox.java
+++ src/main/java/org/opentripplanner/routing/core/vehicle_sharing/Gearbox.java
@@ -1,7 +1,20 @@
 package org.opentripplanner.routing.core.vehicle_sharing;
 
+import java.util.Locale;
+
 public enum Gearbox {
 
     MANUAL,
-    AUTOMAT
+    AUTOMATIC;
+
+    public static Gearbox fromString(String gearbox) {
+        if (gearbox == null) {
+            return null;
+        }
+        try {
+            return Gearbox.valueOf(gearbox.toUpperCase(Locale.US));
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
 }
