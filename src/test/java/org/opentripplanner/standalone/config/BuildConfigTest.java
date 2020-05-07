87be33c94fd0d708c20336d049d72a49d3392e2c
Jay Ni <lbt0506@gmail.com>
2020-05-07 12:28:41 +1200

    fix a test

--- src/test/java/org/opentripplanner/standalone/config/BuildConfigTest.java
+++ src/test/java/org/opentripplanner/standalone/config/BuildConfigTest.java
@@ -1,13 +1,23 @@
 package org.opentripplanner.standalone.config;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import java.io.IOException;
+import org.junit.Before;
 import org.junit.Test;
+import org.opentripplanner.annotation.ComponentAnnotationConfigurator;
 
 import static org.junit.Assert.assertTrue;
 import static org.opentripplanner.standalone.config.JsonSupport.jsonNodeForTest;
 
 
 public class BuildConfigTest {
+
+    @Before
+    public void setUp() throws IOException {
+        ComponentAnnotationConfigurator.getInstance().fromConfig(null);
+        // Create a base path for this test - correspond to OTP BASE_PATH
+    }
+
     @Test
     public void testParsePeriodDate() {
         // Given
