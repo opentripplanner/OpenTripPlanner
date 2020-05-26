4a8abf8e574cdc576eb5079d78e9d32367bcab29
mironszewczyk <m.szewczyk@vooom.pl>
2020-05-26 19:46:30 +0200

    Now kickscooters won't travel further than 4km. Further discussion needed.

--- src/main/java/org/opentripplanner/routing/core/vehicle_sharing/KickScooterDescription.java
+++ src/main/java/org/opentripplanner/routing/core/vehicle_sharing/KickScooterDescription.java
@@ -3,8 +3,9 @@ package org.opentripplanner.routing.core.vehicle_sharing;
 import org.opentripplanner.routing.core.TraverseMode;
 import org.opentripplanner.routing.edgetype.StreetEdge;
 
-public class KickScooterDescription extends VehicleDescription {
+import static java.lang.Double.min;
 
+public class KickScooterDescription extends VehicleDescription {
     private static final double MAX_SPEED_IN_METERS_PER_SECOND_ON_BIKEPATH = 15. * (10. / 36.);
     private static final double MAX_SPEED_IN_METERS_PER_SECOND_ON_PEDESTRIAN_PATH = 10. * (10. / 36.);
 
@@ -15,11 +16,12 @@ public class KickScooterDescription extends VehicleDescription {
     private static final int DROPOFF_TIME_IN_SECONDS = 30;
 
     private static final VehicleType VEHICLE_TYPE = VehicleType.KICKSCOOTER;
-    private static final double DEFAULT_RANGE_IN_METERS = 16 * 1000;
+    //  We don't want to return routes with long kickscooter legs.
+    private static final double DEFAULT_RANGE_IN_METERS = 4 * 1000;
 
     public KickScooterDescription(String providerVehicleId, double longitude, double latitude, FuelType fuelType,
                                   Gearbox gearbox, Provider provider, Double rangeInMeters) {
-        super(providerVehicleId, longitude, latitude, fuelType, gearbox, provider, rangeInMeters);
+        super(providerVehicleId, longitude, latitude, fuelType, gearbox, provider, min(rangeInMeters, DEFAULT_RANGE_IN_METERS));
     }
 
     public KickScooterDescription(String providerVehicleId, double longitude, double latitude, FuelType fuelType,
