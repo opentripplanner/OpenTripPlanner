ab73bae14d321838900635b6901b2bbeac4c8463
Jay Ni <lbt0506@gmail.com>
2020-06-09 14:46:39 +1200

    resolve conflict

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -292,34 +292,6 @@ i18n.init(options, function(t) {
 });
 
 otp.config.modes = {
-<<<<<<< HEAD
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "TRANSIT,WALK"        : _tr("Transit"),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "BUS,WALK"         : _tr("Bus Only"),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "TRAM,RAIL,SUBWAY,FUNICULAR,GONDOLA,WALK"       : _tr("Rail Only"),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "AIRPLANE,WALK"       : _tr("Airplane Only"),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "BICYCLE"             : _tr('Bicycle Only'),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "TRANSIT,BICYCLE"     : _tr("Bicycle &amp; Transit"),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "WALK"                : _tr('Walk Only'),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-        "CAR"                 : _tr('Drive Only'),
-    //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel
-    //Options widgets)
-=======
     //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel Options widgets)
     "TRANSIT,WALK"        : _tr("Transit"),
     //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel Options widgets)
@@ -339,7 +311,6 @@ otp.config.modes = {
     //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel Options widgets)
     "CAR"                 : _tr('Drive Only'),
     //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel Options widgets)
->>>>>>> otp/dev-2.x
     "CAR_PARK,WALK,TRANSIT"     : _tr('Park and Ride'),
     //TRANSLATORS: Travel by: mode of transport (Used in selection in Travel Options widgets)
     "CAR,WALK,TRANSIT"          : _tr('Kiss and Ride'),
