9251a47955cb10d58f38f6891fb0f349100f2838
Paul Swartz <pswartz@mbta.com>
2020-07-07 09:55:51 -0400

    fix: update build scripts (#33)
    - .tool-versions for ASDF
    - `server.sh` also takes the router as an argument
    - `SEMAPHORE_CACHE_DIR` is not required: defaults to $HOME
    - update URL for OSGeo repository

--- mbta/build.sh
+++ mbta/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-mvn -Dmaven.repo.local="${SEMAPHORE_CACHE_DIR}/.m2/" clean install -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
+mvn -Dmaven.repo.local="${SEMAPHORE_CACHE_DIR:-$HOME}/.m2/" clean install -U -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
 cp ./target/otp-1.4.0-SNAPSHOT-shaded.jar .
 java -Xmx8G -jar otp-1.4.0-SNAPSHOT-shaded.jar --build var/graphs/mbta/ --basePath var/
--- mbta/server.sh
+++ mbta/server.sh
@@ -3,4 +3,4 @@ IN_MEMORY="--inMemory"
 if [ -f "var/graphs/mbta/Graph.obj" ]; then
     IN_MEMORY=""
 fi
-java -Xmx4G -jar otp-1.4.0-SNAPSHOT-shaded.jar --server --basePath var/ $IN_MEMORY
+java -Xmx4G -jar otp-1.4.0-SNAPSHOT-shaded.jar --server --basePath var/ $IN_MEMORY --router mbta
