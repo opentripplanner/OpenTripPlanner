574bc2dca0f411fbe160f0818a859e612d73a6dd
Joshua Fabian <jfabian@mbta.com>
2020-07-13 16:40:56 -0400

    chore: Use google_transit GTFS feed (#36)

--- mbta/update_gtfs.sh
+++ mbta/update_gtfs.sh
@@ -4,5 +4,5 @@ set -e
 # it's important for MBTA_GFTS to be first file in the folder,
 # otherwise realtime alerts won't work: https://github.com/mbta/OpenTripPlanner/pull/8
 
-wget -N https://mbta-gtfs-s3.s3.amazonaws.com/open_trip_planner.zip -O var/graphs/mbta/1_MBTA_GTFS.zip
+wget -N https://mbta-gtfs-s3.s3.amazonaws.com/google_transit.zip -O var/graphs/mbta/1_MBTA_GTFS.zip
 wget -N http://www.massport.com/media/4109/massport_gtfs.zip -O var/graphs/mbta/2_loganexpress-ma-us.zip
