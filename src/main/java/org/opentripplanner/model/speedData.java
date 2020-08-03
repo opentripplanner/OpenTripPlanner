96740d7a994a6bd3590094417d3a94ab51d4c1db
fenix7 <mk221123@gmail.com>
2020-08-03 21:18:10 +0200

    Create speedData.java

---
+++ src/main/java/org/opentripplanner/model/speedData.java
@@ -0,0 +1,40 @@
+public class speedData {
+    private long  clusterid;
+      private  int     current_speed;
+       private int id;
+
+    public long getClusterid() {
+        return clusterid;
+    }
+
+    public void setClusterid1(long clusterid1) {
+        this.clusterid = clusterid1;
+    }
+
+    public int getCurrent_speed() {
+        return current_speed;
+    }
+
+    public void setCurrent_speed(int current_speed) {
+        this.current_speed = current_speed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMeasure_time() {
+        return measure_time;
+    }
+
+    public void setMeasure_time(String measure_time) {
+        this.measure_time = measure_time;
+    }
+
+    private   String  measure_time;
+}
+
