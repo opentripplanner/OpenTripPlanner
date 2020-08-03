23f9d570d0ae21cf772aa99b8deb3f8cd21e38c9
fenix7 <mk221123@gmail.com>
2020-08-03 20:47:41 +0200

    Create edgeDara.java

---
+++ src/main/java/org/opentripplanner/model/edgeDara.java
@@ -0,0 +1,83 @@
+public class edegData {
+           private int clusterid;
+           private long endid;
+           private double   endlatitude;
+           private long  endlongitude;
+           private long id;
+           private double  startlatitude;
+           private  long  startlongitude;
+           private long startnodeid;
+           private  long  wayid;
+
+    public int getClusterid() {
+        return clusterid;
+    }
+
+    public void setClusterid(int clusterid) {
+        this.clusterid = clusterid;
+    }
+
+    public long getEndid() {
+        return endid;
+    }
+
+    public void setEndid(long endid) {
+        this.endid = endid;
+    }
+
+    public double getEndlatitude() {
+        return endlatitude;
+    }
+
+    public void setEndlatitude(double endlatitude) {
+        this.endlatitude = endlatitude;
+    }
+
+    public long getEndlongitude() {
+        return endlongitude;
+    }
+
+    public void setEndlongitude(long endlongitude) {
+        this.endlongitude = endlongitude;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getStartlatitude() {
+        return startlatitude;
+    }
+
+    public void setStartlatitude(double startlatitude) {
+        this.startlatitude = startlatitude;
+    }
+
+    public long getStartlongitude() {
+        return startlongitude;
+    }
+
+    public void setStartlongitude(long startlongitude) {
+        this.startlongitude = startlongitude;
+    }
+
+    public long getStartnodeid() {
+        return startnodeid;
+    }
+
+    public void setStartnodeid(long startnodeid) {
+        this.startnodeid = startnodeid;
+    }
+
+    public long getWayid() {
+        return wayid;
+    }
+
+    public void setWayid(long wayid) {
+        this.wayid = wayid;
+    }
+}
