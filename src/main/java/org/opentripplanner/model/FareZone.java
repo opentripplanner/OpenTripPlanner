5fb603f96a6f6c6f26a33e74648dc948f83f1ae0
Gard Mellemstrand <gard.mellemstrand@entur.org>
2020-08-12 11:18:48 +0200

    Add tarrifZones field to StopPlace in transmodel api

--- src/main/java/org/opentripplanner/model/FareZone.java
+++ src/main/java/org/opentripplanner/model/FareZone.java
@@ -1,5 +1,7 @@
 package org.opentripplanner.model;
 
+import java.util.Objects;
+
 public class FareZone extends TransitEntity<FeedScopedId> {
 
   private final FeedScopedId id;
@@ -22,5 +24,19 @@ public class FareZone extends TransitEntity<FeedScopedId> {
 
   public String getName() {
     return name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) { return true; }
+    if (o == null || getClass() != o.getClass()) { return false; }
+    if (!super.equals(o)) { return false; }
+    FareZone fareZone = (FareZone) o;
+    return Objects.equals(id, fareZone.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), id);
   }
 }
