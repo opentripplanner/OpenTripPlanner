88d6713c4bac0d7b04c7832addfdbfc212068aae
Gard Mellemstrand <gard.mellemstrand@entur.org>
2020-08-27 11:54:03 +0200

    Override getName method for TransitStopVertex

--- src/main/java/org/opentripplanner/routing/vertextype/TransitStopVertex.java
+++ src/main/java/org/opentripplanner/routing/vertextype/TransitStopVertex.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 
@@ -88,6 +89,11 @@ public class TransitStopVertex extends Vertex {
 
     public Stop getStop() {
             return this.stop;
+    }
+
+    @Override
+    public String getName(Locale locale) {
+        return this.stop.getName();
     }
 
     @Override
