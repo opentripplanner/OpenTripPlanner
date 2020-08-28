fb573433dbebe93e81c2d084448d78f0e11c064b
AdamWiktor <60605955+AdamWiktor@users.noreply.github.com>
2020-08-28 11:27:11 +0200

    Merge parking zones info into single class (#84)
    * Merge parking zones info into single class
    
    * Serializing parking zones

--- src/test/java/org/opentripplanner/routing/edgetype/rentedgetype/DropoffVehicleEdgeTest.java
+++ src/test/java/org/opentripplanner/routing/edgetype/rentedgetype/DropoffVehicleEdgeTest.java
@@ -3,16 +3,16 @@ package org.opentripplanner.routing.edgetype.rentedgetype;
 import org.junit.Before;
 import org.junit.Test;
 import org.opentripplanner.routing.core.*;
-import org.opentripplanner.routing.core.vehicle_sharing.*;
+import org.opentripplanner.routing.core.vehicle_sharing.CarDescription;
+import org.opentripplanner.routing.core.vehicle_sharing.FuelType;
+import org.opentripplanner.routing.core.vehicle_sharing.Gearbox;
+import org.opentripplanner.routing.core.vehicle_sharing.Provider;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.vertextype.IntersectionVertex;
 
-import java.util.List;
-
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DropoffVehicleEdgeTest {
 
@@ -21,6 +21,7 @@ public class DropoffVehicleEdgeTest {
     private DropoffVehicleEdge edge;
     private RoutingRequest request;
     private State rentingState;
+    private ParkingZoneInfo parkingZoneInfo;
 
     @Before
     public void setUp() {
@@ -35,13 +36,15 @@ public class DropoffVehicleEdgeTest {
         StateEditor se = state.edit(edge);
         se.beginVehicleRenting(CAR_1);
         rentingState = se.makeState();
+
+        parkingZoneInfo = mock(ParkingZoneInfo.class);
     }
 
     @Test
-    public void shouldNotAllowToDropoffVehicleOutsideParkingZone() {
+    public void shouldNotReturnVehicleIfCannotDropoffHere() {
         // given
-        List<ParkingZoneInfo.SingleParkingZone> singleParkingZone = singletonList(new ParkingZoneInfo.SingleParkingZone(2, VehicleType.CAR));
-        edge.updateParkingZones(singleParkingZone, emptyList());
+        edge.setParkingZones(parkingZoneInfo);
+        when(parkingZoneInfo.canDropoffVehicleHere(CAR_1)).thenReturn(false);
 
         // when
         State traversed = edge.traverse(rentingState);
@@ -51,30 +54,17 @@ public class DropoffVehicleEdgeTest {
     }
 
     @Test
-    public void shouldAllowToDropoffVehicleInsideParkingZone() {
+    public void shouldReturnVehicleIfCanDropoffHere() {
         // given
         request.rentingAllowed = true;
-        List<ParkingZoneInfo.SingleParkingZone> singleParkingZone = singletonList(new ParkingZoneInfo.SingleParkingZone(2, VehicleType.CAR));
-        edge.updateParkingZones(singleParkingZone, singleParkingZone);
+        edge.setParkingZones(parkingZoneInfo);
+        when(parkingZoneInfo.canDropoffVehicleHere(CAR_1)).thenReturn(true);
 
         // when
         State traversed = edge.traverse(rentingState);
 
         // then
         assertNotNull(traversed);
-    }
-
-    @Test
-    public void shouldAllowToDropoffVehicleWhenParkingZonesDontApplyToIt() {
-        // given
-        request.rentingAllowed = true;
-        List<ParkingZoneInfo.SingleParkingZone> singleParkingZone = singletonList(new ParkingZoneInfo.SingleParkingZone(2, VehicleType.MOTORBIKE));
-        edge.updateParkingZones(singleParkingZone, emptyList());
-
-        // when
-        State traversed = edge.traverse(rentingState);
-
-        // then
-        assertNotNull(traversed);
+        assertFalse(traversed.isCurrentlyRentingVehicle());
     }
 }
