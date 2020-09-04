55e1e38d9d6c53a8fed67bfa23d44dad543fe4b3
adamwiktor <adam.wiktor97@gmail.com>
2020-09-04 16:59:03 +0200

    Match edges with start and end osm node id

--- src/main/java/org/opentripplanner/graph_builder/module/time/TrafifcPredictionBuilderModule.java
+++ src/main/java/org/opentripplanner/graph_builder/module/time/TrafifcPredictionBuilderModule.java
@@ -7,7 +7,7 @@ import org.opentripplanner.routing.graph.Graph;
 import java.io.File;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.TreeMap;
+import java.util.Map;
 
 import static org.opentripplanner.util.Properties.LOG;
 
@@ -30,24 +30,22 @@ public class TrafifcPredictionBuilderModule implements GraphBuilderModule {
     }
     @Override
     public void buildGraph(Graph graph, HashMap<Class<?>, Object> extra) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Map<EdgeLine, Cluster> map = new HashMap<>();
         for (Cluster c : this.clusterlist.getclusters()) {
             for (EdgeData e : c.getedges())
                 if (c.gettimetable() != null) {
-                    map.put(((int) e.getid()), e.getclusterid() - 1);
-                LOG.info("log1 aurdata Edge {} , Cluste {} start {} endnod{}",e.getid(),e.getclusterid(),e.getstartnodeid(),e.getendnodeid());
+                    map.put(new EdgeLine(e.getstartnodeid(), e.getendnodeid()), c);
+                    LOG.info("log1 aurdata Edge {} , Cluste {} start {} endnod{}", e.getid(), e.getclusterid(), e.getstartnodeid(), e.getendnodeid());
                 }
         }
 
         for (StreetEdge e : graph.getStreetEdges()) {
-            LOG.info("culent graph eid {} osmstart {}  osmend {} , ", e.getId(), e.getStartOsmNodeId(),e.getEndOsmNodeId() );
-            if (map.get(e.getId()) != null) {
-                int clid = map.get(e.getId());
-                matchEdge(e,this.clusterlist.getclusters().get(clid));
-                e.setTimes((this.clusterlist.getclusters().get(clid)).gettimetableas());
+            LOG.info("culent graph eid {} osmstart {}  osmend {} , ", e.getId(), e.getStartOsmNodeId(), e.getEndOsmNodeId());
+            Cluster cluster = map.get(new EdgeLine(e.getStartOsmNodeId(), e.getEndOsmNodeId()));
+            if (cluster != null) {
+                matchEdge(e, cluster);
+                e.setTimes((cluster).gettimetableas());
                 e.getTimes().sort(TimeTable::compareTo);
-
-
             }
         }
     }
