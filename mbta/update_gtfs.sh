2251c7057bd552b0220a16ce8f0075fca8b6ca5f
Eddie Maldonado <emaldonado@mbta.com>
2020-10-06 09:38:50 -0400

    chore: go back to using MassPort's GTFS hosted on their site (#39)

--- mbta/update_gtfs.sh
+++ mbta/update_gtfs.sh
@@ -5,4 +5,4 @@ set -e
 # otherwise realtime alerts won't work: https://github.com/mbta/OpenTripPlanner/pull/8
 
 wget -N https://mbta-gtfs-s3.s3.amazonaws.com/google_transit.zip -O var/graphs/mbta/1_MBTA_GTFS.zip
-wget -N https://mbta-gtfs-s3.s3.amazonaws.com/loganexpress_temp_gtfs.zip -O var/graphs/mbta/2_loganexpress-ma-us.zip
+wget -N http://massport.com/media/4189/massport_gtfs-813.zip -O var/graphs/mbta/2_loganexpress-ma-us.zip
