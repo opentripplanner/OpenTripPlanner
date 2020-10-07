810988815800dfcedbf8fe0311923e3002a0c7bb
Gard Mellemstrand <gard.mellemstrand@entur.org>
2020-10-07 13:08:27 +0200

    Do not throw NPEs when routing without OSM data

--- src/main/java/org/opentripplanner/routing/algorithm/raptor/transit/Transfer.java
+++ src/main/java/org/opentripplanner/routing/algorithm/raptor/transit/Transfer.java
@@ -22,6 +22,7 @@ public class Transfer {
 
     public List<Coordinate> getCoordinates() {
         List<Coordinate> coordinates = new ArrayList<>();
+        if (edges == null) return coordinates;
         for (Edge edge : edges) {
             if (edge.getGeometry() != null) {
                 coordinates.addAll((Arrays.asList(edge.getGeometry().getCoordinates())));
@@ -45,7 +46,7 @@ public class Transfer {
     }
 
     public int getDistanceMeters() {
-        return (int)edges.stream().mapToDouble(Edge::getDistanceMeters).sum();
+        return edges != null ? (int)edges.stream().mapToDouble(Edge::getDistanceMeters).sum() : 0;
     }
 
     public List<Edge> getEdges() {
