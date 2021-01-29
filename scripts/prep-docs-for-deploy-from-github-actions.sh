b31086506eb0d1e57a3c8a624a5fc042c014d686
Rob Beer <rbeer@ibigroup.com>
2021-01-29 12:37:20 +0000

    refactor(Config changes): Added mkdir, different pip command and migrated AWS action

--- scripts/prep-docs-for-deploy-from-github-actions.sh
+++ scripts/prep-docs-for-deploy-from-github-actions.sh
@@ -2,6 +2,8 @@
 mkdocs build
 # build enunciate and JavaDoc
 mvn -DskipTests site
+mkdir -p target/mkdocs/api
+mkdir -p target/mkdocs/JavaDoc
 # copy enunciate API docs into mkdocs folder
 cp -R target/site/enunciate/apidocs target/mkdocs/api
 # copy JavaDoc into mkdocs folder
