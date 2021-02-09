17c6c4876545df9eb4b5883ea166966914f48e18
Gard Mellemstrand <gard.mellemstrand@entur.org>
2021-02-09 16:19:15 +0100

    Add javadoc to Direction enum

--- src/main/java/org/opentripplanner/model/Direction.java
+++ src/main/java/org/opentripplanner/model/Direction.java
@@ -1,7 +1,10 @@
 package org.opentripplanner.model;
 
 /**
- * The direction of travel for a TripPattern.
+ * The direction of travel for a TripPattern. This is mapped 1-to-1 in NeTEx, while in GTFS
+ * only values 0 and 1 are available, so they are mapped to OUTBOUND and INBOUND. When mapping
+ * from the model to the REST API, CLOCKWISE and ANTICLOCKWISE are also mapped to 0 and 1 (as they
+ * would also fit the description in the GTFS specification).
  */
 public enum Direction {
   UNKNOWN(-1),
