6237fac0992b7f7daa1f1d1f71a33e7f5276099d
Gard Mellemstrand <gard.mellemstrand@entur.org>
2021-03-11 11:40:15 +0100

    Add javadoc on concurrency issues

--- src/main/java/org/opentripplanner/graph_builder/linking/StreetSpatialIndex.java
+++ src/main/java/org/opentripplanner/graph_builder/linking/StreetSpatialIndex.java
@@ -15,6 +15,20 @@ import java.util.stream.Stream;
  * PERMANENT: Looks at the permanent index and inserts into the permanent index
  * REALTIME: Looks and the permanent index and inserts into the realtime index
  * REQUEST: Looks at both the permanent and realtime indexes and does not insert into any index
+ *
+ * NOTES ON CONCURRENCY: It is possible that an A Star search would find an edge in the
+ * realTimeIndex which is then removed before the actual routing starts. This could result in a
+ * NullPointerException on the from/to vertex of the Edge being routed on. This happens seldom
+ * enough that we have not accounted for it.
+ *
+ * A simple way to solve this, if needed, would be to just rerun the search in case on an exception.
+ * A more complete solution would have to take into account concurrency not only for the spatial
+ * index, but for the entire street graph, as an edge could be removed in the middle of routing.
+ *
+ * It is also worth noting that the entire reason we have the realTimeIndex in the first place
+ * is so that the origin/destination coordinates of a search can connect directly to the edges
+ * being split by a realtime update. This is so that we would not have to walk all the way to the
+ * end of the edge then back again to where the realtime element was connected.
  */
 class StreetSpatialIndex {
 
