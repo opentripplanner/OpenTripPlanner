fbde4dea6612948a7f6c07d7b9ff13d907aa53cc
Leonard Ehrenfried <mail@leonard.io>
2021-04-07 21:53:22 +0200

    Add cycling-specific turn cost model

--- src/main/java/org/opentripplanner/routing/core/SimpleIntersectionTraversalCostModel.java
+++ src/main/java/org/opentripplanner/routing/core/SimpleIntersectionTraversalCostModel.java
@@ -1,13 +1,16 @@
 package org.opentripplanner.routing.core;
 
-import java.io.Serializable;
-
-import org.opentripplanner.routing.edgetype.StreetEdge;
 import org.opentripplanner.routing.api.request.RoutingRequest;
+import org.opentripplanner.routing.edgetype.StreetEdge;
 import org.opentripplanner.routing.vertextype.IntersectionVertex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
 
 public class SimpleIntersectionTraversalCostModel extends AbstractIntersectionTraversalCostModel implements Serializable {
 
+    public static final Logger LOG = LoggerFactory.getLogger(SimpleIntersectionTraversalCostModel.class);
     // Model parameters are here. //
     // Constants for when there is a traffic light.
 
@@ -31,23 +34,39 @@ public class SimpleIntersectionTraversalCostModel extends AbstractIntersectionTr
     /** Expected time it takes to turn left without a stop light. */
     private Double expectedLeftNoLightTimeSec = 8.0;
 
+    private double cyclingRightTurnMultiplier = 5;
+
+    /** Since doing a left turn on a bike is quite dangerous we add a cost for it**/
+    private double cyclingLeftTurnMultiplier = cyclingRightTurnMultiplier * 3;
+
     @Override
     public double computeTraversalCost(IntersectionVertex v, StreetEdge from, StreetEdge to, TraverseMode mode,
-                                       RoutingRequest options, float fromSpeed, float toSpeed) {
+            RoutingRequest request, float fromSpeed, float toSpeed) {
 
         // If the vertex is free-flowing then (by definition) there is no cost to traverse it.
         if (v.inferredFreeFlowing()) {
             return 0;
         }
 
-        // Non-driving cases are much simpler. Handled generically in the base class.
-        if (!mode.isDriving()) {
+        if (mode.isDriving()) {
+            return computeDrivingTraversalCost(v, from, to, request);
+        }
+        else if(mode.isCycling()) {
+            var c = computeCyclingTraversalCost(v, from, to, fromSpeed, toSpeed, request);
+            if(LOG.isTraceEnabled()){
+                LOG.trace("Turning from {} to {} has a cost of {}", from, to, c);
+            }
+            return c;
+        }
+        else {
             return computeNonDrivingTraversalCost(v, from, to, fromSpeed, toSpeed);
         }
+    }
 
+    private double computeDrivingTraversalCost(IntersectionVertex v, StreetEdge from, StreetEdge to, RoutingRequest request) {
         double turnCost = 0;
 
-        int turnAngle = calculateTurnAngle(from, to, options);
+        int turnAngle = calculateTurnAngle(from, to, request);
         if (v.trafficLight) {
             // Use constants that apply when there are stop lights.
             if (isRightTurn(turnAngle)) {
@@ -77,4 +96,17 @@ public class SimpleIntersectionTraversalCostModel extends AbstractIntersectionTr
         return turnCost;
     }
 
+    private double computeCyclingTraversalCost(IntersectionVertex v, StreetEdge from,
+            StreetEdge to, float fromSpeed, float toSpeed, RoutingRequest request) {
+        var turnAngle = calculateTurnAngle(from, to, request);
+        final var baseCost = computeNonDrivingTraversalCost(v, from, to, fromSpeed, toSpeed);
+
+        if(isLeftTurn(turnAngle)) {
+            return baseCost * cyclingLeftTurnMultiplier;
+        } else if(isRightTurn(turnAngle)) {
+            return baseCost * cyclingRightTurnMultiplier;
+        } else {
+            return baseCost;
+        }
+    }
 }
