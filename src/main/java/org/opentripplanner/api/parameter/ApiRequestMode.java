2c1ca1c12c140c6a901aaaa36c6b30f0b71d39ec
Richárd Kőszegi <richard.koszegi@webvalto.hu>
2021-05-12 09:30:11 +0200

    Add snapshot routing tests using the Portland graph
    ... which is also updated to work.
    
    The portland OSM file is edited to contain:
     * two bicycle rental stations using "test-network"
     * B+R, P+R nodes and areas
    
    BikeRentalStation#locale as also marked as transient since it is only
    used by the API and blocks the serialisation of BikeRentalStations.

--- src/main/java/org/opentripplanner/api/parameter/ApiRequestMode.java
+++ src/main/java/org/opentripplanner/api/parameter/ApiRequestMode.java
@@ -1,8 +1,37 @@
 package org.opentripplanner.api.parameter;
 
+import org.opentripplanner.model.TransitMode;
+
 public enum ApiRequestMode {
     WALK, BICYCLE, CAR,
-    TRAM, SUBWAY, RAIL, BUS, FERRY,
+    TRAM, SUBWAY, RAIL, BUS, COACH, FERRY,
     CABLE_CAR, GONDOLA, FUNICULAR,
-    TRANSIT, AIRPLANE, FLEX
+    TRANSIT, AIRPLANE, FLEX;
+
+    public static ApiRequestMode fromTransitMode(TransitMode transitMode) {
+        switch (transitMode) {
+            case RAIL:
+                return RAIL;
+            case COACH:
+                return COACH;
+            case SUBWAY:
+                return SUBWAY;
+            case BUS:
+                return BUS;
+            case TRAM:
+                return TRAM;
+            case FERRY:
+                return FERRY;
+            case AIRPLANE:
+                return AIRPLANE;
+            case CABLE_CAR:
+                return CABLE_CAR;
+            case GONDOLA:
+                return GONDOLA;
+            case FUNICULAR:
+                return FUNICULAR;
+            default:
+                throw new IllegalArgumentException("Can't convert to ApiRequestMode: " + transitMode);
+        }
+    }
 }
