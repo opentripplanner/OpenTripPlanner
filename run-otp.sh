0ac17b76c814374779c3cc9ed5c0c37d2d138770
Daniil <daniil.kings@gmail.com>
2021-06-01 10:57:02 +0300

    Update run-otp.sh

--- run-otp.sh
+++ run-otp.sh
@@ -1,2 +1,2 @@
 java -Xmx6G -jar java/app.jar --build otp --save && \
-CMD java -Xmx6G -jar /java/app.jar --load --port 8080 --securePort 8081 otp
+CMD java -Xmx6G -jar java/app.jar --load --port 8080 --securePort 8081 otp
