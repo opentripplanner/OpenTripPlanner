ee3cf4a9e93fcf775e8fa75a1791d73e22c4bd9c
Daniilmass <daniil.kings@gmail.com>
2021-06-02 10:10:27 +0300

    Disabled building the graph

--- run-otp.sh
+++ run-otp.sh
@@ -1,2 +1,3 @@
-java -Xmx6G -jar ../java/app.jar --build . --save && \
+# Building the graph is disabled temporarily
+# java -Xmx6G -jar ../java/app.jar --build . --save && \
 java -Xmx6G -jar ../java/app.jar --load --port 8080 --securePort 8081 .
