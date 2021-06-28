b70b4be83f52b1368697d8e2a5eb2e8efdbc0946
Cristen Jones <cjones3@mbta.com>
2021-06-28 14:11:50 -0400

    fix: Try deploy without directory switch, add s3 env var

--- mbta/semaphore/deploy.sh
+++ mbta/semaphore/deploy.sh
@@ -2,7 +2,6 @@
 # should be run as ./semaphore/deploy.sh
 # copied from the initial semaphore EB setup
 set -e
-pushd ${SEMAPHORE_PROJECT_DIR}
 export EB_VERSION=`git rev-parse --short HEAD`-`date +%s`
 export GIT_DESCRIPTION=`git show -s --format=%s HEAD | cut -c -200`
 export FEED_DESCRIPTION=`python mbta/semaphore/gtfs_feed_version.py`
@@ -14,4 +13,3 @@ echo "Your environment is currently updating"; while [[ `aws elasticbeanstalk de
 if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep VersionLabel | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "$EB_VERSION" ]]; then echo "The version of application code on Elastic Beanstalk matches the version that Semaphore sent in this deployment."; echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"; else echo "The version of application code on Elastic Beanstalk does not match the version that Semaphore sent in this deployment. Please check your AWS Elastic Beanstalk Console for more information."; echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"; false; fi
 sleep 5; a="0"; echo "Waiting for environment health to turn Green"; while [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` != "Green" && $a -le 60 ]]; do sleep 2; a=$[$a+1]; printf "."; done; if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Green" ]]; then echo "Your environment status is Green, congrats!"; else echo "Your environment status is not Green, sorry."; false; fi;
 echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"
-popd
