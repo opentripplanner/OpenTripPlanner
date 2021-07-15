e3e91d480680f67a221046ec11a99d1192a68bbd
miles-grant-ibi <miles.grant@ibigroup.com>
2021-07-15 09:21:25 -0400

    refactor(vehicle_rental): move Jackson ignore annotation higher in the class hierarchy

--- src/main/java/org/opentripplanner/updater/vehicle_rental/GBFSMappings/BaseGtfsResponse.java
+++ src/main/java/org/opentripplanner/updater/vehicle_rental/GBFSMappings/BaseGtfsResponse.java
@@ -1,9 +1,13 @@
 package org.opentripplanner.updater.vehicle_rental.GBFSMappings;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Class for parsing base response info that will be present in all GBFS responses.
  * See https://github.com/NABSA/gbfs/blob/master/gbfs.md#output-format
  */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseGtfsResponse {
     public Integer last_updated;
     public Integer ttl;
--- src/main/java/org/opentripplanner/updater/vehicle_rental/GBFSMappings/GbfsResponse.java
+++ src/main/java/org/opentripplanner/updater/vehicle_rental/GBFSMappings/GbfsResponse.java
@@ -1,7 +1,5 @@
 package org.opentripplanner.updater.vehicle_rental.GBFSMappings;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +9,6 @@ import java.util.Map;
  * Response class for the gbfs.json file.
  * See https://github.com/NABSA/gbfs/blob/master/gbfs.md#gbfsjson
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class GbfsResponse extends BaseGtfsResponse {
     public Map<String, GbfsFeeds> data;
 
