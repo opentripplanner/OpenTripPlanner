0eab421ce1442b6f93fc90e129f2dfe6bbee5791
Leonard Ehrenfried <mail@leonard.io>
2021-08-25 14:40:39 +0200

    Rename and add test

---
+++ src/main/java/org/opentripplanner/routing/bike_rental/GeofencingZones.java
@@ -0,0 +1,49 @@
+package org.opentripplanner.routing.bike_rental;
+
+import java.util.Set;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+
+public class GeofencingZones {
+
+    private final Set<GeofencingZone> zones;
+    private final GeometryFactory geometryFactory = new GeometryFactory();
+
+    public GeofencingZones(Set<GeofencingZone> zones) {this.zones = zones;}
+
+    public boolean canDropOffVehicle(Coordinate c) {
+        var point = geometryFactory.createPoint(c);
+        // if there are no rules, this means that you can drop off the bike anywhere (in the world!)
+        if (isEmpty()) {return true;}
+        else {
+            return zones.stream().anyMatch(zone -> zone.dropOffAllowed(point)) &&
+                    zones.stream().noneMatch(zone -> zone.dropOffProhibited(point));
+        }
+    }
+
+    public int size() {return zones.size();}
+
+    public boolean isEmpty() {return zones.isEmpty();}
+
+    public static class GeofencingZone {
+
+        final Geometry geometry;
+        final boolean dropOffAllowed;
+
+        public GeofencingZone(Geometry geometry, boolean dropOffAllowed) {
+            this.geometry = geometry;
+            this.dropOffAllowed = dropOffAllowed;
+        }
+
+        boolean dropOffAllowed(Point p) {
+            return geometry.contains(p) && dropOffAllowed;
+        }
+
+        boolean dropOffProhibited(Point p) {
+            return geometry.contains(p) && !dropOffAllowed;
+        }
+    }
+}
+
--- src/test/java/org/opentripplanner/updater/bike_rental/datasources/GbfsBikeRentalDataSourceTest.java
+++ src/test/java/org/opentripplanner/updater/bike_rental/datasources/GbfsBikeRentalDataSourceTest.java
@@ -1,9 +1,13 @@
 package org.opentripplanner.updater.bike_rental.datasources;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.opentripplanner.routing.bike_rental.GeofencingZones;
 import org.opentripplanner.updater.bike_rental.datasources.GbfsBikeRentalDataSource.GbfsGeofencingZonesDataSource;
 import org.opentripplanner.updater.bike_rental.datasources.params.GbfsBikeRentalDataSourceParameters;
 
@@ -11,6 +15,8 @@ class GbfsBikeRentalDataSourceTest {
 
     @Test
     void parseGeofencingZones() {
+
+        // if you want to see the geojson in the test file on a map visit https://gist.github.com/leonardehrenfried/a1964e20e83faf8d56038480cb82e8f8
 
         var cwd = System.getProperty("user.dir");
         var url = "file://" + cwd + "/src/test/resources/bike/gbfs/tier-oslo-geofencing-zones.json";
@@ -20,8 +26,23 @@ class GbfsBikeRentalDataSourceTest {
         );
         var source = new GbfsGeofencingZonesDataSource(params);
         source.update();
-        
+
         var zones = source.getGeofencingZones();
-        assertFalse(zones.isEmpty());
+        assertEquals(zones.size(), 243);
+
+        // some place in the north of Oslo should be allowed
+        assertTrue(zones.canDropOffVehicle(new Coordinate(10.6918, 59.9489)));
+
+        // another place outside the city should not be allowed (outside business area)
+        assertFalse(zones.canDropOffVehicle(new Coordinate(10.1857, 59.7391)));
+
+        // dropping off in Frogner Park (inside business area but inside a special exclusion zone) should not be allowed
+        assertFalse(zones.canDropOffVehicle(new Coordinate( 10.7036, 59.9277)));
+    }
+
+    @Test
+    void emptyZones(){
+        var zones = new GeofencingZones(Collections.emptySet());
+        zones.canDropOffVehicle(new Coordinate(11.1024, 59.2820));
     }
 }
