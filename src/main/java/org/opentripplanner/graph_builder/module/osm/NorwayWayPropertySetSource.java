6453e02f5797cf23294eabea98700c537a3afd82
Bjørn Lie Rapp <bjoern.lr+gis@gmail.com>
2021-09-14 14:23:49 +0200

    Properties for roads.

--- src/main/java/org/opentripplanner/graph_builder/module/osm/NorwayWayPropertySetSource.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/NorwayWayPropertySetSource.java
@@ -21,14 +21,6 @@ public class NorwayWayPropertySetSource implements WayPropertySetSource {
 
   @Override
   public void populateProperties(WayPropertySet props) {
-    // Replace existing matching properties as the logic is that the first statement registered takes precedence over later statements
-    props.setProperties("highway=trunk_link", StreetTraversalPermission.ALL, 2.06, 2.06);
-    props.setProperties("highway=trunk", StreetTraversalPermission.ALL, 7.47, 7.47);
-
-    // Don't recommend walking in trunk road tunnels (although actually legal unless explicitly forbidden)
-    props.setProperties("highway=trunk;tunnel=yes", StreetTraversalPermission.CAR, 7.47, 7.47);
-    props.setProperties("highway=trunk_link;tunnel=yes", StreetTraversalPermission.CAR, 7.47, 7.47);
-    
 
     // Do not drive on cycleWays
     props.setProperties("highway=cycleway;bicycle=designated",
@@ -54,6 +46,105 @@ public class NorwayWayPropertySetSource implements WayPropertySetSource {
     // Patch missing vehicle=no check
     props.setProperties("highway=primary_link;motorroad=yes;vehicle=no", StreetTraversalPermission.NONE);
     props.setProperties("highway=primary_link;motorroad=yes;vehicle=no", StreetTraversalPermission.NONE);
+
+    /* "highway=trunk" roads ("Riksveier") are often single carriageway, and not only legal for bicycles,
+    but assumed to be  much safer than the  default profile (Except when the speedlimt exceeds 90). */
+    props.setProperties("highway=trunk", StreetTraversalPermission.ALL, 2.25, 2.25);
+    props.setProperties("highway=trunk_link", StreetTraversalPermission.ALL, 2.25, 2.25);
+    // foot=no indicates heavy traffic, and is considered less safe for bicycles.
+    props.setProperties("highway=trunk;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 7, 7);
+    // Patch missing vehicle=no check
+    props.setProperties("highway=trunk;vehicle=no", StreetTraversalPermission.PEDESTRIAN);
+    props.setProperties("highway=trunk_link;vehicle=no", StreetTraversalPermission.PEDESTRIAN);
+    // Don't recommend walking in trunk road tunnels (although actually legal unless explicitly forbidden)
+    props.setProperties("highway=trunk;tunnel=yes", StreetTraversalPermission.CAR);
+    props.setProperties("highway=trunk_link;tunnel=yes", StreetTraversalPermission.CAR);
+    props.setProperties("highway=trunk;maxspeed=90", StreetTraversalPermission.ALL, 7, 7);
+    props.setProperties("highway=trunk_link;maxspeed=90", StreetTraversalPermission.ALL, 7, 7);
+    props.setProperties("highway=trunk;maxspeed=60", StreetTraversalPermission.ALL, 2.06, 2.06);
+    props.setProperties("highway=trunk_link;maxspeed=60", StreetTraversalPermission.ALL, 2.06, 2.06);
+    props.setProperties("highway=trunk;maxspeed=50", StreetTraversalPermission.ALL, 1.5, 1.5);
+    props.setProperties("highway=trunk_link;maxspeed=50", StreetTraversalPermission.ALL, 1.5, 1.5);
+    props.setProperties("highway=trunk;maxspeed=40", StreetTraversalPermission.ALL, 1.5, 1.5);
+    props.setProperties("highway=trunk_link;maxspeed=40", StreetTraversalPermission.ALL, 1.5, 1.5);
+
+    props.setProperties("highway=primary", StreetTraversalPermission.ALL, 2.25, 2.25);
+    props.setProperties("highway=primary_link", StreetTraversalPermission.ALL, 2.25, 2.25);
+    props.setProperties("highway=primary;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 7, 7);
+    props.setProperties("highway=primary_link;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 7, 7);
+    // Patch missing vehicle=no check
+    props.setProperties("highway=primary;vehicle=no", StreetTraversalPermission.PEDESTRIAN, 8, 8);
+    props.setProperties("highway=primary_link;vehicle=no", StreetTraversalPermission.PEDESTRIAN, 8, 8);
+    props.setProperties("highway=primary;maxspeed=90", StreetTraversalPermission.ALL, 7, 7);
+    props.setProperties("highway=primary_link;maxspeed=90", StreetTraversalPermission.ALL, 7, 7);
+    props.setProperties("highway=primary;maxspeed=60", StreetTraversalPermission.ALL, 1.50, 1.50);
+    props.setProperties("highway=primary_link;maxspeed=60", StreetTraversalPermission.ALL, 1.50, 1.50);
+    props.setProperties("highway=primary;maxspeed=50", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=primary_link;maxspeed=50", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=primary;maxspeed=40", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=primary_link;maxspeed=40", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=primary;maxspeed=30", StreetTraversalPermission.ALL, 0.92, 0.92);
+    props.setProperties("highway=primary_link;maxspeed=30", StreetTraversalPermission.ALL, 0.92, 0.92);
+
+    props.setProperties("highway=secondary", StreetTraversalPermission.ALL, 2.06, 2.06);
+    props.setProperties("highway=secondary_link", StreetTraversalPermission.ALL, 2.06, 2.06);
+    props.setProperties("highway=secondary;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 7, 7);
+    props.setProperties("highway=secondary_link;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 7, 7);
+    // Patch missing vehicle=no check
+    props.setProperties("highway=secondary;vehicle=no", StreetTraversalPermission.PEDESTRIAN, 8, 8);
+    props.setProperties("highway=secondary_link;vehicle=no", StreetTraversalPermission.PEDESTRIAN, 8, 8);
+    props.setProperties("highway=secondary;maxspeed=60", StreetTraversalPermission.ALL, 1.50, 1.50);
+    props.setProperties("highway=secondary_link;maxspeed=60", StreetTraversalPermission.ALL, 1.50, 1.50);
+    props.setProperties("highway=secondary;maxspeed=50", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=secondary_link;maxspeed=50", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=secondary;maxspeed=40", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=secondary_link;maxspeed=40", StreetTraversalPermission.ALL, 1.46, 1.46);
+    props.setProperties("highway=secondary;maxspeed=30", StreetTraversalPermission.ALL, 0.92, 0.92);
+    props.setProperties("highway=secondary_link;maxspeed=30", StreetTraversalPermission.ALL, 0.92, 0.92);
+
+    props.setProperties("highway=tertiary", StreetTraversalPermission.ALL, 1.0, 1.0);
+    props.setProperties("highway=tertiary_link", StreetTraversalPermission.ALL, 1.0, 1.0);
+    props.setProperties("highway=tertiary;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 7, 7);
+    props.setProperties("highway=tertiary_link;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 7, 7);
+    // Patch missing vehicle=no check
+    props.setProperties("highway=tertiary;vehicle=no", StreetTraversalPermission.PEDESTRIAN, 8, 8);
+    props.setProperties("highway=tertiary_link;vehicle=no", StreetTraversalPermission.PEDESTRIAN, 8, 8);
+    props.setProperties("highway=tertiary;maxspeed=60", StreetTraversalPermission.ALL, 0.97, 0.97);
+    props.setProperties("highway=tertiary_link;maxspeed=60", StreetTraversalPermission.ALL, 0.97, 0.97);
+    props.setProperties("highway=tertiary;maxspeed=50", StreetTraversalPermission.ALL, 0.92, 0.92);
+    props.setProperties("highway=tertiary_link;maxspeed=50", StreetTraversalPermission.ALL, 0.92, 0.92);
+    props.setProperties("highway=tertiary;maxspeed=40", StreetTraversalPermission.ALL, 0.92, 0.92);
+    props.setProperties("highway=tertiary_link;maxspeed=40", StreetTraversalPermission.ALL, 0.92, 0.92);
+    props.setProperties("highway=tertiary;maxspeed=30", StreetTraversalPermission.ALL, 0.92, 0.92);
+    props.setProperties("highway=tertiary_link;maxspeed=30", StreetTraversalPermission.ALL, 0.92, 0.92);
+
+    props.setProperties("highway=unclassified", StreetTraversalPermission.ALL, 1.0, 1.0);
+    props.setProperties("highway=unclassified;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 6, 6);
+    // These access tags indicates low traffic
+    props.setProperties("highway=unclassified;motor_vehicle=no", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=unclassified;motor_vehicle=private", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=unclassified;motor_vehicle=permit", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=unclassified;motor_vehicle=destination", StreetTraversalPermission.ALL, 0.97, 0.97);
+    props.setProperties("highway=unclassified;maxspeed=70", StreetTraversalPermission.ALL, 2.06, 2.06);
+    props.setProperties("highway=unclassified;maxspeed=80", StreetTraversalPermission.ALL, 2.06, 2.06);
+
+    props.setProperties("highway=residential", StreetTraversalPermission.ALL, 1.0, 1.0);
+    // These access tags indicates low traffic
+    props.setProperties("highway=residential;motor_vehicle=no", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=residential;motor_vehicle=private", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=residential;motor_vehicle=permit", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=residential;motor_vehicle=destination", StreetTraversalPermission.ALL, 0.97, 0.97);
+
+    props.setProperties("highway=service", StreetTraversalPermission.ALL, 1.0, 1.0);
+    props.setProperties("highway=service;foot=no", StreetTraversalPermission.BICYCLE_AND_CAR, 6, 6);
+    // These access tags indicates low traffic
+    props.setProperties("highway=service;motor_vehicle=no", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=service;motor_vehicle=private", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=service;motor_vehicle=permit", StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE, 0.97, 0.97);
+    props.setProperties("highway=service;motor_vehicle=destination", StreetTraversalPermission.ALL, 0.97, 0.97);
+    // Cycling around reversing cars on a parking lot feels unsafe
+    props.setProperties("highway=service;service=parking_aisle", StreetTraversalPermission.ALL, 1.5, 1.5);
+    props.setProperties("highway=service;service=drive-through", StreetTraversalPermission.ALL, 1.5, 1.5);
 
     // Disallow paths that are not fit for walking
     props.setProperties("smoothness=horrible", StreetTraversalPermission.NONE);
