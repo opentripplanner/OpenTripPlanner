cd9fbba244fb33847f5221ff737569bc0a2efa84
Leonard Ehrenfried <mail@leonard.io>
2021-09-14 14:47:53 +0200

    Modify test

--- src/test/java/org/opentripplanner/mmri/WheelchairTest.java
+++ src/test/java/org/opentripplanner/mmri/WheelchairTest.java
@@ -11,6 +11,6 @@ public class WheelchairTest extends MmriTest {
     public void test2b1() {
         Leg leg = plan(+1388530860L, "2b1", "2b2", null, true, false, null, "", "");
 
-        validateLeg(leg, 1388530980000L, 1388531040000L, "2b2", "2b1", null);
+        validateLeg(leg, 1388530860000L, 1388530920000L, "2b2", "2b1", null);
     }
 }
