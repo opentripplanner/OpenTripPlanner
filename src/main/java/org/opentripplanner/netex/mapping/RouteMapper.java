91621ccb7b282c1d51db83a986c3af6e5453c1a7
Leonard Ehrenfried <mail@leonard.io>
2021-09-15 10:02:11 +0200

    Use Netex enums to check for ferries

--- src/main/java/org/opentripplanner/netex/mapping/RouteMapper.java
+++ src/main/java/org/opentripplanner/netex/mapping/RouteMapper.java
@@ -11,6 +11,7 @@ import org.opentripplanner.model.TransitMode;
 import org.opentripplanner.model.impl.EntityById;
 import org.opentripplanner.netex.index.api.NetexEntityIndexReadOnlyView;
 import org.opentripplanner.netex.mapping.support.FeedScopedIdFactory;
+import org.rutebanken.netex.model.AllVehicleModesOfTransportEnumeration;
 import org.rutebanken.netex.model.FlexibleLine_VersionStructure;
 import org.rutebanken.netex.model.Line_VersionStructure;
 import org.rutebanken.netex.model.Network;
@@ -91,7 +92,7 @@ class RouteMapper {
         // but currently it doesn't look it is being parsed.
         // until there is better information from the operators we assume that all ferries allow
         // bicycles on board.
-        if(mode == TransitMode.FERRY) {
+        if(line.getTransportMode().equals(AllVehicleModesOfTransportEnumeration.WATER)) {
             if(ferryIdsNotAllowedForBicycle.contains(line.getId())) {
                 otpRoute.setBikesAllowed(BikeAccess.NOT_ALLOWED);
             } else {
