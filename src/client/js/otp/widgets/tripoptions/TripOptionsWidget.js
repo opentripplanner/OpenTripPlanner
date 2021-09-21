8cc282f1f112a4454940943ebf307dfae12f8d0d
Zsombor Welker <zsombor.welker@realcity.io>
2021-09-21 10:51:55 +0200

    debug-client: correctly restore boolean parameters

--- src/client/js/otp/widgets/tripoptions/TripOptionsWidget.js
+++ src/client/js/otp/widgets/tripoptions/TripOptionsWidget.js
@@ -445,7 +445,9 @@ otp.widgets.tripoptions.WheelChairSelector =
     },
 
     restorePlan : function(data) {
-        $("#"+this.id+"-wheelchair-input").prop("checked", data.queryParams.wheelchair === 'true');
+        var checked = data.queryParams.wheelchair === true || data.queryParams.wheelchair === 'true';
+        this.tripWidget.module.wheelchair = checked;
+        $("#"+this.id+"-wheelchair-input").prop("checked", checked);
     },
 
     isApplicableForMode : function(mode) {
@@ -483,7 +485,9 @@ otp.widgets.tripoptions.DebugItineraryFiltersSelector = otp.Class(
             });
         },
         restorePlan: function (data) {
-            $("#" + this.id + "-debug-filters-input").prop("checked", data.queryParams.debugItineraryFilter === 'true');
+            var checked = data.queryParams.debugItineraryFilter === true || data.queryParams.debugItineraryFilter === 'true';
+            this.tripWidget.module.debugItineraryFilter = checked;
+            $("#" + this.id + "-debug-filters-input").prop("checked", checked);
         },
         isApplicableForMode : function(mode) { return true; }
     }
