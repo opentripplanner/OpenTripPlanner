8d8d2f0c9ab4325533f862e28d64295932de1c09
Leonard Ehrenfried <mail@leonard.io>
2021-09-22 10:40:01 +0200

    Restore pre-PR formatting

--- src/test/java/org/opentripplanner/api/resource/GraphPathToTripPlanConverterTest.java
+++ src/test/java/org/opentripplanner/api/resource/GraphPathToTripPlanConverterTest.java
@@ -116,12 +116,9 @@ public class GraphPathToTripPlanConverterTest {
     public void testGenerateItinerary() {
         GraphPath[] graphPaths = buildPaths();
 
-        compare(GraphPathToTripPlanConverter.generateItinerary(graphPaths[0], true, false,
-                locale), Type.FORWARD);
-        compare(GraphPathToTripPlanConverter.generateItinerary(graphPaths[1], true, false,
-                locale), Type.BACKWARD);
-        compare(GraphPathToTripPlanConverter.generateItinerary(graphPaths[2], true, false,
-                locale), Type.ONBOARD);
+        compare(GraphPathToTripPlanConverter.generateItinerary(graphPaths[0], true, false, locale), Type.FORWARD);
+        compare(GraphPathToTripPlanConverter.generateItinerary(graphPaths[1], true, false, locale), Type.BACKWARD);
+        compare(GraphPathToTripPlanConverter.generateItinerary(graphPaths[2], true, false, locale), Type.ONBOARD);
     }
 
     /**
@@ -133,8 +130,7 @@ public class GraphPathToTripPlanConverterTest {
         // Reuse testGenerateItinerary()'s graph path, but shorten it
         GraphPath graphPath = new GraphPath(buildPaths()[0].states.get(3), false);
 
-        Itinerary itinerary = GraphPathToTripPlanConverter.generateItinerary(graphPath, false, false,
-                locale);
+        Itinerary itinerary = GraphPathToTripPlanConverter.generateItinerary(graphPath, false, false, locale);
 
         assertEquals(1, itinerary.legs.size());
         assertEquals("WALK", itinerary.legs.get(0).mode);
@@ -153,8 +149,7 @@ public class GraphPathToTripPlanConverterTest {
 
         GraphPath graphPath = new GraphPath(new State(options), false);
 
-        GraphPathToTripPlanConverter.generateItinerary(graphPath, false, false,
-                locale);
+        GraphPathToTripPlanConverter.generateItinerary(graphPath, false, false, locale);
     }
 
     /**
