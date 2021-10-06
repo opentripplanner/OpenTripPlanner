8fb2ab185752ec015e16c2a71f076cc7b9a054d1
Hannes Junnila <hannes.junnila@gmail.com>
2021-10-06 11:08:01 +0300

    Properly set error message in REST API
    Fixes #3581

--- src/main/java/org/opentripplanner/api/mapping/PlannerErrorMapper.java
+++ src/main/java/org/opentripplanner/api/mapping/PlannerErrorMapper.java
@@ -5,41 +5,47 @@ import org.opentripplanner.api.model.error.PlannerError;
 import org.opentripplanner.routing.api.response.InputField;
 import org.opentripplanner.routing.api.response.RoutingError;
 
+import java.util.List;
+
 public class PlannerErrorMapper {
 
     public static PlannerError mapMessage(RoutingError domain) {
         if(domain == null) { return null; }
 
-        PlannerError api = new PlannerError();
+        PlannerError api;
 
         switch (domain.code) {
             case NO_TRANSIT_CONNECTION:
             case NO_TRANSIT_CONNECTION_INSIDE_SEARCH_WINDOW:
-                api.message = Message.PATH_NOT_FOUND;
+                api = new PlannerError(Message.PATH_NOT_FOUND);
                 break;
             case OUTSIDE_BOUNDS:
-                api.message = Message.OUTSIDE_BOUNDS;
+                api = new PlannerError(Message.OUTSIDE_BOUNDS);
+                api.setMissing(List.of(domain.inputField.name()));
                 break;
             case OUTSIDE_SERVICE_PERIOD:
-                api.message = Message.NO_TRANSIT_TIMES;
+                api = new PlannerError(Message.NO_TRANSIT_TIMES);
                 break;
             case LOCATION_NOT_FOUND:
                 if (domain.inputField.equals(InputField.FROM_PLACE)) {
-                    api.message = Message.GEOCODE_FROM_NOT_FOUND;
+                    api = new PlannerError(Message.GEOCODE_FROM_NOT_FOUND);
+                    api.setMissing(List.of(domain.inputField.name()));
                 } else if (domain.inputField.equals(InputField.TO_PLACE)) {
-                    api.message = Message.GEOCODE_TO_NOT_FOUND;
+                    api = new PlannerError(Message.GEOCODE_TO_NOT_FOUND);
+                    api.setMissing(List.of(domain.inputField.name()));
                 } else {
                     throw new IllegalArgumentException();
                 }
                 break;
             case NO_STOPS_IN_RANGE:
-                api.message = Message.LOCATION_NOT_ACCESSIBLE;
+                api = new PlannerError(Message.LOCATION_NOT_ACCESSIBLE);
+                api.setMissing(List.of(domain.inputField.name()));
                 break;
             case WALKING_BETTER_THAN_TRANSIT:
-                api.message = Message.TOO_CLOSE;
+                api = new PlannerError(Message.TOO_CLOSE);
                 break;
             case SYSTEM_ERROR:
-                api.message = Message.SYSTEM_ERROR;
+                api = new PlannerError(Message.SYSTEM_ERROR);
                 break;
             default:
                 throw new IllegalArgumentException();
