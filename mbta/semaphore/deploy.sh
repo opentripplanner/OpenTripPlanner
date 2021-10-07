15b3d5f471a3fc25225d9af5c7918f83dc76dec0
Gabe Durazo <gdurazo@mbta.com>
2021-10-07 17:00:58 -0500

    more deploy tweaks (#43)
    * chore: format deploy script
    
    * fix: Try longer waits between requests to AWS during deploy

--- mbta/semaphore/deploy.sh
+++ mbta/semaphore/deploy.sh
@@ -9,7 +9,43 @@ aws s3 cp mbta_otp.zip s3://$S3_BUCKET_NAME/"$EB_APP_NAME"/"$EB_VERSION".zip
 aws elasticbeanstalk create-application-version --application-name "$EB_APP_NAME" --version-label "$EB_VERSION" --source-bundle S3Bucket=$S3_BUCKET_NAME,S3Key="$EB_APP_NAME/$EB_VERSION.zip" --description "GTFS: $FEED_DESCRIPTION Git: $GIT_DESCRIPTION"
 aws elasticbeanstalk update-environment --environment-name "$EB_ENV_NAME" --version-label "$EB_VERSION"
 echo "Environment status: `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'`"
-echo "Your environment is currently updating"; while [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Updating" ]]; do sleep 2; printf "."; done
-if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep VersionLabel | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "$EB_VERSION" ]]; then echo "The version of application code on Elastic Beanstalk matches the version that Semaphore sent in this deployment."; echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"; else echo "The version of application code on Elastic Beanstalk does not match the version that Semaphore sent in this deployment. Please check your AWS Elastic Beanstalk Console for more information."; echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"; false; fi
-sleep 5; a="0"; echo "Waiting for environment health to turn Green"; while [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` != "Green" && $a -le 10 ]]; do sleep 30; a=$[$a+1]; printf "."; done; if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Green" ]]; then echo "Your environment status is Green, congrats!"; else echo "Your environment status is not Green, sorry."; false; fi;
+
+a="0"
+echo "Your environment is currently updating"
+while [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Updating" && $a -le 30]]
+do
+  sleep 60
+  a=$[$a+1]
+  printf "."
+done
+
+if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep VersionLabel | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "$EB_VERSION" ]]
+then
+  echo "The version of application code on Elastic Beanstalk matches the version that GitHub Actions sent in this deployment."
+  echo "Your environment info:"
+  aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"
+else
+  echo "The version of application code on Elastic Beanstalk does not match the version that GitHub Actions sent in this deployment. Please check your AWS Elastic Beanstalk Console for more information."; echo "Your environment info:"
+  aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"
+  false
+fi
+
+sleep 5
+a="0"
+echo "Waiting for environment health to turn Green"
+while [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` != "Green" && $a -le 30 ]]
+do
+  sleep 60
+  a=$[$a+1]
+  printf "."
+done
+
+if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Green" ]]
+then
+  echo "Your environment status is Green, congrats!"
+else
+  echo "Your environment status is not Green, sorry."
+  false
+fi
+
 echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME"
