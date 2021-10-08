e8301daf6fd279233fb9acddc73b32931dcdef6c
Gabe Durazo <gdurazo@mbta.com>
2021-10-07 23:13:39 -0500

    Deploy fixes (#44)
    * fix: bash scripting error in deploy script
    
    * chore: Update deploy script to pass shellcheck

--- mbta/semaphore/deploy.sh
+++ mbta/semaphore/deploy.sh
@@ -2,24 +2,29 @@
 # should be run as ./semaphore/deploy.sh
 # copied from the initial semaphore EB setup
 set -e
-export EB_VERSION=`git rev-parse --short HEAD`-`date +%s`
-export GIT_DESCRIPTION=`git show -s --format=%s HEAD | cut -c -200`
-export FEED_DESCRIPTION=`python mbta/semaphore/gtfs_feed_version.py`
-aws s3 cp mbta_otp.zip s3://$S3_BUCKET_NAME/"$EB_APP_NAME"/"$EB_VERSION".zip
-aws elasticbeanstalk create-application-version --application-name "$EB_APP_NAME" --version-label "$EB_VERSION" --source-bundle S3Bucket=$S3_BUCKET_NAME,S3Key="$EB_APP_NAME/$EB_VERSION.zip" --description "GTFS: $FEED_DESCRIPTION Git: $GIT_DESCRIPTION"
+
+EB_VERSION=$(git rev-parse --short HEAD)-$(date +%s)
+export EB_VERSION
+GIT_DESCRIPTION=$(git show -s --format=%s HEAD | cut -c -200)
+export GIT_DESCRIPTION
+FEED_DESCRIPTION=$(python mbta/semaphore/gtfs_feed_version.py)
+export FEED_DESCRIPTION
+
+aws s3 cp mbta_otp.zip s3://"$S3_BUCKET_NAME"/"$EB_APP_NAME"/"$EB_VERSION".zip
+aws elasticbeanstalk create-application-version --application-name "$EB_APP_NAME" --version-label "$EB_VERSION" --source-bundle S3Bucket="$S3_BUCKET_NAME,S3Key=$EB_APP_NAME/$EB_VERSION.zip" --description "GTFS: $FEED_DESCRIPTION Git: $GIT_DESCRIPTION"
 aws elasticbeanstalk update-environment --environment-name "$EB_ENV_NAME" --version-label "$EB_VERSION"
-echo "Environment status: `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'`"
+echo "Environment status: $(aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//')"
 
 a="0"
 echo "Your environment is currently updating"
-while [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Updating" && $a -le 30]]
+while [[ $(aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//') = "Updating" && $a -le 30 ]]
 do
   sleep 60
-  a=$[$a+1]
+  a=$((a+1))
   printf "."
 done
 
-if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep VersionLabel | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "$EB_VERSION" ]]
+if [[ $(aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep VersionLabel | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//') = "$EB_VERSION" ]]
 then
   echo "The version of application code on Elastic Beanstalk matches the version that GitHub Actions sent in this deployment."
   echo "Your environment info:"
@@ -33,14 +38,14 @@ fi
 sleep 5
 a="0"
 echo "Waiting for environment health to turn Green"
-while [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` != "Green" && $a -le 30 ]]
+while [[ $(aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//') != "Green" && $a -le 30 ]]
 do
   sleep 60
-  a=$[$a+1]
+  a=$((a+1))
   printf "."
 done
 
-if [[ `aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Green" ]]
+if [[ $(aws elasticbeanstalk describe-environments --environment-names "$EB_ENV_NAME" | grep '"Health":' | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//') = "Green" ]]
 then
   echo "Your environment status is Green, congrats!"
 else
