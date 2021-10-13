11f1121fdf4bda5aa82b7dbfeec3a2264beda1b1
Vesa Meskanen <vesa.meskanen@cgi.com>
2021-10-13 10:09:44 +0300

    Update src/main/java/org/opentripplanner/graph_builder/module/PruneNoThruIslands.java
    Co-authored-by: Thomas Gran <t2gran@gmail.com>

--- src/main/java/org/opentripplanner/graph_builder/module/PruneNoThruIslands.java
+++ src/main/java/org/opentripplanner/graph_builder/module/PruneNoThruIslands.java
@@ -25,6 +25,7 @@ import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.routing.vertextype.StreetVertex;
 import org.opentripplanner.routing.vertextype.TransitStopVertex;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
