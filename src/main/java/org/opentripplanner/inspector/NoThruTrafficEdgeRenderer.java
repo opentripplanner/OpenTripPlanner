870417727a3b4f01192b72d3dc1931f5dadf0f2a
Vesa Meskanen <vesa.meskanen@cgi.com>
2021-10-13 10:48:57 +0300

    fix: use Color array for noThru color definitions

--- src/main/java/org/opentripplanner/inspector/NoThruTrafficEdgeRenderer.java
+++ src/main/java/org/opentripplanner/inspector/NoThruTrafficEdgeRenderer.java
@@ -17,38 +17,38 @@ public class NoThruTrafficEdgeRenderer implements EdgeVertexRenderer {
     public NoThruTrafficEdgeRenderer() {
     }
 
+    private static final Color [] colors = {
+        new Color(200, 200, 200), // no limitations = light gray
+        new Color(200, 200, 0),   // no walk thru traffic = yellow
+        new Color(0, 200, 200),   // no bike thru = cyan
+        new Color(0, 200, 0),     // no walk & bike thru = green
+        new Color(0, 0, 200),     // no car thru = blue
+        new Color(200, 100, 0),   // no car & walk thru = orange
+        new Color(200, 0, 200),   // no car & bike thru = purple
+        new Color(200, 0, 0)      // no for all = red
+    };
+
     @Override
     public boolean renderEdge(Edge e, EdgeVisualAttributes attrs) {
         if (e instanceof StreetEdge) {
-            int [][] colors = {
-                { 200, 200, 200 }, // no limitations = light gray
-                { 200, 200, 0},    // no walk thru traffic = yellow
-                { 0, 200, 200 },   // no bike thru = cyan
-                { 0, 200, 0 },     // no walk & bike thru = green
-                { 0, 0, 200},      // no car thru = blue
-                { 200, 100, 0},    // no car & walk thru = orange
-                { 200, 0, 200 },   // no car & bike thru = purple
-                { 200, 0, 0 }      // no for all = red
-            };
-
             StreetEdge pse = (StreetEdge) e;
-            int bits = 0;
+            int colorIndex = 0;
 
             attrs.label = "";
 
             if (pse.isWalkNoThruTraffic()) {
                 attrs.label = " walk ";
-                bits += 1;
+                colorIndex += 1;
             }
             if (pse.isBicycleNoThruTraffic()) {
                 attrs.label += " bike";
-                bits += 2;
+                colorIndex += 2;
             }
             if (pse.isMotorVehicleNoThruTraffic()) {
                 attrs.label += " car";
-                bits += 4;
+                colorIndex += 4;
             }
-            attrs.color = new Color(colors[bits][0], colors[bits][1], colors[bits][2]);
+            attrs.color = colors[colorIndex];
             if (!attrs.label.equals("")) {
                 attrs.label = "No" + attrs.label + " thru traffic";
             }
