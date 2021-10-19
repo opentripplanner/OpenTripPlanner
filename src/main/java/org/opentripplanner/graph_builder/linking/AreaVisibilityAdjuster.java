6abfed8c9e0c86d2a345283bf378256bf5dca37a
Vesa Meskanen <vesa.meskanen@cgi.com>
2021-10-19 09:28:53 +0300

    feature: more advanced transit stop linking to surrounding area nodes
    A simple logic, which checks that edges from the stop do not cross any
    original area edges. This ensures that edges are inside the area (do not pass through buildings etc).
    The created edges also obey the travel permissions of the areas.

--- src/main/java/org/opentripplanner/graph_builder/linking/AreaVisibilityAdjuster.java
+++ src/main/java/org/opentripplanner/graph_builder/linking/AreaVisibilityAdjuster.java
@@ -3,6 +3,9 @@ package org.opentripplanner.graph_builder.linking;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.MultiLineString;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.Geometry;
 import org.opentripplanner.common.geometry.GeometryUtils;
 import org.opentripplanner.common.geometry.SphericalDistanceLibrary;
 import org.opentripplanner.graph_builder.services.DefaultStreetEdgeFactory;
@@ -10,18 +13,22 @@ import org.opentripplanner.graph_builder.services.StreetEdgeFactory;
 import org.opentripplanner.openstreetmap.model.OSMWithTags;
 import org.opentripplanner.routing.edgetype.AreaEdge;
 import org.opentripplanner.routing.edgetype.AreaEdgeList;
+import org.opentripplanner.routing.edgetype.NamedArea;
 import org.opentripplanner.routing.edgetype.StreetTraversalPermission;
 import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.routing.vertextype.IntersectionVertex;
 import org.opentripplanner.routing.vertextype.StreetVertex;
 import org.opentripplanner.util.I18NString;
 import org.opentripplanner.util.LocalizedString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 // TODO Temporary code until we refactor the WalkableAreaBuilder (#3152)
 class AreaVisibilityAdjuster {
+  private static final Logger LOG = LoggerFactory.getLogger(AreaVisibilityAdjuster.class);
 
   private static final GeometryFactory GEOMETRY_FACTORY = GeometryUtils.getGeometryFactory();
 
@@ -31,11 +38,14 @@ class AreaVisibilityAdjuster {
   static void linkTransitToAreaVertices(Vertex splitterVertex, AreaEdgeList area) {
     List<Vertex> vertices = new ArrayList<>();
 
+    Polygon originalEdges = area.getOriginalEdges();
+
     for (AreaEdge areaEdge : area.getEdges()) {
       if (!vertices.contains(areaEdge.getToVertex())) { vertices.add(areaEdge.getToVertex()); }
       if (!vertices.contains(areaEdge.getFromVertex())) { vertices.add(areaEdge.getFromVertex()); }
     }
 
+    int  linkCount = 0;
     for (Vertex vertex : vertices) {
       if (vertex instanceof StreetVertex && !vertex.equals(splitterVertex)) {
         LineString line = GEOMETRY_FACTORY.createLineString(
@@ -47,29 +57,54 @@ class AreaVisibilityAdjuster {
             splitterVertex.getCoordinate(),
             vertex.getCoordinate()
         );
-        I18NString name = new LocalizedString("", new OSMWithTags());
 
-        edgeFactory.createAreaEdge(
-            (IntersectionVertex) splitterVertex,
-            (IntersectionVertex) vertex,
-            line,
-            name,
-            length,
-            StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE,
-            false,
-            area
-        );
-        edgeFactory.createAreaEdge(
-            (IntersectionVertex) vertex,
-            (IntersectionVertex) splitterVertex,
-            line,
-            name,
-            length,
-            StreetTraversalPermission.PEDESTRIAN_AND_BICYCLE,
-            false,
-            area
-        );
+        NamedArea okArea = null;
+        Geometry polygon;
+
+        for (NamedArea a : area.getAreas()) {
+            polygon = a.getPolygon();
+            Geometry intersection = line.intersection(polygon);
+            if (intersection instanceof LineString) {
+                LineString line2 = (LineString)intersection;
+                if (line2.getNumPoints() != 2) {
+                    continue;
+                }
+                Coordinate p1 = line2.getCoordinateN(0);
+                Coordinate p2 = line2.getCoordinateN(1);
+                double length2 = SphericalDistanceLibrary.distance(p1, p2);
+
+                if (length - length2 < 0.000001) {
+                    okArea = a;
+                    break;
+                }
+            }
+        }
+        if (okArea != null) {
+            linkCount++;
+            I18NString name = new LocalizedString("", new OSMWithTags());
+            edgeFactory.createAreaEdge(
+                (IntersectionVertex) splitterVertex,
+                (IntersectionVertex) vertex,
+                line,
+                name,
+                length,
+                okArea.getPermission(),
+                false,
+                area
+            );
+            edgeFactory.createAreaEdge(
+                (IntersectionVertex) vertex,
+                (IntersectionVertex) splitterVertex,
+                line,
+                name,
+                length,
+                okArea.getPermission(),
+                true,
+                area
+            );
+        }
       }
     }
+    LOG.debug("added {} area edges to link a stop", linkCount);
   }
 }
