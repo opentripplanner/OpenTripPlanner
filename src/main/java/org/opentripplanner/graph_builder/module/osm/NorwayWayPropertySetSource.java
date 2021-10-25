7fa03b91abf061ac187780ae549aae8799d91e47
Bjørn Lie Rapp <bjoern.lr+gis@gmail.com>
2021-10-25 14:41:00 +0200

    Forgot "surface=sand" Although sand can be hard to cycle on, it can also be quite nice to ride on, depending on the type of sand. Safety=100 is an exaggeration.

--- src/main/java/org/opentripplanner/graph_builder/module/osm/NorwayWayPropertySetSource.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/NorwayWayPropertySetSource.java
@@ -792,6 +792,7 @@ public class NorwayWayPropertySetSource implements WayPropertySetSource {
     props.setProperties("surface=earth", StreetTraversalPermission.ALL, 1.5, 1.5, true);
     props.setProperties("surface=grass", StreetTraversalPermission.ALL, 1.5, 1.5, true);
     props.setProperties("surface=mud", StreetTraversalPermission.ALL, 1.5, 1.5, true);
+    props.setProperties("surface=sand", StreetTraversalPermission.ALL, 1.5, 1.5, true);
 
     /*
      * Automobile speeds in Norway. General speed limit is 80kph unless signs says otherwise
