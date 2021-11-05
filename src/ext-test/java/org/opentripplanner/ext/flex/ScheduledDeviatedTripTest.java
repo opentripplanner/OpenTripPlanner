8298061e0a41e72d78b1f0f22b8a039251e7d8aa
Leonard Ehrenfried <mail@leonard.io>
2021-11-05 11:11:36 +0100

    Allow getting on and off at the same stop location for ScheduledDeviatedTrip

--- src/ext-test/java/org/opentripplanner/ext/flex/ScheduledDeviatedTripTest.java
+++ src/ext-test/java/org/opentripplanner/ext/flex/ScheduledDeviatedTripTest.java
@@ -2,6 +2,7 @@ package org.opentripplanner.ext.flex;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Set;
@@ -9,37 +10,33 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.ext.flex.trip.FlexTrip;
-import org.opentripplanner.ext.flex.trip.UnscheduledTrip;
-import org.opentripplanner.model.FeedScopedId;
+import org.opentripplanner.ext.flex.trip.ScheduledDeviatedTrip;
+import org.opentripplanner.model.FlexStopLocation;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graphfinder.NearbyStop;
 
 /**
- * This test makes sure that one of the example feeds in the GTFS-Flex repo works. It's the City of
- * Aspen Downtown taxi service which is a completely unscheduled trip that takes you door-to-door in
- * the city.
+ * This tests that the feed for the Cobb County Flex service is processed correctly. This service
+ * contains both flex zones but also scheduled stops. Inside the zone passengers can get on or off
+ * anywhere so there it works more like a taxi.
  * <p>
- * It only contains a single stop time which in GTFS static would not work but is valid in GTFS
- * Flex.
+ * Read about the details at: https://www.cobbcounty.org/transportation/cobblinc/routes-and-schedules/flex
  */
-public class UnscheduledTripTest extends FlexTest {
+public class ScheduledDeviatedTripTest extends FlexTest {
 
-    static final String ASPEN_GTFS = "src/ext-test/resources/flex/aspen-flex-on-demand.gtfs.zip";
+    static final String COBB_COUNTY_GTFS =
+            "src/ext-test/resources/flex/cobblinc-scheduled-deviated-flex.gtfs.zip";
 
     static Graph graph;
 
     @Test
-    public void parseAspenTaxiAsUnscheduledTrip() {
+    public void parseCobbCountyAsScheduledDeviatedTrip() {
         var flexTrips = graph.flexTripsById.values();
         assertFalse(flexTrips.isEmpty());
-        assertEquals(
-                Set.of("t_1289262_b_29084_tn_0", "t_1289257_b_28352_tn_0"),
-                flexTrips.stream().map(FlexTrip::getId).map(FeedScopedId::getId).collect(
-                        Collectors.toSet())
-        );
+        assertEquals(72, flexTrips.size());
 
         assertEquals(
-                Set.of(UnscheduledTrip.class),
+                Set.of(ScheduledDeviatedTrip.class),
                 flexTrips.stream().map(FlexTrip::getClass).collect(
                         Collectors.toSet())
         );
@@ -57,11 +54,11 @@ public class UnscheduledTripTest extends FlexTest {
                 params
         ).collect(Collectors.toList());
 
-        assertEquals(1, accesses.size());
+        assertEquals(2, accesses.size());
 
         var access = accesses.get(0);
-        assertEquals(0, access.fromStopIndex);
-        assertEquals(0, access.toStopIndex);
+        assertEquals(1, access.fromStopIndex);
+        assertEquals(1, access.toStopIndex);
 
     }
 
@@ -76,20 +73,21 @@ public class UnscheduledTripTest extends FlexTest {
                 params
         ).collect(Collectors.toList());
 
-        assertEquals(1, egresses.size());
+        assertEquals(2, egresses.size());
 
         var egress = egresses.get(0);
-        assertEquals(0, egress.fromStopIndex);
-        assertEquals(0, egress.toStopIndex);
+        assertEquals(1, egress.fromStopIndex);
+        assertEquals(1, egress.toStopIndex);
     }
 
     @BeforeAll
     static void setup() {
-        graph = FlexTest.buildFlexGraph(ASPEN_GTFS);
+        graph = FlexTest.buildFlexGraph(COBB_COUNTY_GTFS);
     }
 
     private static NearbyStop getNearbyStop(FlexTrip trip) {
-        var stopLocation = trip.getStops().iterator().next();
+        var stopLocation = trip.getStops().stream().collect(Collectors.toList()).get(2);
+        assertTrue(stopLocation instanceof FlexStopLocation);
         return new NearbyStop(stopLocation, 0, List.of(), null, null);
     }
 
