0c3fbd7cd5abe722bfc547f7ec93395e147371f6
Leonard Ehrenfried <mail@leonard.io>
2021-11-22 17:28:28 +0100

    Remove unused variable

--- src/main/java/org/opentripplanner/graph_builder/module/TransitToTaggedStopsModule.java
+++ src/main/java/org/opentripplanner/graph_builder/module/TransitToTaggedStopsModule.java
@@ -75,8 +75,7 @@ public class TransitToTaggedStopsModule implements GraphBuilderModule {
             // only connect transit stops that (a) are entrances, or (b) have no associated
             // entrances
             if (ts.isEntrance() || !ts.hasEntrances()) {
-                boolean wheelchairAccessible = ts.hasWheelchairEntrance();
-                if (!connectVertexToStop(ts, wheelchairAccessible)) {
+                if (!connectVertexToStop(ts)) {
                     LOG.debug("Could not connect " + ts.getStopCode() + " at " + ts.getCoordinate().toString());
                     //LOG.warn(graph.addBuilderAnnotation(new StopUnlinked(ts)));
                 }
@@ -84,7 +83,7 @@ public class TransitToTaggedStopsModule implements GraphBuilderModule {
         }
     }
 
-    private boolean connectVertexToStop(TransitStop ts, boolean wheelchairAccessible) {
+    private boolean connectVertexToStop(TransitStop ts) {
         String stopCode = ts.getStopCode();
         if (stopCode == null){
             return false;
