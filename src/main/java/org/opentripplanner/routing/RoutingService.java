38feda70197d9533b52a0352002a7aea3c52cf0c
Hannes Junnila <hannes.junnila@gmail.com>
2021-11-29 18:49:49 +0100

    fix: do proper cleanup

--- src/main/java/org/opentripplanner/routing/RoutingService.java
+++ src/main/java/org/opentripplanner/routing/RoutingService.java
@@ -51,8 +51,21 @@ public class RoutingService {
 
     // TODO We should probably not have the Router as a parameter here
     public RoutingResponse route(RoutingRequest request, Router router) {
-        RoutingWorker worker = new RoutingWorker(request, router);
-        return worker.route();
+        RoutingResponse response;
+        try {
+            RoutingWorker worker = new RoutingWorker(
+                    router,
+                    request
+            );
+            response = worker.route();
+        } catch (Exception e) {
+            if (request != null) {
+                request.cleanup();
+            }
+            throw e;
+        }
+        request.cleanup();
+        return response;
     }
 
     /**
