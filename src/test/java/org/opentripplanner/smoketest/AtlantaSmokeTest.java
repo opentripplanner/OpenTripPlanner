d12b35ca7d26e70f0de5ca1ff17d4cde1f68d335
Leonard Ehrenfried <mail@leonard.io>
2021-12-09 11:37:30 +0100

    Add smoke test files

---
+++ src/test/java/org/opentripplanner/smoketest/AtlantaSmokeTest.java
@@ -0,0 +1,15 @@
+package org.opentripplanner.smoketest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SmokeTest.class)
+public class AtlantaSmokeTest {
+
+    @Test
+    public void shouldFail(){
+        fail();
+    }
+}
