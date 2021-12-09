8c1e179053890e1d4a95d58d0a9424d349ea47e4
Leonard Ehrenfried <mail@leonard.io>
2021-12-09 14:27:35 +0100

    Make code more compact

--- src/test/java/org/opentripplanner/smoketest/AtlantaSmokeTest.java
+++ src/test/java/org/opentripplanner/smoketest/AtlantaSmokeTest.java
@@ -3,71 +3,50 @@ package org.opentripplanner.smoketest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.opentripplanner.api.json.JSONObjectMapperProvider;
 import org.opentripplanner.api.resource.TripPlannerResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Category(SmokeTest.class)
 public class AtlantaSmokeTest {
+
     private static final Logger LOG = LoggerFactory.getLogger(AtlantaSmokeTest.class);
 
     HttpClient client = HttpClient.newHttpClient();
 
-    static LocalDate nextMonday() {
-        return LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY));
-    }
-
     @Test
-    public void returnRouteFromCentralAtlantaToPowerSprings() throws IOException, InterruptedException {
+    public void returnRouteFromCentralAtlantaToPowderSprings()
+    throws IOException, InterruptedException {
         var params = Map.of(
                 "fromPlace", "33.714059324224124,-84.37225341796875",
                 "toPlace", "33.882387270695105,-84.51576232910155",
                 "time", "1:00pm",
-                "date", nextMonday().toString(),
+                "date", SmokeTest.nextMonday().toString(),
                 "mode", "TRANSIT,WALK",
                 "showIntermediateStops", "true",
                 "locale", "en"
-        )
-                .entrySet()
-                .stream()
-                .map(kv -> kv.getKey() + "=" + kv.getValue())
-                .collect(Collectors.joining("&"));
+        );
 
-        var uri = URI.create("http://localhost:8080/otp/routers/default/plan?" + params);
-
-        var request = HttpRequest.newBuilder()
-                .uri(uri)
-                .GET()
-                .build();
+        var request = SmokeTest.planRequest(params);
 
         var response = client.send(request, BodyHandlers.ofInputStream());
 
         assertEquals(200, response.statusCode(), "Status code returned by OTP server was not 200");
 
-        var provider = new JSONObjectMapperProvider();
-        var mapper = provider.getContext(null);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
+        var tripPlannerResponse =
+                SmokeTest.mapper.readValue(response.body(), TripPlannerResponse.class);
 
-        var tripPlannerResponse = mapper.readValue(response.body(), TripPlannerResponse.class);
-
-        LOG.info("Request to {} returned {} itineraries", uri, tripPlannerResponse.getPlan().itineraries.size());
+        LOG.info(
+                "Request to {} returned {} itineraries",
+                request.uri(),
+                tripPlannerResponse.getPlan().itineraries.size()
+        );
 
         assertTrue(tripPlannerResponse.getPlan().itineraries.size() > 1);
     }
--- src/test/java/org/opentripplanner/smoketest/SmokeTest.java
+++ src/test/java/org/opentripplanner/smoketest/SmokeTest.java
@@ -1,6 +1,56 @@
 package org.opentripplanner.smoketest;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.opentripplanner.api.json.JSONObjectMapperProvider;
+
 /**
- * This is just a marker class for use as a JUnit category.
+ * This is both a utility class and a category to select or deselect smoke tests during test
+ * execution.
+ * <p>
+ * By default, the smoke tests are not run when you execute `mvn test`.
+ * <p>
+ * If you want run them use the following command: `mvn test -P smoke-tests`
  */
-public class SmokeTest {}
+public class SmokeTest {
+
+    static final ObjectMapper mapper;
+
+    static {
+        var provider = new JSONObjectMapperProvider();
+        mapper = provider.getContext(null);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
+    }
+
+    static LocalDate nextMonday() {
+        return LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+    }
+
+
+    static HttpRequest planRequest(Map<String, String> params) {
+        var urlParams = params.entrySet()
+                .stream()
+                .map(kv -> kv.getKey() + "=" + kv.getValue())
+                .collect(Collectors.joining("&"));
+
+        var uri = URI.create("http://localhost:8080/otp/routers/default/plan?" + urlParams);
+
+        return HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+
+    }
+
+
+}
