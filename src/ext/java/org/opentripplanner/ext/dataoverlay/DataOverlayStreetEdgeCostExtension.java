846536ce5737aba86562f10d04ea5d5c9d91442e
Katja Danilova <katja.danilova@metatavu.fi>
2021-12-21 20:50:04 +0200

    changed the request instant method in plugin to fit the dev-2.x

--- src/ext/java/org/opentripplanner/ext/dataoverlay/DataOverlayStreetEdgeCostExtension.java
+++ src/ext/java/org/opentripplanner/ext/dataoverlay/DataOverlayStreetEdgeCostExtension.java
@@ -66,7 +66,7 @@ class DataOverlayStreetEdgeCostExtension implements StreetEdgeCostExtension, Ser
     private double calculateDataOverlayPenalties(RoutingRequest requestOptions) {
         if(variableValues == null) { return 0d; }
         double totalPenalty = 0d;
-        Instant requestInstant = requestOptions.getDateTime().toInstant();
+        Instant requestInstant = requestOptions.getDateTimeOriginalSearch();
         var context = requestOptions.getRoutingContext().dataOverlayContext;
 
         for (Parameter parameter : context.getParameters()) {
