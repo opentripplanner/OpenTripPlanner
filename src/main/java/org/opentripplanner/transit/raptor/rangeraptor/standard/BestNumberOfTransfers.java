b85ab44acacaf1245beeeac68ee8e1f1a9e9b021
Hannes Junnila <hannes.junnila@gmail.com>
2022-01-31 12:12:11 +0200

    Improve documentation

--- src/main/java/org/opentripplanner/transit/raptor/rangeraptor/standard/BestNumberOfTransfers.java
+++ src/main/java/org/opentripplanner/transit/raptor/rangeraptor/standard/BestNumberOfTransfers.java
@@ -20,8 +20,11 @@ public interface BestNumberOfTransfers {
      * <p/>
      * This method is called after the search is complete, not before.
      * <p/>
-     * The rusult is used to calculate heuristics, so the calculated value
-     * should be less than or equals to the "real vaule".
+     * The result is used to calculate heuristics, so the calculated value
+     * should be less than or equal to the "real value". The value should be at most:
+     * -1 for a stop which is directly reachable via a street access/egress
+     * 0 for a stop which can be reached via a single transit leg
+     * 1 for a stop which can be reached via two separate transit legs
      */
     int calculateMinNumberOfTransfers(int stop);
 }
