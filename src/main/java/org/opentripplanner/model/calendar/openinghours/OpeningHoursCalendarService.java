4132918098147d7b34afc065a0494088eb22a68d
Thomas Gran <t2gran@gmail.com>
2022-02-04 17:33:37 +0100

    Opening hours initial API design

---
+++ src/main/java/org/opentripplanner/model/calendar/openinghours/OpeningHoursCalendarService.java
@@ -0,0 +1,27 @@
+package org.opentripplanner.model.calendar.openinghours;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import org.opentripplanner.routing.trippattern.Deduplicator;
+
+public class OpeningHoursCalendarService {
+
+    private final Deduplicator deduplicator;
+    private final LocalDate startOfPeriod;
+    private final int daysInPeriod;
+
+    public OpeningHoursCalendarService(
+            Deduplicator deduplicator,
+            LocalDate startOfPeriod,
+            LocalDate endOfPeriod
+    ) {
+        this.deduplicator = deduplicator;
+        this.startOfPeriod = startOfPeriod;
+        this.daysInPeriod = Period.between(startOfPeriod, endOfPeriod).getDays();
+    }
+
+    public OHCalendarBuilder newBuilder(ZoneId zoneId) {
+        return new OHCalendarBuilder(deduplicator);
+    }
+}
