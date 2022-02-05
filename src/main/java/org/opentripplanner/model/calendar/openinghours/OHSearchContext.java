36206546305f44f6bcf8508e0cbd53c3ed46db7c
Thomas Gran <t2gran@gmail.com>
2022-02-06 00:52:55 +0100

    Add zoneId to calendar and description to Opening Hours
     This enable us to support timezones at calendar level.

--- src/main/java/org/opentripplanner/model/calendar/openinghours/OHSearchContext.java
+++ src/main/java/org/opentripplanner/model/calendar/openinghours/OHSearchContext.java
@@ -1,23 +1,32 @@
 package org.opentripplanner.model.calendar.openinghours;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public class OHSearchContext {
+    private final Instant searchStartTime;
+    private final Instant searchEndTime;
 
-    public OHSearchContext(Instant searchStartTime) {
-
+    public OHSearchContext(Instant searchStartTime, Duration maxJourneyDuration) {
+        if(maxJourneyDuration.isNegative()) {
+            this.searchStartTime = searchStartTime.plus(maxJourneyDuration);
+            this.searchEndTime = searchStartTime;
+        }
+        else {
+            this.searchStartTime = searchStartTime;
+            this.searchEndTime = searchStartTime.plus(maxJourneyDuration);
+        }
     }
 
-
-    public boolean isOpen(OHCalendar ctx, long epochSecond) {
+    public boolean isOpen(OHCalendar calendar, long timeEpochSecond) {
         return true;
     }
 
-    public boolean enterOk(OHCalendar ctx, long epochSecond) {
-        return isOpen(ctx, epochSecond);
+    public boolean canEnter(OHCalendar calendar, long epochSecond) {
+        return isOpen(calendar, epochSecond);
     }
 
-    public boolean exitOk(OHCalendar ctx, long epochSecond) {
+    public boolean canExit(OHCalendar calendar, long epochSecond) {
         return false;
     }
 }
--- src/test/java/org/opentripplanner/model/calendar/openinghours/OHCalendarTest.java
+++ src/test/java/org/opentripplanner/model/calendar/openinghours/OHCalendarTest.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.model.calendar.openinghours;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -24,45 +25,55 @@ class OHCalendarTest {
         );
 
         // Create a OHCalendarBuilder for each entity with opening hours
-        var cBuilder = service.newBuilder(zoneId);
+        var calBuilder = service.newBuilder(zoneId);
 
         // Simple case 08:00- 16:30  April 1st to April 3rd
-        cBuilder.withHours(time(8,0), time(16,30))
-                .onDate(date(Month.APRIL, 1))
-                .onDate(date(Month.APRIL, 2))
-                .onDate(date(Month.APRIL, 3))
+        calBuilder.openingHours("1-3. April", time(8,0), time(16,30))
+                .on(date(Month.APRIL, 1))
+                .on(date(Month.APRIL, 2))
+                .on(date(Month.APRIL, 3))
                 .add();
 
         // DST is 27. March - What should we do with th hours from 02:00 to 03:00 (apair twice)
-        cBuilder.withHours(time(1,0), time(3,0))
-                .onDate(date(Month.OCTOBER, 25))
+        calBuilder.openingHours("OCT-25", time(1,0), time(3,0))
+                .on(date(Month.OCTOBER, 25))
                 .add();
 
         //  May 17th 22:00 - May 18th 03:00
-        cBuilder.withHours(time(22,0), time(27,0))
-                .onDate(date(Month.MAY, 17))
+        calBuilder.openingHours("17th May", time(22,0), time(23,59))
+                .on(date(Month.MAY, 17))
                 .add();
+
+        //  Possibilities for the adding date methods:
+        //  - on(LocalDate)
+        //  - on(DayOfWeek)
+        //  - everyDay()
+        //  - except(LocalDate)
+        //  - except(DayOfWeek)
+        // The methods will be applied in the order they are called...
+
 
         // Lightweight calendar with all the opening hours above is created and can
         // be assigned to entity
-        var c = cBuilder.build();
+        var c = calBuilder.build();
 
 
         ///////// ROUTING SEARCH  /////////
 
         // The start of the search, this is used to optimize the calculation
-        Instant dateTime = Instant.parse("2022-10-25T03:30Z");
+        Instant dateTime = Instant.parse("2022-10-25T03:30:00Z");
         long time = dateTime.getEpochSecond();
 
-        // The context is used to cache calculations for a search
-        var ctx = new OHSearchContext(dateTime);
+        // The context is used to cache calculations for a search, use negative
+        // duration for arriveBy search
+        var ctx = new OHSearchContext(dateTime, Duration.ofHours(36));
 
         // Is open at specific time?
         boolean ok = ctx.isOpen(c, time - 1234);
 
         // Open in hole period from enter to exit?
-        boolean okToEnter = ctx.enterOk(c, time + 1234);
-        boolean okToExit = ctx.exitOk(c, time + 2345);
+        boolean okToEnter = ctx.canEnter(c, time + 1234);
+        boolean okToExit = ctx.canExit(c, time + 2345);
     }
 
     private static LocalDate date(Month march, int i) {
