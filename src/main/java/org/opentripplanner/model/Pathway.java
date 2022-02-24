850234e6ba261604474e0ef22cc86bc28d4f3bad
Miguel Rodríguez Sorni <miguel.rodriguez-sorni.ext@inetum.world>
2022-02-24 17:56:35 +0100

    feat: change pathway model to use lombok

--- src/main/java/org/opentripplanner/model/Pathway.java
+++ src/main/java/org/opentripplanner/model/Pathway.java
@@ -1,6 +1,11 @@
 /* This file is based on code copied from project OneBusAway, see the LICENSE file for further information. */
 package org.opentripplanner.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public final class Pathway extends TransitEntity {
 
     private static final long serialVersionUID = -2404871423254094109L;
@@ -27,86 +32,6 @@ public final class Pathway extends TransitEntity {
 
     public Pathway(FeedScopedId id) {
         super(id);
-    }
-
-    public void setPathwayMode(int pathwayMode) {
-        this.pathwayMode = pathwayMode;
-    }
-
-    public int getPathwayMode() {
-        return pathwayMode;
-    }
-
-    public void setFromStop(StationElement fromStop) {
-        this.fromStop = fromStop;
-    }
-
-    public StationElement getFromStop() {
-        return fromStop;
-    }
-
-    public void setToStop(StationElement toStop) {
-        this.toStop = toStop;
-    }
-
-    public StationElement getToStop() {
-        return toStop;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getReversedName() {
-        return reversedName;
-    }
-
-    public void setReversedName(String reversedName) {
-        this.reversedName = reversedName;
-    }
-
-    public void setTraversalTime(int traversalTime) {
-        this.traversalTime = traversalTime;
-    }
-
-    public int getTraversalTime() {
-        return traversalTime;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public boolean isBidirectional() {
-        return isBidirectional;
-    }
-
-    public void setBidirectional(boolean bidirectional) {
-        isBidirectional = bidirectional;
-    }
-
-    public int getStairCount() {
-        return stairCount;
-    }
-
-    public void setStairCount(int stairCount) {
-        this.stairCount = stairCount;
-    }
-
-    public double getSlope() {
-        return slope;
-    }
-
-    public void setSlope(double slope) {
-        this.slope = slope;
     }
 
     @Override
