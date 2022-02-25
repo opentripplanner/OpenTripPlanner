7e0134576f09538b7efcc308051c552d9a6cf0d9
Jan Schoeman <jan@whereismytransport.com>
2022-02-25 10:35:38 +0200

    reverted hack on time shifting

--- src/main/java/org/opentripplanner/transit/raptor/api/path/PathBuilderLeg.java
+++ src/main/java/org/opentripplanner/transit/raptor/api/path/PathBuilderLeg.java
@@ -222,7 +222,7 @@ public class PathBuilderLeg<T extends RaptorTripSchedule> {
     public void timeShiftThisAndNextLeg(RaptorSlackProvider slackProvider) {
         if(isAccess()) { timeShiftAccessTime(slackProvider); }
         if(next != null) {
-            if (next.isTransfer() && (this.isAccess() || this.isTransit())) { next.timeShiftTransferTime(slackProvider); }
+            if (next.isTransfer()) { next.timeShiftTransferTime(slackProvider); }
             else if (next.isEgress()) { next.timeShiftEgressTime(slackProvider); }
         }
     }
