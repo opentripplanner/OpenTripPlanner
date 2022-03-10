f3e2f6c4d45e93afcc553636ce2af66f8f38d548
Leonard Ehrenfried <mail@leonard.io>
2022-03-10 13:00:20 +0100

    Port vehicle positions test and add new ones

---
+++ src/test/java/org/opentripplanner/updater/vehicle_positions/VehiclePositionParsingTest.java
@@ -0,0 +1,41 @@
+package org.opentripplanner.updater.vehicle_positions;
+
+import java.io.File;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VehiclePositionParsingTest {
+
+    @Test
+    public void parseFirstPositionsFeed() {
+        var vehiclePositionSource = getVehiclePositionSource("king-county-metro-1.pb");
+        var positions = vehiclePositionSource.getPositions();
+
+        Assertions.assertNotNull(positions);
+
+        Assertions.assertEquals(627, positions.size());
+
+        var first = positions.get(0);
+        Assertions.assertEquals("49195152", first.getTrip().getTripId());
+    }
+
+    @Test
+    public void parseSecondPositionsFeed() {
+        var vehiclePositionSource = getVehiclePositionSource("king-county-metro-2.pb");
+        var positions = vehiclePositionSource.getPositions();
+
+        Assertions.assertNotNull(positions);
+
+        Assertions.assertEquals(570, positions.size());
+
+        var first = positions.get(0);
+        Assertions.assertEquals("49195157", first.getTrip().getTripId());
+    }
+
+    private GtfsRealtimeFileVehiclePositionSource getVehiclePositionSource(String filename) {
+        return new GtfsRealtimeFileVehiclePositionSource(
+                "1",
+                new File("src/test/resources/vehicle-positions/" + filename)
+        );
+    }
+}
