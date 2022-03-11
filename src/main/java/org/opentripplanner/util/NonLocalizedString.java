6feaa1805ed9947b8d931fc0ba11d63ed6ed2db5
Hannes Junnila <hannes.junnila@gmail.com>
2022-03-11 13:28:24 +0200

    Add default name to endpoints, when no itineraries found

--- src/main/java/org/opentripplanner/util/NonLocalizedString.java
+++ src/main/java/org/opentripplanner/util/NonLocalizedString.java
@@ -33,6 +33,30 @@ public class NonLocalizedString implements I18NString, Serializable {
         return new NonLocalizedString(name);
     }
 
+    /**
+     * Check if name is non-null and returns an instance of {@link NonLocalizedString}, otherwise
+     * returns a {@link NonLocalizedString} with the default name.
+     */
+    @Nonnull
+    public static NonLocalizedString ofNullableOrElse(
+            @Nullable String name,
+            @Nonnull String defaultName
+    ){
+        return new NonLocalizedString(name == null ? defaultName : name);
+    }
+
+    /**
+     * Check if name is non-null and returns an instance of {@link NonLocalizedString},  otherwise
+     *      * returns a {@link I18NString} with the default name.
+     */
+    @Nonnull
+    public static I18NString ofNullableOrElse(
+            @Nullable String name,
+            @Nonnull I18NString defaultName
+    ){
+        return name == null ? defaultName : new NonLocalizedString(name);
+    }
+
     @Override
     public boolean equals(Object other) {
         return other instanceof NonLocalizedString && this.name.equals(((NonLocalizedString)other).name);
