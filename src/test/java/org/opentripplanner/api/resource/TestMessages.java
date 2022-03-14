c439d08c5931fb0bbac17fde56d5be076ee87f5d
Thomas Gran <t2gran@gmail.com>
2022-03-14 21:04:21 +0100

    fix: Duplicate messages files #1477

--- src/test/java/org/opentripplanner/api/resource/TestMessages.java
+++ src/test/java/org/opentripplanner/api/resource/TestMessages.java
@@ -1,29 +1,47 @@
 package org.opentripplanner.api.resource;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 import org.opentripplanner.api.common.Message;
 
 import java.util.Locale;
 
-public class TestMessages extends TestCase {
+import static java.util.Locale.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+public class TestMessages {
+
+    @Test
     public void testLanguages() {
+        Locale es = forLanguageTag("es");
+        Locale hu = forLanguageTag("hu");
+        Locale nl = forLanguageTag("nl");
+
+
+        for (var m : Message.values()) {
+            String name = m.name();
+            System.out.println(name);
+            
+            assertFalse(m.get(ENGLISH).isBlank(), "Message code missing(ENGLISH): " + name);
+            assertFalse(m.get(GERMANY).isBlank(), "Message code missing(GERMANY): " + name);
+            assertFalse(m.get(FRENCH).isBlank(), "Message code missing(FRENCH): " + name);
+            assertFalse(m.get(es).isBlank(), "Message code missing(ES): " + name);
+            assertFalse(m.get(hu).isBlank(), "Message code missing(HU): " + name);
+            assertFalse(m.get(nl).isBlank(), "Message code missing(NL): " + name);
+        }
+    }
+
+    @Test
+    public void testDefaultLanguage() {
         // Keep default locale so we can set it back after the test is done
         Locale sysDefaultLocale = Locale.getDefault();
         try {
             // Force default to make test work on non-US machines
-            Locale.setDefault(new Locale("en", "US"));
+            Locale.setDefault(Locale.GERMANY);
 
-            String e = Message.PATH_NOT_FOUND.get();
-            String f = Message.PATH_NOT_FOUND.get(Locale.CANADA_FRENCH);
-            String s = Message.PATH_NOT_FOUND.get(new Locale("es"));
-
-            TestCase.assertNotNull(e);
-            TestCase.assertNotNull(f);
-            TestCase.assertNotNull(s);
-            TestCase.assertNotSame(e, f);
-            TestCase.assertNotSame(e, s);
-            TestCase.assertNotSame(f, s);
+            for (var m : Message.values()) {
+                assertEquals(m.get(), m.get(GERMANY));
+            }
         }
         finally {
             Locale.setDefault(sysDefaultLocale);
