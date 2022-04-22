1123126cd24c603965459181950a0b005ae15d76
Leonard Ehrenfried <mail@leonard.io>
2022-04-22 13:05:49 +0200

    Autoformat smoke tests

--- src/test/java/org/opentripplanner/smoketest/AtlantaSmokeTest.java
+++ src/test/java/org/opentripplanner/smoketest/AtlantaSmokeTest.java
@@ -23,59 +23,76 @@ import org.junit.experimental.categories.Category;
 @Category(SmokeTest.class)
 public class AtlantaSmokeTest {
 
-    String nearGeorgiaStateStation = "33.74139944890028,-84.38607215881348";
-    String powderSpringsInsideFlexZone1 = "33.86916840022388,-84.66315507888794";
+  String nearGeorgiaStateStation = "33.74139944890028,-84.38607215881348";
+  String powderSpringsInsideFlexZone1 = "33.86916840022388,-84.66315507888794";
 
-    @Test
-    public void regularRouteFromCentralAtlantaToPowderSprings() {
-        var params = Map.of(
-                "fromPlace", nearGeorgiaStateStation,
-                "toPlace", powderSpringsInsideFlexZone1,
-                "time", "1:00pm",
-                "date", SmokeTest.nextMonday().toString(),
-                "mode", "TRANSIT,WALK",
-                "showIntermediateStops", "true",
-                "locale", "en",
-                "searchWindow", Long.toString(Duration.ofHours(2).toSeconds())
-        );
-        var otpResponse = SmokeTest.sendPlanRequest(params);
-        var itineraries = otpResponse.getPlan().itineraries;
+  @Test
+  public void regularRouteFromCentralAtlantaToPowderSprings() {
+    var params = Map.of(
+      "fromPlace",
+      nearGeorgiaStateStation,
+      "toPlace",
+      powderSpringsInsideFlexZone1,
+      "time",
+      "1:00pm",
+      "date",
+      SmokeTest.nextMonday().toString(),
+      "mode",
+      "TRANSIT,WALK",
+      "showIntermediateStops",
+      "true",
+      "locale",
+      "en",
+      "searchWindow",
+      Long.toString(Duration.ofHours(2).toSeconds())
+    );
+    var otpResponse = SmokeTest.sendPlanRequest(params);
+    var itineraries = otpResponse.getPlan().itineraries;
 
-        assertTrue(itineraries.size() > 1);
+    assertTrue(itineraries.size() > 1);
 
-        var expectedModes = List.of("WALK", "SUBWAY", "WALK", "BUS", "WALK", "BUS", "WALK");
-        assertThatItineraryHasModes(itineraries, expectedModes);
-    }
+    var expectedModes = List.of("WALK", "SUBWAY", "WALK", "BUS", "WALK", "BUS", "WALK");
+    assertThatItineraryHasModes(itineraries, expectedModes);
+  }
 
-    @Test
-    public void flexRouteFromCentralAtlantaToPowderSprings() {
-        var params = Map.of(
-                "fromPlace", nearGeorgiaStateStation,
-                "toPlace", powderSpringsInsideFlexZone1,
-                "time", "1:00pm",
-                "date", SmokeTest.nextMonday().toString(),
-                "mode", "FLEX_EGRESS,WALK,TRANSIT",
-                "showIntermediateStops", "true",
-                "locale", "en",
-                "searchWindow", Long.toString(Duration.ofHours(2).toSeconds())
-        );
-        var otpResponse = SmokeTest.sendPlanRequest(params);
-        var itineraries = otpResponse.getPlan().itineraries;
+  @Test
+  public void flexRouteFromCentralAtlantaToPowderSprings() {
+    var params = Map.of(
+      "fromPlace",
+      nearGeorgiaStateStation,
+      "toPlace",
+      powderSpringsInsideFlexZone1,
+      "time",
+      "1:00pm",
+      "date",
+      SmokeTest.nextMonday().toString(),
+      "mode",
+      "FLEX_EGRESS,WALK,TRANSIT",
+      "showIntermediateStops",
+      "true",
+      "locale",
+      "en",
+      "searchWindow",
+      Long.toString(Duration.ofHours(2).toSeconds())
+    );
+    var otpResponse = SmokeTest.sendPlanRequest(params);
+    var itineraries = otpResponse.getPlan().itineraries;
 
-        assertTrue(itineraries.size() > 0);
+    assertTrue(itineraries.size() > 0);
 
-        var expectedModes = List.of("WALK", "SUBWAY", "WALK", "BUS", "WALK", "BUS");
-        assertThatItineraryHasModes(itineraries, expectedModes);
+    var expectedModes = List.of("WALK", "SUBWAY", "WALK", "BUS", "WALK", "BUS");
+    assertThatItineraryHasModes(itineraries, expectedModes);
 
-        var transitLegs = itineraries.stream()
-                .flatMap(i -> i.legs.stream().filter(l -> l.transitLeg))
-                .collect(Collectors.toList());
+    var transitLegs = itineraries
+      .stream()
+      .flatMap(i -> i.legs.stream().filter(l -> l.transitLeg))
+      .collect(Collectors.toList());
 
-        var usesZone1Route = transitLegs.stream()
-                .map(l -> l.routeShortName)
-                .anyMatch(name -> name.equals("Zone 1"));
+    var usesZone1Route = transitLegs
+      .stream()
+      .map(l -> l.routeShortName)
+      .anyMatch(name -> name.equals("Zone 1"));
 
-        assertTrue(usesZone1Route);
-
-    }
+    assertTrue(usesZone1Route);
+  }
 }
