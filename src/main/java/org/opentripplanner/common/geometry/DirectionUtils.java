892b3b732550f932131bcd8f89451931820cf411
Vlad Vesa <vvesa@idirect.net>
2022-04-24 10:35:53 +0300

    remove barely used commons-math3 dependency and use internal java alternative

--- src/main/java/org/opentripplanner/common/geometry/DirectionUtils.java
+++ src/main/java/org/opentripplanner/common/geometry/DirectionUtils.java
@@ -1,6 +1,5 @@
 package org.opentripplanner.common.geometry;
 
-import org.apache.commons.math3.util.FastMath;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.LineString;
@@ -17,13 +16,13 @@ public class DirectionUtils {
    * in the range (-180° to +180°). The computation is exact for small delta between A and B.
    */
   public static synchronized double getAzimuth(Coordinate a, Coordinate b) {
-    double cosLat = FastMath.cos(FastMath.toRadians((a.y + b.y) / 2.0));
+    double cosLat = Math.cos(Math.toRadians((a.y + b.y) / 2.0));
     double dY = (b.y - a.y); // in degrees, we do not care about the units
     double dX = (b.x - a.x) * cosLat; // same
     if (Math.abs(dX) < 1e-10 && Math.abs(dY) < 1e-10) {
       return 180;
     }
-    return FastMath.toDegrees(FastMath.atan2(dX, dY));
+    return Math.toDegrees(Math.atan2(dX, dY));
   }
 
   /**
