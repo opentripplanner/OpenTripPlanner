6a120fa23b3c5c57bde571a4c1205c370f54cf06
Leonard Ehrenfried <mail@leonard.io>
2022-04-26 10:46:51 +0200

    Convert tests to Junit 5

--- src/test/java/org/opentripplanner/openstreetmap/model/OSMWayTest.java
+++ src/test/java/org/opentripplanner/openstreetmap/model/OSMWayTest.java
@@ -1,9 +1,9 @@
 package org.opentripplanner.openstreetmap.model;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.opentripplanner.common.model.P2;
 import org.opentripplanner.graph_builder.module.osm.OSMFilter;
 import org.opentripplanner.graph_builder.module.osm.WayProperties;
