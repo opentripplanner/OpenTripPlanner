62ea2e68f05e8512ff8d1f8b03369001a0a7acd7
Hannes Junnila <hannes.junnila@gmail.com>
2022-04-27 11:18:25 +0300

    Read headsign for frequency-only patterns

--- src/ext/java/org/opentripplanner/ext/vectortiles/layers/stops/DigitransitStopPropertyMapper.java
+++ src/ext/java/org/opentripplanner/ext/vectortiles/layers/stops/DigitransitStopPropertyMapper.java
@@ -10,6 +10,7 @@ import org.json.simple.JSONObject;
 import org.opentripplanner.common.model.T2;
 import org.opentripplanner.ext.vectortiles.PropertyMapper;
 import org.opentripplanner.model.Stop;
+import org.opentripplanner.model.Timetable;
 import org.opentripplanner.model.TripPattern;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.vertextype.TransitStopVertex;
@@ -47,9 +48,16 @@ public class DigitransitStopPropertyMapper extends PropertyMapper<TransitStopVer
         .stream()
         .map(tripPattern -> {
           int stopPos = tripPattern.findStopPosition(stop);
-          var headsign = stopPos < 0
-            ? "Not Available"
-            : tripPattern.getScheduledTimetable().getTripTimes().get(0).getHeadsign(stopPos);
+          Timetable scheduledTimetable = tripPattern.getScheduledTimetable();
+          String headsign;
+          if (stopPos > -1 && !scheduledTimetable.getTripTimes().isEmpty()) {
+            headsign = scheduledTimetable.getTripTimes().get(0).getHeadsign(stopPos);
+          } else if (stopPos > -1 && !scheduledTimetable.getFrequencyEntries().isEmpty()) {
+            headsign =
+              scheduledTimetable.getFrequencyEntries().get(0).tripTimes.getHeadsign(stopPos);
+          } else {
+            headsign = "Not Available";
+          }
           JSONObject pattern = new JSONObject();
           pattern.put("headsign", headsign);
           pattern.put("type", tripPattern.getRoute().getMode().name());
