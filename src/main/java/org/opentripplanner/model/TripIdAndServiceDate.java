07bf56d22c17cdfa3f971843201137d8b51386b0
Mikael Jarfors <mikael.jarfors@decerno.se>
2022-04-28 13:54:56 +0200

    Remove Unecessary functions from record

--- src/main/java/org/opentripplanner/model/TripIdAndServiceDate.java
+++ src/main/java/org/opentripplanner/model/TripIdAndServiceDate.java
@@ -6,27 +6,4 @@ import org.opentripplanner.model.calendar.ServiceDate;
 /**
  * Class to use as key in HashMap containing feed id, trip id and service date
  */
-public record TripIdAndServiceDate(FeedScopedId tripId, ServiceDate serviceDate) {
-  @Override
-  public int hashCode() {
-    return Objects.hash(tripId, serviceDate);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    TripIdAndServiceDate other = (TripIdAndServiceDate) obj;
-    return (
-      Objects.equals(this.tripId, other.tripId) &&
-      Objects.equals(this.serviceDate, other.serviceDate)
-    );
-  }
-}
+public record TripIdAndServiceDate(FeedScopedId tripId, ServiceDate serviceDate) {}
