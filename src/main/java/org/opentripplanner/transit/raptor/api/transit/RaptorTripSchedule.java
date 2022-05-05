5375583119c269304d75266e97a6f62df9e744e4
Bartosz-Kruba <98400292+Bartosz-Kruba@users.noreply.github.com>
2022-05-05 08:56:44 +0200

    Update src/main/java/org/opentripplanner/transit/raptor/api/transit/RaptorTripSchedule.java
    Co-authored-by: Thomas Gran <t2gran@gmail.com>

--- src/main/java/org/opentripplanner/transit/raptor/api/transit/RaptorTripSchedule.java
+++ src/main/java/org/opentripplanner/transit/raptor/api/transit/RaptorTripSchedule.java
@@ -118,11 +118,10 @@ public interface RaptorTripSchedule {
 
   /**
    * Find all departure-stop-positions for a stop index after given earliest-departure-time. This is
-   * useful because trip can pass through the same stop more than once.
-   * This is basically on upgraded version of {@link #findDepartureStopPosition} that does not return
-   * only first stop position but all of them.
-   * @param earliestDepartureTime earliest departure time
-   * @param stop stop index
+   * useful because trip can pass through the same stop more than once - if the stop pattern is circular.
+   * This method returns all stop positions, while the {@link #findDepartureStopPosition} only returns the first 
+   * stop-position found.
+   *
    * @return list of all valid stop positions for a given stop index
    */
   default List<Integer> findStopPositionsForStopIndex(int earliestDepartureTime, int stop) {
