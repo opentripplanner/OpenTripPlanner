bd81b857e19056a3adc55c7060d1ca6e4b1203e9
Thomas Gran <t2gran@gmail.com>
2022-05-06 17:47:43 +0200

    fix: Branding in GTFS have id=null, a TransitEntity MUST have an id.

--- src/main/java/org/opentripplanner/gtfs/mapping/BrandingMapper.java
+++ src/main/java/org/opentripplanner/gtfs/mapping/BrandingMapper.java
@@ -3,6 +3,7 @@ package org.opentripplanner.gtfs.mapping;
 import javax.annotation.Nullable;
 import org.onebusaway.gtfs.model.Route;
 import org.opentripplanner.model.Branding;
+import org.opentripplanner.model.FeedScopedId;
 
 /** Responsible for mapping GTFS Route into the OTP Branding model. */
 public class BrandingMapper {
@@ -18,7 +19,13 @@ public class BrandingMapper {
     if (route.getBrandingUrl() == null) {
       return null;
     }
-
-    return new Branding(null, null, null, route.getBrandingUrl(), null, null);
+    return new Branding(
+      new FeedScopedId(route.getId().getAgencyId(), route.getBrandingUrl()),
+      null,
+      null,
+      route.getBrandingUrl(),
+      null,
+      null
+    );
   }
 }
