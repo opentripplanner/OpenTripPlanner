d970b523372ec69437938c8747ffd42765b727d5
Thomas Gran <t2gran@gmail.com>
2022-05-20 14:46:40 +0200

    refactor:Add copy() to TransitObject, and doc.

--- src/main/java/org/opentripplanner/transit/model/basic/TransitObject.java
+++ src/main/java/org/opentripplanner/transit/model/basic/TransitObject.java
@@ -15,5 +15,32 @@ public interface TransitObject<E extends TransitObject<E, T>, T extends Abstract
    */
   boolean sameValue(@Nonnull E other);
 
+  /**
+   * The copy method is used to mutate the existing object by creating a builder and setting
+   * the "original".
+   * <p>
+   * Note! Do not mutate nested transit entities. When crossing aggregate root boundaries, then
+   * you need to fetch the entity from the transit service before changing it. Changing a nested
+   * entity will cause the data to become inconsistent. For example:
+   * <pre>
+   *
+   * Trip t = (get from transit service)
+   *
+   * // This is NOT OK - the new agency is not consistent with the agency in the transit service
+   * var a = t.getAgency().copy().withXYZ(...).build();
+   * t.setAgency(a);
+   *
+   * // Instead
+   * Trip t = (get from transit service)
+   * Agency a = (get from transit service)
+   *
+   * // This is ok
+   * a = a.copy().withXYZ(...).build();
+   * t.copy().withAgency(a).build();
+   * </pre>
+   *
+   * The exception is when a builder reference another builder, and not the entity object. The
+   * nested entity is a <em>composed</em> part of the parent, and not shared with other entities.
+   */
   AbstractBuilder<E, T> copy();
 }
