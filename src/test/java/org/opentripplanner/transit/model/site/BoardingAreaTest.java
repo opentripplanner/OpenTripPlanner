dddefdd287bf744b25ea09b6c42cf18888ba5838
vincent <vincent.paturet@entur.org>
2022-06-07 16:27:22 +0200

    Added test for BrandingArea

---
+++ src/test/java/org/opentripplanner/transit/model/site/BoardingAreaTest.java
@@ -0,0 +1,57 @@
+package org.opentripplanner.transit.model.site;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.opentripplanner.transit.model._data.TransitModelForTest;
+import org.opentripplanner.util.I18NString;
+import org.opentripplanner.util.NonLocalizedString;
+
+class BoardingAreaTest {
+
+  private static final String ID = "1";
+  private static final I18NString NAME = new NonLocalizedString("name");
+  private static final I18NString DESCRIPTION = new NonLocalizedString("description");
+  private static final Stop PARENT_STOP = TransitModelForTest.stop("stopId").build();
+
+  private static final BoardingArea subject = BoardingArea
+    .of(TransitModelForTest.id(ID))
+    .withName(NAME)
+    .withDescription(DESCRIPTION)
+    .withParentStop(PARENT_STOP)
+    .build();
+
+  @Test
+  void copy() {
+    assertEquals(ID, subject.getId().getId());
+
+    // Make a copy, and set the same name (nothing is changed)
+    var copy = subject.copy().withName(NAME).build();
+
+    assertSame(subject, copy);
+
+    // Copy and change name
+    copy = subject.copy().withName(new NonLocalizedString("v2")).build();
+
+    // The two objects are not the same instance, but are equal(same id)
+    assertNotSame(subject, copy);
+    assertEquals(subject, copy);
+
+    assertEquals(ID, copy.getId().getId());
+    assertEquals("v2", copy.getName().toString());
+    assertEquals(PARENT_STOP, copy.getParentStop());
+    assertEquals(DESCRIPTION, copy.getDescription());
+  }
+
+  @Test
+  void sameAs() {
+    assertTrue(subject.sameValue(subject.copy().build()));
+    assertFalse(subject.sameValue(subject.copy().withId(TransitModelForTest.id("X")).build()));
+    assertFalse(subject.sameValue(subject.copy().withName(new NonLocalizedString("X")).build()));
+    assertFalse(subject.sameValue(subject.copy().withDescription(new NonLocalizedString("X")).build()));
+  }
+}
