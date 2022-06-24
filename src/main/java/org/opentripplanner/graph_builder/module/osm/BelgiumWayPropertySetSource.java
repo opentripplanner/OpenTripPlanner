05251d28842b854d5bbd3660fd90989d6d7e0582
thomashermine <hello@thomashermine.be>
2022-06-24 13:02:01 +0200

    Create BelgiumWayPropertySetSource.java

---
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/BelgiumWayPropertySetSource.java
@@ -0,0 +1,35 @@
+package org.opentripplanner.graph_builder.module.osm;
+
+import static org.opentripplanner.graph_builder.module.osm.WayPropertySetSource.DrivingDirection.RIGHT_HAND_TRAFFIC;
+
+import org.opentripplanner.routing.core.intersection_model.IntersectionTraversalCostModel;
+import org.opentripplanner.routing.core.intersection_model.SimpleIntersectionTraversalCostModel;
+import org.opentripplanner.routing.edgetype.StreetTraversalPermission;
+
+/**
+ * OSM way properties for Belgium roads. WIP.
+ *
+ * @author thomashermine
+ * @see WayPropertySetSource
+ * @see DefaultWayPropertySetSource
+ */
+public class BelgiumWayPropertySetSource implements WayPropertySetSource {
+
+  private final DrivingDirection drivingDirection = RIGHT_HAND_TRAFFIC;
+
+  @Override
+  public void populateProperties(WayPropertySet props) {
+    // Read the rest from the default set
+    new DefaultWayPropertySetSource().populateProperties(props);
+  }
+
+  @Override
+  public DrivingDirection drivingDirection() {
+    return drivingDirection;
+  }
+
+  @Override
+  public IntersectionTraversalCostModel getIntersectionTraversalCostModel() {
+    return new SimpleIntersectionTraversalCostModel(drivingDirection);
+  }
+}
