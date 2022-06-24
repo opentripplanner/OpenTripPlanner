7a53b452d5aaa9c10e9089c288feebb717bcd29a
thomashermine <hello@thomashermine.be>
2022-06-24 13:22:02 +0200

    Update WayPropertySetSource.java

--- src/main/java/org/opentripplanner/graph_builder/module/osm/WayPropertySetSource.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/WayPropertySetSource.java
@@ -27,6 +27,8 @@ public interface WayPropertySetSource {
       return new FinlandWayPropertySetSource();
     } else if ("germany".equals(type)) {
       return new GermanyWayPropertySetSource();
+    } else if ("belgium".equals(type)) {
+      return new BelgiumWayPropertySetSource();
     } else {
       throw new IllegalArgumentException(String.format("Unknown osmWayPropertySet: '%s'", type));
     }
