3408606b198b4c87120167c0e74a7783b43da812
Zsombor Welker <zsombor.welker@realcity.io>
2022-07-26 14:36:08 +0200

    Car Pickup/Dropoff: add snapshot tests

--- src/test/java/org/opentripplanner/routing/algorithm/mapping/CarSnapshotTest.java
+++ src/test/java/org/opentripplanner/routing/algorithm/mapping/CarSnapshotTest.java
@@ -34,6 +34,20 @@ public class CarSnapshotTest extends SnapshotTestBase {
     -122.70067
   );
 
+  static GenericLocation p3 = new GenericLocation(
+    "Chapman Elementary School (P3)",
+    null,
+    45.53335,
+    -122.70517
+  );
+
+  static GenericLocation p4 = new GenericLocation(
+    "Legacy Good Samaritan Medical Center (P4)",
+    null,
+    45.53060,
+    -122.69771
+  );
+
   @BeforeAll
   public static void beforeClass() {
     Locale.setDefault(Locale.US);
@@ -54,6 +68,47 @@ public class CarSnapshotTest extends SnapshotTestBase {
       RequestModes.of().withDirectMode(StreetMode.CAR_TO_PARK).clearTransitModes().build();
     request.from = p1;
     request.to = p2;
+
+    expectArriveByToMatchDepartAtAndSnapshot(request);
+  }
+
+  @DisplayName("Direct CAR_PICKUP (with walking both ends)")
+  @Test
+  public void directCarPickupWithWalking() {
+    RoutingRequest request = createTestRequest(2009, 10, 21, 16, 10, 0);
+
+    request.modes =
+      RequestModes.of().withDirectMode(StreetMode.CAR_PICKUP).clearTransitModes().build();
+    request.from = p3;
+    request.to = p4;
+
+    expectRequestResponseToMatchSnapshot(request);
+  }
+
+  @DisplayName("Direct CAR_PICKUP (with walking both ends) - arriveBy")
+  @Test
+  public void directCarPickupWithWalkingArriveBy() {
+    RoutingRequest request = createTestRequest(2009, 10, 21, 16, 16, 54);
+
+    request.modes =
+      RequestModes.of().withDirectMode(StreetMode.CAR_PICKUP).clearTransitModes().build();
+    request.from = p3;
+    request.to = p4;
+    request.arriveBy = true;
+
+    expectRequestResponseToMatchSnapshot(request);
+  }
+
+  @DisplayName("Direct CAR_PICKUP (without walking at either end)")
+  @Test
+  public void directCarPickupWithoutWalking() {
+    RoutingRequest request = createTestRequest(2009, 10, 21, 16, 10, 0);
+
+    request.modes =
+      RequestModes.of().withDirectMode(StreetMode.CAR_PICKUP).clearTransitModes().build();
+    request.from = p1;
+    request.to = p2;
+    request.walkSpeed = 1;
 
     expectArriveByToMatchDepartAtAndSnapshot(request);
   }
