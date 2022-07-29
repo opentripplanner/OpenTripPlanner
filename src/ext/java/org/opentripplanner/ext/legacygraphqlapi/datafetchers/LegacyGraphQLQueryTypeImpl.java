1bb0606ec1a35160d23ea9d72ca1e5bf13373401
Joel Lappalainen <lappalj8@gmail.com>
2022-07-29 12:10:49 +0300

    Remove references to walkOnStreetReluctance as it won't be implemented

--- src/ext/java/org/opentripplanner/ext/legacygraphqlapi/datafetchers/LegacyGraphQLQueryTypeImpl.java
+++ src/ext/java/org/opentripplanner/ext/legacygraphqlapi/datafetchers/LegacyGraphQLQueryTypeImpl.java
@@ -619,7 +619,6 @@ public class LegacyGraphQLQueryTypeImpl
       callWith.argument("bikeWalkingReluctance", request::setBikeWalkingReluctance);
       callWith.argument("carReluctance", request::setCarReluctance);
       callWith.argument("walkReluctance", request::setWalkReluctance);
-      // callWith.argument("walkOnStreetReluctance", request::setWalkOnStreetReluctance);
       callWith.argument("waitReluctance", request::setWaitReluctance);
       callWith.argument("waitAtBeginningFactor", request::setWaitAtBeginningFactor);
       callWith.argument("walkSpeed", (Double v) -> request.walkSpeed = v);
