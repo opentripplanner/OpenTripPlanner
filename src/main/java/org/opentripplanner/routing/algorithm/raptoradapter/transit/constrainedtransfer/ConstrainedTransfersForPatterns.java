1597d0107f4b57570c2f3df9f9a863608869e61e
Hannes Junnila <hannes.junnila@gmail.com>
2022-08-18 11:36:49 +0300

    Use a List to store all constrained transfers

--- src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/constrainedtransfer/ConstrainedTransfersForPatterns.java
+++ src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/constrainedtransfer/ConstrainedTransfersForPatterns.java
@@ -1,12 +1,12 @@
 package org.opentripplanner.routing.algorithm.raptoradapter.transit.constrainedtransfer;
 
-import gnu.trove.map.TIntObjectMap;
+import java.util.List;
 
 /**
  * This is a container for returning a tuple of forward and reverse constrained transfers from the
  * mapper to the transit layer.
  */
 public record ConstrainedTransfersForPatterns(
-  TIntObjectMap<TransferForPatternByStopPos> forward,
-  TIntObjectMap<TransferForPatternByStopPos> reverse
+  List<TransferForPatternByStopPos> forward,
+  List<TransferForPatternByStopPos> reverse
 ) {}
