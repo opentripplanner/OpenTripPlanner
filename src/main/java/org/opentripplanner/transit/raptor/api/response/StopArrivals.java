ee919bea86e26d3ba8050cf2daf935b3e4ae9e50
Thomas Gran <t2gran@gmail.com>
2022-08-25 14:16:48 +0200

    doc: Document why unused parameters in Raptor API should NOT be deleted.

--- src/main/java/org/opentripplanner/transit/raptor/api/response/StopArrivals.java
+++ src/main/java/org/opentripplanner/transit/raptor/api/response/StopArrivals.java
@@ -13,7 +13,10 @@ public interface StopArrivals {
   /**
    * The earliest arrival time at the given stop. If the stop is not reached, the behavior is
    * undefined; It may return an arbitrary value or throw an exception.
+   * <p>
+   * This is currently unused in OTP, but useful for analyses/comparing the results.
    */
+  @SuppressWarnings("unused")
   int bestArrivalTime(int stopIndex);
 
   /**
@@ -30,6 +33,9 @@ public interface StopArrivals {
   /**
    * The smallest number of transfers needed to reach the given stop. If the stop is not reached,
    * the behavior is undefined; It may return an arbitrary value or throw an exception.
+   * <p>
+   * This is currently unused in OTP, but useful for analyses/comparing the results.
    */
+  @SuppressWarnings("unused")
   int smallestNumberOfTransfers(int stopIndex);
 }
