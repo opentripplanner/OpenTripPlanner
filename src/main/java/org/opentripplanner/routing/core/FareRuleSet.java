ae84f24fcec01959459fe80249e7f8aee3f55322
Viljami N <nurmAV@users.noreply.github.com>
2022-09-01 11:22:20 +0300

    Update src/main/java/org/opentripplanner/routing/core/FareRuleSet.java
    Co-authored-by: Hannes Junnila <hannes.junnila@gmail.com>

--- src/main/java/org/opentripplanner/routing/core/FareRuleSet.java
+++ src/main/java/org/opentripplanner/routing/core/FareRuleSet.java
@@ -37,8 +37,8 @@ public class FareRuleSet implements Serializable {
     return originDestinations;
   }
 
-  /* Used for checking ticket validity on HSL special routes on which some tickets are valid
-  /** Used for checking ticket validity on HSL special routes on which some tickets are valid
+  /** 
+   * Used for checking ticket validity on HSL special routes on which some tickets are valid
    * outside their normal validity zones
    */
   public void addRouteOriginDestination(String route, String origin, String destination) {
