2a724ce4704effb864b5008f1b4e50bcf2aafd09
Leonard Ehrenfried <mail@leonard.io>
2022-09-26 18:07:10 +0200

    Add tags and error types

--- src/main/java/org/opentripplanner/updater/stoptime/PollingStoptimeUpdater.java
+++ src/main/java/org/opentripplanner/updater/stoptime/PollingStoptimeUpdater.java
@@ -4,8 +4,14 @@ import com.google.transit.realtime.GtfsRealtime.TripUpdate;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.Tags;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.opentripplanner.model.UpdateError;
 import org.opentripplanner.transit.service.DefaultTransitService;
 import org.opentripplanner.transit.service.TransitModel;
 import org.opentripplanner.updater.GtfsRealtimeFuzzyTripMatcher;
@@ -46,8 +52,11 @@ public class PollingStoptimeUpdater extends PollingGraphUpdater {
    * flag.
    */
   private final BackwardsDelayPropagationType backwardsDelayPropagationType;
-  private final AtomicInteger succesfulCounter;
-  private final AtomicInteger failedCounter;
+  private final AtomicInteger successfulGauge;
+  private final AtomicInteger failureGauge;
+  private final Map<UpdateError.UpdateErrorType, AtomicInteger> failuresByType = new HashMap<>();
+  private final List<Tag> baseTags;
+  final String METRICS_PREFIX = "trip_updates";
   /**
    * Parent update manager. Is used to execute graph writer runnables.
    */
@@ -73,8 +82,15 @@ public class PollingStoptimeUpdater extends PollingGraphUpdater {
         new GtfsRealtimeFuzzyTripMatcher(new DefaultTransitService(transitModel));
     }
 
-    this.succesfulCounter = getGauge("trip_updates.successful", parameters);
-    this.failedCounter = getGauge("trip_updates.failed", parameters);
+    this.baseTags =
+      List.of(
+        Tag.of("configRef", parameters.getConfigRef()),
+        Tag.of("url", parameters.httpSourceParameters().getUrl()),
+        Tag.of("feedId", parameters.getFeedId())
+      );
+
+    this.successfulGauge = getGauge(METRICS_PREFIX + ".successful");
+    this.failureGauge = getGauge(METRICS_PREFIX + ".failed");
 
     LOG.info(
       "Creating stop time updater running every {} seconds : {}",
@@ -114,8 +130,24 @@ public class PollingStoptimeUpdater extends PollingGraphUpdater {
   }
 
   public void sendMetrics(UpdateResult result) {
-    this.succesfulCounter.set(result.successful());
-    this.failedCounter.set(result.failed());
+    this.successfulGauge.set(result.successful());
+    this.failureGauge.set(result.failed());
+
+    for (var errorType : result.failures().keySet()) {
+      var counter = failuresByType.get(errorType);
+      if (Objects.isNull(counter)) {
+        counter = getGauge(METRICS_PREFIX + ".failure_type", Tag.of("errorType", errorType.name()));
+        failuresByType.put(errorType, counter);
+      }
+      counter.set(result.failures().get(errorType).size());
+    }
+
+    var toZero = new HashSet<>(failuresByType.keySet());
+    toZero.removeAll(result.failures().keySet());
+
+    for (var keyToZero : toZero) {
+      failuresByType.get(keyToZero).set(0);
+    }
   }
 
   @Override
@@ -128,14 +160,10 @@ public class PollingStoptimeUpdater extends PollingGraphUpdater {
       .toString();
   }
 
-  private static AtomicInteger getGauge(String name, PollingStoptimeUpdaterParameters parameters) {
+  private AtomicInteger getGauge(String name, Tag... tags) {
     if (OTPFeature.ActuatorAPI.isOn()) {
-      var tags = List.of(
-        Tag.of("configRef", parameters.getConfigRef()),
-        Tag.of("url", parameters.httpSourceParameters().getUrl()),
-        Tag.of("feedId", parameters.getFeedId())
-      );
-      return Metrics.globalRegistry.gauge(name, tags, new AtomicInteger(0));
+      var finalTags = Tags.concat(Arrays.stream(tags).toList(), baseTags);
+      return Metrics.globalRegistry.gauge(name, finalTags, new AtomicInteger(0));
     } else {
       return new AtomicInteger();
     }
