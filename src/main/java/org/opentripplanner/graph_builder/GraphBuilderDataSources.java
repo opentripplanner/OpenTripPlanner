fec3d69f6c6ad488ef393ff3c7545f79a68e26b9
Thomas Gran <t2gran@gmail.com>
2022-10-11 00:08:06 +0200

    refactor: Convert TransitFeeds to a record

--- src/main/java/org/opentripplanner/graph_builder/GraphBuilderDataSources.java
+++ src/main/java/org/opentripplanner/graph_builder/GraphBuilderDataSources.java
@@ -151,7 +151,7 @@ public class GraphBuilderDataSources {
   }
 
   private GtfsFeedParameters getGtfsFeedConfig(DataSource dataSource) {
-    return buildConfig.transitFeeds.gtfsFeeds
+    return buildConfig.transitFeeds.gtfsFeeds()
       .stream()
       .filter(gtfsFeedConfig -> uriMatch(gtfsFeedConfig.source(), dataSource.uri()))
       .findFirst()
@@ -167,7 +167,7 @@ public class GraphBuilderDataSources {
   }
 
   private NetexFeedParameters getNetexFeedConfig(DataSource dataSource) {
-    return buildConfig.transitFeeds.netexFeeds
+    return buildConfig.transitFeeds.netexFeeds()
       .stream()
       .filter(netexFeedConfig -> uriMatch(netexFeedConfig.source(), dataSource.uri()))
       .findFirst()
