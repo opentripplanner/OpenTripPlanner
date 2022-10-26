3a65a07b9cc3ac7a30d99b3a88ae6af19fca3663
Leonard Ehrenfried <mail@leonard.io>
2022-10-26 12:57:50 +0200

    Add doc for VehicleParkingUpdaterParameters.java

--- src/main/java/org/opentripplanner/standalone/config/routerconfig/updaters/VehicleParkingUpdaterConfig.java
+++ src/main/java/org/opentripplanner/standalone/config/routerconfig/updaters/VehicleParkingUpdaterConfig.java
@@ -26,40 +26,64 @@ public class VehicleParkingUpdaterConfig {
   }
 
   public static VehicleParkingUpdaterParameters create(String updaterRef, NodeAdapter c) {
-    var sourceType = c.of("sourceType").since(NA).summary("TODO").asEnum(DataSourceType.class);
-    var feedId = c.of("feedId").since(NA).summary("TODO").asString(null);
-    var timeZone = c.of("timeZone").since(NA).summary("TODO").asZoneId(null);
+    var sourceType = c
+      .of("sourceType")
+      .since(NA)
+      .summary("The source of the vehicle updates.")
+      .asEnum(DataSourceType.class);
+    var feedId = c
+      .of("feedId")
+      .since(NA)
+      .summary("The name of the data source.")
+      .description("This will end up in the API responses as the feed id of of the parking lot.")
+      .asString(null);
+    var timeZone = c
+      .of("timeZone")
+      .since(NA)
+      .summary("The time zone of the feed.")
+      .description("Used for converting abstract opening hours into concrete points in time.")
+      .asZoneId(null);
     switch (sourceType) {
       case HSL_PARK:
         return new HslParkUpdaterParameters(
           updaterRef,
-          c.of("facilitiesFrequencySec").since(NA).summary("TODO").asInt(3600),
-          c.of("facilitiesUrl").since(NA).summary("TODO").asString(null),
+          c
+            .of("facilitiesFrequencySec")
+            .since(NA)
+            .summary("How often the facilities should be updated.")
+            .asInt(3600),
+          c.of("facilitiesUrl").since(NA).summary("URL of the facilities.").asString(null),
           feedId,
           sourceType,
-          c.of("utilizationsFrequencySec").since(NA).summary("TODO").asInt(600),
-          c.of("utilizationsUrl").since(NA).summary("TODO").asString(null),
+          c
+            .of("utilizationsFrequencySec")
+            .since(NA)
+            .summary("How often the utilization should be updated.")
+            .asInt(600),
+          c.of("utilizationsUrl").since(NA).summary("URL of the utilization data.").asString(null),
           timeZone
         );
       case KML:
         return new KmlUpdaterParameters(
           updaterRef,
-          c.of("url").since(NA).summary("TODO").asString(null),
+          c.of("url").since(NA).summary("URL of the KML file.").asString(null),
           feedId,
-          c.of("namePrefix").since(NA).summary("TODO").asString(null),
-          c.of("frequencySec").since(NA).summary("TODO").asInt(60),
-          c.of("zip").since(NA).summary("TODO").asBoolean(false),
+          c.of("namePrefix").since(NA).summary("Prefix for the names.").asString(null),
+          c.of("frequencySec").since(NA).summary("How often to update the parking lots.").asInt(60),
+          c.of("zip").since(NA).summary("Whether the resource is zip-compressed.").asBoolean(false),
           sourceType
         );
       case PARK_API:
       case BICYCLE_PARK_API:
         return new ParkAPIUpdaterParameters(
           updaterRef,
-          c.of("url").since(NA).summary("TODO").asString(null),
+          c.of("url").since(NA).summary("URL of the resource.").asString(null),
           feedId,
-          c.of("frequencySec").since(NA).summary("TODO").asInt(60),
-          c.of("headers").since(NA).summary("TODO").asStringMap(),
-          new ArrayList<>(c.of("tags").since(NA).summary("TODO").asStringSet(Set.of())),
+          c.of("frequencySec").since(NA).summary("How often to update the source.").asInt(60),
+          c.of("headers").since(NA).summary("HTTP headers to add.").asStringMap(),
+          new ArrayList<>(
+            c.of("tags").since(NA).summary("Tags to add to the parking lots.").asStringSet(Set.of())
+          ),
           sourceType,
           timeZone
         );
