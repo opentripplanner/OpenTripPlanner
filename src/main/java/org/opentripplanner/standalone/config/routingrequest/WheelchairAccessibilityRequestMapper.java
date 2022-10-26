7551985fa1750f0e032cbd2ab4627ef76c99bbd3
Leonard Ehrenfried <mail@leonard.io>
2022-10-26 13:19:13 +0200

    Add doc to WheelchairAccessibilityRequestMapper.java

--- src/main/java/org/opentripplanner/standalone/config/routingrequest/WheelchairAccessibilityRequestMapper.java
+++ src/main/java/org/opentripplanner/standalone/config/routingrequest/WheelchairAccessibilityRequestMapper.java
@@ -12,29 +12,67 @@ public class WheelchairAccessibilityRequestMapper {
   static WheelchairPreferences mapAccessibilityRequest(NodeAdapter a) {
     return new WheelchairPreferences(
       mapAccessibilityFeature(
-        a.of("trip").since(NA).summary("TODO").description(/*TODO DOC*/"TODO").asObject(),
+        a
+          .of("trip")
+          .since(NA)
+          .summary("Configuration for when to use inaccessible trips.")
+          .asObject(),
         DEFAULT.trip()
       ),
       mapAccessibilityFeature(
-        a.of("stop").since(NA).summary("TODO").description(/*TODO DOC*/"TODO").asObject(),
+        a
+          .of("stop")
+          .since(NA)
+          .summary("Configuration for when to use inaccessible stops.")
+          .asObject(),
         DEFAULT.stop()
       ),
       mapAccessibilityFeature(
-        a.of("elevator").since(NA).summary("TODO").description(/*TODO DOC*/"TODO").asObject(),
+        a
+          .of("elevator")
+          .since(NA)
+          .summary("Configuration for when to use inaccessible elevators.")
+          .asObject(),
         DEFAULT.elevator()
       ),
       a
         .of("inaccessibleStreetReluctance")
         .since(NA)
-        .summary("TODO")
+        .summary(
+          "The factor to to multiply the cost of traversing a street edge that is not wheelchair-accessible."
+        )
         .asDouble(DEFAULT.inaccessibleStreetReluctance()),
-      a.of("maxSlope").since(NA).summary("TODO").asDouble(DEFAULT.maxSlope()),
+      a
+        .of("maxSlope")
+        .since(NA)
+        .summary("The maximum slope as a fraction of 1.")
+        .description("9 percent would be `0.09`")
+        .asDouble(DEFAULT.maxSlope()),
       a
         .of("slopeExceededReluctance")
         .since(NA)
-        .summary("TODO")
+        .summary("How much streets with high slope should be avoided.")
+        .description(
+          """
+            What factor should be given to street edges, which are over the
+            max slope. The penalty is not static but scales with how much you
+            exceed the maximum slope. Set to negative to disable routing on
+            too steep edges.
+            """
+        )
         .asDouble(DEFAULT.slopeExceededReluctance()),
-      a.of("stairsReluctance").since(NA).summary("TODO").asDouble(DEFAULT.stairsReluctance())
+      a
+        .of("stairsReluctance")
+        .since(NA)
+        .summary("How much stairs should be avoided.")
+        .description(
+          """
+            Stairs are not completely excluded for wheelchair users but
+            severely punished. This value determines how much they are
+            punished. This should be a very high value as you want to only
+            include stairs as a last result."""
+        )
+        .asDouble(DEFAULT.stairsReluctance())
     );
   }
 
@@ -45,11 +83,21 @@ public class WheelchairAccessibilityRequestMapper {
     var onlyAccessible = adapter
       .of("onlyConsiderAccessible")
       .since(NA)
-      .summary("TODO")
+      .summary(
+        "Wheter to only use this entity if it is explicitly marked as wheelchair accessible."
+      )
       .asBoolean(defaultValue.onlyConsiderAccessible());
 
-    var unknownCost = adapter.of("unknownCost").since(NA).summary("TODO").asInt(60 * 10);
-    var inaccessibleCost = adapter.of("inaccessibleCost").since(NA).summary("TODO").asInt(60 * 60);
+    var unknownCost = adapter
+      .of("unknownCost")
+      .since(NA)
+      .summary("The cost to add when traversing an entity with unknown accessibility information.")
+      .asInt(60 * 10);
+    var inaccessibleCost = adapter
+      .of("inaccessibleCost")
+      .since(NA)
+      .summary("The cost to add when traversing an entity which is know to be inaccessible.")
+      .asInt(60 * 60);
     if (onlyAccessible) {
       return AccessibilityPreferences.ofOnlyAccessible();
     } else {
