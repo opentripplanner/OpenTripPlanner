57e2cb949f2a0acb04e5db58d47728d15a05c008
Leonard Ehrenfried <mail@leonard.io>
2022-10-27 15:26:34 +0200

    Revert "Add common.model to allowed packages"
    This reverts commit ec7df2c967b8352ea4d06910d0435b0a1d8cb83e.

--- src/test/java/org/opentripplanner/OtpArchitectureModules.java
+++ src/test/java/org/opentripplanner/OtpArchitectureModules.java
@@ -17,7 +17,6 @@ public interface OtpArchitectureModules {
   Package FRAMEWORK_TEXT = FRAMEWORK.subPackage("text");
   Package UTIL = OTP_ROOT.subPackage("util");
   Package GEO_UTIL = OTP_ROOT.subPackage("common.geometry");
-  Package COMMON_MODEL = OTP_ROOT.subPackage("common.model");
 
   Package RAPTOR_ADAPTER = OTP_ROOT
     .subPackage("routing")
