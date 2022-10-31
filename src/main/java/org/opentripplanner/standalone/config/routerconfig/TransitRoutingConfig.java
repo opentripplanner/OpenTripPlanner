1450cc2a22ea3d8ee9461adc3ef40fe2c513a134
Thomas Gran <t2gran@gmail.com>
2022-10-31 10:10:14 +0100

    doc: Update TransitRoutingConfig

--- src/main/java/org/opentripplanner/standalone/config/routerconfig/TransitRoutingConfig.java
+++ src/main/java/org/opentripplanner/standalone/config/routerconfig/TransitRoutingConfig.java
@@ -217,12 +217,41 @@ for more info."
 
     public DynamicSearchWindowConfig(String parameterName, NodeAdapter root) {
       var dsWin = root
-        .of("dynamicSearchWindow")
+        .of(parameterName)
         .since(NA)
         .summary("The dynamic search window coefficients used to calculate the EDT, LAT and SW.")
         .description(
-          "EDT is earliest-departure-time, LAT is latest-arrival-time and SW is " +
-          "raptor-search-window using heuristics."
+          """
+The dynamic search window coefficients is used to calculate EDT(*earliest-departure-time*),
+LAT(*latest-arrival-time*) and SW(*raptor-search-window*) request parameters using heuristics. The
+heuristics perform a Raptor search (one-iteration) to find a trip which we use to find a lower
+bound for the travel duration time - the "minTransitTime". The heuristic search is used for other
+purposes too, and is very fast.
+
+At least the EDT or the LAT must be passed into Raptor to perform a Range Raptor search. If
+unknown/missing the parameters(EDT, LAT, DW) is dynamically calculated. The dynamic coefficients
+affect the performance and should be tuned to match the deployment.
+
+The request parameters are calculated like this:
+
+```
+    DW  = round_N(C + T * minTransitTime + W * minWaitTime)
+    LAT = EDT + DW + minTransitTime
+    EDT = LAT - (DW + minTransitTime)
+```
+
+The `round_N(...)` method is will round the input to the closest multiplication of N.
+
+The 3 coefficients above are:
+
+ - `C` is parameter: `minWinTimeMinutes`
+ - `T` is parameter: `minTransitTimeCoefficient`
+ - `W` is parameter: `minWaitTimeCoefficient`
+ - `N` is parameter: `stepMinutes`
+
+In addition there this an upper bound on the calculation of the search window:
+`maxWinTimeMinutes`.
+"""
         )
         .asObject();
 
@@ -231,20 +260,62 @@ for more info."
         dsWin
           .of("minTransitTimeCoefficient")
           .since(NA)
-          .summary("TODO")
+          .summary("The coefficient to multiply with `minTransitTime`.")
+          .description(
+            "Use a value between `0.0` and `3.0`. Using `0.0` will eliminate the `minTransitTime` " +
+            "from the dynamic raptor-search-window calculation."
+          )
           .asDouble(dsWinDft.minTransitTimeCoefficient());
       this.minWaitTimeCoefficient =
         dsWin
           .of("minWaitTimeCoefficient")
           .since(NA)
-          .summary("TODO")
+          .summary("The coefficient to multiply with `minWaitTime`.")
+          .description(
+            "Use a value between `0.0` and `1.0`. Using `0.0` will eliminate the `minWaitTime` " +
+            "from the dynamic raptor-search-window calculation."
+          )
           .asDouble(dsWinDft.minWaitTimeCoefficient());
       this.minWinTimeMinutes =
-        dsWin.of("minWinTimeMinutes").since(NA).summary("TODO").asInt(dsWinDft.minWinTimeMinutes());
+        dsWin
+          .of("minWinTimeMinutes")
+          .since(NA)
+          .summary("The constant minimum number of minutes for a raptor-search-window. ")
+          .description("Use a value between 20 to 180 minutes in a normal deployment.")
+          .asInt(dsWinDft.minWinTimeMinutes());
       this.maxWinTimeMinutes =
-        dsWin.of("maxWinTimeMinutes").since(NA).summary("TODO").asInt(dsWinDft.maxWinTimeMinutes());
+        dsWin
+          .of("maxWinTimeMinutes")
+          .since(NA)
+          .summary("Upper limit for the search-window calculation.")
+          .description(
+            """
+Long search windows consumes a lot of resources and may take a long time. Use this parameter to 
+tune the desired maximum search time.
+
+This is the parameter that affect the response time most, the downside is that a search is only
+guarantied to be pareto-optimal within a search-window.
+
+The default is 3 hours. The unit is minutes.
+"""
+          )
+          .asInt(dsWinDft.maxWinTimeMinutes());
       this.stepMinutes =
-        dsWin.of("stepMinutes").since(NA).summary("TODO").asInt(dsWinDft.stepMinutes());
+        dsWin
+          .of("stepMinutes")
+          .since(NA)
+          .summary("Used to set the steps the search-window is rounded to.")
+          .description(
+            """
+The search window is rounded of to the closest multiplication of `stepMinutes`. If `stepMinutes` = 
+10 minutes, the search-window can be 10, 20, 30 ... minutes. It the computed search-window is 5
+minutes and 17 seconds it will be rounded up to 10 minutes.
+
+Use a value between `1` and `60`. This should be less than the `min-raptor-search-window` 
+coefficient.
+"""
+          )
+          .asInt(dsWinDft.stepMinutes());
     }
 
     @Override
