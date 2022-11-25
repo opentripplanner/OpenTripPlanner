9844bc35c27d498d5b296f37da8a8244452d9d0b
Leonard Ehrenfried <mail@leonard.io>
2022-11-25 20:59:02 +0100

    Fix import

--- src/main/java/org/opentripplanner/graph_builder/module/osm/tagmapping/PortlandMapper.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/tagmapping/PortlandMapper.java
@@ -2,7 +2,7 @@ package org.opentripplanner.graph_builder.module.osm.tagmapping;
 
 import static org.opentripplanner.graph_builder.module.osm.WayPropertiesBuilder.withModes;
 import static org.opentripplanner.graph_builder.module.osm.specifier.ExactMatchSpecifier.exact;
-import static org.opentripplanner.routing.edgetype.StreetTraversalPermission.ALL;
+import static org.opentripplanner.street.model.StreetTraversalPermission.ALL;
 
 import org.opentripplanner.graph_builder.module.osm.WayPropertySet;
 import org.opentripplanner.graph_builder.module.osm.specifier.Condition.Absent;
