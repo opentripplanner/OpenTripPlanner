entur/OpenTripPlanner-LegacyHSLFork

Expose relaxCostAtDestination

src/main/java/org/opentripplanner/api/common/RequestToPreferencesMapper.java
@@ -83,6 +83,10 @@ class RequestToPreferencesMapper {
       setIfNotNull(req.alightSlack, tr::withDefaultAlightSlackSec);
       setIfNotNull(req.otherThanPreferredRoutesPenalty, tr::setOtherThanPreferredRoutesPenalty);
       setIfNotNull(req.ignoreRealtimeUpdates, tr::setIgnoreRealtimeUpdates);
+      setIfNotNull(
+        req.relaxCostAtDestination,
+        value -> tr.withRaptor(it -> it.withRelaxCostAtDestination(value))
+      );
     });
 
     return new BoardAndAlightSlack(

opentripplanner/OpenTripPlanner

src/main/java/org/opentripplanner/api/common/RequestToPreferencesMapper.java
@@ -86,6 +86,10 @@ class RequestToPreferencesMapper {
       setIfNotNull(req.alightSlack, tr::withDefaultAlightSlackSec);
       setIfNotNull(req.otherThanPreferredRoutesPenalty, tr::setOtherThanPreferredRoutesPenalty);
       setIfNotNull(req.ignoreRealtimeUpdates, tr::setIgnoreRealtimeUpdates);
+      setIfNotNull(
+        req.relaxTransitSearchCostCriteria,
+        value -> tr.withRaptor(it -> it.withRelaxTransitSearchCostCriteria(value))
+      );
     });
 
     return new BoardAndAlightSlack(