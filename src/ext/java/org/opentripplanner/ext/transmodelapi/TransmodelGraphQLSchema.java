45c637c4bdd2762ef9c91a7073294f114b44f72e
Hannes Junnila <hannes.junnila@gmail.com>
2022-12-09 09:26:11 +0200

    Expose stopPlaces in PtSituationElementType

--- src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelGraphQLSchema.java
+++ src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelGraphQLSchema.java
@@ -278,6 +278,7 @@ public class TransmodelGraphQLSchema {
     GraphQLNamedOutputType ptSituationElementType = PtSituationElementType.create(
       authorityType,
       quayType,
+      stopPlaceType,
       lineType,
       ServiceJourneyType.REF,
       multilingualStringType,
