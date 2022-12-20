a828951e863aedf2f6d17339d72d513a800aab1c
bartosz <bartosz.kruba@purplescout.se>
2022-12-20 14:33:22 +0100

    - Fix bug

--- src/main/java/org/opentripplanner/model/modes/FilterCollection.java
+++ src/main/java/org/opentripplanner/model/modes/FilterCollection.java
@@ -33,4 +33,9 @@ public class FilterCollection implements AllowTransitModeFilter {
     }
     return false;
   }
+
+  @Override
+  public boolean isSubMode() {
+    return filters.stream().anyMatch(AllowTransitModeFilter::isSubMode);
+  }
 }
