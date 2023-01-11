191feb399453536cf28d93b401c3eb8ffe47f8da
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-01-11 09:05:00 +0200

    Add convenient OSM links to some OSM related issues

--- src/main/java/org/opentripplanner/graph_builder/issues/AreaTooComplicated.java
+++ src/main/java/org/opentripplanner/graph_builder/issues/AreaTooComplicated.java
@@ -4,13 +4,15 @@ import org.opentripplanner.graph_builder.issue.api.DataImportIssue;
 
 public class AreaTooComplicated implements DataImportIssue {
 
-  public static final String FMT = "Area %s is too complicated (%s > %s )";
+  public static final String FMT = "Area %s is too complicated (%s > %s)";
+  public static final String HTMLFMT =
+    "Area <a href='http://www.openstreetmap.org/%s'>'%s'</a> is too complicated (%s > %s)";
 
-  final long areaId;
+  final String areaId;
   final int nbNodes;
   final int maxAreaNodes;
 
-  public AreaTooComplicated(long areaId, int nbNodes, int maxAreaNodes) {
+  public AreaTooComplicated(String areaId, int nbNodes, int maxAreaNodes) {
     this.areaId = areaId;
     this.nbNodes = nbNodes;
     this.maxAreaNodes = maxAreaNodes;
@@ -19,6 +21,11 @@ public class AreaTooComplicated implements DataImportIssue {
   @Override
   public String getMessage() {
     return String.format(FMT, areaId, nbNodes, maxAreaNodes);
+  }
+
+  @Override
+  public String getHTMLMessage() {
+    return String.format(HTMLFMT, areaId, areaId, nbNodes, maxAreaNodes);
   }
 
   @Override
