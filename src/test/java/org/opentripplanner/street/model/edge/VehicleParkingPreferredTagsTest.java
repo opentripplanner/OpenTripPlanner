1a3a45ae5aa68c35facb466e1f68043d172b8b2e
Leonard Ehrenfried <mail@leonard.io>
2023-01-29 23:13:02 +0100

    Run tests in arriveBy and departAt modes

--- src/test/java/org/opentripplanner/street/model/edge/VehicleParkingPreferredTagsTest.java
+++ src/test/java/org/opentripplanner/street/model/edge/VehicleParkingPreferredTagsTest.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.opentripplanner.framework.geometry.WgsCoordinate;
 import org.opentripplanner.framework.i18n.NonLocalizedString;
 import org.opentripplanner.routing.api.request.StreetMode;
@@ -18,31 +19,50 @@ import org.opentripplanner.street.model.vertex.VehicleParkingEntranceVertex;
 import org.opentripplanner.street.model.vertex.Vertex;
 import org.opentripplanner.street.search.request.StreetSearchRequest;
 import org.opentripplanner.street.search.state.State;
-import org.opentripplanner.test.support.VariableSource;
 
 class VehicleParkingPreferredTagsTest {
 
   public static final WgsCoordinate COORDINATE = new WgsCoordinate(1, 1);
   public static final int EXTRA_COST = 1000;
   public static final int NO_COST = 0;
-  static Stream<Arguments> testCases = Stream.of(
-    // no preferred tags so no extra cost
-    of(Set.of("locker"), Set.of(), NO_COST),
-    of(Set.of(), Set.of(), NO_COST),
-    // parking lot doesn't have the preferred tag so add extra cost
-    of(Set.of("roof"), Set.of("locker"), EXTRA_COST),
-    of(Set.of("locker"), Set.of("locker"), NO_COST),
-    of(Set.of("locker", "roof"), Set.of("locker"), NO_COST),
-    of(Set.of("locker", "roof"), Set.of("locker", "concierge"), NO_COST),
-    // parking doesn't have any tags so cannot be preferred
-    of(Set.of(), Set.of("locker"), EXTRA_COST)
-  );
+
+  static Stream<Arguments> testCases() {
+    return Stream.of(
+      // no preferred tags so no extra cost
+      of(Set.of("locker"), Set.of(), NO_COST),
+      of(Set.of(), Set.of(), NO_COST),
+      // parking lot doesn't have the preferred tag so add extra cost
+      of(Set.of("roof"), Set.of("locker"), EXTRA_COST),
+      of(Set.of("locker"), Set.of("locker"), NO_COST),
+      of(Set.of("locker", "roof"), Set.of("locker"), NO_COST),
+      of(Set.of("locker", "roof"), Set.of("locker", "concierge"), NO_COST),
+      // parking doesn't have any tags so cannot be preferred
+      of(Set.of(), Set.of("locker"), EXTRA_COST)
+    );
+  }
 
   @ParameterizedTest(
-    name = "Bike parking with tags {0} and preferred tags {1} should lead to a traversal cost of {2}"
+    name = "Bike parking with tags {0} and preferred tags {1} should lead to a traversal cost of {2} (departAt)"
   )
-  @VariableSource("testCases")
-  void preferred(Set<String> parkingTags, Set<String> preferredTags, double expectedCost) {
+  @MethodSource("testCases")
+  void departAt(Set<String> parkingTags, Set<String> preferredTags, double expectedCost) {
+    runTest(parkingTags, preferredTags, expectedCost, false);
+  }
+
+  @ParameterizedTest(
+    name = "Bike parking with tags {0} and preferred tags {1} should lead to a traversal cost of {2} (arriveBy)"
+  )
+  @MethodSource("testCases")
+  void arriveBy(Set<String> parkingTags, Set<String> preferredTags, double expectedCost) {
+    runTest(parkingTags, preferredTags, expectedCost, true);
+  }
+
+  private void runTest(
+    Set<String> parkingTags,
+    Set<String> preferredTags,
+    double expectedCost,
+    boolean arriveBy
+  ) {
     var parking = VehicleParking
       .builder()
       .coordinate(COORDINATE)
@@ -59,7 +79,7 @@ class VehicleParkingPreferredTagsTest {
       .build();
 
     var fromV = new VehicleParkingEntranceVertex(null, entrance);
-    var edge = new VehicleParkingEdge(fromV, new VehicleParkingEntranceVertex(null, entrance));
+    var edge = new VehicleParkingEdge(fromV);
 
     var parkingReq = new VehicleParkingRequest();
     parkingReq.setPreferredTags(preferredTags);
@@ -68,6 +88,7 @@ class VehicleParkingPreferredTagsTest {
     var req = StreetSearchRequest.of();
     req.withMode(StreetMode.BIKE_TO_PARK);
     req.withParking(parkingReq);
+    req.withArriveBy(arriveBy);
     req.withPreferences(p -> p.withBike(bike -> bike.withParkCost(0)));
 
     var result = traverse(fromV, edge, req.build());
