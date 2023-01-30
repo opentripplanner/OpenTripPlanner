0a1ba1f40981acf5088f860d1e9f23535b760cf5
Leonard Ehrenfried <mail@leonard.io>
2023-01-30 13:47:24 +0100

    Move Javadoc to interface

--- src/main/java/org/opentripplanner/service/vehiclepositions/VehiclePositionService.java
+++ src/main/java/org/opentripplanner/service/vehiclepositions/VehiclePositionService.java
@@ -5,5 +5,8 @@ import org.opentripplanner.service.vehiclepositions.model.RealtimeVehiclePositio
 import org.opentripplanner.transit.model.network.TripPattern;
 
 public interface VehiclePositionService {
+  /**
+   * Get the vehicle positions for a certain trip.
+   */
   List<RealtimeVehiclePosition> getVehiclePositions(TripPattern pattern);
 }
--- src/main/java/org/opentripplanner/service/vehiclepositions/internal/DefaultVehiclePositionService.java
+++ src/main/java/org/opentripplanner/service/vehiclepositions/internal/DefaultVehiclePositionService.java
@@ -19,33 +19,16 @@ public class DefaultVehiclePositionService
   @Inject
   public DefaultVehiclePositionService() {}
 
-  /**
-   * For the given pattern set all realtime vehicle positions.
-   * <p>
-   * The list is expected to be exhaustive: all existing positions will be overridden.
-   * <p>
-   * This means that if there are two updaters providing positions for the same pattern they
-   * overwrite each other.
-   */
   @Override
   public void setVehiclePositions(TripPattern pattern, List<RealtimeVehiclePosition> updates) {
     positions.put(pattern, List.copyOf(updates));
   }
 
-  /**
-   * Remove all vehicle positions for a given pattern.
-   * <p>
-   * This is useful to clear old vehicles for which there are no more updates and we assume that
-   * they have stopped their trip.
-   */
   @Override
   public void clearVehiclePositions(TripPattern pattern) {
     positions.remove(pattern);
   }
 
-  /**
-   * Get the vehicle positions for a certain trip.
-   */
   @Override
   public List<RealtimeVehiclePosition> getVehiclePositions(TripPattern pattern) {
     // the list is made immutable during insertion, so we can safely return them
