1955234d44bc06ed1a88ea26a0a1943469a28cd5
Hannes Junnila <hannes.junnila@gmail.com>
2023-02-14 16:03:52 +0200

    Use capital letter for bytes

--- src/main/java/org/opentripplanner/framework/text/FileSizeToTextConverter.java
+++ src/main/java/org/opentripplanner/framework/text/FileSizeToTextConverter.java
@@ -15,7 +15,7 @@ public class FileSizeToTextConverter {
       return String.format(Locale.ROOT, "%.1f MB", n / 1_000_000.0);
     }
     if (n >= 1_000) {
-      return String.format(Locale.ROOT, "%d kb", n / 1_000);
+      return String.format(Locale.ROOT, "%d kB", n / 1_000);
     }
     if (n == 1) {
       return String.format(Locale.ROOT, "%d byte", n);
