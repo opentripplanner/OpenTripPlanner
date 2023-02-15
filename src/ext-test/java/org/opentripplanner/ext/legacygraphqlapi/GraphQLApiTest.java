7ef268e6d319c47921b7413935b4ad2a37bc778a
Leonard Ehrenfried <mail@leonard.io>
2023-02-15 11:55:00 +0100

    Add test for plan queries with alerts

--- src/ext-test/java/org/opentripplanner/ext/legacygraphqlapi/GraphQLApiTest.java
+++ src/ext-test/java/org/opentripplanner/ext/legacygraphqlapi/GraphQLApiTest.java
@@ -2,7 +2,18 @@ package org.opentripplanner.ext.legacygraphqlapi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.opentripplanner.model.plan.PlanTestConstants.A;
+import static org.opentripplanner.model.plan.PlanTestConstants.B;
+import static org.opentripplanner.model.plan.PlanTestConstants.C;
+import static org.opentripplanner.model.plan.PlanTestConstants.D;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_00;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_01;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_15;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_30;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_50;
+import static org.opentripplanner.model.plan.TestItineraryBuilder.newItinerary;
 import static org.opentripplanner.test.support.JsonAssertions.assertEqualJson;
+import static org.opentripplanner.transit.model._data.TransitModelForTest.id;
 
 import jakarta.ws.rs.core.Response;
 import java.io.IOException;
@@ -17,12 +28,15 @@ import org.glassfish.jersey.message.internal.OutboundJaxrsResponse;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.opentripplanner.TestServerContext;
+import org.opentripplanner._support.time.ZoneIds;
 import org.opentripplanner.framework.i18n.NonLocalizedString;
+import org.opentripplanner.model.plan.Itinerary;
+import org.opentripplanner.routing.alertpatch.AlertCause;
+import org.opentripplanner.routing.alertpatch.TransitAlert;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.vehicle_parking.VehicleParking;
-import org.opentripplanner.standalone.api.OtpServerRequestContext;
 import org.opentripplanner.standalone.config.framework.json.JsonSupport;
+import org.opentripplanner.standalone.server.TestServerRequestContext;
 import org.opentripplanner.test.support.FilePatternSource;
 import org.opentripplanner.transit.model._data.TransitModelForTest;
 import org.opentripplanner.transit.model.basic.TransitMode;
@@ -31,7 +45,7 @@ import org.opentripplanner.transit.service.TransitModel;
 @Execution(ExecutionMode.CONCURRENT)
 class GraphQLApiTest {
 
-  static final OtpServerRequestContext context;
+  static final TestServerRequestContext context;
   static final Graph graph = new Graph();
   static final LegacyGraphQLAPI resource;
 
@@ -42,13 +56,14 @@ class GraphQLApiTest {
         List.of(
           VehicleParking
             .builder()
-            .id(TransitModelForTest.id("parking-1"))
+            .id(id("parking-1"))
             .name(NonLocalizedString.ofNullable("parking"))
             .build()
         ),
         List.of()
       );
     var transitModel = new TransitModel();
+    transitModel.initTimeZone(ZoneIds.BERLIN);
     transitModel.index();
     Arrays
       .stream(TransitMode.values())
@@ -64,7 +79,25 @@ class GraphQLApiTest {
               .build()
           )
       );
-    context = TestServerContext.createServerContext(graph, transitModel);
+
+    Itinerary i1 = newItinerary(A, T11_00)
+      .walk(20, B)
+      .bus(122, T11_01, T11_15, C)
+      .rail(439, T11_30, T11_50, D)
+      .build();
+
+    var railLeg = i1.getTransitLeg(2);
+    var alert = TransitAlert
+      .of(id("an-alert"))
+      .withHeaderText(new NonLocalizedString("A header"))
+      .withDescriptionText(new NonLocalizedString("A description"))
+      .withUrl(new NonLocalizedString("https://example.com"))
+      .withCause(AlertCause.MAINTENANCE)
+      .build();
+    railLeg.addAlert(alert);
+
+    context = new TestServerRequestContext(graph, transitModel);
+    context.setRoutingResult(List.of(i1));
     resource = new LegacyGraphQLAPI(context, "ignored");
   }
 
---
+++ src/test/java/org/opentripplanner/standalone/server/TestRoutingService.java
@@ -0,0 +1,49 @@
+package org.opentripplanner.standalone.server;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.util.List;
+import org.opentripplanner.model.plan.Itinerary;
+import org.opentripplanner.model.plan.PlanTestConstants;
+import org.opentripplanner.model.plan.TripPlan;
+import org.opentripplanner.routing.api.RoutingService;
+import org.opentripplanner.routing.api.request.RouteRequest;
+import org.opentripplanner.routing.api.request.RouteViaRequest;
+import org.opentripplanner.routing.api.response.RoutingResponse;
+import org.opentripplanner.routing.api.response.ViaRoutingResponse;
+import org.opentripplanner.routing.framework.DebugTimingAggregator;
+
+public class TestRoutingService implements RoutingService {
+
+  private final Instant instant = OffsetDateTime.parse("2023-01-27T21:08:35+01:00").toInstant();
+  private RoutingResponse routingResponse = null;
+
+  private RouteRequest lastRouteRequest = null;
+
+  public void setRoutingResponse(List<Itinerary> routingResult) {
+    this.routingResponse =
+      new RoutingResponse(
+        new TripPlan(PlanTestConstants.A, PlanTestConstants.B, instant, routingResult),
+        null,
+        null,
+        null,
+        List.of(),
+        new DebugTimingAggregator()
+      );
+  }
+
+  @Override
+  public RoutingResponse route(RouteRequest request) {
+    this.lastRouteRequest = request;
+    return routingResponse;
+  }
+
+  @Override
+  public ViaRoutingResponse route(RouteViaRequest request) {
+    return null;
+  }
+
+  public RouteRequest lastRouteRequest() {
+    return lastRouteRequest;
+  }
+}
