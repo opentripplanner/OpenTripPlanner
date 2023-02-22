e85d60b05d886581cd8d301a2f6edf7adfd84be9
Leonard Ehrenfried <mail@leonard.io>
2023-02-22 13:37:08 +0100

    Add test for arrive by scooter rentals

--- src/test/java/org/opentripplanner/smoketest/PortlandSmokeTest.java
+++ src/test/java/org/opentripplanner/smoketest/PortlandSmokeTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.opentripplanner.framework.geometry.WgsCoordinate;
 import org.opentripplanner.smoketest.util.SmokeTestRequest;
 
@@ -13,12 +15,12 @@ public class PortlandSmokeTest {
 
   WgsCoordinate cennentenial = new WgsCoordinate(45.504602, -122.4968719);
   WgsCoordinate hillside = new WgsCoordinate(45.5275, -122.7110);
+  WgsCoordinate piedmont = new WgsCoordinate(45.5746, -122.6697);
 
   @Test
   public void railRouteAcrossTheCity() {
-    Set<String> modes = Set.of("TRAM", "WALK");
     SmokeTest.basicRouteTest(
-      new SmokeTestRequest(cennentenial, hillside, modes),
+      new SmokeTestRequest(cennentenial, hillside, Set.of("TRAM", "WALK")),
       List.of("WALK", "TRAM", "WALK")
     );
   }
@@ -29,10 +31,18 @@ public class PortlandSmokeTest {
    */
   @Test
   public void geofencingZone() {
-    Set<String> modes = Set.of("SCOOTER_RENT", "WALK");
     SmokeTest.basicRouteTest(
-      new SmokeTestRequest(cennentenial, hillside, modes),
+      new SmokeTestRequest(cennentenial, hillside, Set.of("SCOOTER_RENT", "WALK")),
       List.of("WALK", "SCOOTER", "WALK")
+    );
+  }
+
+  @ParameterizedTest(name = "scooter rental with arriveBy={0}")
+  @ValueSource(booleans = { true, false })
+  void scooterRent(boolean arriveBy) {
+    SmokeTest.basicRouteTest(
+      new SmokeTestRequest(cennentenial, piedmont, Set.of("SCOOTER_RENT", "WALK"), arriveBy),
+      List.of("WALK", "SCOOTER")
     );
   }
 }
