82c1ab60a97dbb06fd83d3514dd48b9790b2c849
Leonard Ehrenfried <mail@leonard.io>
2023-02-28 20:34:21 +0100

    Apply suggestions from code review
    Co-authored-by: Thomas Gran <t2gran@gmail.com>

--- src/test/java/org/opentripplanner/raptor/moduletests/F04_AccessEgressWithRidesNoTransitTest.java
+++ src/test/java/org/opentripplanner/raptor/moduletests/F04_AccessEgressWithRidesNoTransitTest.java
@@ -30,7 +30,9 @@ import org.opentripplanner.raptor.spi.UnknownPathString;
 /**
  * FEATURE UNDER TEST
  * <p>
- * Raptor should be able to timeshift a Flex ~ Walk ~ Flex path.
+ * Raptor should be able to time-shift a Flex ~ Walk ~ Flex path, either to the 
+ * requested departure time (iteration-departure-time) or to the earliest possible 
+ * time within the opening hours.
  */
 public class F04_AccessEgressWithRidesNoTransitTest implements RaptorTestConstants {
 
@@ -46,45 +48,38 @@ public class F04_AccessEgressWithRidesNoTransitTest implements RaptorTestConstan
         // Pattern arrive at stop 2 at 0:03:00
         route(pattern("R1", STOP_A, STOP_D))
           .withTimetable(
-            // First trip is too early: It takes 2m to get to the point of boarding:
-            // --> 00:00:00 + flex 30s + slack(1m + 30s) = 00:02:00
-            schedule().departures("0:03:29, 0:05:29"),
-            // This is the trip we expect to board
-            schedule().departures("0:04:00, 0:10:00").arrivals("0, 00:06:00"),
+            schedule().departures("0:04 0:10").arrivals("0:00 0:06"),
             // REVERSE SEARCH: The last trip arrives too late: It takes 1m40s to get to the
             // point of "boarding" in the reverse search:
             // --> 00:10:00 - (flex 20s + slack(1m + 10s)) = 00:08:30  (arrival time)
             schedule().arrivals("0:04:51, 0:06:51")
           )
       )
-      .withTransfer(RaptorTestConstants.STOP_B, transfer(STOP_C, D5m));
+      .withTransfer(STOP_B, transfer(STOP_C, D5m));
   }
 
   @Nonnull
   private static RaptorRequestBuilder<TestTripSchedule> baseRequestBuilder() {
-    RaptorRequestBuilder<TestTripSchedule> requestBuilder = new RaptorRequestBuilder<>();
+    var requestBuilder = new RaptorRequestBuilder<TestTripSchedule>();
     requestBuilder
       .searchParams()
       .addEgressPaths(flex(STOP_C, D2m, ONE_RIDE, 56_000))
       .earliestDepartureTime(T00_10)
       .latestArrivalTime(T00_30)
-      // Only one iteration is needed - the access should be time-shifted
       .searchWindowInSeconds(D10m);
     return requestBuilder;
   }
 
   static List<RaptorModuleTestCase> flexOnlyNoWalkTestCases() {
     var path = "Flex 2m 1x ~ B ~ Walk 5m ~ C ~ Flex 2m 1x [0:15 0:25 10m 1tx $1620]";
+    var stdPathRev = "Flex 2m 1x ~ B ~ Walk 5m ~ C ~ Flex 2m 1x [0:35 0:45 10m 1tx]";
     var minDurationPath = UnknownPathString.of(D10m, 1);
     return RaptorModuleTestCase
       .of()
       .add(TC_MIN_DURATION, minDurationPath.departureAt(T00_10))
       .add(TC_MIN_DURATION_REV, minDurationPath.arrivalAt(T00_30))
       .add(standard().forwardOnly(), PathUtils.withoutCost(path))
-      .add(
-        standard().reverseOnly(),
-        "Flex 2m 1x ~ B ~ Walk 5m ~ C ~ Flex 2m 1x [0:35 0:45 10m 1tx]"
-      )
+      .add(standard().reverseOnly(), stdPathRev)
       .add(multiCriteria(), path)
       .build();
   }
