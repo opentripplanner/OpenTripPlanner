d6f70b9fe738fc47a238aacba46ec33e7801517e
Leonard Ehrenfried <mail@leonard.io>
2023-03-01 14:15:36 +0100

    Add test for indexed leg products, documentation

--- src/main/java/org/opentripplanner/routing/core/IndexedLegProducts.java
+++ src/main/java/org/opentripplanner/routing/core/IndexedLegProducts.java
@@ -4,4 +4,10 @@ import java.util.Collection;
 import org.opentripplanner.ext.fares.model.FareProduct;
 import org.opentripplanner.model.plan.Leg;
 
+/**
+ * The result of computing the indices of legs that are associated with fares.
+ * @param products
+ * @param leg The leg that the fare products are valid for.
+ * @param legIndex The index of the leg in the itinerary that the fare products are valid for.
+ */
 public record IndexedLegProducts(Collection<FareProduct> products, Leg leg, int legIndex) {}
---
+++ src/test/java/org/opentripplanner/routing/core/ItineraryFaresTest.java
@@ -0,0 +1,65 @@
+package org.opentripplanner.routing.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.opentripplanner.model.plan.PlanTestConstants.A;
+import static org.opentripplanner.model.plan.PlanTestConstants.B;
+import static org.opentripplanner.model.plan.PlanTestConstants.C;
+import static org.opentripplanner.model.plan.PlanTestConstants.D;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_00;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_01;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_15;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_30;
+import static org.opentripplanner.model.plan.PlanTestConstants.T11_50;
+import static org.opentripplanner.model.plan.TestItineraryBuilder.newItinerary;
+import static org.opentripplanner.transit.model._data.TransitModelForTest.id;
+
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.junit.jupiter.api.Test;
+import org.opentripplanner.ext.fares.model.FareProduct;
+import org.opentripplanner.model.plan.Itinerary;
+import org.opentripplanner.transit.model.basic.Money;
+
+class ItineraryFaresTest {
+
+  @Test
+  void indexedLegProducts() {
+    Itinerary i1 = newItinerary(A, T11_00)
+      .walk(20, B)
+      .bus(122, T11_01, T11_15, C)
+      .rail(439, T11_30, T11_50, D)
+      .build();
+
+    var busLeg = i1.getTransitLeg(1);
+    var railLeg = i1.getTransitLeg(2);
+
+    var fares = new ItineraryFares();
+
+    var busTicket = fareProduct("bus");
+    var railTicketA = fareProduct("rail-a");
+    var railTicketB = fareProduct("rail-b");
+
+    fares.addFareProduct(busLeg, busTicket);
+    fares.addFareProduct(railLeg, railTicketA);
+    fares.addFareProduct(railLeg, railTicketB);
+
+    var indexed = fares.indexedLegProducts(i1);
+
+    assertEquals(2, indexed.size());
+
+    var first = indexed.get(0);
+    assertEquals(busLeg, first.leg());
+    assertEquals(1, first.legIndex());
+    assertEquals(List.of(busTicket), first.products());
+
+    var second = indexed.get(1);
+    assertEquals(railLeg, second.leg());
+    assertEquals(2, second.legIndex());
+    assertEquals(List.of(railTicketA, railTicketB), second.products());
+  }
+
+  @Nonnull
+  private static FareProduct fareProduct(String id) {
+    return new FareProduct(id(id), id, Money.euros(1000), null, null, null);
+  }
+}
