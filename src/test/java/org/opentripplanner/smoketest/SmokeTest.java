ff2ab38bbbfe21862241e7d0ab27f504a89ec7ae
Leonard Ehrenfried <mail@leonard.io>
2023-03-02 12:24:08 +0100

    Revert to next Monday

--- src/test/java/org/opentripplanner/smoketest/SmokeTest.java
+++ src/test/java/org/opentripplanner/smoketest/SmokeTest.java
@@ -59,7 +59,7 @@ public class SmokeTest {
    */
   public static LocalDate nextMonday() {
     var today = LocalDate.now();
-    return today.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+    return today.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
   }
 
   public static void assertThatThereAreVehicleRentalStations() {
