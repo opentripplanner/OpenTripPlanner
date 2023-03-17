b82a27f47fb75b7689a8eb83b8dff43a0459699f
Leonard Ehrenfried <mail@leonard.io>
2023-03-17 15:09:28 +0100

    Remove unused car hailing router

--- src/main/java/org/opentripplanner/routing/algorithm/RoutingWorker.java
+++ src/main/java/org/opentripplanner/routing/algorithm/RoutingWorker.java
@@ -11,7 +11,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
-import org.opentripplanner.ext.ridehailing.RideHailingRouter;
 import org.opentripplanner.ext.ridehailing.RideHailingService;
 import org.opentripplanner.framework.application.OTPFeature;
 import org.opentripplanner.framework.time.ServiceDateUtils;
@@ -246,11 +245,7 @@ public class RoutingWorker {
   ) {
     debugTimingAggregator.startedDirectStreetRouter();
     try {
-      if (request.journey().modes().directMode == StreetMode.CAR_HAILING) {
-        itineraries.addAll(RideHailingRouter.routeDirect(serverContext, request));
-      } else {
-        itineraries.addAll(DirectStreetRouter.route(serverContext, request));
-      }
+      itineraries.addAll(DirectStreetRouter.route(serverContext, request));
     } catch (RoutingValidationException e) {
       routingErrors.addAll(e.getRoutingErrors());
     } finally {
