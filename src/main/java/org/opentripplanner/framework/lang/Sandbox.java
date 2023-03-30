35c3b06072b296a44a84cd854761c28fcbc7ae85
Leonard Ehrenfried <mail@leonard.io>
2023-03-30 20:51:42 +0200

    Add documentation for annotation

--- src/main/java/org/opentripplanner/framework/lang/Sandbox.java
+++ src/main/java/org/opentripplanner/framework/lang/Sandbox.java
@@ -8,6 +8,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is purely for documentation purposes and tells developers that a component
+ * is part of sandbox code.
+ */
 @Target({ FIELD, METHOD, CONSTRUCTOR })
 @Retention(RetentionPolicy.SOURCE)
 public @interface Sandbox {
