69412eb7171a5ab77326b1483d39c63fd464334e
Henrik Abrahamsson <henrik.abrahamsson@decerno.se>
2023-04-05 08:51:52 +0000

    Fix staySeated in RealtimeResolver
    Fix issue where RealtimeResolver will drop the information about constrained transfers like "staySeated".

--- src/ext/java/org/opentripplanner/ext/realtimeresolver/RealtimeResolver.java
+++ src/ext/java/org/opentripplanner/ext/realtimeresolver/RealtimeResolver.java
@@ -3,6 +3,8 @@ package org.opentripplanner.ext.realtimeresolver;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.opentripplanner.model.plan.Itinerary;
+import org.opentripplanner.model.plan.Leg;
+import org.opentripplanner.model.plan.ScheduledTransitLeg;
 import org.opentripplanner.transit.service.TransitService;
 
 public class RealtimeResolver {
@@ -21,22 +23,51 @@ public class RealtimeResolver {
       var legs = it
         .getLegs()
         .stream()
-        .map(l -> {
-          var ref = l.getLegReference();
+        .map(leg -> {
+          var ref = leg.getLegReference();
           if (ref == null) {
-            return l;
-          }
-
-          var leg = ref.getLeg(transitService);
-          if (leg != null) {
             return leg;
           }
 
-          return l;
+          // Only ScheduledTransitLeg has leg references atm, so this check is just to be future-proof
+          if (!(leg.isScheduledTransitLeg())) {
+            return leg;
+          }
+
+          var realtimeLeg = ref.getLeg(transitService);
+          if (realtimeLeg != null) {
+            return combineReferenceWithOriginal(
+              realtimeLeg.asScheduledTransitLeg(),
+              leg.asScheduledTransitLeg()
+            );
+          }
+          return leg;
         })
         .collect(Collectors.toList());
 
       it.setLegs(legs);
     });
+  }
+
+  private static Leg combineReferenceWithOriginal(
+    ScheduledTransitLeg reference,
+    ScheduledTransitLeg original
+  ) {
+    var leg = new ScheduledTransitLeg(
+      reference.getTripTimes(),
+      reference.getTripPattern(),
+      reference.getBoardStopPosInPattern(),
+      reference.getAlightStopPosInPattern(),
+      reference.getStartTime(),
+      reference.getEndTime(),
+      reference.getServiceDate(),
+      reference.getZoneId(),
+      original.getTransferFromPrevLeg(),
+      original.getTransferToNextLeg(),
+      original.getGeneralizedCost(),
+      original.accessibilityScore()
+    );
+    reference.getTransitAlerts().forEach(leg::addAlert);
+    return leg;
   }
 }
