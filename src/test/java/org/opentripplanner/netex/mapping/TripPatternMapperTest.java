c7cf9bdf6a863390612ad046170a7f951c424717
vincent <vincent.paturet@entur.org>
2023-04-21 11:11:52 +0200

    Added unit tests

--- src/test/java/org/opentripplanner/netex/mapping/TripPatternMapperTest.java
+++ src/test/java/org/opentripplanner/netex/mapping/TripPatternMapperTest.java
@@ -1,11 +1,16 @@
 package org.opentripplanner.netex.mapping;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.common.collect.ArrayListMultimap;
+import java.time.LocalTime;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.graph_builder.issue.api.DataImportIssueStore;
+import org.opentripplanner.graph_builder.issue.service.DefaultDataImportIssueStore;
+import org.opentripplanner.graph_builder.issues.NegativeDwellTime;
+import org.opentripplanner.graph_builder.issues.NegativeHopTime;
 import org.opentripplanner.netex.index.hierarchy.HierarchicalMap;
 import org.opentripplanner.netex.index.hierarchy.HierarchicalMapById;
 import org.opentripplanner.transit.model._data.TransitModelForTest;
@@ -19,40 +24,24 @@ import org.opentripplanner.transit.model.timetable.TripOnServiceDate;
 import org.opentripplanner.transit.model.timetable.TripTimes;
 import org.rutebanken.netex.model.DatedServiceJourney;
 import org.rutebanken.netex.model.OperatingDay;
+import org.rutebanken.netex.model.ServiceJourney;
+import org.rutebanken.netex.model.TimetabledPassingTime;
 
 /**
  * @author Thomas Gran (Capra) - tgr@capraconsulting.no (29.11.2017)
  */
-public class TripPatternMapperTest {
+class TripPatternMapperTest {
 
   private static final FeedScopedId SERVICE_ID = TransitModelForTest.id("S01");
 
   @Test
-  public void testMapTripPattern() {
+  void testMapTripPattern() {
     NetexTestDataSample sample = new NetexTestDataSample();
 
-    TripPatternMapper tripPatternMapper = new TripPatternMapper(
-      DataImportIssueStore.NOOP,
-      MappingSupport.ID_FACTORY,
-      new EntityById<>(),
-      sample.getStopsById(),
-      new EntityById<>(),
-      new EntityById<>(),
-      sample.getOtpRouteByid(),
-      sample.getRouteById(),
-      sample.getJourneyPatternById(),
-      sample.getQuayIdByStopPointRef(),
-      new HierarchicalMap<>(),
-      new HierarchicalMapById<>(),
+    TripPatternMapper tripPatternMapper = tripPatternMapper(
+      sample,
       sample.getServiceJourneyById(),
-      new HierarchicalMapById<>(),
-      new HierarchicalMapById<>(),
-      new HierarchicalMapById<>(),
-      new HierarchicalMapById<>(),
-      ArrayListMultimap.create(),
-      Map.of(NetexTestDataSample.SERVICE_JOURNEY_ID, SERVICE_ID),
-      new Deduplicator(),
-      150
+      DataImportIssueStore.NOOP
     );
 
     TripPatternMapperResult r = tripPatternMapper.mapTripPattern(sample.getJourneyPattern());
@@ -64,7 +53,7 @@ public class TripPatternMapperTest {
     assertEquals(4, tripPattern.numberOfStops());
     assertEquals(1, tripPattern.scheduledTripsAsStream().count());
 
-    Trip trip = tripPattern.scheduledTripsAsStream().findFirst().get();
+    Trip trip = tripPattern.scheduledTripsAsStream().findFirst().orElseThrow();
 
     assertEquals("RUT:ServiceJourney:1", trip.getId().getId());
     assertEquals("NSR:Quay:1", tripPattern.getStop(0).getId().getId());
@@ -85,7 +74,73 @@ public class TripPatternMapperTest {
   }
 
   @Test
-  public void testMapTripPattern_datedServiceJourney() {
+  void testMapTripPatternWithNonIncreasingDepartureTime() {
+    NetexTestDataSample sample = new NetexTestDataSample();
+
+    HierarchicalMapById<ServiceJourney> serviceJourneyById = sample.getServiceJourneyById();
+    ServiceJourney serviceJourney = serviceJourneyById
+      .localValues()
+      .stream()
+      .findFirst()
+      .orElseThrow();
+    // set the last departure time back to midnight
+    serviceJourney
+      .getPassingTimes()
+      .getTimetabledPassingTime()
+      .get(serviceJourney.getPassingTimes().getTimetabledPassingTime().size() - 1)
+      .withDepartureTime(LocalTime.MIDNIGHT);
+
+    DataImportIssueStore issueStore = new DefaultDataImportIssueStore();
+    TripPatternMapper tripPatternMapper = tripPatternMapper(sample, serviceJourneyById, issueStore);
+
+    TripPatternMapperResult r = tripPatternMapper.mapTripPattern(sample.getJourneyPattern());
+
+    assertEquals(1, r.tripPatterns.size());
+    TripPattern tripPattern = r.tripPatterns.values().stream().findFirst().orElseThrow();
+    assertEquals(4, tripPattern.numberOfStops());
+    assertEquals(
+      0,
+      tripPattern.scheduledTripsAsStream().count(),
+      "Trips with non-increasing stop times should be dropped"
+    );
+    assertTrue(issueStore.listIssues().stream().anyMatch(NegativeHopTime.class::isInstance));
+  }
+
+  @Test
+  void testMapTripPatternWithDepartureTimeBeforeArrivalTime() {
+    NetexTestDataSample sample = new NetexTestDataSample();
+
+    HierarchicalMapById<ServiceJourney> serviceJourneyById = sample.getServiceJourneyById();
+    ServiceJourney serviceJourney = serviceJourneyById
+      .localValues()
+      .stream()
+      .findFirst()
+      .orElseThrow();
+    // set the departure time before the arrival time
+    TimetabledPassingTime timetabledPassingTime = serviceJourney
+      .getPassingTimes()
+      .getTimetabledPassingTime()
+      .get(0);
+    timetabledPassingTime.withArrivalTime(timetabledPassingTime.getDepartureTime().plusMinutes(1));
+
+    DataImportIssueStore issueStore = new DefaultDataImportIssueStore();
+    TripPatternMapper tripPatternMapper = tripPatternMapper(sample, serviceJourneyById, issueStore);
+
+    TripPatternMapperResult r = tripPatternMapper.mapTripPattern(sample.getJourneyPattern());
+
+    assertEquals(1, r.tripPatterns.size());
+    TripPattern tripPattern = r.tripPatterns.values().stream().findFirst().orElseThrow();
+    assertEquals(4, tripPattern.numberOfStops());
+    assertEquals(
+      0,
+      tripPattern.scheduledTripsAsStream().count(),
+      "Trips with departure time before arrival time should be dropped"
+    );
+    assertTrue(issueStore.listIssues().stream().anyMatch(NegativeDwellTime.class::isInstance));
+  }
+
+  @Test
+  void testMapTripPatternWithDatedServiceJourney() {
     NetexTestDataSample sample = new NetexTestDataSample();
 
     HierarchicalMapById<DatedServiceJourney> datedServiceJourneys = new HierarchicalMapById<>();
@@ -121,7 +176,7 @@ public class TripPatternMapperTest {
     assertEquals(2, r.tripOnServiceDates.size());
 
     TripPattern tripPattern = r.tripPatterns.values().stream().findFirst().orElseThrow();
-    Trip trip = tripPattern.scheduledTripsAsStream().findFirst().get();
+    Trip trip = tripPattern.scheduledTripsAsStream().findFirst().orElseThrow();
 
     for (TripOnServiceDate tripOnServiceDate : r.tripOnServiceDates) {
       assertEquals(trip, tripOnServiceDate.getTrip());
@@ -137,5 +192,35 @@ public class TripPatternMapperTest {
           .count()
       );
     }
+  }
+
+  private static TripPatternMapper tripPatternMapper(
+    NetexTestDataSample sample,
+    HierarchicalMapById<ServiceJourney> serviceJourneyById,
+    DataImportIssueStore issueStore
+  ) {
+    return new TripPatternMapper(
+      issueStore,
+      MappingSupport.ID_FACTORY,
+      new EntityById<>(),
+      sample.getStopsById(),
+      new EntityById<>(),
+      new EntityById<>(),
+      sample.getOtpRouteByid(),
+      sample.getRouteById(),
+      sample.getJourneyPatternById(),
+      sample.getQuayIdByStopPointRef(),
+      new HierarchicalMap<>(),
+      new HierarchicalMapById<>(),
+      serviceJourneyById,
+      new HierarchicalMapById<>(),
+      new HierarchicalMapById<>(),
+      new HierarchicalMapById<>(),
+      new HierarchicalMapById<>(),
+      ArrayListMultimap.create(),
+      Map.of(NetexTestDataSample.SERVICE_JOURNEY_ID, SERVICE_ID),
+      new Deduplicator(),
+      150
+    );
   }
 }
