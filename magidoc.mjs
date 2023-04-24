106bee7a054d43033b6322182a0b513916ae8284
Leonard Ehrenfried <mail@leonard.io>
2023-04-24 17:21:45 +0200

    Deploy the compiled HTML

--- magidoc.mjs
+++ magidoc.mjs
@@ -5,18 +5,24 @@ export default {
   },
   website: {
     template: 'carbon-multi-page',
-    output: 'target/magidocs/api/graphql-gtfs/v1/',
+    output: 'target/magidoc/api/graphql-gtfs/v1/',
     options: {
       siteRoot: '/api/dev-2.x/graphql-gtfs/v1',
+      appLogo: 'https://docs.opentripplanner.org/en/dev-2.x/images/otp-logo.svg',
       pages: [{
         title: 'Introduction',
         content: `
-# Static documentation
+# OTP GTFS GraphQL API v1 documentation
 
 This is the static documentation of the OTP GraphQL GTFS API v1.
 
+The GraphQL endpoint of your instance, which you should point your tooling to, is 
+\`http://localhost:8080/otp/routers/default/index/graphql\`
+
 Please also check out the interactive API explorer built into every instance and available
 at http://localhost:8080/graphiql 
+
+![GraphiQL screenshot](https://docs.opentripplanner.org/en/dev-2.x/images/graphiql.png)
 `,
       }],
       appTitle: 'OTP GTFS GraphQL API v1',
