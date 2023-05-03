e91a9ce8514b98d73330825320bc1bbc8080f16d
Leonard Ehrenfried <mail@leonard.io>
2023-05-03 16:27:49 +0200

    Only return WALKING_BETTER_THAN_TRANSIT if there really is any walking

--- src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryListFilterChain.java
+++ src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryListFilterChain.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.opentripplanner.model.plan.Itinerary;
+import org.opentripplanner.model.plan.StreetLeg;
 import org.opentripplanner.routing.algorithm.filterchain.deletionflagger.LatestDepartureTimeFilter;
 import org.opentripplanner.routing.algorithm.filterchain.deletionflagger.RemoveTransitIfStreetOnlyIsBetterFilter;
 import org.opentripplanner.routing.api.response.RoutingError;
@@ -56,7 +57,10 @@ public class ItineraryListFilterChain {
           .stream()
           .anyMatch(notice -> notice.tag.equals(LatestDepartureTimeFilter.TAG));
       if (result.stream().allMatch(isOnStreetAllTheWay.or(isWorseThanStreet))) {
-        routingErrors.add(new RoutingError(WALKING_BETTER_THAN_TRANSIT, null));
+        var nonTransitIsWalking = result.stream().flatMap(Itinerary::getStreetLegs).allMatch(StreetLeg::isWalkingLeg);
+        if(nonTransitIsWalking) {
+          routingErrors.add(new RoutingError(WALKING_BETTER_THAN_TRANSIT, null));
+        }
       } else if (result.stream().allMatch(isOnStreetAllTheWay.or(isOutsideSearchWindow))) {
         routingErrors.add(new RoutingError(NO_TRANSIT_CONNECTION_IN_SEARCH_WINDOW, DATE_TIME));
       }
