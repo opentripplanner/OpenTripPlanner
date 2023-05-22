d1733184c14485d15214d8909d813f14228b654c
bartosz <bartosz.kruba@purplescout.se>
2023-05-22 13:22:34 +0200

    switch to builder

--- src/main/java/org/opentripplanner/raptor/path/Path.java
+++ src/main/java/org/opentripplanner/raptor/path/Path.java
@@ -23,21 +23,17 @@ import org.opentripplanner.raptor.api.path.TransitPathLeg;
  * @param <T> The TripSchedule type defined by the user of the raptor API.
  */
 public class Path<T extends RaptorTripSchedule> implements RaptorPath<T> {
-  // TODO: 2023-05-11 via pass through: figure out how to inject c2 here
-
   private final int iterationDepartureTime;
   private final int startTime;
   private final int endTime;
   private final int numberOfTransfers;
   private final int generalizedCost;
-  // TODO: 2023-05-11 via pass through: this should be final
-  private int c2;
+  private final int c2;
   private final AccessPathLeg<T> accessLeg;
   private final EgressPathLeg<T> egressPathLeg;
 
   /** @see #dummyPath(int, int, int, int, int) */
   private Path(
-    // TODO: 2023-05-11 via pass trough: NOT THIS ONE
     int iterationDepartureTime,
     int startTime,
     int endTime,
@@ -55,8 +51,6 @@ public class Path<T extends RaptorTripSchedule> implements RaptorPath<T> {
   }
 
   public Path(int iterationDepartureTime, AccessPathLeg<T> accessLeg, int generalizedCost, int c2) {
-    // TODO: 2023-05-11 via pass trough: NOT THIS ONE
-
     this.iterationDepartureTime = iterationDepartureTime;
     this.startTime = accessLeg.fromTime();
     this.generalizedCost = generalizedCost;
@@ -67,10 +61,8 @@ public class Path<T extends RaptorTripSchedule> implements RaptorPath<T> {
     this.c2 = c2;
   }
 
-  public Path(int iterationDepartureTime, AccessPathLeg<T> accessLeg) {
-    // TODO: 2023-05-11 via pass through: here w need to in some way include c2
-
-    this(iterationDepartureTime, accessLeg, accessLeg.generalizedCostTotal(), 0);
+  public Path(int iterationDepartureTime, AccessPathLeg<T> accessLeg, int c2) {
+    this(iterationDepartureTime, accessLeg, accessLeg.generalizedCostTotal(), c2);
   }
 
   /** Copy constructor */
@@ -130,12 +122,6 @@ public class Path<T extends RaptorTripSchedule> implements RaptorPath<T> {
   @Override
   public int c2() {
     return c2;
-  }
-
-  // TODO: 2023-05-11 via pass through: get rid of this setter
-  //  c2 should be injected through constructor
-  public void setC2(int c2) {
-    this.c2 = c2;
   }
 
   @Override
