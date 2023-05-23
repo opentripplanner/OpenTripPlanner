4df270dd93d0829b90cc58533397aedc11eb86ba
Leonard Ehrenfried <mail@leonard.io>
2023-05-23 09:46:54 +0200

    Remove unneeded rounding

--- src/ext-test/java/org/opentripplanner/ext/fares/impl/HighestFareInFreeTransferWindowFareServiceTest.java
+++ src/ext-test/java/org/opentripplanner/ext/fares/impl/HighestFareInFreeTransferWindowFareServiceTest.java
@@ -43,7 +43,7 @@ class HighestFareInFreeTransferWindowFareServiceTest implements PlanTestConstant
     float expectedFare
   ) {
     var fares = fareService.calculateFares(i);
-    final Money expected = Money.usDollars(Math.round(expectedFare));
+    final Money expected = Money.usDollars(expectedFare);
     assertEquals(expected, fares.getFare(FareType.regular));
 
     for (var type : fares.getFareTypes()) {
