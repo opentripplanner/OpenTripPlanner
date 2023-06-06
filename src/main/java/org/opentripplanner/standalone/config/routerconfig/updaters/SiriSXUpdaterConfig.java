1c0db4f4ed0533b0e05028338d62fc62dce4e286
Thomas Gran <t2gran@gmail.com>
2023-06-06 18:40:39 +0200

    Update src/main/java/org/opentripplanner/standalone/config/routerconfig/updaters/SiriSXUpdaterConfig.java
    Co-authored-by: Leonard Ehrenfried <mail@leonard.io>

--- src/main/java/org/opentripplanner/standalone/config/routerconfig/updaters/SiriSXUpdaterConfig.java
+++ src/main/java/org/opentripplanner/standalone/config/routerconfig/updaters/SiriSXUpdaterConfig.java
@@ -26,8 +26,8 @@ public class SiriSXUpdaterConfig {
         .summary("This value is subtracted from the actual validity defined in the message.")
         .description(
           """
-          Normally the planed departure time is used, so setting this to 10s will cause the
-          SX-message to be included in trip-results 10 seconds before the the planed departure
+          Normally the planned departure time is used, so setting this to 10s will cause the
+          SX-message to be included in trip-results 10 seconds before the the planned departure
           time."""
         )
         .asDuration(Duration.ZERO),
