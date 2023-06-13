f33fc6af3612962bc5eba3c3e75a01267d6967eb
Viljami Nurminen <viljami.nurminen@cgi.com>
2023-06-13 10:51:38 +0300

    Reduce default escalator reluctance below stairs reluctance

--- src/main/java/org/opentripplanner/routing/api/request/preference/WalkPreferences.java
+++ src/main/java/org/opentripplanner/routing/api/request/preference/WalkPreferences.java
@@ -37,7 +37,7 @@ public final class WalkPreferences implements Serializable {
     this.stairsReluctance = 2.0;
     this.stairsTimeFactor = 3.0;
     this.safetyFactor = 1.0;
-    this.escalatorReluctance = 2.0;
+    this.escalatorReluctance = 1.5;
   }
 
   private WalkPreferences(Builder builder) {
