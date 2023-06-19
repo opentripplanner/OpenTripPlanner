d48226024410baf8acbe310073051437128ea606
Leonard Ehrenfried <mail@leonard.io>
2023-06-19 10:08:03 +0200

    Add test for VehicleRentalEdge

--- src/test/java/org/opentripplanner/street/model/edge/VehicleRentalEdgeTest.java
+++ src/test/java/org/opentripplanner/street/model/edge/VehicleRentalEdgeTest.java
@@ -1,19 +1,29 @@
 package org.opentripplanner.street.model.edge;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.api.request.request.VehicleRentalRequest;
 import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.service.vehiclerental.model.GeofencingZone;
+import org.opentripplanner.service.vehiclerental.model.RentalVehicleType;
 import org.opentripplanner.service.vehiclerental.model.TestVehicleRentalStationBuilder;
+import org.opentripplanner.service.vehiclerental.model.VehicleRentalVehicle;
+import org.opentripplanner.service.vehiclerental.street.GeofencingZoneExtension;
 import org.opentripplanner.service.vehiclerental.street.VehicleRentalEdge;
 import org.opentripplanner.service.vehiclerental.street.VehicleRentalPlaceVertex;
 import org.opentripplanner.street.model.RentalFormFactor;
 import org.opentripplanner.street.search.request.StreetSearchRequest;
 import org.opentripplanner.street.search.state.State;
+import org.opentripplanner.street.search.state.VehicleRentalState;
+import org.opentripplanner.transit.model.framework.FeedScopedId;
 
 class VehicleRentalEdgeTest {
 
@@ -120,6 +130,69 @@ class VehicleRentalEdgeTest {
 
   private void initEdgeAndRequest(StreetMode mode, int vehicles, int spaces) {
     initEdgeAndRequest(mode, vehicles, spaces, false, true, true);
+  }
+
+  @Nested
+  class StartedReverseSearchInNoGeofencingZone {
+
+    private static final String NETWORK = "tier";
+    private static final StreetSearchRequest SEARCH_REQUEST = StreetSearchRequest
+      .of()
+      .withMode(StreetMode.SCOOTER_RENTAL)
+      .withArriveBy(true)
+      .build();
+
+    private static final VehicleRentalVehicle RENTAL_PLACE = new VehicleRentalVehicle();
+
+    static {
+      RENTAL_PLACE.latitude = 1;
+      RENTAL_PLACE.longitude = 1;
+      RENTAL_PLACE.id = new FeedScopedId(NETWORK, "123");
+      RENTAL_PLACE.vehicleType =
+        new RentalVehicleType(
+          new FeedScopedId(NETWORK, "scooter"),
+          "scooter",
+          RentalFormFactor.SCOOTER,
+          RentalVehicleType.PropulsionType.ELECTRIC,
+          100000d
+        );
+    }
+
+    @Test
+    void startedInNoDropOffZone() {
+      var rentalVertex = new VehicleRentalPlaceVertex(graph, RENTAL_PLACE);
+      var rentalEdge = new VehicleRentalEdge(rentalVertex, RentalFormFactor.SCOOTER);
+
+      rentalVertex.addRentalRestriction(noDropOffZone());
+
+      var state = new State(rentalVertex, SEARCH_REQUEST);
+
+      assertEquals(Set.of(NETWORK), state.stateData.noRentalDropOffZonesAtStartOfReverseSearch);
+
+      assertTrue(State.isEmpty(rentalEdge.traverse(state)));
+    }
+
+    @Test
+    void startedOutsideNoDropOffZone() {
+      var rentalVertex = new VehicleRentalPlaceVertex(graph, RENTAL_PLACE);
+      var rentalEdge = new VehicleRentalEdge(rentalVertex, RentalFormFactor.SCOOTER);
+      var state = new State(rentalVertex, SEARCH_REQUEST);
+
+      assertEquals(Set.of(), state.stateData.noRentalDropOffZonesAtStartOfReverseSearch);
+      var result = rentalEdge.traverse(state);
+
+      assertEquals(1, result.length);
+
+      var afterTraversal = result[0];
+      assertEquals(VehicleRentalState.BEFORE_RENTING, afterTraversal.getVehicleRentalState());
+    }
+
+    @Nonnull
+    private GeofencingZoneExtension noDropOffZone() {
+      return new GeofencingZoneExtension(
+        new GeofencingZone(new FeedScopedId(NETWORK, "zone"), null, true, false)
+      );
+    }
   }
 
   private void initEdgeAndRequest(
