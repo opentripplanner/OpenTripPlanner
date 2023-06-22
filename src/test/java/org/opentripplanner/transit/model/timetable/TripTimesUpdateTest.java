f0b0a53973bd6ddb61cd0269fd45e6023efdad26
Jim Martens <jma@hbt.de>
2023-06-22 15:47:50 +0200

    Moved TripTimesUpdateTest

--- src/test/java/org/opentripplanner/transit/model/timetable/TripTimesUpdateTest.java
+++ src/test/java/org/opentripplanner/transit/model/timetable/TripTimesUpdateTest.java
@@ -1,4 +1,4 @@
-package org.opentripplanner.routing.trippattern;
+package org.opentripplanner.transit.model.timetable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -14,11 +14,8 @@ import org.opentripplanner.transit.model._data.TransitModelForTest;
 import org.opentripplanner.transit.model.framework.Deduplicator;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
 import org.opentripplanner.transit.model.site.RegularStop;
-import org.opentripplanner.transit.model.timetable.RealTimeState;
-import org.opentripplanner.transit.model.timetable.Trip;
-import org.opentripplanner.transit.model.timetable.TripTimes;
 
-public class TripTimesTest {
+public class TripTimesUpdateTest {
 
   private static final String TRIP_ID = "testTripId";
 
