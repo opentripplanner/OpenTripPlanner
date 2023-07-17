16ef5274afcc89b95d2128a2100d46228b9baf21
Leonard Ehrenfried <mail@leonard.io>
2023-07-17 10:13:32 +0200

    Convert to parameterised test

--- src/ext-test/java/org/opentripplanner/ext/flex/AreaStopsToVerticesMapperTest.java
+++ src/ext-test/java/org/opentripplanner/ext/flex/AreaStopsToVerticesMapperTest.java
@@ -2,11 +2,21 @@ package org.opentripplanner.ext.flex;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static org.opentripplanner._support.geometry.Coordinates.BERLIN;
 import static org.opentripplanner._support.geometry.Coordinates.HAMBURG;
+import static org.opentripplanner.street.model.StreetTraversalPermission.ALL;
+import static org.opentripplanner.street.model.StreetTraversalPermission.BICYCLE_AND_CAR;
+import static org.opentripplanner.street.model.StreetTraversalPermission.CAR;
+import static org.opentripplanner.street.model.StreetTraversalPermission.PEDESTRIAN;
+import static org.opentripplanner.street.model.StreetTraversalPermission.PEDESTRIAN_AND_CAR;
 
+import java.util.List;
 import java.util.Set;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.locationtech.jts.geom.Coordinate;
 import org.opentripplanner._support.geometry.Polygons;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.street.model.StreetTraversalPermission;
@@ -29,67 +39,49 @@ class AreaStopsToVerticesMapperTest {
     .build();
 
   public static final TransitModel TRANSIT_MODEL = new TransitModel(STOP_MODEL, new Deduplicator());
+  private final List<TestCase> testCases = List.of(
+    new TestCase(BERLIN, ALL, Set.of(BERLIN_AREA_STOP)),
+    new TestCase(BERLIN, PEDESTRIAN_AND_CAR, Set.of(BERLIN_AREA_STOP)),
+    new TestCase(BERLIN, BICYCLE_AND_CAR, Set.of()),
+    new TestCase(HAMBURG, ALL, Set.of()),
+    new TestCase(BERLIN, PEDESTRIAN, Set.of()),
+    new TestCase(HAMBURG, PEDESTRIAN, Set.of()),
+    new TestCase(BERLIN, CAR, Set.of())
+  );
 
-  @Test
-  void addAreaStopToVertex() {
-    var graph = new Graph();
+  @TestFactory
+  Stream<DynamicTest> mapAreaStopsInVertex() {
+    return testCases
+      .stream()
+      .map(tc ->
+        dynamicTest(
+          tc.toString(),
+          () -> {
+            var graph = new Graph();
 
-    var berlinCenterA = StreetModelForTest.intersectionVertex(BERLIN);
-    var berlinCenterB = StreetModelForTest.intersectionVertex(BERLIN);
+            var fromVertex = StreetModelForTest.intersectionVertex(tc.coordinate);
+            var toVertex = StreetModelForTest.intersectionVertex(tc.coordinate);
 
-    var edge = StreetModelForTest.streetEdge(berlinCenterA, berlinCenterB);
-    edge.setPermission(StreetTraversalPermission.ALL);
-    berlinCenterA.addOutgoing(edge);
+            var edge = StreetModelForTest.streetEdge(fromVertex, toVertex);
+            edge.setPermission(tc.permission);
+            fromVertex.addOutgoing(edge);
 
-    graph.addVertex(berlinCenterA);
-    assertTrue(berlinCenterA.areaStops().isEmpty());
+            graph.addVertex(fromVertex);
+            assertTrue(fromVertex.areaStops().isEmpty());
 
-    var mapper = new AreaStopsToVerticesMapper(graph, TRANSIT_MODEL);
+            var mapper = new AreaStopsToVerticesMapper(graph, TRANSIT_MODEL);
 
-    mapper.buildGraph();
+            mapper.buildGraph();
 
-    assertEquals(Set.of(BERLIN_AREA_STOP), berlinCenterA.areaStops());
+            assertEquals(tc.expectedAreaStops, fromVertex.areaStops());
+          }
+        )
+      );
   }
 
-  @Test
-  void dontAddOutsideOfGeometry() {
-    var graph = new Graph();
-
-    var hamburgA = StreetModelForTest.intersectionVertex(HAMBURG);
-    var hamburgB = StreetModelForTest.intersectionVertex(HAMBURG);
-
-    var edge = StreetModelForTest.streetEdge(hamburgA, hamburgB);
-    edge.setPermission(StreetTraversalPermission.ALL);
-    hamburgA.addOutgoing(edge);
-
-    graph.addVertex(hamburgA);
-    assertTrue(hamburgA.areaStops().isEmpty());
-
-    var mapper = new AreaStopsToVerticesMapper(graph, TRANSIT_MODEL);
-
-    mapper.buildGraph();
-
-    assertEquals(Set.of(), hamburgA.areaStops());
-  }
-
-  @Test
-  void dontAddIfNotEligibleForCarPickup() {
-    var graph = new Graph();
-
-    var berlinCenterA = StreetModelForTest.intersectionVertex(BERLIN);
-    var berlinCenterB = StreetModelForTest.intersectionVertex(BERLIN);
-
-    var edge = StreetModelForTest.streetEdge(berlinCenterA, berlinCenterB);
-    edge.setPermission(StreetTraversalPermission.PEDESTRIAN);
-    berlinCenterA.addOutgoing(edge);
-
-    graph.addVertex(berlinCenterA);
-    assertTrue(berlinCenterA.areaStops().isEmpty());
-
-    var mapper = new AreaStopsToVerticesMapper(graph, TRANSIT_MODEL);
-
-    mapper.buildGraph();
-
-    assertEquals(Set.of(), berlinCenterA.areaStops());
-  }
+  private record TestCase(
+    Coordinate coordinate,
+    StreetTraversalPermission permission,
+    Set<AreaStop> expectedAreaStops
+  ) {}
 }
