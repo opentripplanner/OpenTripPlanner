f283e8ae43e5063515ed1b8911ad90b7371aa614
Vincent Paturet <vincent.paturet@entur.org>
2023-07-20 16:28:18 +0200

    Validate missing intermediate location.

--- src/ext/java/org/opentripplanner/ext/transmodelapi/mapping/ViaLocationMapper.java
+++ src/ext/java/org/opentripplanner/ext/transmodelapi/mapping/ViaLocationMapper.java
@@ -1,17 +1,33 @@
 package org.opentripplanner.ext.transmodelapi.mapping;
 
+import static org.opentripplanner.routing.api.response.InputField.INTERMEDIATE_PLACE;
+
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 import org.opentripplanner.routing.api.request.ViaLocation;
+import org.opentripplanner.routing.api.response.RoutingError;
+import org.opentripplanner.routing.api.response.RoutingErrorCode;
+import org.opentripplanner.routing.error.RoutingValidationException;
 
 class ViaLocationMapper {
 
   static ViaLocation mapViaLocation(Map<String, Object> viaLocation) {
+    validateViaLocation(viaLocation);
+
     return new ViaLocation(
       GenericLocationMapper.toGenericLocation(viaLocation),
       false,
       (Duration) viaLocation.get("minSlack"),
       (Duration) viaLocation.get("maxSlack")
     );
+  }
+
+  private static void validateViaLocation(Map<String, Object> viaLocation) {
+    if (viaLocation.get("place") == null && viaLocation.get("coordinates") == null) {
+      throw new RoutingValidationException(
+        List.of(new RoutingError(RoutingErrorCode.LOCATION_NOT_FOUND, INTERMEDIATE_PLACE))
+      );
+    }
   }
 }
