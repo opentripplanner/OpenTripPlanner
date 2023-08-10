9c073741bee9d92cabbcd88e90e20014a90b0f31
Viljami N <nurmAV@users.noreply.github.com>
2023-08-10 14:43:19 +0300

    Apply feedback from review
    Co-authored-by: Leonard Ehrenfried <mail@leonard.io>

--- src/ext/java/org/opentripplanner/ext/fares/impl/HSLFareServiceImpl.java
+++ src/ext/java/org/opentripplanner/ext/fares/impl/HSLFareServiceImpl.java
@@ -62,7 +62,7 @@ public class HSLFareServiceImpl extends DefaultFareService {
       .stream()
       .map(leg -> leg.getAgency().getId().getFeedId())
       .collect(Collectors.toSet());
-    if (!Sets.difference(legFeedIds, fareRuleFeedIds).isEmpty()) return Optional.ofNullable(null);
+    if (!Sets.difference(legFeedIds, fareRuleFeedIds).isEmpty()) return Optional.empty();
 
     for (Leg leg : legs) {
       lastRideStartTime = leg.getStartTime();
