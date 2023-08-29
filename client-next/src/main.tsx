f73f819e3c0e59a9402e79f2dc1c35fdffe12220
Tom Erik Støwer <testower@gmail.com>
2023-08-29 09:27:27 +0200

    Initial commit of next debug client

---
+++ client-next/src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>OTP debug client (next)</h1>
+  </React.StrictMode>,
+)
