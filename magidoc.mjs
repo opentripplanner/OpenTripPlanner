0ba31e57a9764b9531ac4180e983b2dbd5a30cb6
Leonard Ehrenfried <mail@leonard.io>
2023-08-31 10:03:18 +0200

    Change path of schema file

--- magidoc.mjs
+++ magidoc.mjs
@@ -1,7 +1,7 @@
 export default {
   introspection: {
     type: 'sdl',
-    paths: ['src/ext/resources/gtfsgraphqlapi/schema.graphqls'],
+    paths: ['src/main/resources/gtfsgraphqlapi/schema.graphqls'],
   },
   website: {
     template: 'carbon-multi-page',
