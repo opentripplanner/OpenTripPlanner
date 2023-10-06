839c9f9e57b4ad9759c021a0c080c5dd34acb8f0
Thomas Gran <t2gran@gmail.com>
2023-10-06 16:37:57 +0200

    refactor: Clean code

--- src/main/java/org/opentripplanner/routing/algorithm/transferoptimization/model/OptimizeTransfersFilterChain.java
+++ src/main/java/org/opentripplanner/routing/algorithm/transferoptimization/model/OptimizeTransfersFilterChain.java
@@ -2,7 +2,16 @@ package org.opentripplanner.routing.algorithm.transferoptimization.model;
 
 import java.util.Set;
 
+/**
+ * TODO PT - This need JavaDoc
+ */
 public interface OptimizeTransfersFilterChain<T> {
+  /**
+   * TODO PT - This need JavaDoc
+   */
   Set<T> filter(Set<T> elements);
+  /**
+   * TODO PT - This need JavaDoc
+   */
   Set<T> finalizeFilter(Set<T> elements);
 }
