7c06ca805b66d283aa1ab67b3aaf3fff9a14d9e5
eibakke <eivind.morris.bakke@entur.org>
2023-10-17 20:42:38 +0200

    Makes use of the ItinerarySortKey interface in the SortingFilter and SortOrderComparator for sorting instead of the Itinerary directly.

--- src/main/java/org/opentripplanner/model/plan/ItinerarySortKey.java
+++ src/main/java/org/opentripplanner/model/plan/ItinerarySortKey.java
@@ -1,7 +1,14 @@
 package org.opentripplanner.model.plan;
 
 import java.time.ZonedDateTime;
+import org.opentripplanner.routing.algorithm.filterchain.comparator.SortOrderComparator;
+import org.opentripplanner.routing.algorithm.filterchain.filter.SortingFilter;
 
+/**
+ * This interface is used to sort itineraries and other instances that we might want to sort among
+ * itineraries. It is used in the {@link SortingFilter} as defined by the
+ * {@link SortOrderComparator}.
+ */
 public interface ItinerarySortKey {
   ZonedDateTime startTime();
   ZonedDateTime endTime();
