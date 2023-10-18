16ab72f90eff4fdb2369d050eb950ca8a8494bc5
Eivind Morris Bakke <eivind.morris.bakke@entur.org>
2023-10-18 13:46:56 +0200

    Apply suggestions from code review
    Co-authored-by: Thomas Gran <t2gran@gmail.com>

--- src/main/java/org/opentripplanner/model/plan/ItinerarySortKey.java
+++ src/main/java/org/opentripplanner/model/plan/ItinerarySortKey.java
@@ -8,6 +8,9 @@ import org.opentripplanner.routing.algorithm.filterchain.filter.SortingFilter;
  * This interface is used to sort itineraries and other instances that we might want to sort among
  * itineraries. It is used in the {@link SortingFilter} as defined by the
  * {@link SortOrderComparator}.
+    <p>
+    The methods in this interface are NOT documented here, but in the Itinerary class. To keep it simple, this
+    interface should be kept in sync with method names in the itinerary. 
  */
 public interface ItinerarySortKey {
   ZonedDateTime startTime();
