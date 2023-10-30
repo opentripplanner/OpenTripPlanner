997f93217ab7c4c749e5f8915325fec3df9e061d
Tom Erik Støwer <testower@gmail.com>
2023-10-30 13:38:47 +0100

    fix useCallback deps

--- client-next/src/components/SearchBar/TransitModeSelect.tsx
+++ client-next/src/components/SearchBar/TransitModeSelect.tsx
@@ -43,7 +43,7 @@ export function TransitModeSelect({
         });
       }
     },
-    [tripQueryVariables.modes, setTripQueryVariables],
+    [tripQueryVariables, setTripQueryVariables],
   );
 
   return (
