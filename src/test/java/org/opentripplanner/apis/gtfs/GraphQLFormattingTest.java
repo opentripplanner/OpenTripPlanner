125a560cbd018b295509973be5be2b03a8235e7d
Joel Lappalainen <lappalj8@gmail.com>
2023-11-06 14:40:10 +0200

    Add test for applying formatting to GTFS API schema

---
+++ src/test/java/org/opentripplanner/apis/gtfs/GraphQLFormattingTest.java
@@ -0,0 +1,24 @@
+package org.opentripplanner.apis.gtfs;
+
+import static org.opentripplanner.framework.io.FileUtils.assertFileEquals;
+import static org.opentripplanner.framework.io.FileUtils.readFile;
+import static org.opentripplanner.framework.io.FileUtils.writeFile;
+
+import graphql.schema.idl.SchemaPrinter;
+import java.io.File;
+import org.junit.jupiter.api.Test;
+
+public class GraphQLFormattingTest {
+
+  public static final File SCHEMA_FILE = new File(
+    "src/main/resources/org/opentripplanner/apis/gtfs/schema.graphqls"
+  );
+
+  @Test
+  public void format() {
+    String original = readFile(SCHEMA_FILE);
+    var schema = GtfsGraphQLIndex.buildSchema();
+    writeFile(SCHEMA_FILE, new SchemaPrinter().print(schema));
+    assertFileEquals(original, SCHEMA_FILE);
+  }
+}
