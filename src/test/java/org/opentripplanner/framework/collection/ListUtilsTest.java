da96ed617e68ba701c68b8b0551ac67591ef44f4
Leonard Ehrenfried <mail@leonard.io>
2023-11-07 09:30:42 +0100

    Make test clearer

--- src/test/java/org/opentripplanner/framework/collection/ListUtilsTest.java
+++ src/test/java/org/opentripplanner/framework/collection/ListUtilsTest.java
@@ -20,12 +20,21 @@ class ListUtilsTest {
   @Test
   void distinctByKey() {
     var first = new Wrapper(10, makeHello());
-    var last = new Wrapper(20, "HI");
-    var duplicates = List.of(first, new Wrapper(20, makeHello()), last);
+    var second = new Wrapper(20, makeHello());
+    var third = new Wrapper(20, "HI");
 
+    // the first and second elements have equal values for the "string" property but different
+    // values for the "i" property
+    var duplicates = List.of(first, second, third);
+
+    // with a normal stream().distinct() or Set.copyOf no item would be removed but when
+    // we tell the deduplication logic to only look at a single property those with equal
+    // "string" values are de-duplicated
     var deduplicated = ListUtils.distinctByKey(duplicates, w -> w.string);
 
-    assertEquals(List.of(first, last), deduplicated);
+    // as a result the second element is removed from the result as its "string" value is
+    // equal to the first element's
+    assertEquals(List.of(first, third), deduplicated);
   }
 
   private record Wrapper(int i, String string) {}
