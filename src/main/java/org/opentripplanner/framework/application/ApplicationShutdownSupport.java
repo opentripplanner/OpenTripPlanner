64866231678ef8d9b569f317acfb2ad57f551a17
Johan Torin <johan.torin@adesso-sweden.se>
2023-11-08 10:06:12 +0100

    Don't execute teardown logic twice on shutdown.
    At least in the case of the AzureSiriUpdater this leads to errors the second time.
    
    * Provide an alternative API for adding system shutdown hooks that takes a Runnable directly.
    * State the shutdown logic directly to the shutdown hook, remove the teardown method.
    * Handle the execute-hook-directly-if-shutting-down logic internally in ApplicationShutdownSupport.

--- src/main/java/org/opentripplanner/framework/application/ApplicationShutdownSupport.java
+++ src/main/java/org/opentripplanner/framework/application/ApplicationShutdownSupport.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.framework.application;
 
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,5 +31,36 @@ public final class ApplicationShutdownSupport {
       );
       return false;
     }
+  }
+
+  /**
+   * Attempt to add a shutdown hook. If the application is already shutting down, the shutdown hook
+   * will be executed immediately.
+   *
+   * @param hookName the name of the thread
+   * @param shutdownHook the payload to be executed in the thread
+   * @return an Optional possibly containing the created thread, needed to un-schedule the shutdown hook
+   */
+  public static Optional<Thread> addShutdownHook(String hookName, Runnable shutdownHook) {
+    final Thread shutdownThread = new Thread(shutdownHook, hookName);
+    try {
+      LOG.info("Adding shutdown hook '{}'.", hookName);
+      Runtime.getRuntime().addShutdownHook(shutdownThread);
+      return Optional.of(shutdownThread);
+    } catch (IllegalStateException ignore) {
+      LOG.info("OTP is already shutting down, running shutdown hook '{}' immediately.", hookName);
+      shutdownThread.start();
+    }
+    return Optional.empty();
+  }
+
+  /**
+   * Remove a previously scheduled shutdown hook.
+   *
+   * @param shutdownThread an Optional possibly containing a thread
+   */
+  public static void removeShutdownHook(Thread shutdownThread) {
+    LOG.info("Removing shutdown hook '{}'.", shutdownThread.getName());
+    Runtime.getRuntime().removeShutdownHook(shutdownThread);
   }
 }
