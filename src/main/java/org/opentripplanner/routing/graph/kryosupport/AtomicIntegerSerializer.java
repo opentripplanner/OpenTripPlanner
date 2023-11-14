b072de89a554aece8b5c1600283e033138b7dc06
Leonard Ehrenfried <mail@leonard.io>
2023-11-14 17:26:29 +0100

    Add documentation

--- src/main/java/org/opentripplanner/routing/graph/kryosupport/AtomicIntegerSerializer.java
+++ src/main/java/org/opentripplanner/routing/graph/kryosupport/AtomicIntegerSerializer.java
@@ -6,6 +6,11 @@ import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * A custom serializer for an {@link AtomicInteger}.
+ * <p>
+ * Required for writing the stop index counter to the graph, so we later can create new stops.
+ */
 public class AtomicIntegerSerializer extends Serializer<AtomicInteger> {
   @Override
   public void write(Kryo kryo, Output output, AtomicInteger obj) {
