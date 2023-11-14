8e2e2309fa0a3c275f900efd21dc554ada37b2bf
Leonard Ehrenfried <mail@leonard.io>
2023-11-14 17:48:25 +0100

    Use input/output instead of Kryo

--- src/main/java/org/opentripplanner/routing/graph/kryosupport/AtomicIntegerSerializer.java
+++ src/main/java/org/opentripplanner/routing/graph/kryosupport/AtomicIntegerSerializer.java
@@ -9,17 +9,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * A custom serializer for an {@link AtomicInteger}.
  * <p>
- * Required for writing the stop index counter to the graph, so we later can create new stops.
+ * Required for writing the stop index counter to the graph, so we can create new stops later.
  */
 public class AtomicIntegerSerializer extends Serializer<AtomicInteger> {
+
   @Override
   public void write(Kryo kryo, Output output, AtomicInteger obj) {
-    kryo.writeObject(output, obj.get());
+    output.writeInt(obj.intValue());
   }
 
   @Override
   public AtomicInteger read(Kryo kryo, Input input, Class<? extends AtomicInteger> type) {
-    var value = kryo.readObject(input, Integer.class);
+    var value = input.readInt();
     return new AtomicInteger(value);
   }
 }
