6461290b36c10c5d8f15070d91debbcc8c9124fc
Thomas Gran <t2gran@gmail.com>
2023-11-26 18:58:46 +0100

    feature: Add CharacterEscapeFormatter to escape a special character.

---
+++ src/test/java/org/opentripplanner/framework/text/CharacterEscapeFormatterTest.java
@@ -0,0 +1,20 @@
+package org.opentripplanner.framework.text;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+class CharacterEscapeFormatterTest {
+  @Test
+  public void encodeDecode() {
+    var subject = new CharacterEscapeFormatter('\\', ';', '|');
+
+    var original = "This;is;a:text: ;\\;, \\; ;\\ |\\|, \\| |\\";
+    var escapedText = subject.encode(original);
+    var result = subject.decode(escapedText);
+
+    assertEquals(original, result);
+    assertFalse(escapedText.contains(";"), escapedText);
+  }
+}
