1abed1191c2df7a747ef21cd3b669c14d54c3011
Thomas Gran <t2gran@gmail.com>
2023-12-18 13:35:52 +0100

    refactor: Cleanup PathParetoSetComparatorsTest.
    Unused configurations are not tested - removed later.

---
+++ src/main/java/org/opentripplanner/raptor/rangeraptor/internalapi/ParetoSetTime.java
@@ -0,0 +1,20 @@
+package org.opentripplanner.raptor.rangeraptor.internalapi;
+
+/**
+ * These are the different time configurations Raptor support. Each configuration will
+ * be used to change the pareto-function. Note! There is a small difference betw */
+public enum ParetoSetTime {
+  /**
+   * Uses iteration-departure-time and arrival-time as criteria in pareto function. Note!
+   * iteration-departure-time is slightly different from the more precise departure-time.
+   */
+  USE_TIMETABLE,
+  /**
+   * Uses arrival-time as criteria in pareto function.
+   */
+  USE_ARRIVAL_TIME,
+  /**
+   * Uses departure-time as criteria in pareto function.
+   */
+  USE_DEPARTURE_TIME,
+}
