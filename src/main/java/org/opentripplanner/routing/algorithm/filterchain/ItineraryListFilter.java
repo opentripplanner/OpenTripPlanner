15ed36cb487c771710ccd9b7fcae4ed847485a04
Thomas Gran <t2gran@gmail.com>
2024-01-05 14:25:45 +0100

    doc: Fix JavaDoc in ItineraryListFilter

--- src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryListFilter.java
+++ src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryListFilter.java
@@ -8,11 +8,11 @@ import org.opentripplanner.model.plan.Itinerary;
  * List. It should treat the list as immutable. Do not change the list passed into the filter,
  * instead　make a copy, change it and return the copy. It is allowed to return the list unchanged.
  * <p>
- * A filter should do only one thing! For example do not change the itineraries and delete elements
+ * A filter should do only one thing! For example, do not change the itineraries and delete elements
  * in the same filter. Instead, create two filters and insert them after each other in the filter
  * chain.
  * <p>
- * This allows decoration of each filter and make it easier to reuse logic. Like the {@link
+ * This allows decoration of each filter and makes it easier to reuse logic. Like the {@link
  * org.opentripplanner.routing.algorithm.filterchain.deletionflagger.MaxLimitFilter} is reused in
  * several places.
  */
@@ -21,7 +21,7 @@ public interface ItineraryListFilter {
    * Process the given itineraries returning the result.
    * <p>
    * This function should not change the List instance passed in, but may change the elements. It
-   * must return a List with all the elements passed in (and possibly new elements). Note! that the
+   * must return a List with all the elements passed in (and possibly new elements). Note! The
    * list passed into the filter might be immutable.
    * <p>
    * This can be achieved using streams. Example:
