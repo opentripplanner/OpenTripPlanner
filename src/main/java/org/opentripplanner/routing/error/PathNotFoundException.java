8a8856b5b130e478b8d1ad89daa9ed24ed24c44c
Leonard Ehrenfried <mail@leonard.io>
2024-01-05 23:09:02 +0100

    Update src/main/java/org/opentripplanner/routing/error/PathNotFoundException.java
    Co-authored-by: Thomas Gran <t2gran@gmail.com>

--- src/main/java/org/opentripplanner/routing/error/PathNotFoundException.java
+++ src/main/java/org/opentripplanner/routing/error/PathNotFoundException.java
@@ -6,6 +6,5 @@ import org.opentripplanner.ext.restapi.resources.PlannerResource;
  * Indicates that the call to org.opentripplanner.routing.services.PathService returned either null
  * or ZERO paths.
  *
- * @see PlannerResource for where this is (locally) thrown.
  */
 public class PathNotFoundException extends RuntimeException {}
