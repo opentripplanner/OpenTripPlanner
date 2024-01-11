fdb900f1b38709ead9df393b0e357ad441f54d38
Leonard Ehrenfried <mail@leonard.io>
2024-01-11 11:10:18 +0100

    Add combinations to Portland smoke test

--- src/test/java/org/opentripplanner/smoketest/PortlandSmokeTest.java
+++ src/test/java/org/opentripplanner/smoketest/PortlandSmokeTest.java
@@ -1,9 +1,12 @@
 package org.opentripplanner.smoketest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.opentripplanner.client.model.RequestMode.SCOOTER_RENT;
 import static org.opentripplanner.client.model.RequestMode.TRAM;
+import static org.opentripplanner.client.model.RequestMode.TRANSIT;
 import static org.opentripplanner.client.model.RequestMode.WALK;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Disabled;
@@ -11,19 +14,22 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.opentripplanner.client.model.Coordinate;
+import org.opentripplanner.client.parameters.TripPlanParameters;
+import org.opentripplanner.smoketest.util.RequestCombinationsBuilder;
 import org.opentripplanner.smoketest.util.SmokeTestRequest;
 
 @Tag("smoke-test")
 @Tag("portland")
 public class PortlandSmokeTest {
 
-  Coordinate cennentenial = new Coordinate(45.504602, -122.4968719);
-  Coordinate buckman = new Coordinate(45.51720, -122.652289867);
-  Coordinate hazelwood = new Coordinate(45.52463, -122.5583);
-  Coordinate piedmont = new Coordinate(45.5746, -122.6697);
-  Coordinate mountTaborPark = new Coordinate(45.511399, -122.594203);
+  final static Coordinate cennentenial = new Coordinate(45.504602, -122.4968719);
+  final static Coordinate buckman = new Coordinate(45.51720, -122.652289867);
+  final static Coordinate hazelwood = new Coordinate(45.52463, -122.5583);
+  final static Coordinate piedmont = new Coordinate(45.5746, -122.6697);
+  final static Coordinate mountTaborPark = new Coordinate(45.511399, -122.594203);
 
   @Test
   public void railTrip() {
@@ -37,8 +43,26 @@ public class PortlandSmokeTest {
     SmokeTest.assertThatAllTransitLegsHaveFareProducts(plan);
   }
 
+  static List<TripPlanParameters> buildCombinations() {
+    return new RequestCombinationsBuilder()
+      .withLocations(cennentenial, buckman, hazelwood, piedmont, mountTaborPark)
+      .withModes(TRANSIT, WALK)
+      .withTime(SmokeTest.weekdayAtNoon())
+      .includeWheelchair()
+      .includeArriveBy()
+      .build();
+  }
+
+  @ParameterizedTest
+  @MethodSource("buildCombinations")
+  public void accessibleRouting(TripPlanParameters params) throws IOException {
+    var tripPlan = SmokeTest.API_CLIENT.plan(params);
+    assertFalse(tripPlan.transitItineraries().isEmpty());
+    SmokeTest.assertThatAllTransitLegsHaveFareProducts(tripPlan);
+  }
+
   @Nested
-  @Disabled
+  @Disabled("Disabled because it seems that the rental services have closed for the winter")
   class GeofencingZones {
 
     /**
