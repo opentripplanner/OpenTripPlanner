ded7050e670e3e3b5268cc81e0e489415c2a91c2
Leonard Ehrenfried <mail@leonard.io>
2024-01-17 22:19:17 +0100

    Reuse helper class

--- src/test/java/org/opentripplanner/test/support/HttpForTest.java
+++ src/test/java/org/opentripplanner/test/support/HttpForTest.java
@@ -7,12 +7,16 @@ import org.glassfish.jersey.server.ContainerRequest;
 
 public class HttpForTest {
 
-  public static ContainerRequest containerRequest() {
+  public static ContainerRequest containerRequest(String method) {
     try {
       URI uri = new URI("https://localhost:8080");
-      return new ContainerRequest(uri, uri, "GET", null, new MapPropertiesDelegate(), null);
+      return new ContainerRequest(uri, uri, method, null, new MapPropertiesDelegate(), null);
     } catch (URISyntaxException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public static ContainerRequest containerRequest() {
+    return containerRequest("GET");
   }
 }
