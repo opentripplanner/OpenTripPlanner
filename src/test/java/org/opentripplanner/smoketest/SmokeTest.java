e704e9a36ce65581597f404123917cf406e3dd54
Leonard Ehrenfried <mail@leonard.io>
2024-01-18 19:42:37 +0100

    Run tests on Friday

--- src/test/java/org/opentripplanner/smoketest/SmokeTest.java
+++ src/test/java/org/opentripplanner/smoketest/SmokeTest.java
@@ -49,7 +49,7 @@ public class SmokeTest {
    */
   public static LocalDateTime weekdayAtNoon() {
     var today = LocalDate.now();
-    return today.with(TemporalAdjusters.next(DayOfWeek.THURSDAY)).atTime(LocalTime.of(12, 0));
+    return today.with(TemporalAdjusters.next(DayOfWeek.FRIDAY)).atTime(LocalTime.of(12, 0));
   }
 
   public static void assertThatThereAreVehicleRentalStations() {
