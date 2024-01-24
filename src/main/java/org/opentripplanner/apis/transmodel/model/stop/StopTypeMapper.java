264ab35db7ccb53db042ea0256f1f2f09a6f73bd
eibakke <eivind.morris.bakke@entur.org>
2024-01-24 16:11:57 +0100

    Adds documentation to the new enums and mapper.

--- src/main/java/org/opentripplanner/apis/transmodel/model/stop/StopTypeMapper.java
+++ src/main/java/org/opentripplanner/apis/transmodel/model/stop/StopTypeMapper.java
@@ -3,13 +3,24 @@ package org.opentripplanner.apis.transmodel.model.stop;
 import graphql.schema.GraphQLEnumType;
 import org.opentripplanner.transit.model.site.StopType;
 
+/**
+ * Maps the StopType enum to a GraphQL enum.
+ */
 public class StopTypeMapper {
 
   public static final GraphQLEnumType STOP_TYPE = GraphQLEnumType
     .newEnum()
     .name("StopType")
-    .value("regular", StopType.REGULAR)
-    .value("flexible_area", StopType.FLEXIBLE_AREA)
-    .value("flexible_group", StopType.FLEXIBLE_GROUP)
+    .value("regular", StopType.REGULAR, "A regular stop defined geographically as a point.")
+    .value(
+      "flexible_area",
+      StopType.FLEXIBLE_AREA,
+      "Boarding and alighting is allowed anywhere within the geographic area of this stop."
+    )
+    .value(
+      "flexible_group",
+      StopType.FLEXIBLE_GROUP,
+      "A stop that consists of multiple other stops, area or regular."
+    )
     .build();
 }
