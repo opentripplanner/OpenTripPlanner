4e92d8343459e5c67d32c0c7c6c7d684058c3d2d
Joel Lappalainen <lappalj8@gmail.com>
2024-02-08 15:21:57 +0200

    Add tests for service

---
+++ src/test/java/org/opentripplanner/street/service/DefaultStreetLimitationParametersServiceTest.java
@@ -0,0 +1,25 @@
+package org.opentripplanner.street.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.opentripplanner.street.model.StreetLimitationParameters;
+
+public class DefaultStreetLimitationParametersServiceTest {
+
+  @Test
+  public void getMaxCarSpeed() {
+    var maxSpeed = 25f;
+    var model = new StreetLimitationParameters();
+    model.initMaxCarSpeed(maxSpeed);
+    var service = new DefaultStreetLimitationParametersService(model);
+    assertEquals(maxSpeed, service.getMaxCarSpeed());
+  }
+
+  @Test
+  public void getDefaultMaxCarSpeed() {
+    var model = new StreetLimitationParameters();
+    var service = new DefaultStreetLimitationParametersService(model);
+    assertEquals(40f, service.getMaxCarSpeed());
+  }
+}
