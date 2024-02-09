b7d037ee5c6caf33ab61b2c9cd6d4d928b0382ff
Eivind Morris Bakke <eivind.morris.bakke@entur.org>
2024-02-09 15:28:38 +0100

    Set defaults of the modes WALK, even if one and not the others are set (#5675)
    * Sets modes only if they are set in the request.
    
    Setting them to null overrides the default of WALK, which leads to no results.
    
    * Adds comment to the RequestModesMapper explaining defaults.
    
    * Adds unittest for RequestModesMapper and removes an unneeded unchecked warning suppression.
    
    * Makes keys constants in RequestModesMapper and changes map initialization in RequestModesMapperTest.
    
    * Makes use of Map.of everywhere in RequestModesMapperTest.

--- src/main/java/org/opentripplanner/apis/transmodel/mapping/RequestModesMapper.java
+++ src/main/java/org/opentripplanner/apis/transmodel/mapping/RequestModesMapper.java
@@ -7,23 +7,30 @@ import org.opentripplanner.routing.api.request.StreetMode;
 
 class RequestModesMapper {
 
-  /**
-   * Maps a GraphQL Modes input type to a RequestModes.
-   *
-   * This only maps access, egress, direct & transfer.
-   * Transport modes are now part of filters.
-   * Only in case filters are not present we will use this mapping
-   */
-  @SuppressWarnings("unchecked")
-  static RequestModes mapRequestModes(Map<String, ?> modesInput) {
-    StreetMode accessMode = (StreetMode) modesInput.get("accessMode");
-    RequestModesBuilder mBuilder = RequestModes
-      .of()
-      .withAccessMode(accessMode)
-      .withEgressMode((StreetMode) modesInput.get("egressMode"))
-      .withDirectMode((StreetMode) modesInput.get("directMode"));
+  private static final String accessModeKey = "accessMode";
+  private static final String egressModeKey = "egressMode";
+  private static final String directModeKey = "directMode";
 
-    mBuilder.withTransferMode(accessMode == StreetMode.BIKE ? StreetMode.BIKE : StreetMode.WALK);
+  /**
+   * Maps GraphQL Modes input type to RequestModes.
+   * <p>
+   * This only maps access, egress, direct & transfer modes. Transport modes are set using filters.
+   * Default modes are WALK for access, egress, direct & transfer.
+   */
+  static RequestModes mapRequestModes(Map<String, ?> modesInput) {
+    RequestModesBuilder mBuilder = RequestModes.of();
+
+    if (modesInput.containsKey(accessModeKey)) {
+      StreetMode accessMode = (StreetMode) modesInput.get(accessModeKey);
+      mBuilder.withAccessMode(accessMode);
+      mBuilder.withTransferMode(accessMode == StreetMode.BIKE ? StreetMode.BIKE : StreetMode.WALK);
+    }
+    if (modesInput.containsKey(egressModeKey)) {
+      mBuilder.withEgressMode((StreetMode) modesInput.get(egressModeKey));
+    }
+    if (modesInput.containsKey(directModeKey)) {
+      mBuilder.withDirectMode((StreetMode) modesInput.get(directModeKey));
+    }
 
     return mBuilder.build();
   }
