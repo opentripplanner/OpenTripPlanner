f41cc65831ea6e702a20c595397ff0532c80dc2a
Jim Martens <jma@hbt.de>
2024-02-23 14:28:45 +0100

    refactor: Remove obsolete import

--- src/test/java/org/opentripplanner/openstreetmap/tagmapping/HamburgMapperTest.java
+++ src/test/java/org/opentripplanner/openstreetmap/tagmapping/HamburgMapperTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.opentripplanner.openstreetmap.model.OSMWithTags;
 
