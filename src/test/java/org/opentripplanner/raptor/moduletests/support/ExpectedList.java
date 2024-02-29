49096b1a82f5f7b0ec1394a4ddc6fd9b71705835
Thomas Gran <t2gran@gmail.com>
2024-02-29 17:44:45 +0100

    fix: Include time-penalty in the startTime/endTime/duration in path criteria comparison

--- src/test/java/org/opentripplanner/raptor/moduletests/support/ExpectedList.java
+++ src/test/java/org/opentripplanner/raptor/moduletests/support/ExpectedList.java
@@ -22,20 +22,20 @@ public class ExpectedList {
     return range(0, n);
   }
 
-  public String get(int index) {
-    return items[index];
-  }
-
-  public String[] get(int... indexes) {
-    return Arrays.stream(indexes).mapToObj(i -> items[i]).toList().toArray(new String[0]);
-  }
-
   public String last() {
     return items[items.length - 1];
   }
 
   public String[] last(int n) {
     return range(items.length - n, items.length);
+  }
+
+  public String get(int index) {
+    return items[index];
+  }
+
+  public String[] get(int... indexes) {
+    return Arrays.stream(indexes).mapToObj(i -> items[i]).toList().toArray(new String[0]);
   }
 
   public String[] range(int startInclusive, int endExclusive) {
