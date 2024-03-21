36809bee6a8a3590c80ec662e6558c1a291fe51a
Joel Lappalainen <lappalj8@gmail.com>
2024-03-21 16:13:40 +0200

    Add tests for bike preferences

--- src/test/java/org/opentripplanner/apis/gtfs/mapping/RouteRequestMapperTest.java
+++ src/test/java/org/opentripplanner/apis/gtfs/mapping/RouteRequestMapperTest.java
@@ -20,16 +20,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Coordinate;
 import org.opentripplanner._support.time.ZoneIds;
 import org.opentripplanner.apis.gtfs.GraphQLRequestContext;
 import org.opentripplanner.apis.gtfs.TestRoutingService;
 import org.opentripplanner.ext.fares.impl.DefaultFareService;
+import org.opentripplanner.framework.model.Cost;
 import org.opentripplanner.model.plan.paging.cursor.PageCursor;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.api.request.preference.ItineraryFilterDebugProfile;
+import org.opentripplanner.routing.core.VehicleRoutingOptimizeType;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graphfinder.GraphFinder;
 import org.opentripplanner.service.realtimevehicles.internal.DefaultRealtimeVehicleService;
@@ -332,6 +335,256 @@ class RouteRequestMapperTest {
     assertEquals(keepOne, itinFilter.groupSimilarityKeepOne());
     assertEquals(keepThree, itinFilter.groupSimilarityKeepThree());
     assertEquals(multiplier, itinFilter.groupedOtherThanSameLegsMaxCostMultiplier());
+  }
+
+  @Test
+  void testBasicBikePreferences() {
+    var bicycleArgs = createArgsCopy();
+    var reluctance = 7.5;
+    var speed = 15d;
+    var boardCost = Cost.costOfSeconds(50);
+    bicycleArgs.put(
+      "preferences",
+      Map.ofEntries(
+        entry(
+          "street",
+          Map.ofEntries(
+            entry(
+              "bicycle",
+              Map.ofEntries(
+                entry("reluctance", reluctance),
+                entry("speed", speed),
+                entry("boardCost", boardCost)
+              )
+            )
+          )
+        )
+      )
+    );
+    var env = executionContext(bicycleArgs);
+    var routeRequest = RouteRequestMapper.toRouteRequest(env, context);
+    var bikePreferences = routeRequest.preferences().bike();
+    assertEquals(reluctance, bikePreferences.reluctance());
+    assertEquals(speed, bikePreferences.speed());
+    assertEquals(boardCost.toSeconds(), bikePreferences.boardCost());
+  }
+
+  @Test
+  void testBikeWalkPreferences() {
+    var bicycleArgs = createArgsCopy();
+    var walkSpeed = 7d;
+    var mountDismountTime = Duration.ofSeconds(23);
+    var mountDismountCost = Cost.costOfSeconds(35);
+    var walkReluctance = 6.3;
+    bicycleArgs.put(
+      "preferences",
+      Map.ofEntries(
+        entry(
+          "street",
+          Map.ofEntries(
+            entry(
+              "bicycle",
+              Map.ofEntries(
+                entry(
+                  "walk",
+                  Map.ofEntries(
+                    entry("speed", walkSpeed),
+                    entry("mountDismountTime", mountDismountTime),
+                    entry(
+                      "cost",
+                      Map.ofEntries(
+                        entry("mountDismountCost", mountDismountCost),
+                        entry("reluctance", walkReluctance)
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    );
+    var env = executionContext(bicycleArgs);
+    var routeRequest = RouteRequestMapper.toRouteRequest(env, context);
+    var bikeWalkingPreferences = routeRequest.preferences().bike().walking();
+    assertEquals(walkSpeed, bikeWalkingPreferences.speed());
+    assertEquals(mountDismountTime, bikeWalkingPreferences.mountDismountTime());
+    assertEquals(mountDismountCost, bikeWalkingPreferences.mountDismountCost());
+    assertEquals(walkReluctance, bikeWalkingPreferences.reluctance());
+  }
+
+  @Test
+  void testBikeTrianglePreferences() {
+    var bicycleArgs = createArgsCopy();
+    var bikeSafety = 0.3;
+    var bikeFlatness = 0.5;
+    var bikeTime = 0.2;
+    bicycleArgs.put(
+      "preferences",
+      Map.ofEntries(
+        entry(
+          "street",
+          Map.ofEntries(
+            entry(
+              "bicycle",
+              Map.ofEntries(
+                entry(
+                  "optimization",
+                  Map.ofEntries(
+                    entry(
+                      "triangle",
+                      Map.ofEntries(
+                        entry("safety", bikeSafety),
+                        entry("flatness", bikeFlatness),
+                        entry("time", bikeTime)
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    );
+    var env = executionContext(bicycleArgs);
+    var routeRequest = RouteRequestMapper.toRouteRequest(env, context);
+    var bikePreferences = routeRequest.preferences().bike();
+    assertEquals(VehicleRoutingOptimizeType.TRIANGLE, bikePreferences.optimizeType());
+    var bikeTrianglePreferences = bikePreferences.optimizeTriangle();
+    assertEquals(bikeSafety, bikeTrianglePreferences.safety());
+    assertEquals(bikeFlatness, bikeTrianglePreferences.slope());
+    assertEquals(bikeTime, bikeTrianglePreferences.time());
+  }
+
+  @Test
+  void testBikeOptimizationPreferences() {
+    var bicycleArgs = createArgsCopy();
+    bicycleArgs.put(
+      "preferences",
+      Map.ofEntries(
+        entry(
+          "street",
+          Map.ofEntries(
+            entry(
+              "bicycle",
+              Map.ofEntries(entry("optimization", Map.ofEntries(entry("type", "SAFEST_STREETS"))))
+            )
+          )
+        )
+      )
+    );
+    var env = executionContext(bicycleArgs);
+    var routeRequest = RouteRequestMapper.toRouteRequest(env, context);
+    var bikePreferences = routeRequest.preferences().bike();
+    assertEquals(VehicleRoutingOptimizeType.SAFEST_STREETS, bikePreferences.optimizeType());
+  }
+
+  @Test
+  void testBikeRentalPreferences() {
+    var bicycleArgs = createArgsCopy();
+    var allowed = Set.of("foo", "bar");
+    var banned = Set.of("not");
+    var allowKeeping = true;
+    var keepingCost = Cost.costOfSeconds(150);
+    bicycleArgs.put(
+      "preferences",
+      Map.ofEntries(
+        entry(
+          "street",
+          Map.ofEntries(
+            entry(
+              "bicycle",
+              Map.ofEntries(
+                entry(
+                  "rental",
+                  Map.ofEntries(
+                    entry("allowedNetworks", allowed.stream().toList()),
+                    entry("bannedNetworks", banned.stream().toList()),
+                    entry(
+                      "destinationBicyclePolicy",
+                      Map.ofEntries(
+                        entry("allowKeeping", allowKeeping),
+                        entry("keepingCost", keepingCost)
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    );
+    var env = executionContext(bicycleArgs);
+    var routeRequest = RouteRequestMapper.toRouteRequest(env, context);
+    var bikeRentalPreferences = routeRequest.preferences().bike().rental();
+    assertEquals(allowed, bikeRentalPreferences.allowedNetworks());
+    assertEquals(banned, bikeRentalPreferences.bannedNetworks());
+    assertEquals(allowKeeping, bikeRentalPreferences.allowArrivingInRentedVehicleAtDestination());
+    assertEquals(keepingCost, bikeRentalPreferences.arrivingInRentalVehicleAtDestinationCost());
+  }
+
+  @Test
+  void testBikeParkingPreferences() {
+    var bicycleArgs = createArgsCopy();
+    var unpreferredCost = Cost.costOfSeconds(150);
+    var notFilter = List.of("wheelbender");
+    var selectFilter = List.of("locker", "roof");
+    var unpreferred = List.of("bad");
+    var preferred = List.of("a", "b");
+    bicycleArgs.put(
+      "preferences",
+      Map.ofEntries(
+        entry(
+          "street",
+          Map.ofEntries(
+            entry(
+              "bicycle",
+              Map.ofEntries(
+                entry(
+                  "parking",
+                  Map.ofEntries(
+                    entry("unpreferredCost", unpreferredCost),
+                    entry(
+                      "filters",
+                      List.of(
+                        Map.ofEntries(
+                          entry("not", List.of(Map.of("tags", notFilter))),
+                          entry("select", List.of(Map.of("tags", selectFilter)))
+                        )
+                      )
+                    ),
+                    entry(
+                      "preferred",
+                      List.of(
+                        Map.ofEntries(
+                          entry("not", List.of(Map.of("tags", unpreferred))),
+                          entry("select", List.of(Map.of("tags", preferred)))
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    );
+    var env = executionContext(bicycleArgs);
+    var routeRequest = RouteRequestMapper.toRouteRequest(env, context);
+    var bikeParkingPreferences = routeRequest.preferences().bike().parking();
+    assertEquals(unpreferredCost, bikeParkingPreferences.unpreferredVehicleParkingTagCost());
+    assertEquals(
+      "VehicleParkingFilter{not: [tags=%s], select: [tags=%s]}".formatted(notFilter, selectFilter),
+      bikeParkingPreferences.filter().toString()
+    );
+    assertEquals(
+      "VehicleParkingFilter{not: [tags=%s], select: [tags=%s]}".formatted(unpreferred, preferred),
+      bikeParkingPreferences.preferred().toString()
+    );
   }
 
   private Map<String, Object> createArgsCopy() {
