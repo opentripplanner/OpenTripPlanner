95c4f6c27f26d0e11b925f35d2ec587544168fee
Leonard Ehrenfried <mail@leonard.io>
2024-03-26 09:57:07 +0100

    Add nicer toString

--- src/ext/java/org/opentripplanner/ext/vehicleparking/noi/NoiUpdater.java
+++ src/ext/java/org/opentripplanner/ext/vehicleparking/noi/NoiUpdater.java
@@ -3,6 +3,7 @@ package org.opentripplanner.ext.vehicleparking.noi;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.opentripplanner.framework.geometry.WgsCoordinate;
 import org.opentripplanner.framework.i18n.NonLocalizedString;
+import org.opentripplanner.framework.tostring.ToStringBuilder;
 import org.opentripplanner.routing.vehicle_parking.VehicleParking;
 import org.opentripplanner.routing.vehicle_parking.VehicleParkingSpaces;
 import org.opentripplanner.routing.vehicle_parking.VehicleParkingState;
@@ -15,12 +16,11 @@ import org.opentripplanner.updater.spi.GenericJsonDataSource;
 public class NoiUpdater extends GenericJsonDataSource<VehicleParking> {
 
   private static final String JSON_PARSE_PATH = "data/stations";
-
-  private final String feedId;
+  private final NoiUpdaterParameters params;
 
   public NoiUpdater(NoiUpdaterParameters parameters) {
     super(parameters.url().toString(), JSON_PARSE_PATH, parameters.httpHeaders());
-    this.feedId = parameters.feedId();
+    this.params = parameters;
   }
 
   @Override
@@ -38,14 +38,14 @@ public class NoiUpdater extends GenericJsonDataSource<VehicleParking> {
       throw new IllegalArgumentException("Unknown type '%s'".formatted(type));
     }
 
-    var vehicleParkId = new FeedScopedId(feedId, jsonNode.path("station_id").asText());
+    var vehicleParkId = new FeedScopedId(params.feedId(), jsonNode.path("station_id").asText());
     var name = new NonLocalizedString(jsonNode.path("name").asText());
     double lat = jsonNode.path("lat").asDouble();
     double lon = jsonNode.path("lon").asDouble();
     var coordinate = new WgsCoordinate(lat, lon);
     VehicleParking.VehicleParkingEntranceCreator entrance = builder ->
       builder
-        .entranceId(new FeedScopedId(feedId, vehicleParkId.getId() + "/entrance"))
+        .entranceId(new FeedScopedId(params.feedId(), vehicleParkId.getId() + "/entrance"))
         .coordinate(coordinate)
         .walkAccessible(true)
         .carAccessible(true);
@@ -65,5 +65,13 @@ public class NoiUpdater extends GenericJsonDataSource<VehicleParking> {
 
   private static VehicleParkingSpaces extractSpaces(JsonNode jsonNode, String free) {
     return VehicleParkingSpaces.builder().carSpaces(jsonNode.get(free).asInt()).build();
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder
+      .of(this.getClass())
+      .addStr("url", this.params.url().toString())
+      .toString();
   }
 }
