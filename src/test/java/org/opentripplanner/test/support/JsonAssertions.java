0cf310cbc1e91b8400b2b82b4a5a0ab3baf98371
Leonard Ehrenfried <mail@leonard.io>
2024-03-28 14:19:51 +0100

    Sort order for JSON files

--- src/test/java/org/opentripplanner/test/support/JsonAssertions.java
+++ src/test/java/org/opentripplanner/test/support/JsonAssertions.java
@@ -5,16 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.opentripplanner.standalone.config.framework.json.JsonSupport;
 
 public class JsonAssertions {
 
   private static final ObjectMapper MAPPER = new ObjectMapper();
-
-  static {
-    MAPPER.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
-  }
 
   /**
    * Take two JSON documents and reformat them before comparing {@code actual} with {@code expected}.
@@ -32,8 +27,17 @@ public class JsonAssertions {
    */
   public static void assertEqualJson(String expected, JsonNode actual) {
     try {
+      var actualNode = MAPPER.readTree(actual.toString());
       var exp = MAPPER.readTree(expected);
-      assertEquals(JsonSupport.prettyPrint(exp), JsonSupport.prettyPrint(actual));
+      assertEquals(
+        exp,
+        actualNode,
+        () ->
+          "Expected '%s' but actual was '%s'".formatted(
+              JsonSupport.prettyPrint(exp),
+              JsonSupport.prettyPrint(actualNode)
+            )
+      );
     } catch (JsonProcessingException e) {
       throw new RuntimeException(e);
     }
