4b6a27ea9f89ecefb5b6049de05688b40c81f961
Leonard Ehrenfried <mail@leonard.io>
2024-03-28 13:48:20 +0100

    Sort by keys before comparing

--- src/test/java/org/opentripplanner/test/support/JsonAssertions.java
+++ src/test/java/org/opentripplanner/test/support/JsonAssertions.java
@@ -5,11 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.opentripplanner.standalone.config.framework.json.JsonSupport;
 
 public class JsonAssertions {
 
   private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  static {
+    MAPPER.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+  }
 
   /**
    * Take two JSON documents and reformat them before comparing {@code actual} with {@code expected}.
