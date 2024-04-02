258398db202f97434662e5bf37fe82b18ac490b2
Leonard Ehrenfried <mail@leonard.io>
2024-04-02 10:57:07 +0200

    Remove import

--- src/test/java/org/opentripplanner/framework/lang/StringUtilsTest.java
+++ src/test/java/org/opentripplanner/framework/lang/StringUtilsTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.opentripplanner._support.geometry.Polygons;
 
 class StringUtilsTest {
 
