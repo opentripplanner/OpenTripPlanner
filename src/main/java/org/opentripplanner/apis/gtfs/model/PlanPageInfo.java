22a976301224299c0e1061775b6ec12ff78d192b
Joel Lappalainen <lappalj8@gmail.com>
2024-04-26 20:14:57 +0300

    Refactor PlanPageInfo to be a class

--- src/main/java/org/opentripplanner/apis/gtfs/model/PlanPageInfo.java
+++ src/main/java/org/opentripplanner/apis/gtfs/model/PlanPageInfo.java
@@ -2,11 +2,76 @@ package org.opentripplanner.apis.gtfs.model;
 
 import graphql.relay.ConnectionCursor;
 import java.time.Duration;
+import java.util.Objects;
 
-public record PlanPageInfo(
-  ConnectionCursor startCursor,
-  ConnectionCursor endCursor,
-  boolean hasPreviousPage,
-  boolean hasNextPage,
-  Duration searchWindowUsed
-) {}
+public class PlanPageInfo {
+
+  private final ConnectionCursor startCursor;
+  private final ConnectionCursor endCursor;
+  private final boolean hasPreviousPage;
+  private final boolean hasNextPage;
+  private final Duration searchWindowUsed;
+
+  public PlanPageInfo(
+    ConnectionCursor startCursor,
+    ConnectionCursor endCursor,
+    boolean hasPreviousPage,
+    boolean hasNextPage,
+    Duration searchWindowUsed
+  ) {
+    this.startCursor = startCursor;
+    this.endCursor = endCursor;
+    this.hasPreviousPage = hasPreviousPage;
+    this.hasNextPage = hasNextPage;
+    this.searchWindowUsed = searchWindowUsed;
+  }
+
+  public ConnectionCursor startCursor() {
+    return startCursor;
+  }
+
+  public ConnectionCursor endCursor() {
+    return endCursor;
+  }
+
+  public boolean hasPreviousPage() {
+    return hasPreviousPage;
+  }
+
+  public boolean hasNextPage() {
+    return hasNextPage;
+  }
+
+  public Duration searchWindowUsed() {
+    return searchWindowUsed;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(
+      startCursor.getValue(),
+      endCursor.getValue(),
+      hasPreviousPage,
+      hasNextPage,
+      searchWindowUsed
+    );
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PlanPageInfo that = (PlanPageInfo) o;
+    return (
+      Objects.equals(startCursor.getValue(), that.startCursor.getValue()) &&
+      Objects.equals(endCursor.getValue(), that.endCursor.getValue()) &&
+      Objects.equals(searchWindowUsed, that.searchWindowUsed) &&
+      hasPreviousPage == that.hasPreviousPage &&
+      hasNextPage == that.hasNextPage
+    );
+  }
+}
