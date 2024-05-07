305c6cea0431e00c8ea5c8b350193b62ee0a152e
Vincent Paturet <vincent.paturet@entur.org>
2024-05-07 14:36:28 +0200

    Add unit tests

--- src/test/java/org/opentripplanner/apis/transmodel/support/GqlUtilTest.java
+++ src/test/java/org/opentripplanner/apis/transmodel/support/GqlUtilTest.java
@@ -2,11 +2,14 @@ package org.opentripplanner.apis.transmodel.support;
 
 import static graphql.execution.ExecutionContextBuilder.newExecutionContextBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import graphql.ExecutionInput;
 import graphql.execution.ExecutionContext;
 import graphql.execution.ExecutionId;
+import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.DataFetchingEnvironmentImpl;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -14,6 +17,7 @@ import org.junit.jupiter.api.Test;
 public class GqlUtilTest {
 
   static final ExecutionContext executionContext;
+  private static final String TEST_ARGUMENT = "testArgument";
 
   static {
     ExecutionInput executionInput = ExecutionInput
@@ -27,6 +31,54 @@ public class GqlUtilTest {
         .executionInput(executionInput)
         .executionId(ExecutionId.from("GqlUtilTest"))
         .build();
+  }
+
+  @Test
+  void testGetPositiveNonNullIntegerArgumentWithStrictlyPositiveValue() {
+    var env = buildEnvWithTestValue(1);
+    assertEquals(1, GqlUtil.getPositiveNonNullIntegerArgument(env, TEST_ARGUMENT));
+  }
+
+  @Test
+  void testGetPositiveNonNullIntegerArgumentWithZeroValue() {
+    var env = buildEnvWithTestValue(0);
+    assertEquals(0, GqlUtil.getPositiveNonNullIntegerArgument(env, TEST_ARGUMENT));
+  }
+
+  @Test
+  void testGetPositiveNonNullIntegerArgumentWithNegativeValue() {
+    var env = buildEnvWithTestValue(-1);
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> GqlUtil.getPositiveNonNullIntegerArgument(env, TEST_ARGUMENT)
+    );
+  }
+
+  @Test
+  void testGetPositiveNonNullIntegerArgumentWithNullValue() {
+    var env = buildEnvWithTestValue(null);
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> GqlUtil.getPositiveNonNullIntegerArgument(env, TEST_ARGUMENT)
+    );
+  }
+
+  @Test
+  void testGetPositiveNonNullIntegerArgumentWithoutValue() {
+    var env = DataFetchingEnvironmentImpl.newDataFetchingEnvironment(executionContext).build();
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> GqlUtil.getPositiveNonNullIntegerArgument(env, TEST_ARGUMENT)
+    );
+  }
+
+  private static DataFetchingEnvironment buildEnvWithTestValue(Integer value) {
+    Map<String, Object> argsMap = new HashMap<>();
+    argsMap.put(TEST_ARGUMENT, value);
+    return DataFetchingEnvironmentImpl
+      .newDataFetchingEnvironment(executionContext)
+      .arguments(argsMap)
+      .build();
   }
 
   @Test
