0ba3e70e9b53ed99b882d2366eb6538892bd8a9e
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-06-06 15:22:47 -0700

    test: test if ConsolidatedStop decorator copies fares

--- src/ext-test/java/org/opentripplanner/ext/stopconsolidation/DecorateConsolidatedStopNamesTest.java
+++ src/ext-test/java/org/opentripplanner/ext/stopconsolidation/DecorateConsolidatedStopNamesTest.java
@@ -5,17 +5,33 @@ import static org.opentripplanner.ext.stopconsolidation.TestStopConsolidationMod
 import static org.opentripplanner.ext.stopconsolidation.TestStopConsolidationModel.STOP_D;
 import static org.opentripplanner.model.plan.PlanTestConstants.T11_05;
 import static org.opentripplanner.model.plan.PlanTestConstants.T11_12;
+import static org.opentripplanner.transit.model._data.TransitModelForTest.id;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.ext.stopconsolidation.internal.DefaultStopConsolidationRepository;
 import org.opentripplanner.ext.stopconsolidation.internal.DefaultStopConsolidationService;
 import org.opentripplanner.ext.stopconsolidation.model.ConsolidatedStopGroup;
+import org.opentripplanner.model.fare.FareProduct;
+import org.opentripplanner.model.fare.FareProductUse;
 import org.opentripplanner.model.plan.Place;
 import org.opentripplanner.model.plan.PlanTestConstants;
 import org.opentripplanner.model.plan.TestItineraryBuilder;
+import org.opentripplanner.transit.model.basic.Money;
 
 class DecorateConsolidatedStopNamesTest {
+
+  private static final FareProduct fp = new FareProduct(
+    id("fp"),
+    "fare product",
+    Money.euros(10.00f),
+    null,
+    null,
+    null
+  );
+  private static final List<FareProductUse> fpu = List.of(
+    new FareProductUse("c1a04702-1fb6-32d4-ba02-483bf68111ed", fp)
+  );
 
   @Test
   void changeNames() {
@@ -35,10 +51,15 @@ class DecorateConsolidatedStopNamesTest {
       .bus(1, T11_05, T11_12, PlanTestConstants.F)
       .build();
 
+    itinerary.getLegs().getFirst().setFareProducts(fpu);
+
     filter.decorate(itinerary);
 
     var updatedLeg = itinerary.getLegs().getFirst();
     assertEquals(STOP_C.getName(), updatedLeg.getFrom().name);
     assertEquals(STOP_D.getName(), updatedLeg.getTo().name);
+
+    // Check that the fares were carried over
+    assertEquals(fpu, updatedLeg.fareProducts());
   }
 }
