52c2c555b39b063440b78f59eee4d7182f628c0a
Thomas Gran <t2gran@gmail.com>
2024-06-19 17:09:36 +0200

    Apply code review feedback

--- src/main/java/org/opentripplanner/apis/transmodel/model/plan/TripPlanTimePenaltyDto.java
+++ src/main/java/org/opentripplanner/apis/transmodel/model/plan/TripPlanTimePenaltyDto.java
@@ -3,6 +3,7 @@ package org.opentripplanner.apis.transmodel.model.plan;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
+import javax.annotation.Nullable;
 import org.opentripplanner.framework.model.TimeAndCost;
 import org.opentripplanner.model.plan.Itinerary;
 
@@ -13,17 +14,16 @@ import org.opentripplanner.model.plan.Itinerary;
  */
 public record TripPlanTimePenaltyDto(String appliesTo, TimeAndCost penalty) {
   static List<TripPlanTimePenaltyDto> of(Itinerary itinerary) {
-    // This check for null to be robust - in case of a mistake in the future.
-    // The check is redundant on purpose.
-    if (itinerary == null) {
-      return List.of();
-    }
     return Stream
       .of(of("access", itinerary.getAccessPenalty()), of("egress", itinerary.getEgressPenalty()))
       .filter(Objects::nonNull)
       .toList();
   }
 
+   /**
+    * Package local to be unit-testable.
+    */
+  @Nullable
   static TripPlanTimePenaltyDto of(String appliedTo, TimeAndCost penalty) {
     return penalty == null || penalty.isZero()
       ? null
