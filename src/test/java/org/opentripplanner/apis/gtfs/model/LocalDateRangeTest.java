b617f95f7e3a525fd7299eff3dd433fbe28a934e
Leonard Ehrenfried <mail@leonard.io>
2024-07-08 11:18:50 +0200

    Rework test

--- src/test/java/org/opentripplanner/apis/gtfs/model/LocalDateRangeTest.java
+++ src/test/java/org/opentripplanner/apis/gtfs/model/LocalDateRangeTest.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.apis.gtfs.model;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,14 @@ class LocalDateRangeTest {
   private static final LocalDate DATE = LocalDate.parse("2024-06-01");
 
   @Test
-  void unlimited() {
+  void limited() {
     assertFalse(new LocalDateRange(DATE, DATE).unlimited());
+    assertFalse(new LocalDateRange(DATE, null).unlimited());
+    assertFalse(new LocalDateRange(null, DATE).unlimited());
   }
 
   @Test
-  void limited() {
-    assertFalse(new LocalDateRange(DATE, null).unlimited());
-    assertFalse(new LocalDateRange(null, DATE).unlimited());
+  void unlimited() {
+    assertTrue(new LocalDateRange(null, null).unlimited());
   }
 }
