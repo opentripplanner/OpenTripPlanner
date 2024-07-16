1238429266cf33bc079cbb6770f9e6f6e8ecd648
Leonard Ehrenfried <mail@leonard.io>
2024-07-16 15:19:03 +0200

    Add Javadoc

--- src/main/java/org/opentripplanner/netex/config/IgnorableFeature.java
+++ src/main/java/org/opentripplanner/netex/config/IgnorableFeature.java
@@ -1,5 +1,8 @@
 package org.opentripplanner.netex.config;
 
+/**
+ * Optional data that can be ignored during the NeTEx parsing process.
+ */
 public enum IgnorableFeature {
   FARE_FRAME,
   PARKING,
