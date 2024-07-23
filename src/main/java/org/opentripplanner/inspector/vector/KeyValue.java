1cfaa3800ddf777e47785329b27199cff3b8599a
Leonard Ehrenfried <mail@leonard.io>
2024-07-23 16:22:57 +0200

    Add Javadoc for KeyValue

--- src/main/java/org/opentripplanner/inspector/vector/KeyValue.java
+++ src/main/java/org/opentripplanner/inspector/vector/KeyValue.java
@@ -1,14 +1,30 @@
 package org.opentripplanner.inspector.vector;
 
+import jakarta.annotation.Nullable;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
 
+/**
+ * A key value pair that represents data being sent to the vector tile library for visualisation
+ * in a map (including popups).
+ * <p>
+ * The underlying format (and library) supports only a limited number of Java types and silently
+ * drops those that aren't supported: https://github.com/CI-CMG/mapbox-vector-tile/blob/master/src/main/java/edu/colorado/cires/cmg/mvt/encoding/MvtValue.java#L18-L40
+ * <p>
+ * For this reason this class also has static initializer that automatically converts common
+ * OTP classes into vector tile-compatible strings.
+ */
 public record KeyValue(String key, Object value) {
   public static KeyValue kv(String key, Object value) {
     return new KeyValue(key, value);
   }
-  public static KeyValue kv(String key, FeedScopedId value) {
+
+  /**
+   * A {@link FeedScopedId} is not a type that can be converted to a vector tile feature property
+   * value. Therefore, we convert it to a string after performing a null check.
+   */
+  public static KeyValue kv(String key, @Nullable FeedScopedId value) {
     if (value != null) {
       return new KeyValue(key, value.toString());
     } else {
