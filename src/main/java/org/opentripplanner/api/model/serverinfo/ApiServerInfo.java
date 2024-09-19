c61819a200ed8ee4cb6b9bee8cc70a1a786e178c
Leonard Ehrenfried <mail@leonard.io>
2024-09-19 23:23:14 +0200

    Return transit model timezone in server info

--- src/main/java/org/opentripplanner/api/model/serverinfo/ApiServerInfo.java
+++ src/main/java/org/opentripplanner/api/model/serverinfo/ApiServerInfo.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.api.model.serverinfo;
 
+import java.time.ZoneId;
 import org.opentripplanner.model.projectinfo.OtpProjectInfo;
 
 public class ApiServerInfo {
@@ -10,13 +11,15 @@ public class ApiServerInfo {
   public final ApiVersionControlInfo versionControl;
   public final ApiConfigInfo config;
   public final String otpSerializationVersionId;
+  public final String transitTimeZone;
 
-  public ApiServerInfo(String cpuName, int nCores, OtpProjectInfo projectInfo) {
+  public ApiServerInfo(String cpuName, int nCores, OtpProjectInfo projectInfo, ZoneId transitTimeZone) {
     this.cpuName = cpuName;
     this.nCores = nCores;
     this.version = new ApiProjectVersion(projectInfo.version);
     this.versionControl = new ApiVersionControlInfo(projectInfo.versionControl);
     this.config = new ApiConfigInfo(projectInfo);
     this.otpSerializationVersionId = projectInfo.getOtpSerializationVersionId();
+    this.transitTimeZone = transitTimeZone.toString();
   }
 }
--- src/main/java/org/opentripplanner/api/resource/ServerInfo.java
+++ src/main/java/org/opentripplanner/api/resource/ServerInfo.java
@@ -3,19 +3,29 @@ package org.opentripplanner.api.resource;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
 import org.opentripplanner.api.model.serverinfo.ApiServerInfo;
 import org.opentripplanner.model.projectinfo.OtpProjectInfo;
+import org.opentripplanner.standalone.api.OtpServerRequestContext;
 
 @Path("/")
 public class ServerInfo {
 
-  private static final ApiServerInfo SERVER_INFO = createServerInfo();
+  private final ZoneId timeZone;
+  private static final String cpuName;
+  private static final int nCores ;
+
+  public ServerInfo(@Context OtpServerRequestContext serverContext) {
+    this.timeZone = serverContext.transitService().getTimeZone();
+  }
+
 
   /**
    * Determine the OTP version and CPU type of the running server. This information should not
@@ -23,28 +33,30 @@ public class ServerInfo {
    * available before the graph is loaded, so for this to work this class should not be loaded
    * BEFORE that.
    */
-  public static ApiServerInfo createServerInfo() {
-    String cpuName = "unknown";
-    int nCores = 0;
+  static {
+    var cpu = "unknown";
+    int cores = 0;
     try {
       InputStream fis = new FileInputStream("/proc/cpuinfo");
       BufferedReader br = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8));
       String line;
       while ((line = br.readLine()) != null) {
         if (line.startsWith("model name")) {
-          cpuName = line.split(": ")[1];
-          nCores += 1;
+          cpu = line.split(": ")[1];
+          cores+= 1;
         }
       }
       fis.close();
     } catch (Exception ignore) {}
-
-    return new ApiServerInfo(cpuName, nCores, OtpProjectInfo.projectInfo());
+    cpuName = cpu;
+    nCores = cores;
   }
+
+
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public ApiServerInfo getServerInfo() {
-    return SERVER_INFO;
+    return new ApiServerInfo(cpuName, nCores, OtpProjectInfo.projectInfo(), timeZone);
   }
 }
