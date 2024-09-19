5ee083b92d3c26dfcd427bf61ea07f8f07ab19b5
Leonard Ehrenfried <mail@leonard.io>
2024-09-19 09:44:00 +0200

    Use line colours in debug UI

--- client/src/static/query/tripQuery.tsx
+++ client/src/static/query/tripQuery.tsx
@@ -64,6 +64,9 @@ export const query = graphql(`
             publicCode
             name
             id
+            presentation {
+              colour
+            }
           }
           authority {
             name
