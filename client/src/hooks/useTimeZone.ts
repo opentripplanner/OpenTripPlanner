eda11ecd9b289efd971c9befc365f173dda510cf
Leonard Ehrenfried <mail@leonard.io>
2024-09-23 23:25:44 +0200

    Use configuration value for URL

--- client/src/hooks/useTimeZone.ts
+++ client/src/hooks/useTimeZone.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 
+const serverInfoUrl = import.meta.env.VITE_SERVER_INFO_URL;
+
 type ServerInfo = {
   transitTimeZone: string;
 };
 
-const fetchServerInfo = (): Promise<ServerInfo> => fetch('http://localhost:8080/otp/').then((r) => r.json());
+const fetchServerInfo = (): Promise<ServerInfo> => fetch(serverInfoUrl).then((r) => r.json());
 
 export const useTimeZone = () => {
   const [data, setData] = useState<ServerInfo | null>(null);
