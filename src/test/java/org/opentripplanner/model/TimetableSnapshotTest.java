11f01231b69e68ece4259fc3cc0c64deefc607b6
Vincent Paturet <vincent.paturet@entur.org>
2024-10-10 16:52:13 +0200

    Clear added patterns in TimetableSnapshot

--- src/test/java/org/opentripplanner/model/TimetableSnapshotTest.java
+++ src/test/java/org/opentripplanner/model/TimetableSnapshotTest.java
@@ -16,16 +16,23 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ConcurrentModificationException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.ConstantsForTests;
 import org.opentripplanner.TestOtpModel;
 import org.opentripplanner._support.time.ZoneIds;
+import org.opentripplanner.transit.model.framework.Deduplicator;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
 import org.opentripplanner.transit.model.framework.Result;
+import org.opentripplanner.transit.model.network.Route;
 import org.opentripplanner.transit.model.network.TripPattern;
+import org.opentripplanner.transit.model.timetable.Trip;
+import org.opentripplanner.transit.model.timetable.TripIdAndServiceDate;
+import org.opentripplanner.transit.model.timetable.TripOnServiceDate;
 import org.opentripplanner.transit.model.timetable.TripTimes;
+import org.opentripplanner.transit.model.timetable.TripTimesFactory;
 import org.opentripplanner.transit.service.TransitModel;
 import org.opentripplanner.updater.spi.UpdateError;
 import org.opentripplanner.updater.trip.BackwardsDelayPropagationType;
@@ -33,6 +40,7 @@ import org.opentripplanner.updater.trip.BackwardsDelayPropagationType;
 public class TimetableSnapshotTest {
 
   private static final ZoneId timeZone = ZoneIds.GMT;
+  public static final LocalDate SERVICE_DATE = LocalDate.of(2024, 1, 1);
   private static Map<FeedScopedId, TripPattern> patternIndex;
   static String feedId;
 
@@ -302,6 +310,59 @@ public class TimetableSnapshotTest {
       ConcurrentModificationException.class,
       () -> committedSnapshot.revertTripToScheduledTripPattern(null, null)
     );
+  }
+
+  @Test
+  void testClear() {
+    TimetableSnapshot snapshot = new TimetableSnapshot();
+    TripPattern pattern = patternIndex.get(new FeedScopedId(feedId, "1.1"));
+    Trip trip = pattern.scheduledTripsAsStream().findFirst().orElseThrow();
+
+    TripIdAndServiceDate tripIdAndServiceDate = new TripIdAndServiceDate(
+      trip.getId(),
+      SERVICE_DATE
+    );
+    TripTimes updatedTriptimes = TripTimesFactory.tripTimes(
+      trip,
+      List.of(new StopTime()),
+      new Deduplicator()
+    );
+    RealTimeTripUpdate realTimeTripUpdate = new RealTimeTripUpdate(
+      pattern,
+      updatedTriptimes,
+      SERVICE_DATE,
+      TripOnServiceDate.of(trip.getId()).withTrip(trip).withServiceDate(SERVICE_DATE).build(),
+      true,
+      true
+    );
+
+    snapshot.update(realTimeTripUpdate);
+
+    assertNotNull(snapshot.getRealTimeAddedTrip(trip.getId()));
+    assertNotNull(snapshot.getRealTimeAddedPatternForTrip(trip));
+    assertNotNull(snapshot.getRealTimeAddedPatternForRoute(pattern.getRoute()));
+    assertNotNull(snapshot.getRealTimeAddedTripOnServiceDateById(trip.getId()));
+    assertNotNull(snapshot.getRealTimeAddedTripOnServiceDateForTripAndDay(tripIdAndServiceDate));
+    assertNotNull(snapshot.getRealtimeAddedRoute(pattern.getRoute().getId()));
+
+    snapshot.clear(trip.getId().getFeedId());
+
+    assertNull(snapshot.getRealTimeAddedTrip(trip.getId()));
+    assertNull(snapshot.getRealTimeAddedPatternForTrip(trip));
+    assertNull(snapshot.getRealTimeAddedTripOnServiceDateById(trip.getId()));
+    assertNull(snapshot.getRealTimeAddedTripOnServiceDateForTripAndDay(tripIdAndServiceDate));
+    assertNull(snapshot.getRealtimeAddedRoute(pattern.getRoute().getId()));
+    assertTrue(snapshot.getRealTimeAddedPatternForRoute(pattern.getRoute()).isEmpty());
+
+    snapshot.update(realTimeTripUpdate);
+    snapshot.clear("another feed id");
+
+    assertNotNull(snapshot.getRealTimeAddedTrip(trip.getId()));
+    assertNotNull(snapshot.getRealTimeAddedPatternForTrip(trip));
+    assertNotNull(snapshot.getRealTimeAddedPatternForRoute(pattern.getRoute()));
+    assertNotNull(snapshot.getRealTimeAddedTripOnServiceDateById(trip.getId()));
+    assertNotNull(snapshot.getRealTimeAddedTripOnServiceDateForTripAndDay(tripIdAndServiceDate));
+    assertNotNull(snapshot.getRealtimeAddedRoute(pattern.getRoute().getId()));
   }
 
   private static TimetableSnapshot createCommittedSnapshot() {
