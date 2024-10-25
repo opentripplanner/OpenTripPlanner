1cb96d66f0df91672ded08b10bcd9f9647be62c3
Thomas Gran <t2gran@gmail.com>
2024-10-25 10:53:49 +0200

    review: Add more doc to ExtraMcRouterSearch

--- application/src/main/java/org/opentripplanner/raptor/spi/ExtraMcRouterSearch.java
+++ application/src/main/java/org/opentripplanner/raptor/spi/ExtraMcRouterSearch.java
@@ -14,7 +14,11 @@ import org.opentripplanner.raptor.api.path.RaptorPath;
  * <p>
  * When changing the transit data, you may also invalidate the heuristics created by Raptor. If this
  * is the case, you need to turn off the {@link org.opentripplanner.raptor.api.request.Optimization#PARETO_CHECK_AGAINST_DESTINATION}.
- * For the heuristics to work, you may add extra cost or filter away data.
+ * For the heuristics to work, you may add extra cost or filter away data. But you cannot decrease
+ * the cost, add transfer or add new trips.
+ * <p>
+ * This will alter the multi-criteria search, if only a sadard search is requested any extra
+ * multi-criteria search is ignored.
  * <p>
  * @param <T> The TripSchedule type defined by the user of the raptor API.
  */
