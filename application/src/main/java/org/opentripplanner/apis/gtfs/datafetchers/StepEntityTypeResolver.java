d1067c6679a68ee252f8bda96300d28dee8cfe75
Henrik Sundell <henrik.sundell@gmail.com>
2024-10-28 16:06:52 +0200

    Remove unnecessary imports

--- application/src/main/java/org/opentripplanner/apis/gtfs/datafetchers/StepEntityTypeResolver.java
+++ application/src/main/java/org/opentripplanner/apis/gtfs/datafetchers/StepEntityTypeResolver.java
@@ -4,16 +4,7 @@ import graphql.TypeResolutionEnvironment;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.TypeResolver;
-import org.opentripplanner.apis.gtfs.model.RouteTypeModel;
-import org.opentripplanner.apis.gtfs.model.StopOnRouteModel;
-import org.opentripplanner.apis.gtfs.model.StopOnTripModel;
-import org.opentripplanner.apis.gtfs.model.UnknownModel;
 import org.opentripplanner.model.plan.Entrance;
-import org.opentripplanner.transit.model.network.Route;
-import org.opentripplanner.transit.model.network.TripPattern;
-import org.opentripplanner.transit.model.organization.Agency;
-import org.opentripplanner.transit.model.site.RegularStop;
-import org.opentripplanner.transit.model.timetable.Trip;
 
 public class StepEntityTypeResolver implements TypeResolver {
 
