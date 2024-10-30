09005eb32b34174259503bf22d88a5f834fdc873
eibakke <eivind.morris.bakke@entur.org>
2024-10-30 15:52:15 +0100

    Resolving merge conflict.

--- application/src/main/java/org/opentripplanner/transit/api/request/TripRequest.java
+++ application/src/main/java/org/opentripplanner/transit/api/request/TripRequest.java
@@ -4,48 +4,49 @@ import java.time.LocalDate;
 import java.util.List;
 import org.opentripplanner.framework.collection.ListUtils;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
+import org.opentripplanner.transit.model.timetable.Trip;
 
-/*
- * A request for trips.
+/**
+ * A request for {@link Trip}s.
  *
- * This request is used to retrieve Trips that match the provided criteria.
+ * This request is used to retrieve {@link Trip}s that match the provided criteria.
  */
 public class TripRequest {
 
-  private final List<FeedScopedId> authorities;
-  private final List<FeedScopedId> lines;
-  private final List<String> privateCodes;
-  private final List<LocalDate> activeDates;
+  private final List<FeedScopedId> agencies;
+  private final List<FeedScopedId> routes;
+  private final List<String> netexInternalPlanningCode;
+  private final List<LocalDate> serviceDates;
 
   protected TripRequest(
-    List<FeedScopedId> authorities,
-    List<FeedScopedId> lines,
-    List<String> privateCodes,
-    List<LocalDate> activeDates
+    List<FeedScopedId> agencies,
+    List<FeedScopedId> routes,
+    List<String> netexInternalPlanningCodes,
+    List<LocalDate> serviceDates
   ) {
-    this.authorities = ListUtils.nullSafeImmutableList(authorities);
-    this.lines = ListUtils.nullSafeImmutableList(lines);
-    this.privateCodes = ListUtils.nullSafeImmutableList(privateCodes);
-    this.activeDates = ListUtils.nullSafeImmutableList(activeDates);
+    this.agencies = ListUtils.nullSafeImmutableList(agencies);
+    this.routes = ListUtils.nullSafeImmutableList(routes);
+    this.netexInternalPlanningCode = ListUtils.nullSafeImmutableList(netexInternalPlanningCodes);
+    this.serviceDates = ListUtils.nullSafeImmutableList(serviceDates);
   }
 
   public static TripRequestBuilder of() {
     return new TripRequestBuilder();
   }
 
-  public List<FeedScopedId> authorities() {
-    return authorities;
+  public List<FeedScopedId> agencies() {
+    return agencies;
   }
 
-  public List<FeedScopedId> lines() {
-    return lines;
+  public List<FeedScopedId> routes() {
+    return routes;
   }
 
-  public List<String> privateCodes() {
-    return privateCodes;
+  public List<String> netexInternalPlanningCodes() {
+    return netexInternalPlanningCode;
   }
 
-  public List<LocalDate> activeDates() {
-    return activeDates;
+  public List<LocalDate> serviceDates() {
+    return serviceDates;
   }
 }
