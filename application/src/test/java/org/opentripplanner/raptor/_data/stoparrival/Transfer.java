36e1b91d40cc8fcd54ac4ef65732ac57a6bdea47
Thomas Gran <t2gran@gmail.com>
2024-11-02 23:00:27 +0100

    Add back Raptor test framework and deprecate it.

---
+++ application/src/test/java/org/opentripplanner/raptor/_data/stoparrival/Transfer.java
@@ -0,0 +1,43 @@
+package org.opentripplanner.raptor._data.stoparrival;
+
+import static org.opentripplanner.raptor.api.model.PathLegType.TRANSFER;
+
+import org.opentripplanner.raptor._data.transit.TestTripSchedule;
+import org.opentripplanner.raptor.api.model.PathLegType;
+import org.opentripplanner.raptor.api.model.RaptorTransfer;
+import org.opentripplanner.raptor.api.view.ArrivalView;
+
+/*
+ * @deprecated This was earlier part of Raptor and should not be used outside the Raptor
+ *             module. Use the OTP model entities instead.
+ */
+@Deprecated
+class Transfer extends AbstractStopArrival {
+
+  private final RaptorTransfer transfer;
+
+  Transfer(
+    int round,
+    int arrivalTime,
+    RaptorTransfer transfer,
+    ArrivalView<TestTripSchedule> previous
+  ) {
+    super(round, transfer.stop(), arrivalTime, transfer.c1(), previous.c2(), previous);
+    this.transfer = transfer;
+  }
+
+  @Override
+  public PathLegType arrivedBy() {
+    return TRANSFER;
+  }
+
+  @Override
+  public RaptorTransfer transfer() {
+    return transfer;
+  }
+
+  @Override
+  public boolean arrivedOnBoard() {
+    return false;
+  }
+}
