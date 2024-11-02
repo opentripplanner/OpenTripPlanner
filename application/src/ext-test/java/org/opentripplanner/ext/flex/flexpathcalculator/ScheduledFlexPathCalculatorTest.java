42bf36c24abf2cd147928f4f6a954a6430f9f77c
Leonard Ehrenfried <mail@leonard.io>
2024-11-02 16:27:45 +0100

    Fix import

--- application/src/ext-test/java/org/opentripplanner/ext/flex/flexpathcalculator/ScheduledFlexPathCalculatorTest.java
+++ application/src/ext-test/java/org/opentripplanner/ext/flex/flexpathcalculator/ScheduledFlexPathCalculatorTest.java
@@ -2,6 +2,7 @@ package org.opentripplanner.ext.flex.flexpathcalculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.opentripplanner.ext.flex.FlexStopTimesForTest.area;
+import static org.opentripplanner.ext.flex.FlexStopTimesForTest.regularStop;
 import static org.opentripplanner.street.model._data.StreetModelForTest.V1;
 import static org.opentripplanner.street.model._data.StreetModelForTest.V2;
 import static org.opentripplanner.transit.model._data.TimetableRepositoryForTest.id;
@@ -10,7 +11,6 @@ import java.time.Duration;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner._support.geometry.LineStrings;
-import org.opentripplanner.ext.flex.FlexStopTimesForTest;
 import org.opentripplanner.ext.flex.trip.ScheduledDeviatedTrip;
 
 class ScheduledFlexPathCalculatorTest {
@@ -19,9 +19,9 @@ class ScheduledFlexPathCalculatorTest {
     .of(id("123"))
     .withStopTimes(
       List.of(
-        FlexStopTimesForTest.regularStop("10:00", "10:01"),
+        regularStop("10:00", "10:01"),
         area("10:10", "10:20"),
-        FlexStopTimesForTest.regularStop("10:25", "10:26"),
+        regularStop("10:25", "10:26"),
         area("10:40", "10:50")
       )
     )
