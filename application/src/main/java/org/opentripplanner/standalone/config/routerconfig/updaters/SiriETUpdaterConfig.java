b77d591456c6726cac2f548a7e3af23214cc51c4
eibakke <eivind.morris.bakke@entur.org>
2024-11-05 15:12:38 +0100

    Addresses comments from PR.

--- application/src/main/java/org/opentripplanner/standalone/config/routerconfig/updaters/SiriETUpdaterConfig.java
+++ application/src/main/java/org/opentripplanner/standalone/config/routerconfig/updaters/SiriETUpdaterConfig.java
@@ -2,6 +2,7 @@ package org.opentripplanner.standalone.config.routerconfig.updaters;
 
 import static org.opentripplanner.standalone.config.framework.json.OtpVersion.V2_0;
 import static org.opentripplanner.standalone.config.framework.json.OtpVersion.V2_3;
+import static org.opentripplanner.standalone.config.framework.json.OtpVersion.V2_7;
 
 import java.time.Duration;
 import org.opentripplanner.standalone.config.framework.json.NodeAdapter;
@@ -46,7 +47,7 @@ public class SiriETUpdaterConfig {
       HttpHeadersConfig.headers(c, V2_3),
       c
         .of("producerMetrics")
-        .since(V2_0)
+        .since(V2_7)
         .summary("If failure, success, and warning metrics should be collected per producer.")
         .asBoolean(false)
     );
