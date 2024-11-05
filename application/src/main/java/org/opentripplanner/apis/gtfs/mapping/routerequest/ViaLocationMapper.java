f900796071e479f2cd92aaee911254ccbdde21da
Leonard Ehrenfried <mail@leonard.io>
2024-11-05 06:51:04 +0100

    Fix imports

--- application/src/main/java/org/opentripplanner/apis/gtfs/mapping/routerequest/ViaLocationMapper.java
+++ application/src/main/java/org/opentripplanner/apis/gtfs/mapping/routerequest/ViaLocationMapper.java
@@ -4,11 +4,11 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
-import org.opentripplanner.framework.collection.ListUtils;
 import org.opentripplanner.routing.api.request.via.PassThroughViaLocation;
 import org.opentripplanner.routing.api.request.via.ViaLocation;
 import org.opentripplanner.routing.api.request.via.VisitViaLocation;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
+import org.opentripplanner.utils.collection.ListUtils;
 
 /**
  * Maps the input data to the data structure needed for via routing.
