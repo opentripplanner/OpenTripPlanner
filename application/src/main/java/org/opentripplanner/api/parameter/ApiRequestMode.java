f92ff7e2799a8e046bae7e1426d4d1b19ced288d
Michael Tsang <michael.tsang@jnction.co.uk>
2024-11-05 14:44:38 +0000

    remove unused import
    Co-authored-by: Leonard Ehrenfried <mail@leonard.io>

--- application/src/main/java/org/opentripplanner/api/parameter/ApiRequestMode.java
+++ application/src/main/java/org/opentripplanner/api/parameter/ApiRequestMode.java
@@ -2,7 +2,6 @@ package org.opentripplanner.api.parameter;
 
 import java.util.Collection;
 import java.util.List;
-import org.opentripplanner.framework.application.OTPFeature;
 import org.opentripplanner.transit.model.basic.TransitMode;
 
 public enum ApiRequestMode {
