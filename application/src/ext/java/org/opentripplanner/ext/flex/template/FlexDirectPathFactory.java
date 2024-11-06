4d11307aacdc70db8040d3b8ba7aa3d6bb85f2b2
Zsombor Welker <zsombor.welker@realcity.io>
2024-11-06 09:40:46 +0100

    Street search: traverse edges which may return multiple states

--- application/src/ext/java/org/opentripplanner/ext/flex/template/FlexDirectPathFactory.java
+++ application/src/ext/java/org/opentripplanner/ext/flex/template/FlexDirectPathFactory.java
@@ -113,7 +113,7 @@ public class FlexDirectPathFactory {
 
     final State[] afterFlexState = flexEdge.traverse(accessNearbyStop.state);
 
-    var finalStateOpt = EdgeTraverser.traverseEdges(afterFlexState[0], egress.edges);
+    var finalStateOpt = EdgeTraverser.traverseEdges(afterFlexState, egress.edges);
 
     if (finalStateOpt.isEmpty()) {
       return Optional.empty();
