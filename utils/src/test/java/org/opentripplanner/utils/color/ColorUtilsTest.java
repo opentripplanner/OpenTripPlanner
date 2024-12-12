d12373f22544733ad6995d59be3473f267024c56
Michael Tsang <michael.tsang@jnction.co.uk>
2024-12-12 13:31:24 +0000

    use MethodSource instead of FieldSource

--- utils/src/test/java/org/opentripplanner/utils/color/ColorUtilsTest.java
+++ utils/src/test/java/org/opentripplanner/utils/color/ColorUtilsTest.java
@@ -5,26 +5,29 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.opentripplanner.utils.color.ColorUtils.computeBrightness;
 
 import java.awt.Color;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.FieldSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ColorUtilsTest {
 
-  static final Arguments[] brightnessExpectations = {
-    arguments(Color.black, Brightness.DARK),
-    arguments(Color.green, Brightness.LIGHT),
-    arguments(Color.blue, Brightness.DARK),
-    arguments(Color.red, Brightness.LIGHT),
-    arguments(Color.yellow, Brightness.LIGHT),
-    arguments(Color.white, Brightness.LIGHT),
-    arguments(Color.pink, Brightness.LIGHT),
-    arguments(Color.orange, Brightness.LIGHT),
-    arguments(Color.cyan, Brightness.LIGHT),
-  };
+  private static Stream<Arguments> brightnessExpectations() {
+    return Stream.of(
+      arguments(Color.black, Brightness.DARK),
+      arguments(Color.green, Brightness.LIGHT),
+      arguments(Color.blue, Brightness.DARK),
+      arguments(Color.red, Brightness.LIGHT),
+      arguments(Color.yellow, Brightness.LIGHT),
+      arguments(Color.white, Brightness.LIGHT),
+      arguments(Color.pink, Brightness.LIGHT),
+      arguments(Color.orange, Brightness.LIGHT),
+      arguments(Color.cyan, Brightness.LIGHT)
+    );
+  }
 
   @ParameterizedTest
-  @FieldSource("brightnessExpectations")
+  @MethodSource("brightnessExpectations")
   void testBrightness(Color color, Brightness brightness) {
     assertEquals(computeBrightness(color), brightness);
   }
