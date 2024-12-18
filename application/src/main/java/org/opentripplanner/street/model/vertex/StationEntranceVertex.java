2eb0e7b45c87941ea5fb928c4929f748fa089d5b
Leonard Ehrenfried <mail@leonard.io>
2024-12-18 11:33:51 +0100

    Add documentation

--- application/src/main/java/org/opentripplanner/street/model/vertex/StationEntranceVertex.java
+++ application/src/main/java/org/opentripplanner/street/model/vertex/StationEntranceVertex.java
@@ -5,28 +5,46 @@ import org.opentripplanner.transit.model.basic.Accessibility;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
 import org.opentripplanner.utils.tostring.ToStringBuilder;
 
+/**
+ * A station entrance extracted from OSM and therefore not (yet) associated with the transit
+ * entity {@link org.opentripplanner.transit.model.site.Station}.
+ */
 public class StationEntranceVertex extends OsmVertex {
 
+  private static final String FEED_ID = "osm";
   private final String code;
-  private final boolean accessible;
+  private final boolean wheelchairAccessible;
 
-  public StationEntranceVertex(double x, double y, long nodeId, String code, boolean accessible) {
-    super(x, y, nodeId);
+  public StationEntranceVertex(
+    double lat,
+    double lon,
+    long nodeId,
+    String code,
+    boolean wheelchairAccessible
+  ) {
+    super(lat, lon, nodeId);
     this.code = code;
-    this.accessible = accessible;
+    this.wheelchairAccessible = wheelchairAccessible;
   }
 
+  /**
+   * The id of the entrance which may or may not be human-readable.
+   */
   public FeedScopedId id() {
-    return new FeedScopedId("osm", String.valueOf(nodeId));
+    return new FeedScopedId(FEED_ID, String.valueOf(nodeId));
   }
 
+  /**
+   * Short human-readable code of the exit, like A or H3.
+   * If we need a proper name like "Oranienplatz" we have to add a name field.
+   */
   @Nullable
   public String code() {
     return code;
   }
 
   public Accessibility wheelchairAccessibility() {
-    return accessible ? Accessibility.POSSIBLE : Accessibility.NOT_POSSIBLE;
+    return wheelchairAccessible ? Accessibility.POSSIBLE : Accessibility.NOT_POSSIBLE;
   }
 
   @Override
