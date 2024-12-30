f639ce5dcebfdb2e36fc81df87d7425fbb32f201
Joel Lappalainen <lappalj8@gmail.com>
2024-12-30 17:31:00 +0200

    Only construct schema if feature flag is on

--- application/src/main/java/org/opentripplanner/apis/gtfs/service/configure/SchemaServiceModule.java
+++ application/src/main/java/org/opentripplanner/apis/gtfs/service/configure/SchemaServiceModule.java
@@ -5,12 +5,14 @@ import dagger.Provides;
 import jakarta.inject.Singleton;
 import org.opentripplanner.apis.gtfs.SchemaFactory;
 import org.opentripplanner.apis.gtfs.service.SchemaService;
+import org.opentripplanner.framework.application.OTPFeature;
 import org.opentripplanner.routing.api.request.preference.RoutingPreferences;
 
 /**
  * The service is used during application serve phase, not loading, and it depends on the default
  * routing preferences, which are injected from the
- * {@link org.opentripplanner.standalone.config.RouterConfig}.
+ * {@link org.opentripplanner.standalone.config.RouterConfig}. The {@link SchemaService} is only
+ * constructed if the API feature flag is on.
  */
 @Module
 public class SchemaServiceModule {
@@ -18,6 +20,8 @@ public class SchemaServiceModule {
   @Provides
   @Singleton
   public SchemaService provideSchemaService(RoutingPreferences defaultPreferences) {
-    return new SchemaService(SchemaFactory.createSchema(defaultPreferences));
+    return OTPFeature.GtfsGraphQlApi.isOn()
+      ? new SchemaService(SchemaFactory.createSchema(defaultPreferences))
+      : null;
   }
 }
