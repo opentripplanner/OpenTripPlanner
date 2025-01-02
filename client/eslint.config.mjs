2d4ff0c69e44bc38a8173d75ba77ac0d9c06ba16
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-01-02 16:37:42 +0100

    Updates for eslint v9

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,70 +1,40 @@
-import { fixupConfigRules } from "@eslint/compat";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importPlugin from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-config-prettier"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: [
-        "**/node_modules",
-        "**/dist",
-        "**/.prettierrc.js",
-        "**/.eslintrc.cjs",
-        "src/gql/**/*",
-    ],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "eslint-config-prettier",
-)), {
-    plugins: {
-        "react-refresh": reactRefresh,
-    },
-
+export default tseslint.config(
+  { ignores: ['output', 'node_modules', 'src/gql/**/*'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
-
-        parser: tsParser,
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-
-    settings: {
-        react: {
-            version: "detect",
-        },
-
-        "import/resolver": {
-            node: {
-                paths: ["src"],
-                extensions: [".js", ".jsx", ".ts", ".tsx"],
-            },
-        },
+    plugins: {
+      'react': react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      'import': importPlugin
     },
-
     rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
-
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "import/named": "off",
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      '@typescript-eslint/ban-ts-comment': "off",
+      '@typescript-eslint/no-unused-vars': ["error", { "caughtErrors": "all", "caughtErrorsIgnorePattern": "^_" }]
     },
-}];
+  },
+  eslintConfigPrettier,
+)
