4248ffe1644ae50d1970d26310b5593acf7fe508
Leonard Ehrenfried <mail@leonard.io>
2025-01-10 15:38:12 +0100

    Fix mapping in Transmodel API

--- application/src/main/java/org/opentripplanner/apis/transmodel/model/plan/PathGuidanceType.java
+++ application/src/main/java/org/opentripplanner/apis/transmodel/model/plan/PathGuidanceType.java
@@ -5,6 +5,7 @@ import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLList;
 import graphql.schema.GraphQLNonNull;
 import graphql.schema.GraphQLObjectType;
+import org.opentripplanner.apis.transmodel.mapping.RelativeDirectionMapper;
 import org.opentripplanner.apis.transmodel.model.EnumTypes;
 import org.opentripplanner.framework.graphql.GraphQLUtils;
 import org.opentripplanner.model.plan.WalkStep;
@@ -31,7 +32,9 @@ public class PathGuidanceType {
           .name("relativeDirection")
           .description("The relative direction of this step.")
           .type(EnumTypes.RELATIVE_DIRECTION)
-          .dataFetcher(environment -> ((WalkStep) environment.getSource()).getRelativeDirection())
+          .dataFetcher(environment ->
+            RelativeDirectionMapper.map(((WalkStep) environment.getSource()).getRelativeDirection())
+          )
           .build()
       )
       .field(
