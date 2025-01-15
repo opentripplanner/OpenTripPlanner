cda87ff049fe240ffb509948dd108fb461b70bc6
Leonard Ehrenfried <mail@leonard.io>
2025-01-15 10:12:16 +0100

    Incorporate review feedback

--- application/src/test/java/org/opentripplanner/transit/speed_test/SetupHelper.java
+++ application/src/test/java/org/opentripplanner/transit/speed_test/SetupHelper.java
@@ -2,6 +2,7 @@ package org.opentripplanner.transit.speed_test;
 
 import java.io.File;
 import java.net.URI;
+import javax.annotation.Nullable;
 import org.opentripplanner.datastore.OtpDataStore;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.routing.graph.SerializedGraphObject;
@@ -15,7 +16,7 @@ import org.opentripplanner.transit.speed_test.options.SpeedTestCmdLineOpts;
  */
 class SetupHelper {
 
-  static LoadModel loadGraph(File baseDir, URI path) {
+  static LoadModel loadGraph(File baseDir, @Nullable URI path) {
     File file = path == null
       ? OtpDataStore.graphFile(baseDir)
       : path.isAbsolute() ? new File(path) : new File(baseDir, path.getPath());
@@ -23,7 +24,9 @@ class SetupHelper {
     Graph graph = serializedGraphObject.graph;
 
     if (graph == null) {
-      throw new IllegalStateException();
+      throw new IllegalStateException(
+        "Could not find graph at %s".formatted(file.getAbsolutePath())
+      );
     }
 
     TimetableRepository timetableRepository = serializedGraphObject.timetableRepository;
