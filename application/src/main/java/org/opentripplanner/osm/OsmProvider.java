9706451cbb98b4f751ff3212defcd944292fac37
Leonard Ehrenfried <mail@leonard.io>
2025-01-29 08:09:44 +0100

    Update docs

--- application/src/main/java/org/opentripplanner/osm/OsmProvider.java
+++ application/src/main/java/org/opentripplanner/osm/OsmProvider.java
@@ -5,7 +5,13 @@ import org.opentripplanner.graph_builder.module.osm.OsmDatabase;
 import org.opentripplanner.osm.tagmapping.OsmTagMapper;
 import org.opentripplanner.osm.wayproperty.WayPropertySet;
 
+/**
+ * Provides OSM data to the graph build by filling in {@link OsmDatabase}.
+ */
 public interface OsmProvider {
+  /**
+   * Fill the mutable {@link OsmDatabase} with data.
+   */
   void readOsm(OsmDatabase osmdb);
 
   OsmTagMapper getOsmTagMapper();
