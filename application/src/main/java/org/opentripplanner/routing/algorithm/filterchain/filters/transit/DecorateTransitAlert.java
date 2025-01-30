14f350900a3e88dbe25783d2dd613523c3398503
Leonard Ehrenfried <mail@leonard.io>
2025-01-30 18:06:31 +0100

    Use Box instead of AtomicBoolean

--- application/src/main/java/org/opentripplanner/routing/algorithm/filterchain/filters/transit/DecorateTransitAlert.java
+++ application/src/main/java/org/opentripplanner/routing/algorithm/filterchain/filters/transit/DecorateTransitAlert.java
@@ -1,6 +1,5 @@
 package org.opentripplanner.routing.algorithm.filterchain.filters.transit;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import org.opentripplanner.model.plan.Itinerary;
 import org.opentripplanner.routing.algorithm.filterchain.framework.spi.ItineraryDecorator;
@@ -8,6 +7,7 @@ import org.opentripplanner.routing.algorithm.mapping.AlertToLegMapper;
 import org.opentripplanner.routing.services.TransitAlertService;
 import org.opentripplanner.transit.model.site.MultiModalStation;
 import org.opentripplanner.transit.model.site.Station;
+import org.opentripplanner.utils.lang.Box;
 
 public class DecorateTransitAlert implements ItineraryDecorator {
 
@@ -22,7 +22,7 @@ public class DecorateTransitAlert implements ItineraryDecorator {
 
   @Override
   public void decorate(Itinerary itinerary) {
-    final var firstLeg = new AtomicBoolean(true);
+    final var firstLeg = Box.of(true);
     itinerary.transformTransitLegs(leg -> {
       if (leg.isTransitLeg()) {
         var l = alertToLegMapper.decorateWithAlerts(leg, firstLeg.get());
