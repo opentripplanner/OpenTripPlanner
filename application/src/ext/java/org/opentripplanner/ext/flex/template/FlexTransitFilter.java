e015ea34beac04a610a950e1dc3bffbf86d91b71
Leonard Ehrenfried <mail@leonard.io>
2025-02-03 10:48:21 +0100

    Add Javadoc

--- application/src/ext/java/org/opentripplanner/ext/flex/template/FlexTransitFilter.java
+++ application/src/ext/java/org/opentripplanner/ext/flex/template/FlexTransitFilter.java
@@ -2,6 +2,13 @@ package org.opentripplanner.ext.flex.template;
 
 import java.util.List;
 
+/**
+ * Filters trips based on filter criteria. This is a much reduced version of the general
+ * filtering that applies to fixed-schedule transit. It only supports the following features:
+ *
+ *  - selecting (whitelisting) agencies and routes
+ *  - banning (blacklisting) agencies and routes
+ */
 public class FlexTransitFilter {
 
   public static FlexTransitFilter ALLOW_ALL = new FlexTransitFilter(
@@ -13,6 +20,9 @@ public class FlexTransitFilter {
     this.filters = filters;
   }
 
+  /**
+   * Should the trip be considered according to the filter criteria.
+   */
   public boolean allowsTrip(ClosestTrip closestTrip) {
     for (var filter : filters) {
       if (!filter.allowsTrip(closestTrip.flexTrip().getTrip())) {
