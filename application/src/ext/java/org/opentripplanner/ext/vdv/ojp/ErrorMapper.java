7f4ffa465562880b555256c66f0be5f21465ad4a
Leonard Ehrenfried <mail@leonard.io>
2025-02-13 22:56:57 +0100

    Handle error condition

--- application/src/ext/java/org/opentripplanner/ext/vdv/ojp/ErrorMapper.java
+++ application/src/ext/java/org/opentripplanner/ext/vdv/ojp/ErrorMapper.java
@@ -2,6 +2,7 @@ package org.opentripplanner.ext.vdv.ojp;
 
 import de.vdv.ojp20.OJP;
 import de.vdv.ojp20.OJPResponseStructure;
+import de.vdv.ojp20.siri.CapabilityNotSupportedErrorStructure;
 import de.vdv.ojp20.siri.ErrorDescriptionStructure;
 import de.vdv.ojp20.siri.ServiceDelivery;
 import de.vdv.ojp20.siri.ServiceDeliveryStructure;
