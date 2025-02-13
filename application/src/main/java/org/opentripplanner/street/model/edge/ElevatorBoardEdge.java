022707fda8743d47749e1eb133ad6d5c212074e8
Teemu Kalvas <teemu.kalvas@hsl.fi>
2025-02-13 15:42:02 +0200

    use incrementTimeInSeconds instead of Milliseconds whenever applicable

--- application/src/main/java/org/opentripplanner/street/model/edge/ElevatorBoardEdge.java
+++ application/src/main/java/org/opentripplanner/street/model/edge/ElevatorBoardEdge.java
@@ -49,7 +49,7 @@ public class ElevatorBoardEdge extends Edge implements BikeWalkableEdge, Elevato
     var streetPreferences = s0.getPreferences().street();
 
     s1.incrementWeight(streetPreferences.elevator().boardCost());
-    s1.incrementTimeInMilliseconds(1000L * streetPreferences.elevator().boardTime());
+    s1.incrementTimeInSeconds(streetPreferences.elevator().boardTime());
 
     return s1.makeStateArray();
   }
