e56ca7e7edef8bd7c82b14348250429ab9e4068c
Leonard Ehrenfried <mail@leonard.io>
2025-02-14 13:39:19 +0100

    Implement previous/onward calls

--- application/src/ext/java/org/opentripplanner/ext/vdv/ojp/OjpService.java
+++ application/src/ext/java/org/opentripplanner/ext/vdv/ojp/OjpService.java
@@ -1,9 +1,15 @@
 package org.opentripplanner.ext.vdv.ojp;
 
+import static java.lang.Boolean.TRUE;
+import static org.opentripplanner.ext.vdv.ojp.StopEventResponseMapper.OptionalFeature.ONWARD_CALLS;
+import static org.opentripplanner.ext.vdv.ojp.StopEventResponseMapper.OptionalFeature.PREVIOUS_CALLS;
+
 import de.vdv.ojp20.OJP;
 import de.vdv.ojp20.OJPStopEventRequestStructure;
+import de.vdv.ojp20.StopEventParamStructure;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import org.opentripplanner.ext.vdv.VdvService;
@@ -31,7 +37,26 @@ public class OjpService {
       .map(s -> s.getNumberOfResults())
       .map(i -> i.intValue())
       .orElse(1);
+
     var tripTimesOnDate = vdvService.findStopTimesInPattern(stopId, time.toInstant(), numResults);
-    return mapper.mapStopTimesInPattern(tripTimesOnDate, ZonedDateTime.now(), Set.of());
+    return mapper.mapStopTimesInPattern(
+      tripTimesOnDate,
+      ZonedDateTime.now(),
+      mapOptionalFeatures(ser.getParams())
+    );
+  }
+
+  private static Set<StopEventResponseMapper.OptionalFeature> mapOptionalFeatures(
+    StopEventParamStructure params
+  ) {
+    var res = new HashSet<StopEventResponseMapper.OptionalFeature>();
+
+    if (TRUE.equals(params.isIncludePreviousCalls())) {
+      res.add(PREVIOUS_CALLS);
+    }
+    if (TRUE.equals(params.isIncludeOnwardCalls())) {
+      res.add(ONWARD_CALLS);
+    }
+    return res;
   }
 }
