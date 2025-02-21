516e3a89f5dd55a38701406f3e6ac30baa40f702
Michael Tsang <michael.tsang@jnction.co.uk>
2025-02-21 10:59:18 +0000

    fix test

--- application/src/test/java/org/opentripplanner/model/TimetableSnapshotTest.java
+++ application/src/test/java/org/opentripplanner/model/TimetableSnapshotTest.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.model;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
