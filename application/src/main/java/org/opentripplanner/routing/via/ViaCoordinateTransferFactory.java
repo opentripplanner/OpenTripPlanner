e5177b817b7b47fdc55d23e9714cf41fa540c178
Thomas Gran <t2gran@gmail.com>
2025-02-25 15:24:08 +0100

    doc: Add JavaDoc to ViaCoordinateTransferFactory

--- application/src/main/java/org/opentripplanner/routing/via/ViaCoordinateTransferFactory.java
+++ application/src/main/java/org/opentripplanner/routing/via/ViaCoordinateTransferFactory.java
@@ -5,6 +5,10 @@ import org.opentripplanner.framework.geometry.WgsCoordinate;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.via.model.ViaCoordinateTransfer;
 
+/**
+ * This class is a factory for creating {@link ViaCoordinateTransfer}s. It is injected into
+ * the transit router using dependency injection.
+ */
 public interface ViaCoordinateTransferFactory {
   List<ViaCoordinateTransfer> createViaTransfers(
     RouteRequest request,
