78ec5742326e2dfa8941f19d0193a320f40ff9e1
Leonard Ehrenfried <mail@leonard.io>
2025-03-04 12:22:05 +0100

    Add test for previous/next

--- application/src/test/java/org/opentripplanner/model/TripTimeOnDateTest.java
+++ application/src/test/java/org/opentripplanner/model/TripTimeOnDateTest.java
@@ -1,25 +1,29 @@
 package org.opentripplanner.model;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.model.plan.PlanTestConstants;
 import org.opentripplanner.transit.model._data.TimetableRepositoryForTest;
 import org.opentripplanner.transit.model.basic.TransitMode;
 import org.opentripplanner.transit.model.framework.Deduplicator;
+import org.opentripplanner.transit.model.site.RegularStop;
 import org.opentripplanner.transit.model.timetable.TripTimesFactory;
 
 class TripTimeOnDateTest implements PlanTestConstants {
 
+  private static final TimetableRepositoryForTest TEST_MODEL = TimetableRepositoryForTest.of();
+
   @Test
   void gtfsSequence() {
-    var testModel = TimetableRepositoryForTest.of();
-    var pattern = testModel.pattern(TransitMode.BUS).build();
+    var pattern = TEST_MODEL.pattern(TransitMode.BUS).build();
     var trip = TimetableRepositoryForTest.trip("123").build();
-    var stopTimes = testModel.stopTimesEvery5Minutes(3, trip, "11:00");
+    var stopTimes = TEST_MODEL.stopTimesEvery5Minutes(3, trip, "11:00");
 
     var tripTimes = TripTimesFactory.tripTimes(trip, stopTimes, new Deduplicator());
 
@@ -34,10 +38,9 @@ class TripTimeOnDateTest implements PlanTestConstants {
 
   @Test
   void isRecordedStop() {
-    var testModel = TimetableRepositoryForTest.of();
-    var pattern = testModel.pattern(TransitMode.BUS).build();
+    var pattern = TEST_MODEL.pattern(TransitMode.BUS).build();
     var trip = TimetableRepositoryForTest.trip("123").build();
-    var stopTimes = testModel.stopTimesEvery5Minutes(3, trip, "11:00");
+    var stopTimes = TEST_MODEL.stopTimesEvery5Minutes(3, trip, "11:00");
 
     var tripTimes = TripTimesFactory.tripTimes(trip, stopTimes, new Deduplicator());
     tripTimes.setRecorded(1);
@@ -49,5 +52,41 @@ class TripTimeOnDateTest implements PlanTestConstants {
     subject = new TripTimeOnDate(tripTimes, 1, pattern);
 
     assertTrue(subject.isRecordedStop());
+  }
+
+  @Test
+  void previousTimes() {
+    var subject = tripTimeOnDate();
+
+    var ids = subject.previousTimes().stream().map(t -> t.getStop().getId().toString()).toList();
+    assertEquals(List.of("F:stop-10", "F:stop-20"), ids);
+    assertThat(subject.previousTimes().getFirst().previousTimes()).isEmpty();
+  }
+
+  @Test
+  void nextTimes() {
+    var subject = tripTimeOnDate();
+    var ids = subject.nextTimes().stream().map(t -> t.getStop().getId().toString()).toList();
+    assertEquals(List.of("F:stop-40", "F:stop-50"), ids);
+    var secondLast = subject.nextTimes().getFirst();
+    var lastStop = secondLast
+      .nextTimes()
+      .stream()
+      .map(t -> t.getStop().getId().toString())
+      .toList();
+    assertEquals(List.of("F:stop-50"), lastStop);
+    assertThat(secondLast.nextTimes().getFirst().nextTimes()).isEmpty();
+  }
+
+  private static TripTimeOnDate tripTimeOnDate() {
+    var trip = TimetableRepositoryForTest.trip("123").build();
+    var stopTimes = TEST_MODEL.stopTimesEvery5Minutes(5, trip, "11:00");
+    var stops = stopTimes.stream().map(StopTime::getStop).map(RegularStop.class::cast).toList();
+    var pattern = TEST_MODEL
+      .pattern(TransitMode.BUS)
+      .withStopPattern(TimetableRepositoryForTest.stopPattern(stops))
+      .build();
+    var tripTimes = TripTimesFactory.tripTimes(trip, stopTimes, new Deduplicator());
+    return new TripTimeOnDate(tripTimes, 2, pattern);
   }
 }
