3eacf27d5c53d6f79c1fe1fd10f17f00ea246056
Leonard Ehrenfried <mail@leonard.io>
2025-03-10 14:25:17 +0100

    Add information about disabled stations/vehicles

--- application/src/main/java/org/opentripplanner/inspector/vector/rental/RentalPropertyMapper.java
+++ application/src/main/java/org/opentripplanner/inspector/vector/rental/RentalPropertyMapper.java
@@ -7,17 +7,32 @@ import java.util.List;
 import org.opentripplanner.apis.support.mapping.PropertyMapper;
 import org.opentripplanner.inspector.vector.KeyValue;
 import org.opentripplanner.service.vehiclerental.model.VehicleRentalPlace;
+import org.opentripplanner.service.vehiclerental.model.VehicleRentalStation;
+import org.opentripplanner.service.vehiclerental.model.VehicleRentalVehicle;
+import org.opentripplanner.utils.collection.ListUtils;
 
-public class RentalPropertyMapper extends PropertyMapper<VehicleRentalPlace> {
+class RentalPropertyMapper extends PropertyMapper<VehicleRentalPlace> {
 
   @Override
   protected Collection<KeyValue> map(VehicleRentalPlace input) {
-    return List.of(
+    var base = List.of(
       kv("class", input.getClass().getSimpleName()),
       kv("id", input.getName()),
       kv("network", input.getNetwork()),
       kv("vehiclesAvailable", input.getVehiclesAvailable()),
       kv("spacesAvailable", input.getSpacesAvailable())
     );
+
+    if (input instanceof VehicleRentalVehicle vehicle) {
+      var props = List.of(kv("disabled", vehicle.isDisabled));
+      return ListUtils.combine(base, props);
+    } else if (input instanceof VehicleRentalStation station) {
+      var props = List.of(
+        kv("spacesDisabled", station.spacesDisabled),
+        kv("vehiclesDisabled", station.vehiclesDisabled)
+      );
+      return ListUtils.combine(base, props);
+    }
+    return base;
   }
 }
