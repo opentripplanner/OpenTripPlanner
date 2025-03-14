ed3a813ed5af0886b137b9f91fc0fc3335e7ff0a
Thomas Gran <t2gran@gmail.com>
2025-03-14 15:58:10 +0100

    refactor: Simplify DefaultEmissionsService options

--- application/src/ext/java/org/opentripplanner/ext/emissions/DefaultEmissionsService.java
+++ application/src/ext/java/org/opentripplanner/ext/emissions/DefaultEmissionsService.java
@@ -19,17 +19,13 @@ public class DefaultEmissionsService implements EmissionsService {
 
   @Override
   public Optional<Emissions> getEmissionsPerMeterForRoute(FeedScopedId feedScopedRouteId) {
-    Optional<Double> co2Emissions = this.emissionsDataModel.getCO2EmissionsById(feedScopedRouteId);
-    return co2Emissions.isPresent()
-      ? Optional.of(new Emissions(new Gram(co2Emissions.get())))
-      : Optional.empty();
+    return this.emissionsDataModel.getCO2EmissionsById(feedScopedRouteId).map(co2 ->
+        new Emissions(new Gram(co2))
+      );
   }
 
   @Override
   public Optional<Emissions> getEmissionsPerMeterForCar() {
-    Optional<Double> co2Emissions = this.emissionsDataModel.getCarAvgCo2PerMeter();
-    return co2Emissions.isPresent()
-      ? Optional.of(new Emissions(new Gram(co2Emissions.get())))
-      : Optional.empty();
+    return this.emissionsDataModel.getCarAvgCo2PerMeter().map(co2 -> new Emissions(new Gram(co2)));
   }
 }
