ee451f15b49154d886c7378e3a9f576c5f239a7f
Thomas Gran <t2gran@gmail.com>
2025-03-18 16:01:56 +0100

    refactor: Move GtfsFeedIdResolverTest to correct package

--- application/src/test/java/org/opentripplanner/gtfs/graphbuilder/GtfsFeedIdResolverTest.java
+++ application/src/test/java/org/opentripplanner/gtfs/graphbuilder/GtfsFeedIdResolverTest.java
@@ -1,4 +1,4 @@
-package org.opentripplanner.graph_builder.module;
+package org.opentripplanner.gtfs.graphbuilder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.opentripplanner.gtfs.graphbuilder.GtfsFeedIdResolver;
 
 class GtfsFeedIdResolverTest {
 
