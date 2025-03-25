556a8f3adb79c4a13ab65baa5236d1ac7087bcb5
Ville Pihlava <ville.pihlava@cgi.com>
2025-03-25 17:36:35 +0200

    Add toString method.

--- application/src/main/java/org/opentripplanner/routing/algorithm/filterchain/filters/transit/RemoveTransitIfStreetOnlyIsBetterResults.java
+++ application/src/main/java/org/opentripplanner/routing/algorithm/filterchain/filters/transit/RemoveTransitIfStreetOnlyIsBetterResults.java
@@ -1,10 +1,18 @@
 package org.opentripplanner.routing.algorithm.filterchain.filters.transit;
 
 import org.opentripplanner.framework.model.Cost;
+import org.opentripplanner.utils.tostring.ToStringBuilder;
 
 /**
  * The RemoveTransitIfStreetOnlyIsBetter filter removes itineraries from a list of itineraries based on
  * a generalizedCostMaxLimit. Transit itineraries that have a higher generalized cost than the limit
  * will be filtered away. This results class is used as input for the PageCursor.
  */
-public record RemoveTransitIfStreetOnlyIsBetterResults(Cost generalizedCostMaxLimit) {}
+public record RemoveTransitIfStreetOnlyIsBetterResults(Cost generalizedCostMaxLimit) {
+  @Override
+  public String toString() {
+    return ToStringBuilder.of(RemoveTransitIfStreetOnlyIsBetterResults.class)
+      .addObj("generalizedCostMaxLimit", generalizedCostMaxLimit)
+      .toString();
+  }
+}
