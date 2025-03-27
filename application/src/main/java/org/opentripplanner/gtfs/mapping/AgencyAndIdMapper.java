3e463b5edba4f973fbb49c3284bcbe76606e0136
Leonard Ehrenfried <mail@leonard.io>
2025-03-27 11:25:33 +0100

    Remove unused code, make class package-private

--- application/src/main/java/org/opentripplanner/gtfs/mapping/AgencyAndIdMapper.java
+++ application/src/main/java/org/opentripplanner/gtfs/mapping/AgencyAndIdMapper.java
@@ -1,22 +1,15 @@
 package org.opentripplanner.gtfs.mapping;
 
+import javax.annotation.Nullable;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
 
 /** Responsible for mapping GTFS AgencyAndId into the OTP model. */
-public class AgencyAndIdMapper {
+class AgencyAndIdMapper {
 
   /** Map from GTFS to OTP model, {@code null} safe. */
-  public static FeedScopedId mapAgencyAndId(AgencyAndId id) {
+  public static FeedScopedId mapAgencyAndId(@Nullable AgencyAndId id) {
     return id == null ? null : new FeedScopedId(id.getAgencyId(), id.getId());
   }
 
-  /**
-   * Returns the id part of the {@link AgencyAndId} if its non-null. Otherwise, returns null.
-   */
-  public static String mapNullableId(AgencyAndId id) {
-    if (id != null) {
-      return id.getId();
-    } else return null;
-  }
 }
