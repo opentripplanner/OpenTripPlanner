61a3e554b7cc96e0107c301b25f99e9a162927e9
Ville Pihlava <ville.pihlava@cgi.com>
2025-03-28 14:08:22 +0200

    Add PageCursorInputAggregator to ItineraryListFilterChain.

--- application/src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryListFilterChain.java
+++ application/src/main/java/org/opentripplanner/routing/algorithm/filterchain/ItineraryListFilterChain.java
@@ -6,21 +6,25 @@ import org.opentripplanner.model.plan.Itinerary;
 import org.opentripplanner.routing.algorithm.filterchain.framework.filterchain.DeleteResultHandler;
 import org.opentripplanner.routing.algorithm.filterchain.framework.filterchain.RoutingErrorsAttacher;
 import org.opentripplanner.routing.algorithm.filterchain.framework.spi.ItineraryListFilter;
+import org.opentripplanner.routing.algorithm.filterchain.paging.PageCursorInputAggregator;
 import org.opentripplanner.routing.api.response.RoutingError;
 
 public class ItineraryListFilterChain {
 
   private final List<ItineraryListFilter> filters;
   private final DeleteResultHandler debugHandler;
+  private final PageCursorInputAggregator pageCursorInputAggregator;
 
   private final List<RoutingError> routingErrors = new ArrayList<>();
 
   public ItineraryListFilterChain(
     List<ItineraryListFilter> filters,
-    DeleteResultHandler debugHandler
+    DeleteResultHandler debugHandler,
+    PageCursorInputAggregator pageCursorInputAggregator
   ) {
     this.debugHandler = debugHandler;
     this.filters = filters;
+    this.pageCursorInputAggregator = pageCursorInputAggregator;
   }
 
   public List<Itinerary> filter(List<Itinerary> itineraries) {
@@ -28,6 +32,8 @@ public class ItineraryListFilterChain {
     for (ItineraryListFilter filter : filters) {
       result = filter.filter(result);
     }
+
+    pageCursorInputAggregator.createPageCursorInput();
 
     routingErrors.addAll(RoutingErrorsAttacher.computeErrors(itineraries, result));
 
