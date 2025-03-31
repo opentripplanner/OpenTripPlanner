a0ee7df22237d89ea3a5598e8fd3b1682874ada2
Leonard Ehrenfried <mail@leonard.io>
2025-03-31 14:27:58 +0200

    Rename FilterValues implementations, make package-private

--- application/src/main/java/org/opentripplanner/transit/api/model/NullIsEverythingFilter.java
+++ application/src/main/java/org/opentripplanner/transit/api/model/NullIsEverythingFilter.java
@@ -3,12 +3,12 @@ package org.opentripplanner.transit.api.model;
 import java.util.Collection;
 
 /**
- * {@link FilterValuesNullIsEverything} is a subclass of {@link FilterValues} that
+ * {@link NullIsEverythingFilter} is a subclass of {@link FilterValues} that
  * includes everything only if the values collection is null.
  */
-public class FilterValuesNullIsEverything<E> extends FilterValues<E> {
+class NullIsEverythingFilter<E> extends FilterValues<E> {
 
-  FilterValuesNullIsEverything(String name, Collection<E> values) {
+  NullIsEverythingFilter(String name, Collection<E> values) {
     super(name, values);
   }
 
