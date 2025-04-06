6224f46a25ad52c273941a743cff367e459d5e93
Leonard Ehrenfried <mail@leonard.io>
2025-04-06 21:34:40 +0200

    Remove debug statement

--- application/src/ext/java/org/opentripplanner/ext/fares/DecorateWithFare.java
+++ application/src/ext/java/org/opentripplanner/ext/fares/DecorateWithFare.java
@@ -3,15 +3,12 @@ package org.opentripplanner.ext.fares;
 import org.opentripplanner.model.plan.Itinerary;
 import org.opentripplanner.routing.algorithm.filterchain.framework.spi.ItineraryDecorator;
 import org.opentripplanner.routing.fares.FareService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Computes the fares of an itinerary and adds them.
  */
 public class DecorateWithFare implements ItineraryDecorator {
 
-  private static final Logger LOG = LoggerFactory.getLogger(DecorateWithFare.class);
   private final FareService fareService;
 
   public DecorateWithFare(FareService fareService) {
@@ -20,7 +17,6 @@ public class DecorateWithFare implements ItineraryDecorator {
 
   @Override
   public Itinerary decorate(Itinerary itinerary) {
-    LOG.error("Decorating with {}.", fareService);
     var fare = fareService.calculateFares(itinerary);
     return (fare != null)
       ? ItineraryFaresDecorator.decorateItineraryWithFare(itinerary, fare)
