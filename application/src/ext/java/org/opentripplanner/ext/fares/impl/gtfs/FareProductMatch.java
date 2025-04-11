c4c2ff8c7824c85432801e1c01624706d098bd5d
Leonard Ehrenfried <mail@leonard.io>
2025-04-11 15:16:06 +0200

    Remove unused class

--- application/src/ext/java/org/opentripplanner/ext/fares/impl/gtfs/FareProductMatch.java
+++
@@ -1,7 +0,0 @@
-package org.opentripplanner.ext.fares.impl.gtfs;
-
-import java.util.Set;
-import org.opentripplanner.model.fare.FareProduct;
-import org.opentripplanner.model.plan.Leg;
-
-record FareProductMatch(Leg leg, Set<FareProduct> fareProducts) {}
