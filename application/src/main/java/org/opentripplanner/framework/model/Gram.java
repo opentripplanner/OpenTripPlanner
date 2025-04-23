84cc2528b9ca9cff5d5a0a7ac473bbb3975eb496
Thomas Gran <t2gran@gmail.com>
2025-04-23 15:00:12 +0200

    refactor: Limit Gram to 1/100 g, add of(String) factory method.
    The Gram class is converted to use int as its internal data type. In addition a factory function taking a string as input is added. This enable us to use grams with a unit as input.

--- application/src/main/java/org/opentripplanner/framework/model/Gram.java
+++ application/src/main/java/org/opentripplanner/framework/model/Gram.java
@@ -3,38 +3,78 @@ package org.opentripplanner.framework.model;
 import static java.lang.Double.compare;
 
 import java.io.Serializable;
+import java.util.regex.Pattern;
 
 /**
- * A representation of the weight of something in grams.
+ * A representation of the weight of something in grams. The precision is 1/100g.
  */
 public final class Gram implements Serializable, Comparable<Gram> {
 
-  public static final Gram ZERO = new Gram(0.0);
+  public static final Gram ZERO = new Gram(0);
 
-  private final double value;
+  // PAttern used to split number and unit with an optonal space in between
+  private static final Pattern PATTERN = Pattern.compile("([\\d\\.]+) ?([kg]*)");
+  private static final int PRECISION = 100;
+  private static final int HALF_PRECISION = PRECISION / 2;
+  private static final int KILO_GRAM_PRECISION = 1_000 * PRECISION;
 
-  private Gram(double value) {
+  private final int value;
+
+  private Gram(int value) {
     this.value = value;
   }
 
+  private static Gram ofPrecisionDouble(double value) {
+    int v = (int) Math.round(value);
+    return v == 0 ? ZERO : new Gram(v);
+  }
+
+  private static Gram ofPrecisionInt(int value) {
+    return value == 0 ? ZERO : new Gram(value);
+  }
+
+  public static Gram of(int value) {
+    return ofPrecisionInt(value * PRECISION);
+  }
+
   public static Gram of(double value) {
-    return value == 0.0 ? ZERO : new Gram(value);
+    return ofPrecisionDouble(value * PRECISION);
+  }
+
+  /**
+   * Create a new Gram object based on the string input. An optional unit can be added.
+   * 'g'(grams) and 'kg'(kilograms) is supported.
+   * <p>
+   * Format: {@code [Decimal number] ('g'|'kg')?}
+   * <p>
+   * Examples: {@code 0}, {@code 7g}, {@code 2.5kg} and {@code 1.1 kg}.
+   */
+  public static Gram of(String value) {
+    return parse(value);
   }
 
   public static Gram ofNullable(Double value) {
-    return value == null ? ZERO : Gram.of(value);
+    return value == null ? ZERO : of(value.doubleValue());
   }
 
   public Gram plus(Gram g) {
-    return of(this.value + g.value);
+    return new Gram(this.value + g.value);
+  }
+
+  public Gram multiply(int factor) {
+    return new Gram(this.value * factor);
   }
 
   public Gram multiply(double factor) {
-    return of(this.value * factor);
+    return ofPrecisionDouble(this.value * factor);
+  }
+
+  public Gram dividedBy(int scalar) {
+    return ofPrecisionDouble(value / (double) scalar);
   }
 
   public Gram dividedBy(double scalar) {
-    return new Gram(this.value / scalar);
+    return ofPrecisionDouble(this.value / scalar);
   }
 
   @Override
@@ -61,14 +101,42 @@ public final class Gram implements Serializable, Comparable<Gram> {
 
   @Override
   public String toString() {
-    return this.value + "g";
+    if (value % KILO_GRAM_PRECISION == 0) {
+      return value / KILO_GRAM_PRECISION + "kg";
+    }
+    if (value % PRECISION == 0) {
+      return value / PRECISION + "g";
+    }
+    return asDouble() + "g";
+  }
+
+  public double asInt() {
+    return (value + HALF_PRECISION) / PRECISION;
   }
 
   public double asDouble() {
-    return this.value;
+    return (double) this.value / PRECISION;
   }
 
   public boolean isZero() {
-    return value == 0.0;
+    return value == 0;
+  }
+
+  private static Gram parse(String value) {
+    var m = PATTERN.matcher(value);
+    if (!m.matches()) {
+      throw new IllegalArgumentException("Parse error! Illegal gram value: '%s'".formatted(value));
+    }
+    var num = m.group(1);
+    var unit = m.group(2);
+
+    double v = Double.parseDouble(num);
+    if ("kg".equalsIgnoreCase(unit)) {
+      return ofPrecisionDouble(KILO_GRAM_PRECISION * v);
+    }
+    if ("g".equalsIgnoreCase(unit) || unit.isBlank()) {
+      return ofPrecisionDouble(PRECISION * v);
+    }
+    throw new IllegalArgumentException("Parse error! Illegal gram value: '%s'".formatted(value));
   }
 }
--- application/src/test/java/org/opentripplanner/framework/model/GramTest.java
+++ application/src/test/java/org/opentripplanner/framework/model/GramTest.java
@@ -2,6 +2,7 @@ package org.opentripplanner.framework.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,16 @@ import org.junit.jupiter.api.Test;
 class GramTest {
 
   private static final double VALUE = 7.5;
-  private static final double VALUE_ONE = 1.0;
+  private static final int VALUE_ONE = 1;
 
   private Gram one = Gram.of(VALUE_ONE);
   private Gram subject = Gram.of(VALUE);
+
+  @Test
+  void ofNullable() {
+    assertEquals(Gram.ZERO, Gram.ofNullable(null));
+    assertEquals(subject, Gram.ofNullable(VALUE));
+  }
 
   @Test
   void plus() {
@@ -23,10 +30,12 @@ class GramTest {
   @Test
   void multiply() {
     assertEquals(Gram.of(VALUE * 2), subject.multiply(2.0));
+    assertEquals(Gram.of(VALUE * 2), subject.multiply(2));
   }
 
   @Test
   void dividedBy() {
+    assertEquals(Gram.of(VALUE / 2), subject.dividedBy(2));
     assertEquals(Gram.of(VALUE / 2), subject.dividedBy(2.0));
   }
 
@@ -41,16 +50,26 @@ class GramTest {
   @Test
   void compareTo() {
     var same = Gram.of(VALUE);
-    var less = Gram.of(VALUE - 0.000001);
+    // Within precission (100)
+    var less = Gram.of(VALUE - 0.01);
 
     assertTrue(subject.compareTo(same) == 0);
     assertTrue(less.compareTo(subject) < 0);
     assertTrue(subject.compareTo(less) > 0);
+
+    // The delta is to small (precission is 100)
+    var sameValue = Gram.of(VALUE - 0.0049);
+    assertEquals(subject, sameValue);
+    assertTrue(sameValue.compareTo(subject) == 0);
   }
 
   @Test
   void testToString() {
     assertEquals("7.5g", subject.toString());
+    assertEquals("0.01g", Gram.of(0.01).toString());
+    assertEquals("19g", Gram.of(19).toString());
+    assertEquals("1kg", Gram.of(1000).toString());
+    assertEquals("1001g", Gram.of(1001).toString());
   }
 
   @Test
@@ -59,9 +78,33 @@ class GramTest {
   }
 
   @Test
+  void asInt() {
+    assertEquals(Math.round(VALUE), subject.asInt());
+  }
+
+  @Test
   void isZero() {
     assertTrue(Gram.ZERO.isZero());
     assertTrue(Gram.of(0).isZero());
     assertFalse(subject.isZero());
+  }
+
+  @Test
+  void ofString() {
+    assertEquals(Gram.ZERO, Gram.of("0"));
+    assertEquals(Gram.ZERO, Gram.of("0g"));
+    assertEquals(Gram.ZERO, Gram.of("0kg"));
+    assertEquals(200, Gram.of("200").asInt());
+    assertEquals(200, Gram.of("200 g").asInt());
+    assertEquals(2000, Gram.of("2 kg").asInt());
+
+    var ex = assertThrows(IllegalArgumentException.class, () -> Gram.of("200.0 tonn"));
+    assertEquals("Parse error! Illegal gram value: '200.0 tonn'", ex.getMessage());
+
+    ex = assertThrows(IllegalArgumentException.class, () -> Gram.of("0x001 g"));
+    assertEquals("Parse error! Illegal gram value: '0x001 g'", ex.getMessage());
+
+    ex = assertThrows(IllegalArgumentException.class, () -> Gram.of("1 gk"));
+    assertEquals("Parse error! Illegal gram value: '1 gk'", ex.getMessage());
   }
 }
