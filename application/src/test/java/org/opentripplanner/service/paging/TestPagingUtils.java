a44d534e8805de669855d5decf66ddeb57f4394d
Ville Pihlava <ville.pihlava@cgi.com>
2025-05-07 11:09:21 +0300

    Combine regex, add comments, and change test with leading zeros.

--- application/src/test/java/org/opentripplanner/service/paging/TestPagingUtils.java
+++ application/src/test/java/org/opentripplanner/service/paging/TestPagingUtils.java
@@ -4,6 +4,10 @@ import org.opentripplanner.model.plan.ItinerarySortKey;
 
 class TestPagingUtils {
 
+  /**
+   * This method changes the format of a DateTime string for tests.
+   * It also removes leading zeros from an hh:mm string.
+   */
   static String cleanStr(Object value) {
     if (value == null) {
       return "";
@@ -13,8 +17,7 @@ class TestPagingUtils {
       .replaceAll("2020-02-01T(\\d\\d:\\d\\d):00Z", "$1-1d")
       .replaceAll("2020-02-02T(\\d\\d:\\d\\d):00Z", "$1")
       .replaceAll("2020-02-03T(\\d\\d:\\d\\d):00Z", "$1+1d")
-      .replaceAll("0(\\d:00)", "$1")
-      .replaceAll("0(\\d:01)", "$1");
+      .replaceAll("0(\\d:\\d\\d)", "$1");
   }
 
   static String toString(ItinerarySortKey it) {
