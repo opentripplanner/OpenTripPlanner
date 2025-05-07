eca30f0d81910a0759b2b8efec0fa11c1e387579
Teemu Kalvas <teemu.kalvas@hsl.fi>
2025-05-07 15:14:58 +0300

    move javadoc in SubsidiaryVertex to class from constructors, some fields can be final

--- application/src/main/java/org/opentripplanner/street/model/vertex/SubsidiaryVertex.java
+++ application/src/main/java/org/opentripplanner/street/model/vertex/SubsidiaryVertex.java
@@ -2,24 +2,23 @@ package org.opentripplanner.street.model.vertex;
 
 import org.opentripplanner.framework.i18n.I18NString;
 
+/**
+ * When applying turn restrictions to a graph makes copies of an IntersectionVertex,
+ * the original is the parent vertex and the copies are subsidiary vertices. The
+ * subsidiary vertices know their parent, and for most purposes of checking whether
+ * any two vertices are the same, you should check that the parents are the same
+ * object. A subsidiary vertex is at exactly the same geographical coordinates than
+ * the parent, it just has a different arrival history. From a given starting location
+ * and traverse mode, you can only traverse into one vertex of a group of vertices
+ * like this.
+ */
 public class SubsidiaryVertex extends IntersectionVertex {
 
-  private IntersectionVertex parent;
-  private int counter;
+  private final IntersectionVertex parent;
+  private final int counter;
 
   private static int counterSource = 0;
 
-  /**
-   * When applying turn restrictions to a graph makes copies of an IntersectionVertex,
-   * the original is the parent vertex and the copies are subsidiary vertices. The
-   * subsidiary vertices know their parent, and for most purposes of checking whether
-   * any two vertices are the same, you should check that the parents are the same
-   * object. A subsidiary vertex is at exactly the same geographical coordinates than
-   * the parent, it just has a different arrival history. From a given starting location
-   * and traverse mode, you can only traverse into one vertex of a group of vertices
-   * like this.
-   * @param parent The original IntersectionVertex which is a template for this
-   */
   public SubsidiaryVertex(IntersectionVertex parent) {
     super(
       parent.getX(),
