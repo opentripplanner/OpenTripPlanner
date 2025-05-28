f3a5dd51da80bf174ce76d1b6c2bf5032d512372
Leonard Ehrenfried <mail@leonard.io>
2025-05-28 10:42:24 +0200

    Add test for combinations

--- application/src/test/java/org/opentripplanner/apis/gtfs/mapping/routerequest/RouteRequestMapperFiltersTest.java
+++ application/src/test/java/org/opentripplanner/apis/gtfs/mapping/routerequest/RouteRequestMapperFiltersTest.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.apis.gtfs.mapping.routerequest;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -11,7 +12,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
-import org.opentripplanner.routing.api.request.RouteRequest;
 
 class RouteRequestMapperFiltersTest {
 
@@ -19,16 +19,28 @@ class RouteRequestMapperFiltersTest {
     "transit",
     Map.of("transit", List.of(Map.of("mode", "TRAM"), Map.of("mode", "FERRY")))
   );
-  private static final Map<String, Object> TRANSIT = Map.of(
+  public static final Map<String, Object> INCLUDE_ROUTE = Map.of(
+    "include",
+    List.of(Map.of("routes", List.of("f:r1")))
+  );
+  public static final Map<String, Object> EXCLUDE_AGENCY = Map.of(
+    "exclude",
+    List.of(Map.of("agencies", List.of("f:a1")))
+  );
+  private static final Map<String, Object> INCLUDE_ROUTE_FILTERS = Map.of(
     "transit",
-    Map.of("filters", List.of(Map.of("include", List.of(Map.of("routes", List.of("f:r1"))))))
+    Map.of("filters", List.of(INCLUDE_ROUTE))
+  );
+  private static final Map<String, Object> INCLUDE_ROUTE_EXCLUDE_AGENCY_FILTERS = Map.of(
+    "transit",
+    Map.of("filters", List.of(INCLUDE_ROUTE, EXCLUDE_AGENCY))
   );
 
   @Test
   void modesAndFilter() {
     var args = createArgsCopy(RouteRequestMapperTest.ARGS);
     args.put("modes", TRAM_AND_FERRY_MODES);
-    args.put("preferences", TRANSIT);
+    args.put("preferences", INCLUDE_ROUTE_FILTERS);
     assertTransitFilters(
       "[TransitFilterRequest{select: [SelectRequest{transportModes: [FERRY, TRAM]}, SelectRequest{transportModes: [], routes: [f:r1]}]}]",
       args
@@ -41,6 +53,37 @@ class RouteRequestMapperFiltersTest {
     args.put("modes", TRAM_AND_FERRY_MODES);
     assertTransitFilters(
       "[TransitFilterRequest{select: [SelectRequest{transportModes: [FERRY, TRAM]}]}]",
+      args
+    );
+  }
+
+  @Test
+  void filtersOnly() {
+    var args = createArgsCopy(RouteRequestMapperTest.ARGS);
+    args.put("preferences", INCLUDE_ROUTE_FILTERS);
+    assertTransitFilters(
+      "[TransitFilterRequest{select: [SelectRequest{transportModes: ALL-MAIN-MODES}, SelectRequest{transportModes: [], routes: [f:r1]}]}]",
+      args
+    );
+  }
+
+  @Test
+  void twoFilters() {
+    var args = createArgsCopy(RouteRequestMapperTest.ARGS);
+    args.put("preferences", INCLUDE_ROUTE_EXCLUDE_AGENCY_FILTERS);
+    assertTransitFilters(
+      "[TransitFilterRequest{select: [SelectRequest{transportModes: ALL-MAIN-MODES}, SelectRequest{transportModes: [], routes: [f:r1]}]}, TransitFilterRequest{select: [SelectRequest{transportModes: ALL-MAIN-MODES}], not: [SelectRequest{transportModes: [], agencies: [f:a1]}]}]",
+      args
+    );
+  }
+
+  @Test
+  void modesAndtwoFilters() {
+    var args = createArgsCopy(RouteRequestMapperTest.ARGS);
+    args.put("modes", TRAM_AND_FERRY_MODES);
+    args.put("preferences", INCLUDE_ROUTE_EXCLUDE_AGENCY_FILTERS);
+    assertTransitFilters(
+      "[TransitFilterRequest{select: [SelectRequest{transportModes: [FERRY, TRAM]}, SelectRequest{transportModes: [], routes: [f:r1]}]}, TransitFilterRequest{select: [SelectRequest{transportModes: [FERRY, TRAM]}], not: [SelectRequest{transportModes: [], agencies: [f:a1]}]}]",
       args
     );
   }
