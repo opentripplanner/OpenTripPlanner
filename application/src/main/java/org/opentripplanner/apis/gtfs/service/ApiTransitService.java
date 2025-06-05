af880ec73f94ff0b7888f8f05c1ccf07c3202ad6
Leonard Ehrenfried <mail@leonard.io>
2025-06-05 12:19:18 +0200

    Resolve conflict

--- application/src/main/java/org/opentripplanner/apis/gtfs/service/ApiTransitService.java
+++ application/src/main/java/org/opentripplanner/apis/gtfs/service/ApiTransitService.java
@@ -43,7 +43,7 @@ public class ApiTransitService {
       .distinct()
       .flatMap(tripPattern ->
         transitService
-          .findTripTimeOnDate(
+          .findTripTimesOnDate(
             stop,
             tripPattern,
             startTime,
