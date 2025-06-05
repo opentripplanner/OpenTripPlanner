f9673287491797230e8cd8012e76bb1cb2c10942
Leonard Ehrenfried <mail@leonard.io>
2025-06-05 14:54:50 +0200

    Re-add helper method

--- application/src/test/java/org/opentripplanner/test/support/ResourceLoader.java
+++ application/src/test/java/org/opentripplanner/test/support/ResourceLoader.java
@@ -75,6 +75,17 @@ public class ResourceLoader {
   }
 
   /**
+   * Returns the string content of a file.
+   */
+  public String fileToString(String p) {
+    try {
+      return Files.readString(file(p).toPath());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
    * Returns a File instance in the original main resources folder.
    */
   public File mainResourceFile(String path) {
