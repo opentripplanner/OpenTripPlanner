9b7b56c0abbad587214dcc800afd5b704aaddebf
Michael Tsang <michael.tsang@jnction.co.uk>
2025-06-13 10:40:30 +0100

    remove wildcard imports

--- application/src/test/java/org/opentripplanner/apis/gtfs/mapping/routerequest/StreetModeMapperTest.java
+++ application/src/test/java/org/opentripplanner/apis/gtfs/mapping/routerequest/StreetModeMapperTest.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.apis.gtfs.mapping.routerequest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
