1d5cbba8c5a3028b4e4eb8eaa42a5ec0730e0b2d
Thomas Gran <t2gran@gmail.com>
2025-06-13 15:17:45 +0200

    Apply suggestions from code review
    Co-authored-by: Leonard Ehrenfried <mail@leonard.io>

--- utils/src/main/java/org/opentripplanner/utils/collection/EnumSetUtils.java
+++ utils/src/main/java/org/opentripplanner/utils/collection/EnumSetUtils.java
@@ -8,13 +8,13 @@ import java.util.Set;
 public class EnumSetUtils {
 
   /**
-   * This is private to avoid instansiation of static utility classs.
+   * This is private to avoid instantiation of static utility class.
    */
   private EnumSetUtils() {}
 
   /**
    * Create an {@link EnumSet} wrapped using {@link Collections#unmodifiableSet(Set)}. The
-   * retuned set has almost the same efficiensy as EnumSet and is immutable, witch EnumSet is not.
+   * returned set has almost the same efficiency as EnumSet and is immutable, which EnumSet is not.
    */
   public static <E extends Enum> Set<E> unmodifiableEnumSet(Collection<E> values, Class<E> type) {
     var enumSet = values.isEmpty() ? EnumSet.noneOf(type) : EnumSet.copyOf(values);
