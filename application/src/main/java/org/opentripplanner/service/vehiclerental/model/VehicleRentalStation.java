74aa7c4549c15436a87ce5968661593aeab6fb22
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-07-01 15:15:49 +0200

    refactor: add builders and accessors to vehicle rental model classes

--- application/src/main/java/org/opentripplanner/service/vehiclerental/model/VehicleRentalStation.java
+++ application/src/main/java/org/opentripplanner/service/vehiclerental/model/VehicleRentalStation.java
@@ -5,19 +5,27 @@ import static java.util.Locale.ROOT;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.annotation.Nullable;
 import org.locationtech.jts.geom.Geometry;
 import org.opentripplanner.framework.i18n.I18NString;
 import org.opentripplanner.street.model.RentalFormFactor;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
 import org.opentripplanner.utils.collection.SetUtils;
+import org.opentripplanner.utils.tostring.ToStringBuilder;
 
 /**
  * Implements the {@link VehicleRentalPlace} class which contains Javadoc.
+ * <p>
+ * THIS CLASS IS IMMUTABLE AND THREAD-SAFE.
  */
-public class VehicleRentalStation implements VehicleRentalPlace {
+public final class VehicleRentalStation implements VehicleRentalPlace {
+
+  public static final VehicleRentalStation DEFAULT = new VehicleRentalStation();
 
   // GBFS  Static information
   public FeedScopedId id;
@@ -57,6 +65,223 @@ public class VehicleRentalStation implements VehicleRentalPlace {
   public boolean isArrivingInRentalVehicleAtDestinationAllowed = false;
   public boolean realTimeData = true;
 
+  public VehicleRentalStation() {
+    this.id = null;
+    this.name = null;
+    this.shortName = null;
+    this.longitude = 0.0;
+    this.latitude = 0.0;
+    this.address = null;
+    this.crossStreet = null;
+    this.regionId = null;
+    this.postCode = null;
+    this.rentalMethods = Set.of();
+    this.isVirtualStation = false;
+    this.stationArea = null;
+    this.capacity = null;
+    this.vehicleTypeAreaCapacity = Map.of();
+    this.vehicleTypeDockCapacity = Map.of();
+    this.isValetStation = false;
+    this.system = null;
+    this.rentalUris = null;
+    this.vehiclesAvailable = 0;
+    this.vehiclesDisabled = 0;
+    this.vehicleTypesAvailable = Map.of();
+    this.spacesAvailable = 0;
+    this.spacesDisabled = 0;
+    this.vehicleSpacesAvailable = Map.of();
+    this.isInstalled = true;
+    this.isRenting = true;
+    this.isReturning = true;
+    this.lastReported = null;
+    this.overloadingAllowed = false;
+    this.isArrivingInRentalVehicleAtDestinationAllowed = false;
+    this.realTimeData = true;
+  }
+
+  private VehicleRentalStation(Builder builder) {
+    this.id = builder.id;
+    this.name = builder.name;
+    this.shortName = builder.shortName;
+    this.longitude = builder.longitude;
+    this.latitude = builder.latitude;
+    this.address = builder.address;
+    this.crossStreet = builder.crossStreet;
+    this.regionId = builder.regionId;
+    this.postCode = builder.postCode;
+    this.rentalMethods = builder.rentalMethods != null
+      ? Set.copyOf(builder.rentalMethods)
+      : Set.of();
+    this.isVirtualStation = builder.isVirtualStation;
+    this.stationArea = builder.stationArea;
+    this.capacity = builder.capacity;
+    this.vehicleTypeAreaCapacity = builder.vehicleTypeAreaCapacity != null
+      ? Map.copyOf(builder.vehicleTypeAreaCapacity)
+      : Map.of();
+    this.vehicleTypeDockCapacity = builder.vehicleTypeDockCapacity != null
+      ? Map.copyOf(builder.vehicleTypeDockCapacity)
+      : Map.of();
+    this.isValetStation = builder.isValetStation;
+    this.system = builder.system;
+    this.rentalUris = builder.rentalUris;
+    this.vehiclesAvailable = builder.vehiclesAvailable;
+    this.vehiclesDisabled = builder.vehiclesDisabled;
+    this.vehicleTypesAvailable = builder.vehicleTypesAvailable != null
+      ? Map.copyOf(builder.vehicleTypesAvailable)
+      : Map.of();
+    this.spacesAvailable = builder.spacesAvailable;
+    this.spacesDisabled = builder.spacesDisabled;
+    this.vehicleSpacesAvailable = builder.vehicleSpacesAvailable != null
+      ? Map.copyOf(builder.vehicleSpacesAvailable)
+      : Map.of();
+    this.isInstalled = builder.isInstalled;
+    this.isRenting = builder.isRenting;
+    this.isReturning = builder.isReturning;
+    this.lastReported = builder.lastReported;
+    this.overloadingAllowed = builder.overloadingAllowed;
+    this.isArrivingInRentalVehicleAtDestinationAllowed =
+      builder.isArrivingInRentalVehicleAtDestinationAllowed;
+    this.realTimeData = builder.realTimeData;
+  }
+
+  public static Builder of() {
+    return DEFAULT.copyOf();
+  }
+
+  public Builder copyOf() {
+    return new Builder(this);
+  }
+
+  // New accessor methods following foo() convention
+  @Nullable
+  public FeedScopedId id() {
+    return id;
+  }
+
+  @Nullable
+  public I18NString name() {
+    return name;
+  }
+
+  @Nullable
+  public String shortName() {
+    return shortName;
+  }
+
+  public double longitude() {
+    return longitude;
+  }
+
+  public double latitude() {
+    return latitude;
+  }
+
+  @Nullable
+  public String address() {
+    return address;
+  }
+
+  @Nullable
+  public String crossStreet() {
+    return crossStreet;
+  }
+
+  @Nullable
+  public String regionId() {
+    return regionId;
+  }
+
+  @Nullable
+  public String postCode() {
+    return postCode;
+  }
+
+  public Set<String> rentalMethods() {
+    return rentalMethods;
+  }
+
+  public boolean isVirtualStation() {
+    return isVirtualStation;
+  }
+
+  @Nullable
+  public Geometry stationArea() {
+    return stationArea;
+  }
+
+  @Nullable
+  public Integer capacity() {
+    return capacity;
+  }
+
+  public Map<RentalVehicleType, Integer> vehicleTypeAreaCapacity() {
+    return vehicleTypeAreaCapacity;
+  }
+
+  public Map<RentalVehicleType, Integer> vehicleTypeDockCapacity() {
+    return vehicleTypeDockCapacity;
+  }
+
+  public boolean isValetStation() {
+    return isValetStation;
+  }
+
+  @Nullable
+  public VehicleRentalSystem system() {
+    return system;
+  }
+
+  @Nullable
+  public VehicleRentalStationUris rentalUris() {
+    return rentalUris;
+  }
+
+  public int vehiclesAvailable() {
+    return vehiclesAvailable;
+  }
+
+  public int vehiclesDisabled() {
+    return vehiclesDisabled;
+  }
+
+  public Map<RentalVehicleType, Integer> vehicleTypesAvailable() {
+    return vehicleTypesAvailable;
+  }
+
+  public int spacesAvailable() {
+    return spacesAvailable;
+  }
+
+  public int spacesDisabled() {
+    return spacesDisabled;
+  }
+
+  public Map<RentalVehicleType, Integer> vehicleSpacesAvailable() {
+    return vehicleSpacesAvailable;
+  }
+
+  public boolean isInstalled() {
+    return isInstalled;
+  }
+
+  public boolean isRenting() {
+    return isRenting;
+  }
+
+  public boolean isReturning() {
+    return isReturning;
+  }
+
+  @Nullable
+  public Instant lastReported() {
+    return lastReported;
+  }
+
+  public boolean realTimeData() {
+    return realTimeData;
+  }
+
+  // Legacy interface methods - these delegate to the new accessors
   @Override
   public FeedScopedId getId() {
     return id;
@@ -108,11 +333,6 @@ public class VehicleRentalStation implements VehicleRentalPlace {
   }
 
   @Override
-  public boolean overloadingAllowed() {
-    return overloadingAllowed;
-  }
-
-  @Override
   public boolean isAllowPickup() {
     return isRenting;
   }
@@ -161,6 +381,11 @@ public class VehicleRentalStation implements VehicleRentalPlace {
   @Override
   public boolean isArrivingInRentalVehicleAtDestinationAllowed() {
     return isArrivingInRentalVehicleAtDestinationAllowed;
+  }
+
+  @Override
+  public boolean overloadingAllowed() {
+    return overloadingAllowed;
   }
 
   @Override
@@ -225,5 +450,335 @@ public class VehicleRentalStation implements VehicleRentalPlace {
       .stream()
       .map(vtc -> new RentalVehicleTypeCount(vtc.getKey(), vtc.getValue()))
       .toList();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    VehicleRentalStation that = (VehicleRentalStation) o;
+    return (
+      Double.compare(that.longitude, longitude) == 0 &&
+      Double.compare(that.latitude, latitude) == 0 &&
+      isVirtualStation == that.isVirtualStation &&
+      isValetStation == that.isValetStation &&
+      vehiclesAvailable == that.vehiclesAvailable &&
+      vehiclesDisabled == that.vehiclesDisabled &&
+      spacesAvailable == that.spacesAvailable &&
+      spacesDisabled == that.spacesDisabled &&
+      isInstalled == that.isInstalled &&
+      isRenting == that.isRenting &&
+      isReturning == that.isReturning &&
+      overloadingAllowed == that.overloadingAllowed &&
+      isArrivingInRentalVehicleAtDestinationAllowed ==
+      that.isArrivingInRentalVehicleAtDestinationAllowed &&
+      realTimeData == that.realTimeData &&
+      Objects.equals(id, that.id) &&
+      Objects.equals(name, that.name) &&
+      Objects.equals(shortName, that.shortName) &&
+      Objects.equals(address, that.address) &&
+      Objects.equals(crossStreet, that.crossStreet) &&
+      Objects.equals(regionId, that.regionId) &&
+      Objects.equals(postCode, that.postCode) &&
+      Objects.equals(rentalMethods, that.rentalMethods) &&
+      Objects.equals(stationArea, that.stationArea) &&
+      Objects.equals(capacity, that.capacity) &&
+      Objects.equals(vehicleTypeAreaCapacity, that.vehicleTypeAreaCapacity) &&
+      Objects.equals(vehicleTypeDockCapacity, that.vehicleTypeDockCapacity) &&
+      Objects.equals(system, that.system) &&
+      Objects.equals(rentalUris, that.rentalUris) &&
+      Objects.equals(vehicleTypesAvailable, that.vehicleTypesAvailable) &&
+      Objects.equals(vehicleSpacesAvailable, that.vehicleSpacesAvailable) &&
+      Objects.equals(lastReported, that.lastReported)
+    );
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(
+      id,
+      name,
+      shortName,
+      longitude,
+      latitude,
+      address,
+      crossStreet,
+      regionId,
+      postCode,
+      rentalMethods,
+      isVirtualStation,
+      stationArea,
+      capacity,
+      vehicleTypeAreaCapacity,
+      vehicleTypeDockCapacity,
+      isValetStation,
+      system,
+      rentalUris,
+      vehiclesAvailable,
+      vehiclesDisabled,
+      vehicleTypesAvailable,
+      spacesAvailable,
+      spacesDisabled,
+      vehicleSpacesAvailable,
+      isInstalled,
+      isRenting,
+      isReturning,
+      lastReported,
+      overloadingAllowed,
+      isArrivingInRentalVehicleAtDestinationAllowed,
+      realTimeData
+    );
+  }
+
+  public static class Builder {
+
+    private final VehicleRentalStation original;
+    private FeedScopedId id;
+    private I18NString name;
+    private String shortName;
+    private double longitude;
+    private double latitude;
+    private String address;
+    private String crossStreet;
+    private String regionId;
+    private String postCode;
+    private Set<String> rentalMethods;
+    private boolean isVirtualStation;
+    private Geometry stationArea;
+    private Integer capacity;
+    private Map<RentalVehicleType, Integer> vehicleTypeAreaCapacity;
+    private Map<RentalVehicleType, Integer> vehicleTypeDockCapacity;
+    private boolean isValetStation;
+    private VehicleRentalSystem system;
+    private VehicleRentalStationUris rentalUris;
+    private int vehiclesAvailable;
+    private int vehiclesDisabled;
+    private Map<RentalVehicleType, Integer> vehicleTypesAvailable;
+    private int spacesAvailable;
+    private int spacesDisabled;
+    private Map<RentalVehicleType, Integer> vehicleSpacesAvailable;
+    private boolean isInstalled;
+    private boolean isRenting;
+    private boolean isReturning;
+    private Instant lastReported;
+    private boolean overloadingAllowed;
+    private boolean isArrivingInRentalVehicleAtDestinationAllowed;
+    private boolean realTimeData;
+
+    private Builder(VehicleRentalStation original) {
+      this.original = original;
+      this.id = original.id;
+      this.name = original.name;
+      this.shortName = original.shortName;
+      this.longitude = original.longitude;
+      this.latitude = original.latitude;
+      this.address = original.address;
+      this.crossStreet = original.crossStreet;
+      this.regionId = original.regionId;
+      this.postCode = original.postCode;
+      this.rentalMethods = original.rentalMethods;
+      this.isVirtualStation = original.isVirtualStation;
+      this.stationArea = original.stationArea;
+      this.capacity = original.capacity;
+      this.vehicleTypeAreaCapacity = original.vehicleTypeAreaCapacity;
+      this.vehicleTypeDockCapacity = original.vehicleTypeDockCapacity;
+      this.isValetStation = original.isValetStation;
+      this.system = original.system;
+      this.rentalUris = original.rentalUris;
+      this.vehiclesAvailable = original.vehiclesAvailable;
+      this.vehiclesDisabled = original.vehiclesDisabled;
+      this.vehicleTypesAvailable = original.vehicleTypesAvailable;
+      this.spacesAvailable = original.spacesAvailable;
+      this.spacesDisabled = original.spacesDisabled;
+      this.vehicleSpacesAvailable = original.vehicleSpacesAvailable;
+      this.isInstalled = original.isInstalled;
+      this.isRenting = original.isRenting;
+      this.isReturning = original.isReturning;
+      this.lastReported = original.lastReported;
+      this.overloadingAllowed = original.overloadingAllowed;
+      this.isArrivingInRentalVehicleAtDestinationAllowed =
+        original.isArrivingInRentalVehicleAtDestinationAllowed;
+      this.realTimeData = original.realTimeData;
+    }
+
+    public VehicleRentalStation original() {
+      return original;
+    }
+
+    public Builder withId(@Nullable FeedScopedId id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder withName(@Nullable I18NString name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder withShortName(@Nullable String shortName) {
+      this.shortName = shortName;
+      return this;
+    }
+
+    public Builder withLongitude(double longitude) {
+      this.longitude = longitude;
+      return this;
+    }
+
+    public Builder withLatitude(double latitude) {
+      this.latitude = latitude;
+      return this;
+    }
+
+    public Builder withAddress(@Nullable String address) {
+      this.address = address;
+      return this;
+    }
+
+    public Builder withCrossStreet(@Nullable String crossStreet) {
+      this.crossStreet = crossStreet;
+      return this;
+    }
+
+    public Builder withRegionId(@Nullable String regionId) {
+      this.regionId = regionId;
+      return this;
+    }
+
+    public Builder withPostCode(@Nullable String postCode) {
+      this.postCode = postCode;
+      return this;
+    }
+
+    public Builder withRentalMethods(@Nullable Set<String> rentalMethods) {
+      this.rentalMethods = rentalMethods;
+      return this;
+    }
+
+    public Builder withIsVirtualStation(boolean isVirtualStation) {
+      this.isVirtualStation = isVirtualStation;
+      return this;
+    }
+
+    public Builder withStationArea(@Nullable Geometry stationArea) {
+      this.stationArea = stationArea;
+      return this;
+    }
+
+    public Builder withCapacity(@Nullable Integer capacity) {
+      this.capacity = capacity;
+      return this;
+    }
+
+    public Builder withVehicleTypeAreaCapacity(
+      @Nullable Map<RentalVehicleType, Integer> vehicleTypeAreaCapacity
+    ) {
+      this.vehicleTypeAreaCapacity = vehicleTypeAreaCapacity;
+      return this;
+    }
+
+    public Builder withVehicleTypeDockCapacity(
+      @Nullable Map<RentalVehicleType, Integer> vehicleTypeDockCapacity
+    ) {
+      this.vehicleTypeDockCapacity = vehicleTypeDockCapacity;
+      return this;
+    }
+
+    public Builder withIsValetStation(boolean isValetStation) {
+      this.isValetStation = isValetStation;
+      return this;
+    }
+
+    public Builder withSystem(@Nullable VehicleRentalSystem system) {
+      this.system = system;
+      return this;
+    }
+
+    public Builder withRentalUris(@Nullable VehicleRentalStationUris rentalUris) {
+      this.rentalUris = rentalUris;
+      return this;
+    }
+
+    public Builder withVehiclesAvailable(int vehiclesAvailable) {
+      this.vehiclesAvailable = vehiclesAvailable;
+      return this;
+    }
+
+    public Builder withVehiclesDisabled(int vehiclesDisabled) {
+      this.vehiclesDisabled = vehiclesDisabled;
+      return this;
+    }
+
+    public Builder withVehicleTypesAvailable(
+      @Nullable Map<RentalVehicleType, Integer> vehicleTypesAvailable
+    ) {
+      this.vehicleTypesAvailable = vehicleTypesAvailable;
+      return this;
+    }
+
+    public Builder withSpacesAvailable(int spacesAvailable) {
+      this.spacesAvailable = spacesAvailable;
+      return this;
+    }
+
+    public Builder withSpacesDisabled(int spacesDisabled) {
+      this.spacesDisabled = spacesDisabled;
+      return this;
+    }
+
+    public Builder withVehicleSpacesAvailable(
+      @Nullable Map<RentalVehicleType, Integer> vehicleSpacesAvailable
+    ) {
+      this.vehicleSpacesAvailable = vehicleSpacesAvailable;
+      return this;
+    }
+
+    public Builder withIsInstalled(boolean isInstalled) {
+      this.isInstalled = isInstalled;
+      return this;
+    }
+
+    public Builder withIsRenting(boolean isRenting) {
+      this.isRenting = isRenting;
+      return this;
+    }
+
+    public Builder withIsReturning(boolean isReturning) {
+      this.isReturning = isReturning;
+      return this;
+    }
+
+    public Builder withLastReported(@Nullable Instant lastReported) {
+      this.lastReported = lastReported;
+      return this;
+    }
+
+    public Builder withOverloadingAllowed(boolean overloadingAllowed) {
+      this.overloadingAllowed = overloadingAllowed;
+      return this;
+    }
+
+    public Builder withIsArrivingInRentalVehicleAtDestinationAllowed(
+      boolean isArrivingInRentalVehicleAtDestinationAllowed
+    ) {
+      this.isArrivingInRentalVehicleAtDestinationAllowed =
+        isArrivingInRentalVehicleAtDestinationAllowed;
+      return this;
+    }
+
+    public Builder withRealTimeData(boolean realTimeData) {
+      this.realTimeData = realTimeData;
+      return this;
+    }
+
+    public Builder apply(Consumer<Builder> body) {
+      body.accept(this);
+      return this;
+    }
+
+    public VehicleRentalStation build() {
+      var value = new VehicleRentalStation(this);
+      return original.equals(value) ? original : value;
+    }
   }
 }
--- application/src/main/java/org/opentripplanner/service/vehiclerental/model/VehicleRentalVehicle.java
+++ application/src/main/java/org/opentripplanner/service/vehiclerental/model/VehicleRentalVehicle.java
@@ -2,15 +2,23 @@ package org.opentripplanner.service.vehiclerental.model;
 
 import java.time.Instant;
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.Consumer;
+import javax.annotation.Nullable;
 import org.opentripplanner.framework.i18n.I18NString;
 import org.opentripplanner.street.model.RentalFormFactor;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
+import org.opentripplanner.utils.tostring.ToStringBuilder;
 
 /**
  * Implements the {@link VehicleRentalPlace} class which contains Javadoc.
+ * <p>
+ * THIS CLASS IS IMMUTABLE AND THREAD-SAFE.
  */
-public class VehicleRentalVehicle implements VehicleRentalPlace {
+public final class VehicleRentalVehicle implements VehicleRentalPlace {
+
+  public static final VehicleRentalVehicle DEFAULT = new VehicleRentalVehicle();
 
   public FeedScopedId id;
   public I18NString name;
@@ -27,6 +35,115 @@ public class VehicleRentalVehicle implements VehicleRentalPlace {
   public String pricingPlanId;
   public RentalVehicleFuel fuel;
   public OffsetDateTime availableUntil;
+
+  public VehicleRentalVehicle() {
+    this.id = null;
+    this.name = null;
+    this.longitude = 0.0;
+    this.latitude = 0.0;
+    this.system = null;
+    this.vehicleType = null;
+    this.rentalUris = null;
+    this.isReserved = false;
+    this.isDisabled = false;
+    this.lastReported = null;
+    this.station = null;
+    this.pricingPlanId = null;
+    this.fuel = null;
+    this.availableUntil = null;
+  }
+
+  private VehicleRentalVehicle(Builder builder) {
+    this.id = builder.id;
+    this.name = builder.name;
+    this.longitude = builder.longitude;
+    this.latitude = builder.latitude;
+    this.system = builder.system;
+    this.vehicleType = builder.vehicleType;
+    this.rentalUris = builder.rentalUris;
+    this.isReserved = builder.isReserved;
+    this.isDisabled = builder.isDisabled;
+    this.lastReported = builder.lastReported;
+    this.station = builder.station;
+    this.pricingPlanId = builder.pricingPlanId;
+    this.fuel = builder.fuel;
+    this.availableUntil = builder.availableUntil;
+  }
+
+  public static Builder of() {
+    return DEFAULT.copyOf();
+  }
+
+  public Builder copyOf() {
+    return new Builder(this);
+  }
+
+  // New accessor methods following foo() convention
+  @Nullable
+  public FeedScopedId id() {
+    return id;
+  }
+
+  @Nullable
+  public I18NString name() {
+    return name;
+  }
+
+  public double longitude() {
+    return longitude;
+  }
+
+  public double latitude() {
+    return latitude;
+  }
+
+  @Nullable
+  public VehicleRentalSystem system() {
+    return system;
+  }
+
+  @Nullable
+  public RentalVehicleType vehicleType() {
+    return vehicleType;
+  }
+
+  @Nullable
+  public VehicleRentalStationUris rentalUris() {
+    return rentalUris;
+  }
+
+  public boolean isReserved() {
+    return isReserved;
+  }
+
+  public boolean isDisabled() {
+    return isDisabled;
+  }
+
+  @Nullable
+  public Instant lastReported() {
+    return lastReported;
+  }
+
+  @Nullable
+  public VehicleRentalStation station() {
+    return station;
+  }
+
+  @Nullable
+  public String pricingPlanId() {
+    return pricingPlanId;
+  }
+
+  @Nullable
+  public RentalVehicleFuel fuel() {
+    return fuel;
+  }
+
+  @Nullable
+  public OffsetDateTime availableUntil() {
+    return availableUntil;
+  }
 
   @Override
   public FeedScopedId getId() {
@@ -142,5 +259,189 @@ public class VehicleRentalVehicle implements VehicleRentalPlace {
 
   public OffsetDateTime getAvailableUntil() {
     return availableUntil;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    VehicleRentalVehicle that = (VehicleRentalVehicle) o;
+    return (
+      Double.compare(that.longitude, longitude) == 0 &&
+      Double.compare(that.latitude, latitude) == 0 &&
+      isReserved == that.isReserved &&
+      isDisabled == that.isDisabled &&
+      Objects.equals(id, that.id) &&
+      Objects.equals(name, that.name) &&
+      Objects.equals(system, that.system) &&
+      Objects.equals(vehicleType, that.vehicleType) &&
+      Objects.equals(rentalUris, that.rentalUris) &&
+      Objects.equals(lastReported, that.lastReported) &&
+      Objects.equals(station, that.station) &&
+      Objects.equals(pricingPlanId, that.pricingPlanId) &&
+      Objects.equals(fuel, that.fuel) &&
+      Objects.equals(availableUntil, that.availableUntil)
+    );
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(
+      id,
+      name,
+      longitude,
+      latitude,
+      system,
+      vehicleType,
+      rentalUris,
+      isReserved,
+      isDisabled,
+      lastReported,
+      station,
+      pricingPlanId,
+      fuel,
+      availableUntil
+    );
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.of(VehicleRentalVehicle.class)
+      .addObj("id", id, DEFAULT.id)
+      .addObj("name", name, DEFAULT.name)
+      .addNum("longitude", longitude, DEFAULT.longitude)
+      .addNum("latitude", latitude, DEFAULT.latitude)
+      .addObj("system", system, DEFAULT.system)
+      .addObj("vehicleType", vehicleType, DEFAULT.vehicleType)
+      .addObj("rentalUris", rentalUris, DEFAULT.rentalUris)
+      .addBoolIfTrue("isReserved", isReserved)
+      .addBoolIfTrue("isDisabled", isDisabled)
+      .addObj("lastReported", lastReported, DEFAULT.lastReported)
+      .addObj("station", station, DEFAULT.station)
+      .addStr("pricingPlanId", pricingPlanId, DEFAULT.pricingPlanId)
+      .addObj("fuel", fuel, DEFAULT.fuel)
+      .addObj("availableUntil", availableUntil, DEFAULT.availableUntil)
+      .toString();
+  }
+
+  public static class Builder {
+
+    private final VehicleRentalVehicle original;
+    private FeedScopedId id;
+    private I18NString name;
+    private double longitude;
+    private double latitude;
+    private VehicleRentalSystem system;
+    private RentalVehicleType vehicleType;
+    private VehicleRentalStationUris rentalUris;
+    private boolean isReserved;
+    private boolean isDisabled;
+    private Instant lastReported;
+    private VehicleRentalStation station;
+    private String pricingPlanId;
+    private RentalVehicleFuel fuel;
+    private OffsetDateTime availableUntil;
+
+    private Builder(VehicleRentalVehicle original) {
+      this.original = original;
+      this.id = original.id;
+      this.name = original.name;
+      this.longitude = original.longitude;
+      this.latitude = original.latitude;
+      this.system = original.system;
+      this.vehicleType = original.vehicleType;
+      this.rentalUris = original.rentalUris;
+      this.isReserved = original.isReserved;
+      this.isDisabled = original.isDisabled;
+      this.lastReported = original.lastReported;
+      this.station = original.station;
+      this.pricingPlanId = original.pricingPlanId;
+      this.fuel = original.fuel;
+      this.availableUntil = original.availableUntil;
+    }
+
+    public VehicleRentalVehicle original() {
+      return original;
+    }
+
+    public Builder withId(@Nullable FeedScopedId id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder withName(@Nullable I18NString name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder withLongitude(double longitude) {
+      this.longitude = longitude;
+      return this;
+    }
+
+    public Builder withLatitude(double latitude) {
+      this.latitude = latitude;
+      return this;
+    }
+
+    public Builder withSystem(@Nullable VehicleRentalSystem system) {
+      this.system = system;
+      return this;
+    }
+
+    public Builder withVehicleType(@Nullable RentalVehicleType vehicleType) {
+      this.vehicleType = vehicleType;
+      return this;
+    }
+
+    public Builder withRentalUris(@Nullable VehicleRentalStationUris rentalUris) {
+      this.rentalUris = rentalUris;
+      return this;
+    }
+
+    public Builder withIsReserved(boolean isReserved) {
+      this.isReserved = isReserved;
+      return this;
+    }
+
+    public Builder withIsDisabled(boolean isDisabled) {
+      this.isDisabled = isDisabled;
+      return this;
+    }
+
+    public Builder withLastReported(@Nullable Instant lastReported) {
+      this.lastReported = lastReported;
+      return this;
+    }
+
+    public Builder withStation(@Nullable VehicleRentalStation station) {
+      this.station = station;
+      return this;
+    }
+
+    public Builder withPricingPlanId(@Nullable String pricingPlanId) {
+      this.pricingPlanId = pricingPlanId;
+      return this;
+    }
+
+    public Builder withFuel(@Nullable RentalVehicleFuel fuel) {
+      this.fuel = fuel;
+      return this;
+    }
+
+    public Builder withAvailableUntil(@Nullable OffsetDateTime availableUntil) {
+      this.availableUntil = availableUntil;
+      return this;
+    }
+
+    public Builder apply(Consumer<Builder> body) {
+      body.accept(this);
+      return this;
+    }
+
+    public VehicleRentalVehicle build() {
+      var value = new VehicleRentalVehicle(this);
+      return original.equals(value) ? original : value;
+    }
   }
 }
