db9b05f3fa0023efaa12af2335f3fae22bdf832a
Michael Tsang <michael@aubin.app>
2025-07-03 17:36:18 +0100

    remove unused imports

--- application/src/main/java/org/opentripplanner/updater/trip/gtfs/TripTimesUpdater.java
+++ application/src/main/java/org/opentripplanner/updater/trip/gtfs/TripTimesUpdater.java
@@ -11,10 +11,8 @@ import static org.opentripplanner.updater.spi.UpdateError.UpdateErrorType.TRIP_N
 import com.google.transit.realtime.GtfsRealtime;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import org.opentripplanner.framework.i18n.I18NString;
 import org.opentripplanner.gtfs.mapping.PickDropMapper;
