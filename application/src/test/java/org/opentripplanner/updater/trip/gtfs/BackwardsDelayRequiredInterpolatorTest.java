772ae516339d514025440e445958526101d676b0
Leonard Ehrenfried <mail@leonard.io>
2025-07-17 10:20:05 +0200

    Merge upstream code

--- application/src/test/java/org/opentripplanner/updater/trip/gtfs/BackwardsDelayRequiredInterpolatorTest.java
+++ application/src/test/java/org/opentripplanner/updater/trip/gtfs/BackwardsDelayRequiredInterpolatorTest.java
@@ -8,16 +8,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.opentripplanner.transit.model._data.TimetableRepositoryForTest;
 import org.opentripplanner.transit.model.framework.Deduplicator;
-import org.opentripplanner.transit.model.timetable.ScheduledTripTimes;
 import org.opentripplanner.transit.model.timetable.StopRealTimeState;
 import org.opentripplanner.transit.model.timetable.Trip;
+import org.opentripplanner.transit.model.timetable.TripTimes;
 import org.opentripplanner.transit.model.timetable.TripTimesFactory;
 
 class BackwardsDelayRequiredInterpolatorTest {
 
   static final Trip TRIP = TimetableRepositoryForTest.trip("TRIP_ID").build();
   public static final int STOP_COUNT = 5;
-  static final ScheduledTripTimes SCHEDULED_TRIP_TIMES = TripTimesFactory.tripTimes(
+  static final TripTimes SCHEDULED_TRIP_TIMES = TripTimesFactory.tripTimes(
     TRIP,
     TimetableRepositoryForTest.of().stopTimesEvery5Minutes(STOP_COUNT, TRIP, "00:00"),
     new Deduplicator()
