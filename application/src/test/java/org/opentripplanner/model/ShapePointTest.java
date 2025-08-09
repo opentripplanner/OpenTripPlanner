4e57b3603a16fdb834c7ccc6397768ef2ba28040
Leonard Ehrenfried <mail@leonard.io>
2025-08-09 11:43:33 +0200

    Add test for ShapePoint#equals

---
+++ application/src/test/java/org/opentripplanner/model/ShapePointTest.java
@@ -0,0 +1,12 @@
+package org.opentripplanner.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShapePointTest {
+
+  @Test
+  void testEquals() {
+  }
+}
