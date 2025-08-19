2eddede3ce77640964819aa0bb5a910a5df2b348
Leonard Ehrenfried <mail@leonard.io>
2025-08-19 17:58:26 +0200

    Remove separate large area tracker

--- application/src/ext/java/org/opentripplanner/ext/flex/AreaStopsToVerticesMapper.java
+++ application/src/ext/java/org/opentripplanner/ext/flex/AreaStopsToVerticesMapper.java
@@ -1,15 +1,12 @@
 package org.opentripplanner.ext.flex;
 
 import jakarta.inject.Inject;
-import java.util.Collection;
 import java.util.List;
-import javax.annotation.Nullable;
 import org.locationtech.jts.geom.Point;
 import org.opentripplanner.framework.geometry.GeometryUtils;
 import org.opentripplanner.graph_builder.model.GraphBuilderModule;
 import org.opentripplanner.routing.graph.Graph;
 import org.opentripplanner.street.model.vertex.StreetVertex;
-import org.opentripplanner.street.model.vertex.Vertex;
 import org.opentripplanner.transit.model.site.AreaStop;
 import org.opentripplanner.transit.service.TimetableRepository;
 import org.opentripplanner.utils.logging.ProgressTracker;
@@ -23,11 +20,6 @@ import org.slf4j.LoggerFactory;
 public class AreaStopsToVerticesMapper implements GraphBuilderModule {
 
   private static final Logger LOG = LoggerFactory.getLogger(AreaStopsToVerticesMapper.class);
-  /**
-   * The number of vertices that above which an area is considered "large" and therefore will
-   * have a progress tracker about processing the geometry.
-   */
-  private static final int LARGE_AREA_LIMIT = 30_000;
 
   private final Graph graph;
   private final TimetableRepository timetableRepository;
@@ -69,9 +61,6 @@ public class AreaStopsToVerticesMapper implements GraphBuilderModule {
   private static void applyAreaStopToVertices(AreaStop areaStop, Graph graph) {
     var vertices = graph.findVertices(areaStop.getGeometry().getEnvelopeInternal());
 
-    @Nullable
-    var progress = progressTracker(areaStop, vertices);
-
     var geometryFactory = GeometryUtils.getGeometryFactory();
 
     vertices
@@ -83,34 +72,9 @@ public class AreaStopsToVerticesMapper implements GraphBuilderModule {
         // The street index overselects, so need to check for exact geometry inclusion
         Point p = geometryFactory.createPoint(vertx.getCoordinate());
         var intersects = areaStop.getGeometry().intersects(p);
-        if (progress != null) {
-          // Keep lambda! A method-ref would cause incorrect class and line number to be logged
-          progress.step(m -> LOG.info(m));
-        }
         if (intersects) {
           vertx.addAreaStops(List.of(areaStop));
         }
       });
-    if (progress != null) {
-      LOG.info(progress.completeMessage());
-    }
-  }
-
-  /**
-   * We want a vertex progress tracker only for large areas, otherwise it becomes too noisy.
-   */
-  @Nullable
-  private static ProgressTracker progressTracker(AreaStop areaStop, Collection<Vertex> vertices) {
-    if (vertices.size() < LARGE_AREA_LIMIT) {
-      return null;
-    } else {
-      var progress = ProgressTracker.track(
-        "Computing vertices of area stop %s".formatted(areaStop.getId()),
-        10000,
-        vertices.size()
-      );
-      LOG.info(progress.startMessage());
-      return progress;
-    }
   }
 }
