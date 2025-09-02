9276250fc54dbed249285fd266ee0ad5e83f0c3f
Ville Pihlava <ville.pihlava@cgi.com>
2025-09-02 12:20:43 +0300

    Remove id from VerticalTransportationUse.

--- application/src/main/java/org/opentripplanner/model/plan/walkstep/VerticalTransportationUse.java
+++ application/src/main/java/org/opentripplanner/model/plan/walkstep/VerticalTransportationUse.java
@@ -2,7 +2,6 @@ package org.opentripplanner.model.plan.walkstep;
 
 import javax.annotation.Nullable;
 import org.opentripplanner.framework.i18n.I18NString;
-import org.opentripplanner.transit.model.framework.FeedScopedId;
 
 /**
  * Represents information about vertical transportation equipment stored in
@@ -10,7 +9,6 @@ import org.opentripplanner.transit.model.framework.FeedScopedId;
  */
 public record VerticalTransportationUse(
   @Nullable Double toLevel,
-  FeedScopedId id,
   @Nullable I18NString name,
   @Nullable Double fromLevel,
   VerticalTransportationType type
