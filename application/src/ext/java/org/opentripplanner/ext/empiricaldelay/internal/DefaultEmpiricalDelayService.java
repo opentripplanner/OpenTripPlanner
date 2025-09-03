5c9569485b413a93aaf4e361da25dbb4b0e2dce7
Thomas Gran <t2gran@gmail.com>
2025-09-03 22:02:46 +0200

    feat: Add EmpiricalDelayService

---
+++ application/src/ext/java/org/opentripplanner/ext/empiricaldelay/internal/DefaultEmpiricalDelayService.java
@@ -0,0 +1,26 @@
+package org.opentripplanner.ext.empiricaldelay.internal;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import org.opentripplanner.ext.empiricaldelay.EmpiricalDelayRepository;
+import org.opentripplanner.ext.empiricaldelay.EmpiricalDelayService;
+import org.opentripplanner.ext.empiricaldelay.model.EmpiricalDelay;
+import org.opentripplanner.transit.model.framework.FeedScopedId;
+
+public class DefaultEmpiricalDelayService implements EmpiricalDelayService {
+
+  private final EmpiricalDelayRepository repository;
+
+  public DefaultEmpiricalDelayService(EmpiricalDelayRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public Optional<EmpiricalDelay> findEmpiricalDelay(
+    FeedScopedId tripId,
+    LocalDate serviceDate,
+    int stopPosInPattern
+  ) {
+    return repository.findEmpiricalDelay(tripId, serviceDate, stopPosInPattern);
+  }
+}
