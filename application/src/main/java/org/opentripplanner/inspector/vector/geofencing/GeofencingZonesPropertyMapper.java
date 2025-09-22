af20c344edbaea7d4033cf3212c979a31a121851
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-09-22 14:51:53 +0200

    Use kv shorthand

--- application/src/main/java/org/opentripplanner/inspector/vector/geofencing/GeofencingZonesPropertyMapper.java
+++ application/src/main/java/org/opentripplanner/inspector/vector/geofencing/GeofencingZonesPropertyMapper.java
@@ -1,5 +1,7 @@
 package org.opentripplanner.inspector.vector.geofencing;
 
+import static org.opentripplanner.inspector.vector.KeyValue.kv;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import org.opentripplanner.apis.support.mapping.PropertyMapper;
@@ -15,16 +17,16 @@ public class GeofencingZonesPropertyMapper extends PropertyMapper<GeofencingZone
   protected Collection<KeyValue> map(GeofencingZone zone) {
     var properties = new ArrayList<KeyValue>();
 
-    properties.add(new KeyValue("id", zone.id().toString()));
-    properties.add(new KeyValue("name", zone.name().toString()));
-    properties.add(new KeyValue("network", zone.id().getFeedId()));
+    properties.add(kv("id", zone.id()));
+    properties.add(kv("name", zone.name()));
+    properties.add(kv("network", zone.id().getFeedId()));
 
     if (zone.isBusinessArea()) {
-      properties.add(new KeyValue("type", "business-area"));
+      properties.add(kv("type", "business-area"));
     } else if (zone.traversalBanned()) {
-      properties.add(new KeyValue("type", "no-traversal"));
+      properties.add(kv("type", "no-traversal"));
     } else if (zone.dropOffBanned()) {
-      properties.add(new KeyValue("type", "no-drop-off"));
+      properties.add(kv("type", "no-drop-off"));
     }
 
     return properties;
