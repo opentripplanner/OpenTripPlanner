b33fd59cc79e615feb0fa3087f56aade6b192264
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-09-22 20:22:44 +0200

    Formatting

--- application/src/main/java/org/opentripplanner/inspector/vector/KeyValue.java
+++ application/src/main/java/org/opentripplanner/inspector/vector/KeyValue.java
@@ -26,7 +26,9 @@ public record KeyValue(String key, Object value) {
   public static KeyValue kv(String key, @Nullable Object value) {
     if (value == null) {
       return new KeyValue(key, null);
-    } else if (value instanceof FeedScopedId || value instanceof Enum<?> || value instanceof I18NString) {
+    } else if (
+      value instanceof FeedScopedId || value instanceof Enum<?> || value instanceof I18NString
+    ) {
       return new KeyValue(key, value.toString());
     } else {
       return new KeyValue(key, value);
