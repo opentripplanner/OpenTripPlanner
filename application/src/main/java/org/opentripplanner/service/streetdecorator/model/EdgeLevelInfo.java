bab8499d79faf642e73668badb63b43ab7cb6f41
Ville Pihlava <ville.pihlava@cgi.com>
2025-09-23 15:25:54 +0300

    Add matchesNodes function.

--- application/src/main/java/org/opentripplanner/service/streetdecorator/model/EdgeLevelInfo.java
+++ application/src/main/java/org/opentripplanner/service/streetdecorator/model/EdgeLevelInfo.java
@@ -1,3 +1,12 @@
 package org.opentripplanner.service.streetdecorator.model;
 
-public record EdgeLevelInfo(VertexLevelInfo lowerVertexInfo, VertexLevelInfo upperVertexInfo) {}
+public record EdgeLevelInfo(VertexLevelInfo lowerVertexInfo, VertexLevelInfo upperVertexInfo) {
+  public boolean matchesNodes(long firstOsmVertexId, long secondOsmVertexId) {
+    return (
+      (lowerVertexInfo.osmVertexId() == firstOsmVertexId &&
+        upperVertexInfo.osmVertexId() == secondOsmVertexId) ||
+      (lowerVertexInfo.osmVertexId() == secondOsmVertexId &&
+        upperVertexInfo.osmVertexId() == firstOsmVertexId)
+    );
+  }
+}
