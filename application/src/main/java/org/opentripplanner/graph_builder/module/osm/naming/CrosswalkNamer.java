21e3289546dbc5b976ce5ceb46a9e54c9696d8e1
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-09-29 17:26:51 -0400

    feat(CrosswalkNamer): Implement basic crosswalk naming.

--- application/src/main/java/org/opentripplanner/graph_builder/module/osm/naming/CrosswalkNamer.java
+++ application/src/main/java/org/opentripplanner/graph_builder/module/osm/naming/CrosswalkNamer.java
@@ -1,14 +1,23 @@
 package org.opentripplanner.graph_builder.module.osm.naming;
 
+import static org.opentripplanner.osm.model.TraverseDirection.FORWARD;
+
 import gnu.trove.list.TLongList;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.opentripplanner.framework.i18n.I18NString;
 import org.opentripplanner.graph_builder.module.osm.StreetEdgePair;
 import org.opentripplanner.graph_builder.services.osm.EdgeNamer;
 import org.opentripplanner.osm.model.OsmEntity;
 import org.opentripplanner.osm.model.OsmWay;
+import org.opentripplanner.osm.model.TraverseDirection;
+import org.opentripplanner.street.model.edge.StreetEdge;
+import org.opentripplanner.utils.lang.DoubleUtils;
+import org.opentripplanner.utils.logging.ProgressTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,6 +36,9 @@ public class CrosswalkNamer implements EdgeNamer {
 
   private static final Logger LOG = LoggerFactory.getLogger(CrosswalkNamer.class);
 
+  private Collection<OsmWay> streets = new ArrayList<>();
+  private Collection<EdgeOnLevel> unnamedCrosswalks = new ArrayList<>();
+
   @Override
   public I18NString name(OsmEntity way) {
     return way.getAssumedName();
@@ -34,16 +46,88 @@ public class CrosswalkNamer implements EdgeNamer {
 
   @Override
   public void recordEdges(OsmEntity way, StreetEdgePair pair) {
-
+    if (way instanceof OsmWay osmWay) {
+      // Record unnamed crossings to a list.
+      if (
+        osmWay.isFootway() &&
+        osmWay.isMarkedCrossing() &&
+          way.hasNoName() &&
+          !way.isExplicitlyUnnamed()
+      ) {
+        pair
+          .asIterable()
+          .forEach(edge -> unnamedCrosswalks.add(new EdgeOnLevel(osmWay, edge, way.getLevels())));
+      }
+      // Record named streets, service roads, and slip/turn lanes to a list.
+      else if (!osmWay.isFootway()) {
+        Optional<TraverseDirection> oneWayCar = osmWay.isOneWay("motorcar");
+        if (way.isNamed() || osmWay.isServiceRoad() || oneWayCar.isPresent() && FORWARD.equals(oneWayCar.get())) {
+          streets.add(osmWay);
+        }
+      }
+    }
   }
 
   @Override
   public void postprocess() {
+    ProgressTracker progress = ProgressTracker.track(
+      "Assigning names to crosswalks",
+      500,
+      unnamedCrosswalks.size()
+    );
 
+    final AtomicInteger namesApplied = new AtomicInteger(0);
+    unnamedCrosswalks
+      .parallelStream()
+      .forEach(crosswalkOnLevel -> {
+        assignNameToCrosswalk(crosswalkOnLevel, namesApplied);
+
+        // Keep lambda! A method-ref would cause incorrect class and line number to be logged
+        // noinspection Convert2MethodRef
+        progress.step(m -> LOG.info(m));
+      });
+
+    LOG.info(
+      "Assigned names to {} of {} of crosswalks ({}%)",
+      namesApplied.get(),
+      unnamedCrosswalks.size(),
+      DoubleUtils.roundTo2Decimals(((double) namesApplied.get() / unnamedCrosswalks.size()) * 100)
+    );
+
+    LOG.info(progress.completeMessage());
+
+    // Set the indices to null so they can be garbage-collected
+    streets = null;
+    unnamedCrosswalks = null;
+  }
+
+  /**
+   * The actual worker method that runs the business logic on an individual sidewalk edge.
+   */
+  private void assignNameToCrosswalk(EdgeOnLevel crosswalkOnLevel, AtomicInteger namesApplied) {
+    var crosswalk = crosswalkOnLevel.edge;
+    var crossStreetOpt = getIntersectingStreet(crosswalkOnLevel.way, streets);
+
+    if (crossStreetOpt.isPresent()) {
+      OsmWay crossStreet = crossStreetOpt.get();
+      Optional<TraverseDirection> isOneWayCar;
+      // TODO: i18n
+      if (crossStreet.isNamed()) {
+        crosswalk.setName(I18NString.of(String.format("crossing over %s", crossStreet.getAssumedName())));
+      } else if (crossStreet.isServiceRoad()) {
+        crosswalk.setName(I18NString.of("crossing over service road"));
+      } else if ((isOneWayCar = crossStreet.isOneWay("motorcar")).isPresent() && FORWARD.equals(isOneWayCar.get())) {
+        crosswalk.setName(I18NString.of("crossing over turn lane"));
+      } else {
+        // Default on using the OSM way ID, which should not happen.
+        crosswalk.setName(I18NString.of(String.format("crossing %s", crosswalkOnLevel.way.getId())));
+      }
+      namesApplied.incrementAndGet();
+    }
   }
 
   /** Gets the intersecting street, if any, for the given way and candidate streets. */
-  public static Optional<OsmWay> getIntersectingStreet(OsmWay way, List<OsmWay> streets) {
+  public static Optional<OsmWay> getIntersectingStreet(OsmWay way, Collection<OsmWay> streets) {
     TLongList nodeRefs = way.getNodeRefs();
     if (nodeRefs.size() >= 3) {
       // There needs to be at least three nodes: 2 extremities that are on the sidewalk,
@@ -57,4 +141,14 @@ public class CrosswalkNamer implements EdgeNamer {
     }
     return Optional.empty();
   }
+
+  public Collection<EdgeOnLevel> getUnnamedCrosswalks() {
+    return unnamedCrosswalks;
+  }
+
+  public Collection<OsmWay> getStreets() {
+    return streets;
+  }
+
+  public record EdgeOnLevel(OsmWay way, StreetEdge edge, Set<String> levels) {}
 }
--- application/src/test/java/org/opentripplanner/graph_builder/module/osm/naming/CrosswalkNamerTest.java
+++ application/src/test/java/org/opentripplanner/graph_builder/module/osm/naming/CrosswalkNamerTest.java
@@ -4,17 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.opentripplanner.framework.geometry.GeometryUtils;
+import org.opentripplanner.framework.geometry.WgsCoordinate;
+import org.opentripplanner.framework.i18n.I18NString;
+import org.opentripplanner.graph_builder.module.osm.StreetEdgePair;
+import org.opentripplanner.graph_builder.services.osm.EdgeNamer;
 import org.opentripplanner.osm.model.OsmWay;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.opentripplanner.street.model.StreetTraversalPermission;
+import org.opentripplanner.street.model._data.StreetModelForTest;
+import org.opentripplanner.street.model.edge.StreetEdge;
+import org.opentripplanner.street.model.edge.StreetEdgeBuilder;
 
 class CrosswalkNamerTest {
-
-  private static final Logger LOG = LoggerFactory.getLogger(CrosswalkNamerTest.class);
-
   private static final OsmWay CROSSWALK = new OsmWay();
   private static final OsmWay STREET = new OsmWay();
   private static final OsmWay OTHER_STREET = new OsmWay();
@@ -23,6 +29,8 @@ class CrosswalkNamerTest {
   @BeforeAll
   static void setUp() {
     CROSSWALK.addTag("highway", "footway");
+    CROSSWALK.addTag("footway", "crossing");
+    CROSSWALK.addTag("crossing:markings", "yes");
     CROSSWALK.getNodeRefs().add(new long[] { 10001, 10002, 10003, 10004 });
 
     STREET.setId(50001);
@@ -31,6 +39,8 @@ class CrosswalkNamerTest {
     STREET.getNodeRefs().add(new long[] { 20001, 20002, 20003, 10002, 20004, 20005 });
 
     OTHER_STREET.setId(50002);
+    OTHER_STREET.addTag("highway", "primary");
+    OTHER_STREET.addTag("name", "Other Street");
     OTHER_STREET.getNodeRefs().add(new long[] { 30001, 30002, 30003, 30004, 30005 });
   }
 
@@ -43,4 +53,69 @@ class CrosswalkNamerTest {
     var intersectingStreet2 = CrosswalkNamer.getIntersectingStreet(CROSSWALK, List.of(OTHER_STREET));
     assertFalse(intersectingStreet2.isPresent());
   }
+
+  @Test
+  void recordEdgesAndPostprocess() {
+    var builder = new ModelBuilder();
+    var crosswalk = builder.addWay(
+      CROSSWALK,
+      new WgsCoordinate(33.9527949, -83.9954059),
+      new WgsCoordinate(33.9527436, -83.9954582)
+    );
+    builder.addWay(
+      STREET,
+      new WgsCoordinate( 33.9528839, -83.9956473),
+      new WgsCoordinate(33.9526837, -83.9953494)
+    );
+    builder.addWay(
+      OTHER_STREET,
+      new WgsCoordinate(33.9528839, -83.9956473),
+      new WgsCoordinate(33.9521700, -83.9954001)
+    );
+
+    CrosswalkNamer namer = new CrosswalkNamer();
+    builder.recordEdges(namer);
+    assertEquals(1, namer.getUnnamedCrosswalks().size());
+    assertEquals(2, namer.getStreets().size());
+
+    namer.postprocess();
+    assertEquals("crossing over 3rd Street", crosswalk.edge.getName().toString());
+    assertFalse(crosswalk.edge.nameIsDerived());
+  }
+
+  private static class ModelBuilder {
+
+    private final List<EdgePair> pairs = new ArrayList<>();
+
+    EdgePair addWay(OsmWay way, WgsCoordinate... coordinates) {
+      var edge = edgeBuilder(coordinates)
+        .withPermission(way.isFootway() ? StreetTraversalPermission.PEDESTRIAN : StreetTraversalPermission.CAR)
+        .withName(way.isNamed() ? way.getAssumedName() : I18NString.of("path"))
+        .withBogusName(!way.isNamed())
+        .buildAndConnect();
+
+      var p = new EdgePair(way, edge);
+      pairs.add(p);
+      return p;
+    }
+
+    void recordEdges(EdgeNamer namer) {
+      pairs.forEach(p -> namer.recordEdges(p.way, new StreetEdgePair(p.edge, null)));
+    }
+
+    private static StreetEdgeBuilder<?> edgeBuilder(WgsCoordinate... c) {
+      var coordinates = Arrays.stream(c).toList();
+      var ls = GeometryUtils.makeLineString(c);
+      return new StreetEdgeBuilder<>()
+        .withFromVertex(
+          StreetModelForTest.intersectionVertex(coordinates.getFirst().asJtsCoordinate())
+        )
+        .withToVertex(
+          StreetModelForTest.intersectionVertex(coordinates.getLast().asJtsCoordinate())
+        )
+        .withGeometry(ls);
+    }
+  }
+
+  private record EdgePair(OsmWay way, StreetEdge edge) {}
 }
