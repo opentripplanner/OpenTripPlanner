989f7c21b3644e3cbb0b54bd00d756cdeb92e2f5
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-09-30 14:56:37 -0400

    test(CrosswalkNamer): Support service roads and turn lanes.

--- application/src/test/java/org/opentripplanner/graph_builder/module/osm/naming/CrosswalkNamerTest.java
+++ application/src/test/java/org/opentripplanner/graph_builder/module/osm/naming/CrosswalkNamerTest.java
@@ -7,8 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.opentripplanner.framework.geometry.GeometryUtils;
 import org.opentripplanner.framework.geometry.WgsCoordinate;
 import org.opentripplanner.framework.i18n.I18NString;
@@ -23,6 +27,8 @@ import org.opentripplanner.street.model.edge.StreetEdgeBuilder;
 class CrosswalkNamerTest {
   private static final OsmWay CROSSWALK = new OsmWay();
   private static final OsmWay STREET = new OsmWay();
+  private static final OsmWay SERVICE_ROAD = new OsmWay();
+  private static final OsmWay TURN_LANE = new OsmWay();
   private static final OsmWay OTHER_STREET = new OsmWay();
 
 
@@ -42,6 +48,16 @@ class CrosswalkNamerTest {
     OTHER_STREET.addTag("highway", "primary");
     OTHER_STREET.addTag("name", "Other Street");
     OTHER_STREET.getNodeRefs().add(new long[] { 30001, 30002, 30003, 30004, 30005 });
+
+    // Reusing ids and nodes for SERVICE_ROAD and TURN_LANE as they are not used together with STREET.
+    SERVICE_ROAD.setId(50001);
+    SERVICE_ROAD.addTag("highway", "service");
+    SERVICE_ROAD.getNodeRefs().add(new long[] { 20001, 20002, 20003, 10002, 20004, 20005 });
+
+    TURN_LANE.setId(50001);
+    TURN_LANE.addTag("highway", "primary_link");
+    TURN_LANE.addTag("oneway", "yes");
+    TURN_LANE.getNodeRefs().add(new long[] { 20001, 20002, 20003, 10002, 20004, 20005 });
   }
 
   @Test
@@ -54,8 +70,9 @@ class CrosswalkNamerTest {
     assertFalse(intersectingStreet2.isPresent());
   }
 
-  @Test
-  void recordEdgesAndPostprocess() {
+  @ParameterizedTest
+  @MethodSource("streetTypes")
+  void recordEdgesAndPostprocess(OsmWay crossStreet, String name) {
     var builder = new ModelBuilder();
     var crosswalk = builder.addWay(
       CROSSWALK,
@@ -63,7 +80,7 @@ class CrosswalkNamerTest {
       new WgsCoordinate(33.9527436, -83.9954582)
     );
     builder.addWay(
-      STREET,
+      crossStreet,
       new WgsCoordinate( 33.9528839, -83.9956473),
       new WgsCoordinate(33.9526837, -83.9953494)
     );
@@ -79,8 +96,16 @@ class CrosswalkNamerTest {
     assertEquals(2, namer.getStreets().size());
 
     namer.postprocess();
-    assertEquals("crossing over 3rd Street", crosswalk.edge.getName().toString());
+    assertEquals(String.format("crossing over %s", name), crosswalk.edge.getName().toString());
     assertFalse(crosswalk.edge.nameIsDerived());
+  }
+
+  private static Stream<Arguments> streetTypes() {
+    return Stream.of(
+      Arguments.of(STREET, STREET.getTag("name")),
+      Arguments.of(SERVICE_ROAD, "service road"),
+      Arguments.of(TURN_LANE, "turn lane")
+    );
   }
 
   private static class ModelBuilder {
