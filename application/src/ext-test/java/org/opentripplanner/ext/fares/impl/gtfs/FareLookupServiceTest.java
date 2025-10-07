57b09b1ddb70cf943033b688d5afe58ed9b6bb5a
Leonard Ehrenfried <mail@leonard.io>
2025-10-07 15:31:54 +0200

    Add test for rule priority

--- application/src/ext-test/java/org/opentripplanner/ext/fares/impl/gtfs/FareLookupServiceTest.java
+++ application/src/ext-test/java/org/opentripplanner/ext/fares/impl/gtfs/FareLookupServiceTest.java
@@ -10,22 +10,27 @@ import org.junit.jupiter.api.Test;
 import org.opentripplanner.ext.fares.impl._support.FareTestConstants;
 import org.opentripplanner.ext.fares.model.FareLegRule;
 import org.opentripplanner.model.plan.Place;
+import org.opentripplanner.model.plan.TransitLeg;
 import org.opentripplanner.transit.model._data.TimetableRepositoryForTest;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
-import org.opentripplanner.transit.model.network.GroupOfRoutes;
+import org.opentripplanner.transit.model.network.Route;
 import org.opentripplanner.transit.model.site.RegularStop;
 
 class FareLookupServiceTest implements FareTestConstants {
 
   private static final FeedScopedId A_1 = id("a1");
-  public static final FeedScopedId N_1 = TimetableRepositoryForTest.id("n1");
+  private static final Route ROUTE = TimetableRepositoryForTest.route("r1")
+    .withGroupOfRoutes(List.of(NETWORK_A))
+    .build();
   private final TimetableRepositoryForTest testModel = TimetableRepositoryForTest.of();
   private final RegularStop STOP_1 = testModel.stop("stop1").build();
   private final RegularStop STOP_2 = testModel.stop("stop2").build();
 
   @Test
-  void test() {
-    var r1 = FareLegRule.of(id("r1"), List.of(FARE_PRODUCT_A)).withNetworkId(N_1).build();
+  void presenceOfAreaLeadsToEmptyResults() {
+    var r1 = FareLegRule.of(id("r1"), List.of(FARE_PRODUCT_A))
+      .withNetworkId(NETWORK_A.getId())
+      .build();
     var r2 = FareLegRule.of(id("r2"), List.of(FARE_PRODUCT_B))
       .withFromAreaId(A_1)
       .withToAreaId(A_1)
@@ -36,15 +41,36 @@ class FareLookupServiceTest implements FareTestConstants {
       ImmutableMultimap.of(STOP_1.getId(), A_1, STOP_2.getId(), A_1)
     );
 
-    var route = TimetableRepositoryForTest.route("r1")
-      .withGroupOfRoutes(List.of(GroupOfRoutes.of(N_1).build()))
-      .build();
-
-    var leg = newItinerary(Place.forStop(STOP_1))
-      .bus(route, 100, 100, 160, Place.forStop(STOP_2))
-      .build()
-      .transitLeg(0);
+    var leg = leg();
     var rules = service.legRules(leg);
     assertThat(rules).isEmpty();
+  }
+
+  @Test
+  void priorityLeadsToResults() {
+    var r1 = FareLegRule.of(id("r1"), List.of(FARE_PRODUCT_A))
+      .withNetworkId(NETWORK_A.getId())
+      .build();
+    var r2 = FareLegRule.of(id("r2"), List.of(FARE_PRODUCT_B))
+      .withFromAreaId(A_1)
+      .withToAreaId(A_1)
+      .withPriority(1)
+      .build();
+    var service = new FareLookupService(
+      List.of(r1, r2),
+      List.of(),
+      ImmutableMultimap.of(STOP_1.getId(), A_1, STOP_2.getId(), A_1)
+    );
+
+    var leg = leg();
+    var rules = service.legRules(leg);
+    assertThat(rules).containsExactly(r1);
+  }
+
+  private TransitLeg leg() {
+    return newItinerary(Place.forStop(STOP_1))
+      .bus(ROUTE, 100, 100, 160, Place.forStop(STOP_2))
+      .build()
+      .transitLeg(0);
   }
 }
