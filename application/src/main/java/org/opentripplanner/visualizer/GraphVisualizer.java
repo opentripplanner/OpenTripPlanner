opentripplanner/OpenTripPlanner

commit 445ba256803c795a055a07a348e6b86aa7d53a14

Move FromToViaVertexRequest and create a builder for TemporaryVerticesContainer

application/src/main/java/org/opentripplanner/visualizer/GraphVisualizer.java
@@ -63,7 +63,6 @@ import org.opentripplanner.astar.model.ShortestPathTree;
 import org.opentripplanner.astar.spi.DominanceFunction;
 import org.opentripplanner.astar.spi.TraverseVisitor;
 import org.opentripplanner.graph_builder.issue.api.DataImportIssue;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.RouteRequestBuilder;
 import org.opentripplanner.routing.core.VehicleRoutingOptimizeType;
@@ -512,24 +511,14 @@ public class GraphVisualizer extends JFrame implements VertexSelectionListener {
     long t0 = System.currentTimeMillis();
     // TODO: check options properly intialized (AMB)
     try (
-      var temporaryVertices = new TemporaryVerticesContainer(
-        graph,
-        request.from(),
-        request.to(),
-        List.of(),
-        request.journey().direct().mode()
-      )
+      var temporaryVerticesContainer = TemporaryVerticesContainer.of(graph)
+        .withFrom(request.from(), request.journey().direct().mode())
+        .withTo(request.from(), request.journey().direct().mode())
+        .build()
     ) {
-      var fromToViaVertexRequest = new FromToViaVertexRequest(
-        temporaryVertices.getFromVertices(),
-        temporaryVertices.getToVertices(),
-        temporaryVertices.getFromStopVertices(),
-        temporaryVertices.getToStopVertices(),
-        temporaryVertices.getVisitViaLocationVertices()
-      );
       List<GraphPath<State, Edge, Vertex>> paths = finder.graphPathFinderEntryPoint(
         request,
-        fromToViaVertexRequest
+        temporaryVerticesContainer.createFromToViaVertexRequest()
       );
       long dt = System.currentTimeMillis() - t0;
       searchTimeElapsedLabel.setText("search time elapsed: " + dt + "ms");

application/src/ext-test/java/org/opentripplanner/ext/flex/trip/ScheduledDeviatedTripIntegrationTest.java
@@ -23,7 +23,6 @@ import org.opentripplanner.model.StopTime;
 import org.opentripplanner.model.plan.Itinerary;
 import org.opentripplanner.routing.algorithm.raptoradapter.router.AdditionalSearchDays;
 import org.opentripplanner.routing.algorithm.raptoradapter.router.TransitRouter;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.framework.DebugTimingAggregator;
@@ -172,13 +171,10 @@ class ScheduledDeviatedTripIntegrationTest {
     var additionalSearchDays = AdditionalSearchDays.defaults(dateTime);
 
     try (
-      var temporaryVertices = new TemporaryVerticesContainer(
-        serverContext.graph(),
-        from,
-        to,
-        List.of(),
-        StreetMode.WALK
-      )
+      var temporaryVerticesContainer = TemporaryVerticesContainer.of(serverContext.graph())
+        .withFrom(from, StreetMode.WALK)
+        .withTo(to, StreetMode.WALK)
+        .build()
     ) {
       var result = TransitRouter.route(
         request,
@@ -187,13 +183,7 @@ class ScheduledDeviatedTripIntegrationTest {
         transitStartOfTime,
         additionalSearchDays,
         new DebugTimingAggregator(),
-        new FromToViaVertexRequest(
-          temporaryVertices.getFromVertices(),
-          temporaryVertices.getToVertices(),
-          temporaryVertices.getFromStopVertices(),
-          temporaryVertices.getToStopVertices(),
-          temporaryVertices.getVisitViaLocationVertices()
-        )
+        temporaryVerticesContainer.createFromToViaVertexRequest()
       );
 
       return result.getItineraries();

application/src/main/java/org/opentripplanner/routing/algorithm/RoutingWorker.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
@@ -24,7 +25,6 @@ import org.opentripplanner.routing.algorithm.raptoradapter.router.FilterTransitW
 import org.opentripplanner.routing.algorithm.raptoradapter.router.TransitRouter;
 import org.opentripplanner.routing.algorithm.raptoradapter.router.street.DirectFlexRouter;
 import org.opentripplanner.routing.algorithm.raptoradapter.router.street.DirectStreetRouter;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.api.request.request.StreetRequest;
@@ -34,6 +34,7 @@ import org.opentripplanner.routing.framework.DebugTimingAggregator;
 import org.opentripplanner.service.paging.PagingService;
 import org.opentripplanner.standalone.api.OtpServerRequestContext;
 import org.opentripplanner.street.search.TemporaryVerticesContainer;
+import org.opentripplanner.street.search.request.FromToViaVertexRequest;
 import org.opentripplanner.transit.model.network.grouppriority.TransitGroupPriorityService;
 import org.opentripplanner.utils.time.ServiceDateUtils;
 import org.slf4j.Logger;
@@ -99,25 +100,8 @@ public class RoutingWorker {
 
     var result = RoutingResult.empty();
 
-    try (
-      var temporaryVertices = new TemporaryVerticesContainer(
-        serverContext.graph(),
-        request.from(),
-        request.to(),
-        request.getVisitViaLocations(),
-        request.journey().access().mode(),
-        request.journey().egress().mode(),
-        request.journey().direct().mode(),
-        request.journey().transfer().mode()
-      )
-    ) {
-      var requestVertexService = new FromToViaVertexRequest(
-        temporaryVertices.getFromVertices(),
-        temporaryVertices.getToVertices(),
-        temporaryVertices.getFromStopVertices(),
-        temporaryVertices.getToStopVertices(),
-        temporaryVertices.getVisitViaLocationVertices()
-      );
+    try (var temporaryVerticesContainer = createTemporaryVerticesContainer()) {
+      var requestVertexService = temporaryVerticesContainer.createFromToViaVertexRequest();
 
       if (OTPFeature.ParallelRouting.isOn()) {
         // TODO: This is not using {@link OtpRequestThreadFactory} which means we do not get
@@ -320,5 +304,27 @@ public class RoutingWorker {
       pageCursorInput,
       itineraries
     );
+  }
+
+  private TemporaryVerticesContainer createTemporaryVerticesContainer() {
+    return TemporaryVerticesContainer.of(serverContext.graph())
+      .withFrom(
+        request.from(),
+        EnumSet.of(request.journey().access().mode(), request.journey().direct().mode())
+      )
+      .withTo(
+        request.to(),
+        EnumSet.of(request.journey().egress().mode(), request.journey().direct().mode())
+      )
+      .withVia(
+        request.getVisitViaLocations(),
+        EnumSet.of(
+          request.journey().access().mode(),
+          request.journey().egress().mode(),
+          request.journey().direct().mode(),
+          request.journey().transfer().mode()
+        )
+      )
+      .build();
   }
 }

application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/TransitRouter.java
@@ -34,7 +34,6 @@ import org.opentripplanner.routing.algorithm.raptoradapter.transit.mappers.Rapto
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.request.RaptorRoutingRequestTransitData;
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.request.RouteRequestTransitDataProviderFilter;
 import org.opentripplanner.routing.algorithm.transferoptimization.configure.TransferOptimizationServiceConfigurator;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.api.request.request.StreetRequest;
@@ -45,6 +44,7 @@ import org.opentripplanner.routing.error.RoutingValidationException;
 import org.opentripplanner.routing.framework.DebugTimingAggregator;
 import org.opentripplanner.routing.via.ViaCoordinateTransferFactory;
 import org.opentripplanner.standalone.api.OtpServerRequestContext;
+import org.opentripplanner.street.search.request.FromToViaVertexRequest;
 import org.opentripplanner.transit.model.framework.EntityNotFoundException;
 import org.opentripplanner.transit.model.framework.FeedScopedId;
 import org.opentripplanner.transit.model.network.grouppriority.TransitGroupPriorityService;

application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/AccessEgressRouter.java
@@ -8,10 +8,10 @@ import javax.annotation.Nullable;
 import org.opentripplanner.ext.dataoverlay.routing.DataOverlayContext;
 import org.opentripplanner.framework.application.OTPRequestTimeoutException;
 import org.opentripplanner.graph_builder.module.nearbystops.StreetNearbyStopFinder;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.request.StreetRequest;
 import org.opentripplanner.routing.graphfinder.NearbyStop;
+import org.opentripplanner.street.search.request.FromToViaVertexRequest;
 import org.opentripplanner.utils.collection.ListUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/DirectFlexRouter.java
@@ -9,11 +9,11 @@ import org.opentripplanner.ext.flex.filter.FilterMapper;
 import org.opentripplanner.framework.application.OTPRequestTimeoutException;
 import org.opentripplanner.model.plan.Itinerary;
 import org.opentripplanner.routing.algorithm.raptoradapter.router.AdditionalSearchDays;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.graphfinder.NearbyStop;
 import org.opentripplanner.standalone.api.OtpServerRequestContext;
+import org.opentripplanner.street.search.request.FromToViaVertexRequest;
 
 public class DirectFlexRouter {
 

application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/DirectStreetRouter.java
@@ -8,7 +8,6 @@ import org.opentripplanner.framework.geometry.SphericalDistanceLibrary;
 import org.opentripplanner.model.plan.Itinerary;
 import org.opentripplanner.routing.algorithm.mapping.GraphPathToItineraryMapper;
 import org.opentripplanner.routing.algorithm.mapping.ItinerariesHelper;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.error.PathNotFoundException;
@@ -16,6 +15,7 @@ import org.opentripplanner.routing.impl.GraphPathFinder;
 import org.opentripplanner.standalone.api.OtpServerRequestContext;
 import org.opentripplanner.street.model.edge.Edge;
 import org.opentripplanner.street.model.vertex.Vertex;
+import org.opentripplanner.street.search.request.FromToViaVertexRequest;
 import org.opentripplanner.street.search.state.State;
 
 /**

application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/FlexAccessEgressRouter.java
@@ -9,12 +9,12 @@ import org.opentripplanner.ext.flex.FlexRouter;
 import org.opentripplanner.ext.flex.filter.FilterMapper;
 import org.opentripplanner.framework.application.OTPRequestTimeoutException;
 import org.opentripplanner.routing.algorithm.raptoradapter.router.AdditionalSearchDays;
-import org.opentripplanner.routing.api.request.FromToViaVertexRequest;
 import org.opentripplanner.routing.api.request.RouteRequest;
 import org.opentripplanner.routing.api.request.StreetMode;
 import org.opentripplanner.routing.api.request.request.StreetRequest;
 import org.opentripplanner.routing.graphfinder.NearbyStop;
 import org.opentripplanner.standalone.api.OtpServerRequestContext;
+import org.opentripplanner.street.search.request.FromToViaVertexRequest;
 import org.opentripplanner.transit.service.TransitService;
 
 public class FlexAccessEgressRouter {

commit 3aac060ac127f9cdb1bf2990896124238738fde2

Fix mapping bug in GraphVisualizer introduced in 445ba256803c795a055a07a348e6b86aa7d53a14

application/src/main/java/org/opentripplanner/visualizer/GraphVisualizer.java
@@ -525,7 +525,7 @@ public class GraphVisualizer extends JFrame implements VertexSelectionListener {
         )
       )
         .withFrom(request.from(), request.journey().direct().mode())
-        .withTo(request.from(), request.journey().direct().mode())
+        .withTo(request.to(), request.journey().direct().mode())
         .build();
       List<GraphPath<State, Edge, Vertex>> paths = finder.graphPathFinderEntryPoint(
         request,