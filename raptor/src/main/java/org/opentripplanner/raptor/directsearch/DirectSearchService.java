7860ac7d61b2dff08d2a99e15ff24b4cf65f92de
Thomas Gran <t2gran@gmail.com>
2025-10-22 14:03:18 +0200

    feat: add constructor to DirectSearchService for RaptorTransitDataProvider

--- raptor/src/main/java/org/opentripplanner/raptor/directsearch/DirectSearchService.java
+++ raptor/src/main/java/org/opentripplanner/raptor/directsearch/DirectSearchService.java
@@ -1,4 +1,12 @@
 package org.opentripplanner.raptor.directsearch;
 
+import org.opentripplanner.raptor.spi.RaptorTransitDataProvider;
+
 public class DirectSearchService {
+  private final RaptorTransitDataProvider data;
+
+  public DirectSearchService(RaptorTransitDataProvider data) {
+    this.data = data;
+  }
+
 }
