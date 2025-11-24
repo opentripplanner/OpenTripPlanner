a142c8028c6450cd458f6f1f2e7db909bdaa82bf
Joel Lappalainen <lappalj8@gmail.com>
2025-11-24 12:42:58 +0200

    Refactor direct street router to use template method pattern

--- application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/DefaultDirectStreetRouter.java
+++ application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/DefaultDirectStreetRouter.java
@@ -1,118 +1,78 @@
 package org.opentripplanner.routing.algorithm.raptoradapter.router.street;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import org.opentripplanner.framework.application.OTPRequestTimeoutException;
 import org.opentripplanner.model.plan.Itinerary;
-import org.opentripplanner.routing.algorithm.mapping.ItinerariesHelper;
 import org.opentripplanner.routing.algorithm.mapping.LegsToItineraryMapper;
 import org.opentripplanner.routing.algorithm.mapping.StreetPathToLegsMapper;
 import org.opentripplanner.routing.api.request.RouteRequest;
-import org.opentripplanner.routing.error.PathNotFoundException;
 import org.opentripplanner.routing.graphfinder.TransitServiceResolver;
 import org.opentripplanner.routing.impl.GraphPathFinder;
 import org.opentripplanner.routing.linking.LinkingContext;
 import org.opentripplanner.standalone.api.OtpServerRequestContext;
 import org.opentripplanner.street.geometry.SphericalDistanceLibrary;
 import org.opentripplanner.street.model.StreetMode;
+import org.opentripplanner.street.model.path.StreetPath;
 
 /**
  * Generates "direct" street routes, i.e. those that do not use transit and are on the street
- * network for the entire itinerary.
- *
- * @see DirectFlexRouter
+ * network for the entire itinerary. Doesn't support via locations or flex.
  */
-public class DefaultDirectStreetRouter {
+public class DefaultDirectStreetRouter extends DirectStreetRouter {
 
-  public static List<Itinerary> route(
+  @Override
+  List<Itinerary> findItineraries(
     OtpServerRequestContext serverContext,
-    RouteRequest request,
-    LinkingContext linkingContext
+    GraphPathFinder graphPathFinder,
+    LinkingContext linkingContext,
+    RouteRequest request
   ) {
-    if (request.journey().direct().mode() == StreetMode.NOT_SET) {
-      return Collections.emptyList();
-    }
-    OTPRequestTimeoutException.checkForTimeout();
-    try {
-      var maxCarSpeed = serverContext.streetLimitationParametersService().maxCarSpeed();
-      if (!straightLineDistanceIsWithinLimit(request, maxCarSpeed, linkingContext)) {
-        return Collections.emptyList();
-      }
-
-      // we could also get a persistent router-scoped GraphPathFinder but there's no setup cost here
-      GraphPathFinder gpFinder = new GraphPathFinder(
-        serverContext.listExtensionRequestContexts(request),
-        maxCarSpeed
-      );
-      var paths = gpFinder.graphPathFinderEntryPoint(request, linkingContext);
-
-      // Convert the internal GraphPaths to itineraries
-      final StreetPathToLegsMapper streetPathToLegsMapper = new StreetPathToLegsMapper(
-        new TransitServiceResolver(serverContext.transitService()),
-        serverContext.transitService().getTimeZone(),
-        serverContext.graph().streetNotesService,
-        serverContext.streetDetailsService(),
-        serverContext.graph().ellipsoidToGeoidDifference
-      );
-      List<Itinerary> itineraries = new ArrayList<>();
-      for (var path : paths) {
-        var legs = streetPathToLegsMapper.map(path, request);
-        var itinerary = LegsToItineraryMapper.map(
-          legs,
-          path.lastState().isRentingVehicleFromStation(),
-          path.calculateElevations()
-        );
-        itinerary.ifPresent(itineraries::add);
-      }
-      return ItinerariesHelper.decorateItinerariesWithRequestData(
-        itineraries,
-        request.journey().wheelchair(),
-        request.preferences().wheelchair()
-      );
-    } catch (PathNotFoundException e) {
-      return Collections.emptyList();
-    }
+    var paths = graphPathFinder.graphPathFinderEntryPoint(request, linkingContext);
+    return mapToItineraries(serverContext, request, paths);
   }
 
-  private static boolean straightLineDistanceIsWithinLimit(
+  @Override
+  boolean isRequestInvalidForRouting(RouteRequest request) {
+    return request.journey().direct().mode() == StreetMode.NOT_SET;
+  }
+
+  @Override
+  boolean isStraightLineDistanceWithinLimit(
+    LinkingContext linkingContext,
     RouteRequest request,
-    float maxCarSpeed,
-    LinkingContext linkingContext
+    double maxDistanceLimit
   ) {
     // TODO This currently only calculates the distances between the first fromVertex
     //      and the first toVertex
     double distance = SphericalDistanceLibrary.distance(
-      linkingContext.findVertices(request.from()).iterator().next().getCoordinate(),
-      linkingContext.findVertices(request.to()).iterator().next().getCoordinate()
+      getFirstCoordinateForLocation(linkingContext, request.from()),
+      getFirstCoordinateForLocation(linkingContext, request.to())
     );
-    return distance < calculateDistanceMaxLimit(request, maxCarSpeed);
+    return distance < maxDistanceLimit;
   }
 
-  /**
-   * Calculates the maximum distance in meters based on the maxDirectStreetDuration and the
-   * fastest mode available. This assumes that it is not possible to exceed the speed defined in the
-   * RouteRequest.
-   */
-  private static double calculateDistanceMaxLimit(RouteRequest request, float maxCarSpeed) {
-    var preferences = request.preferences();
-    double distanceLimit;
-    StreetMode mode = request.journey().direct().mode();
-
-    double durationLimit = preferences.street().maxDirectDuration().valueOf(mode).toSeconds();
-
-    if (mode.includesDriving()) {
-      distanceLimit = durationLimit * maxCarSpeed;
-    } else if (mode.includesBiking()) {
-      distanceLimit = durationLimit * preferences.bike().speed();
-    } else if (mode.includesScooter()) {
-      distanceLimit = durationLimit * preferences.scooter().speed();
-    } else if (mode.includesWalking()) {
-      distanceLimit = durationLimit * preferences.walk().speed();
-    } else {
-      throw new IllegalStateException("Could not set max limit for StreetMode");
+  private List<Itinerary> mapToItineraries(
+    OtpServerRequestContext serverContext,
+    RouteRequest request,
+    List<StreetPath> paths
+  ) {
+    StreetPathToLegsMapper streetPathToLegsMapper = new StreetPathToLegsMapper(
+      new TransitServiceResolver(serverContext.transitService()),
+      serverContext.transitService().getTimeZone(),
+      serverContext.graph().streetNotesService,
+      serverContext.streetDetailsService(),
+      serverContext.graph().ellipsoidToGeoidDifference
+    );
+    List<Itinerary> itineraries = new ArrayList<>();
+    for (var path : paths) {
+      var legs = streetPathToLegsMapper.map(path, request);
+      var itinerary = LegsToItineraryMapper.map(
+        legs,
+        path.lastState().isRentingVehicleFromStation(),
+        path.calculateElevations()
+      );
+      itinerary.ifPresent(itineraries::add);
     }
-
-    return distanceLimit;
+    return itineraries;
   }
 }
---
+++ application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/DirectStreetRouter.java
@@ -0,0 +1,124 @@
+package org.opentripplanner.routing.algorithm.raptoradapter.router.street;
+
+import java.util.Collections;
+import java.util.List;
+import org.locationtech.jts.geom.Coordinate;
+import org.opentripplanner.framework.application.OTPRequestTimeoutException;
+import org.opentripplanner.model.GenericLocation;
+import org.opentripplanner.model.plan.Itinerary;
+import org.opentripplanner.routing.algorithm.mapping.ItinerariesHelper;
+import org.opentripplanner.routing.api.request.RouteRequest;
+import org.opentripplanner.routing.error.PathNotFoundException;
+import org.opentripplanner.routing.impl.GraphPathFinder;
+import org.opentripplanner.routing.linking.LinkingContext;
+import org.opentripplanner.standalone.api.OtpServerRequestContext;
+import org.opentripplanner.street.model.StreetMode;
+
+/**
+ * Abstract class for generating "direct" street routes, i.e. those that do not use transit and are
+ * on the street network for the entire itinerary. For flex routing, use {@link DirectFlexRouter}.
+ * Follows template method pattern.
+ */
+public abstract class DirectStreetRouter {
+
+  /**
+   * @return direct street itineraries.
+   */
+  public List<Itinerary> route(
+    OtpServerRequestContext serverContext,
+    RouteRequest request,
+    LinkingContext linkingContext
+  ) {
+    if (isRequestInvalidForRouting(request)) {
+      return Collections.emptyList();
+    }
+    OTPRequestTimeoutException.checkForTimeout();
+
+    var maxCarSpeed = serverContext.streetLimitationParametersService().maxCarSpeed();
+    var maxDistanceLimit = calculateDistanceMaxLimit(request, maxCarSpeed);
+    if (!isStraightLineDistanceWithinLimit(linkingContext, request, maxDistanceLimit)) {
+      return Collections.emptyList();
+    }
+
+    try {
+      // we could also get a persistent router-scoped GraphPathFinder but there's no setup cost here
+      GraphPathFinder gpFinder = new GraphPathFinder(
+        serverContext.listExtensionRequestContexts(request),
+        maxCarSpeed
+      );
+      var itineraries = findItineraries(serverContext, gpFinder, linkingContext, request);
+      return decorateItineraries(request, itineraries);
+    } catch (PathNotFoundException e) {
+      return Collections.emptyList();
+    }
+  }
+
+  /**
+   * Checks that the route request is configured to allow direct street results.
+   */
+  abstract boolean isRequestInvalidForRouting(RouteRequest request);
+
+  /**
+   * Checks that as the crow flies distance between locations in the search are within the maximum
+   * distance limit.
+   */
+  abstract boolean isStraightLineDistanceWithinLimit(
+    LinkingContext linkingContext,
+    RouteRequest request,
+    double maxDistanceLimit
+  );
+
+  /**
+   * Find graph paths between the locations in the request.
+   */
+  abstract List<Itinerary> findItineraries(
+    OtpServerRequestContext serverContext,
+    GraphPathFinder graphPathFinder,
+    LinkingContext linkingContext,
+    RouteRequest request
+  );
+
+  static Coordinate getFirstCoordinateForLocation(
+    LinkingContext context,
+    GenericLocation location
+  ) {
+    return context.findVertices(location).iterator().next().getCoordinate();
+  }
+
+  /**
+   * Calculates the maximum distance in meters based on the maxDirectStreetDuration and the
+   * fastest mode available. This assumes that it is not possible to exceed the speed defined in the
+   * RouteRequest.
+   */
+  private static double calculateDistanceMaxLimit(RouteRequest request, float maxCarSpeed) {
+    var preferences = request.preferences();
+    StreetMode mode = request.journey().direct().mode();
+
+    double durationLimit = preferences.street().maxDirectDuration().valueOf(mode).toSeconds();
+
+    if (mode.includesDriving()) {
+      return durationLimit * maxCarSpeed;
+    }
+    if (mode.includesBiking()) {
+      return durationLimit * preferences.bike().speed();
+    }
+    if (mode.includesScooter()) {
+      return durationLimit * preferences.scooter().speed();
+    }
+    if (mode.includesWalking()) {
+      return durationLimit * preferences.walk().speed();
+    }
+    throw new IllegalStateException("Could not set max limit for StreetMode");
+  }
+
+  private static List<Itinerary> decorateItineraries(
+    RouteRequest request,
+    List<Itinerary> itineraries
+  ) {
+    return ItinerariesHelper.decorateItinerariesWithRequestData(
+      itineraries,
+      request.journey().wheelchair(),
+      request.preferences().wheelchair()
+    );
+  }
+}
