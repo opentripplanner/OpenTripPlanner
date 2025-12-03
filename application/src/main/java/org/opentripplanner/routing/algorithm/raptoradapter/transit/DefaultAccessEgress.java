c6c0554354692e10faa160382068d569292a4f08
Joel Lappalainen <lappalj8@gmail.com>
2025-12-03 13:20:23 +0200

    Implement numberOfViaLocationsVisited in DefaultAccessEgress

--- application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/DefaultAccessEgress.java
+++ application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/DefaultAccessEgress.java
@@ -164,6 +164,11 @@ public class DefaultAccessEgress implements RoutingAccessEgress {
   }
 
   @Override
+  public int numberOfViaLocationsVisited() {
+    return Math.max(0, finalStates.size() - 1);
+  }
+
+  @Override
   public String toString() {
     return asString(true, true, summary());
   }
