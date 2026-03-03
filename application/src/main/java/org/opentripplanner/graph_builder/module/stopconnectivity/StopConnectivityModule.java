fd304f3d4422f31744d76414a435b19c1566f25e
Leonard Ehrenfried <mail@leonard.io>
2026-03-03 14:20:06 +0100

    Finetune stop connectivity check

--- application/src/main/java/org/opentripplanner/graph_builder/module/stopconnectivity/StopConnectivityModule.java
+++ application/src/main/java/org/opentripplanner/graph_builder/module/stopconnectivity/StopConnectivityModule.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.graph_builder.module.stopconnectivity;
 
 import java.time.Duration;
+import java.util.Objects;
 import org.opentripplanner.astar.strategy.DurationTerminationStrategy;
 import org.opentripplanner.graph_builder.issue.api.DataImportIssueStore;
 import org.opentripplanner.graph_builder.issues.IsolatedStop;
@@ -12,7 +13,6 @@ import org.opentripplanner.street.search.StreetSearchBuilder;
 import org.opentripplanner.street.search.request.StreetSearchRequest;
 
 public class StopConnectivityModule implements GraphBuilderModule {
-
   public static final Duration DURATION = Duration.ofMinutes(10);
   private final Graph graph;
   private final DataImportIssueStore issueStore;
@@ -24,24 +24,32 @@ public class StopConnectivityModule implements GraphBuilderModule {
 
   @Override
   public void buildGraph() {
-    var stops = graph.getVerticesOfType(TransitStopVertex.class);
 
-    for (var stop : stops) {
-      var spt = StreetSearchBuilder.of()
-        .withPreStartHook(() -> {})
-        .withRequest(StreetSearchRequest.of().withMode(StreetMode.WALK).build())
-        .withFrom(stop)
-        .withTerminationStrategy(new DurationTerminationStrategy(DURATION.plusMinutes(1)))
-        .getShortestPathTree();
+    var issues = graph
+      .getVerticesOfType(TransitStopVertex.class)
+      .parallelStream()
+      .map(stop -> {
+        var spt = StreetSearchBuilder.of()
+          .withPreStartHook(() -> {})
+          .withRequest(StreetSearchRequest.of().withMode(StreetMode.WALK).build())
+          .withFrom(stop)
+          .withTerminationStrategy(new DurationTerminationStrategy(DURATION.plusMinutes(1)))
+          .getShortestPathTree();
 
-      var isIsolated = spt
-        .getAllStates()
-        .stream()
-        .noneMatch(s -> s.getElapsedTimeSeconds() > DURATION.toSeconds());
+        var isIsolated = spt
+          .getAllStates()
+          .stream()
+          .noneMatch(s -> s.getElapsedTimeSeconds() > DURATION.toSeconds());
 
-      if (isIsolated) {
-        issueStore.add(new IsolatedStop(stop));
-      }
-    }
+        if (isIsolated) {
+          return new IsolatedStop(stop);
+        } else {
+          return null;
+        }
+      })
+      .filter(Objects::nonNull)
+      .toList();
+
+    issues.forEach(issueStore::add);
   }
 }
