18306cb86d418d25f54f0ca765293662fc6faf3d
Arnljot Arntsen <arnljot.arntsen@entur.org>
2026-03-09 11:56:39 +0100

    Added test after code review.

--- application/src/ext-test/java/org/opentripplanner/ext/carpooling/CarpoolEstimatedVehicleJourneyData.java
+++ application/src/ext-test/java/org/opentripplanner/ext/carpooling/CarpoolEstimatedVehicleJourneyData.java
@@ -6,6 +6,13 @@ import static org.opentripplanner.ext.carpooling.CarpoolTestCoordinates.OSLO_NOR
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
+import net.opengis.gml._3.AbstractRingPropertyType;
+import net.opengis.gml._3.DirectPositionListType;
+import net.opengis.gml._3.LinearRingType;
+import net.opengis.gml._3.ObjectFactory;
+import net.opengis.gml._3.PolygonType;
+import org.locationtech.jts.geom.GeometryFactory;
 import org.opentripplanner.street.geometry.WgsCoordinate;
 import uk.org.siri.siri21.AimedFlexibleArea;
 import uk.org.siri.siri21.CircularAreaStructure;
@@ -16,6 +23,9 @@ import uk.org.siri.siri21.OperatorRefStructure;
 import uk.org.siri.siri21.StopAssignmentStructure;
 
 public class CarpoolEstimatedVehicleJourneyData {
+  private static final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+  private static final String FIRST_STOP_POLYGON = "10.813864907662264 59.904961620490184 10.818271230878196 59.903856857974404 10.818498026925795 59.90393809176436 10.818604482213118 59.9039055982723 10.818386943147004 59.90379187079944 10.818215688988204 59.90380579663352 10.81386953615231 59.90490591905822 10.813864907662264 59.904961620490184";
+  private static final String LAST_STOP_POLYGON = "10.197976677739746 59.74492585818382 10.19775667855555 59.74465335895027 10.197230513839912 59.74475958772939 10.19714251416596 59.74464319791892 10.197628345697694 59.744487087140186 10.197844678229416 59.744667214897106 10.198856674477867 59.74438085749492 10.19892634088606 59.744463993767596 10.198640341946088 59.74453696877012 10.19883100790608 59.74475404536645 10.197976677739746 59.74492585818382";
 
   public static EstimatedVehicleJourney arrivalIsAfterDepartureTime() {
     var journey = new EstimatedVehicleJourney();
@@ -64,6 +74,62 @@ public class CarpoolEstimatedVehicleJourneyData {
     return journey;
   }
 
+  public static EstimatedVehicleJourney minimalCompleteJourneyWithPolygon() {
+    var journey = new EstimatedVehicleJourney();
+    var operator = new OperatorRefStructure();
+    operator.setValue("ENT");
+    journey.setEstimatedVehicleJourneyCode("unittest");
+    journey.setOperatorRef(operator);
+
+    var firstStop = forPolygon(FIRST_STOP_POLYGON);
+    firstStop.setAimedDepartureTime(ZonedDateTime.now());
+    var firstName = new NaturalLanguageStringStructure();
+    firstName.setValue("First stop");
+    firstStop.getStopPointNames().add(firstName);
+
+    var lastStop = forPolygon(LAST_STOP_POLYGON);
+    lastStop.setAimedDepartureTime(ZonedDateTime.now());
+    var lastName = new NaturalLanguageStringStructure();
+    lastName.setValue("Last stop");
+    lastStop.getStopPointNames().add(lastName);
+    lastStop.setAimedArrivalTime(ZonedDateTime.now().plusMinutes(45));
+
+    journey.setEstimatedCalls(new EstimatedVehicleJourney.EstimatedCalls());
+    journey.getEstimatedCalls().getEstimatedCalls().add(firstStop);
+    journey.getEstimatedCalls().getEstimatedCalls().add(lastStop);
+
+    return journey;
+  }
+
+  public static EstimatedVehicleJourney tripHasAimedTimesOnly() {
+    var journey = minimalCompleteJourney();
+
+    var firstStop = journey.getEstimatedCalls().getEstimatedCalls().getFirst();
+    var lastStop = journey.getEstimatedCalls().getEstimatedCalls().getLast();
+
+    firstStop.setAimedDepartureTime(ZonedDateTime.now());
+    firstStop.setExpectedDepartureTime(null);
+    lastStop.setAimedArrivalTime(ZonedDateTime.now().plusMinutes(45));
+    lastStop.setExpectedArrivalTime(null);
+
+    return journey;
+  }
+
+  public static EstimatedVehicleJourney tripHasExpectedTimesOnly() {
+
+    var journey = minimalCompleteJourney();
+
+    var firstStop = journey.getEstimatedCalls().getEstimatedCalls().getFirst();
+    var lastStop = journey.getEstimatedCalls().getEstimatedCalls().getLast();
+
+    firstStop.setAimedDepartureTime(null);
+    firstStop.setExpectedDepartureTime(ZonedDateTime.now());
+    lastStop.setAimedArrivalTime(null);
+    lastStop.setExpectedArrivalTime(ZonedDateTime.now().plusMinutes(45));
+
+    return journey;
+  }
+
   public static EstimatedVehicleJourney stopTimesAreOutOfOrder() {
     var journey = new EstimatedVehicleJourney();
     journey.setEstimatedCalls(new EstimatedVehicleJourney.EstimatedCalls());
@@ -94,5 +160,34 @@ public class CarpoolEstimatedVehicleJourneyData {
     call.getDepartureStopAssignments().add(stop);
 
     return call;
+  }
+
+  static EstimatedCall forPolygon(String posList) {
+    var call = new EstimatedCall();
+    call.setAimedDepartureTime(ZonedDateTime.now());
+
+    var stop = new StopAssignmentStructure();
+    var flexibleStop = poslistToAimedFlexibleArea(posList);
+    stop.setAimedFlexibleArea(flexibleStop);
+
+    call.getDepartureStopAssignments().add(stop);
+    return call;
+  }
+
+  static AimedFlexibleArea poslistToAimedFlexibleArea(String coordinates) {
+    var gmlFactory = new ObjectFactory();
+
+    var poslist = Arrays.stream(coordinates.trim().split("\\s+"))
+      .map(Double::valueOf)
+      .toList();
+    var polygon = new PolygonType()
+      .withExterior(new AbstractRingPropertyType()
+        .withAbstractRing(gmlFactory.createLinearRing(new LinearRingType()
+          .withPosList(new DirectPositionListType()
+            .withValue(poslist)))));
+
+    var area = new AimedFlexibleArea();
+    area.setPolygon(polygon);
+    return area;
   }
 }
--- application/src/ext-test/java/org/opentripplanner/ext/carpooling/updater/CarpoolSiriMapperTest.java
+++ application/src/ext-test/java/org/opentripplanner/ext/carpooling/updater/CarpoolSiriMapperTest.java
@@ -1,11 +1,14 @@
 package org.opentripplanner.ext.carpooling.updater;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.opentripplanner.ext.carpooling.CarpoolEstimatedVehicleJourneyData.arrivalIsAfterDepartureTime;
 import static org.opentripplanner.ext.carpooling.CarpoolEstimatedVehicleJourneyData.lessThanTwoStops;
 import static org.opentripplanner.ext.carpooling.CarpoolEstimatedVehicleJourneyData.minimalCompleteJourney;
 import static org.opentripplanner.ext.carpooling.CarpoolEstimatedVehicleJourneyData.stopTimesAreOutOfOrder;
+import static org.opentripplanner.ext.carpooling.CarpoolEstimatedVehicleJourneyData.tripHasAimedTimesOnly;
+import static org.opentripplanner.ext.carpooling.CarpoolEstimatedVehicleJourneyData.tripHasExpectedTimesOnly;
 
 import org.junit.jupiter.api.Test;
 import uk.org.siri.siri21.EstimatedCall;
@@ -66,6 +69,36 @@ public class CarpoolSiriMapperTest {
       .getValue();
     assertEquals("First stop", startName);
     assertEquals("Last stop", endName);
+  }
+
+  @Test
+  void mapSiriToCarpoolTrip_minimalDataUsingPolygonStops_mapsOk() {
+    var journey = minimalCompleteJourney();
+    var mapped = mapper.mapSiriToCarpoolTrip(journey);
+
+    var firstStop = mapped.stops().getFirst();
+    var lastStop = mapped.stops().getLast();
+
+    assertNotNull(firstStop.getCoordinate());
+    assertNotNull(lastStop.getCoordinate());
+  }
+
+  @Test
+  void mapSiriToCarpoolTrip_tripHasOnlyAimedTimes_mapsOk() {
+    var journey = tripHasAimedTimesOnly();
+    var mapped = mapper.mapSiriToCarpoolTrip(journey);
+
+    assertNotNull(mapped.startTime());
+    assertNotNull(mapped.endTime());
+  }
+
+  @Test
+  void mapSiriToCarpoolTrip_tripHasOnlyExpectedTimes_mapsOk() {
+    var journey = tripHasExpectedTimesOnly();
+    var mapped = mapper.mapSiriToCarpoolTrip(journey);
+
+    assertNotNull(mapped.startTime());
+    assertNotNull(mapped.endTime());
   }
 
   @Test
