eacfde57ae72e560d953f13d2d5e4edf09324b03
Henrik Abrahamsson <henrik.abrahamsson@decerno.se>
2026-03-10 15:27:20 +0100

    Fix formatting

--- application/src/main/java/org/opentripplanner/updater/spi/UpdateException.java
+++ application/src/main/java/org/opentripplanner/updater/spi/UpdateException.java
@@ -54,7 +54,7 @@ public class UpdateException extends RuntimeException {
     return new UpdateException(tripId, this.errorType, this.stopIndex);
   }
 
-  /// The index of the GTFS-RT stop time update or SIRI call in the list of updates, which 
+  /// The index of the GTFS-RT stop time update or SIRI call in the list of updates, which
   /// does not necessarily correspond to the stop position in pattern.
   @Nullable
   public Integer stopIndex() {
