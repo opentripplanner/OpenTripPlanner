d82de9034e15cd3df6ebd152395adeaffc9dfa03
Leonard Ehrenfried <mail@leonard.io>
2026-07-29 07:24:18 +0200

    Move record to test code

--- application/src/main/java/org/opentripplanner/graph_builder/module/osm/model/StreetTraversalPermissionPair.java
+++
@@ -1,8 +0,0 @@
-package org.opentripplanner.graph_builder.module.osm.model;
-
-import org.opentripplanner.street.model.StreetTraversalPermission;
-
-public record StreetTraversalPermissionPair(
-  StreetTraversalPermission main,
-  StreetTraversalPermission back
-) {}
