840bdb0d27bc7afdd8ab4d0587799f387235e4b5
Thomas Gran <t2gran@gmail.com>
2026-08-12 12:51:37 +0200

    refactor: decouple SplitEdgeTurnRestrictionsTest from OtpServerRequestContext
    Same pattern as the other street integration tests: only unwrapped 6
    accessors off TestServerContext.ofGraph(graph) to feed
    DirectStreetRouter.route(...) directly. Build each piece via
    TestServerContext helpers instead.
    
    Verified both tests pass, including the polyline geometry assertions.

--- application/src/test/java/org/opentripplanner/street/integration/SplitEdgeTurnRestrictionsTest.java
+++ application/src/test/java/org/opentripplanner/street/integration/SplitEdgeTurnRestrictionsTest.java
@@ -27,6 +27,8 @@ import org.opentripplanner.street.linking.TemporaryVerticesContainer;
 import org.opentripplanner.street.model.StreetMode;
 import org.opentripplanner.street.search.TraverseMode;
 import org.opentripplanner.test.support.ResourceLoader;
+import org.opentripplanner.transfer.regular.TransferServiceTestFactory;
+import org.opentripplanner.transit.service.TransitRepository;
 
 /*
  * When bus stops are added to graph they split an existing edge in two parts so that an artificial
@@ -175,15 +177,18 @@ public class SplitEdgeTurnRestrictionsTest {
       var linkingContextFactory = new LinkingContextFactory(graph, vertexCreationService);
       var linkingRequest = LinkingContextRequestMapper.map(request);
       var linkingContext = linkingContextFactory.create(temporaryVerticesContainer, linkingRequest);
-      var ctx = TestServerContext.ofGraph(graph);
+      var transitService = TestServerContext.createTransitService(
+        new TransitRepository(),
+        TransferServiceTestFactory.defaultTransferRepository()
+      );
 
       var itineraries = DirectStreetRouter.route(
-        ctx.graph(),
-        ctx.transitService(),
-        ctx.streetLimitationParametersService(),
-        ctx.vehicleRentalService(),
-        ctx.streetDetailsService(),
-        ctx.dataOverlayParameterBindings(),
+        graph,
+        transitService,
+        TestServerContext.createStreetLimitationParametersService(),
+        TestServerContext.createVehicleRentalService(),
+        TestServerContext.createStreetDetailsService(),
+        null,
         request,
         linkingContext
       );
