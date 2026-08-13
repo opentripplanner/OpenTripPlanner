12fa4340c2f56c33193c4cc5575ac41041732299
Thomas Gran <t2gran@gmail.com>
2026-08-13 20:21:31 +0200

    refactor: remove Transit Editor Service reference from DefaultTransitService doc

--- application/src/main/java/org/opentripplanner/transit/service/DefaultTransitService.java
+++ application/src/main/java/org/opentripplanner/transit/service/DefaultTransitService.java
@@ -71,7 +71,6 @@ import org.opentripplanner.utils.collection.SetUtils;
 import org.opentripplanner.utils.time.ServiceDateUtils;
 
 /**
- * Default implementation of the Transit Service and Transit Editor Service.
  * A new instance of this class should be created for each request.
  * This ensures that the same TimetableRepositorySnapshot is used for the
  * duration of the request (which may involve several method calls).
