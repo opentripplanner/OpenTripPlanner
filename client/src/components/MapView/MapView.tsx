b23810ec1d296be74ad42a3d1c5ad4314c341b41
Leonard Ehrenfried <mail@leonard.io>
2026-08-19 23:30:38 +0200

    Update worker URL

--- client/src/components/MapView/MapView.tsx
+++ client/src/components/MapView/MapView.tsx
@@ -8,7 +8,7 @@ import {
   MapRef,
 } from 'react-map-gl/maplibre';
 import * as maplibregl from 'maplibre-gl';
-import maplibreWorkerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?url';
+import maplibreWorkerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 // maplibre-gl resolves its worker script relative to `import.meta.url` at runtime, which Vite
