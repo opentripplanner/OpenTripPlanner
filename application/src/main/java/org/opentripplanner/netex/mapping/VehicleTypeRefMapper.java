b4fb70891c17567c99eab923ee4599f00fb502a6
Sigurd Totland <sigurdtotland@gmail.com>
2026-08-25 12:36:35 +0200

    docs: Remove the reference to ServiceJourney in VehicleTypeRefMapper

--- application/src/main/java/org/opentripplanner/netex/mapping/VehicleTypeRefMapper.java
+++ application/src/main/java/org/opentripplanner/netex/mapping/VehicleTypeRefMapper.java
@@ -5,7 +5,7 @@ import javax.annotation.Nullable;
 import org.rutebanken.netex.model.VehicleTypeRefStructure;
 
 /**
- * Maps the NeTEx VehicleTypeRef for the type of vehicle planned to operate a service journey.
+ * Maps the NeTEx VehicleTypeRef field from the netex representation to a string.
  */
 class VehicleTypeRefMapper {
 
@@ -13,7 +13,7 @@ class VehicleTypeRefMapper {
   private VehicleTypeRefMapper() {}
 
   /**
-   * Return ref of the vehicle type if it is given.
+   * Return ref of the vehicle type as a string if it is given.
    */
   @Nullable
   static String mapVehicleTypeRef(
