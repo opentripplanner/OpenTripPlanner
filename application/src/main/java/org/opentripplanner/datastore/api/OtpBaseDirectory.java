640af6ae5d1a461da903da6f11fb1b2428283d1f
Thomas Gran <t2gran@gmail.com>
2026-09-08 20:18:49 +0200

    refactor: migrate DI qualifier annotations from javax.inject to jakarta.inject
    Standardizes on jakarta.inject.Qualifier across the codebase, matching the
    other DI annotations already in use.

--- application/src/main/java/org/opentripplanner/datastore/api/OtpBaseDirectory.java
+++ application/src/main/java/org/opentripplanner/datastore/api/OtpBaseDirectory.java
@@ -1,8 +1,8 @@
 package org.opentripplanner.datastore.api;
 
+import jakarta.inject.Qualifier;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-import javax.inject.Qualifier;
 
 /**
  * This qualifier is used to inject the OTP base directory where the configuration files are
