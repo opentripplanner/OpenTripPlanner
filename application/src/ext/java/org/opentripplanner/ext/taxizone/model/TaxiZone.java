cae65c823585c614c282cd75829599da41bd6d28
Ville Pihlava <ville.pihlava@cgi.com>
2026-09-17 14:38:30 +0300

    Use readObject to build preparedGeometry after deserialization.

--- application/src/ext/java/org/opentripplanner/ext/taxizone/model/TaxiZone.java
+++ application/src/ext/java/org/opentripplanner/ext/taxizone/model/TaxiZone.java
@@ -1,5 +1,7 @@
 package org.opentripplanner.ext.taxizone.model;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Objects;
 import javax.annotation.Nullable;
@@ -24,7 +26,7 @@ public final class TaxiZone implements Serializable {
   @Nullable
   private final BookingInfo dropOffBookingInfo;
 
-  private transient volatile PreparedGeometry preparedGeometry;
+  private transient PreparedGeometry preparedGeometry;
 
   public TaxiZone(
     Geometry geometry,
@@ -36,6 +38,7 @@ public final class TaxiZone implements Serializable {
     this.route = Objects.requireNonNull(route);
     this.pickupBookingInfo = pickupBookingInfo;
     this.dropOffBookingInfo = dropOffBookingInfo;
+    this.preparedGeometry = PreparedGeometryFactory.prepare(geometry);
   }
 
   public Geometry geometry() {
@@ -50,7 +53,7 @@ public final class TaxiZone implements Serializable {
    */
   public boolean contains(WgsCoordinate coordinate) {
     Point point = GeometryUtils.getGeometryFactory().createPoint(coordinate.asJtsCoordinate());
-    return preparedGeometry().contains(point);
+    return preparedGeometry.contains(point);
   }
 
   public Route route() {
@@ -99,10 +102,12 @@ public final class TaxiZone implements Serializable {
       .toString();
   }
 
-  private PreparedGeometry preparedGeometry() {
-    if (preparedGeometry == null) {
-      preparedGeometry = PreparedGeometryFactory.prepare(geometry);
-    }
-    return preparedGeometry;
+  /**
+   * Rebuilds the transient {@link #preparedGeometry} cache after deserialization, since
+   * {@link PreparedGeometry} is not {@link Serializable}.
+   */
+  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+    in.defaultReadObject();
+    this.preparedGeometry = PreparedGeometryFactory.prepare(geometry);
   }
 }
